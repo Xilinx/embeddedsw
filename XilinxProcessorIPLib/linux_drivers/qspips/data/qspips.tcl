deddf8a6a652d56cd4664f4a6d6dee474fa7e3e4
Heera Nand <heeran@xilinx.com>
2014-04-11 16:05:25 +0530

    replaced dts. with dtg. in driver meta data
    Wall Data:
      Originally submitted from RDI_heeran_MY_RDI_EXP3
      Build Type: none
      Test Types: pre-commits, gui pre-commits
    [git-p4: depot-paths = "//Rodin/HEAD/data/embeddedsw/": change = 886370]

--- XilinxProcessorIPLib/linux_drivers/qspips/data/qspips.tcl
+++ XilinxProcessorIPLib/linux_drivers/qspips/data/qspips.tcl
@@ -9,7 +9,7 @@ proc generate {drv_handle} {
     set_property CONFIG.is-dual $is_dual $drv_handle
 
     set primary_flash [hsm::utils::add_new_child_node $drv_handle "primary_flash"]
-    hsm::utils::add_new_property $primary_flash "dts.device_type" string "ps7-qspi"
+    hsm::utils::add_new_property $primary_flash "dtg.device_type" string "ps7-qspi"
     hsm::utils::add_new_property $primary_flash reg hexint 0
     hsm::utils::add_new_property $primary_flash spi-max-frequency int 50000000
 }
