e1f5cf58b457f3924af125d2f9c2e5de99a7d792
Durga challa <vnsldurg@xilinx.com>
2014-07-15 19:29:03 +0530

    ccm: Updated ccm_v6_0
    Added source files,ccm_selftest_example,doxygen report.
    
    Signed-off-by: Durga challa <vnsldurg@xilinx.com>
    Acked-by: Srikanth Vemula <svemula@xilinx.com>

--- XilinxProcessorIPLib/drivers/ccm/data/ccm.tcl
+++ XilinxProcessorIPLib/drivers/ccm/data/ccm.tcl
@@ -1,9 +1,9 @@
-###############################################################################
+##############################################################################
 #
 # Copyright (C) 2011 - 2014 Xilinx, Inc.  All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
+# of this software and associated documentation files (the "Software"),to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
@@ -27,11 +27,13 @@
 # Except as contained in this notice, the name of the Xilinx shall not be used
 # in advertising or otherwise to promote the sale, use or other dealings in
 # this Software without prior written authorization from Xilinx.
-#
 ###############################################################################
 
 proc generate {drv_handle} {
-    xdefine_include_file $drv_handle "xparameters.h" "CCM" "C_BASEADDR" "NUM_INSTANCES" "DEVICE_ID" "C_HIGHADDR" "C_S_AXIS_VIDEO_DATA_WIDTH" "C_S_AXIS_VIDEO_FORMAT" "C_M_AXIS_VIDEO_DATA_WIDTH" "C_M_AXIS_VIDEO_FORMAT" "C_ACTIVE_COLS" "C_ACTIVE_ROWS" "C_CLIP" "C_CLAMP"  "C_K11" "C_K12" "C_K13" "C_K21" "C_K22" "C_K23" "C_K31" "C_K32" "C_K33" "C_ROFFSET" "C_GOFFSET" "C_BOFFSET"
+    
+     xdefine_include_file $drv_handle "xparameters.h" "CCM" "C_BASEADDR" "NUM_INSTANCES" "DEVICE_ID" "C_HIGHADDR" "C_S_AXIS_VIDEO_DATA_WIDTH" "C_S_AXIS_VIDEO_FORMAT" "C_M_AXIS_VIDEO_DATA_WIDTH" "C_M_AXIS_VIDEO_FORMAT" "C_ACTIVE_COLS" "C_ACTIVE_ROWS" "C_CLIP" "C_CLAMP"  "C_K11" "C_K12" "C_K13" "C_K21" "C_K22" "C_K23" "C_K31" "C_K32" "C_K33" "C_ROFFSET" "C_GOFFSET" "C_BOFFSET" "C_MAX_COLS" "C_HAS_DEBUG" "C_HAS_INTC_IF" "C_S_AXI_CLK_FREQ_HZ"
 
-    xdefine_canonical_xpars $drv_handle "xparameters.h" "CCM" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" "C_S_AXIS_VIDEO_DATA_WIDTH" "C_S_AXIS_VIDEO_FORMAT" "C_M_AXIS_VIDEO_DATA_WIDTH" "C_M_AXIS_VIDEO_FORMAT" "C_ACTIVE_COLS" "C_ACTIVE_ROWS" "C_CLIP" "C_CLAMP"  "C_K11" "C_K12" "C_K13" "C_K21" "C_K22" "C_K23" "C_K31" "C_K32" "C_K33" "C_ROFFSET" "C_GOFFSET" "C_BOFFSET"
+    xdefine_config_file $drv_handle "xccm_g.c" "XCcm" "DEVICE_ID" "C_BASEADDR" "C_S_AXIS_VIDEO_FORMAT" "C_M_AXIS_VIDEO_FORMAT" "C_MAX_COLS" "C_ACTIVE_COLS" "C_ACTIVE_ROWS" "C_HAS_DEBUG" "C_HAS_INTC_IF" "C_CLIP" "C_CLAMP"  "C_K11" "C_K12" "C_K13" "C_K21" "C_K22" "C_K23" "C_K31" "C_K32" "C_K33" "C_ROFFSET" "C_GOFFSET" "C_BOFFSET" "C_S_AXI_CLK_FREQ_HZ"
+
+    xdefine_canonical_xpars $drv_handle "xparameters.h" "CCM" "C_BASEADDR" "NUM_INSTANCES" "DEVICE_ID" "C_HIGHADDR" "C_S_AXIS_VIDEO_DATA_WIDTH" "C_S_AXIS_VIDEO_FORMAT" "C_M_AXIS_VIDEO_DATA_WIDTH" "C_M_AXIS_VIDEO_FORMAT" "C_ACTIVE_COLS" "C_ACTIVE_ROWS" "C_CLIP" "C_CLAMP"  "C_K11" "C_K12" "C_K13" "C_K21" "C_K22" "C_K23" "C_K31" "C_K32" "C_K33" "C_ROFFSET" "C_GOFFSET" "C_BOFFSET" "C_MAX_COLS" "C_HAS_DEBUG" "C_HAS_INTC_IF" "C_S_AXI_CLK_FREQ_HZ"
 }
---
+++ XilinxProcessorIPLib/drivers/ccm/examples/ccm_selftest_example.c
@@ -0,0 +1,158 @@
+/******************************************************************************
+*
+* (c) Copyright 2014 Xilinx, Inc. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running on a Xilinx device, or
+* (b) that interact with a Xilinx device through a bus or interconnect.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* XILINX CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+/*****************************************************************************/
+/**
+*
+* @file ccm_selftest_example.c
+*
+* This file contains an example using the XCCM driver to do self test
+* on the device.
+*
+* @note
+*
+* None
+*
+* MODIFICATION HISTORY:
+* <pre>
+* Ver   Who     Date     Changes
+* ----- ------- -------- -----------------------------------------------
+* 6.00  adk     03/06/14 First Release.
+*                        Implimented XCcmSelfTestExample function.
+*                        Adherence to Xilinx coding guidelines.
+*
+* </pre>
+******************************************************************************/
+
+/***************************** Include Files *********************************/
+
+#include "xccm.h"
+#include "xparameters.h"
+#include "xil_printf.h"
+
+
+/************************** Constant Definitions *****************************/
+
+/*
+ * The following constants map to the XPAR parameters created in the
+ * xparameters.h file. They are defined here such that a user can easily
+ * change all the needed parameters in one place.
+ */
+#define CCM_DEVICE_ID		XPAR_CCM_0_DEVICE_ID	/**< CCM Device ID */
+
+/**************************** Type Definitions *******************************/
+
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+/************************** Function Prototypes ******************************/
+
+
+int XCcmSelfTestExample(u16 DeviceId);
+
+/************************** Variable Definitions *****************************/
+
+
+XCcm Ccm;		/**<Instance of the CCM Device */
+/*****************************************************************************/
+/**
+*
+* Main function to call the example.
+*
+* @return
+*		- XST_SUCCESS if succesful.
+*		- XST_FAILURE if failed.
+*
+* @note		None.
+*
+******************************************************************************/
+int main(void)
+{
+	int Status;
+
+	/* Run the selftest example */
+	Status = XCcmSelfTestExample((u16)CCM_DEVICE_ID);
+	if (Status != XST_SUCCESS) {
+		xil_printf("CCM Selftest Example Failed\r\n");
+		return XST_FAILURE;
+	}
+
+	xil_printf("Successfully ran CCM Selftest Example\r\n");
+	return XST_SUCCESS;
+}
+
+/*****************************************************************************/
+/**
+*
+* This function does a minimal test on the XCCM driver.
+*
+*
+* @param	DeviceId is the XPAR_<CCM_instance>_DEVICE_ID value from
+*		xparameters.h.
+*
+* @return
+*		- XST_SUCCESS if succesful.
+*		- XST_FAILURE if failed.
+*
+* @note		None.
+*
+******************************************************************************/
+int XCcmSelfTestExample(u16 DeviceId)
+{
+	int Status;
+	XCcm_Config *Config;
+
+	/*
+	 * Initialize the CCM driver so that it's ready to use
+	 * look up the configuration in the config table,
+	 * then initialize it.
+	 */
+	Config = XCcm_LookupConfig(DeviceId);
+	if (NULL == Config) {
+		return XST_FAILURE;
+	}
+
+	Status = XCcm_CfgInitialize(&Ccm, Config, Config->BaseAddress);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Performs the self-test to check hardware build.
+	 */
+
+	Status = XCcm_SelfTest(&Ccm);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	return XST_SUCCESS;
+}
