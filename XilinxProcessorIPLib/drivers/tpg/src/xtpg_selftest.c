a5b11586d5ea64a574fb0d37b84a989c6da39628
Durga challa <vnsldurg@xilinx.com>
2014-07-15 12:12:18 +0530

    tpg: Added files to tpg.
    Added source files, intgration files and selftest example.
    
    Signed-off-by: Jagannadha Sutradharudu Teki <jaganna@xilinx.com>
    Signed-off-by: Durga challa <vnsldurg@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/tpg/src/xtpg_selftest.c
@@ -0,0 +1,109 @@
+/******************************************************************************
+*
+* Copyright (C) 2014 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running on a Xilinx device, or
+* (b) that interact with a Xilinx device through a bus or interconnect.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* XILINX CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+/*****************************************************************************/
+/**
+*
+* @file xtpg_selftest.c
+*
+* This file contains the self-test functions for the TPG driver.
+* The self test function reads the Version Register.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who    Date     Changes
+* ----- -----  ------- ----------------------------------------------
+* 3.0   adk   02/19/14 First Release.
+*                      Implemented XTpg_SelfTest function.
+* </pre>
+*
+******************************************************************************/
+
+/***************************** Include Files *********************************/
+
+#include "xtpg.h"
+#include "xstatus.h"
+
+/************************** Constant Definitions *****************************/
+
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+
+/**************************** Type Definitions *******************************/
+
+
+/************************** Function Prototypes ******************************/
+
+
+/************************** Variable Definitions *****************************/
+
+
+/************************** Function Definitions *****************************/
+
+/*****************************************************************************/
+/**
+*
+* This function reads Version register of TPG core and compares with zero
+* as part of self test.
+*
+* @param	InstancePtr is a pointer to the TPG instance.
+*
+* @return
+*		- XST_SUCCESS if the Version register read test was successful.
+*		- XST_FAILURE if the Version register read test failed.
+*
+* @note		None.
+*
+******************************************************************************/
+int XTpg_SelfTest(XTpg *InstancePtr)
+{
+	u32 Version;
+	int Status;
+
+	/* Verify arguments. */
+	Xil_AssertNonvoid(InstancePtr != NULL);
+
+	/* Read TPG core version register. */
+	Version = XTpg_ReadReg(InstancePtr->Config.BaseAddress,
+				(XTPG_VERSION_OFFSET));
+
+	/* Compare version with zero. */
+	if(Version != (u32)0x0) {
+		Status = (XST_SUCCESS);
+	}
+	else {
+		Status = (XST_FAILURE);
+	}
+
+	return Status;
+}
