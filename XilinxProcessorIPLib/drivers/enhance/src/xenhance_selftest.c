3e69599b57a1193742df950b5b5c726a6acdabf5
Shravan Kumar A <skumara@xilinx.com>
2014-07-15 12:16:55 +0530

    enhance: Added files to enhance driver.
    Added source files, integration files, self test example,
    mdd and tcl files to enhance driver.
    
    Signed-off-by: Shravan Kumar A <skumara@xilinx.com>
    Signed-off-by: Jagannadha Sutradharudu Teki <jaganna@xilinx.com>
    Acked-by: Srikanth Vemula <svemula@xilinx.com>

--- XilinxProcessorIPLib/drivers/enhance/src/xenhance_selftest.c
+++ XilinxProcessorIPLib/drivers/enhance/src/xenhance_selftest.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2009 - 2014 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2014 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -32,32 +32,80 @@
 /*****************************************************************************/
 /**
 *
-* @file enhance.c
+* @file xenhance_selftest.c
 *
-* This is main code of Xilinx Image Enhancement (ENHANCE)
-* device driver. Please see enhance.h for more details of the driver.
+* This file contains the self-test functions for the Enhance driver.
+* The self test function reads the Version register.
 *
+* <pre>
 * MODIFICATION HISTORY:
 *
 * Ver   Who  Date     Changes
-* ----- ---- -------- -------------------------------------------------------
-* 5.00a vyc  06/19/13  Updated for ENHANCE V8.0
-* 4.00a vyc  04/24/12  Updated for ENHANCE V4.00.a
-* 3.00a rc   09/11/11  Updated for ENHANCE V3.0
-* 2.00a vc   12/14/10  Updated for ENHANCE V2.0
+* ----- ---- -------- -----------------------------------------------
+* 7.0   adk  19/02/14 First Release.
+*                     Implemented the following function
+*                     XEnhance_SelfTest
+* </pre>
 *
 ******************************************************************************/
 
 /***************************** Include Files *********************************/
 
-#include "enhance.h"
-#include "xenv.h"
+#include "xenhance.h"
+#include "xstatus.h"
+
+/************************** Constant Definitions *****************************/
 
 
+/***************** Macros (Inline Functions) Definitions *********************/
+
+
+/**************************** Type Definitions *******************************/
+
+
+/************************** Function Prototypes ******************************/
+
+
+/************************** Variable Definitions *****************************/
+
+
+/************************** Function Definitions *****************************/
 
 /*****************************************************************************/
-// Note: All functions are currently implemented as high-performance macros
-// within enhance.h
-/*****************************************************************************/
+/**
+*
+* This function reads complete Version register of Enhance core and compares
+* with zero values as part of self test.
+*
+* @param	InstancePtr is a pointer to the XEnhance instance to be worked
+*		on.
+*
+* @return
+*		- XST_SUCCESS if the test was successful.
+*		- XST_FAILURE if the test failed.
+*
+* @note		None.
+*
+******************************************************************************/
+int XEnhance_SelfTest(XEnhance *InstancePtr)
+{
+	u32 Version;
+	int Status;
 
+	/* Verify arguments. */
+	Xil_AssertNonvoid(InstancePtr != NULL);
 
+	/* Read Enhance core version register. */
+	Version = XEnhance_ReadReg((InstancePtr)->Config.BaseAddress,
+			(XENH_VERSION_OFFSET));
+
+	/* Compare Version with non-zero */
+	if(Version != (u32)0x00) {
+		Status = (XST_SUCCESS);
+	}
+	else {
+		Status = (XST_FAILURE);
+	}
+
+	return Status;
+}
---
+++ XilinxProcessorIPLib/drivers/enhance/src/xenhance_sinit.c
@@ -0,0 +1,110 @@
+/******************************************************************************
+*
+* Copyright (C) 2014 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running on a Xilinx device, or
+* (b) that interact with a Xilinx device through a bus or interconnect.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* XILINX CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+/*****************************************************************************/
+/**
+*
+* @file xenhance_sinit.c
+*
+* This file contains static initialization methods for Xilinx Enhance core.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who  Date     Changes
+* ----- ---- -------- ------------------------------------------------------
+* 7.0   adk  02/19/14 First release.
+*                     Implemented the following function
+*                     XEnhance_LookupConfig
+* </pre>
+*
+******************************************************************************/
+
+/***************************** Include Files *********************************/
+
+#include "xenhance.h"
+#include "xparameters.h"
+
+/************************** Constant Definitions *****************************/
+
+
+/**************************** Type Definitions *******************************/
+
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+
+/************************** Function Prototypes ******************************/
+
+
+/************************** Variable Declaration *****************************/
+
+
+/************************** Function Definitions *****************************/
+
+/*****************************************************************************/
+/**
+*
+* This function returns a reference to an XEnhance_Config structure
+* based on the unique device id, <i>DeviceId</i>. The return value will refer
+* to an entry in the device configuration table defined in the xenhance_g.c.
+* file.
+*
+* @param	DeviceId is the unique device ID of the device for the lookup
+*		operation.
+*
+* @return	XEnhance_LookupConfig returns a reference to a config record in
+*		the configuration table (in xenhance_g.c) corresponding to
+*		<i>DeviceId</i>, or NULL if no match is found.
+*
+* @note		None.
+*
+******************************************************************************/
+XEnhance_Config *XEnhance_LookupConfig(u16 DeviceId)
+{
+	extern XEnhance_Config
+			XEnhance_ConfigTable[XPAR_ENHANCE_NUM_INSTANCES];
+	XEnhance_Config *CfgPtr = NULL;
+	u32 Index;
+
+	/* To get the reference pointer to XEnhance_Config structure */
+	for (Index = (u32)0x0; Index < (u32)(XPAR_ENHANCE_NUM_INSTANCES);
+								Index++) {
+
+		/* Compare device Id with configTable's device Id */
+		if (XEnhance_ConfigTable[Index].DeviceId == DeviceId) {
+			CfgPtr = &XEnhance_ConfigTable[Index];
+			break;
+		}
+	}
+
+	return (XEnhance_Config *)CfgPtr;
+}
