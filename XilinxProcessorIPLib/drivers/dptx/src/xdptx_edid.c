2133642349af67a83a6d62f6a8bfc0aea88c7b58
Andrei-Liviu Simion <andrei.simion@xilinx.com>
2014-11-04 11:33:42 -0800

    dptx: Added EDID utility file and functions for fetching an EDID block.
    Created a file containing utility functions related to the Extended Display
    Identification Information (EDID).
    The initial functions included are used to fetch EDID blocks.
    
    Block numbering starts with 0 being the base EDID block, and extension block
    starting at 1.
    
    EDID blocks are 128 bytes in size.
    
    Signed-off-by: Andrei-Liviu Simion <andrei.simion@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/dptx/src/xdptx_edid.c
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ *
+ * Copyright (C) 2014 Xilinx, Inc.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Use of the Software is limited solely to applications:
+ * (a) running on a Xilinx device, or
+ * (b) that interact with a Xilinx device through a bus or interconnect.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * XILINX CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Except as contained in this notice, the name of the Xilinx shall not be used
+ * in advertising or otherwise to promote the sale, use or other dealings in
+ * this Software without prior written authorization from Xilinx.
+ *
+*******************************************************************************/
+/******************************************************************************/
+/**
+ *
+ * @file xdptx_edid.c
+ *
+ * This file contains functions related to accessing the Extended Display
+ * Identification Data (EDID) of a specified sink using the XDptx driver.
+ *
+ * @note	None.
+ *
+ * <pre>
+ * MODIFICATION HISTORY:
+ *
+ * Ver   Who  Date     Changes
+ * ----- ---- -------- -----------------------------------------------
+ * 3.0   als  11/04/14 Initial release.
+ * </pre>
+ *
+*******************************************************************************/
+
+/******************************* Include Files ********************************/
+#include "xstatus.h"
+#include "xdptx.h"
+
+/**************************** Function Definitions ****************************/
+
+u32 XDptx_GetEdidBlock(XDptx *InstancePtr, u8 *Data, u8 BlockNum)
+{
+	u32 Status;
+	u16 Offset;
+
+	/* Calculate the I2C offset for the specified EDID block. */
+	Offset = BlockNum * 128;
+
+	/* Issue the I2C read for the specified EDID block. */
+	Status = XDptx_IicRead(InstancePtr, XDPTX_EDID_ADDR, Offset, 128, Data);
+
+	return Status;
+}
+
+u32 XDptx_GetRemoteEdidBlock(XDptx *InstancePtr, u8 *Data, u8 BlockNum,
+					u8 LinkCountTotal, u8 *RelativeAddress)
+{
+	u32 Status;
+	u16 Offset;
+
+	/* Calculate the I2C offset for the specified EDID block. */
+	Offset = BlockNum * 128;
+
+	/* Issue the I2C read for the specified EDID block. */
+	Status = XDptx_RemoteIicRead(InstancePtr, LinkCountTotal,
+		RelativeAddress, XDPTX_EDID_ADDR, Offset, 128, Data);
+
+	return Status;
+}
