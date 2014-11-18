4be8a028031cb23ee54ded56e975c3f69fa92762
Rohit Consul <rohit.consul@xilinx.com>
2014-11-18 12:29:40 -0800

    video_common: Added video timings.
    Signed-off-by: Rohit Consul <rohit.consul@xilinx.com>

--- XilinxProcessorIPLib/drivers/video_common/src/xvid.h
+++ XilinxProcessorIPLib/drivers/video_common/src/xvid.h
@@ -157,6 +157,42 @@ typedef enum {
 	XVID_VM_LAST = XVID_VM_USE_EDID_PREFERRED
 } XVid_VideoMode;
 
+typedef enum {
+    XV_RATE_24HZ = 0,
+    XV_RATE_25HZ,
+    XV_RATE_30HZ,
+    XV_RATE_50HZ,
+    XV_RATE_60HZ,
+    XV_NUM_SUPPORTED_RATE
+}FrameRate_t;
+
+typedef enum {
+	//Video
+    XV_576I25 = 0,
+    XV_480I30,
+    XV_1080I25,
+    XV_1080I30,
+	XV_480P60,
+    XV_720P50,
+    XV_720P60,
+    XV_1080P24,
+    XV_1080P25,
+    XV_1080P30,
+    XV_1080P50,
+    XV_1080P60,
+    XV_4K2KP24,
+    XV_4K2KP25,
+    XV_4K2KP30,
+    XV_4K2KP60,
+    //PC Resolutions
+    XV_WXGAP60,
+    XV_UXGAP60,
+    XV_WUXGAP60,
+    XV_WSXGAP60,
+    XV_NUM_SUPPORTED_MODES,  //Add new modes before this index
+    XV_NO_INPUT
+}Resolution_t;
+
 /**
  * This typedef contains the display monitor timing attributes for a video mode.
  */
@@ -189,9 +225,39 @@ typedef struct {
 	u32 VBackPorch;			/**< Vertical back porch (in lines). */
 } XVid_DmtMode;
 
+/**
+ * Video Timing Structure
+ */
+typedef struct {
+    u32 HActive;
+    u32 HFrontPorch;
+    u32 HSyncWidth;
+    u32 HBackPorch;
+    u32 HTotal;
+    u32 HSyncPolarity;
+    u32 VActive;
+    u32 F0PVFrontPorch;
+    u32 F0PVSyncWidth;
+    u32 F0PVBackPorch;
+    u32 F0PVTotal;
+    u32 F1VFrontPorch;
+    u32 F1VSyncWidth;
+    u32 F1VBackPorch;
+    u32 F1VTotal;
+    u32 VSyncPolarity;
+}VideoTiming_t;
+
+typedef struct {
+	Resolution_t  ResId;
+	char          *Name;
+    FrameRate_t   FrameRate;
+    VideoTiming_t Timing;
+}ModeTable_t;
+
 /*************************** Variable Declarations ****************************/
 
 extern const XVid_DmtMode XVid_DmtModes[];
+extern const ModeTable_t ModeTable[XV_NUM_SUPPORTED_MODES];
 
 /**************************** Function Prototypes *****************************/
 
