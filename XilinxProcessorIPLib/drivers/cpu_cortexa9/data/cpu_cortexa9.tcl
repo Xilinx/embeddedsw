074b430c2589e60945f6531299ff0008f9b5b76c
Kishore Kumar Korathaluri <kkorath@xilinx.com>
2014-12-23 11:01:17 +0530

    cpu_cortexa9 : Modified namespace from HSM to HSI.
    Signed-off-by: Kishore Kumar Korathaluri <kkorath@xilinx.com>

--- XilinxProcessorIPLib/drivers/cpu_cortexa9/data/cpu_cortexa9.tcl
+++ XilinxProcessorIPLib/drivers/cpu_cortexa9/data/cpu_cortexa9.tcl
@@ -69,11 +69,11 @@ proc xdefine_cortexa9_params {drvhandle} {
 	    	file delete -force test.a
     	}   
     }
-    set periphs [::hsm::utils::get_common_driver_ips $drvhandle]
+    set periphs [::hsi::utils::get_common_driver_ips $drvhandle]
     set lprocs [get_cells -filter "IP_NAME==ps7_cortexa9"]
     set lprocs [lsort $lprocs]
 
-    set config_inc [::hsm::utils::open_include_file "xparameters.h"]
+    set config_inc [::hsi::utils::open_include_file "xparameters.h"]
     puts $config_inc "/* Definition for CPU ID */"
 
     foreach periph $periphs {
@@ -99,7 +99,7 @@ proc xdefine_addr_params_for_ext_intf {drvhandle file_name} {
     set hw_proc_handle [get_cells [get_property HW_INSTANCE $sw_proc_handle ]]
     
  # Open include file
-   set file_handle [::hsm::utils::open_include_file $file_name]
+   set file_handle [::hsi::utils::open_include_file $file_name]
 
    set mem_ranges [get_mem_ranges -of_objects $hw_proc_handle] 
    foreach mem_range $mem_ranges {
@@ -124,7 +124,7 @@ proc xdefine_addr_params_for_ext_intf {drvhandle file_name} {
            
 
            if {$bparam_value != ""} {
-               set value [::hsm::utils::format_addr_string $bparam_value $bparam_name]
+               set value [::hsi::utils::format_addr_string $bparam_value $bparam_name]
                    set param [string toupper $bparam_name]
                    if {[string match C_* $param]} {
                        set name [format "%s%s" $name [string range $param 2 end]]
@@ -138,7 +138,7 @@ proc xdefine_addr_params_for_ext_intf {drvhandle file_name} {
 	   set name [string toupper [get_property NAME $mem_range]]
            set name [format "XPAR_%s_" $name]
            if {$hparam_value != ""} {
-               set value [::hsm::utils::format_addr_string $hparam_value $hparam_name]
+               set value [::hsi::utils::format_addr_string $hparam_value $hparam_name]
                 set param [string toupper $hparam_name]
                    if {[string match C_* $param]} {
                        set name [format "%s%s" $name [string range $param 2 end]]
