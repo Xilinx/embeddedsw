7e543c96d9fff8fb60adc6eda7aad2672db0a780
Kishore Kumar Korathaluri <kkorath@xilinx.com>
2015-01-05 15:12:43 +0530

    wdtps : added namespace for all HSI TCL commands.
    Signed-off-by: Kishore Kumar Korathaluri <kkorath@xilinx.com>

--- XilinxProcessorIPLib/drivers/wdtps/data/wdtps_tapp.tcl
+++ XilinxProcessorIPLib/drivers/wdtps/data/wdtps_tapp.tcl
@@ -89,9 +89,9 @@ proc gen_testfunc_call {swproj mhsinst} {
         return ""
     }
 
-    set ipname [get_property NAME $mhsinst] 
+    set ipname [get_property NAME $mhsinst]
     set deviceid [hsm::utils::get_ip_param_name $mhsinst "DEVICE_ID"]
-    set stdout [get_property CONFIG.STDOUT [get_os]]
+    set stdout [get_property CONFIG.STDOUT [hsi::get_os]]
     if { $stdout == "" || $stdout == "none" } {
        set hasStdout 0
     } else {
