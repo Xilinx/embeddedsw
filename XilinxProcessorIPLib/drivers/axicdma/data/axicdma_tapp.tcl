b02a80d05578c911386001291b705b3ffba48e58
Kishore Kumar Korathaluri <kkorath@xilinx.com>
2015-01-05 14:42:01 +0530

    axicdma : added namespace for all HSI tcl commands.
    Signed-off-by: Kishore Kumar Korathaluri <kkorath@xilinx.com>

--- XilinxProcessorIPLib/drivers/axicdma/data/axicdma_tapp.tcl
+++ XilinxProcessorIPLib/drivers/axicdma/data/axicdma_tapp.tcl
@@ -170,7 +170,7 @@ proc gen_testfunc_call {swproj mhsinst} {
   set axicdmaintr [::hsi::utils::is_ip_interrupting_current_proc $mhsinst]
   set ipname [get_property NAME  $mhsinst] 
   set deviceid [::hsi::utils::get_ip_param_name $mhsinst "DEVICE_ID"]
-  set stdout [get_property CONFIG.STDOUT [get_os]]
+  set stdout [get_property CONFIG.STDOUT [hsi::get_os]]
     if { $stdout == "" || $stdout == "none" } {
        set hasStdout 0
     } else {
@@ -179,10 +179,10 @@ proc gen_testfunc_call {swproj mhsinst} {
   set sg [get_property CONFIG.C_INCLUDE_SG $mhsinst]
   
   if {$axicdmaintr == 1} {
-      set intr_pin_name [get_pins -of_objects [get_cells $ipname]  -filter "TYPE==INTERRUPT"]
+      set intr_pin_name [hsi::get_pins -of_objects [hsi::get_cells $ipname]  -filter "TYPE==INTERRUPT"]
       set intcname [::hsi::utils::get_connected_intr_cntrl $ipname  $intr_pin_name]
       set intcvar intc
-      set proc [get_property IP_NAME [get_cells [get_sw_processor]]]
+      set proc [get_property IP_NAME [hsi::get_cells [hsi::get_sw_processor]]]
   }
     
   set testfunc_call ""
@@ -355,7 +355,7 @@ proc gen_testfunc_call {swproj mhsinst} {
 }
 
 proc check_if_ddr_is_present {mhsinst} {
-     set ips [get_cells $mhsinst "*"]
+     set ips [hsi::get_cells $mhsinst "*"]
      set ddrpresent -1
      set migddrpresent -1
 
