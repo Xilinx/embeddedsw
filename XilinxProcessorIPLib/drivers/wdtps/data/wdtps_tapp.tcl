81834a573629f108e5ffa8e4a9756d65896fbaf6
Kishore Kumar Korathaluri <kkorath@xilinx.com>
2015-01-12 09:35:56 +0530

    wdtps : added common namespace to all common tcl commands.
    Signed-off-by: Kishore Kumar Korathaluri <kkorath@xilinx.com>

--- XilinxProcessorIPLib/drivers/wdtps/data/wdtps_tapp.tcl
+++ XilinxProcessorIPLib/drivers/wdtps/data/wdtps_tapp.tcl
@@ -89,9 +89,9 @@ proc gen_testfunc_call {swproj mhsinst} {
         return ""
     }
 
-    set ipname [get_property NAME $mhsinst]
+    set ipname [common::get_property NAME $mhsinst]
     set deviceid [hsm::utils::get_ip_param_name $mhsinst "DEVICE_ID"]
-    set stdout [get_property CONFIG.STDOUT [hsi::get_os]]
+    set stdout [common::get_property CONFIG.STDOUT [hsi::get_os]]
     if { $stdout == "" || $stdout == "none" } {
        set hasStdout 0
     } else {
