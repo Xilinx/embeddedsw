4717af7c5823f947a14a213df000b774168cf216
Kishore Kumar Korathaluri <kkorath@xilinx.com>
2015-01-12 09:42:25 +0530

    rsa_auth_app : added common namespace to all common tcl commands.
    Signed-off-by: Kishore Kumar Korathaluri <kkorath@xilinx.com>

--- lib/sw_apps/rsa_auth_app/data/rsa_auth_app.tcl
+++ lib/sw_apps/rsa_auth_app/data/rsa_auth_app.tcl
@@ -11,14 +11,14 @@ proc get_stdout {} {
     if { $os == "" } {
         error "No Operating System specified in the Board Support Package.";
     }
-    set stdout [get_property CONFIG.STDOUT $os];
+    set stdout [common::get_property CONFIG.STDOUT $os];
     return $stdout;
 }
 
 proc check_stdout_hw {} {
-	set slaves [get_property SLAVES [hsi::get_cells [hsi::get_sw_processor]]]
+	set slaves [common::get_property SLAVES [hsi::get_cells [hsi::get_sw_processor]]]
 	foreach slave $slaves {
-		set slave_type [get_property IP_NAME [hsi::get_cells $slave]];
+		set slave_type [common::get_property IP_NAME [hsi::get_cells $slave]];
 		# Check for MDM-Uart peripheral. The MDM would be listed as a peripheral
 		# only if it has a UART interface. So no further check is required
 		if { $slave_type == "ps7_uart" || $slave_type == "axi_uartlite" ||
@@ -42,9 +42,9 @@ proc swapp_is_supported_hw {} {
 
     # check processor type
     set proc_instance [hsi::get_sw_processor];
-    set hw_processor [get_property HW_INSTANCE $proc_instance]
+    set hw_processor [common::get_property HW_INSTANCE $proc_instance]
 
-    set proc_type [get_property IP_NAME [hsi::get_cells $hw_processor]];
+    set proc_type [common::get_property IP_NAME [hsi::get_cells $hw_processor]];
     
     if { $proc_type != "ps7_cortexa9" } {
                 error "This application is supported only for CortexA9 processors.";
