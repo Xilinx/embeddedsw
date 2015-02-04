a850d9dcc2cb06104d4a19263feec8a70157b48b
Kishore Kumar Korathaluri <kkorath@xilinx.com>
2015-02-04 12:33:04 +0530

    gamma : added hsi namespace to xdefine procs.
    Signed-off-by: Kishore Kumar Korathaluri <kkorath@xilinx.com>

--- XilinxProcessorIPLib/drivers/gamma/data/gamma.tcl
+++ XilinxProcessorIPLib/drivers/gamma/data/gamma.tcl
@@ -31,7 +31,7 @@
 ###############################################################################
 
 proc generate {drv_handle} {
-    xdefine_include_file $drv_handle "xparameters.h" "GAMMA" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" "C_S_AXIS_VIDEO_DATA_WIDTH" "C_S_AXIS_VIDEO_FORMAT" "C_M_AXIS_VIDEO_DATA_WIDTH" "C_M_AXIS_VIDEO_FORMAT" "C_M_AXIS_VIDEO_FORMAT" "C_ACTIVE_COLS" "C_ACTIVE_ROWS" "C_INTPOL" "C_LUTS" "C_DBL_BUF"
+    ::hsi::utils::define_include_file $drv_handle "xparameters.h" "GAMMA" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" "C_S_AXIS_VIDEO_DATA_WIDTH" "C_S_AXIS_VIDEO_FORMAT" "C_M_AXIS_VIDEO_DATA_WIDTH" "C_M_AXIS_VIDEO_FORMAT" "C_M_AXIS_VIDEO_FORMAT" "C_ACTIVE_COLS" "C_ACTIVE_ROWS" "C_INTPOL" "C_LUTS" "C_DBL_BUF"
 
-    xdefine_canonical_xpars $drv_handle "xparameters.h" "GAMMA" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" "C_S_AXIS_VIDEO_DATA_WIDTH" "C_S_AXIS_VIDEO_FORMAT" "C_M_AXIS_VIDEO_DATA_WIDTH" "C_M_AXIS_VIDEO_FORMAT" "C_M_AXIS_VIDEO_FORMAT" "C_ACTIVE_COLS" "C_ACTIVE_ROWS" "C_INTPOL" "C_LUTS" "C_DBL_BUF"
+    ::hsi::utils::define_canonical_xpars $drv_handle "xparameters.h" "GAMMA" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" "C_S_AXIS_VIDEO_DATA_WIDTH" "C_S_AXIS_VIDEO_FORMAT" "C_M_AXIS_VIDEO_DATA_WIDTH" "C_M_AXIS_VIDEO_FORMAT" "C_M_AXIS_VIDEO_FORMAT" "C_ACTIVE_COLS" "C_ACTIVE_ROWS" "C_INTPOL" "C_LUTS" "C_DBL_BUF"
 }
