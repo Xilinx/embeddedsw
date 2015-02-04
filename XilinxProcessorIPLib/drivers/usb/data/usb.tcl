5cdb2444fbd60dbbfb725e6ac83270a5cc3b8591
Kishore Kumar Korathaluri <kkorath@xilinx.com>
2015-02-04 15:32:35 +0530

    usb : added hsi namespace to xdefine procs.
    Signed-off-by: Kishore Kumar Korathaluri <kkorath@xilinx.com>

--- XilinxProcessorIPLib/drivers/usb/data/usb.tcl
+++ XilinxProcessorIPLib/drivers/usb/data/usb.tcl
@@ -35,13 +35,13 @@
 # 5.0      adk    12/10/13 Updated as per the New Tcl API's
 ##############################################################################
 ## @BEGIN_CHANGELOG EDK_M
-##  Removed the local xdefine_canonical_xpars API as there is 
+##  Removed the local ::hsi::utils::define_canonical_xpars API as there is
 ##  a common API in the tcl of the tools 
 ##
 ## @END_CHANGELOG
 
 proc generate {drv_handle} {
-    xdefine_include_file $drv_handle "xparameters.h" "XUsb" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_INCLUDE_DMA"
-    xdefine_config_file $drv_handle "xusb_g.c" "XUsb"  "DEVICE_ID" "C_BASEADDR" "C_INCLUDE_DMA"
-    xdefine_canonical_xpars $drv_handle "xparameters.h" "Usb" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" "C_INCLUDE_DMA"
+    ::hsi::utils::define_include_file $drv_handle "xparameters.h" "XUsb" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_INCLUDE_DMA"
+    ::hsi::utils::define_config_file $drv_handle "xusb_g.c" "XUsb"  "DEVICE_ID" "C_BASEADDR" "C_INCLUDE_DMA"
+    ::hsi::utils::define_canonical_xpars $drv_handle "xparameters.h" "Usb" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" "C_INCLUDE_DMA"
 }
