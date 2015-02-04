450bc511a25208f2f5d70af7782657e9949e7c10
Kishore Kumar Korathaluri <kkorath@xilinx.com>
2015-02-04 11:55:40 +0530

    axipcie : added hsi namespace to xdefine procs.
    Signed-off-by: Kishore Kumar Korathaluri <kkorath@xilinx.com>

--- XilinxProcessorIPLib/drivers/axipcie/data/axipcie.tcl
+++ XilinxProcessorIPLib/drivers/axipcie/data/axipcie.tcl
@@ -88,7 +88,7 @@ proc generate {drv_handle} {
 	"C_PCIEBAR2AXIBAR_3"\
 	"C_INCLUDE_RC" 
   
-        xdefine_config_file $drv_handle "xaxipcie_g.c" "XAxiPcie" \
+        ::hsi::utils::define_config_file $drv_handle "xaxipcie_g.c" "XAxiPcie" \
         "DEVICE_ID" \
         "C_BASEADDR" \
         "C_AXIBAR_NUM" \
