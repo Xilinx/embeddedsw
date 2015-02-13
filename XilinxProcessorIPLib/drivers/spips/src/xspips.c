48a29fce5ae5eac02e3c9297e427d0204c837e1d
Venkata Naga Sai Krishna Kolapalli <venkata.naga.sai.krishna.kolapalli@xilinx.com>
2015-02-13 16:50:08 +0530

    spips_v3_0 : Modification history and Copyright year updated.
    This patch updates the modification history and
    changes copyright year to 2015.
    
    Signed-off-by: Venkata Naga Sai Krishna Kolapalli <venkatan@xilinx.com>

--- XilinxProcessorIPLib/drivers/spips/src/xspips.c
+++ XilinxProcessorIPLib/drivers/spips/src/xspips.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2010 - 2014 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2015 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -63,6 +63,7 @@
 *                       Added masking ConfigReg before writing in SetSlaveSel
 *                       Added extended slave select support - CR#722569.
 *                       Added check for MODF in polled transfer function.
+* 3.00  kvn    02/13/15 Modified code for MISRA-C:2012 compliance.
 *
 * </pre>
 *
