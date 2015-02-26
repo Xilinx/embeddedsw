857653a245c024def304f171f29b7a0cdf5ce9b1
P L Sai Krishna <lakshmi.sai.krishna.potthuri@xilinx.com>
2015-02-26 20:28:26 +0530

    scutimer: Updated the modification history and copyright.
    This patch updates the modification history and copyright year to 2015.
    
    Signed-off-by: P L Sai Krishna <lakshmis@xilinx.com>

--- XilinxProcessorIPLib/drivers/scutimer/src/xscutimer.h
+++ XilinxProcessorIPLib/drivers/scutimer/src/xscutimer.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2010 - 2014 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2015 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -99,6 +99,7 @@
 *		     when the xstatus.h in the common driver overwrites
 *		     the xstatus.h of the standalone BSP during the
 *		     libgen.
+* 2.1 	sk  02/26/15 Modified the code for MISRA-C:2012 compliance.
 * </pre>
 *
 ******************************************************************************/
