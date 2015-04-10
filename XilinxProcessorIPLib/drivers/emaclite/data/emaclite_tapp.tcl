8bbf41c284b69da985532c10053aa76abe7bce3e
Nava kishore Manne <navam@xilinx.com>
2015-04-10 11:15:57 +0530

    emaclite : added common namespace to all common tcl commands.
    Signed-off-by: Nava kishore Manne <navam@xilinx.com>

--- XilinxProcessorIPLib/drivers/emaclite/data/emaclite_tapp.tcl
+++ XilinxProcessorIPLib/drivers/emaclite/data/emaclite_tapp.tcl
@@ -105,7 +105,7 @@ proc gen_init_code {swproj mhsinst} {
     }
     if {$swproj == 1} {
         
-      set ipname [get_property NAME $mhsinst]
+      set ipname [common::get_property NAME $mhsinst]
       set ifemacliteintr [::hsi::utils::is_ip_interrupting_current_proc $mhsinst]
       if {$ifemacliteintr == 1} {
           set decl "   static XEmacLite ${ipname}_EmacLite;"
@@ -125,9 +125,9 @@ proc gen_testfunc_call {swproj mhsinst} {
   }
 
   set ifemacliteintr [::hsi::utils::is_ip_interrupting_current_proc $mhsinst]
-  set ipname [get_property NAME $mhsinst] 
+  set ipname [common::get_property NAME $mhsinst]
   set deviceid [::hsi::utils::get_ip_param_name $mhsinst "DEVICE_ID"]
-  set stdout [get_property CONFIG.STDOUT [get_os]]
+  set stdout [common::get_property CONFIG.STDOUT [hsi::get_os]]
   if { $stdout == "" || $stdout == "none" } {
        set hasStdout 0
   } else {
@@ -136,10 +136,10 @@ proc gen_testfunc_call {swproj mhsinst} {
 
 
   if {$ifemacliteintr == 1} {
-      set intr_pin_name [get_pins -of_objects [get_cells $ipname]  -filter "TYPE==INTERRUPT"]
+      set intr_pin_name [hsi::get_pins -of_objects [hsi::get_cells $ipname]  -filter "TYPE==INTERRUPT"]
       set intcname [::hsi::utils::get_connected_intr_cntrl $ipname  $intr_pin_name]
       set intcvar intc
-      set proc [get_property IP_NAME [get_cells [get_sw_processor]]]
+      set proc [common::get_property IP_NAME [hsi::get_cells [hsi::get_sw_processor]]]
   }
   
   set testfunc_call ""
