1de32ec5064f97270fe5f572c272a71b30a85188
Rohit Consul <rohit.consul@xilinx.com>
2015-05-27 08:11:29 +0800

    v_vscaler: Added new driver
    HLS generated driver along with manually written layer 2. Driver
    tcl update is pending
    
    Signed-off-by: Rohit Consul <rohit.consul@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/v_vscaler/src/xv_vscaler_l2.c
@@ -0,0 +1,494 @@
+/******************************************************************************
+ *
+ * Copyright (C) 2015 Xilinx, Inc.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Use of the Software is limited solely to applications:
+ * (a) running on a Xilinx device, or
+ * (b) that interact with a Xilinx device through a bus or interconnect.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * XILINX CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Except as contained in this notice, the name of the Xilinx shall not be used
+ * in advertising or otherwise to promote the sale, use or other dealings in
+ * this Software without prior written authorization from Xilinx.
+ *
+******************************************************************************/
+/*****************************************************************************/
+/**
+*
+* @file xv_vscaler_l2.c
+*
+* The Vertical Scaler Layer-2 Driver.
+* The functions in this file provides an abstraction from the register peek/poke
+* methodology by implementing most common use-case provided by the sub-core.
+* See xv_vscaler_l2.h for a detailed description of the layer-2 driver
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who    Date     Changes
+* ----- ---- -------- -------------------------------------------------------
+* 1.00  rc   05/01/15   Initial Release
+
+* </pre>
+*
+******************************************************************************/
+
+/***************************** Include Files *********************************/
+#include <math.h>
+#include "xv_vscaler_l2.h"
+
+/************************** Constant Definitions *****************************/
+#define PI                     (3.14159265358979)
+#define STEP_PRECISION         (65536)  // 2^16
+#define COEFF_PRECISION        (4096)   // 2^12
+#define COEFF_QUANT            (4096)
+
+/* Mask definitions for Low and high 16 bits in a 32 bit number */
+#define XMASK_LOW_16BITS       (0x0000FFFF)
+#define XMASK_HIGH_16BITS      (0xFFFF0000)
+
+/**************************** Type Definitions *******************************/
+
+/**************************** Local Global *******************************/
+static float SincCoeffs[XV_VSCALER_MAX_V_PHASES][XV_VSCALER_MAX_V_TAPS];
+static float TempCoeffs[XV_VSCALER_MAX_V_PHASES][XV_VSCALER_MAX_V_TAPS];
+static float WinCoeffs[XV_VSCALER_MAX_V_PHASES][XV_VSCALER_MAX_V_TAPS];
+static float NormCoeffs[XV_VSCALER_MAX_V_PHASES][XV_VSCALER_MAX_V_TAPS];
+
+/************************** Function Prototypes ******************************/
+static float hamming( int x, int taps);
+static float sinc(float x);
+static void XV_VScalerGetCoeff(XV_vscaler *InstancePtr,
+                               XV_vscaler_l2 *pVscL2Data,
+                               u32 HeightIn,
+                               u32 HeightOut);
+static void XV_VScalerSetCoeff(XV_vscaler *InstancePtr,
+                               XV_vscaler_l2 *pVscL2Data);
+
+/*****************************************************************************/
+/**
+* This function starts the vertical scaler core
+*
+* @param  InstancePtr is a pointer to the core instance to be worked on.
+*
+* @return None
+*
+******************************************************************************/
+void XV_VScalerStart(XV_vscaler *InstancePtr)
+{
+  Xil_AssertVoid(InstancePtr != NULL);
+
+  XV_vscaler_EnableAutoRestart(InstancePtr);
+  XV_vscaler_Start(InstancePtr);
+}
+
+/*****************************************************************************/
+/**
+* This function stops the vertical scaler core
+*
+* @param  InstancePtr is a pointer to the core instance to be worked on.
+*
+* @return None
+*
+******************************************************************************/
+void XV_VScalerStop(XV_vscaler *InstancePtr)
+{
+  Xil_AssertVoid(InstancePtr != NULL);
+
+  XV_vscaler_DisableAutoRestart(InstancePtr);
+}
+
+/*****************************************************************************/
+/**
+* This function applies the hamming filter on specified pixel position
+*
+* @param  x is the pixel coordinate in horizontal direction
+* @param  taps is the number of taps available to the scaler
+*
+* @return hamming filter result
+*
+******************************************************************************/
+static float hamming( int x, int taps)
+{
+    //0.54 + 0.46 * cos(pi * x / filter_size); 0.54 - 0.46 * cos(2*pi * x / filter_size)
+    return (float) (0.54 + (0.46*cos((PI*x)/(taps+1))));
+}
+
+/*****************************************************************************/
+/**
+* This function applies the SIN function to specified pixel position
+*
+* @param  x is the pixel coordinate in horizontal direction
+*
+* @return Sine function result
+*
+******************************************************************************/
+static float sinc(float x)
+{
+    if (x==0)
+       return 1;
+
+    return (float) sin(x*PI)/(float)(x*PI);
+}
+
+/*****************************************************************************/
+/**
+* This function loads user defined filter coefficients in the scaler coefficient
+* storage
+*
+* @param  InstancePtr is a pointer to the core instance to be worked on.
+* @param  pVscL2Data is a pointer to the core instance layer 2 data.
+* @param  VCoeff is the user defined filter coefficients
+******************************************************************************/
+void XV_VscalerLoadUsrCoeffients(XV_vscaler *InstancePtr,
+                                 XV_vscaler_l2 *pVscL2Data,
+                                 const short VCoeff[XV_VSCALER_MAX_V_PHASES][XV_VSCALER_MAX_V_TAPS])
+{
+  int i,j,k, pad, offset;
+  int num_phases = XV_VSCALER_MAX_V_PHASES;
+  int num_taps   = pVscL2Data->EffectiveTaps;
+
+  /*
+   * Assert validates the input arguments
+   */
+  Xil_AssertVoid(InstancePtr != NULL);
+  Xil_AssertVoid(pVscL2Data != NULL);
+  Xil_AssertVoid((pVscL2Data->EffectiveTaps > 0) &&
+                 (pVscL2Data->EffectiveTaps <= XV_VSCALER_MAX_V_TAPS));
+
+  //determine if coefficient needs padding (effective vs. max taps)
+  pad = XV_VSCALER_MAX_V_TAPS - num_taps;
+  offset = ((pad) ? (pad>>1) : 0);
+
+  //Load User defined coefficients into scaler coefficient table
+  for (i = 0; i < num_phases; i++)
+  {
+    for (k=0,j=offset; j<num_taps; ++j,++k)
+    {
+      pVscL2Data->coeff[i][j] = VCoeff[i][k];
+    }
+  }
+
+  if(pad) //effective taps < max_taps
+  {
+    for (i = 0; i < num_phases; i++)
+    {
+      //pad left
+      for (j = 0; j < offset; j++)
+      {
+        pVscL2Data->coeff[i][j] = 0;
+      }
+        //pad right
+      for (j = (num_taps+offset); j < XV_VSCALER_MAX_V_TAPS; j++)
+      {
+        pVscL2Data->coeff[i][j] = 0;
+      }
+    }
+  }
+}
+
+/*****************************************************************************/
+/**
+* This function computes the filter coefficients based on scaling ratio and
+* stores them into the layer 2 data storage
+*
+* @param  InstancePtr is a pointer to the core instance to be worked on.
+* @param  pVscL2Data is a pointer to the core instance layer 2 data.
+* @param  HeightIn is the input frame height
+* @param  HeightOut is the scaled frame height
+*
+* @return None
+*
+******************************************************************************/
+static void XV_VScalerGetCoeff(XV_vscaler    *InstancePtr,
+                               XV_vscaler_l2 *pVscL2Data,
+                               u32            HeightIn,
+                               u32            HeightOut)
+{
+  int num_phases = XV_VSCALER_MAX_V_PHASES;
+  int num_taps   = pVscL2Data->EffectiveTaps;
+  int center_tap = num_taps/2;
+  int i,j, pad, offset;
+  float x, fc;
+  float sum[XV_VSCALER_MAX_V_PHASES];
+  float cos_win[XV_VSCALER_MAX_V_TAPS];
+
+  /*
+   * Assert validates the input arguments
+   */
+  Xil_AssertVoid((pVscL2Data->EffectiveTaps > 0) &&
+                 (pVscL2Data->EffectiveTaps <= XV_VSCALER_MAX_V_TAPS));
+
+  if(HeightIn < HeightOut)
+  {
+    fc = (float)HeightIn/(float)HeightOut;
+  }
+  else
+  {
+    fc = (float)HeightOut/(float)HeightIn;
+  }
+
+  //determine if coefficient needs padding (effective vs. max taps)
+  pad = XV_VSCALER_MAX_V_TAPS - num_taps;
+  offset = ((pad) ? (pad>>1) : 0);
+
+  for(i=0; i<num_phases; ++i)
+  {
+    for(j=0; j<num_taps; ++j)
+    {
+      x = ((float) (j - center_tap)) + (((float)i)/(float)num_phases);
+      TempCoeffs[i][j] = x;
+      SincCoeffs[i][j] = sinc(fc*x);
+    }
+  }
+
+  switch(pVscL2Data->FilterSel)
+  {
+    case XV_VFILT_LANCZOS:
+      //Window is a sinc function instead of cosine function
+      // if using lanczos2 or lanczos3 kernel
+      // lanczos(x) = sinc(x) * sinc(x / filter_size);
+
+      for (i = 0; i < num_phases; i++)
+      {
+          for (j = 0; j < num_taps; j++)
+          {
+              x = TempCoeffs[i][j];
+              WinCoeffs[i][j] = SincCoeffs[i][j] * sinc((fc*x)/num_taps);
+          }
+      }
+      break;
+
+    case XV_VFILT_WINDOWED_SINC:
+      for (j = 1; j <= num_taps; j++)
+      {
+        cos_win[j-1] = hamming(j, num_taps);
+      }
+
+      for (i = 0; i < num_phases; i++)
+      {
+          for (j = 0; j < num_taps; j++)
+          {
+              WinCoeffs[i][j] = SincCoeffs[i][j] * cos_win[j];
+          }
+      }
+      break;
+  }
+
+  // normalize to unity and quantize
+  for (i = 0; i < num_phases; i++)
+  {
+    sum[i] = 0;
+    for (j = 0; j < num_taps; j++)
+    {
+      sum[i] += WinCoeffs[i][j];
+    }
+  }
+
+  for (i = 0; i < num_phases; i++)
+  {
+    for (j = offset; j < num_taps; j++)
+    {
+      NormCoeffs[i][j] = WinCoeffs[i][j]/sum[i];
+      pVscL2Data->coeff[i][j] = (short) ((NormCoeffs[i][j] * COEFF_QUANT) + 0.5);
+    }
+  }
+
+  if(pad) //effective taps < max_taps
+  {
+    for (i = 0; i < num_phases; i++)
+    {
+      //pad left
+      for (j = 0; j < offset; j++)
+      {
+        pVscL2Data->coeff[i][j] = 0;
+      }
+        //pad right
+      for (j = (num_taps+offset); j < XV_VSCALER_MAX_V_TAPS; j++)
+      {
+        pVscL2Data->coeff[i][j] = 0;
+      }
+    }
+  }
+}
+
+
+/*****************************************************************************/
+/**
+* This function programs the computed filter coefficients and phase data into
+* core registers
+*
+* @param  InstancePtr is a pointer to the core instance to be worked on.
+* @param  VCoeff is the array that holds computed coefficients
+*
+* @return None
+*
+* @Note  This version of driver does not make use of computed coefficients.
+*        Pre-computed coefficients are stored in a local table which are used
+*        to overwrite any computed coefficients before being programmed into
+*        the core registers. Control flow still computes the coefficients to
+*        maintain the sw latency for driver version which would eventually use
+*        computed coefficients
+******************************************************************************/
+static void XV_VScalerSetCoeff(XV_vscaler *InstancePtr,
+                               XV_vscaler_l2 *pVscL2Data)
+{
+  int num_phases = XV_VSCALER_MAX_V_PHASES;
+  int num_taps   = XV_VSCALER_MAX_V_TAPS/2;
+  int val,i,j;
+  u32 baseAddr;
+
+  baseAddr = XV_vscaler_Get_HwReg_vfltCoeff_BaseAddress(InstancePtr);
+  for (i=0; i < num_phases; i++)
+  {
+    for(j=0; j < num_taps; j++)
+    {
+       val = (pVscL2Data->coeff[i][(j*2)+1] << 16) | (pVscL2Data->coeff[i][j*2] & XMASK_LOW_16BITS);
+       Xil_Out32(baseAddr+((i*num_taps+j)*4), val);
+    }
+  }
+}
+
+/*****************************************************************************/
+/**
+* This function configures the scaler core registers with the specified
+* configuration parameters of the axi stream
+*
+* @param  InstancePtr is a pointer to the core instance to be worked on.
+* @param  WidthIn is the input stream width
+* @param  HeightIn is the input stream height
+* @param  HeightOut is the output stream height
+*
+* @return None
+*
+******************************************************************************/
+void XV_VScalerSetup(XV_vscaler  *InstancePtr,
+                     XV_vscaler_l2 *pVscL2Data,
+                     u32         WidthIn,
+                     u32         HeightIn,
+                     u32         HeightOut)
+{
+  u32 LineRate;
+
+  /*
+   * Assert validates the input arguments
+   */
+  Xil_AssertVoid(InstancePtr != NULL);
+  Xil_AssertVoid(pVscL2Data != NULL);
+
+  if(pVscL2Data->ScalerType == XV_VSCALER_POLYPHASE)
+  {
+    if(!pVscL2Data->UseExtCoeff) //No predefined coefficients
+    {
+      /* Generate coefficients for vertical scaling ratio */
+      XV_VScalerGetCoeff(InstancePtr,
+                         pVscL2Data,
+                         HeightIn,
+                         HeightOut);
+    }
+
+    /* Program coefficients into the IP register bank */
+    XV_VScalerSetCoeff(InstancePtr, pVscL2Data);
+  }
+
+  LineRate = (u32) ((float)((HeightIn * STEP_PRECISION) + (HeightOut/2))/(float)HeightOut);
+
+  XV_vscaler_Set_HwReg_HeightIn(InstancePtr,   HeightIn);
+  XV_vscaler_Set_HwReg_Width(InstancePtr,      WidthIn);
+  XV_vscaler_Set_HwReg_HeightOut(InstancePtr,  HeightOut);
+  XV_vscaler_Set_HwReg_LineRate(InstancePtr,   LineRate);
+}
+
+/*****************************************************************************/
+/**
+*
+* This function prints V Scaler status on the console
+*
+* @param	InstancePtr is the instance pointer to the core instance.
+*
+* @return	None
+*
+******************************************************************************/
+void XV_VScalerDbgReportStatus(XV_vscaler *InstancePtr)
+{
+  XV_vscaler *pVsc = InstancePtr;
+  u32 done, idle, ready, ctrl;
+  u32 widthin, heightin, heightout, linerate;
+  u32 type = 3; //hard-coded to polyphase for now
+  u32 baseAddr, taps, phases;
+  int val,i,j;
+
+  /*
+   * Assert validates the input arguments
+   */
+  Xil_AssertVoid(InstancePtr != NULL);
+
+  xil_printf("\r\n\r\n----->V SCALER IP STATUS<----\r\n");
+  done  = XV_vscaler_IsDone(pVsc);
+  idle  = XV_vscaler_IsIdle(pVsc);
+  ready = XV_vscaler_IsReady(pVsc);
+  ctrl =  XV_vscaler_ReadReg(pVsc->Ctrl_BaseAddress, XV_VSCALER_CTRL_ADDR_AP_CTRL);
+
+
+  heightin  = XV_vscaler_Get_HwReg_HeightIn(pVsc);
+  widthin   = XV_vscaler_Get_HwReg_Width(pVsc);
+  heightout = XV_vscaler_Get_HwReg_HeightOut(pVsc);
+//  type      = XV_vscaler_GetHwreg_scaletype(pVsc);
+  linerate  = XV_vscaler_Get_HwReg_LineRate(pVsc);
+
+  taps   = XV_VSCALER_MAX_V_TAPS/2;
+  phases = XV_VSCALER_MAX_V_PHASES;
+
+  xil_printf("IsDone:  %d\r\n", done);
+  xil_printf("IsIdle:  %d\r\n", idle);
+  xil_printf("IsReady: %d\r\n", ready);
+  xil_printf("Ctrl:    0x%x\r\n\r\n", ctrl);
+
+  xil_printf("Input Width:     %d\r\n",widthin);
+  xil_printf("Input Height:    %d\r\n",heightin);
+  xil_printf("Output Height:   %d\r\n",heightout);
+//  xil_printf("Scaler Type:     %d\r\n",type);
+  xil_printf("Line Rate:       %d\r\n",linerate);
+  xil_printf("Num Phases:      %d\r\n",phases);
+  xil_printf("Num Taps:        %d\r\n",taps*2);
+
+  if(type == 3)
+  {
+    short lsb, msb;
+
+    xil_printf("\r\nCoefficients:");
+
+    baseAddr = XV_vscaler_Get_HwReg_vfltCoeff_BaseAddress(pVsc);
+    for(i = 0; i < phases; i++)
+    {
+      xil_printf("\r\nPhase %2d: ",i);
+      for(j=0; j< taps; j++)
+      {
+        val = Xil_In32(baseAddr+((i*taps+j)*4));
+
+        //coefficients are 12-bits
+        lsb = (short)(val & XMASK_LOW_16BITS);
+        msb = (short)((val & XMASK_HIGH_16BITS)>>16);
+
+        xil_printf("%5d %5d ", lsb, msb);
+      }
+    }
+  }
+}
---
+++ XilinxProcessorIPLib/drivers/v_vscaler/src/xv_vscaler_l2.h
@@ -0,0 +1,181 @@
+/******************************************************************************
+ *
+ * Copyright (C) 2015 Xilinx, Inc.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Use of the Software is limited solely to applications:
+ * (a) running on a Xilinx device, or
+ * (b) that interact with a Xilinx device through a bus or interconnect.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * XILINX CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Except as contained in this notice, the name of the Xilinx shall not be used
+ * in advertising or otherwise to promote the sale, use or other dealings in
+ * this Software without prior written authorization from Xilinx.
+ *
+******************************************************************************/
+/*****************************************************************************/
+/**
+*
+* @file xv_vscaler_l2.h
+*
+* This header file contains layer 2 API's of the verrtical scaler sub-core
+* driver.The functions contained herein provides a high level implementation of
+* features provided by the IP, abstracting away the register level details from
+* the user
+*
+* <b>V Scaler IP Features </b>
+*
+* This V-Scaler IP supports following features
+* 	- 3 Channel Scaler
+* 	- Scale vertically to 2K lines at 60Hz
+* 	- up to 16bits color depth
+*	- 1, 2 or 4 pixel per clock processing
+*
+* <b>Dependency</b>
+*
+* This driver makes use of the video enumerations and data types defined in the
+* Xilinx Video Common Driver (video_common_vX.x) and as such the common driver
+* must be included as dependency to compile this driver
+
+* <b>Initialization & Configuration</b>
+*
+* The device driver enables higher layer software (e.g., an application) to
+* communicate to the vscaler core.
+*
+* Before using the layer-2 API's user must initialize the core by calling
+* Layer-1 API XV_vscaler_Initialize(). This function will look for a
+* configuration structure for the device and initialize it to defined HW
+* settings. After initialization Layer-2 API's can be used to configure
+* the core. It is recommended user always make use of Layer-2 API to
+* interact with this core.
+* Advanced users always have the capability to directly interact with the IP
+* core using Layer-1 API's that perform low level register peek/poke.
+*
+* <b> Interrupts </b>
+*
+* This driver does not have any interrupts
+*
+* <b> Virtual Memory </b>
+*
+* This driver supports Virtual Memory. The RTOS is responsible for calculating
+* the correct device base address in Virtual Memory space.
+*
+* <b> Threads </b>
+*
+* This driver is not thread safe. Any needs for threads or thread mutual
+* exclusion must be satisfied by the layer above this driver.
+*
+* <b>Limitations</b>
+*
+* Current implementation of "computed coefficients" is non-entrant. If multiple
+* instances of the core are used in the design, user application has to make sure
+* that scale configuration is an atomic operation and cannot be interrupted in
+* middle to context switch to a different instance of the core
+* However fixed coefficient version of the code (default) is safe to be used with
+* multiple instances of the core.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who    Date     Changes
+* ----- ---- -------- -------------------------------------------------------
+* 1.00  rc   05/01/15   Initial Release
+
+* </pre>
+*
+******************************************************************************/
+#ifndef XV_VSCALER_L2_H 	 /* prevent circular inclusions */
+#define XV_VSCALER_L2_H		 /* by using protection macros  */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "xvidc.h"
+#include "xv_vscaler.h"
+
+/************************** Constant Definitions *****************************/
+ /** @name Hw Configuration
+  * @{
+  * The following constants define the scaler HW configuration
+  * TODO:
+  * Below defined Parameters are static configuration of H Scaler IP
+  * The tool needs to export these parameters to SDK and the driver will
+  * be populated with these option settings. i.e. these settings could be
+  * accessible via instance pointer
+  *
+  */
+ #define XV_VSCALER_MAX_V_TAPS           (6)
+ #define XV_VSCALER_MAX_V_PHASES         (64)
+
+/**************************** Type Definitions *******************************/
+/**
+ * This typedef contains the Scaler Type
+ */
+typedef enum
+{
+  XV_VSCALER_BILINEAR = 0,
+  XV_VSCALER_BICUBIC,
+  XV_VSCALER_POLYPHASE
+}XV_VSCALER_TYPE;
+
+/**
+ * This typedef contains the type of filters available for scaling operation
+ */
+typedef enum
+{
+  XV_VFILT_LANCZOS = 0,
+  XV_VFILT_WINDOWED_SINC
+}XV_VFILTER_ID;
+
+
+/**
+ * V Scaler Layer 2 data. The user is required to allocate a variable
+ * of this type for every V Scaler device in the system. A pointer to a
+ * variable of this type is then passed to the driver API functions.
+ */
+typedef struct
+{
+  u8 EffectiveTaps;
+  u8 UseExtCoeff;
+  XV_VFILTER_ID FilterSel;
+  XV_VSCALER_TYPE ScalerType;
+  u8 Gain;
+  short coeff[XV_VSCALER_MAX_V_PHASES][XV_VSCALER_MAX_V_TAPS];
+}XV_vscaler_l2;
+
+/************************** Function Prototypes ******************************/
+void XV_VScalerStart(XV_vscaler *InstancePtr);
+void XV_VScalerStop(XV_vscaler *InstancePtr);
+void XV_VscalerLoadUsrCoeffients(XV_vscaler *InstancePtr,
+                                 XV_vscaler_l2 *pVscL2Data,
+                                 const short VCoeff[XV_VSCALER_MAX_V_PHASES][XV_VSCALER_MAX_V_TAPS]);
+void XV_VScalerSetup(XV_vscaler *InstancePtr,
+                     XV_vscaler_l2 *pVscL2Data,
+                     u32 WidthIn,
+                     u32 HeightIn,
+                     u32 HeightOut);
+
+void XV_VScalerDbgReportStatus(XV_vscaler *InstancePtr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
