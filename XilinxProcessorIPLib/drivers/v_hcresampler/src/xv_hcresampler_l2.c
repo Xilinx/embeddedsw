f37b5011ea98d4dfaefb80e84f9db95f70326b81
Rohit Consul <rohit.consul@xilinx.com>
2015-05-27 08:11:30 +0800

    v_hcresampler: Added new driver
    HLS generated driver along with manually written layer 2. Driver
    tcl update is pending
    
    Signed-off-by: Rohit Consul <rohit.consul@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/v_hcresampler/src/xv_hcresampler_l2.c
@@ -0,0 +1,238 @@
+/******************************************************************************
+ *
+ * Copyright (C) 2015 Xilinx, Inc.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Use of the Software is limited solely to applications:
+ * (a) running on a Xilinx device, or
+ * (b) that interact with a Xilinx device through a bus or interconnect.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * XILINX CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Except as contained in this notice, the name of the Xilinx shall not be used
+ * in advertising or otherwise to promote the sale, use or other dealings in
+ * this Software without prior written authorization from Xilinx.
+ *
+******************************************************************************/
+/*****************************************************************************/
+/**
+*
+* @file xv_hcresampler_l2.c
+*
+* The Horizontal Chroma Resampler Layer-2 Driver.
+* The functions in this file provides an abstraction from the register peek/poke
+* methodology by implementing most common use-case provided by the sub-core.
+* See xv_hcresampler_l2.h for a detailed description of the layer-2 driver
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who    Date     Changes
+* ----- ---- -------- -------------------------------------------------------
+* 1.00  rc   05/01/15   Initial Release
+
+* </pre>
+*
+******************************************************************************/
+
+/***************************** Include Files *********************************/
+#include "xv_hcresampler_l2.h"
+
+/************************** Constant Definitions *****************************/
+
+/**************************** Type Definitions *******************************/
+
+/**************************** Local Global *******************************/
+
+/************************** Function Prototypes ******************************/
+static void hcrUpdateCoefficients(XV_hcresampler *pHCrsmplr, u32 coeff[2][4]);
+
+/*****************************************************************************/
+/**
+ * This function starts the Chroma resampler core
+ *
+ * @param  InstancePtr is a pointer to the core instance to be worked on.
+ *
+ * @return None
+ *
+ *****************************************************************************/
+void XV_HCrsmplStart(XV_hcresampler *InstancePtr)
+{
+  Xil_AssertVoid(InstancePtr != NULL);
+
+  XV_hcresampler_EnableAutoRestart(InstancePtr);
+  XV_hcresampler_Start(InstancePtr);
+}
+
+/*****************************************************************************/
+/**
+ * This function stops the Chroma resampler core
+ *
+ * @param  InstancePtr is a pointer to the core instance to be worked on.
+ *
+ * @return None
+ *
+ *****************************************************************************/
+void XV_HCrsmplStop(XV_hcresampler *InstancePtr)
+{
+  Xil_AssertVoid(InstancePtr != NULL);
+
+  XV_hcresampler_DisableAutoRestart(InstancePtr);
+}
+
+/*****************************************************************************/
+/**
+* This function configures the Chroma resampler active resolution
+*
+* @param  InstancePtr is a pointer to the core instance to be worked on.
+* @param  width is the active frame width
+* @param  height is the active frame height
+*
+* @return None
+*
+******************************************************************************/
+void XV_HCrsmplSetActiveSize(XV_hcresampler *InstancePtr,
+                             u32             width,
+                             u32             height)
+{
+  Xil_AssertVoid(InstancePtr != NULL);
+
+  XV_hcresampler_Set_HwReg_width(InstancePtr,  width);
+  XV_hcresampler_Set_HwReg_height(InstancePtr, height);
+}
+
+/*****************************************************************************/
+/**
+* This function configures the Chroma resampler for the required format
+* conversion
+*
+* @param  InstancePtr is a pointer to the core instance to be worked on.
+* @param  formatIn is the input chroma format
+* @param  formatOut is required chroma format
+*
+* @return None
+*
+******************************************************************************/
+void XV_HCrsmplSetFormat(XV_hcresampler   *InstancePtr,
+                         XVidC_ColorFormat formatIn,
+                         XVidC_ColorFormat formatOut)
+{
+  u32 K[2][4] = {{0}};
+
+  /*
+   * Assert validates the input arguments
+   */
+  Xil_AssertVoid(InstancePtr != NULL);
+
+  XV_hcresampler_Set_HwReg_input_video_format(InstancePtr,  formatIn);
+  XV_hcresampler_Set_HwReg_output_video_format(InstancePtr, formatOut);
+
+  if((formatIn == XVIDC_CSF_YCRCB_422) &&
+     (formatOut == XVIDC_CSF_YCRCB_444))
+  {
+    K[0][0] = 0;
+    K[0][1] = 0;
+    K[0][2] = 4096;
+    K[0][3] = 0;
+    K[1][0] = 506;
+    K[1][1] = 1542;
+    K[1][2] = 1542;
+    K[1][3] = 506;
+  }
+  else if((formatIn == XVIDC_CSF_YCRCB_444) &&
+          (formatOut == XVIDC_CSF_YCRCB_422))
+
+  {
+    K[0][0] = 0;
+    K[0][1] = 1024;
+    K[0][2] = 2048;
+    K[0][3] = 1024;
+    K[1][0] = 0;
+    K[1][1] = 0;
+    K[1][2] = 0;
+    K[1][3] = 0;
+  }
+  hcrUpdateCoefficients(InstancePtr, K);
+}
+
+/*****************************************************************************/
+/**
+* This function updates the core registers with computed coefficients for
+* required conversion
+*
+* @param  pHCrsmplr is a pointer to the core instance to be worked on.
+* @param  coeff is the array holding computed coefficients
+*
+* @return None
+*
+******************************************************************************/
+static void hcrUpdateCoefficients(XV_hcresampler *pHCrsmplr, u32 coeff[2][4])
+{
+  XV_hcresampler_Set_HwReg_coefs_0_0(pHCrsmplr, coeff[0][0]);
+  XV_hcresampler_Set_HwReg_coefs_0_1(pHCrsmplr, coeff[0][1]);
+  XV_hcresampler_Set_HwReg_coefs_0_2(pHCrsmplr, coeff[0][2]);
+  XV_hcresampler_Set_HwReg_coefs_0_3(pHCrsmplr, coeff[0][3]);
+  XV_hcresampler_Set_HwReg_coefs_1_0(pHCrsmplr, coeff[1][0]);
+  XV_hcresampler_Set_HwReg_coefs_1_1(pHCrsmplr, coeff[1][1]);
+  XV_hcresampler_Set_HwReg_coefs_1_2(pHCrsmplr, coeff[1][2]);
+  XV_hcresampler_Set_HwReg_coefs_1_3(pHCrsmplr, coeff[1][3]);
+}
+
+/*****************************************************************************/
+/**
+*
+* This function prints Chroma Resampler status on the console
+*
+* @param  InstancePtr is a pointer to the core instance to be worked on.
+*
+* @return None
+*
+******************************************************************************/
+void XV_HCrsmplDbgReportStatus(XV_hcresampler *InstancePtr)
+{
+  XV_hcresampler *pHCrsmplr = InstancePtr;
+  u32 done, idle, ready, ctrl;
+  u32 vidfmtIn, vidfmtOut, height, width;
+
+  /*
+   * Assert validates the input arguments
+   */
+  Xil_AssertVoid(InstancePtr != NULL);
+
+  xil_printf("\r\n\r\n----->H Chroma Resampler IP STATUS<----\r\n");
+
+  done  = XV_hcresampler_IsDone(pHCrsmplr);
+  idle  = XV_hcresampler_IsIdle(pHCrsmplr);
+  ready = XV_hcresampler_IsReady(pHCrsmplr);
+  ctrl  = XV_hcresampler_ReadReg(pHCrsmplr->Ctrl_BaseAddress, XV_HCRESAMPLER_CTRL_ADDR_AP_CTRL);
+
+  vidfmtIn  = XV_hcresampler_Get_HwReg_input_video_format(pHCrsmplr);
+  vidfmtOut = XV_hcresampler_Get_HwReg_output_video_format(pHCrsmplr);
+  height    = XV_hcresampler_Get_HwReg_height(pHCrsmplr);
+  width     = XV_hcresampler_Get_HwReg_width(pHCrsmplr);
+
+  xil_printf("IsDone:  %d\r\n", done);
+  xil_printf("IsIdle:  %d\r\n", idle);
+  xil_printf("IsReady: %d\r\n", ready);
+  xil_printf("Ctrl:    0x%x\r\n\r\n", ctrl);
+
+  xil_printf("Video Format In:  %d\r\n", vidfmtIn);
+  xil_printf("Video Format Out: %d\r\n", vidfmtOut);
+  xil_printf("Width:            %d\r\n", width);
+  xil_printf("Height:           %d\r\n", height);
+}
---
+++ XilinxProcessorIPLib/drivers/v_hcresampler/src/xv_hcresampler_l2.h
@@ -0,0 +1,126 @@
+/******************************************************************************
+ *
+ * Copyright (C) 2015 Xilinx, Inc.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Use of the Software is limited solely to applications:
+ * (a) running on a Xilinx device, or
+ * (b) that interact with a Xilinx device through a bus or interconnect.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * XILINX CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Except as contained in this notice, the name of the Xilinx shall not be used
+ * in advertising or otherwise to promote the sale, use or other dealings in
+ * this Software without prior written authorization from Xilinx.
+ *
+******************************************************************************/
+/*****************************************************************************/
+/**
+*
+* @file xv_hcresampler_l2.h
+*
+* This header file contains layer 2 API's of the horizontal chroma resampler
+* sub-core driver.The functions contained herein provides a high level
+* implementation of features provided by the IP, abstracting away the register
+* level details from the user
+*
+* <b>H Chroma Resampler IP Features </b>
+*
+* The horizontal chroma resampler IP supports following features
+* 	- convert YUV422 -> YUV444 and YUV444 -> YUV422
+*	- Supports resolution up to 4k2k 60Hz
+*	- up to 16 bits color depth
+*	- 1, 2 or 4 pixel per clock processing
+*
+* <b>Dependency</b>
+*
+* This driver makes use of the video enumerations and data types defined in the
+* Xilinx Video Common Driver (video_common_vX.x) and as such the common driver
+* must be included as dependency to compile this driver
+
+* <b>Initialization & Configuration</b>
+*
+* The device driver enables higher layer software (e.g., an application) to
+* communicate to the hcresampler core.
+*
+* Before using the layer-2 API's user must initialize the core by calling
+* Layer-1 API XV_hcresampler_Initialize(). This function will look for a
+* configuration structure for the device and initialize it to defined HW
+* settings. After initialization Layer-2 API's can be used to configure
+* the core. It is recommended user always make use of Layer-2 API to
+* interact with this core.
+* Advanced users always have the capability to directly interact with the IP
+* core using Layer-1 API's that perform low level register peek/poke.
+*
+* <b> Interrupts </b>
+*
+* This driver does not have any interrupts
+*
+* <b> Virtual Memory </b>
+*
+* This driver supports Virtual Memory. The RTOS is responsible for calculating
+* the correct device base address in Virtual Memory space.
+*
+* <b> Threads </b>
+*
+* This driver is not thread safe. Any needs for threads or thread mutual
+* exclusion must be satisfied by the layer above this driver.
+*
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who    Date     Changes
+* ----- ---- -------- -------------------------------------------------------
+* 1.00  rc   05/01/15   Initial Release
+
+* </pre>
+*
+******************************************************************************/
+#ifndef XV_HCHROMA_RESAMPLER_L2_H	   /* prevent circular inclusions */
+#define XV_HCHROMA_RESAMPLER_L2_H    /* by using protection macros  */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "xvidc.h"
+#include "xv_hcresampler.h"
+
+/************************** Constant Definitions *****************************/
+
+/**************************** Type Definitions *******************************/
+
+/************************** Function Prototypes ******************************/
+void XV_HCrsmplStart(XV_hcresampler *InstancePtr);
+void XV_HCrsmplStop(XV_hcresampler *InstancePtr);
+
+void XV_HCrsmplSetActiveSize(XV_hcresampler *InstancePtr,
+                             u32            width,
+                             u32            height);
+
+void XV_HCrsmplSetFormat(XV_hcresampler   *InstancePtr,
+                         XVidC_ColorFormat formatIn,
+                         XVidC_ColorFormat formatOut);
+
+void XV_HCrsmplDbgReportStatus(XV_hcresampler *InstancePtr);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
