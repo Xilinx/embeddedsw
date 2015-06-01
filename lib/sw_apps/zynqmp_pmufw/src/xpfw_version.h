20c80ed9adc82adba006469e858d10bf38fed3d8
Soren Brinkmann <soren.brinkmann@xilinx.com>
2015-06-01 16:54:59 -0700

    PMUFW: scheduler: Fix print format string
    A print format string had a typo in the conversion specifier.
    
    Fixes: ae1b22f628eeae491136205dd99cac745bad5b54 "Scheduler: Add Scheduler files"
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>
    Reviewed-by: Jyotheeswar Reddy <jyothee@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2015.1-swbeta2-7-gaea3444396c3"
+ #define ZYNQMP_XPFW_VERSION	"2015.1-swbeta2-14-g2a2f9ea1077b"
  #endif
