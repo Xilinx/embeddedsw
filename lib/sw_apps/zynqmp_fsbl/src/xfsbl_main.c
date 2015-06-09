83eaa550d6f51457f380c39c00f8e6723f73b2ce
Sarat Chand Savitala <sarat.chand.savitala@xilinx.com>
2015-06-09 19:12:16 +0530

    sw_apps:zynqmp_fsbl: Updated release version to 2015.3
    Updated release version from 2015.1 SW Beta2 to 2015.3
    
    Signed-off-by: Sarat Chand Savitala <saratcha@xilinx.com>

--- lib/sw_apps/zynqmp_fsbl/src/xfsbl_main.c
+++ lib/sw_apps/zynqmp_fsbl/src/xfsbl_main.c
@@ -286,7 +286,7 @@ void XFsbl_PrintFsblBanner(void )
 	XFsbl_Printf(DEBUG_PRINT_ALWAYS,
                  "Xilinx Zynq MP First Stage Boot Loader \n\r");
 	XFsbl_Printf(DEBUG_PRINT_ALWAYS,
-                 "Release %d.%d SW Beta2   %s  -  %s\r\n",
+                 "Release %d.%d   %s  -  %s\r\n",
                  SDK_RELEASE_YEAR, SDK_RELEASE_QUARTER,__DATE__,__TIME__);
 	/**
 	 * Print the platform
