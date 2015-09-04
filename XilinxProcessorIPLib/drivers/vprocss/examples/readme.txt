d0f4ed35bb559e58e84893f7bd6eb12622a31748
Rohit Consul <rohit.consul@xilinx.com>
2015-09-04 10:44:50 -0700

    vprocss: Update example tcl to accept command line parameter
    Vivado Example design tool flow change resulted in hdf file name
    change of video processing subsystem example design. Update the
    script to accept hdf file name from command line to avoid any
    dependency on further name changes
    
    Signed-off-by: Rohit Consul <rohit.consul@xilinx.com>
    Acked-by: Anirudha Sarangi <anirudh@xilinx.com>

--- XilinxProcessorIPLib/drivers/vprocss/examples/readme.txt
+++ XilinxProcessorIPLib/drivers/vprocss/examples/readme.txt
@@ -3,11 +3,12 @@ vpss_example.tcl automates the process of generating the downloadable bit & elf 
 Example application design source files (contained within "examples/src" folder) are tightly coupled with the video processing subsystem example design available in Vivado Catalogue.
 To run the provided tcl
   1. Copy the exported example design hdf file in the "examples" directory of the driver
-     (a) Default hdf name is "design_1_v_proc_ss_0_0_design_synth_wrapper.hdf". If generated hdf name is different then vpss_example.tcl needs to be updated with the new name
   2. Launch the xsct terminal
   3. cd into the examples directory
-  4. run the tcl
-       xsct%>source vpss_example.tcl
+  4. source the tcl file
+		xsct%>source vpss_example.tcl
+  4. execute the script
+		xsct%>vpss_example <hdf_file_name.hdf>
 
 Script will perform following operations
   1. Create workspace
@@ -20,6 +21,6 @@ After the process is complete required files will be available in
   bit file -> vpss_example.sdk/vpss_example_hw_platform folder
   elf file -> vpss_example.sdk/vpss_example_design/{Debug/Release} folder
 
-When executed on the board the example application will determine the video processing subsystem topology set the input and output stream configuration accordingly. Test pattern generator IP is used to generated the input stream. Video Lock Monitor IP will then monitor the output of the subsystem (to vidout) to determine if lock is achieved and present the status on the terminal
+When executed on the board the example application will determine the video processing subsystem topology and set the input and output stream configuration accordingly. Test pattern generator IP is used to generate the input stream. Video Lock Monitor IP will then monitor the output of the subsystem (to vidout) to determine if lock is achieved and present the status (Pass/Fail) on the terminal
 
 Note: Serial terminal baud rate should be set to 9600
--- XilinxProcessorIPLib/drivers/vprocss/examples/vpss_example.tcl
+++ XilinxProcessorIPLib/drivers/vprocss/examples/vpss_example.tcl
@@ -1,23 +1,35 @@
-#set workspace
-puts "Create Workspace"
-sdk set_workspace vpss_example.sdk
+proc vpss_example args {
 
-#create hw project
-puts "Create HW Project"
-sdk create_hw_project -name vpss_example_hw_platform -hwspec ./design_1_v_proc_ss_0_0_design_synth_wrapper.hdf
+	if {[llength $args] != 1} {
+		puts "error: hdf file name missing from command line"
+		puts "Please specify hdf to process"
+		puts "Example Usage: vpss_example.tcl design1.hdf"
+	} else {
 
-#create bsp
-puts "Create BSP"
-sdk create_bsp_project -name vpss_example_bsp -hwproject vpss_example_hw_platform -proc microblaze_ss_microblaze_0 -os standalone
+		set hdf [lindex $args 0]
 
-#create application project
-puts "Create Application Project"
-sdk create_app_project -name vpss_example_design -hwproject vpss_example_hw_platform -proc microblaze_ss_microblaze_0 -os standalone -lang C -app {Empty Application} -bsp vpss_example_bsp
+		#set workspace
+		puts "Create Workspace"
+		sdk set_workspace vpss_example.sdk
 
-#copy example source files tp app project
-puts "Get Example Design Source Files"
-sdk import_sources -name vpss_example_design -path ./src
+		#create hw project
+		puts "Create HW Project"
+		sdk create_hw_project -name vpss_example_hw_platform -hwspec ./$hdf
 
-#build project
-puts "Build Project"
-sdk build_project -type all
+		#create bsp
+		puts "Create BSP"
+		sdk create_bsp_project -name vpss_example_bsp -hwproject vpss_example_hw_platform -proc microblaze_ss_microblaze_0 -os standalone
+
+		#create application project
+		puts "Create Application Project"
+		sdk create_app_project -name vpss_example_design -hwproject vpss_example_hw_platform -proc microblaze_ss_microblaze_0 -os standalone -lang C -app {Empty Application} -bsp vpss_example_bsp
+
+		#copy example source files tp app project
+		puts "Get Example Design Source Files"
+		sdk import_sources -name vpss_example_design -path ./src
+
+		#build project
+		puts "Build Project"
+		sdk build_project -type all
+	}
+}
