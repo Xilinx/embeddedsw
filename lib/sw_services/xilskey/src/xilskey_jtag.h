ea31c106080ca5f07d73270c2bce2fac0d98a34a
VNSL Durga <vnsl.durga.challa@xilinx.com>
2015-10-20 14:58:11 +0530

    xilskey: Added c++ boundary blocks
    Signed-off-by: VNSL Durga <vnsldurg@xilinx.com>
    Reviewed-by: Harini Katakam <harinik@xilinx.com>

--- lib/sw_services/xilskey/src/xilskey_jtag.h
+++ lib/sw_services/xilskey/src/xilskey_jtag.h
@@ -33,6 +33,10 @@
 #ifndef XILSKEY_JTAG_H
 #define XILSKEY_JTAG_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "xil_types.h"
 
 void JtagInitGpio();
@@ -47,5 +51,9 @@ void JtagRead_Ultrascale(u8 Row, u32 *RowData, u8 MarginOption,
 void JtagRead_Status_Ultrascale(u32 *Rowdata);
 void Jtag_Read_Sysmon(u8 Row, u32 *Row_Data);
 u32 JtagAES_Check_Ultrascale(u32 *Crc, u8 MarginOption);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /*XILSKEY_JTAG_H*/
