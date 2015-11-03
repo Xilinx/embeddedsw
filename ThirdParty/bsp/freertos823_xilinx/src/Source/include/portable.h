419545b52a50e2f3f90ec5c7733277d311da165a
Kinjal Pravinbhai Patel <patel.kinjal.pravinbhai@xilinx.com>
2015-11-03 17:37:43 +0530

    ThirdParty BSP: FreeRTOS kernel has been upgraded to 8.2.3 version
    Signed-off-by: Kinjal Pravinbhai Patel <patelki@xilinx.com>
    Acked-by: Anirudha Sarangi <anirudh@xilinx.com>

--- ThirdParty/bsp/freertos823_xilinx/src/Source/include/portable.h
+++ ThirdParty/bsp/freertos823_xilinx/src/Source/include/portable.h
@@ -1,5 +1,5 @@
 /*
-    FreeRTOS V8.2.1 - Copyright (C) 2015 Real Time Engineers Ltd.
+    FreeRTOS V8.2.3 - Copyright (C) 2015 Real Time Engineers Ltd.
     All rights reserved
 
     VISIT http://www.FreeRTOS.org TO ENSURE YOU ARE USING THE LATEST VERSION.
@@ -8,7 +8,7 @@
 
     FreeRTOS is free software; you can redistribute it and/or modify it under
     the terms of the GNU General Public License (version 2) as published by the
-    Free Software Foundation >>!AND MODIFIED BY!<< the FreeRTOS exception.
+    Free Software Foundation >>>> AND MODIFIED BY <<<< the FreeRTOS exception.
 
     ***************************************************************************
     >>!   NOTE: The modification to the GPL is included to allow you to     !<<
@@ -162,7 +162,7 @@ typedef struct HeapRegion
  * terminated by a HeapRegions_t structure that has a size of 0.  The region
  * with the lowest start address must appear first in the array.
  */
-void vPortDefineHeapRegions( const HeapRegion_t * const pxHeapRegions );
+void vPortDefineHeapRegions( const HeapRegion_t * const pxHeapRegions ) PRIVILEGED_FUNCTION;
 
 
 /*
