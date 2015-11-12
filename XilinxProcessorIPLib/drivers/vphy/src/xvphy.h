5f495caaabbe6aeba4a162074c9bef342ab12c51
Gilbert Magnaye <gmagnay@xilinx.com>
2015-11-11 19:19:55 -0800

    vphy: Added API for resetting the MMCM.
    Signed-off-by: Andrei-Liviu Simion <andrei.simion@xilinx.com>
    Acked-by: Srikanth Vemula <svemula@xilinx.com>

--- XilinxProcessorIPLib/drivers/vphy/src/xvphy.h
+++ XilinxProcessorIPLib/drivers/vphy/src/xvphy.h
@@ -707,6 +707,8 @@ void XVphy_MmcmReset(XVphy *InstancePtr, u8 QuadId, XVphy_DirectionType Dir,
 void XVphy_MmcmPowerDown(XVphy *InstancePtr, u8 QuadId, XVphy_DirectionType Dir,
 		u8 Hold);
 void XVphy_MmcmStart(XVphy *InstancePtr, u8 QuadId, XVphy_DirectionType Dir);
+void XVphy_MmcmLockedMaskEnable(XVphy *InstancePtr, u8 QuadId,
+		XVphy_DirectionType Dir, u8 Enable);
 void XVphy_BufgGtReset(XVphy *InstancePtr, XVphy_DirectionType Dir, u8 Reset);
 void XVphy_SetBufgGtDiv(XVphy *InstancePtr, XVphy_DirectionType Dir, u8 Div);
 void XVphy_IBufDsEnable(XVphy *InstancePtr, u8 QuadId, XVphy_DirectionType Dir,
