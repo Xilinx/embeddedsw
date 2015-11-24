7d691015d059d6a22fa6f48c1550813bb1034cf8
Sarat Chand Savitala <sarat.chand.savitala@xilinx.com>
2015-11-24 15:51:23 +0530

    sw_apps:zynq_fsbl: Updated misc folder for 2015.4
    Updated ps7 init and xparameters files in misc folder for
    zc702, zc706, zed boards - with 2015.4
    
    Signed-off-by: Sarat Chand Savitala <saratcha@xilinx.com>
    Acked-by: Srikanth Vemula <svemula@xilinx.com>

--- lib/sw_apps/zynq_fsbl/misc/zc702/xparameters.h
+++ lib/sw_apps/zynq_fsbl/misc/zc702/xparameters.h
@@ -41,6 +41,14 @@
 
 /******************************************************************/
 
+
+/* Definitions for peripheral PS7_DDR_0 */
+#define XPAR_PS7_DDR_0_S_AXI_BASEADDR 0x00100000
+#define XPAR_PS7_DDR_0_S_AXI_HIGHADDR 0x3FFFFFFF
+
+
+/******************************************************************/
+
 /* Definitions for driver DEVCFG */
 #define XPAR_XDCFG_NUM_INSTANCES 1
 
@@ -142,16 +150,6 @@
 /* Definitions for peripheral PS7_AFI_3 */
 #define XPAR_PS7_AFI_3_S_AXI_BASEADDR 0xF800B000
 #define XPAR_PS7_AFI_3_S_AXI_HIGHADDR 0xF800BFFF
-
-
-/* Definitions for peripheral PS7_CORESIGHT_COMP_0 */
-#define XPAR_PS7_CORESIGHT_COMP_0_S_AXI_BASEADDR 0xF8800000
-#define XPAR_PS7_CORESIGHT_COMP_0_S_AXI_HIGHADDR 0xF88FFFFF
-
-
-/* Definitions for peripheral PS7_DDR_0 */
-#define XPAR_PS7_DDR_0_S_AXI_BASEADDR 0x00100000
-#define XPAR_PS7_DDR_0_S_AXI_HIGHADDR 0x3FFFFFFF
 
 
 /* Definitions for peripheral PS7_DDRC_0 */
@@ -291,8 +289,6 @@
 
 /******************************************************************/
 
-
-/***Definitions for Core_nIRQ/nFIQ interrupts ****/
 /* Definitions for driver SCUGIC */
 #define XPAR_XSCUGIC_NUM_INSTANCES 1
 
@@ -360,6 +356,8 @@
 #define XPAR_PS7_SD_0_BASEADDR 0xE0100000
 #define XPAR_PS7_SD_0_HIGHADDR 0xE0100FFF
 #define XPAR_PS7_SD_0_SDIO_CLK_FREQ_HZ 50000000
+#define XPAR_PS7_SD_0_HAS_CD 1
+#define XPAR_PS7_SD_0_HAS_WP 1
 
 
 /******************************************************************/
@@ -369,6 +367,8 @@
 #define XPAR_XSDPS_0_BASEADDR 0xE0100000
 #define XPAR_XSDPS_0_HIGHADDR 0xE0100FFF
 #define XPAR_XSDPS_0_SDIO_CLK_FREQ_HZ 50000000
+#define XPAR_XSDPS_0_HAS_CD 1
+#define XPAR_XSDPS_0_HAS_WP 1
 
 
 /******************************************************************/
