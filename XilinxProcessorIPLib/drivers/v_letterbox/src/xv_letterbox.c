1e82f162eb5dd158b7df9c8c701a70be6a4ec368
Rohit Consul <rohit.consul@xilinx.com>
2016-01-05 16:23:33 -0800

    v_letterbox: clean up level 1 driver
    removed c library string.h inclusion from level-1 driver
    
    Signed-off-by: Rohit Consul <rohit.consul@xilinx.com>
    Acked-by: Dan Campbell   <dcampbel@xilinx.com>

--- XilinxProcessorIPLib/drivers/v_letterbox/src/xv_letterbox.c
+++ XilinxProcessorIPLib/drivers/v_letterbox/src/xv_letterbox.c
@@ -7,7 +7,6 @@
 
 /***************************** Include Files *********************************/
 #include "xv_letterbox.h"
-#include "string.h"
 
 /************************** Function Implementation *************************/
 #ifndef __linux__
@@ -19,10 +18,7 @@ int XV_letterbox_CfgInitialize(XV_letterbox *InstancePtr,
     Xil_AssertNonvoid(EffectiveAddr != (u32)0x0);
 
     /* Setup the instance */
-    (void)memset((void *)InstancePtr, 0, sizeof(XV_letterbox));
-    (void)memcpy((void *)&(InstancePtr->Config), (const void *)ConfigPtr,
-                    sizeof(XV_letterbox_Config));
-
+    InstancePtr->Config = *ConfigPtr;
     InstancePtr->Config.BaseAddress = EffectiveAddr;
 
     /* Set the flag to indicate the driver is ready */
