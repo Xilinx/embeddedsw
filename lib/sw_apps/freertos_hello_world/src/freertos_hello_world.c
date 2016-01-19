59a3e5dbf90369c7180231760234077f22a06bad
Anirudha Sarangi <anirudha.sarangi@xilinx.com>
2016-01-19 16:36:07 +0530

    freertos_hello_world: Improve the freertos hello world example
    This patch improves the freertos hello world example to add a software
    timer. The timer expires and checks if the tasks have been running
    properly till then. The timer callback then prints a success/failure
    message accordingly.
    
    Signed-off-by: Anirudha Sarangi <anirudh@xilinx.com>
    Acked by: Kinjal Pravinbhai Patel <patelki@xilinx.com>

--- lib/sw_apps/freertos_hello_world/src/freertos_hello_world.c
+++ lib/sw_apps/freertos_hello_world/src/freertos_hello_world.c
@@ -97,27 +97,37 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
-
+#include "timers.h"
 /* Xilinx includes. */
 #include "xil_printf.h"
 #include "xparameters.h"
 
+#define TIMER_ID	1
+#define DELAY_10_SECONDS	10000UL
+#define DELAY_1_SECOND		1000UL
+#define TIMER_CHECK_THRESHOLD	9
 /*-----------------------------------------------------------*/
 
 /* The Tx and Rx tasks as described at the top of this file. */
 static void prvTxTask( void *pvParameters );
 static void prvRxTask( void *pvParameters );
-
+static void vTimerCallback( TimerHandle_t pxTimer );
 /*-----------------------------------------------------------*/
 
 /* The queue used by the Tx and Rx tasks, as described at the top of this
 file. */
+static TaskHandle_t xTxTask;
+static TaskHandle_t xRxTask;
 static QueueHandle_t xQueue = NULL;
+static TimerHandle_t xTimer = NULL;
 char HWstring[15] = "Hello World";
+long RxtaskCntr = 0;
 
 int main( void )
 {
-	xil_printf( "Hello from Freertos\r\n" );
+	const TickType_t x10seconds = pdMS_TO_TICKS( DELAY_10_SECONDS );
+
+	xil_printf( "Hello from Freertos example main\r\n" );
 
 	/* Create the two tasks.  The Tx task is given a lower priority than the
 	Rx task, so the Rx task will leave the Blocked state and pre-empt the Tx
@@ -127,9 +137,14 @@ int main( void )
 					configMINIMAL_STACK_SIZE, 	/* The stack allocated to the task. */
 					NULL, 						/* The task parameter is not used, so set to NULL. */
 					tskIDLE_PRIORITY,			/* The task runs at the idle priority. */
-					NULL );
+					&xTxTask );
 
-	xTaskCreate( prvRxTask, ( const char * ) "GB",	configMINIMAL_STACK_SIZE, NULL,	tskIDLE_PRIORITY + 1, NULL );
+	xTaskCreate( prvRxTask,
+				 ( const char * ) "GB",
+				 configMINIMAL_STACK_SIZE,
+				 NULL,
+				 tskIDLE_PRIORITY + 1,
+				 &xRxTask );
 
 	/* Create the queue used by the tasks.  The Rx task has a higher priority
 	than the Tx task, so will preempt the Tx task and remove values from the
@@ -140,6 +155,25 @@ int main( void )
 
 	/* Check the queue was created. */
 	configASSERT( xQueue );
+
+	/* Create a timer with a timer expiry of 10 seconds. The timer would expire
+	 after 10 seconds and the timer call back would get called. In the timer call back
+	 checks are done to ensure that the tasks have been running properly till then.
+	 The tasks are deleted in the timer call back and a message is printed to convey that
+	 the example has run successfully.
+	 The timer expiry is set to 10 seconds and the timer set to not auto reload. */
+	xTimer = xTimerCreate( (const char *) "Timer",
+							x10seconds,
+							pdFALSE,
+							(void *) TIMER_ID,
+							vTimerCallback);
+	/* Check the timer was created. */
+	configASSERT( xTimer );
+
+	/* start the timer with a block time of 0 ticks. This means as soon
+	   as the schedule starts the timer will start running and will expire after
+	   10 seconds */
+	xTimerStart( xTimer, 0 );
 
 	/* Start the tasks and timer running. */
 	vTaskStartScheduler();
@@ -156,7 +190,7 @@ int main( void )
 /*-----------------------------------------------------------*/
 static void prvTxTask( void *pvParameters )
 {
-const TickType_t x1second = pdMS_TO_TICKS( 1000UL );
+const TickType_t x1second = pdMS_TO_TICKS( DELAY_1_SECOND );
 
 	for( ;; )
 	{
@@ -185,6 +219,34 @@ char Recdstring[15] = "";
 
 		/* Print the received data. */
 		xil_printf( "Rx task received string from Tx task: %s\r\n", Recdstring );
+		RxtaskCntr++;
 	}
+}
+
+/*-----------------------------------------------------------*/
+static void vTimerCallback( TimerHandle_t pxTimer )
+{
+	long lTimerId;
+	configASSERT( pxTimer );
+
+	lTimerId = ( long ) pvTimerGetTimerID( pxTimer );
+
+	if (lTimerId != TIMER_ID) {
+		xil_printf("FreeRTOS Hello World Example FAILED");
+	}
+
+	/* If the RxtaskCntr is updated every time the Rx task is called. The
+	 Rx task is called every time the Tx task sends a message. The Tx task
+	 sends a message every 1 second.
+	 The timer expires after 10 seconds. We expect the RxtaskCntr to at least
+	 have a value of 9 (TIMER_CHECK_THRESHOLD) when the timer expires. */
+	if (RxtaskCntr >= TIMER_CHECK_THRESHOLD) {
+		xil_printf("FreeRTOS Hello World Example PASSED");
+	} else {
+		xil_printf("FreeRTOS Hello World Example FAILED");
+	}
+
+	vTaskDelete( xRxTask );
+	vTaskDelete( xTxTask );
 }
 
