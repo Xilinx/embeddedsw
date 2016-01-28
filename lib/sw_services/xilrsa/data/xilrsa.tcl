f68288be46eb9945fb4b600eb158b0bcc8206bce
P L Sai Krishna <lakshmi.sai.krishna.potthuri@xilinx.com>
2016-01-28 18:58:21 +0530

    xilrsa: Added support for Linaro tool chain in tcl.
    This patch modifies the tcl file in such a way that
    based on the compiler it will pick the binary file.
    
    Signed-off-by: P L Sai Krishna <lakshmis@xilinx.com>
    Reviewed-by: Harini Katakam <harinik@xilinx.com>

--- lib/sw_services/xilrsa/data/xilrsa.tcl
+++ lib/sw_services/xilrsa/data/xilrsa.tcl
@@ -36,6 +36,7 @@
 # ----- ---- -------- -----------------------------------------------
 # 1.00  hk   27/01/14 Initial Release
 # 1.1   hk   04/16/14 Error out when processor is not cortexa9. CR # 783276.
+# 1.2   sk   01/28/16 Added support for CortexA9 with Linaro Tool chain.
 #
 ##############################################################################
 
@@ -53,6 +54,15 @@ proc rsa_drc {libhandle} {
                 error "ERROR: This library is supported only for CortexA9 processors.";
                 return;
     }    
+
+    set procdrv [hsi::get_sw_processor]
+    set compiler [common::get_property CONFIG.compiler $procdrv]
+    if {[string compare -nocase $compiler "arm-none-eabi-gcc"] == 0} {
+		file delete -force ./src/librsa.a
+			file rename -force ./src/librsa_linaro.a ./src/librsa.a
+    } elseif {[string compare -nocase $compiler "arm-xilinx-eabi-gcc"] == 0} {
+		file delete -force ./src/librsa_linaro.a
+    }
 }
 
 proc generate {libhandle} {
