d73a1ca611286382b26feff6cd475f2849a51818
Jyotheeswar Reddy <jyothee@xilinx.com>
2016-06-07 07:47:16 -0700

    PMUFW: pm_proc: Fix resume address save and restore handlers for RPU1
    Reported-by: Stephen MacMahon <stephenm@xilinx.com>
    Signed-off-by: Jyotheeswar Reddy <jyothee@xilinx.com>
    Acked-by: Sören Brinkmann <soren.brinkmann@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2016.1-rc2-24-gc50f589ec03e"
+ #define ZYNQMP_XPFW_VERSION	"2016.1-rc2-25-g66a290bdcb77"
  #endif
