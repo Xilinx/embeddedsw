6923b59f614cd91aa7c5f894846994f1a1a943b9
Mirela Simonovic <mirela.simonovic@aggios.com>
2016-07-22 17:07:28 +0530

    PMUFW: pm: Process PmSetWakeUpSource for all masters
    The request needs to be processed for all masters in order to properly
    set the flags used for tracking the set wake-up source requests.
    Compared to the previous implementation, if the slave has been set as
    the wake source the PMU needs to make sure that the slave enters state
    in which it has PM_CAP_WAKEUP capability (the PM_CAP_WAKEUP capability
    doesn't have to be provided with the PmSetRequirement call, a master
    doesn't have to take care about these dependencies).
    If the call is not processed for all masters the PMU will not know that
    there is such a dependency (master wake-up depends on slave's state).
    
    Signed-off-by: Mirela Simonovic <mirela.simonovic@aggios.com>
    Acked-by: Sören Brinkmann <soren.brinkmann@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2016.1-rc2-49-ga785bd9"
+ #define ZYNQMP_XPFW_VERSION	"2016.1-rc2-50-g654ec8f"
  #endif
