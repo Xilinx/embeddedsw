a6f1d6c160eb30b76fce161b23726933e2d031f2
Mirela Simonovic <mirela.simonovic@aggios.com>
2016-08-01 10:31:23 -0700

    PMUFW: pm_usb: Added missing initialization of parent pointers
    This is needed due to the MISRA
    
    Signed-off-by: Mirela Simonovic <mirela.simonovic@aggios.com>
    Acked-for-series: Jyotheeswar Reddy Mutthareddyvari <jyothee@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2016.1-rc2-61-g0e6634d857cd"
+ #define ZYNQMP_XPFW_VERSION	"2016.1-rc2-62-gdefc48d4f264"
  #endif
