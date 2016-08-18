228e122a12285f880a5c425cc31b7a3bcc5694a1
Sam Sortais <sam.sortais@xilinx.com>
2016-08-18 06:13:16 -0700

    openamp: rpc demo zynq7 libmetal
    Signed-off-by: Sam Sortais <sam.sortais@xilinx.com>
    Acked-for-series: Wendy Liang <jliang@xilinx.com>

--- lib/sw_apps/openamp_rpc_demo/src/machine/zynq7/platform_info.c
+++ lib/sw_apps/openamp_rpc_demo/src/machine/zynq7/platform_info.c
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2015 Xilinx, Inc. All rights reserved.
  * Copyright (c) 2014, Mentor Graphics Corporation
  * All rights reserved.
+ * Copyright (c) 2015 Xilinx, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -41,201 +41,63 @@
  **************************************************************************/
 
 #include "openamp/hil.h"
+#include "openamp/remoteproc_plat.h"
+#include "metal/atomic.h"
 #include "platform_info.h"
+
+#define REMOTE_CPU_ID                     0
 
 /* Reference implementation that show cases platform_get_cpu_info and
  platform_get_for_firmware API implementation for Bare metal environment */
 
-extern struct hil_platform_ops proc_ops;
+extern struct hil_platform_ops zynq_a9_proc_ops;
 
-/* IPC Device parameters */
-#define SHM_ADDR                          (void *)0x08008000
-#define SHM_SIZE                          0x00200000
-#define MASTER_CPU_ID                     0
-#define REMOTE_CPU_ID                     1
 
-/**
- * This array provides definition of CPU nodes for master and remote
- * context. It contains two nodes because the same file is intended
- * to use with both master and remote configurations. On zynq platform
- * only one node definition is required for master/remote as there
- * are only two cores present in the platform.
- *
- * Only platform specific info is populated here. Rest of information
- * is obtained during resource table parsing.The platform specific
- * information includes;
- *
- * -CPU ID
- * -Shared Memory
- * -Interrupts
- * -Channel info.
- *
- * Although the channel info is not platform specific information
- * but it is convenient to keep it in HIL so that user can easily
- * provide it without modifying the generic part.
- *
- * It is good idea to define hil_proc structure with platform
- * specific fields populated as this can be easily copied to hil_proc
- * structure passed as parameter in platform_get_processor_info. The
- * other option is to populate the required structures individually
- * and copy them one by one to hil_proc structure in platform_get_processor_info
- * function. The first option is adopted here.
- *
- *
- * 1) First node in the array is intended for the remote contexts and it
- *    defines Master CPU ID, shared memory, interrupts info, number of channels
- *    and there names. This node defines only one channel
- *   "rpmsg-openamp-demo-channel".
- *
- * 2)Second node is required by the master and it defines remote CPU ID,
- *   shared memory and interrupts info. In general no channel info is required by the
- *   Master node, however in bare-metal master and linux remote case the linux
- *   rpmsg bus driver behaves as master so the rpmsg driver on linux side still needs
- *   channel info. This information is not required by the masters for bare-metal
- *   remotes.
- *
- */
+struct rproc_info_plat_local proc_table = {
+	{
+		/* CPU ID of master */
+		REMOTE_CPU_ID,
 
-struct hil_proc proc_table []=
-{
-
-    /* CPU node for remote context */
-    {
-        /* CPU ID of master */
-        MASTER_CPU_ID,
-
-        /* Shared memory info - Last field is not used currently */
-        {
-            SHM_ADDR, SHM_SIZE, 0x00
-        },
-
-        /* VirtIO device info */
-        {
-            /* Leave these three fields empty as these are obtained from rsc
-             * table.
-             */
-             0, 0, 0,
-
-             /* Vring info */
-            {
-
-                {
-                     /* Provide only vring interrupts info here. Other fields are
-                      * obtained from the resource table so leave them empty.
-                      */
-                     NULL, NULL, 0, 0,
-                     {
-                         VRING0_IPI_INTR_VECT,0x1006,1,NULL
-                     }
-                },
-                {
-                    NULL, NULL, 0, 0,
-                    {
-                        VRING1_IPI_INTR_VECT,0x1006,1,NULL
-                    }
-                }
-            }
-        },
-
-        /* Number of RPMSG channels */
-        1,
-
-        /* RPMSG channel info - Only channel name is expected currently */
-        {
-            {"rpmsg-openamp-demo-channel"}
-        },
-
-        /* HIL platform ops table. */
-        &proc_ops,
-
-        /* Next three fields are for future use only */
-        0,
-        0,
-        NULL
-    },
-
-    /* CPU node for remote context */
-    {
-        /* CPU ID of remote */
-        REMOTE_CPU_ID,
-
-        /* Shared memory info - Last field is not used currently */
-        {
-            SHM_ADDR, SHM_SIZE, 0x00
-        },
-
-        /* VirtIO device info */
-        {
-            0, 0, 0,
-            {
-                {
-                    /* Provide vring interrupts info here. Other fields are obtained
-                     * from the rsc table so leave them empty.
-                     */
-                    NULL, NULL, 0, 0,
-                    {
-                        VRING0_IPI_INTR_VECT,0x1006,1
-                    }
-                },
-                {
-                    NULL, NULL, 0, 0,
-                    {
-                        VRING1_IPI_INTR_VECT,0x1006,1
-                    }
-                }
-            }
-        },
-
-        /* Number of RPMSG channels */
-        1,
-
-        /* RPMSG channel info - Only channel name is expected currently */
-        {
-            {"rpmsg-openamp-demo-channel"}
-        },
-
-        /* HIL platform ops table. */
-        &proc_ops,
-
-        /* Next three fields are for future use only */
-        0,
-        0,
-        NULL
-    }
+		/* HIL platform ops table. */
+		&zynq_a9_proc_ops,
+	},
+	/* vring0 ipi device and vring descriptors memory device */
+	{
+		PLAT_RSC_VRING,
+		"generic",
+		"ipi0",
+		NULL,
+		"generic",
+		"vrings",
+	},
+	/* vring1 ipi device and vring descriptors memory device */
+	{
+		PLAT_RSC_VRING,
+		"generic",
+		"ipi1",
+		NULL,
+		"generic",
+		"vrings",
+	},
+	/* Shared memory device */
+	{
+		PLAT_RSC_SHM,
+		"shm",
+		0,  /* UNDEFINED */
+	},
+	/* Shared memory device */
+	{
+		PLAT_RSC_RPMSG_CHANNEL,
+		"rpmsg-openamp-demo-channel",
+	},
+	PLAT_RSC_LAST,
 };
 
-const int proc_table_size = sizeof (proc_table)/sizeof(struct hil_proc);
-
-
-/**
- * platform_get_processor_info
- *
- * Copies the target info from the user defined data structures to
- * HIL proc  data structure.In case of remote contexts this function
- * is called with the reserved CPU ID HIL_RSVD_CPU_ID, because for
- * remotes there is only one master.
- *
- * @param proc   - HIL proc to populate
- * @param cpu_id - CPU ID
- *
- * return  - status of execution
- */
-int platform_get_processor_info(struct hil_proc *proc, int cpu_id)
+const struct firmware_info fw_table[] =
 {
-	int idx;
-	for (idx = 0; idx < sizeof(proc_table) / sizeof(struct hil_proc); idx++) {
-		if ((cpu_id == HIL_RSVD_CPU_ID)
-		    || (proc_table[idx].cpu_id == cpu_id)) {
-			env_memcpy(proc, &proc_table[idx],
-				   sizeof(struct hil_proc));
-			return 0;
-		}
-	}
-	return -1;
-}
+	{"unknown",
+	 0,
+	 0}
+};
 
-int platform_get_processor_for_fw(char *fw_name)
-{
-
-	return 1;
-}
+int fw_table_size = sizeof(fw_table)/sizeof(struct firmware_info);
