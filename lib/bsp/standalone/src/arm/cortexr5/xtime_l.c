161ffd5023ea20be115851b849c03a9fcc4f1c04
Mubin Sayyed <mubin.usman.sayyed@xilinx.com>
2016-09-07 14:16:23 +0530

    BSP:r5:Corrected debug print syntax
    Corrected debug print syntax to fix compilation errors
    in BSP, built with "DEBUG" flag.
    
    Signed-off-by: Mubin Sayyed <mubinusm@xilinx.com>
    Acked-by: Kinjal Pravinbhai Patel <patelki@xilinx.com>

--- lib/bsp/standalone/src/arm/cortexr5/xtime_l.c
+++ lib/bsp/standalone/src/arm/cortexr5/xtime_l.c
@@ -153,7 +153,8 @@ void XTime_StartTimer(void)
 void XTime_SetTime(XTime Xtime_Global)
 {
 /*Timer cannot be set to desired value, so the API is left unimplemented*/
-    xdbg_printf("XTime_SetTime:Timer cannot be set to desired value, so API is not implemented\n");
+    xdbg_printf(XDBG_DEBUG_GENERAL,
+                "XTime_SetTime:Timer cannot be set to desired value,so API is not implemented\n");
 }
 
 /****************************************************************************
