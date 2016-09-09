2cba9162033d85321473895cf53807e201d16560
Sam Sortais <sam.sortais@xilinx.com>
2016-09-09 15:30:56 -0700

    openamp: mat and rpc apps use vectbl in TCM
    TCM is preferred to OCM for vector table, it reduce the risk of having
    two R5 conflicting in setting their vec table at same location in OCM.
    TRM also recommend using TCM for lower jitter and reduced latency.
    
    Signed-off-by: Sam Sortais <sam.sortais@xilinx.com>
    Acked-by: Wendy Liang <jliang@xilinx.com>

--- lib/sw_apps/openamp_matrix_multiply/src/machine/zynqmp_r5/rsc_table.c
+++ lib/sw_apps/openamp_matrix_multiply/src/machine/zynqmp_r5/rsc_table.c
@@ -41,6 +41,7 @@
 
 #define RPMSG_IPU_C0_FEATURES        1
 
+
 /* VirtIO rpmsg device id */
 #define VIRTIO_ID_RPMSG_             7
 
@@ -56,21 +57,20 @@
 #define TCM_0_START_PA              0xFFE00000
 #define TCM_1_START_DA              0x00020000
 #define TCM_1_LEN                   0x10000
-#define TCM_1_START_PA              0xFFE40000
+#define TCM_1_START_PA              0xFFE20000
 #define DDR_ELF_START               0x3ED00000
-#define DDR_ELF_LEN                 0x40000
+#define DDR_ELF_LEN                	0x40000
 #define NUM_VRINGS                  0x02
 #define VRING_ALIGN                 0x1000
 #define RING_TX                     0x3ED40000
 #define RING_RX                     0x3ED44000
 #define VRING_SIZE                  256
-
 #define NUM_TABLE_ENTRIES           3
-#define CARVEOUT_SRC_OFFSETS        offsetof(struct remote_resource_table, ocm_1_cout), \
-							        offsetof(struct remote_resource_table, ddr_cout),
+#define CARVEOUT_SRC_OFFSETS        offsetof(struct remote_resource_table, tcm_0_cout), \
+                                    offsetof(struct remote_resource_table, ddr_cout), \
 
-#define CARVEOUT_SRC                {RSC_CARVEOUT, OCM_1_START, OCM_1_START, OCM_1_LEN, 0, 0, "OCM1_COUT",}, \
-					                {RSC_CARVEOUT, DDR_ELF_START, DDR_ELF_START, DDR_ELF_LEN, 0, 0, "ELF_DATA_COUT",},
+#define CARVEOUT_SRC                {RSC_CARVEOUT, TCM_0_START_DA, TCM_0_START_PA, TCM_0_LEN, 0, 0, "TCM0_COUT",}, \
+                                    {RSC_CARVEOUT, DDR_ELF_START, DDR_ELF_START, DDR_ELF_LEN, 0, 0, "DDR_COUT",}, \
 
 
 const struct remote_resource_table __resource resources =
