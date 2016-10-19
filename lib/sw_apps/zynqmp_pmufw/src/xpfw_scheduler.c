f85855939de2a992a00504ad76e3dca2265033ca
Venkata Naga Sai Krishna Kolapalli <venkata.naga.sai.krishna.kolapalli@xilinx.com>
2016-10-19 09:26:51 -0700

    PMUFW: MISRA-C:2012 Compliance: Fix Rule 12.1 violations
    This patch modifies the pmufw code to comply for misra rule R.12.1
    which says "The precedence of operators within expressions should
    be made explicit"
    
    Signed-off-by: Venkata Naga Sai Krishna Kolapalli <venkatan@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_scheduler.c
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_scheduler.c
@@ -185,7 +185,7 @@ XStatus XPfw_SchedulerRemoveTask(XPfw_Scheduler_t *SchedPtr, u32 OwnerId, u32 Mi
 	for (Idx = 0U; Idx < XPFW_SCHED_MAX_TASK; Idx++) {
 		if ((Callback == SchedPtr->TaskList[Idx].Callback) &&
 		    (SchedPtr->TaskList[Idx].OwnerId == OwnerId) &&
-		    ((SchedPtr->TaskList[Idx].Interval == MilliSeconds/TICK_MILLISECONDS) ||
+		    ((SchedPtr->TaskList[Idx].Interval == (MilliSeconds/TICK_MILLISECONDS)) ||
 				(0U == MilliSeconds))) {
 			SchedPtr->TaskList[Idx].Interval = 0U;
 			SchedPtr->TaskList[Idx].OwnerId = 0U;
