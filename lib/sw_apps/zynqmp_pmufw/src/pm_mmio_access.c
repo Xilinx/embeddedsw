a9254fc5a2679b18644dc8e9a815bfbe23ce43fc
Jyotheeswar Reddy <jyotheeswar.reddy.mutthareddyvari@xilinx.com>
2016-10-19 09:26:52 -0700

    PMUFW: pm_mmio: Open up MIO registers in IOU_SLCR for APU
    MIO registers need to be accessed to perform I2C bus recovery.
    
    Signed-off-by: Jyotheeswar Reddy <jyothee@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/pm_mmio_access.c
+++ lib/sw_apps/zynqmp_pmufw/src/pm_mmio_access.c
@@ -128,7 +128,7 @@ static const PmAccessRegion pmAccessTable[] = {
 
 	/* IOU SLCR Registers required for Linux */
 	{
-		.startAddr = PM_MMIO_IOU_SLCR_BASE + 0x300,
+		.startAddr = PM_MMIO_IOU_SLCR_BASE,
 		.endAddr = PM_MMIO_IOU_SLCR_BASE + 0x524,
 		.access = MMIO_ACCESS_RW(IPI_PMU_0_IER_APU_MASK),
 	},
