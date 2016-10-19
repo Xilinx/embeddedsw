7fcab26294cd1b340c59744f9d628b782d2b25c1
Soren Brinkmann <soren.brinkmann@xilinx.com>
2016-10-19 09:26:31 -0700

    PMUFW: pm: DDR: Postfix numeric constants with 'U'
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>
    Acked-for-series: Jyotheeswar Reddy Mutthareddyvari <jyothee@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2016.3-3-g73c90d715c50"
+ #define ZYNQMP_XPFW_VERSION	"2016.3-4-gffd01841d0be"
  #endif
