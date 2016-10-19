b082624d0d46a45d7b19a141cd53ca0aa0c27753
Soren Brinkmann <soren.brinkmann@xilinx.com>
2016-10-19 09:26:37 -0700

    PMUFW: pm: mmio: Remove unused/dummy regions
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>
    Acked-for-series: Jyotheeswar Reddy Mutthareddyvari <jyothee@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2016.3-9-gdcc0553fc37d"
+ #define ZYNQMP_XPFW_VERSION	"2016.3-10-gaa316c0630f8"
  #endif
