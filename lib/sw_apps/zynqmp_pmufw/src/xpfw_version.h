ddd15b733e718f4c8eac883b608f2ed8f999fcd8
Raja Ravindra Jyothula <raja.ravindra.jyothula@xilinx.com>
2016-10-19 09:26:54 -0700

    PMUFW: pm_proc.c, pm_proc.h: MISRA-C:2012: Fix R.20.7 violations
    This patch adds brackets for macro parameters.
    
    Signed-off-by: Raja Ravindra Jyothula <rajaravi@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2016.3-26-gacfffb5b0f6c"
+ #define ZYNQMP_XPFW_VERSION	"2016.3-27-ge8c85f4df937"
  #endif
