1fff61d1b6717a3dc240f7099c0ff69511adb532
Raja Ravindra Jyothula <raja.ravindra.jyothula@xilinx.com>
2016-10-19 09:26:54 -0700

    PMUFW: pm_proc.c, pm_proc.h: MISRA-C:2012: Fix R.20.7 violations
    This patch adds brackets for macro parameters.
    
    Signed-off-by: Raja Ravindra Jyothula <rajaravi@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/pm_proc.h
+++ lib/sw_apps/zynqmp_pmufw/src/pm_proc.h
@@ -55,7 +55,7 @@ typedef u8 PmProcEvent;
 #define PM_PROC_RPU_1   1U
 #define PM_PROC_RPU_MAX 2U
 
-#define DISABLE_WFI(mask)   XPfw_RMW32(PMU_LOCAL_GPI2_ENABLE, mask, ~(mask));
+#define DISABLE_WFI(mask)   XPfw_RMW32(PMU_LOCAL_GPI2_ENABLE, (mask), ~(mask));
 
 /*
  * Processor is powered down as requested by a master which is priviledged
