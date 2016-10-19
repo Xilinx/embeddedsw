e1960a822400fe501817ccb8b880d071ba7e3fe8
Soren Brinkmann <soren.brinkmann@xilinx.com>
2016-10-19 09:26:30 -0700

    PMUFW: pm: DDR: Trigger RDIMM init
    Trigger RDIMM initialization if an RDIMM is detected.
    
    Signed-off-by: Soren Brinkmann <soren.brinkmann@xilinx.com>
    Acked-for-series: Jyotheeswar Reddy Mutthareddyvari <jyothee@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2016.3-2-gee2e7fbf4fc4"
+ #define ZYNQMP_XPFW_VERSION	"2016.3-3-g73c90d715c50"
  #endif
