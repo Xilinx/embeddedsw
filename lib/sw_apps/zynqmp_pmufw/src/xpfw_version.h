a5e8b01f88eda156077d6a945782886bca634cb7
Raja Ravindra Jyothula <raja.ravindra.jyothula@xilinx.com>
2016-11-01 11:27:59 +0530

    PMUFW: pm_callbacks: MISRA-C:2012: Fix R.20.7 violations
    Violation: Macro parameter not in brackets.
    Rule: Expressions resulting from the expansion of macro
     parameters shall be enclosed in parenthesis.
    
    This patch adds brackets for every macro parameter.
    
    Signed-off-by: Raja Ravindra Jyothula <rajaravi@xilinx.com>
    Acked-by: Sören Brinkmann <soren.brinkmann@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2016.4-rc1-4-g01afb2d"
+ #define ZYNQMP_XPFW_VERSION	"2016.4-rc1-5-gafbfa4c"
  #endif
