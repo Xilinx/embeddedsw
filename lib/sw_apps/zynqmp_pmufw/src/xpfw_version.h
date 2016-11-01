2baa457c8795b66acc3a59a89c8b9bc45d4d20d1
Raja Ravindra Jyothula <raja.ravindra.jyothula@xilinx.com>
2016-11-01 11:28:00 +0530

    PMUFW: pm_clock: MISRA-C:2012: Fix R.8.2 violations
    Violation: Empty parameter list to procedure/function.
    Rule: Function types shall be in prototyped form with named
    parameters.
    
    This patch adds “void “ to parameter list.
    
    Signed-off-by: Raja Ravindra Jyothula <rajaravi@xilinx.com>
    Acked-by: Sören Brinkmann <soren.brinkmann@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2016.4-rc1-5-gafbfa4c"
+ #define ZYNQMP_XPFW_VERSION	"2016.4-rc1-6-gf418f0f"
  #endif
