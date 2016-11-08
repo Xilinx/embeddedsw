424ec1d87ac5e7a722372c5702886b816cbb5ca1
VNSL Durga <vnsl.durga.challa@xilinx.com>
2016-11-08 17:23:52 +0530

    sw_apps:zynqmp_fsbl:Removing PL isolation by calling API from psu_init.c
    PS PL isolation is removed by calling psu_ps_pl_isolation_removal_data
    procedure from psu_init.c instead of using fsbl local API XFsbl_PowerUpIsland.
    psu_ps_pl_isolation_removal_data function also has AXI data width
    configurations which needs to be configured by fsbl.
    
    Signed-off-by: VNSL Durga <vnsldurg@xilinx.com>
    Acked-by: Sarat Chand Savitala <saratcha@xilinx.com>

--- lib/sw_apps/zynqmp_fsbl/src/xfsbl_partition_load.c
+++ lib/sw_apps/zynqmp_fsbl/src/xfsbl_partition_load.c
@@ -1406,13 +1406,7 @@ static u32 XFsbl_PartitionValidation(XFsblPs * FsblInstancePtr,
 		 * PL is powered-up before its configuration, but will be in isolation.
 		 * Now since PL configuration is done, just remove the isolation
 		 */
-		Status = XFsbl_PowerUpIsland(PMU_GLOBAL_PWR_STATE_PL_MASK);
-
-		if (Status != XFSBL_SUCCESS) {
-			Status = XFSBL_ERROR_PL_POWER_UP;
-			XFsbl_Printf(DEBUG_GENERAL, "XFSBL_ERROR_PL_POWER_UP\r\n");
-			goto END;
-		}
+		psu_ps_pl_isolation_removal_data();
 
 		/* Reset PL, if configured for */
 		psu_ps_pl_reset_config_data();
