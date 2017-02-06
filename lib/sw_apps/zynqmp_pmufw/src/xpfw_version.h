bd00a34ef49ebd2190c6550455424410ec456502
Jyotheeswar Reddy <jyotheeswar.reddy.mutthareddyvari@xilinx.com>
2017-02-06 21:52:08 +0530

    PMUFW: pm: Bump up API version to 0.3
    PM now needs a config object to be loaded before the framework
    can be used, which requires a version match between PMUFW and client
    API to ensure correct functionality. So bump up the version.
    
    Signed-off-by: Jyotheeswar Reddy <jyothee@xilinx.com>
    Acked-by: Will Wong <willw@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2017.1-rc3-1-g7dbfc9e"
+ #define ZYNQMP_XPFW_VERSION	"2017.1-rc3-2-g5b306f6"
  #endif
