1634fdbdd7f2a700c961e3e2e2d67e048ba60c32
Mirela Simonovic <mirela.simonovic@aggios.com>
2017-02-13 20:33:02 +0100

    PMUFW: pm_pll: Fix pointer to PLL node class (not slaves anymore)
    Signed-off-by: Mirela Simonovic <mirela.simonovic@aggios.com>
    Acked-for-series: Will Wong <willw@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2017.1-rc4-18-gcb5407b"
+ #define ZYNQMP_XPFW_VERSION	"2017.1-rc4-19-g0e031a3"
  #endif
