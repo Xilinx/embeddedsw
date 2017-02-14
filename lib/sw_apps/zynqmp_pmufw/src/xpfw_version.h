904070eb3949ea4e37d3156c76a46b1ceb652f56
Filip Drazic <filip.drazic@aggios.com>
2017-02-14 18:50:03 +0100

    PMUFW: pm_pll: Fix function pointer to get power consumption data for PLLs
    Signed-off-by: Filip Drazic <filip.drazic@aggios.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2017.1-rc5-17-gc477dac"
+ #define ZYNQMP_XPFW_VERSION	"2017.1-rc5-18-gbc90edd"
  #endif
