708977e5c0be50a99df05dd27ad32713d65d3972
Mounika Grace Akula <mounika.grace.akula@xilinx.com>
2017-02-17 23:29:11 +0530

    PMUFW: ipi: Use xparameters for ipi masks only if they are defined
    Currently ipi masks are defined using macros from xparameters. Based on
    how the user configures ipi channels in PCW, some of these macros may not
    be defined and may break the build. So use xparameters only if they are
    defined, else make the mask zero.
    
    Signed-off-by: Mounika Grace Akula <makula@xilinx.com>
    Signed-off-by: Jyotheeswar Reddy Mutthareddyvari <jyothee@xilinx.com>
    Acked-for-series: Will Wong <willw@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2017.1-rc5-1-g0f40c96"
+ #define ZYNQMP_XPFW_VERSION	"2017.1-rc5-2-gfb1405d"
  #endif
