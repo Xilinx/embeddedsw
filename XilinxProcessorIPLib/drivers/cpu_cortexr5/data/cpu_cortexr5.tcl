c43cf2953ea311e7f0c9ee2e12146f38ded445af
Mubin Sayyed <mubin.usman.sayyed@xilinx.com>
2017-02-20 15:36:13 +0530

    cpu_cortexr5: Guard xparameters.h by protection macros
    This patch updates tcl to add protection macros for
    xparameters.h
    
    Signed-off-by: Mubin Sayyed <mubinusm@xilinx.com>
    Acked-for-series: Anirudha Sarangi <anirudh@xilinx.com>

--- XilinxProcessorIPLib/drivers/cpu_cortexr5/data/cpu_cortexr5.tcl
+++ XilinxProcessorIPLib/drivers/cpu_cortexr5/data/cpu_cortexr5.tcl
@@ -35,6 +35,7 @@
 # Ver   Who  Date     Changes
 # ----- ---- -------- -----------------------------------------------
 # 1.00  pkp  07/21/14 Initial common::version
+# 1.2   mus  02/20/17 Updated tcl to guard xparameters.h by protection macros
 #
 ##############################################################################
 #uses "xillib.tcl"
@@ -72,6 +73,9 @@ proc xdefine_cortexr5_params {drvhandle} {
     set lprocs [lsort $lprocs]
 
     set config_inc [::hsi::utils::open_include_file "xparameters.h"]
+    puts $config_inc "#ifndef XPARAMETERS_H   /* prevent circular inclusions */"
+    puts $config_inc "#define XPARAMETERS_H   /* by using protection macros */"
+    puts $config_inc ""
     puts $config_inc "/* Definition for CPU ID */"
 
     foreach periph $periphs {
@@ -175,4 +179,16 @@ proc xdefine_addr_params_for_ext_intf {drvhandle file_name} {
       }
 
     close $file_handle
+}
+
+proc post_generate_final {drv_handle} {
+
+	set type [get_property CLASS $drv_handle]
+	if {[string equal $type "driver"]} {
+	   return
+	}
+
+	set file_handle [::hsi::utils::open_include_file "xparameters.h"]
+	puts $file_handle "#endif  /* end of protection macro */"
+	close $file_handle
 }
