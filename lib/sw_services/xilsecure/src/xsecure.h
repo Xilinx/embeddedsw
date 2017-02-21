10c7972094ba7b3a9a1c32227378004a6876a811
Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
2017-02-21 16:59:08 +0530

    sw_services: xilsecure: Add interface to support secure image loading
    Add interface support to load secure image loading using the
    xilsecure APIs. This supports loading of encrypted, authenticated
    or both encrypted and authenticated images based on flags
    received.
    
    Signed-off-by: Siva Durga Prasad Paladugu <sivadur@xilinx.com>
    Acked-by: Durga Challa <vnsldurg@xilinx.com>

---
+++ lib/sw_services/xilsecure/src/xsecure.h
@@ -0,0 +1,92 @@
+/******************************************************************************
+*
+* Copyright (C) 2017 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running on a Xilinx device, or
+* (b) that interact with a Xilinx device through a bus or interconnect.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+
+/*****************************************************************************/
+/**
+*
+* @file xsecure.h
+*
+* This is the header file which contains secure library interface function prototype
+* for authentication and decryption of images.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who  Date        Changes
+* ----- ---- -------- -------------------------------------------------------
+* 1.00  dp   02/15/17 Initial release
+*
+* </pre>
+*
+* @note
+*
+******************************************************************************/
+
+#ifndef XSECURE_H
+#define XSECURE_H
+
+/************************** Include Files ***********************************/
+
+/************************** Constant Definitions *****************************/
+
+#define XSECURE_AES		1
+#define XSECURE_RSA		2
+#define XSECURE_RSA_AES		3
+#define XSECURE_MASK		(XSECURE_AES | XSECURE_RSA)
+
+#define XSECURE_KEY_STR_LEN		64	/* String length */
+#define XSECURE_IV_STR_LEN		24	/* String length */
+#define XSECURE_KEY_LEN			8
+#define XSECURE_IV_LEN			3
+#define XSECURE_GCM_TAG_LEN		128
+#define XSECURE_WORD_LEN		4
+#define XSECURE_MAX_NIBBLES		8
+
+#define XSECURE_ARRAY_LENGTH(array) (sizeof((array))/sizeof((array)[0]))
+
+#define XSECURE_ERROR_CSUDMA_INIT_FAIL	0x1
+#define XSECURE_STRING_INVALID_ERROR	0x2
+#define XSECURE_INVALID_FLAG		0x3
+
+#define XSECURE_CSUDMA_DEVICEID		0
+
+/**************************** Type Definitions *******************************/
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+/************************** Function Prototypes ******************************/
+
+u32 XSecure_RsaAes(u32 SrcAddrHigh, u32 SrcAddrLow, u32 WrSize, u32 flags);
+
+/************************** Variable Definitions *****************************/
+
+#endif  /* XSECURE_HW_H */
