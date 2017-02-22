869248a33a5784662f0bd207d578c83a68918a3f
Raja Ravindra Jyothula <raja.ravindra.jyothula@xilinx.com>
2017-02-22 16:39:08 +0530

    PMUFW: xpfw_scheduler: MISRA-C:2012: Fix R.7.2 violations
    Violation: Literal value requires a U suffix.
    Rule: A “u” or “U” suffix shall be applied to all integer
     constants that are represented in an unsigned type.
    
    This patch applies “U” suffix for unsigned integers.
    
    Signed-off-by: Raja Ravindra Jyothula <rajaravi@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_scheduler.c
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_scheduler.c
@@ -233,5 +233,5 @@ XStatus XPfw_SchedulerRemoveTask(XPfw_Scheduler_t *SchedPtr, u32 OwnerId, u32 Mi
 
 	fw_printf("%s: Removed %lu tasks\r\n", __func__, TaskCount);
 
-	return ((TaskCount > 0) ? XST_SUCCESS : XST_FAILURE);
+	return ((TaskCount > 0U) ? XST_SUCCESS : XST_FAILURE);
 }
