148a11ea84dcf98d309220493aa80029bf497f19
Bharath Odedara <bharath.odedara@xilinx.com>
2017-02-24 04:53:27 -0500

    driver:cpu_cortexa53:Added zynqmp_fsbl_bsp check in cpu_cortexa53 tcl
    This patch will do condition check in cpu_cortexa53 tcl,if bsp need to generate for fsbl app.
    Then cpu_cortexa53 tcl will add LTO related flags in fsbl bsp ECC_FLAGS.
    
    Signed-off-by: Bharath Odedara <bharatho@xilinx.com>
    Signed-off-by: Mubin Sayyed <mubinusm@xilinx.com>
    
    Changes for v2:
    	- Moved this changes from standalone tcl to cpu_cortexa53 tcl

--- XilinxProcessorIPLib/drivers/cpu_cortexa53/data/cpu_cortexa53.tcl
+++ XilinxProcessorIPLib/drivers/cpu_cortexa53/data/cpu_cortexa53.tcl
@@ -63,6 +63,18 @@ proc xdefine_cortexa53_params {drvhandle} {
 	common::set_property -name {EXTRA_COMPILER_FLAGS} -value $new_flags -objects [hsi::get_sw_processor]
 
     }
+
+    #Append LTO flag in EXTRA_COMPILER_FLAGS for zynqmp_fsbl_bsp
+    set is_zynqmp_fsbl_bsp [common::get_property CONFIG.ZYNQMP_FSBL_BSP [hsi::get_os]]
+    if {$is_zynqmp_fsbl_bsp == TRUE} {
+		set extra_flags [common::get_property CONFIG.extra_compiler_flags [hsi::get_sw_processor]]
+		#Append LTO flag in EXTRA_COMPILER_FLAGS if not exist previoulsy.
+		if {[string first "-flto" $extra_flags] == -1 } {
+			append extra_flags " -Os -flto -ffat-lto-objects"
+			common::set_property -name {EXTRA_COMPILER_FLAGS} -value $extra_flags -objects [hsi::get_sw_processor]
+		}
+    }
+
     set archiver [common::get_property CONFIG.archiver $procdrv]
     if {[string first "iarchive" $archiver] < 0 } {
     } else {
