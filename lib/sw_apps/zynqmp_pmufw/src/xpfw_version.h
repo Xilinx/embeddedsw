2bfa32bece87052918dfb57970e943744f4eaacd
Mirela Simonovic <mirela.simonovic@aggios.com>
2017-02-27 15:52:48 +0100

    PMUFW: pm_proc: APU_1/2/3 processors are initially forced down
    Sleep state is reserved for processors that have been suspended
    (have context). Since APU_1/2/3 have not been suspended (have no
    context). Their state is initially forced off.
    
    Signed-off-by: Mirela Simonovic <mirela.simonovic@aggios.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2017.1-rc5-12-gbe9fbf1"
+ #define ZYNQMP_XPFW_VERSION	"2017.1-rc5-13-gc0e8992"
  #endif
