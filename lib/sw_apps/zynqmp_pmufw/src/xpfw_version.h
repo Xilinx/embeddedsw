d5235e25c455a011215e4916b2f582912cb57b6d
Mirela Simonovic <mirela.simonovic@aggios.com>
2017-02-27 15:52:43 +0100

    PMUFW: pm_master: Fix suspendTimeout value in set config for masters
    Signed-off-by: Mirela Simonovic <mirela.simonovic@aggios.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2017.1-rc5-7-g1a862fd"
+ #define ZYNQMP_XPFW_VERSION	"2017.1-rc5-8-g75c008b"
  #endif
