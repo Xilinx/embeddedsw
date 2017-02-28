c499f2f31079efab9cd0acae2e4d6180aa9a5012
Mirela Simonovic <mirela.simonovic@aggios.com>
2017-02-28 19:00:53 +0100

    PMUFW: Revert "pm: config: Do not power down unusable nodes by default"
    This reverts commit 7dbfc9e930af62df08b24d491bc1b003deaafb5a.

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2017.1-rc5-30-g5401df5"
+ #define ZYNQMP_XPFW_VERSION	"2017.1-rc5-31-gc02d9b5"
  #endif
