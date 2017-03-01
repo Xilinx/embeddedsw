c58b526c39b0bc1896e7ac6578fd55babb62dc6b
Sam Sortais <sam.sortais@xilinx.com>
2017-03-01 10:28:34 -0800

    openamp: mat mul adapted for SDK
    Signed-off-by: Sam Sortais <sam.sortais@xilinx.com>

--- lib/sw_apps/openamp_matrix_multiply/src/system/generic/matrix_multiply.c
+++ lib/sw_apps/openamp_matrix_multiply/src/system/generic/matrix_multiply.c
@@ -1,11 +1,79 @@
-/* This is a sample demonstration application that showcases usage of remoteproc
-and rpmsg APIs on the remote core. This application is meant to run on the remote CPU
-running baremetal code. This applicationr receives two matrices from the master,
-multiplies them and returns the result to the master core. */
+/*
+ * Copyright (c) 2014, Mentor Graphics Corporation
+ * All rights reserved.
+ *
+ * Copyright (C) 2015 Xilinx, Inc.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of Mentor Graphics Corporation nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+/**************************************************************************************
+* This is a sample demonstration application that showcases usage of remoteproc
+* and rpmsg APIs on the remote core. This application is meant to run on the remote CPU
+* running bare-metal code. It receives two matrices from the master,
+* multiplies them and returns the result to the master core.
+*
+* The init_system is called in the main function which defines a shared memory region in
+* MPU settings for the communication between master and remote using
+* zynqMP_r5_map_mem_region API,it also initializes interrupt controller
+* GIC and register the interrupt service routine for IPI using
+* zynqMP_r5_gic_initialize API.
+*
+* The remoteproc_resource_init API is being called to create the virtio/RPMsg devices
+* required for IPC with the master context. Invocation of this API causes remoteproc on
+* the bare-metal to use the rpmsg name service announcement feature to advertise the
+* rpmsg channels served by the application.
+*
+* The master receives the advertisement messages and performs the following tasks:
+* 	1. Invokes the channel created callback registered by the master application
+* 	2. Responds to remote context with a name service acknowledgement message
+* After the acknowledgement is received from master, remoteproc on the bare-metal
+* invokes the RPMsg channel-created callback registered by the remote application.
+* The RPMsg channel is established at this point. All RPMsg APIs can be used subsequently
+* on both sides for run time communications between the master and remote software contexts.
+*
+* Upon running the master application to send data to remote core, master will
+* generate the matrices and send to remote (bare-metal) by informing the bare-metal with
+* an IPI, the remote will compute the resulting matrix and send the data back to
+* master. Once the application is ran and task by the
+* bare-metal application is done, master needs to properly shut down the remote
+* processor
+*
+* To shut down the remote processor, the following steps are performed:
+* 	1. The master application sends an application-specific shutdown message
+* 	   to the remote context
+* 	2. This bare-metal application cleans up application resources,
+* 	   sends a shutdown acknowledge to master, and invokes remoteproc_resource_deinit
+* 	   API to de-initialize remoteproc on the bare-metal side.
+* 	3. On receiving the shutdown acknowledge message, the master application invokes
+* 	   the remoteproc_shutdown API to shut down the remote processor and de-initialize
+* 	   remoteproc using remoteproc_deinit on its side.
+*
+**************************************************************************************/
+
+#include "xil_printf.h"
 #include "openamp/open_amp.h"
 #include "rsc_table.h"
 #include "platform_info.h"
@@ -15,8 +83,7 @@ multiplies them and returns the result to the master core. */
 
 #define SHUTDOWN_MSG	0xEF56A55A
 
-//#define LPRINTF(format, ...) printf(format, ##__VA_ARGS__)
-#define LPRINTF(format, ...)
+#define LPRINTF(format, ...) xil_printf(format, ##__VA_ARGS__)
 #define LPERROR(format, ...) LPRINTF("ERROR: " format, ##__VA_ARGS__)
 
 typedef struct _matrix {
@@ -130,7 +197,7 @@ int app(struct hil_proc *hproc)
 /*-----------------------------------------------------------------------------*
  *  Application entry point
  *-----------------------------------------------------------------------------*/
-int main(int argc, char *argv[])
+int main(void)
 {
 	unsigned long proc_id = 0;
 	unsigned long rsc_id = 0;
@@ -141,14 +208,6 @@ int main(int argc, char *argv[])
 
 	/* Initialize HW system components */
 	init_system();
-
-	if (argc >= 2) {
-		proc_id = strtoul(argv[1], NULL, 0);
-	}
-
-	if (argc >= 3) {
-		rsc_id = strtoul(argv[2], NULL, 0);
-	}
 
 	/* Create HIL proc */
 	hproc = platform_create_proc(proc_id);
@@ -165,7 +224,12 @@ int main(int argc, char *argv[])
 	}
 
 	LPRINTF("Stopping application...\n");
-
 	cleanup_system();
+
+	/* Suspend processor execution */
+	while (1) {
+		__asm__("wfi\n\t");
+	}
+
 	return status;
 }
