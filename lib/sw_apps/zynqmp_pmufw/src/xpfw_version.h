e9884d3c26ac73b41c0469765898d7f67b07498d
Mirela Simonovic <mirela.simonovic@aggios.com>
2017-03-03 13:18:14 +0100

    PMUFW: pm_master: If subsystem restart is requested for non-APU report error
    Only APU restart is currently supported. Check if the master is APU,
    if it's not APU return and report XST_NO_FEATURE error.
    
    Signed-off-by: Mirela Simonovic <mirela.simonovic@aggios.com>
    Acked-for-series: Will Wong <willw@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2017.1-rc6-2-g29adf0f"
+ #define ZYNQMP_XPFW_VERSION	"2017.1-rc6-3-gd8f46c5"
  #endif
