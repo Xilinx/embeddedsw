52499289baba1f62fecd36648fa9728ce0240423
Jyotheeswar Reddy <jyothee@xilinx.com>
2017-03-04 16:17:51 +0530

    PMUFW: Revert "Revert "pm: config: Do not power down unusable nodes by default"" Acked-for-series: Will Wong <willw@xilinx.com>
    This reverts commit c02d9b589c63d26a2406b80cb7204c01cfede5fe.
    It is still breaking the latest Linux boot. This can be reverted after
    we debug and fix that issue, it may be in Linux or PMUFW.

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2017.1-rc6-4-gf8a8484"
+ #define ZYNQMP_XPFW_VERSION	"2017.1-rc6-5-g0c0ce8a"
  #endif
