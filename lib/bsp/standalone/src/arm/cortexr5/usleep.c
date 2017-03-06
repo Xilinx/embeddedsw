235ed2670b9fdbdba13b0523e5fad4aa3cfb7dd3
Mubin Sayyed <mubin.usman.sayyed@xilinx.com>
2017-03-06 21:13:23 +0530

    BSP:R5: Fixed MISRA C mandatory standard violations
    This patch fixes following MISRA C mandatory standard violation,
    
    Rule: MISRA-C:2012 R.9.1: The value of an object with automatic
                              storage duration shall not be read before
                              it has been set.
    Violation:A particular variable is used before it has explicitly been
              given a value
    
    Signed-off-by: Mubin Sayyed <mubinusm@xilinx.com>
    Acked-for-series: Anirudha Sarangi <anirudh@xilinx.com>

--- lib/bsp/standalone/src/arm/cortexr5/usleep.c
+++ lib/bsp/standalone/src/arm/cortexr5/usleep.c
@@ -91,8 +91,8 @@ int usleep(unsigned long useconds)
 	u64 tEnd;
 	u64 tCur;
 	u32 TimeHighVal;
-	XTime TimeLowVal1;
-	XTime TimeLowVal2;
+	XTime TimeLowVal1 = 0U;
+	XTime TimeLowVal2 = 0U;
 
 	TimeHighVal = 0;
 
