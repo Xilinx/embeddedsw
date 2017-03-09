bb825fbd1c69010761b15ac4d8bb63c68ac242f6
Manish Narani <manish.narani@xilinx.com>
2017-03-09 10:03:02 +0530

    usbpsu: fixed required MISRA-C rule R.15.6
    This patch fixes MISRA-C:2012 R.15.6 rule which is a required
    stardard.
    
    Signed-off-by: Manish Narani <mnarani@xilinx.com>
    Acked-for-series: Srinivas Goud <sgoud@xilinx.com>

--- XilinxProcessorIPLib/drivers/usbpsu/src/xusbpsu_intr.c
+++ XilinxProcessorIPLib/drivers/usbpsu/src/xusbpsu_intr.c
@@ -133,8 +133,9 @@ void XUsbPsu_DisconnectIntr(struct XUsbPsu *InstancePtr)
 	InstancePtr->Speed = XUSBPSU_SPEED_UNKNOWN;
 
 	/* Call the handler if necessary */
-	if (InstancePtr->DisconnectIntrHandler != NULL)
+	if (InstancePtr->DisconnectIntrHandler != NULL) {
 		InstancePtr->DisconnectIntrHandler(InstancePtr);
+	}
 }
 
 /****************************************************************************/
@@ -174,8 +175,9 @@ void XUsbPsu_ResetIntr(struct XUsbPsu *InstancePtr)
 	XUsbPsu_WriteReg(InstancePtr, XUSBPSU_DCFG, RegVal);
 
 	/* Call the handler if necessary */
-	if (InstancePtr->ResetIntrHandler != NULL)
+	if (InstancePtr->ResetIntrHandler != NULL) {
 		InstancePtr->ResetIntrHandler(InstancePtr);
+	}
 }
 
 /****************************************************************************/
