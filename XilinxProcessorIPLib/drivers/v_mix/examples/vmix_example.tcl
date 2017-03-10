439c089da79b2dc2c6ae427c7ec81e58990c262c
Vanessa Chou <vanessay@xilinx.com>
2017-03-09 18:16:30 -0800

    v_mix: updated processor name in example to reflect change in hardware example design
    Signed-off-by: Vanessa Chou <vanessay@xilinx.com>
    Acked-by: Rohit Consul <rohitco@xilinx.com>
    CR:971470

--- XilinxProcessorIPLib/drivers/v_mix/examples/vmix_example.tcl
+++ XilinxProcessorIPLib/drivers/v_mix/examples/vmix_example.tcl
@@ -18,11 +18,11 @@ proc vmix_example args {
 
 		#create bsp
 		puts "Create BSP"
-		sdk createbsp -name vmix_example_bsp -hwproject vmix_example_hw_platform -proc microblaze_ss_microblaze_0 -os standalone
+		sdk createbsp -name vmix_example_bsp -hwproject vmix_example_hw_platform -proc processor_ss_processor -os standalone
 
 		#create application project
 		puts "Create Application Project"
-		sdk createapp -name vmix_example_design -hwproject vmix_example_hw_platform -proc microblaze_ss_microblaze_0 -os standalone -lang C -app {Empty Application} -bsp vmix_example_bsp
+		sdk createapp -name vmix_example_design -hwproject vmix_example_hw_platform -proc processor_ss_processor -os standalone -lang C -app {Empty Application} -bsp vmix_example_bsp
 
 		#copy example source files to app project
 		puts "Get Example Design Source Files"
