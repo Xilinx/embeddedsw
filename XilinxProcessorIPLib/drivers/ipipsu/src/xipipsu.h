845fb4a0b21a9d5acf63d4e626c22632227bc298
Venkata Naga Sai Krishna Kolapalli <venkata.naga.sai.krishna.kolapalli@xilinx.com>
2017-03-13 11:03:09 +0530

    ipipsu: Add support for updating ConfigTable at run time
    This patch adds support for modifying the driver ConfigTable
    at run time for a specfic device id.
    
    Signed-off-by: Venkata Naga Sai Krishna Kolapalli <venkatan@xilinx.com>
    Acked-for-series: Anirudha Sarangi <anirudh@xilinx.com>

--- XilinxProcessorIPLib/drivers/ipipsu/src/xipipsu.h
+++ XilinxProcessorIPLib/drivers/ipipsu/src/xipipsu.h
@@ -15,14 +15,12 @@
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
 *
-* Except as contained in this notice, the name of the Xilinx shall not be used
-* in advertising or otherwise to promote the sale, use or other dealings in
-* this Software without prior written authorization from Xilinx.
+*
 *
 ******************************************************************************/
 /*****************************************************************************/
@@ -48,7 +46,7 @@
  * <b>Initialization</b>
  * The config data for the driver is loaded and is based on the HW build. The
  * XIpiPsu_Config data structure contains all the data related to the
- * IPI driver instance and also teh available Target CPUs.
+ * IPI driver instance and also the available Target CPUs.
  *
  * <b>Sending an IPI</b>
  * The following steps can be followed to send an IPI:
