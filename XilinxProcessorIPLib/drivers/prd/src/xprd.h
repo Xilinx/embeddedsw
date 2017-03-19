ad46245c5222037bb3533a4c5c958a69fb2e1ffb
Swapna Manupati <swapnam@xilinx.com>
2017-03-19 18:24:15 +0530

    prd: Added readme.txt file to generate doxygen for examples
    This patch adds readme.txt file in examples folder of prd
    for doxygen generation.
    
    Signed-off-by: Swapna Manupati <swapnam@xilinx.com>

--- XilinxProcessorIPLib/drivers/prd/src/xprd.h
+++ XilinxProcessorIPLib/drivers/prd/src/xprd.h
@@ -94,6 +94,8 @@
 *                            in the generated xprd_g.c for fixing MISRA-C
 *                            files. This is a fix for CR-966099 based on the
 *                            update in the tools.
+*       ms     03/17/17      Added readme.txt file in examples folder for
+*                            doxygen generation.
 *
 * </pre>
 *
