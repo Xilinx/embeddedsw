83f2d3f76274d57a76ea0503bc8c78785d81878f
Dinesh Sudani <dinesh.sudani@xilinx.com>
2017-04-18 19:05:12 +0530

    sw_apps: zynqmp_fsbl: Updated the zcu102 misc folder for 2017.1
    This patch will update the misc folder for versionless 2017.1 zynqmp fsbl.
    
    Signed-off-by: Dinesh Sudani <dinesh.sudani@xilinx.com>
    Acked-by: Srikanth Vemula <svemula@xilinx.com>

--- lib/sw_apps/zynqmp_fsbl/misc/zcu102/bspconfig.h
+++ lib/sw_apps/zynqmp_fsbl/misc/zcu102/bspconfig.h
@@ -5,7 +5,7 @@
 * Version:
 * DO NOT EDIT.
 *
-* Copyright (C) 2010-2016 Xilinx, Inc. All Rights Reserved.*
+* Copyright (C) 2010-2017 Xilinx, Inc. All Rights Reserved.*
 *Permission is hereby granted, free of charge, to any person obtaining a copy
 *of this software and associated documentation files (the Software), to deal
 *in the Software without restriction, including without limitation the rights
@@ -38,3 +38,6 @@
 *******************************************************************/
 
 #define MICROBLAZE_PVR_NONE
+#define EL3 1
+#define EL1_NONSECURE 0
+#define HYP_GUEST 0
