07ccde2dff467d9c00952173b5413262055b8a7c
Swapna Manupati <swapnam@xilinx.com>
2017-04-30 17:29:41 +0530

    xadcps: Changed modification history in examples
    This patch changes modification history in examples and header file
    to avoid warnings.
    
    Signed-off-by: Swapna Manupati <swapnam@xilinx.com>

--- XilinxProcessorIPLib/drivers/xadcps/src/xadcps.h
+++ XilinxProcessorIPLib/drivers/xadcps/src/xadcps.h
@@ -176,7 +176,7 @@
 *                       for CR-965028.
 *       ms     03/17/17 Added readme.txt file in examples folder for doxygen
 *                       generation.
-*       ms     04/05/17 Comment lines begin with /** in functions of xadcps
+*       ms     04/05/17 Modified Comment lines in functions of xadcps
 *                       examples to recognize it as documentation block
 *                       for doxygen generation.
 *
