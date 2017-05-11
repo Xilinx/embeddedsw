bd6f64cd50b7b610398c4b247a0cac50179fedc2
Mirela Simonovic <mirela.simonovic@aggios.com>
2017-05-11 10:38:07 +0530

    PMUFW: pm: Print newline in PmSetConfiguration
    Signed-off-by: Mirela Simonovic <mirela.simonovic@aggios.com>
    Acked-for-Series: Jyotheeswar Reddy Mutthareddyvari <jyothee@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,4 +1,4 @@
 #ifndef ZYNQMP_XPFW_VERSION__H_
  #define ZYNQMP_XPFW_VERSION__H_
- #define ZYNQMP_XPFW_VERSION	"2017.1-rc6-35-g11b63f1"
+ #define ZYNQMP_XPFW_VERSION	"2017.1-rc6-36-gcd1f245"
  #endif
