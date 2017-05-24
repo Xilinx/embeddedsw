ff366e3197cabd4d89756c4d7919ee00b30a8fcd
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2017-05-24 17:49:11 +0530

    axiethernet: Increased time-out values
    As per new h/w update on Ultscale+ devices,
    i.e Increased transceiver initialization times in ultrascale+ devices.
    Axiethernet peripheral test's are failing on ultrascale+ devices
    Because driver doesn't have adequate timeout values for this use case.
    
    This patch fixes this issue.
    
    Signed-off-by: Kedareswara rao Appana <appanad@xilinx.com>
    Acked-for-series: Anirudha Sarangi <anirudh@xilinx.com>

--- XilinxProcessorIPLib/drivers/axiethernet/src/xaxiethernet_hw.h
+++ XilinxProcessorIPLib/drivers/axiethernet/src/xaxiethernet_hw.h
@@ -12,10 +12,6 @@
 * The above copyright notice and this permission notice shall be included in
 * all copies or substantial portions of the Software.
 *
-* Use of the Software is limited solely to applications:
-* (a) running on a Xilinx device, or
-* (b) that interact with a Xilinx device through a bus or interconnect.
-*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -32,7 +28,7 @@
 /**
 *
 * @file xaxiethernet_hw.h
-* @addtogroup axiethernet_v5_7
+* @addtogroup axiethernet_v5_8
 * @{
 *
 * This header file contains identifiers and macros that can be used to access
