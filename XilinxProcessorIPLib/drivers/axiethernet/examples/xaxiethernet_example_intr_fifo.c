1f327dad067e7b5cb71d616bf06c5496dfdf868b
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2017-07-03 13:00:48 +0530

    axiethernet: Fix compilation issues in the example
    This patch fixes the compilation issues with the
    example xaxiethernet_example_intr_fifo.c.
    
    Signed-off-by: Kedareswara rao Appana <appanad@xilinx.com>
    Acked-by: Harini Katakam <harinik@xilinx.com>

--- XilinxProcessorIPLib/drivers/axiethernet/examples/xaxiethernet_example_intr_fifo.c
+++ XilinxProcessorIPLib/drivers/axiethernet/examples/xaxiethernet_example_intr_fifo.c
@@ -15,14 +15,12 @@
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
 *
-* Except as contained in this notice, the name of the Xilinx shall not be used
-* in advertising or otherwise to promote the sale, use or other dealings in
-* this Software without prior written authorization from Xilinx.
+*
 *
 ******************************************************************************/
 /**
@@ -822,7 +820,7 @@ int AxiEthernetMultipleFramesIntrExample(XAxiEthernet *AxiEthernetInstancePtr,
 	 * With the xps_ll_fifo core we can't stuff the fifo with data from
 	 * multiple packets and then send them. Instead, the code needs to
 	 * write the data, and then immediately send the packet before
-	 * writting the data for the next packet.
+	 * writing the data for the next packet.
 	 */
 	for (Index = 0; Index < FramesToLoopback; Index++) {
 		/*
@@ -1217,7 +1215,7 @@ static int AxiEthernetSetupIntrSystem(INTC *IntcInstancePtr,
 	Status = XIntc_Initialize(IntcInstancePtr, INTC_DEVICE_ID);
 	if (Status != XST_SUCCESS) {
 		AxiEthernetUtilErrorTrap
-			("Unable to intialize the interrupt controller");
+			("Unable to initialize the interrupt controller");
 		return XST_FAILURE;
 	}
 #endif
