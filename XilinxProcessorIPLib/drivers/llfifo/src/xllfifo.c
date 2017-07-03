b1c3e8d87a861a7c68496a956c7ed8b3203c9a98
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2017-07-03 13:00:27 +0530

    llfifo: Fix doxygen issues in the driver
    This patch fixes the doxygen comments in the driver
    to generate the proper doxygen description for each driver API.
    
    Signed-off-by: Kedareswara rao Appana <appanad@xilinx.com>
    Acked-by: Harini Katakam <harinik@xilinx.com>

--- XilinxProcessorIPLib/drivers/llfifo/src/xllfifo.c
+++ XilinxProcessorIPLib/drivers/llfifo/src/xllfifo.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2005 - 2014 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2005 - 2018 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@
 /**
  *
  * @file xllfifo.c
-* @addtogroup llfifo_v5_2
+* @addtogroup llfifo_v5_3
 * @{
  *
  * The Xilinx local link FIFO driver component. This driver supports the
