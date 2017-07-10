8883f1803e0fee7080269b30142354e62e1c5280
VNSL Durga <vnsl.durga.challa@xilinx.com>
2017-07-10 13:01:53 +0530

    XilRsa: Updated doxygen tags
    Xilrsa library is updated with doxygen tags
    
    Signed-off-by: VNSL Durga <vnsldurg@xilinx.com>
    
    Acked-by: Krishna Chaitanya Patakamuri <kpataka@xilinx.com>

--- lib/sw_services/xilrsa/src/include/xilrsa.h
+++ lib/sw_services/xilrsa/src/include/xilrsa.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2014 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2014 - 17 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -33,7 +33,9 @@
 /**
 *
 * @file xilrsa.h
-*
+* @addtogroup xilrsa_apis	XilRSA APIs and Descriptions
+* @{
+* @cond xilrsa_internal
 * This file contains the RSA algorithm functions
 *
 * <pre>
@@ -42,6 +44,7 @@
 * Ver	Who	Date		Changes
 * ----- ---- -------- -------------------------------------------------------
 * 1.0   hk   27/01/14 First release
+* 1.4   vns  07/06/17 Added dooxygen tags.
 *
 * </pre>
 *
@@ -126,39 +129,128 @@ extern "C" {
  * Includes SHA-256 state, coalescing buffer to collect the processed strings, and
  * total byte length counter (used both to manage the buffer and for padding)
  */
+ //! [sha2_context]
 typedef struct
 {
 	unsigned int state[8];
 	unsigned char buffer[SHA_BLKBYTES];
 	unsigned long long bytes;
 } sha2_context;
+//! [sha2_context]
+/** @}
+@endcond */
 
 /*
  * RSA-2048 user interfaces
  */
+/*****************************************************************************/
+/**
+ * @brief
+ * This function is used to encrypt the data using 2048 bit private key.
+ *
+ * @param	modular		A char pointer which contains the key modulus
+ * @param	modular_ext	A char pointer which contains the key modulus
+ *		extension
+ * @param	exponent	A char pointer which contains the private key
+ *		exponent
+ * @param	result		A char pointer which contains the encrypted data
+ *
+ * @return	None
+ *
+ ******************************************************************************/
 void rsa2048_exp(const unsigned char *base, const unsigned char * modular,
 		const unsigned char *modular_ext, const unsigned char *exponent,
 		unsigned char *result);
+/*****************************************************************************/
+/**
+ * @brief
+ * This function is used to decrypt the data using 2048 bit public key
+ *
+ * @param	a	RSA_NUMBER containing the decrypted data.
+ * @param	x	RSA_NUMBER containing the input data
+ * @param	e	Unsigned number containing the public key exponent
+ * @param	m	RSA_NUMBER containing the public key modulus
+ * @param	rrm	RSA_NUMBER containing the public key modulus extension.
+ *
+ * @return	None
+ *
+ ******************************************************************************/
 void rsa2048_pubexp(RSA_NUMBER a, RSA_NUMBER x,
 		unsigned long e, RSA_NUMBER m, RSA_NUMBER rrm);
 
 /*
  * SHA-256 user interfaces
  */
+/*****************************************************************************/
+/**
+ * @brief
+ * This function calculates the hash for the input data using SHA-256
+ * algorithm. This function internally calls the sha2_init, updates and
+ * finishes functions and updates the result.
+ *
+ * @param	In	Char pointer which contains the input data.
+ * @param	Size	Length of the input data
+ * @param	Out	Pointer to location where resulting hash will be
+ *		written.
+ *
+ * @return	None
+ *
+ ******************************************************************************/
 void sha_256(const unsigned char *in, const unsigned int size, unsigned char *out);
+/*****************************************************************************/
+/**
+ * @brief
+ * This function initializes the SHA2 context.
+ *
+ * @param	ctx 	Pointer to sha2_context structure that stores status and
+ *		buffer.
+ *
+ * @return	None
+ *
+ ******************************************************************************/
 void sha2_starts(sha2_context *ctx);
+/*****************************************************************************/
+/**
+ * @brief
+ * This function adds the input data to SHA256 calculation.
+ *
+ * @param	ctx 	Pointer to sha2_context structure that stores status and
+ * 		buffer.
+ * @param	input 	Pointer to the data to add.
+ * @param	Out 	Length of the input data.
+ *
+ * @return	None
+ *
+ ******************************************************************************/
 void sha2_update(sha2_context *ctx, unsigned char* input, unsigned int ilen);
+/*****************************************************************************/
+/**
+ * @brief
+ * This function finishes the SHA calculation.
+ *
+ * @param	ctx 	Pointer to sha2_context structure that stores status and
+ *		buffer.
+ * @param	output 	Pointer to the calculated hash data.
+ *
+ * @return	None
+ *
+ *
+ ******************************************************************************/
 void sha2_finish(sha2_context *ctx, unsigned char* output);
 
 /*
  * Preprocessing interface (pre-computation of R*R mod M)
  */
+/**@cond xilrsa_internal */
 void modular_ext(const unsigned char *modular, unsigned char *res);
+/** @}
+@endcond */
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* ___XIL_RSA_H___ */
+/** @} */
 
 
