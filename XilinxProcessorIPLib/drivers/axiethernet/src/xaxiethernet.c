bbf8fe688fc5bb1790456c0be767c19f9606d2fc
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2017-07-17 12:28:31 +0530

    axiethernet: Add support for mcdma
    This patch adds support for axiethernet with mcdma
    configuration in the driver.
    
    Signed-off-by: Kedareswara rao Appana <appanad@xilinx.com>
    
    Acked-by: Harini Katakam <harinik@xilinx.com>

--- XilinxProcessorIPLib/drivers/axiethernet/src/xaxiethernet.c
+++ XilinxProcessorIPLib/drivers/axiethernet/src/xaxiethernet.c
@@ -15,14 +15,12 @@
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
 *
-* Except as contained in this notice, the name of the Xilinx shall not be used
-* in advertising or otherwise to promote the sale, use or other dealings in
-* this Software without prior written authorization from Xilinx.
+*
 *
 ******************************************************************************/
 /**
@@ -155,7 +153,7 @@ int XAxiEthernet_CfgInitialize(XAxiEthernet *InstancePtr,
 * @return	XST_SUCCESS.
 *
 * @note		When user calls this function he should ensure the hardware
-*		is in a quiescent state by reseting all the hardware
+*		is in a quiescent state by resetting all the hardware
 *		Configurations.
 *
 ******************************************************************************/
