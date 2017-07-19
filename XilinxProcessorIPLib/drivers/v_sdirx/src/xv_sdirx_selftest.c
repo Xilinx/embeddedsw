6052a97ae6f0467323d2ae60fe8a0d4e1724ade6
Siva Rajesh <siva.rajesh.jarugula@xilinx.com>
2017-07-19 17:19:36 +0530

    v_sdirx: Initial version of UHDSDI Rx soft IP driver
    This is an initial version of the UHDSDI Rx soft IP driver.
    
    Signed-off-by: Siva Rajesh <siva.rajesh.jarugula@xilinx.com>
    
    Acked-for-series: Vishal Sagar<vishal.sagar@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/v_sdirx/src/xv_sdirx_selftest.c
@@ -0,0 +1,121 @@
+/******************************************************************************
+*
+* Copyright (C)2017 Xilinx, Inc. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running on a Xilinx device, or
+* (b) that interact with a Xilinx device through a bus or interconnect.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+/*****************************************************************************/
+/**
+*
+* @file xv_sdirx_selftest.c
+* @addtogroup xv_sdirx_v1_0
+* @{
+*
+* Contains diagnostic/self-test functions for the SDI Rx Controller core.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver Who Date     Changes
+* --- --- -------- ------------------------------------------------------------
+* 1.0 jsr 07/17/17 Initial release
+* </pre>
+*
+******************************************************************************/
+
+/***************************** Include Files *********************************/
+
+#include "xil_io.h"
+#include "xstatus.h"
+#include "xv_sdirx.h"
+
+/************************** Constant Definitions *****************************/
+
+/**************************** Type Definitions *******************************/
+
+/*************************** Macros Definitions ******************************/
+
+#define SDIRX_MDL_CTRL_MASK	0xFFFF
+#define SDIRX_MDL_CTRL_DEFAULT	0x00
+#define SDIRX_ST_RST_MASK	0xFF
+#define SDIRX_ST_RST_DEFAULT	0x00
+#define ISR_MASK		0xFF
+#define ISR_DEFAULT		0x00
+
+/************************** Function Prototypes ******************************/
+
+/************************** Variable Definitions *****************************/
+
+/************************** Function Definitions *****************************/
+
+/*****************************************************************************/
+/**
+*
+* Runs a self-test on the driver/device. This test checks if the LaneCount
+* present in register matches the one from the generated file.
+*
+* @param	InstancePtr is a pointer to the XV_SdiRx instance.
+*
+* @return
+*		- XST_SUCCESS if self-test was successful
+*		- XST_FAILURE if the read value was not equal to _g.c file
+*
+* @note		None
+*
+******************************************************************************/
+u32 XV_SdiRx_SelfTest(XV_SdiRx *InstancePtr)
+{
+	u32 Result, RegValue;
+
+	/* Verify arguments */
+	Xil_AssertNonvoid(InstancePtr != NULL);
+	Xil_AssertNonvoid(InstancePtr->IsReady == XIL_COMPONENT_IS_READY);
+
+	Result = XV_SdiRx_Stop(InstancePtr);
+
+	if (Result == XST_FAILURE)
+		return XST_FAILURE;
+
+	RegValue = XV_SdiRx_ReadReg(InstancePtr->Config.BaseAddress,
+					XV_SDIRX_MDL_CTRL_OFFSET);
+	if ((RegValue & SDIRX_MDL_CTRL_MASK) != SDIRX_MDL_CTRL_DEFAULT)
+		return XST_FAILURE;
+
+	RegValue = XV_SdiRx_ReadReg(InstancePtr->Config.BaseAddress,
+					XV_SDIRX_ST_RST_OFFSET);
+	if ((RegValue & SDIRX_ST_RST_MASK) != SDIRX_ST_RST_DEFAULT)
+		return XST_FAILURE;
+
+	RegValue = XV_SdiRx_ReadReg(InstancePtr->Config.BaseAddress,
+					XV_SDIRX_INT_STS_OFFSET);
+	if ((RegValue & ISR_MASK) != ISR_DEFAULT)
+		return XST_FAILURE;
+
+	return XST_SUCCESS;
+}
+/** @} */
