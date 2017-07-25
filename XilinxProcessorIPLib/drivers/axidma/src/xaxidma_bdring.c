d306b2b59b95139392cf6c2f3dfc3aad4dd783c4
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2017-07-25 11:26:36 +0530

    axidma: Add support for cyclic DMA mode
    This patch does the below
    --> Add support for cyclic DMA mode in the driver
    --> Add example support for cyclic dma mode.
    
    Signed-off-by: Kedareswara rao Appana <appanad@xilinx.com>
    
    Acked-for-series: Anirudha Sarangi <anirudh@xilinx.com>

--- XilinxProcessorIPLib/drivers/axidma/src/xaxidma_bdring.c
+++ XilinxProcessorIPLib/drivers/axidma/src/xaxidma_bdring.c
@@ -363,6 +363,7 @@ u32 XAxiDma_BdRingCreate(XAxiDma_BdRing *RingPtr, UINTPTR PhysAddr,
 	RingPtr->HwCnt = 0;
 	RingPtr->PreCnt = 0;
 	RingPtr->PostCnt = 0;
+	RingPtr->Cyclic = 0;
 
 	/* Make sure Alignment parameter meets minimum requirements */
 	if (Alignment < XAXIDMA_BD_MINIMUM_ALIGNMENT) {
@@ -468,6 +469,7 @@ u32 XAxiDma_BdRingCreate(XAxiDma_BdRing *RingPtr, UINTPTR PhysAddr,
 	RingPtr->HwTail = (XAxiDma_Bd *) VirtAddr;
 	RingPtr->PostHead = (XAxiDma_Bd *) VirtAddr;
 	RingPtr->BdaRestart = (XAxiDma_Bd *) PhysAddr;
+	RingPtr->CyclicBd = (XAxiDma_Bd *) malloc(sizeof(XAxiDma_Bd));
 
 	return XST_SUCCESS;
 }
@@ -594,6 +596,16 @@ int XAxiDma_StartBdRingHw(XAxiDma_BdRing * RingPtr)
 		if (RingPtr->HwCnt > 0) {
 
 			XAXIDMA_CACHE_INVALIDATE(RingPtr->HwTail);
+			if (RingPtr->Cyclic) {
+				XAxiDma_WriteReg(RingPtr->ChanBase,
+						 XAXIDMA_TDESC_OFFSET,
+						 (u32)XAXIDMA_VIRT_TO_PHYS(RingPtr->CyclicBd));
+				if (RingPtr->Addr_ext)
+					XAxiDma_WriteReg(RingPtr->ChanBase,
+							 XAXIDMA_TDESC_MSB_OFFSET,
+							 UPPER_32_BITS(XAXIDMA_VIRT_TO_PHYS(RingPtr->CyclicBd)));
+				return XST_SUCCESS;
+			}
 
 			if ((XAxiDma_BdRead(RingPtr->HwTail,
 				    XAXIDMA_BD_STS_OFFSET) &
@@ -1109,6 +1121,17 @@ int XAxiDma_BdRingToHw(XAxiDma_BdRing * RingPtr, int NumBd,
 
 	/* If it is running, signal the engine to begin processing */
 	if (RingPtr->RunState == AXIDMA_CHANNEL_NOT_HALTED) {
+			if (RingPtr->Cyclic) {
+				XAxiDma_WriteReg(RingPtr->ChanBase,
+						 XAXIDMA_TDESC_OFFSET,
+						 (u32)XAXIDMA_VIRT_TO_PHYS(RingPtr->CyclicBd));
+				if (RingPtr->Addr_ext)
+					XAxiDma_WriteReg(RingPtr->ChanBase,
+							 XAXIDMA_TDESC_MSB_OFFSET,
+							 UPPER_32_BITS(XAXIDMA_VIRT_TO_PHYS(RingPtr->CyclicBd)));
+				return XST_SUCCESS;
+			}
+
 			if (RingPtr->IsRxChannel) {
 				if (!RingIndex) {
 					XAxiDma_WriteReg(RingPtr->ChanBase,
@@ -1297,8 +1320,10 @@ int XAxiDma_BdRingFromHw(XAxiDma_BdRing * RingPtr, int BdLimit,
 	 */
 	if (BdCount) {
 		*BdSetPtr = RingPtr->HwHead;
-		RingPtr->HwCnt -= BdCount;
-		RingPtr->PostCnt += BdCount;
+		if (!RingPtr->Cyclic) {
+			RingPtr->HwCnt -= BdCount;
+			RingPtr->PostCnt += BdCount;
+		}
 		XAXIDMA_RING_SEEKAHEAD(RingPtr, RingPtr->HwHead, BdCount);
 
 		return BdCount;
--- XilinxProcessorIPLib/drivers/axidma/src/xaxidma_bdring.h
+++ XilinxProcessorIPLib/drivers/axidma/src/xaxidma_bdring.h
@@ -84,8 +84,7 @@ extern "C" {
 
 #include "xstatus.h"
 #include "xaxidma_bd.h"
-
-
+#include <stdlib.h>
 
 /************************** Constant Definitions *****************************/
 /* State of a DMA channel
@@ -127,12 +126,14 @@ typedef struct {
 	XAxiDma_Bd *HwTail;	/**< Last BD in the work group */
 	XAxiDma_Bd *PostHead;	/**< First BD in the post-work group */
 	XAxiDma_Bd *BdaRestart;	/**< BD to load when channel is started */
+	XAxiDma_Bd *CyclicBd;	/**< Useful for Cyclic DMA operations */
 	int FreeCnt;		/**< Number of allocatable BDs in free group */
 	int PreCnt;		/**< Number of BDs in pre-work group */
 	int HwCnt;		/**< Number of BDs in work group */
 	int PostCnt;		/**< Number of BDs in post-work group */
 	int AllCnt;		/**< Total Number of BDs for channel */
 	int RingIndex;		/**< Ring Index */
+	int Cyclic;		/**< Check for cyclic DMA Mode */
 } XAxiDma_BdRing;
 
 /***************** Macros (Inline Functions) Definitions *********************/
@@ -489,6 +490,23 @@ typedef struct {
 #define XAxiDma_BdRingAckIrq(RingPtr, Mask)				\
 		XAxiDma_WriteReg((RingPtr)->ChanBase, XAXIDMA_SR_OFFSET,\
 			(Mask) & XAXIDMA_IRQ_ALL_MASK)
+
+/****************************************************************************/
+/**
+* Enable Cyclic DMA Mode.
+*
+* @param	RingPtr is the channel instance to operate on.
+*
+* @note
+*		C-style signature:
+*		void XAxiDma_BdRingEnableCyclicDMA(XAxiDma_BdRing* RingPtr)
+*		This function is used only when system is configured as SG mode
+*
+*****************************************************************************/
+#define XAxiDma_BdRingEnableCyclicDMA(RingPtr)			\
+		(RingPtr->Cyclic = 1)
+
+/****************************************************************************/
 
 /************************* Function Prototypes ******************************/
 
