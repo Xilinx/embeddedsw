f7707d5cef0e1cf3382d776a546ed5e1bdcaee0a
Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
2017-08-16 14:52:24 +0530

    drivers: USBPSU: Add CCI support for USB
    This patch add support for CCI in the USB
    driver and examples
    
    Signed-off-by: Anurag Kumar Vulisha <anuragku@xilinx.com>
    
    Acked-by: Harini Katakam <harinik@xilinx.com>

--- XilinxProcessorIPLib/drivers/usbpsu/data/usbpsu.tcl
+++ XilinxProcessorIPLib/drivers/usbpsu/data/usbpsu.tcl
@@ -36,6 +36,7 @@
 # Ver   Who  Date     Changes
 # ----- ---- -------- -----------------------------------------------
 # 1.0   sg  06/06/16 First release
+# 1.3	vak 16/08/17 Export CCI related information
 #
 ##############################################################################
 
@@ -44,8 +45,32 @@
 proc generate {drv_handle} {
     ::hsi::utils::define_zynq_include_file $drv_handle "xparameters.h" "XUsbPsu" "NUM_INSTANCES" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR"
 
-    ::hsi::utils::define_zynq_config_file $drv_handle "xusbpsu_g.c" "XUsbPsu" "DEVICE_ID" "C_S_AXI_BASEADDR"
+    generate_cci_params $drv_handle "xparameters.h"
+
+    ::hsi::utils::define_zynq_config_file $drv_handle "xusbpsu_g.c" "XUsbPsu" "DEVICE_ID" "C_S_AXI_BASEADDR" "IS_CACHE_COHERENT"
 
     ::hsi::utils::define_zynq_canonical_xpars $drv_handle "xparameters.h" "XUsbPsu" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR"
 
+}
+
+proc generate_cci_params {drv_handle file_name} {
+	set file_handle [::hsi::utils::open_include_file $file_name]
+	# Get all peripherals connected to this driver
+	set ips [::hsi::utils::get_common_driver_ips $drv_handle]
+
+	set sw_processor [hsi::get_sw_processor]
+	set processor [hsi::get_cells -hier [common::get_property HW_INSTANCE $sw_processor]]
+	set processor_type [common::get_property IP_NAME $processor]
+
+	foreach ip $ips {
+		set is_cc 0
+		if {$processor_type == "psu_cortexa53"} {
+			set is_xen [common::get_property CONFIG.hypervisor_guest [hsi::get_os]]
+			if {$is_xen == "true"} {
+				set is_cc [common::get_property CONFIG.IS_CACHE_COHERENT $ip]
+			}
+		}
+		puts $file_handle "\#define [::hsi::utils::get_driver_param_name $ip "IS_CACHE_COHERENT"] $is_cc"
+	}
+	close $file_handle
 }
