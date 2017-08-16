1fc91e3914778cfb3761486ee90a671fb7773492
Swapna Manupati <swapna.manupati@xilinx.com>
2017-08-16 10:57:14 +0530

    axivdma: Fixed compiler warnings
    This patch fixes compiler warnings in xaxivdma_sinit.c
    
    Signed-off-by: Swapna Manupati <swapnam@xilinx.com>
    
    Acked-for-series: Srinivas Goud <sgoud@xilinx.com>

--- XilinxProcessorIPLib/drivers/axivdma/src/xaxivdma.h
+++ XilinxProcessorIPLib/drivers/axivdma/src/xaxivdma.h
@@ -15,14 +15,12 @@
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
 *
-* Except as contained in this notice, the name of the Xilinx shall not be used
-* in advertising or otherwise to promote the sale, use or other dealings in
-* this Software without prior written authorization from Xilinx.
+*
 *
 ******************************************************************************/
 /*****************************************************************************/
@@ -94,7 +92,7 @@
 * set the RUN/STOP bit in the XAXIVDMA_CR_OFFSET register.
 * - After any reset you need to do, write the head of your BD ring into the
 * XAXIVDMA_CDESC_OFFSET register.
-* - You can do other setup for the harware.
+* - You can do other setup for the hardware.
 * - Start your hardware, by setting up the RUN/STOP bit in the
 * XAXIVDMA_CR_OFFSET register.
 * - You can do other setup for the hardware.
@@ -187,7 +185,7 @@
 * "word" is defined by C_M_AXIS_MM2S_TDATA_WIDTH and C_S_AXIS_S2MM_TDATA_WIDTH
 * for the read and write channel specifically.
 *
-* If the horizonal frame size is not word-aligned, then DRE must be enabled
+* If the horizontal frame size is not word-aligned, then DRE must be enabled
 * in the hardware. Otherwise, undefined results happen.
 *
 * <b>Address Translation</b>
@@ -367,7 +365,7 @@ extern "C" {
 #define XAXIVDMA_ENABLE_DBG_DLY_CNTR	0x08
 #define XAXIVDMA_ENABLE_DBG_ALL_FEATURES	0x0F
 
-/* Defined for backward compatiblity.
+/* Defined for backward compatibility.
  * This  is a typical DMA Internal Error, which on detection doesnt require a
  * reset (as opposed to other errors). So user on seeing this need only to
  * reinitialize channels.
