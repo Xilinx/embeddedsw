10c967157a98bf5ef09a1b77215b19598783c8bb
Swapna Manupati <swapna.manupati@xilinx.com>
2017-08-16 10:57:15 +0530

    bram: Fixed compiler warnings
    This patch fixes compiler warnings and updated copyright
    year in xbram_sinit.c, xbram.h files
    
    Signed-off-by: Swapna Manupati <swapnam@xilinx.com>
    
    Acked-for-series: Srinivas Goud <sgoud@xilinx.com>

--- XilinxProcessorIPLib/drivers/bram/src/xbram.h
+++ XilinxProcessorIPLib/drivers/bram/src/xbram.h
@@ -1,32 +1,12 @@
 /******************************************************************************
-*
-* Copyright (C) 2010 - 2019 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
-*
+* Copyright (C) 2010 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 /*****************************************************************************/
 /**
 * @file xbram.h
-* @addtogroup bram_v4_2
+* @addtogroup bram_v4_4
 * @{
 * @details
 *
@@ -116,8 +96,6 @@
 * 4.2   ms  04/18/17 Modified tcl file to add suffix U for all macro
 *                    definitions of bram in xparameters.h
 *       ms  08/07/17 Fixed compilation warnings in xbram_sinit.c
-* 4.3   aru 03/23/19 Used UINTPTR instead of u32 for MemBaseAddress and
-*                    MemHighAddress.
 * </pre>
 *****************************************************************************/
 #ifndef XBRAM_H		/* prevent circular inclusions */
@@ -166,8 +144,8 @@ typedef struct {
 					     *  register in h/w */
 	int WriteAccess;		   /**< Is write access enabled in
 					     *  h/w */
-	UINTPTR MemBaseAddress;		   /**< Device memory base address */
-	UINTPTR MemHighAddress;		   /**< Device memory high address */
+	u32 MemBaseAddress;		   /**< Device memory base address */
+	u32 MemHighAddress;		   /**< Device memory high address */
 	UINTPTR CtrlBaseAddress;		   /**< Device register base address.*/
 	UINTPTR CtrlHighAddress;		   /**< Device register base address.*/
 } XBram_Config;
