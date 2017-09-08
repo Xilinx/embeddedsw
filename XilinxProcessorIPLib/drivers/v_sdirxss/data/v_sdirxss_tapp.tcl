50cd86c019c761ff81b0526acec0bd8b786e9536
Siva Rajesh <siva.rajesh.jarugula@xilinx.com>
2017-09-08 18:23:50 +0530

    v_sdirxss: Add support for peripheral tests
    Adding the support for v_sdirxss driver for running peripheral tests.
    
    Signed-off-by: Siva Rajesh <siva.rajesh.jarugula@xilinx.com>
    
    Acked-for-series: Saurabh Sengar <saurabhs@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/v_sdirxss/data/v_sdirxss_tapp.tcl
@@ -0,0 +1,121 @@
+###############################################################################
+#
+# Copyright (C) 2017 Xilinx, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# Use of the Software is limited solely to applications:
+# (a) running on a Xilinx device, or
+# (b) that interact with a Xilinx device through a bus or interconnect.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+# OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Except as contained in this notice, the name of the Xilinx shall not be used
+# in advertising or otherwise to promote the sale, use or other dealings in
+# this Software without prior written authorization from Xilinx.
+#
+##############################################################################
+# Uses $XILINX_EDK/bin/lib/xillib_sw.tcl
+
+# -----------------------------------------------------------------
+# Software Project Types (swproj):
+#   0 : MemoryTest - Calls basic  memorytest routines from common driver dir
+#   1 : PeripheralTest - Calls any existing polled_example and/or selftest
+# -----------------------------------------------------------------
+
+# -----------------------------------------------------------------
+# TCL Procedures:
+# -----------------------------------------------------------------
+
+proc gen_include_files {swproj mhsinst} {
+  if {$swproj == 0} {
+    return ""
+  }
+  if {$swproj == 1} {
+    set inc_file_lines {xv_sdirxss_header.h}
+    return $inc_file_lines
+  }
+}
+
+proc gen_src_files {swproj mhsinst} {
+  if {$swproj == 0} {
+    return ""
+  }
+  if {$swproj == 1} {
+    set inc_file_lines {examples/xv_sdirxss_selftest_example.c data/xv_sdirxss_header.h}
+    return $inc_file_lines
+  }
+}
+
+proc gen_testfunc_def {swproj mhsinst} {
+  return ""
+}
+
+proc gen_init_code {swproj mhsinst} {
+  return ""
+}
+
+proc gen_testfunc_call {swproj mhsinst} {
+
+  if {$swproj == 0} {
+    return ""
+  }
+
+  set ipname [get_property NAME $mhsinst]
+  set deviceid [::hsi::utils::get_ip_param_name $mhsinst "DEVICE_ID"]
+   set stdout [get_property CONFIG.STDOUT [get_os]]
+   if { $stdout == "" || $stdout == "none" } {
+       set hasStdout 0
+   } else {
+       set hasStdout 1
+   }
+
+  set testfunc_call ""
+
+  if {${hasStdout} == 0} {
+
+      append testfunc_call "
+
+   {
+      int Status;
+
+      Status = SdiRxSs_SelfTestExample(${deviceid});
+
+   }"
+  } else {
+
+      append testfunc_call "
+
+   {
+      int Status;
+
+
+      print(\"\\r\\nRunning SdiRxSsSelfTestExample() for ${ipname}...\\r\\n\");
+
+      Status = SdiRxSs_SelfTestExample(${deviceid});
+
+      if (Status == 0) {
+         print(\"SdiRxSs_SelfTestExample PASSED\\r\\n\");
+      }
+      else {
+         print(\"SdiRxSs_SelfTestExample FAILED\\r\\n\");
+      }
+   }"
+  }
+
+  return $testfunc_call
+}
