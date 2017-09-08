8853550b3eb747477b57ea10c15b13b287fb2a7b
Vanessa Chou <vanessay@xilinx.com>
2017-09-08 16:22:03 -0700

    v_csc: Support for YUV 4:2:0
    Added support for conversion from any video format to any video format
    
    Signed-off-by: Vanessa Chou <vanessay@xilinx.com>
    
    Acked-for-Series: Rohit Consul <rohitco@xilinx.com>

--- XilinxProcessorIPLib/drivers/v_csc/data/v_csc.tcl
+++ XilinxProcessorIPLib/drivers/v_csc/data/v_csc.tcl
@@ -33,6 +33,7 @@
 # -------- ------ -------- ----------------------------------------------------
 #  1.0      rco    07/21/15 Initial version of vprocss csc subcore tcl
 #  2.0      dmc    12/17/15 Include new args ENABLE_422 and ENABLE_WINDOW
+#  2.2      vyc    10/04/17 Include new arg ENABLE_420
 #
 ###############################################################################
 
@@ -47,6 +48,7 @@ proc generate {drv_handle} {
         "V_CSC_MAX_HEIGHT" \
         "MAX_DATA_WIDTH" \
         "ENABLE_422" \
+        "ENABLE_420" \
         "ENABLE_WINDOW"
 
     xdefine_config_file $drv_handle "xv_csc_g.c" "XV_csc" \
@@ -57,6 +59,7 @@ proc generate {drv_handle} {
         "V_CSC_MAX_HEIGHT" \
         "MAX_DATA_WIDTH" \
         "ENABLE_422" \
+        "ENABLE_420" \
         "ENABLE_WINDOW"
 
     xdefine_canonical_xpars $drv_handle "xparameters.h" "XV_csc" \
@@ -68,5 +71,6 @@ proc generate {drv_handle} {
         "V_CSC_MAX_HEIGHT" \
         "MAX_DATA_WIDTH" \
         "ENABLE_422" \
+        "ENABLE_420" \
         "ENABLE_WINDOW"
 }
