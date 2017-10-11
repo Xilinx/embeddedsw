67f4f6399d41a75d0fc8cac82d8e3bc0f51a8f9a
b.vikram@xilinx.com <b.vikram@xilinx.com>
2017-10-11 15:21:43 +0530

    sw_apps :zynq_fsbl: Update fsbl version in master branch to 2018.1
    Update Zynq FSBL version in master branch to 2018.1
    
    Signed-off-by: Vikram Sreenivasa Batchali <bvikram@xilinx.com>
    Acked-by: Sarat Chand Savitala <saratcha@xilinx.com>
    
    Acked-by: Sarat Chand Savitala <saratcha@xilinx.com>

--- lib/sw_apps/zynq_fsbl/src/fsbl.h
+++ lib/sw_apps/zynq_fsbl/src/fsbl.h
@@ -1,28 +1,8 @@
 /******************************************************************************
-*
-* Copyright (C) 2012 - 2018 Xilinx, Inc.  All rights reserved.
-* 
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal 
-* in the Software without restriction, including without limitation the rights 
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell  
-* copies of the Software, and to permit persons to whom the Software is 
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in 
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
-*
+* Copyright (c) 2012 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 /*****************************************************************************/
 /**
 *
@@ -167,7 +147,7 @@
 * 						after PCAP transfer completion
 *						726178 In the 14.6 FSBL function FabricInit() PROG_B 
 *						is kept active for 5mS.
-*						Resolution: PROG_B is now kept active for 5ms only in case
+*						Resolution: PROG_B is now kept active for 5ms only in case 
 *						if efuse is the aes key source.
 *						755245 FSBL does not load partition if eMMC has only 
 *						one partition
@@ -332,8 +312,8 @@ extern "C" {
 /*
  * SDK release version
  */
-#define SDK_RELEASE_YEAR	2019
-#define SDK_RELEASE_QUARTER	2
+#define SDK_RELEASE_YEAR	2020
+#define SDK_RELEASE_QUARTER	1
 
 #define WORD_LENGTH_SHIFT	2
 
