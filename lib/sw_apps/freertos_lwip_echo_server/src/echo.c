0766f57558f185b50a94d5d1245a61338c92d809
Himali Patel <himali.patel@xilinx.com>
2017-10-13 08:06:19 -0400

    LWIP: IPv6 echo server freertos application
    - Create and assign Link local IPv6 address to network interface
    - Added support in echo.c application to listen on any IPv6 address
    
    Signed-off-by: Himali Patel <himalip@xilinx.com>

--- lib/sw_apps/freertos_lwip_echo_server/src/echo.c
+++ lib/sw_apps/freertos_lwip_echo_server/src/echo.c
@@ -92,8 +92,11 @@ void process_echo_request(void *p)
 void echo_application_thread()
 {
 	int sock, new_sd;
-	struct sockaddr_in address, remote;
 	int size;
+#if LWIP_IPV6==0
+	struct sockaddr_in address, remote;
+
+	memset(&address, 0, sizeof(address));
 
 	if ((sock = lwip_socket(AF_INET, SOCK_STREAM, 0)) < 0)
 		return;
@@ -101,6 +104,20 @@ void echo_application_thread()
 	address.sin_family = AF_INET;
 	address.sin_port = htons(echo_port);
 	address.sin_addr.s_addr = INADDR_ANY;
+#else
+	struct sockaddr_in6 address, remote;
+
+	memset(&address, 0, sizeof(address));
+
+	address.sin6_len = sizeof(address);
+	address.sin6_family = AF_INET6;
+	address.sin6_port = htons(echo_port);
+
+	memset(&(address.sin6_addr), 0, sizeof(address.sin6_addr));
+
+	if ((sock = lwip_socket(AF_INET6, SOCK_STREAM, 0)) < 0)
+		return;
+#endif
 
 	if (lwip_bind(sock, (struct sockaddr *)&address, sizeof (address)) < 0)
 		return;
