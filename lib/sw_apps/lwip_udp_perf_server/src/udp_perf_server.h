7934745a7ef6526e9d73245f1d294b9887b428bc
Rahul Bhansali <rahul.bhansali@xilinx.com>
2017-10-16 04:28:52 -0400

    LwIP: Raw mode UDP server performance application
    This patch adds UDP server application for downlink performance
    measurement with remote Iperf-2.0.5 UDP client
    
    Signed-off-by: Rahul Bhansali <rahul.bhansali@xilinx.com>
    
    Acked-for-series: Harini Katakam <harinik@xilinx.com>

---
+++ lib/sw_apps/lwip_udp_perf_server/src/udp_perf_server.h
@@ -0,0 +1,103 @@
+/******************************************************************************
+*
+* Copyright (C) 2017 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running on a Xilinx device, or
+* (b) that interact with a Xilinx device through a bus or interconnect.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+
+#ifndef __UDP_PERF_SERVER_H_
+#define __UDP_PERF_SERVER_H_
+
+#include "lwipopts.h"
+#include "lwip/ip_addr.h"
+#include "lwip/err.h"
+#include "lwip/udp.h"
+#include "lwip/inet.h"
+#include "xil_printf.h"
+#include "platform.h"
+
+/* used as indices into kLabel[] */
+enum {
+	KCONV_UNIT,
+	KCONV_KILO,
+	KCONV_MEGA,
+	KCONV_GIGA,
+};
+
+/* labels for formats [KMG] */
+const char kLabel[] =
+{
+	' ',
+	'K',
+	'M',
+	'G'
+};
+
+/* used as type of print */
+enum measure_t {
+	BYTES,
+	SPEED
+};
+
+/* Report type */
+enum report_type {
+	/* The Intermediate report */
+	INTER_REPORT,
+	/* The server side test is done */
+	UDP_DONE_SERVER,
+	/* Remote side aborted the test */
+	UDP_ABORTED_REMOTE
+};
+
+struct interim_report {
+	u64_t start_time;
+	u64_t last_report_time;
+	u32_t total_bytes;
+	u32_t cnt_datagrams;
+	u32_t cnt_dropped_datagrams;
+};
+
+struct perf_stats {
+	u8_t client_id;
+	u64_t start_time;
+	u64_t end_time;
+	u64_t total_bytes;
+	u64_t cnt_datagrams;
+	u64_t cnt_dropped_datagrams;
+	u32_t cnt_out_of_order_datagrams;
+	s32_t expected_datagram_id;
+	struct interim_report i_report;
+};
+
+/* seconds between periodic bandwidth reports */
+#define INTERIM_REPORT_INTERVAL 5
+
+/* server port to listen on/connect to */
+#define UDP_CONN_PORT 5001
+
+#endif /* __UDP_PERF_SERVER_H_ */
