632e9e0426935afbfc97b537fe2eceb255db2085
Anand Ashok Dumbre <anand.ashok.dumbre@xilinx.com>
2017-10-19 18:01:56 -0700

    dppsu: Removed unapplicable registers from the driver
    Signed-off-by: Anand Ashok Dumbre <anand.ashok.dumbre@xilinx.com>

--- XilinxProcessorIPLib/drivers/dppsu/src/xdppsu_selftest.c
+++ XilinxProcessorIPLib/drivers/dppsu/src/xdppsu_selftest.c
@@ -46,6 +46,7 @@
  * Ver   Who  Date     Changes
  * ----- ---- -------- -----------------------------------------------
  * 1.0   aad  01/17/17 Initial release.
+ * 1.1   aad  10/04/17 Removed not applicable registers
  * </pre>
  *
 *******************************************************************************/
@@ -58,7 +59,7 @@
 /**************************** Variable Definitions ****************************/
 
 /**************************** Constant Definitions ****************************/
-#define XDPPSU_NUM_RESET_VALUES		38
+#define XDPPSU_NUM_RESET_VALUES		34
 #define XDPPSU_NUM_MSA_RESET_VALUES	19
 /**
  * This table contains the default values for the DisplayPort TX core's general
@@ -89,15 +90,11 @@ u32 ResetValues[XDPPSU_NUM_RESET_VALUES][2] =
 	{XDPPSU_REPLY_DATA_COUNT, 0},
 	{XDPPSU_REPLY_STATUS, 0x10},
 	{XDPPSU_PHY_CONFIG, 0x03},
-	{XDPPSU_PHY_VOLTAGE_DIFF_LANE_0, 0},
-	{XDPPSU_PHY_VOLTAGE_DIFF_LANE_1, 0},
 	{XDPPSU_PHY_TRANSMIT_PRBS7, 0},
 	{XDPPSU_PHY_CLOCK_SELECT, 0},
 	{XDPPSU_TX_PHY_POWER_DOWN, 0},
 	{XDPPSU_PHY_PRECURSOR_LANE_0, 0},
 	{XDPPSU_PHY_PRECURSOR_LANE_1, 0},
-	{XDPPSU_PHY_POSTCURSOR_LANE_0, 0},
-	{XDPPSU_PHY_POSTCURSOR_LANE_1, 0},
 	{XDPPSU_TX_AUDIO_CONTROL, 0},
 	{XDPPSU_TX_AUDIO_CHANNELS, 0},
 	{XDPPSU_TX_AUDIO_INFO_DATA, 0},
