185efd548e0b1a77e6d3f876c58cbe20c509b57c
Eric Bong <nyongnyi@xilinx.com>
2017-10-26 19:10:30 +0800

    v_hdmitxss: Updated audio and reduced stack size V2
    Signed-off-by: Eric Bong <nyongnyi@xilinx.com>
    Acked-by:Ilias Ibrahim

--- XilinxProcessorIPLib/drivers/v_hdmitxss/src/xv_hdmitxss.h
+++ XilinxProcessorIPLib/drivers/v_hdmitxss/src/xv_hdmitxss.h
@@ -85,6 +85,7 @@
 * 4.0   YH     19/07/17 Added Video Masking APIs
 *       MH     09/08/17 Added function XV_HdmiTxSs_HdcpSetCapability
 *              22/08/17 Added function XV_HdmiTxSs_SetAudioFormat
+*       EB     10/10/17 Added function XV_HdmiTxSs_GetAudioFormat
 * </pre>
 *
 ******************************************************************************/
@@ -422,7 +423,9 @@ void XV_HdmiTxSs_SendAuxInfoframe(XV_HdmiTxSs *InstancePtr, void *AuxPtr);
 void XV_HdmiTxSs_SendGenericAuxInfoframe(XV_HdmiTxSs *InstancePtr, void *AuxPtr);
 void XV_HdmiTxSs_SetAudioChannels(XV_HdmiTxSs *InstancePtr, u8 AudioChannels);
 void XV_HdmiTxSs_AudioMute(XV_HdmiTxSs *InstancePtr, u8 Enable);
-void XV_HdmiTxSs_SetAudioFormat(XV_HdmiTxSs *InstancePtr, u8 format);
+void XV_HdmiTxSs_SetAudioFormat(XV_HdmiTxSs *InstancePtr, 
+    XV_HdmiTx_AudioFormatType format);
+XV_HdmiTx_AudioFormatType XV_HdmiTxSs_GetAudioFormat(XV_HdmiTxSs *InstancePtr);
 u32 XV_HdmiTxSs_SetStream(XV_HdmiTxSs *InstancePtr,
     XVidC_VideoMode VideoMode,
     XVidC_ColorFormat ColorFormat,
