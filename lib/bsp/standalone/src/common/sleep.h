58ee3e4a97fa07628517f3ff79b74ce6839134f8
Srija Malyala <srija.malyala@xilinx.com>
2017-12-22 12:43:57 +0530

    BSP: Added a Macro to replace the conditional loops
    This patch adds a macro which polls an address periodically until the
    condition is met or timeout occurs. This can be called where a
    conditinal loop is required
    
    Signed-off-by: Srija Malyala <srijam@xilinx.com>
    
    Acked-for-series: Anirudha Sarangi <anirudh@xilinx.com>

--- lib/bsp/standalone/src/common/sleep.h
+++ lib/bsp/standalone/src/common/sleep.h
@@ -59,6 +59,48 @@
 extern "C" {
 #endif
 
+/*****************************************************************************/
+/**
+*
+* This macro polls an address periodically until a condition is met or till the
+* timeout occurs.
+* The minimum timeout for calling this macro is 100us. If the timeout is less
+* than 100us, it still waits for 100us. Also the unit for the timeout is 100us.
+* If the timeout is not a multiple of 100us, it waits for a timeout of
+* the next usec value which is a multiple of 100us.
+*
+* @param            IO_func - accessor function to read the register contents.
+*                   Depends on the register width.
+* @param            ADDR - Address to be polled
+* @param            VALUE - variable to read the value
+* @param            COND - Condition to checked (usually involves VALUE)
+* @param            TIMEOUT_US - timeout in micro seconds
+*
+* @return           0 - when the condition is met
+*                   -1 - when the condition is not met till the timeout period
+*
+* @note             none
+*
+*****************************************************************************/
+#define Xil_poll_timeout(IO_func, ADDR, VALUE, COND, TIMEOUT_US) \
+ ( {	  \
+	u64 timeout = TIMEOUT_US/100;    \
+	if(TIMEOUT_US%100!=0)	\
+		timeout++;   \
+	for(;;) { \
+		VALUE = IO_func(ADDR); \
+		if(COND) \
+			break; \
+		else {    \
+			usleep(100);  \
+			timeout--; \
+			if(timeout==0) \
+			break;  \
+		}  \
+	}    \
+	(timeout>0) ? 0 : -1;  \
+ }  )
+
 void usleep(unsigned long useconds);
 void sleep(unsigned int seconds);
 int usleep_R5(unsigned long useconds);
