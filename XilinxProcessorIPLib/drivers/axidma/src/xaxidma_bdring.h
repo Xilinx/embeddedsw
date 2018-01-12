954b40e8a2da090aa5b43652fe7a4dc6c8a02095
Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
2018-01-12 15:27:08 +0530

    axidma: Use UINTPTR type for storing address
    u32 baseaddr variables doesn't work with 64-bit virtual/physical
    address.Issue is reported when driver is used as a UIO user space
    module on a 64-bit platform.
    
    Signed-off-by: Radhey Shyam Pandey <radheys@xilinx.com>
    
    Acked-for-series: Harini Katakam <harinik@xilinx.com>

--- XilinxProcessorIPLib/drivers/axidma/src/xaxidma_bdring.h
+++ XilinxProcessorIPLib/drivers/axidma/src/xaxidma_bdring.h
@@ -15,21 +15,19 @@
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
 *
-* Except as contained in this notice, the name of the Xilinx shall not be used
-* in advertising or otherwise to promote the sale, use or other dealings in
-* this Software without prior written authorization from Xilinx.
+*
 *
 ******************************************************************************/
 /*****************************************************************************/
 /**
 *
 * @file xaxidma_bdring.h
-* @addtogroup axidma_v9_9
+* @addtogroup axidma_v9_10
 * @{
 *
 * This file contains DMA channel related structure and constant definition
