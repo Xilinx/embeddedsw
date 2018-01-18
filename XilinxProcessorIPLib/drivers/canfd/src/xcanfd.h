dba7d4f22e016761b0b4c5eb9d9be73da3fbee1a
Mousumi Jana <mousumi.jana@xilinx.com>
2018-01-18 15:50:29 +0530

    canfd: Fixed compilation warnings.
    This patch has a fix for compilation warnings in canfd driver.
    1.xcanfd.h:387:18: warning: result of '2650 << 21' requires
      34 bits to represent, but 'int' only has 32 bits [-Wshift-overflow=].
    2.xcanfd.c:1006:20: warning: comparison of unsigned expression >= 0 is
      always true [-Wtype-limits].
    3.xcanfd.c:1044:1: warning: control reaches end of non-void function.
    
    Signed-off-by: Mousumi Jana <mousumij@xilinx.com>
    Acked-by: Kedareswara rao Appana <appanad@xilinx.com>
    
    Acked-by: Kedareswara rao Appana <appanad@xilinx.com>

--- XilinxProcessorIPLib/drivers/canfd/src/xcanfd.h
+++ XilinxProcessorIPLib/drivers/canfd/src/xcanfd.h
@@ -382,7 +382,7 @@ typedef struct {
 *****************************************************************************/
 #define XCanFd_CreateIdValue(StandardId, SubRemoteTransReq, IdExtension, \
 		ExtendedId, RemoteTransReq) \
-	((((StandardId) << XCANFD_IDR_ID1_SHIFT) & XCANFD_IDR_ID1_MASK) | \
+	(((((u64)StandardId) << XCANFD_IDR_ID1_SHIFT) & XCANFD_IDR_ID1_MASK) | \
 	(((SubRemoteTransReq) << XCANFD_IDR_SRR_SHIFT) & XCANFD_IDR_SRR_MASK) | \
 	(((IdExtension) << XCANFD_IDR_IDE_SHIFT) & XCANFD_IDR_IDE_MASK) | \
 	(((ExtendedId) << XCANFD_IDR_ID2_SHIFT) & XCANFD_IDR_ID2_MASK) | \
