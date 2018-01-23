96e7bb53eea6aa5c21dd8d150c1d45d8f8a80a75
Jyothisri Ravula <jyothisr@xilinx.com>
2018-01-23 13:45:26 +0530

    Addtogroup version updated for iomodule
    Signed-off-by: Jyothisri Ravula <jyothisr@xilinx.com>

--- XilinxProcessorIPLib/drivers/iomodule/src/xiomodule_selftest.c
+++ XilinxProcessorIPLib/drivers/iomodule/src/xiomodule_selftest.c
@@ -33,7 +33,7 @@
 /**
 *
 * @file xiomodule_selftest.c
-* @addtogroup iomodule_v2_2
+* @addtogroup iomodule_v2_5
 * @{
 *
 * Contains diagnostic self-test functions for the XIOModule component. This
