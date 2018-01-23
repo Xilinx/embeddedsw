f0ed5a92a3e97d91c960ee2c569a7b34d695bbd9
Jyothisri Ravula <jyothisr@xilinx.com>
2018-01-23 13:46:11 +0530

    Addtogroup version updated for wdttb
    Signed-off-by: Jyothisri Ravula <jyothisr@xilinx.com>

--- XilinxProcessorIPLib/drivers/wdttb/src/xwdttb.h
+++ XilinxProcessorIPLib/drivers/wdttb/src/xwdttb.h
@@ -33,7 +33,7 @@
 /**
 *
 * @file xwdttb.h
-* @addtogroup wdttb_v4_0
+* @addtogroup wdttb_v4_2
 * @{
 * @details
 *
--- XilinxProcessorIPLib/drivers/wdttb/src/xwdttb_l.h
+++ XilinxProcessorIPLib/drivers/wdttb/src/xwdttb_l.h
@@ -33,7 +33,7 @@
 /**
 *
 * @file xwdttb_l.h
-* @addtogroup wdttb_v4_0
+* @addtogroup wdttb_v4_2
 * @{
 *
 * This header file contains identifiers and basic driver functions (or
