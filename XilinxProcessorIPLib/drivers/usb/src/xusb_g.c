8d6b8de8dc48d5d55d9b5b9d0bf4ce30d5893123
Jyothisri Ravula <jyothisr@xilinx.com>
2018-01-25 16:02:38 +0530

    updated copyright for usb
    Signed-off-by: Jyothisri Ravula <jyothisr@xilinx.com>

--- XilinxProcessorIPLib/drivers/usb/src/xusb_g.c
+++ XilinxProcessorIPLib/drivers/usb/src/xusb_g.c
@@ -1,4 +1,3 @@
-/* $Id: xusb_g.c,v 1.1.2.1 2011/08/23 11:35:45 vidhum Exp $ */
 /******************************************************************************
 *
 * Copyright (C) 2006 Vreelin Engineering, Inc.  All Rights Reserved.
