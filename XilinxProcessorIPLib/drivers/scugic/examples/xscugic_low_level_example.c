c309fbf097b8ad54ab266bb88bf5748b0f8f9d70
Jyothisri Ravula <jyothisr@xilinx.com>
2018-01-25 16:02:18 +0530

    updated copyright for scugic
    Signed-off-by: Jyothisri Ravula <jyothisr@xilinx.com>

--- XilinxProcessorIPLib/drivers/scugic/examples/xscugic_low_level_example.c
+++ XilinxProcessorIPLib/drivers/scugic/examples/xscugic_low_level_example.c
@@ -1,4 +1,3 @@
-/* $Id: xscugic_low_level_example.c,v 1.1.2.2 2011/03/23 07:46:42 sadanan Exp $ */
 /******************************************************************************
 *
 * Copyright (C) 2010 - 2015 Xilinx, Inc.  All rights reserved.
