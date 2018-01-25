1d78833494fe8e83dc91a33dbed9861a496a08c9
Jyothisri Ravula <jyothisr@xilinx.com>
2018-01-25 16:01:39 +0530

    updated copyright for bram
    Signed-off-by: Jyothisri Ravula <jyothisr@xilinx.com>

--- XilinxProcessorIPLib/drivers/bram/data/bram.tcl
+++ XilinxProcessorIPLib/drivers/bram/data/bram.tcl
@@ -28,6 +28,7 @@
 # in advertising or otherwise to promote the sale, use or other dealings in
 # this Software without prior written authorization from Xilinx.
 #
+###############################################################################
 #
 # Ver      Who    Date     Changes
 # -------- ------ -------- ----------------------------------------------------
