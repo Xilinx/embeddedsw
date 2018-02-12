7f444ce97a62c0a4cbf2c1d103f14786791a279f
Harini Katakam <harini.katakam@xilinx.com>
2018-02-12 19:06:29 +0530

    drivers: Add new driver for XXV Ethernet MAC
    XXV Ethernet is a soft IP MAC that supports upto 10G speed.
    This patch adds a new driver for the IP along with an example
    that demonstrates use of this IP with MCDMA.
    
    Signed-off-by: Harini Katakam <harinik@xilinx.com>
    
    Acked-by: Kedareswara rao Appana <appanad@xilinx.com>

--- XilinxProcessorIPLib/drivers/xxvethernet/src/xxxvethernet.h
+++ XilinxProcessorIPLib/drivers/xxvethernet/src/xxxvethernet.h
@@ -12,10 +12,6 @@
 * The above copyright notice and this permission notice shall be included in
 * all copies or substantial portions of the Software.
 *
-* Use of the Software is limited solely to applications:
-* (a) running on a Xilinx device, or
-* (b) that interact with a Xilinx device through a bus or interconnect.
-*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -32,7 +28,7 @@
 /**
 *
 * @file xxxvethernet.h
-* @addtogroup xxvethernet_v1_0
+* @addtogroup xxvethernet_v1_1
 * @{
 * @details
 *
@@ -135,6 +131,8 @@
 * ----- ---- -------- ---------------------------------------------------------
 * 1.0   hk   6/16/17  First release
 *       hk   2/15/18  Add support for USXGMII
+* 1.1   mj   3/30/18  Add Macro XXxvEthernet_IsMcDma(InstancePtr) to check
+*                     McDma is connected or not.
 * </pre>
 *
 ******************************************************************************/
@@ -472,6 +470,24 @@ typedef struct XXxvEthernet {
 	XXxvEthernet_ReadReg((InstancePtr)->Config.BaseAddress,		\
 				XXE_ANSR_OFFSET);
 
+/*****************************************************************************/
+/**
+*
+* XXxvEthernet_IsMcDma reports if the device is currently connected to MCDMA.
+*
+* @param	InstancePtr is a pointer to the Xxv Ethernet instance to be
+*		worked on.
+* @return
+*		- TRUE if the Xxv Ethernet device is connected MCDMA.
+*		- FALSE.if the Xxv Ethernet device is NOT connected to MCDMA
+*
+* @note 	C-style signature:
+* 		u32 XXxvEthernet_IsMcDma(XXxvEthernet *InstancePtr)
+*
+******************************************************************************/
+#define XXxvEthernet_IsMcDma(InstancePtr) \
+	(((InstancePtr)->Config.XxvDevType == XPAR_MCDMA) ? TRUE: FALSE)
+
 /************************** Function Prototypes ******************************/
 
 /*
@@ -487,6 +503,7 @@ void XXxvEthernet_Reset(XXxvEthernet *InstancePtr);
  * Initialization functions in xxxvtemac_sinit.c
  */
 XXxvEthernet_Config *XXxvEthernet_LookupConfig(u16 DeviceId);
+XXxvEthernet_Config *XXxvEthernet_LookupConfigBaseAddr(UINTPTR Baseaddr);
 
 /*
  * MAC configuration/control functions in xxxvethernet.c
--- XilinxProcessorIPLib/drivers/xxvethernet/src/xxxvethernet_hw.h
+++ XilinxProcessorIPLib/drivers/xxvethernet/src/xxxvethernet_hw.h
@@ -12,10 +12,6 @@
 * The above copyright notice and this permission notice shall be included in
 * all copies or substantial portions of the Software.
 *
-* Use of the Software is limited solely to applications:
-* (a) running on a Xilinx device, or
-* (b) that interact with a Xilinx device through a bus or interconnect.
-*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -32,7 +28,7 @@
 /**
 *
 * @file xxxvethernet_hw.h
-* @addtogroup xxvethernet_v1_0
+* @addtogroup xxvethernet_v1_1
 * @{
 *
 * This file contains definitions for register offset, masks and low level
