f2f0c10526024866761ba72f7648f0c1a608aa6b
Rahul Bhansali <rahul.bhansali@xilinx.com>
2018-02-15 09:33:03 -0500

    LwIP: Socket mode TCP perf server application
    This patch adds FreeRTOS TCP perf server application for downlink
    performance measurement with remote Iperf-2.0.5 TCP client.
    
    Signed-off-by: Rahul Bhansali <rahul.bhansali@xilinx.com>
    Signed-off-by: Maulik Jodhani <maulik.jodhani@xilinx.com>

---
+++ lib/sw_apps/freertos_lwip_tcp_perf_server/src/freertos_tcp_perf_server.c
@@ -0,0 +1,226 @@
+/******************************************************************************
+*
+* Copyright (C) 2018 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running on a Xilinx device, or
+* (b) that interact with a Xilinx device through a bus or interconnect.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+
+#include "freertos_tcp_perf_server.h"
+
+extern struct netif server_netif;
+static struct perf_stats server;
+
+/* Interval time in seconds */
+#define REPORT_INTERVAL_TIME (INTERIM_REPORT_INTERVAL * 1000)
+
+void print_app_header(void)
+{
+	xil_printf("TCP server listening on port %d\r\n",
+			TCP_CONN_PORT);
+	xil_printf("On Host: Run $iperf -c %s -i %d -t 300 -w 2M\r\n",
+			inet_ntoa(server_netif.ip_addr),
+			INTERIM_REPORT_INTERVAL);
+}
+
+static void print_tcp_conn_stats(int sock)
+{
+	struct sockaddr_in local, remote;
+	int size;
+
+	size = sizeof(local);
+	getsockname(sock, (struct sockaddr *)&local, (socklen_t *)&size);
+	xil_printf("[%3d] local %s port %d connected with ", server.client_id,
+			inet_ntoa(local.sin_addr), ntohs(local.sin_port));
+
+	getpeername(sock, (struct sockaddr *)&remote, (socklen_t *)&size);
+	xil_printf("%s port %d\r\n", inet_ntoa(remote.sin_addr),
+			ntohs(local.sin_port));
+
+	xil_printf("[ ID] Interval    Transfer     Bandwidth\n\r");
+}
+
+static void stats_buffer(char* outString, double data, enum measure_t type)
+{
+	int conv = KCONV_UNIT;
+	const char *format;
+	double unit = 1024.0;
+
+	if (type == SPEED)
+		unit = 1000.0;
+
+	while (data >= unit && conv <= KCONV_GIGA) {
+		data /= unit;
+		conv++;
+	}
+
+	/* Fit data in 4 places */
+	if (data < 9.995) { /* 9.995 rounded to 10.0 */
+		format = "%4.2f %c"; /* #.## */
+	} else if (data < 99.95) { /* 99.95 rounded to 100 */
+		format = "%4.1f %c"; /* ##.# */
+	} else {
+		format = "%4.0f %c"; /* #### */
+	}
+	sprintf(outString, format, data, kLabel[conv]);
+}
+
+/* The report function of a TCP server session */
+static void tcp_conn_report(u64_t diff, enum report_type report_type)
+{
+	u64_t total_len;
+	double duration, bandwidth = 0;
+	char data[16], perf[16], time[64];
+
+	if (report_type == INTER_REPORT) {
+		total_len = server.i_report.total_bytes;
+	} else {
+		server.i_report.last_report_time = 0;
+		total_len = server.total_bytes;
+	}
+
+	/* Converting duration from milliseconds to secs,
+	 * and bandwidth to bits/sec .
+	 */
+	duration = diff / 1000.0; /* secs */
+	if (duration)
+		bandwidth = (total_len / duration) * 8.0;
+
+	stats_buffer(data, total_len, BYTES);
+	stats_buffer(perf, bandwidth, SPEED);
+	/* On 32-bit platforms, xil_printf is not able to print
+	 * u64_t values, so converting these values in strings and
+	 * displaying results
+	 */
+	sprintf(time, "%4.1f-%4.1f sec",
+			(double)server.i_report.last_report_time,
+			(double)(server.i_report.last_report_time + duration));
+	xil_printf("[%3d] %s  %sBytes  %sbits/sec\n\r", server.client_id,
+			time, data, perf);
+
+	if (report_type == INTER_REPORT)
+		server.i_report.last_report_time += duration;
+}
+
+/* thread spawned for each connection */
+void tcp_recv_perf_traffic(void *p)
+{
+	char recv_buf[RECV_BUF_SIZE];
+	int read_bytes;
+	int sock = *((int *)p);
+
+	server.start_time = sys_now() * portTICK_RATE_MS;
+	server.client_id++;
+	server.i_report.last_report_time = 0;
+	server.i_report.start_time = 0;
+	server.i_report.total_bytes = 0;
+	server.total_bytes = 0;
+
+	print_tcp_conn_stats(sock);
+
+	while (1) {
+		/* read a max of RECV_BUF_SIZE bytes from socket */
+		if ((read_bytes = lwip_recvfrom(sock, recv_buf, RECV_BUF_SIZE,
+						0, NULL, NULL)) < 0) {
+			u64_t now = sys_now() * portTICK_RATE_MS;
+			u64_t diff_ms = now - server.start_time;
+			tcp_conn_report(diff_ms, TCP_ABORTED_REMOTE);
+			break;
+		}
+
+		/* break if client closed connection */
+		if (read_bytes == 0) {
+			u64_t now = sys_now() * portTICK_RATE_MS;
+			u64_t diff_ms = now - server.start_time;
+			tcp_conn_report(diff_ms, TCP_DONE_SERVER);
+			xil_printf("TCP test passed Successfully\n\r");
+			break;
+		}
+
+		if (REPORT_INTERVAL_TIME) {
+			u64_t now = sys_now() * portTICK_RATE_MS;
+			server.i_report.total_bytes += read_bytes;
+			if (server.i_report.start_time) {
+				u64_t diff_ms = now - server.i_report.start_time;
+
+				if (diff_ms >= REPORT_INTERVAL_TIME) {
+					tcp_conn_report(diff_ms, INTER_REPORT);
+					server.i_report.start_time = 0;
+					server.i_report.total_bytes = 0;
+				}
+			} else {
+				server.i_report.start_time = now;
+			}
+		}
+		/* Record total bytes for final report */
+		server.total_bytes += read_bytes;
+	}
+
+	/* close connection */
+	close(sock);
+	vTaskDelete(NULL);
+}
+
+void start_application(void)
+{
+	int sock, new_sd;
+	struct sockaddr_in address, remote;
+	int size;
+
+	if ((sock = lwip_socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		xil_printf("TCP server: Error creating Socket\r\n");
+		return;
+	}
+
+	address.sin_family = AF_INET;
+	address.sin_port = htons(TCP_CONN_PORT);
+	address.sin_addr.s_addr = INADDR_ANY;
+
+	if (bind(sock, (struct sockaddr *)&address, sizeof (address)) < 0) {
+		xil_printf("TCP server: Unable to bind to port %d\r\n",
+				TCP_CONN_PORT);
+		close(sock);
+		return;
+	}
+
+	if (listen(sock, 1) < 0) {
+		xil_printf("TCP server: tcp_listen failed\r\n");
+		close(sock);
+		return;
+	}
+
+	size = sizeof(remote);
+
+	while (1) {
+		if ((new_sd = accept(sock, (struct sockaddr *)&remote,
+						(socklen_t *)&size)) > 0)
+			sys_thread_new("TCP_recv_perf thread",
+				tcp_recv_perf_traffic, (void*)&new_sd,
+				TCP_SERVER_THREAD_STACKSIZE,
+				DEFAULT_THREAD_PRIO);
+	}
+}
