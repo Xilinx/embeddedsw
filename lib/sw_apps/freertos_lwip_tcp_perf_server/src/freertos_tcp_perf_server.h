05a80fb1023212f7bcbce88caddfa48cef3740c7
Rahul Bhansali <rahul.bhansali@xilinx.com>
2018-02-15 09:33:03 -0500

    LwIP: Socket mode TCP perf server application
    This patch adds FreeRTOS TCP perf server application for downlink
    performance measurement with remote Iperf-2.0.5 TCP client.
    
    Signed-off-by: Rahul Bhansali <rahul.bhansali@xilinx.com>
    Signed-off-by: Maulik Jodhani <maulik.jodhani@xilinx.com>

---
+++ lib/sw_apps/freertos_lwip_tcp_perf_server/src/freertos_tcp_perf_server.h
@@ -0,0 +1,100 @@
+/******************************************************************************
+*
+* Copyright (C) 2018 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running on a Xilinx device, or
+* (b) that interact with a Xilinx device through a bus or interconnect.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+
+#ifndef __FREERTOS_TCP_SERVER_APP_H_
+#define __FREERTOS_TCP_SERVER_APP_H_
+
+#include "lwipopts.h"
+#include "lwip/tcp.h"
+#include "lwip/ip_addr.h"
+#include "lwip/inet.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "xil_printf.h"
+
+/* used as indices into kLabel[] */
+enum {
+	KCONV_UNIT,
+	KCONV_KILO,
+	KCONV_MEGA,
+	KCONV_GIGA,
+};
+
+/* labels for formats [KMG] */
+const char kLabel[] =
+{
+	' ',
+	'K',
+	'M',
+	'G'
+};
+
+/* used as type of print */
+enum measure_t {
+	BYTES,
+	SPEED
+};
+
+/* Report type */
+enum report_type {
+	/* The Intermediate report */
+	INTER_REPORT,
+	/* The server side test is done */
+	TCP_DONE_SERVER,
+	/* Remote side aborted the test */
+	TCP_ABORTED_REMOTE
+};
+
+struct interim_report {
+	u64_t start_time;
+	u64_t last_report_time;
+	u32_t total_bytes;
+};
+
+struct perf_stats {
+	u8_t client_id;
+	u64_t start_time;
+	u64_t total_bytes;
+	struct interim_report i_report;
+};
+
+#define RECV_BUF_SIZE 1500
+
+#define TCP_SERVER_THREAD_STACKSIZE 2048
+
+/* server port to listen on/connect to */
+#define TCP_CONN_PORT 5001
+
+/* seconds between periodic bandwidth reports */
+#define INTERIM_REPORT_INTERVAL 5
+
+#endif /* __FREERTOS_TCP_SERVER_APP_H_ */
