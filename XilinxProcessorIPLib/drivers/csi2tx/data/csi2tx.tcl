814c1098d820dac39ca5980d0f8050f5e0f97cc1
Vishal Sagar <vishal.sagar@xilinx.com>
2018-03-01 10:02:07 +0530

    csi2tx : Add Frame End generation support
    Adds support for Frame End generation feature.
    There are 2 APIs added to set or get the Line count per Virtual Channel.
    Callback functions can be registered per VC for Line Count status.
    These are called when Line Count in VC is less or more lines are
    received than programmed. To clear the Line Count interrupt, we need to
    write 0x3 in the corresponding bit-field in ISR.
    
    Signed-off-by: Vishal Sagar <vsagar@xilinx.com>

--- XilinxProcessorIPLib/drivers/csi2tx/data/csi2tx.tcl
+++ XilinxProcessorIPLib/drivers/csi2tx/data/csi2tx.tcl
@@ -32,6 +32,7 @@
 # Ver Who Date     Changes
 # --- --- -------- -----------------------------------------------------------
 # 1.0 sss 07/21/16 Initial version
+# 1.1 vsa 02/28/18 Added Frame Generation feature
 ##############################################################################
 
 #uses "xillib.tcl"
@@ -39,7 +40,7 @@
 set periph_ninstances    0
 
 proc generate {drv_handle} {
-  ::hsi::utils::define_include_file $drv_handle "xparameters.h" "XCsi2Tx" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_CSI_LANES" "C_CSI_EN_ACTIVELANES"
-  ::hsi::utils::define_canonical_xpars $drv_handle "xparameters.h" "Csi2Tx" "DEVICE_ID" "C_BASEADDR" "C_CSI_LANES" "C_CSI_EN_ACTIVELANES"
-  ::hsi::utils::define_config_file  $drv_handle "xcsi2tx_g.c" "XCsi2Tx" "DEVICE_ID" "C_BASEADDR" "C_CSI_LANES" "C_CSI_EN_ACTIVELANES"
+  ::hsi::utils::define_include_file $drv_handle "xparameters.h" "XCsi2Tx" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_CSI_LANES" "C_CSI_EN_ACTIVELANES" "C_EN_REG_BASED_FE_GEN"
+  ::hsi::utils::define_canonical_xpars $drv_handle "xparameters.h" "Csi2Tx" "DEVICE_ID" "C_BASEADDR" "C_CSI_LANES" "C_CSI_EN_ACTIVELANES" "C_EN_REG_BASED_FE_GEN"
+  ::hsi::utils::define_config_file  $drv_handle "xcsi2tx_g.c" "XCsi2Tx" "DEVICE_ID" "C_BASEADDR" "C_CSI_LANES" "C_CSI_EN_ACTIVELANES" "C_EN_REG_BASED_FE_GEN"
 }
