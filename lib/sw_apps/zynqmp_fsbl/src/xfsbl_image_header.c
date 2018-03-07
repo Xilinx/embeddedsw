4476b1cc4b7b6acf36cdd0d8742b06e9f0582f95
VNSL Durga <vnsl.durga.challa@xilinx.com>
2018-03-07 17:14:26 +0530

    Zynqmp_fsbl: Added check for ENC_ONLY
    When eFUSE bit ENC_ONLY bit is blown, it is mandatory to
    encrypt all the partitions, if any of the partition
    is not been encrypted, FSBL throws an error.
    This check can be skipped by setting FSBL_FORCE_ENC_EXCLUDE_VAL
    in xfsbl_config.h file during compilation.
    
    Signed-off-by: VNSL Durga <vnsldurg@xilinx.com>

--- lib/sw_apps/zynqmp_fsbl/src/xfsbl_image_header.c
+++ lib/sw_apps/zynqmp_fsbl/src/xfsbl_image_header.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2015 - 17 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2015 - 18 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -51,6 +51,8 @@
 *       vns  02/17/17 If authentication is enabled whole image header
 *                     is been validated by copying to OCM.
 *                     Updated destination CPU for PMUFW.
+* 3.0   vns  03/07/18 All the partitions should be encrypted when ENC_ONLY
+*                     eFUSE bit is set, if not encrypted FSBL throw an error.
 * </pre>
 *
 * @note
@@ -606,7 +608,9 @@ u32 XFsbl_ValidatePartitionHeader(
 	u32 DestinationCpu;
 	u32 DestinationDevice;
 	u32 IsEncrypted, IsAuthenticated;
-
+#if defined(XFSBL_SECURE) && defined(XFSBL_FORCE_ENC)
+	u32 EncOnly;
+#endif
 
 	/**
 	 * Update the variables
@@ -631,6 +635,18 @@ u32 XFsbl_ValidatePartitionHeader(
 		IsAuthenticated = FALSE;
 	}
 
+#if defined(XFSBL_SECURE) && defined(XFSBL_FORCE_ENC)
+	/* Check if encryption is mandatory */
+	EncOnly = XFsbl_In32(EFUSE_SEC_CTRL) & EFUSE_SEC_CTRL_ENC_ONLY_MASK;
+	if ((EncOnly != 0x00) && (IsEncrypted == FALSE)) {
+		Status = XFSBL_ERROR_ENC_IS_MANDATORY;
+		XFsbl_Printf(DEBUG_GENERAL,
+				"XFSBL_ERROR_ENC_IS_MANDATORY as "
+				"eFUSE ENC_ONLY bit is set\r\n");
+		Status = XFSBL_FAILURE;
+		goto END;
+	}
+#endif
 
 	DestinationCpu = XFsbl_GetDestinationCpu(PartitionHeader);
 		if(ResetType == XFSBL_APU_ONLY_RESET){
