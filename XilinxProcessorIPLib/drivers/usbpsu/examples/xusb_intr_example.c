dcbf39b469cd3ff6386783449dc146688c7e905b
Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>
2018-03-14 13:01:11 +0530

    usbpsu: move the setup interrupt system calls from driver to examples
    This patch modifies the usbpsu driver to move the setup interrupt
    system calls from the driver to examples.
    
    Signed-off-by: Anurag Kumar Vulisha <anuragku@xilinx.com>
    
    Acked-for-series: Ajay Yugalkishore Pandey <APANDEY@xilinx.com>

--- XilinxProcessorIPLib/drivers/usbpsu/examples/xusb_intr_example.c
+++ XilinxProcessorIPLib/drivers/usbpsu/examples/xusb_intr_example.c
@@ -47,7 +47,8 @@
  * 1.4   BK  12/01/18 Renamed the file and added changes to have a common
  *		      example for all USB IPs.
  *	 vak 22/01/18 Added changes for supporting microblaze platform
- *
+ *	 vak 13/03/18 Moved the setup interrupt system calls from driver to
+ *		      example.
  * </pre>
  *
  *****************************************************************************/
@@ -59,6 +60,16 @@
 #include <stdio.h>
 #include "xusb_ch9_storage.h"
 #include "xusb_class_storage.h"
+#include "xusb_wrapper.h"
+#include "xil_exception.h"
+
+#ifdef __MICROBLAZE__
+#ifdef XPAR_INTC_0_DEVICE_ID
+#include "xintc.h"
+#endif /* XPAR_INTC_0_DEVICE_ID */
+#elif defined PLATFORM_ZYNQMP
+#include "xscugic.h"
+#endif
 
 /************************** Constant Definitions ****************************/
 #define MEMORY_SIZE (64 * 1024)
@@ -79,28 +90,34 @@ void BulkOutHandler(void *CallBackRef, u32 RequestedBytes,
 							u32 BytesTxed);
 void BulkInHandler(void *CallBackRef, u32 RequestedBytes,
 							u32 BytesTxed);
+static s32 SetupInterruptSystem(struct XUsbPsu *InstancePtr, u16 IntcDeviceID,
+		u16 USB_INTR_ID, void *IntcPtr);
 
 /************************** Variable Definitions *****************************/
 struct Usb_DevData UsbInstance;
 
 Usb_Config *UsbConfigPtr;
 
+#ifdef __MICROBLAZE__
 #ifdef XPAR_INTC_0_DEVICE_ID
 XIntc	InterruptController;	/*XIntc interrupt controller instance */
+#endif /* XPAR_INTC_0_DEVICE_ID */
 #else
 XScuGic	InterruptController;	/* Interrupt controller instance */
 #endif
 
-#ifdef	XPAR_INTC_0_DEVICE_ID	/* MICROBLAZE */
+#ifdef __MICROBLAZE__		/* MICROBLAZE */
+#ifdef	XPAR_INTC_0_DEVICE_ID
 #define	INTC_DEVICE_ID		XPAR_INTC_0_DEVICE_ID
-#define	USB_INTR_ID		XPAR_AXI_INTC_0_ZYNQ_ULTRA_PS_E_0_PS_PL_IRQ_USB3_0_ENDPOINT_0_INTR
+#define	USB_INT_ID		XPAR_AXI_INTC_0_ZYNQ_ULTRA_PS_E_0_PS_PL_IRQ_USB3_0_ENDPOINT_0_INTR
+#endif /* MICROBLAZE */
 #elif	defined	PLATFORM_ZYNQMP	/* ZYNQMP */
 #define	INTC_DEVICE_ID		XPAR_SCUGIC_SINGLE_DEVICE_ID
-#define	USB_INTR_ID		XPAR_XUSBPS_0_INTR
+#define	USB_INT_ID		XPAR_XUSBPS_0_INTR
 #define	USB_WAKEUP_INTR_ID	XPAR_XUSBPS_0_WAKE_INTR
 #else	/* OTHERS */
 #define	INTC_DEVICE_ID		0
-#define	USB_INTR_ID		0
+#define	USB_INT_ID		0
 #endif
 
 /* Buffer for virtual flash disk space. */
@@ -196,7 +213,7 @@ int main(void)
 	}
 
 	/*
-     * set endpoint handlers
+	 * set endpoint handlers
 	 * BulkOutHandler - to be called when data is received
 	 * BulkInHandler -  to be called when data is sent
 	 */
@@ -207,7 +224,7 @@ int main(void)
 
 	/* setup interrupts */
 	Status = SetupInterruptSystem(UsbInstance.PrivateData, INTC_DEVICE_ID,
-					USB_INTR_ID, (void *)&InterruptController);
+					USB_INT_ID, (void *)&InterruptController);
 	if (Status != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
@@ -284,4 +301,180 @@ void BulkInHandler(void *CallBackRef, u32 RequestedBytes,
 		/* Receive next CBW */
 		EpBufferRecv(InstancePtr->PrivateData, 1, (u8*)&CBW, sizeof(CBW));
 	}
+}
+
+/****************************************************************************/
+/**
+* This function setups the interrupt system such that interrupts can occur.
+* This function is application specific since the actual system may or may not
+* have an interrupt controller.  The USB controller could be
+* directly connected to a processor without an interrupt controller.
+* The user should modify this function to fit the application.
+*
+* @param	InstancePtr is a pointer to the XUsbPsu instance.
+* @param	IntcDeviceID is the unique ID of the interrupt controller
+* @param	USB_INTR_ID is the interrupt ID of the USB controller
+* @param	IntcPtr is a pointer to the interrupt controller
+*			instance.
+*
+* @return	XST_SUCCESS if successful, otherwise XST_FAILURE.
+*
+* @note		None.
+*
+*****************************************************************************/
+static s32 SetupInterruptSystem(struct XUsbPsu *InstancePtr, u16 IntcDeviceID,
+		u16 USB_INTR_ID, void *IntcPtr)
+{
+	/*
+	 * This below is done to remove warnings which occur when usbpsu
+	 * driver is compiled for platforms other than MICROBLAZE or ZYNQMP
+	 */
+	(void)InstancePtr;
+	(void)IntcDeviceID;
+	(void)IntcPtr;
+
+#ifdef __MICROBLAZE__
+#ifdef XPAR_INTC_0_DEVICE_ID
+	s32 Status;
+
+	XIntc *IntcInstancePtr = (XIntc *)IntcPtr;
+
+	/*
+	 * Initialize the interrupt controller driver.
+	 */
+	Status = XIntc_Initialize(IntcInstancePtr, IntcDeviceID);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+
+	/*
+	 * Connect a device driver handler that will be called when an interrupt
+	 * for the USB device occurs.
+	 */
+	Status = XIntc_Connect(IntcInstancePtr, USB_INTR_ID,
+			       (Xil_ExceptionHandler)XUsbPsu_IntrHandler,
+			       (void *) InstancePtr);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Start the interrupt controller such that interrupts are enabled for
+	 * all devices that cause interrupts, specific real mode so that
+	 * the USB can cause interrupts through the interrupt controller.
+	 */
+	Status = XIntc_Start(IntcInstancePtr, XIN_REAL_MODE);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Enable the interrupt for the USB.
+	 */
+	XIntc_Enable(IntcInstancePtr, USB_INTR_ID);
+
+	/*
+	 * Initialize the exception table
+	 */
+	Xil_ExceptionInit();
+
+	/*
+	 * Enable interrupts for Reset, Disconnect, ConnectionDone, Link State
+	 * Wakeup and Overflow events.
+	 */
+	XUsbPsu_EnableIntr(InstancePtr, XUSBPSU_DEVTEN_EVNTOVERFLOWEN |
+                        XUSBPSU_DEVTEN_WKUPEVTEN |
+                        XUSBPSU_DEVTEN_ULSTCNGEN |
+                        XUSBPSU_DEVTEN_CONNECTDONEEN |
+                        XUSBPSU_DEVTEN_USBRSTEN |
+                        XUSBPSU_DEVTEN_DISCONNEVTEN);
+
+	/*
+	 * Register the interrupt controller handler with the exception table
+	 */
+	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT,
+				(Xil_ExceptionHandler)XIntc_InterruptHandler,
+				IntcInstancePtr);
+#endif /* XPAR_INTC_0_DEVICE_ID */
+#elif defined PLATFORM_ZYNQMP
+	s32 Status;
+
+	XScuGic_Config *IntcConfig; /* The configuration parameters of the
+					interrupt controller */
+
+	XScuGic *IntcInstancePtr = (XScuGic *)IntcPtr;
+
+	/*
+	 * Initialize the interrupt controller driver
+	 */
+	IntcConfig = XScuGic_LookupConfig(IntcDeviceID);
+	if (NULL == IntcConfig) {
+		return XST_FAILURE;
+	}
+
+	Status = XScuGic_CfgInitialize(IntcInstancePtr, IntcConfig,
+								   IntcConfig->CpuBaseAddress);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Connect to the interrupt controller
+	 */
+	Status = XScuGic_Connect(IntcInstancePtr, USB_INTR_ID,
+							(Xil_ExceptionHandler)XUsbPsu_IntrHandler,
+							(void *)InstancePtr);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+#ifdef XUSBPSU_HIBERNATION_ENABLE
+	Status = XScuGic_Connect(IntcInstancePtr, USB_WAKEUP_INTR_ID,
+							(Xil_ExceptionHandler)XUsbPsu_WakeUpIntrHandler,
+							(void *)InstancePtr);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+#endif
+
+	/*
+	 * Enable the interrupt for the USB
+	 */
+	XScuGic_Enable(IntcInstancePtr, USB_INTR_ID);
+#ifdef XUSBPSU_HIBERNATION_ENABLE
+	XScuGic_Enable(IntcInstancePtr, USB_WAKEUP_INTR_ID);
+#endif
+
+	/*
+	 * Enable interrupts for Reset, Disconnect, ConnectionDone, Link State
+	 * Wakeup and Overflow events.
+	 */
+	XUsbPsu_EnableIntr(InstancePtr, XUSBPSU_DEVTEN_EVNTOVERFLOWEN |
+                        XUSBPSU_DEVTEN_WKUPEVTEN |
+                        XUSBPSU_DEVTEN_ULSTCNGEN |
+                        XUSBPSU_DEVTEN_CONNECTDONEEN |
+                        XUSBPSU_DEVTEN_USBRSTEN |
+                        XUSBPSU_DEVTEN_DISCONNEVTEN);
+
+#ifdef XUSBPSU_HIBERNATION_ENABLE
+	if (InstancePtr->HasHibernation)
+		XUsbPsu_EnableIntr(InstancePtr,
+				XUSBPSU_DEVTEN_HIBERNATIONREQEVTEN);
+#endif
+
+	/*
+	 * Connect the interrupt controller interrupt handler to the hardware
+	 * interrupt handling logic in the ARM processor.
+	 */
+	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT,
+								(Xil_ExceptionHandler)XScuGic_InterruptHandler,
+								IntcInstancePtr);
+#endif /* PLATFORM_ZYNQMP */
+
+	/*
+	 * Enable interrupts in the ARM
+	 */
+	Xil_ExceptionEnable();
+
+	return XST_SUCCESS;
 }
