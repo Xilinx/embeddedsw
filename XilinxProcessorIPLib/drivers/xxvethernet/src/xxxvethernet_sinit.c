c6c1a1c766f005f3f9b794014968099d0c26b679
Maulik Jodhani <maulik.jodhani@xilinx.com>
2018-03-19 06:29:07 -0400

    xxvethernet: Add API to lookup by base address & IsMcdma
    - Add API XXxvEthernet_LookupConfigBaseAddr(UINTPTR Baseaddr)
      to lookup config by base address.
    - Add Macro XXxvEthernet_IsMcDma(InstancePtr) to check Mcdma is
      connected or not.
    - Update driver and addtogroup doxygen tags to xxvethernet_v1_1.
    
    Signed-off-by: Maulik Jodhani <maulik.jodhani@xilinx.com>

--- XilinxProcessorIPLib/drivers/xxvethernet/src/xxxvethernet_sinit.c
+++ XilinxProcessorIPLib/drivers/xxvethernet/src/xxxvethernet_sinit.c
@@ -32,7 +32,7 @@
 /**
 *
 * @file xxxvethernet_sinit.c
-* @addtogroup xxvethernet_v1_0
+* @addtogroup xxvethernet_v1_1
 * @{
 *
 * This file contains static initialzation functionality for XXV Ethernet driver.
@@ -43,6 +43,8 @@
 * Ver   Who  Date     Changes
 * ----- ---- -------- -------------------------------------------------------
 * 1.0   hk   6/16/17  First release
+* 1.1   mj   3/30/18  Add XXxvEthernet_LookupConfigBaseAddr(UINTPTR Baseaddr)
+*                     API to lookup config by base address.
 * </pre>
 *
 ******************************************************************************/
@@ -87,6 +89,38 @@ XXxvEthernet_Config *XXxvEthernet_LookupConfig(u16 DeviceId)
 
 	for (Index = 0; Index < XPAR_XXXVETHERNET_NUM_INSTANCES; Index++) {
 		if (XXxvEthernet_ConfigTable[Index].DeviceId == DeviceId) {
+			CfgPtr = &XXxvEthernet_ConfigTable[Index];
+			break;
+		}
+	}
+
+	return (CfgPtr);
+}
+
+/*****************************************************************************/
+/**
+* XXxvEthernet_LookupConfigBaseAddr returns a reference to an
+* XXxvEthernet_Config structure based on base address. The return value
+* will refer to an entry in the device configuration table defined in the
+* xxxvethernet_g.c file.
+*
+* @param	 Baseaddr is the base address of the device to lookup for.
+*
+* @return
+*		- Returns a reference to a config record in the
+*		  configuration table (in xxxvethernet_g.c) corresponding to
+*		  <i>Baseaddr</i>, or NULL
+*		- NULL if no match is found.
+*
+******************************************************************************/
+XXxvEthernet_Config *XXxvEthernet_LookupConfigBaseAddr(UINTPTR Baseaddr)
+{
+	extern XXxvEthernet_Config XXxvEthernet_ConfigTable[];
+	XXxvEthernet_Config *CfgPtr = NULL;
+	int Index;
+
+	for (Index = 0; Index < XPAR_XXXVETHERNET_NUM_INSTANCES; Index++) {
+		if (XXxvEthernet_ConfigTable[Index].BaseAddress == Baseaddr) {
 			CfgPtr = &XXxvEthernet_ConfigTable[Index];
 			break;
 		}
