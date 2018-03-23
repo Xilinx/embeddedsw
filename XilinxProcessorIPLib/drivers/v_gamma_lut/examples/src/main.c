ad9b8a37b0e736bbfd0064ed9012f017e31d5ca9
Vanessa Chou <vanessa.chou@xilinx.com>
2018-03-24 05:26:27 +0530

    v_gamma_lut: update example design
    Example design tcl and application code updated to match
    changes in HW
    
    Signed-off-by: Vanessa Chou <vanessay@xilinx.com>

--- XilinxProcessorIPLib/drivers/v_gamma_lut/examples/src/main.c
+++ XilinxProcessorIPLib/drivers/v_gamma_lut/examples/src/main.c
@@ -1,6 +1,5 @@
-// d52cbaca0ef8cf4fd3d6354deb5066970fb6511d02d18d15835e6014ed847fb0
 #include "xparameters.h"
-#include "microblaze_sleep.h"
+#include "sleep.h"
 #include "xv_gamma_lut.h"
 #include "xv_tpg.h"
 #include "xvtc.h"
@@ -20,9 +19,9 @@ u32 volatile        *gpio_hlsIpReset;
 u32 volatile        *gpio_videoLockMonitor;
 
 #define VideoClockGen_WriteReg(RegOffset, Data) \
-    Xil_Out32((XPAR_VIDEO_CLK_BASEADDR) + (RegOffset), (u32)(Data))
+    Xil_Out32((XPAR_VIDEO_CLK_WIZ_BASEADDR) + (RegOffset), (u32)(Data))
 #define VideoClockGen_ReadReg(RegOffset) \
-    Xil_In32((XPAR_VIDEO_CLK_BASEADDR) + (RegOffset))
+    Xil_In32((XPAR_VIDEO_CLK_WIZ_BASEADDR) + (RegOffset))
 
 int driverInit()
 {
