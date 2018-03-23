352bcfda9a8ef8d5ec4c96df3d138a513430a4ae
Vanessa Chou <vanessa.chou@xilinx.com>
2018-03-24 05:26:27 +0530

    v_gamma_lut: update example design
    Example design tcl and application code updated to match
    changes in HW
    
    Signed-off-by: Vanessa Chou <vanessay@xilinx.com>
    
    Acked-for-series:  Kei Ito <keiito@xilinx.com>

--- XilinxProcessorIPLib/drivers/v_gamma_lut/examples/vgamma_lut_example.tcl
+++ XilinxProcessorIPLib/drivers/v_gamma_lut/examples/vgamma_lut_example.tcl
@@ -24,11 +24,11 @@ proc vgamma_lut_example args {
 
 		#create bsp
 		puts "Create BSP"
-		sdk createbsp -name vgamma_lut_example_bsp -hwproject vgamma_lut_example_hw_platform -proc microblaze_0 -os standalone
+		sdk createbsp -name vgamma_lut_example_bsp -hwproject vgamma_lut_example_hw_platform -proc processor_ss_processor -os standalone
 
 		#create application project
 		puts "Create Application Project"
-		sdk createapp -name vgamma_lut_example_design -hwproject vgamma_lut_example_hw_platform -proc microblaze_0 -os standalone -lang C -app {Empty Application} -bsp vgamma_lut_example_bsp
+		sdk createapp -name vgamma_lut_example_design -hwproject vgamma_lut_example_hw_platform -proc processor_ss_processor -os standalone -lang C -app {Empty Application} -bsp vgamma_lut_example_bsp
 
 		#copy example source files tp app project
 		puts "Get Example Design Source Files"
