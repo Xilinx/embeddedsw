3e0f3c716779fd4fbf4682b84595bb94649b0b5a
Swapna M <swapna.manupati@xilinx.com>
2018-03-28 11:24:39 +0530

    ipipsu: Add support for importing examples in SDK
    This patch provides support to import examples in SDK by
    adding index file in examples folder.
    
    Signed-off-by: Swapna Manupati <swapnam@xilinx.com>
    
    Acked-By: Srinivas Goud <sgoud@xilinx.com>
    
    Acked-By: Srinivas Goud <sgoud@xilinx.com>

--- XilinxProcessorIPLib/drivers/ipipsu/src/xipipsu.h
+++ XilinxProcessorIPLib/drivers/ipipsu/src/xipipsu.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2015 - 2016 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2015 - 2018 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -90,6 +90,8 @@
  *                    generation.
  * 2.3  ms  04/11/17  Modified tcl file to add suffix U for all macro
  *                    definitions of ipipsu in xparameters.h
+ *      ms  03/28/17  Add index.html to provide support for importing
+ *                    examples in SDK.
  * </pre>
  *
  *****************************************************************************/
