9768b80abd676f09013bb7fa5e33443ba9a8bf29
Rajnikant Bhojani <rajnikant.bhojani@xilinx.com>
2018-04-25 16:59:04 -0400

    zynqmp: usbpsu: freertos: audio example
    demonstrate usb audio functionality on freertos. zcu102 board
    will be detected as usb audio device on host
    
    Signed-off-by: Rajnikant Bhojani <rajnikant.bhojani@xilinx.com>
    Acked-for-series: Anurag Kumar Vulisha <anurag.kumar.vulisha@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/usbpsu/examples/xusb_freertos_ch9_audio.h
@@ -0,0 +1,513 @@
+/******************************************************************************
+ *
+ * Copyright (C) 2018 Xilinx, Inc.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Use of the Software is limited solely to applications:
+ * (a) running on a Xilinx device, or
+ * (b) that interact with a Xilinx device through a bus or interconnect.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Except as contained in this notice, the name of the Xilinx shall not be used
+ * in advertising or otherwise to promote the sale, use or other dealings in
+ * this Software without prior written authorization from Xilinx.
+ *
+ ******************************************************************************/
+/*****************************************************************************/
+/**
+ *
+ * @file xusb_freertos_ch9_audio.h
+ *
+ * This file contains definitions used in the chapter 9 code.
+ *
+ * <pre>
+ * MODIFICATION HISTORY:
+ *
+ * Ver   Who  Date     Changes
+ * ----- ---- -------- -------------------------------------------------------
+ * 1.0   rb   26/03/18 First release
+ *
+ * </pre>
+ *
+ ******************************************************************************/
+
+#ifndef XUSB_CH9_AUDIO_H
+#define XUSB_CH9_AUDIO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/***************************** Include Files *********************************/
+#include "xil_types.h"
+#include "xstatus.h"
+#include "xusb_ch9.h"
+#include "xparameters.h"	/* XPAR parameters */
+
+/************************** Constant Definitions *****************************/
+/* UAC2 works fine with Linux but not with Windows */
+/*
+ * UAC2 native driver support has been added in Windows 10 Creator's Update,
+ * however, it does not work.
+ */
+
+/* USB types, the second of three bRequestType fields */
+#define USB_TYPE_CLASS			(0x01 << 5)
+
+/*
+ * Conventional codes for class-specific descriptors.  The convention is
+ * defined in the USB "Common Class" Spec (3.11).  Individual class specs
+ * are authoritative for their usage, not the "common class" writeup.
+ */
+#define USB_DT_CS_INTERFACE		(USB_TYPE_CLASS | USB_TYPE_INTERFACE_DESC)
+#define USB_DT_CS_ENDPOINT		(USB_TYPE_CLASS | USB_TYPE_ENDPOINT_CFG_DESC)
+
+/* Audio Interface Subclass Codes */
+#define USB_SUBCLASS_AUDIOCONTROL	0x01
+#define USB_SUBCLASS_AUDIOSTREAMING	0x02
+
+/* Audio Class-Specific AC Interface Descriptor Subtypes */
+#define UAC_INPUT_TERMINAL		0x02
+#define UAC_OUTPUT_TERMINAL		0x03
+#define UAC_FEATURE_UNIT		0x06
+
+/* Format Type Codes */
+#define UAC_FORMAT_TYPE_I		0x1
+#define UAC_FORMAT_TYPE_SUBTYPE		0x02
+
+/* Audio Class-Specific AS Interface Descriptor Subtypes */
+#define UAC_AS_GENERAL			0x01
+
+/* Audio Class-Specific Endpoint Descriptor Subtypes */
+#define UAC_EP_GENERAL			0x01
+
+/* bInterfaceProtocol values to denote the version of the standard used */
+#define UAC_VERSION			0x20
+
+/* Terminal Unit */
+#define USB_OUT_IT_ID			1
+#define USB_OUT_OT_ID			2
+#define USB_IN_IT_ID			3
+#define USB_IN_OT_ID			4
+
+/* Clock Control Unit */
+#define NUM_CLK_SRC			1
+#define USB_CLK_SRC_ID			5
+#define USB_CLK_SEL_ID			6
+
+/* Feature Unit */
+#define OUT_FETR_UNT_ID			7
+#define IN_FETR_UNT_ID			8
+
+#define CONTROL_RDONLY			1
+#define CONTROL_RDWR			3
+
+#define CLK_FREQ_CTRL			0
+#define COPY_CTRL			0
+
+/* Audio Function Category Codes */
+#define UAC2_FUNCTION_SUBCLASS_UNDEFINED	0x00
+#define UAC2_FUNCTION_IO_BOX			0x08
+
+/* Audio Class-Specific Clock Source Descriptor Subtype */
+#define UAC2_CLOCK_SOURCE		0x0a
+
+/* Audio Class-Specific Clock Selector Descriptor Subtype */
+#define UAC2_CLOCK_SELECTOR		0x0b
+
+/* bmAttribute fields */
+#define UAC2_CLOCK_SOURCE_TYPE_INT_FIXED	0x1
+
+/* Audio Data Format Type I Codes */
+#define UAC2_FORMAT_TYPE_I_PCM		0x01
+
+/* The USB 3.0 spec redefines bits 5:4 of bmAttributes as interrupt ep type */
+#define USB_ENDPOINT_SYNC_ASYNC		(1 << 2)
+
+/*
+ * Number of channel in AUDIO
+ * Stereo->2 / Mono->1
+ */
+#define AUDIO_CHANNEL_NUM		0x02
+
+/**************************** Type Definitions *******************************/
+
+/* USB_DT_INTERFACE_ASSOCIATION: groups interfaces */
+#ifdef __ICCARM__
+#pragma pack(push, 1)
+#endif
+typedef struct {
+	u8  bLength;
+	u8  bDescriptorType;
+
+	u8  bFirstInterface;
+	u8  bInterfaceCount;
+	u8  bFunctionClass;
+	u8  bFunctionSubClass;
+	u8  bFunctionProtocol;
+	u8  iFunction;
+#ifdef __ICCARM__
+} USB_IF_ASSOC_DESC;
+#pragma pack(pop)
+#else
+} __attribute__((__packed__))USB_IF_ASSOC_DESC;
+#endif
+
+/* UAC2 class specific audio interface header descriptor */
+#ifdef __ICCARM__
+#pragma pack(push, 1)
+#endif
+typedef struct {
+	u8 bLength;
+	u8 bDescriptorType;
+	u8 bDescriptorSubtype;
+	u8 bcdADCL;
+	u8 bcdADCH;
+	u8 bCategory;
+	u16 wTotalLength;
+	u8 bmControls;
+#ifdef __ICCARM__
+} UAC2_AC_HEADER_DESC;
+#pragma pack(pop)
+#else
+} __attribute__((__packed__))UAC2_AC_HEADER_DESC;
+#endif
+
+#ifdef __ICCARM__
+#pragma pack(push, 1)
+#endif
+typedef struct {
+	u8 bLength;
+	u8 bDescriptorType;
+	u8 bDescriptorSubtype;
+	u8 bClockId;
+	u8 bmAttributes;
+	u8 bmControls;
+	u8 bAssocTerminal;
+	u8 iClockSource;
+#ifdef __ICCARM__
+} UAC2_CLOCK_SOURCE_DESC;
+#pragma pack(pop)
+#else
+} __attribute__((__packed__))UAC2_CLOCK_SOURCE_DESC;
+#endif
+
+/* UAC2 class specific audio Input terminal descriptor */
+#ifdef __ICCARM__
+#pragma pack(push, 1)
+#endif
+typedef struct {
+	u8 bLength;
+	u8 bDescriptorType;
+	u8 bDescriptorSubtype;
+	u8 bTerminalId;
+	u8 wTerminalTypeL;
+	u8 wTerminalTypeH;
+	u8 bAssocTerminal;
+	u8 bCSourceId;
+	u8 bNrChannels;
+	u8 wChannelConfigL1;
+	u8 wChannelConfigL2;
+	u8 wChannelConfigL3;
+	u8 wChannelConfigL4;
+	u8 iChannelNames;
+	u16 bmControls;;
+	u8 iTerminal;
+#ifdef __ICCARM__
+} UAC2_INPUT_TERMINAL_DESC;
+#pragma pack(pop)
+#else
+} __attribute__((__packed__))UAC2_INPUT_TERMINAL_DESC;
+#endif
+
+/* UAC2 class specific audio Output terminal descriptor */
+#ifdef __ICCARM__
+#pragma pack(push, 1)
+#endif
+typedef struct {
+	u8 bLength;
+	u8 bDescriptorType;
+	u8 bDescriptorSubtype;
+	u8 bTerminalId;
+	u8 wTerminalTypeL;
+	u8 wTerminalTypeH;
+	u8 bAssocTerminal;
+	u8 bSourceId;
+	u8 bCSourceId;
+	u16 bmControls;
+	u8 iTerminal;
+#ifdef __ICCARM__
+} UAC2_OUTPUT_TERMINAL_DESC;
+#pragma pack(pop)
+#else
+} __attribute__((__packed__))UAC2_OUTPUT_TERMINAL_DESC;
+#endif
+
+/* UAC2 class specific audio feature unit descriptor */
+#ifdef __ICCARM__
+#pragma pack(push, 1)
+#endif
+typedef struct {
+	u8 bLength;
+	u8 bDescriptorType;
+	u8 bDescriptorSubtype;
+	u8 bUnitID;
+	u8 bSourceID;
+	u32 bmaControls[AUDIO_CHANNEL_NUM + 1];
+	u8 iFeature;
+#ifdef __ICCARM__
+} UAC2_FEATURE_UNIT_DESC;
+#pragma pack(pop)
+#else
+} __attribute__((__packed__))UAC2_FEATURE_UNIT_DESC;
+#endif
+
+/* UAC2 Class-Specific AS Interface Descriptor */
+#ifdef __ICCARM__
+#pragma pack(push, 1)
+#endif
+typedef struct {
+	u8 bLength;
+	u8 bDescriptorType;
+	u8 bDescriptorSubtype;
+	u8 bTerminalLink;
+	u8 bmControls;
+	u8 bFormatType;
+	u8 bmFormatsL1;
+	u8 bmFormatsL2;
+	u8 bmFormatsL3;
+	u8 bmFormatsL4;
+	u8 bNrChannels;
+	u8 bmChannelConfigL0;
+	u8 bmChannelConfigL1;
+	u8 bmChannelConfigL2;
+	u8 bmChannelConfigL3;
+	u8 iChannelNames;
+#ifdef __ICCARM__
+} UAC2_AS_HEADER_DESC;
+#pragma pack(pop)
+#else
+} __attribute__((__packed__))UAC2_AS_HEADER_DESC;
+#endif
+
+/* Type I Format Type Descriptor (Frmts20 final.pdf) */
+#ifdef __ICCARM__
+#pragma pack(push, 1)
+#endif
+typedef struct {
+	u8 bLength;
+	u8 bDescriptorType;
+	u8 bDescriptorSubtype;
+	u8 bFormatType;
+	u8 bSubSlotSize;
+	u8 bBitResolution;
+#ifdef __ICCARM__
+} UAC2_FORMAT_TYPE_I_DESC;
+#pragma pack(pop)
+#else
+} __attribute__((__packed__))UAC2_FORMAT_TYPE_I_DESC;
+#endif
+
+/* USB Standard Audio Endpoint descriptor */
+#ifdef __ICCARM__
+#pragma pack(push, 1)
+#endif
+typedef struct {
+	u8 bLength;
+	u8 bDescriptorType;
+	u8 bEndpointAddress;
+	u8 bmAttributes;
+	u8 bMaxPacketSizeL;
+	u8 bMaxPacketSizeH;
+	u8 bInterval;
+#ifdef __ICCARM__
+} USB_EP_DESC;
+#pragma pack(pop)
+#else
+} __attribute__((__packed__))USB_EP_DESC;
+#endif
+
+/* UAC2 Class Specific Audio Data Endpoint descriptor */
+#ifdef __ICCARM__
+#pragma pack(push, 1)
+#endif
+typedef struct {
+	u8 bLength;
+	u8 bDescriptorType;
+	u8 bDescriptorSubtype;
+	u8 bmAttributes;
+	u8 bmControls;
+	u8 bLockDelayUnits;
+	u8 wLockDelayL;
+	u8 wLockDelayH;
+#ifdef __ICCARM__
+} UAC2_ISO_EP_DESC;
+#pragma pack(pop)
+#else
+} __attribute__((__packed__))UAC2_ISO_EP_DESC;
+#endif
+
+#ifdef __ICCARM__
+#pragma pack(push, 1)
+#endif
+typedef struct {
+	u8 bLength;
+	u8 bDescriptorType;
+	u8 bDescriptorSubtype;
+	u8 bClockId;
+	u8 bNrPins;
+	u8 baCSourceID[NUM_CLK_SRC];
+	u8 bmControl;
+	u8 iClockSelector;
+#ifdef __ICCARM__
+} UAC2_CLOCK_SELECTOR_DESC;
+#pragma pack(pop)
+#else
+} __attribute__((__packed__))UAC2_CLOCK_SELECTOR_DESC;
+#endif
+
+#ifdef __ICCARM__
+#pragma pack(push, 1)
+#endif
+typedef struct {
+	USB_STD_CFG_DESC 		stdCfg;
+	USB_IF_ASSOC_DESC		iadDesc;
+	USB_STD_IF_DESC 		stdAcIfdesc;
+	UAC2_AC_HEADER_DESC		acHdrDesc;
+
+	UAC2_CLOCK_SOURCE_DESC		ClkSrcDesc;
+	UAC2_CLOCK_SELECTOR_DESC	ClkSltrDesc;
+
+	/* Out Direction */
+	UAC2_INPUT_TERMINAL_DESC	usbOutItDesc;
+	UAC2_FEATURE_UNIT_DESC		OutfeatureUnitDesc;
+	UAC2_OUTPUT_TERMINAL_DESC	usbOutOtDesc;
+
+	/* In Direction */
+	UAC2_INPUT_TERMINAL_DESC	usbInItDesc;
+	UAC2_FEATURE_UNIT_DESC		InfeatureUnitDesc;
+	UAC2_OUTPUT_TERMINAL_DESC	usbInOtDesc;
+
+	/* Output Stream */
+	USB_STD_IF_DESC			stdAsOutIf0Desc;
+	USB_STD_IF_DESC			stdAsOutIf1Desc;
+	UAC2_AS_HEADER_DESC 		asOutHdrDesc;
+	UAC2_FORMAT_TYPE_I_DESC		asOutFmt1Desc;
+	USB_EP_DESC			asEpOutDesc;
+	UAC2_ISO_EP_DESC		asIsoEpOutDesc;
+
+	/* Input Stream */
+	USB_STD_IF_DESC			stdAsInIf0Desc;
+	USB_STD_IF_DESC			stdAsInIf1Desc;
+	UAC2_AS_HEADER_DESC 		asInHdrDesc;
+	UAC2_FORMAT_TYPE_I_DESC		asInFmt1Desc;
+	USB_EP_DESC			asEpInDesc;
+	UAC2_ISO_EP_DESC		asIsoEpInDesc;
+#ifdef __ICCARM__
+} USB_CONFIG;
+#pragma pack(pop)
+#else
+} __attribute__((__packed__))USB_CONFIG;
+#endif
+
+#ifdef __ICCARM__
+#pragma pack(push, 1)
+#endif
+typedef struct {
+	USB_STD_CFG_DESC		stdCfg;
+	USB_IF_ASSOC_DESC 		iadDesc;
+	USB_STD_IF_DESC 		stdAcIfdesc;
+	UAC2_AC_HEADER_DESC		acHdrDesc;
+
+	UAC2_CLOCK_SOURCE_DESC		ClkSrcDesc;
+	UAC2_CLOCK_SELECTOR_DESC	ClkSltrDesc;
+
+	/* Out Direction */
+	UAC2_INPUT_TERMINAL_DESC	usbOutItDesc;
+	UAC2_FEATURE_UNIT_DESC		OutfeatureUnitDesc;
+	UAC2_OUTPUT_TERMINAL_DESC	usbOutOtDesc;
+
+	/* In Direction */
+	UAC2_INPUT_TERMINAL_DESC	usbInItDesc;
+	UAC2_FEATURE_UNIT_DESC		InfeatureUnitDesc;
+	UAC2_OUTPUT_TERMINAL_DESC	usbInOtDesc;
+
+	/* Output Stream */
+	USB_STD_IF_DESC			stdAsOutIf0Desc;
+	USB_STD_IF_DESC			stdAsOutIf1Desc;
+	UAC2_AS_HEADER_DESC 		asOutHdrDesc;
+	UAC2_FORMAT_TYPE_I_DESC		asOutFmt1Desc;
+	USB_EP_DESC			asEpOutDesc;
+	USB_STD_EP_SS_COMP_DESC		asEpOutCompDesc;
+	UAC2_ISO_EP_DESC		asIsoEpOutDesc;
+
+	/* Input Stream */
+	USB_STD_IF_DESC			stdAsInIf0Desc;
+	USB_STD_IF_DESC			stdAsInIf1Desc;
+	UAC2_AS_HEADER_DESC 		asInHdrDesc;
+	UAC2_FORMAT_TYPE_I_DESC		asInFmt1Desc;
+	USB_EP_DESC			asEpInDesc;
+	USB_STD_EP_SS_COMP_DESC		asEpInCompDesc;
+	UAC2_ISO_EP_DESC		asIsoEpInDesc;
+#ifdef __ICCARM__
+} USB30_CONFIG;
+#pragma pack(pop)
+#else
+} __attribute__((__packed__))USB30_CONFIG;
+#endif
+
+/***************** Macros (Inline Functions) Definitions *********************/
+#define ISO_EP				1
+
+#define INTERVAL_PER_SECOND		(u32)(8000)
+
+/* Isoc interval for AUDIO */
+#define AUDIO_INTERVAL			0x04
+
+/* Array of supported sampling frequencies */
+#define MAX_AUDIO_FREQ			0x04
+
+/* Bytes per Audio transfered frame */
+#define AUDIO_FRAME_SIZE		0x02
+#define CUR_AUDIO_FREQ			0x01
+
+/* Bits per Audio transfered frame */
+#define BIT_RESOLUTION			((AUDIO_FRAME_SIZE) * (0x08))
+
+#define DIV_ROUND_UP(n, d)		(((n) + (d) - 1) / (d))
+/************************** Function Prototypes ******************************/
+
+u32 Usb_Ch9SetupDevDescReply(struct Usb_DevData *InstancePtr,
+		u8 *BufPtr, u32 BufLen);
+u32 Usb_Ch9SetupCfgDescReply(struct Usb_DevData *InstancePtr,
+		u8 *BufPtr, u32 BufLen);
+u32 Usb_Ch9SetupBosDescReply(u8 *BufPtr, u32 BufLen);
+u32 Usb_Ch9SetupStrDescReply(struct Usb_DevData *InstancePtr, u8 *BufPtr,
+		u32 BufLen, u8 Index);
+s32 Usb_SetConfiguration(struct Usb_DevData *InstancePtr, SetupPacket *Ctrl);
+s32 Usb_SetConfigurationApp(struct Usb_DevData *InstancePtr,
+		SetupPacket *Ctrl);
+void Usb_SetInterfaceHandler(struct Usb_DevData *InstancePtr,
+		SetupPacket *Ctrl);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* XUSB_CH9_AUDIO_H */
---
+++ XilinxProcessorIPLib/drivers/usbpsu/examples/xusb_freertos_class_audio.c
@@ -0,0 +1,430 @@
+/******************************************************************************
+*
+* Copyright (C) 2018 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* Use of the Software is limited solely to applications:
+* (a) running on a Xilinx device, or
+* (b) that interact with a Xilinx device through a bus or interconnect.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+/*****************************************************************************/
+/**
+*
+* @file xusb_freertos_class_audio.c
+*
+* This file contains the implementation of the audio specific class code
+* for the example.
+*
+*<pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who  Date     Changes
+* ----- ---- -------- -------------------------------------------------------
+* 1.0   rb   26/03/18 First release
+*
+*</pre>
+*
+******************************************************************************/
+
+/***************************** Include Files *********************************/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "xusb_freertos_ch9_audio.h"
+#include "xusb_freertos_class_audio.h"
+
+/************************** Constant Definitions *****************************/
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+/**************************** Type Definitions *******************************/
+
+/************************** Function Prototypes ******************************/
+
+/************************** Variable Definitions *****************************/
+
+/****************************************************************************/
+/**
+ * This function is called by Chapter9 handler when class request is received
+ * from Host.
+ *
+ * @param	InstancePtr is pointer to Usb_DevData instance.
+ * @param	SetupData is the setup packet received from Host.
+ *
+ * @note	None.
+ *
+ *****************************************************************************/
+void Usb_ClassReq(struct Usb_DevData *InstancePtr, SetupPacket *SetupData)
+{
+	u32 ReplyLen;
+	u8 Error = 0;
+	static u8 Reply[USB_REQ_REPLY_LEN] ALIGNMENT_CACHELINE;
+	u8 UnitId = SetupData->wIndex >> 8;
+
+	/* Check that the requested reply length is not bigger than our reply
+	 * buffer. This should never happen...
+	 */
+	if (SetupData->wLength > USB_REQ_REPLY_LEN)
+		return;
+
+	switch (SetupData->bRequest) {
+
+	case UAC2_CS_CUR:
+		switch (UnitId) {
+		case USB_CLK_SRC_ID:
+			switch (SetupData->wValue >> 8) {
+			case UAC2_CS_CONTROL_SAM_FREQ:
+				if ((SetupData->bRequestType &
+							USB_ENDPOINT_DIR_MASK) == 0) {
+					/* Set Request */
+					ReplyLen = SetupData->wLength;
+					EpBufferRecv(InstancePtr->PrivateData,
+							0, Reply, ReplyLen);
+
+					EpBufferSend(InstancePtr->PrivateData,
+							0, NULL, 0);
+				} else {
+					/* Get Request */
+					ReplyLen = SetupData->wLength > 4 ? 4 :
+						SetupData->wLength;
+
+					Reply[0] = (u8)0x44;
+					Reply[1] = (u8)0xAC;
+					Reply[2] = (u8)0x00;
+					Reply[3] = (u8)0x00;
+
+					EpBufferSend(InstancePtr->PrivateData,
+							0, Reply, ReplyLen);
+				}
+				break;
+
+			case UAC2_CS_CONTROL_CLOCK_VALID:
+				ReplyLen = SetupData->wLength > 4 ? 4 :
+					SetupData->wLength;
+				/* Internal clock always valid */
+				Reply[0] = (u8)0x01;
+
+				EpBufferSend(InstancePtr->PrivateData, 0,
+						Reply, ReplyLen);
+				break;
+
+			default:
+				/* Unknown Control Selector for Clock Unit */
+				Error = 1;
+				break;
+			}
+
+			break;
+
+		case USB_CLK_SEL_ID:
+			if ((SetupData->bRequestType &
+						USB_ENDPOINT_DIR_MASK) == 0) {
+				/* Set Request */
+				ReplyLen = SetupData->wLength;
+				EpBufferRecv(InstancePtr->PrivateData, 0,
+						Reply, ReplyLen);
+
+				EpBufferSend(InstancePtr->PrivateData, 0,
+						NULL, 0);
+			} else {
+				/* Get Request */
+				ReplyLen = SetupData->wLength > 4 ? 4 :
+					SetupData->wLength;
+				Reply[0] = (u8)0x01;
+
+				EpBufferSend(InstancePtr->PrivateData, 0,
+						Reply, ReplyLen);
+			}
+			break;
+
+		case OUT_FETR_UNT_ID:
+		case IN_FETR_UNT_ID:
+			switch (SetupData->wValue >> 8) {
+			case UAC2_FU_VOLUME_CONTROL:
+				/* Feature not available */
+				if ((SetupData->bRequestType &
+							USB_ENDPOINT_DIR_MASK) == 0) {
+					/* Set Request */
+					ReplyLen = SetupData->wLength;
+					EpBufferRecv(InstancePtr->PrivateData,
+							0, Reply, ReplyLen);
+
+					EpBufferSend(InstancePtr->PrivateData,
+							0, NULL, 0);
+				} else {
+					/* Get Request */
+					ReplyLen = SetupData->wLength > 4 ? 4 :
+						SetupData->wLength;
+					Reply[0] = 0x00;
+					Reply[1] = 0x00;
+
+					EpBufferSend(InstancePtr->PrivateData,
+							0, Reply, ReplyLen);
+				}
+				break;
+
+			case UAC2_FU_MUTE_CONTROL:
+				/* Feature not available */
+				if ((SetupData->bRequestType &
+							USB_ENDPOINT_DIR_MASK) == 0) {
+					/* Set Request */
+					ReplyLen = SetupData->wLength;
+					EpBufferRecv(InstancePtr->PrivateData,
+							0, Reply, ReplyLen);
+
+					EpBufferSend(InstancePtr->PrivateData,
+							0, NULL, 0);
+				} else {
+					/* Get Request */
+					ReplyLen = SetupData->wLength > 4 ? 4 :
+						SetupData->wLength;
+					Reply[0] = 0x01;
+
+					EpBufferSend(InstancePtr->PrivateData,
+							0, Reply, ReplyLen);
+				}
+				break;
+
+			default:
+				/* Unknown Control Selector for Feature Unit */
+				Error = 1;
+				break;
+			}
+
+			break;
+		default:
+			/* Unknown unit ID */
+			Error = 1;
+			break;
+		}
+		break;
+
+	case UAC2_CS_RANGE:
+		switch (UnitId) {
+		case USB_CLK_SRC_ID:
+			switch (SetupData->wValue >> 8) {
+			case UAC2_CS_CONTROL_SAM_FREQ:
+				ReplyLen = SetupData->wLength > 14 ? 14 :
+					SetupData->wLength;
+				Reply[0] = (u8)0x01;
+				Reply[1] = (u8)0x00;
+				Reply[2] = (u8)0x44;
+				Reply[3] = (u8)0xAC;
+				Reply[4] = (u8)0x00;
+				Reply[5] = (u8)0x00;
+				Reply[6] = (u8)0x44;
+				Reply[7] = (u8)0xAC;
+				Reply[8] = (u8)0x00;
+				Reply[9] = (u8)0x00;
+				Reply[10] = (u8)0x00;
+				Reply[11] = (u8)0x00;
+				Reply[12] = (u8)0x00;
+				Reply[13] = (u8)0x00;
+
+				EpBufferSend(InstancePtr->PrivateData, 0,
+						Reply, ReplyLen);
+				break;
+
+			default:
+				/* Unknown Clock Source Range Request */
+				Error = 1;
+				break;
+			}
+			break;
+
+		case OUT_FETR_UNT_ID:
+		case IN_FETR_UNT_ID:
+			switch (SetupData->wValue >> 8) {
+			case UAC2_FU_VOLUME_CONTROL:
+				/* Feature not available */
+				ReplyLen = SetupData->wLength > 14 ? 14 :
+					SetupData->wLength;
+				Reply[0] = (u8)0x01;
+				Reply[1] = (u8)0x00;
+				Reply[2] = (u8)0x00;
+				Reply[3] = (u8)0x81;
+				Reply[4] = (u8)0x00;
+				Reply[5] = (u8)0x00;
+				Reply[6] = (u8)0x00;
+				Reply[7] = (u8)0x01;
+
+				EpBufferSend(InstancePtr->PrivateData, 0,
+						Reply, ReplyLen);
+				break;
+
+			default:
+				/* Unknown Control Selector for Feature Unit */
+				Error = 1;
+				break;
+			}
+			break;
+
+		default:
+			/* Unknown unit ID */
+			Error = 1;
+			break;
+		}
+
+		break;
+	default:
+		Error = 1;
+		Ep0StallRestart(InstancePtr->PrivateData);
+		break;
+	}
+
+	/* Set the send stall bit if there is an error */
+	if (Error)
+		Ep0StallRestart(InstancePtr->PrivateData);
+}
+
+/****************************************************************************/
+/**
+* This task implements audio record functionality
+*
+* @param	pvParameters private parameters.
+*
+* @note		None.
+*
+*****************************************************************************/
+void prvRecordTask(void *pvParameters)
+{
+	u32 Size;
+	u16 MaxPktSize = 1024;
+	struct Usb_DevData *InstancePtr = pvParameters;
+	USBCH9_DATA *ch9_ptr =
+		(USBCH9_DATA *)Get_DrvData(InstancePtr->PrivateData);
+	struct audio_dev *dev = (struct audio_dev *)(ch9_ptr->data_ptr);
+
+	dev->index = 0;
+	dev->residue = 0;
+	dev->firstpkt = 1;
+
+	dev->xSemaphoreRecord = xSemaphoreCreateBinary();
+
+	SetEpInterval(InstancePtr->PrivateData, ISO_EP,
+			USB_EP_DIR_IN, AUDIO_INTERVAL);
+
+	/* Endpoint enables - not needed for Control EP */
+	EpEnable(InstancePtr->PrivateData, ISO_EP, USB_EP_DIR_IN,
+			MaxPktSize, USB_EP_TYPE_ISOCHRONOUS);
+
+	StreamOn(InstancePtr->PrivateData, ISO_EP, USB_EP_DIR_OUT,
+			BufferPtrTemp);
+
+	while (1) {
+
+		xSemaphoreTake(dev->xSemaphoreRecord, portMAX_DELAY);
+
+		Size = dev->packetsize;
+		dev->residue += dev->packetresidue;
+
+		if ((dev->residue / dev->interval) >= dev->framesize) {
+			Size += dev->framesize;
+			dev->residue -= dev->framesize * dev->interval;
+		}
+
+		/* Buffer is completed, retransmitting the same file data */
+		if ((dev->index + Size) > dev->disksize)
+			dev->index = 0;
+
+		if (EpBufferSend(InstancePtr->PrivateData, ISO_EP,
+					dev->virtualdisk + dev->index,
+					Size) == XST_SUCCESS) {
+			dev->index += Size;
+
+			if (dev->firstpkt) {
+				Size = dev->packetsize;
+				dev->residue += dev->packetresidue;
+
+				if ((dev->residue / dev->interval) >=
+						dev->framesize) {
+					Size += dev->framesize;
+					dev->residue -= dev->framesize *
+						dev->interval;
+				}
+
+				if ((dev->index + Size) > dev->disksize)
+					dev->index = 0;
+				else
+					dev->index += Size;
+
+				dev->firstpkt = 0;
+			}
+		} else
+			xSemaphoreGive(dev->xSemaphoreRecord);
+	}
+}
+
+/****************************************************************************/
+/**
+* This task implements audio playback functionality
+*
+* @param	pvParameters private parameters.
+*
+* @note		None.
+*
+*****************************************************************************/
+void prvPlayBackTask(void *pvParameters)
+{
+	u16 MaxPktSize = 1024;
+	struct Usb_DevData *InstancePtr = pvParameters;
+	USBCH9_DATA *ch9_ptr =
+		(USBCH9_DATA *)Get_DrvData(InstancePtr->PrivateData);
+	struct audio_dev *dev = (struct audio_dev *)(ch9_ptr->data_ptr);
+
+	dev->xSemaphorePlay = xSemaphoreCreateBinary();
+#if defined(PLATFORM_ZYNQ)
+	xSemaphoreGive(dev->xSemaphorePlay);
+#endif
+
+	dev->index = 0;
+	dev->residue = 0;
+	dev->firstpkt = 1;
+	dev->bytesRecv = 0;
+
+	SetEpInterval(InstancePtr->PrivateData, ISO_EP,
+			USB_EP_DIR_OUT, AUDIO_INTERVAL);
+
+	/* Endpoint enables - not needed for Control EP */
+	EpEnable(InstancePtr->PrivateData, ISO_EP,
+			USB_EP_DIR_OUT, MaxPktSize,
+			USB_EP_TYPE_ISOCHRONOUS);
+
+	StreamOn(InstancePtr->PrivateData, ISO_EP, USB_EP_DIR_IN,
+			BufferPtrTemp);
+
+	while (1) {
+
+		xSemaphoreTake(dev->xSemaphorePlay, portMAX_DELAY);
+		if ((dev->index + dev->bytesRecv) > dev->disksize)
+			dev->index = 0;
+
+		memcpy(dev->virtualdisk + dev->index, BufferPtrTemp,
+				dev->bytesRecv);
+		dev->index += dev->bytesRecv;
+
+		EpBufferRecv(InstancePtr->PrivateData, ISO_EP,
+				BufferPtrTemp, 1024);
+	}
+}
