114d7e0c49de6d869ebf5795527c895e4e188a27
Nava kishore Manne <nava.manne@xilinx.com>
2018-06-19 14:42:38 +0530

    xilfpga: Updated the examples to the new API's
    As part of the Xilfpga refactoring the top level API's
    exists in the library are  changed to Interface Agnostic API's.
    This Patch updates the example form Interface specific API's
    to Interface Agnostic API's
    
    Signed-off-by: Nava kishore Manne <navam@xilinx.com>
    
    Acked-for-series: Kedareswara rao Appana <appanad@xilinx.com>

--- lib/sw_services/xilfpga/examples/xfpga_reg_readback_example.c
+++ lib/sw_services/xilfpga/examples/xfpga_reg_readback_example.c
@@ -1,65 +1,63 @@
 /******************************************************************************
-*
-* Copyright (C) 2017 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* Use of the Software is limited solely to applications:
-* (a) running on a Xilinx device, or
-* (b) that interact with a Xilinx device through a bus or interconnect.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*
-* Except as contained in this notice, the name of the Xilinx shall not be used
-* in advertising or otherwise to promote the sale, use or other dealings in
-* this Software without prior written authorization from Xilinx.
-*
-******************************************************************************/
-
-#include "xil_printf.h"
-#include "xilfpga_pcap.h"
-#include "xfpga_config.h"
+ *
+ * Copyright (C) 2017 - 2018 Xilinx, Inc.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Use of the Software is limited solely to applications:
+ * (a) running on a Xilinx device, or
+ * (b) that interact with a Xilinx device through a bus or interconnect.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Except as contained in this notice, the name of the Xilinx shall not be used
+ * in advertising or otherwise to promote the sale, use or other dealings in
+ * this Software without prior written authorization from Xilinx.
+ *
+ *****************************************************************************/
 
 /**
-* @file  xfpga_reg_readback_example.c
-*
-*
-* This example prints out the values of all the configuration registers in the
-* FPGA.
-*
-* This example assumes that there is a UART Device or STDIO Device in the
-* hardware system.
-*
-* @note		None.
-*
-* MODIFICATION HISTORY:
-*
-*<pre>
-* Ver   Who  Date     Changes
-* ----- ---- -------- ---------------------------------------------
-* 2.1   Nava  05/10/17  First Release
-*</pre>
-******************************************************************************/
+ * @file  xfpga_reg_readback_example.c
+ *
+ *
+ * This example prints out the values of all the configuration registers in the
+ * FPGA.
+ *
+ * This example assumes that there is a UART Device or STDIO Device in the
+ * hardware system.
+ *
+ * @note		None.
+ *
+ * MODIFICATION HISTORY:
+ *
+ *<pre>
+ * Ver   Who  Date     Changes
+ * ----- ---- -------- ---------------------------------------------
+ * 2.1   Nava  05/10/17  First Release
+ * 4.2   Nava    30/05/18  Refactor the xilfpga library to support
+ *                         different PL programming Interfaces.
+ *</pre>
+ ******************************************************************************/
 
 /***************************** Include Files *********************************/
 
 #include "xil_printf.h"
-#include "xilfpga_pcap.h"
 #include "xfpga_config.h"
+#include "xilfpga.h"
 
 /************************** Constant Definitions *****************************/
 
@@ -97,27 +95,28 @@
 
 /************************** Function Prototypes ******************************/
 
-int Xfpga_RegReadExample();
+static int Xfpga_RegReadExample(void);
 /************************** Variable Definitions *****************************/
 
 
 /*****************************************************************************/
 /**
-*
-* Main function to call the Xfpga Reg Read example.
-*
-* @param	None.
-*
-* @return
-*		- XST_SUCCESS if successful
-*		- XST_FAILURE if unsuccessful
-*
-* @note		None.
-*
-******************************************************************************/
+ *
+ * Main function to call the Xfpga Reg Read example.
+ *
+ * @param	None.
+ *
+ * @return
+ *		- XST_SUCCESS if successful
+ *		- XST_FAILURE if unsuccessful
+ *
+ * @note		None.
+ *
+ *****************************************************************************/
 int main(void)
 {
 	int Status;
+
 	xil_printf("Register Read back example\r\n");
 
 	Xil_DCacheDisable();
@@ -140,135 +139,135 @@ int main(void)
 
 /*****************************************************************************/
 /**
-*
-* This function reads the configuration registers inside the FPGA.
-*
-* @param	DeviceId is the unique device id of the device.
-*
-* @return
-*		- XST_SUCCESS if successful
-*		- XST_FAILURE if unsuccessful
-*
-* @note		None.
-*
-******************************************************************************/
-int Xfpga_RegReadExample()
+ *
+ * This function reads the configuration registers inside the FPGA.
+ *
+ * @param	DeviceId is the unique device id of the device.
+ *
+ * @return
+ *		- XST_SUCCESS if successful
+ *		- XST_FAILURE if unsuccessful
+ *
+ * @note		None.
+ *
+ *****************************************************************************/
+static int Xfpga_RegReadExample(void)
 {
 	unsigned int ValueBack;
 
 
 	xil_printf("Value of the Configuration Registers. \r\n\n");
 
-	if (Xfpga_GetConfigReg(CRC, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(CRC, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" CRC -> \t %x \t\r\n", ValueBack);
 
 
-	if (Xfpga_GetConfigReg(FAR, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(FAR, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" FAR -> \t %x \t\r\n", ValueBack);
 
-	if (Xfpga_GetConfigReg(FDRI, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(FDRI, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" FDRI -> \t %x \t\r\n", ValueBack);
 
-	if (Xfpga_GetConfigReg(FDRO, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(FDRO, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" FDRO -> \t %x \t\r\n", ValueBack);
 
-	if (Xfpga_GetConfigReg(CMD, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(CMD, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" CMD -> \t %x \t\r\n", ValueBack);
 
-	if (Xfpga_GetConfigReg(CTL0, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(CTL0, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" CTL0 -> \t %x \t\r\n", ValueBack);
 
-	if (Xfpga_GetConfigReg(MASK, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(MASK, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" MASK -> \t %x \t\r\n", ValueBack);
 
-	if (Xfpga_GetConfigReg(STAT, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(STAT, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" STAT -> \t %x \t\r\n", ValueBack);
 
-	if (Xfpga_GetConfigReg(LOUT, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(LOUT, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" LOUT -> \t %x \t\r\n", ValueBack);
 
-	if (Xfpga_GetConfigReg(COR0, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(COR0, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" COR0 -> \t %x \t\r\n", ValueBack);
 
-	if (Xfpga_GetConfigReg(MFWR, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(MFWR, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" MFWR -> \t %x \t\r\n", ValueBack);
 
-	if (Xfpga_GetConfigReg(CBC, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(CBC, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" CBC -> \t %x \t\r\n", ValueBack);
 
-	if (Xfpga_GetConfigReg(IDCODE, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(IDCODE, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" IDCODE -> \t %x \t\r\n", ValueBack & IDCODE_MASK);
 
-	if (Xfpga_GetConfigReg(AXSS, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(AXSS, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" AXSS -> \t %x \t\r\n", ValueBack);
 
-	if (Xfpga_GetConfigReg(COR1, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(COR1, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" COR1 -> \t %x \t\r\n", ValueBack);
 
-	if (Xfpga_GetConfigReg(WBSTAR, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(WBSTAR, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" WBSTAR -> \t %x \t\r\n", ValueBack);
 
-	if (Xfpga_GetConfigReg(TIMER, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(TIMER, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" TIMER -> \t %x \t\r\n", ValueBack);
 
-	if (Xfpga_GetConfigReg(BOOTSTS, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(BOOTSTS, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" BOOTSTS -> \t %x \t\r\n", ValueBack);
 
-	if (Xfpga_GetConfigReg(CTL1, (u32 *)&ValueBack) !=
+	if (XFpga_GetPlConfigReg(CTL1, (u32 *)&ValueBack) !=
 		XST_SUCCESS) {
 		return XST_FAILURE;
 	}
