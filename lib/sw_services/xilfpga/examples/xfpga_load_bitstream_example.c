b2925aa09639a60bcca339f4cfeef7cc9d84312c
Nava kishore Manne <nava.manne@xilinx.com>
2018-06-19 14:42:38 +0530

    xilfpga: Updated the examples to the new API's
    As part of the Xilfpga refactoring the top level API's
    exists in the library are  changed to Interface Agnostic API's.
    This Patch updates the example form Interface specific API's
    to Interface Agnostic API's
    
    Signed-off-by: Nava kishore Manne <navam@xilinx.com>
    
    Acked-for-series: Kedareswara rao Appana <appanad@xilinx.com>

--- lib/sw_services/xilfpga/examples/xfpga_load_bitstream_example.c
+++ lib/sw_services/xilfpga/examples/xfpga_load_bitstream_example.c
@@ -1,59 +1,60 @@
 /******************************************************************************
-*
-* Copyright (C) 2016-2018 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* Use of the Software is limited solely to applications:
-* (a) running on a Xilinx device, or
-* (b) that interact with a Xilinx device through a bus or interconnect.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*
-* Except as contained in this notice, the name of the Xilinx shall not be used
-* in advertising or otherwise to promote the sale, use or other dealings in
-* this Software without prior written authorization from Xilinx.
-*
-******************************************************************************/
+ *
+ * Copyright (C) 2016-2018 Xilinx, Inc.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Use of the Software is limited solely to applications:
+ * (a) running on a Xilinx device, or
+ * (b) that interact with a Xilinx device through a bus or interconnect.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Except as contained in this notice, the name of the Xilinx shall not be used
+ * in advertising or otherwise to promote the sale, use or other dealings in
+ * this Software without prior written authorization from Xilinx.
+ *
+ ******************************************************************************/
 /*****************************************************************************/
 /**
-*
-* @file xfpga_load_bitstream_example.c
-*
-* This file contains the example using Xilfpga library to transfer the user provided Bit-stream
-* into zynqmp pl region.
-*
-* <pre>
-* MODIFICATION HISTORY:
-*
-* Ver   Who     Date     Changes
-* ----- ------  -------- ------------------------------------------------------
-* 1.0   Nava    08/06/16  First release
-* 4.0   Nava	21/02/18  Updated the example relevant to src code changes.
-*
-* </pre>
-*
-******************************************************************************/
+ *
+ * @file xfpga_load_bitstream_example.c
+ *
+ * This file contains the example using Xilfpga library to transfer the user
+ * provided Bit-stream into zynqmp pl region.
+ *
+ * <pre>
+ * MODIFICATION HISTORY:
+ *
+ * Ver   Who     Date     Changes
+ * ----- ------  -------- ------------------------------------------------------
+ * 1.0   Nava    08/06/16  First release
+ * 4.0   Nava	21/02/18  Updated the example relevant to src code changes.
+ * 4.2   Nava    30/05/18  Refactor the xilfpga library to support
+ *                         different PL programming Interfaces.
+ * </pre>
+ *
+ ******************************************************************************/
 
 #include "xil_printf.h"
-#include "xilfpga_pcap.h"
+#include "xilfpga.h"
 #include "xfpga_config.h"
 
-int main()
+int main(void)
 {
 	u64 addr = XFPGA_BASE_ADDRESS;
 	s32 Status;
@@ -62,9 +63,9 @@ int main()
 				XFPGA_BASE_ADDRESS);
 	xil_printf("Trying to configure the PL ......\n\r");
 
-	Status = XFpga_PL_BitSream_Load(addr, 0, 0);
+	Status = XFpga_PL_BitStream_Load(addr, 0, 0);
 
-	if(Status == XFPGA_SUCCESS)
+	if (Status == XFPGA_SUCCESS)
 		xil_printf("PL Configuration done successfully");
 	else
 		xil_printf("PL configuration failed\n\r");
