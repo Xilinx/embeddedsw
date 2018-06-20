4fd680972d29aeeb783f0be883d66a75401dcf33
Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>
2018-06-20 13:10:15 +0530

    nandps: Update copyright year and addtogroup
    This patch updates copyright year and addtogroup with the
    latest driver version.
    
    Signed-off-by: Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>
    Acked-for-series: Srinivas Goud <sgoud@xilinx.com>

--- XilinxProcessorIPLib/drivers/nandps/src/xnandps_bbm.h
+++ XilinxProcessorIPLib/drivers/nandps/src/xnandps_bbm.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2009 - 2015 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2009 - 2018 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@
 /**
 *
 * @file xnandps_bbm.h
-* @addtogroup nandps_v2_3
+* @addtogroup nandps_v2_4
 * @{
 *
 * This file implements the Bad Block Management(BBM) functionality. This is
