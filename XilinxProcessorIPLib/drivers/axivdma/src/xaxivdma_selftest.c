15e7c3fc513e5715333c1feb31fe8316ee369c15
Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
2018-07-03 12:24:59 +0530

    axivdma: Updated copyright year and addtogroup version
    Updated copyright year and addtogroup(axivdma_v6_6) version.
    
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    
    Acked-for-series: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

--- XilinxProcessorIPLib/drivers/axivdma/src/xaxivdma_selftest.c
+++ XilinxProcessorIPLib/drivers/axivdma/src/xaxivdma_selftest.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2015-2017 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2015-2018 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -33,7 +33,7 @@
 /**
 *
 * @file xaxivdma_selftest.c
-* @addtogroup axivdma_v6_5
+* @addtogroup axivdma_v6_6
 * @{
 *
 * Contains diagnostic/self-test functions for the XAxiVdma component.
