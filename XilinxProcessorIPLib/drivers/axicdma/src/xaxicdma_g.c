b9c05a2638c40e8d0aa4df1c34b1025f3532108f
Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
2018-07-04 01:12:47 -0600

    axicdma: Include missing initializers for 'XAxiCdma_Config' fields
    Include missing initializers for 'XAxiCdma_Config' fields.
    It's only for readability as xaxicdma_g.c in SDK flow is
    auto generated based on HW design.
    
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>

--- XilinxProcessorIPLib/drivers/axicdma/src/xaxicdma_g.c
+++ XilinxProcessorIPLib/drivers/axicdma/src/xaxicdma_g.c
@@ -42,6 +42,7 @@
 * Ver   Who  Date     Changes
 * ----- ---- -------- -------------------------------------------------------
 * 1.00a jz   08/16/10 First release
+* 4.5   rsp  07/04/18 Sync XAxiCdma_Config initializer fields
 * </pre>
 *
 ******************************************************************************/
@@ -59,7 +60,9 @@ XAxiCdma_Config XAxiCdma_ConfigTable[] =
 		XPAR_AXICDMA_0_BASEADDR,
 		XPAR_AXICDMA_0_INCLUDE_DRE,
 		XPAR_AXICDMA_0_USE_DATAMOVER_LITE,
-		XPAR_AXICDMA_0_M_AXI_DATA_WIDTH
+		XPAR_AXICDMA_0_M_AXI_DATA_WIDTH,
+		XPAR_AXICDMA_0_M_AXI_MAX_BURST_LEN,
+		XPAR_AXICDMA_0_ADDR_WIDTH
 	}
 };
 /** @} */
