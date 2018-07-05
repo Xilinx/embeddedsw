6a6b6599eb93beb8652828215837a115d0e2cc07
Shireesha Kothakonda <shireesh@xilinx.com>
2018-07-05 02:50:17 -0400

    Updated the Copyright information
    Signed-off-by: Shireesha Kothakonda <shireesh@xilinx.com>
    Acked-by: Siva Addepalli<saddepal@xilinx.com>

--- XilinxProcessorIPLib/drivers/v_hdmitxss/examples/xhdmi_example/dp159.c
+++ XilinxProcessorIPLib/drivers/v_hdmitxss/examples/xhdmi_example/dp159.c
@@ -1,20 +1,30 @@
-/*
-	DP159
-
-	Written by Marco Groeneveld
-	Copyright (c) 2014 -2016 Xilinx, Inc. All rights reserved.
-
-	History
-	-------
-	v1.0 - Initial release
-	v1.1 - Updated DP159 setting to automatic redriver to retimer
-	       for HDMI 1.4 data rates
-	v1.2 - Added Reset to I2C controller before issuing new transaction
-	v1.3 - Changed printf usage to xil_printf
-	v1.4 - Update vswing setting to recommened values to pass compliance
-	v1.5 - Update the register setting sequence to write 0x0A the last
-           to set APPLY_RXTX_CHANGES
-*/
+/******************************************************************************
+*
+* Copyright (C) 2014 - 2018 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
 
 #include "dp159.h"
 #include "sleep.h"
