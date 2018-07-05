607280d623643148acb601ba722b011eda1ae9ef
Siva Addepalli <saddepal@xilinx.com>
2018-07-05 19:32:44 +0530

    Updated to have latest copyright information
    Signed-off-by: Siva Addepalli <saddepal@xilinx.com>
    Acked-by: Srikanth Vemula <svemula@xilinx.com>

--- XilinxProcessorIPLib/drivers/i2srx/src/xi2srx_chsts.h
+++ XilinxProcessorIPLib/drivers/i2srx/src/xi2srx_chsts.h
@@ -8,10 +8,7 @@
  * furnished to do so, subject to the following conditions:
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * Use of the Software is limited solely to applications: (a) running on a
- * Xilinx device, or (b) that interact with a Xilinx device through a bus or
- * interconnect.
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
  * XILINX BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
