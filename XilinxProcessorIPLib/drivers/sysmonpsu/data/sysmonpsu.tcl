06439bd03f4a1824436545f72a687ea9313e232e
Manish Narani <manish.narani@xilinx.com>
2018-07-06 18:59:29 +0530

    sysmonpsu: Add initializer macro for Input Clock MHz
    This patch adds initializer macro for Input Clock MHz for Input Clock
    frequency information.
    
    Signed-off-by: Manish Narani <manish.narani@xilinx.com>
    
    Acked-for-series: Srinivas Goud <sgoud@xilinx.com>
    
    Acked-for-series: Srinivas Goud <sgoud@xilinx.com>

--- XilinxProcessorIPLib/drivers/sysmonpsu/data/sysmonpsu.tcl
+++ XilinxProcessorIPLib/drivers/sysmonpsu/data/sysmonpsu.tcl
@@ -47,6 +47,7 @@ proc generate {drv_handle} {
 
     ::hsi::utils::define_zynq_canonical_xpars $drv_handle "xparameters.h" "XSysMonPsu" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR"
 
+    get_ams_clk_info_canonical $drv_handle "xparameters.h"
 }
 
 proc get_ams_clk_info {drv_handle file_name} {
@@ -59,6 +60,22 @@ proc get_ams_clk_info {drv_handle file_name} {
 			if {[lsearch -nocase $avail_param "CONFIG.PSU__CRL_APB__AMS_REF_CTRL__ACT_FREQMHZ"] >= 0} {
 				set nr_freq [get_property CONFIG.PSU__CRL_APB__AMS_REF_CTRL__ACT_FREQMHZ [get_cells -hier $periph]]
 				puts $file_handle "#define XPAR_PSU_AMS_REF_FREQMHZ $nr_freq"
+			}
+		}
+	}
+	close $file_handle
+}
+
+proc get_ams_clk_info_canonical {drv_handle file_name} {
+	set file_handle [::hsi::utils::open_include_file $file_name]
+	set periph_list [get_cells -hier]
+	foreach periph $periph_list {
+		set zynq_ultra_ps [get_property IP_NAME $periph]
+		if {[string match -nocase $zynq_ultra_ps "zynq_ultra_ps_e"] } {
+			set avail_param [list_property [get_cells -hier $periph]]
+			if {[lsearch -nocase $avail_param "CONFIG.PSU__CRL_APB__AMS_REF_CTRL__ACT_FREQMHZ"] >= 0} {
+				set nr_freq [get_property CONFIG.PSU__CRL_APB__AMS_REF_CTRL__ACT_FREQMHZ [get_cells -hier $periph]]
+				puts $file_handle "#define XPAR_XSYSMONPSU_0_REF_FREQMHZ $nr_freq"
 			}
 		}
 	}
