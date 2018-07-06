70498b12c1d4d1ca80aac742243f53d37d1316aa
Manish Narani <manish.narani@xilinx.com>
2018-07-06 18:59:29 +0530

    sysmonpsu: Add initializer macro for Input Clock MHz
    This patch adds initializer macro for Input Clock MHz for Input Clock
    frequency information.
    
    Signed-off-by: Manish Narani <manish.narani@xilinx.com>
    
    Acked-for-series: Srinivas Goud <sgoud@xilinx.com>
    
    Acked-for-series: Srinivas Goud <sgoud@xilinx.com>

--- XilinxProcessorIPLib/drivers/sysmonpsu/src/xsysmonpsu_g.c
+++ XilinxProcessorIPLib/drivers/sysmonpsu/src/xsysmonpsu_g.c
@@ -40,6 +40,7 @@
 * Ver   Who    Date	Changes
 * ----- -----  -------- -------------------------------------------------------
 * 1.0   kvn  04/21/15 First release.
+* 2.5   mn     07/06/18 Added Input Clock Frequency Information
 * </pre>
 *
 ******************************************************************************/
@@ -69,7 +70,8 @@
  */
 XSysMonPsu_Config XSysMonPsu_ConfigTable[XPAR_XSYSMONPSU_NUM_INSTANCES] = {
 	{
-		(u16)XPAR_XSYSMONPSU_0_DEVICE_ID,	/* Unique ID of device */
-		(u32)XPAR_XSYSMONPSU_0_BASEADDR	/* Base address of device */
+		(u16)XPAR_XSYSMONPSU_0_DEVICE_ID,/* Unique ID of device */
+		(u32)XPAR_XSYSMONPSU_0_BASEADDR, /* Base address of device */
+		XPAR_XSYSMONPSU_0_REF_FREQMHZ    /* Input Clock Frequency */
 	}
 };
