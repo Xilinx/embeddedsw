86e94c7597b620659cb46c9ff8300b88ed0c4587
Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
2018-07-11 20:43:40 +0530

    axipmon: Fix Doxygen reported warnings
    Fix a typo and a missed out param tag.
    Also fixes below warning
    Found unknown command`\praram'
    
    Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
    
    Acked-for-series: Srinivas Goud <sgoud@xilinx.com>

--- XilinxProcessorIPLib/drivers/axipmon/src/xaxipmon.c
+++ XilinxProcessorIPLib/drivers/axipmon/src/xaxipmon.c
@@ -116,6 +116,7 @@
 * 6.3	kvn  07/02/15	Modified code according to MISRA-C:2012 guidelines.
 * 6.4   sk   11/10/15 Used UINTPTR instead of u32 for Baseaddress CR# 867425.
 *                     Changed the prototype of XAxiPmon_CfgInitialize API.
+* 6.7   sd   17/11/18 Fix Doxygen reported warnings
 * </pre>
 *
 *****************************************************************************/
@@ -602,7 +603,7 @@ s32 XAxiPmon_SetMetrics(XAxiPmon *InstancePtr, u8 Slot, u8 Metrics,
 *		The valid values are 0 to 9.
 * @param	Metrics is a reference parameter from application where metrics
 *		of specified counter is filled.
-* @praram	Slot is a reference parameter in which slot Id of
+* @param	Slot is a reference parameter in which slot Id of
 *		specified counter is filled
 * @return	XST_SUCCESS if Success
 *		XST_FAILURE if Failure
@@ -1024,7 +1025,7 @@ s32 XAxiPmon_StopEventLog(XAxiPmon *InstancePtr)
 *   - All sampled metric counters
 *
 * @param    InstancePtr is a pointer to the XAxiPmon instance.
-*           SampleInterval is the sample interval for the sampled metric
+* @param    SampleInterval is the sample interval for the sampled metric
 *           counters
 *
 * @return   XST_SUCCESS
