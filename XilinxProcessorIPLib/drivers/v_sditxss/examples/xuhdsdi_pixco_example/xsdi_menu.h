3bd8c854ed14213828cc8496ffbe3b6e36d2983d
Siva Rajesh <siva.rajesh.jarugula@xilinx.com>
2018-07-12 20:51:21 +0530

    v_sditxss: Example design application for UHDSDI Tx subsystem with PIXCO module
    This application is used to test the UHDSDI TX subsystem example
    design which used the PIXCO RTL module for clocking.
    
    Signed-off-by: Siva Rajesh <siva.rajesh.jarugula@xilinx.com>
    Acked-by: Karthik Padala <karthikp@xilinx.com>
    
    Acked-by: Karthik Padala <karthikp@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/v_sditxss/examples/xuhdsdi_pixco_example/xsdi_menu.h
@@ -0,0 +1,100 @@
+/******************************************************************************
+ *
+ * Copyright (C) 2018 Xilinx, Inc.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * XILINX BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Except as contained in this notice, the name of the Xilinx shall not be used
+ * in advertising or otherwise to promote the sale, use or other dealings in
+ * this Software without prior written authorization from Xilinx.
+ *
+ *****************************************************************************/
+/*****************************************************************************/
+/**
+*
+* @file xsdi_menu.h
+*
+* This is the main header file for the Xilinx Menu implementation as used
+* in the SDI example design.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who  Date       Changes
+* ---- ---- ---------- --------------------------------------------------
+* 1.0	ssh	 07/05/2018 Initial version
+* </pre>
+*
+******************************************************************************/
+#ifndef XSDI_MENU_H_
+#define XSDI_MENU_H_  /**< Prevent circular inclusions
+                         *  by using protection macros */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/***************************** Include Files *********************************/
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+#include "xil_assert.h"
+#include "xil_types.h"
+#include "xil_printf.h"
+#include "xstatus.h"
+#include "xvidc.h"
+#include "xuartps.h"
+#include "xv_sdirxss.h"
+#include "xv_sditxss.h"
+
+/************************** Constant Definitions *****************************/
+
+
+/**************************** Type Definitions *******************************/
+/**
+* The SDI menu types.
+*/
+typedef enum
+{
+	XSDI_MAIN_MENU,
+	XSDI_SUBCORE_MENU,
+	XSDI_NUM_MENUS
+} XSdi_MenuType;
+
+/**
+* The SDI menu instance data.
+*/
+typedef struct
+{
+	XSdi_MenuType		CurrentMenu;	/* Current menu */
+	u32			UartBaseAddress;		/* Uart base address */
+	u8			Value;					/* Sub menu value */
+	u8			WaitForColorbar;
+} XSdi_Menu;
+
+/************************** Function Prototypes ******************************/
+void XSdi_MenuInitialize(XSdi_Menu *InstancePtr, u32 UartBaseAddress);
+void XSdi_MenuProcess(XSdi_Menu *InstancePtr);
+void XSdi_MenuReset(XSdi_Menu *InstancePtr);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* End of protection macro */
