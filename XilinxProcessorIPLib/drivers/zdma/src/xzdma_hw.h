f483d71ad07ff4c5b67bf26b52c06080694510b0
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2018-07-13 12:00:01 +0530

    zdma: Update driver version and copyright year
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
    
    Acked-for-series: Harini Katakam <harini.katakam@xilinx.com>

--- XilinxProcessorIPLib/drivers/zdma/src/xzdma_hw.h
+++ XilinxProcessorIPLib/drivers/zdma/src/xzdma_hw.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2014 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2014 - 2018 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@
 /**
 *
 * @file xzdma_hw.h
-* @addtogroup zdma_v1_5
+* @addtogroup zdma_v1_6
 * @{
 *
 * This header file contains identifiers and register-level driver functions (or
