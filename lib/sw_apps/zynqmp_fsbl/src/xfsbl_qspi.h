1f384a7298f5e7f0acda560666d63266ee3c7da2
b.vikram@xilinx.com <b.vikram@xilinx.com>
2018-07-20 17:23:39 +0530

    sw_apps :zynqmp_fsbl: Add support for Macronix 1.8v flash parts in FSBL
    This patch adds support for MX66U51235FZ4I and MX25U25635FMI
    flash parts in FSBL.
    
    Signed-off-by: Vikram Sreenivasa Batchali <bvikram@xilinx.com>
    Acked-by:  Tejas Prajapati Rameshchandra <tejaspra@xilinx.com>
    
    Acked-by:  Tejas Prajapati Rameshchandra <tejaspra@xilinx.com>

--- lib/sw_apps/zynqmp_fsbl/src/xfsbl_qspi.h
+++ lib/sw_apps/zynqmp_fsbl/src/xfsbl_qspi.h
@@ -175,6 +175,8 @@ extern "C" {
 #define MACRONIX_FLASH_SIZE_ID_512M		(0x1AU)
 #define MACRONIX_FLASH_SIZE_ID_1G		(0x1BU)
 #define MACRONIX_FLASH_1_8_V_SIZE_ID_1G  	(0x3BU)
+#define MACRONIX_FLASH_1_8_V_MX25_ID_256	(0x39U)
+#define MACRONIX_FLASH_1_8_V_MX66_ID_512	(0x3AU)
 
 /*
  * Size in bytes
