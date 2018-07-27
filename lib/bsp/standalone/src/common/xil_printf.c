2672dd7b5f422e8bec3a06fc2ae4d047dc9b4463
Arunima Mishra <anunima.mishra@xilinx.com>
2018-07-27 10:25:31 +0530

    bsp: Removed checkpatch errors and warnings in xil_printf.c
    Signed-off-by: Arunima Mishra <anunima.mishra@xilinx.com>
    
    Acked-for-series:
    Anirudha Sarangi <anirudh@xilinx.com>

--- lib/bsp/standalone/src/common/xil_printf.c
+++ lib/bsp/standalone/src/common/xil_printf.c
@@ -5,6 +5,21 @@
 /* For Embedded Systems Programming, 1991            */
 /*                                                   */
 /*---------------------------------------------------*/
+
+/*---------------------------------------------------*/
+/* It supports below formats for 32 bit              */
+/* and 64 platforms                                  */
+/*                                                   */
+/* %c, %s, %d, %ld, %lld, %u, %p                     */
+/* %i, %x, %lx, %llx                                 */
+/* It also supports padding                          */
+/* (number with a certain width)                     */
+/* e.g:("%05d",6); it will pad 4 zeros before 6      */
+/* output:00006                                      */
+/* e.g:("%5d",6);It will pad 4 spaces before 6       */
+/* output:    6                                      */
+/*---------------------------------------------------*/
+
 #include "xil_printf.h"
 #include "xil_types.h"
 #include "xil_assert.h"
@@ -12,18 +27,18 @@
 #include <string.h>
 #include <stdarg.h>
 
-static void padding( const s32 l_flag,const struct params_s *par);
+static void padding(const s32 l_flag, const struct params_s *par);
 static void outs(const charptr lp, struct params_s *par);
-static s32 getnum( charptr* linep);
+static s32 getnum(charptr *linep);
 
 typedef struct params_s {
-    s32 len;
-    s32 num1;
-    s32 num2;
-    char8 pad_character;
-    s32 do_padding;
-    s32 left_flag;
-    s32 unsigned_flag;
+	s32 len;
+	s32 num1;
+	s32 num2;
+	char8 pad_character;
+	s32 do_padding;
+	s32 left_flag;
+	s32 unsigned_flag;
 } params_t;
 
 
@@ -41,18 +56,18 @@ typedef struct params_s {
 /* This routine puts pad characters into the output  */
 /* buffer.                                           */
 /*                                                   */
-static void padding( const s32 l_flag, const struct params_s *par)
+static void padding(const s32 l_flag, const struct params_s *par)
 {
-    s32 i;
+	s32 i;
 
-    if ((par->do_padding != 0) && (l_flag != 0) && (par->len < par->num1)) {
-		i=(par->len);
-        for (; i<(par->num1); i++) {
+	if ((par->do_padding != 0) && (l_flag != 0) && (par->len < par->num1)) {
+		i = (par->len);
+		for (; i < (par->num1); i++) {
 #ifdef STDOUT_BASEADDRESS
-            outbyte( par->pad_character);
+			outbyte(par->pad_character);
 #endif
 		}
-    }
+	}
 }
 
 /*---------------------------------------------------*/
@@ -62,27 +77,28 @@ static void padding( const s32 l_flag, const struct params_s *par)
 /*                                                   */
 static void outs(const charptr lp, struct params_s *par)
 {
-    charptr LocalPtr;
+	charptr LocalPtr;
 	LocalPtr = lp;
-    /* pad on left if needed                         */
-	if(LocalPtr != NULL) {
-		par->len = (s32)strlen( LocalPtr);
-	}
-    padding( !(par->left_flag), par);
 
-    /* Move string to the buffer                     */
-    while (((*LocalPtr) != (char8)0) && ((par->num2) != 0)) {
+
+	/* pad on left if needed                  */
+	if (LocalPtr != NULL)
+		par->len = (s32)strlen(LocalPtr);
+	padding(!(par->left_flag), par);
+
+	/* Move string to the buffer                     */
+	while (((*LocalPtr) != (char8)0) && ((par->num2) != 0)) {
 		(par->num2)--;
 #ifdef STDOUT_BASEADDRESS
-        outbyte(*LocalPtr);
+		outbyte(*LocalPtr);
 #endif
 		LocalPtr += 1;
-}
+	}
 
-    /* Pad on right if needed                        */
-    /* CR 439175 - elided next stmt. Seemed bogus.   */
-    /* par->len = strlen( lp)                      */
-    padding( par->left_flag, par);
+	/* Pad on right if needed                     */
+	/* CR 439175 - elided next stmt. Seemed bogus */
+	/* par->len = strlen(lp)                      */
+	padding(par->left_flag, par);
 }
 
 /*---------------------------------------------------*/
@@ -91,135 +107,132 @@ static void outs(const charptr lp, struct params_s *par)
 /* as directed by the padding and positioning flags. */
 /*                                                   */
 
-static void outnum( const s32 n, const s32 base, struct params_s *par)
+static void outnum(const s32 n, const s32 base, struct params_s *par)
 {
-    s32 negative;
+	s32 negative;
 	s32 i;
-    char8 outbuf[32];
-    const char8 digits[] = "0123456789ABCDEF";
-    u32 num;
-    for(i = 0; i<32; i++) {
-	outbuf[i] = '0';
-    }
+	char8 outbuf[32];
+	const char8 digits[] = "0123456789ABCDEF";
+	u32 num;
 
-    /* Check if number is negative                   */
-    if ((par->unsigned_flag == 0) && (base == 10) && (n < 0L)) {
-        negative = 1;
-		num =(-(n));
-    }
-    else{
-        num = n;
-        negative = 0;
-    }
-    /* Build number (backwards) in outbuf            */
-    i = 0;
-    do {
+	for (i = 0; i < 32; i++)
+		outbuf[i] = '0';
+
+	/* Check if number is negative                   */
+	if ((par->unsigned_flag == 0) && (base == 10) && (n < 0L)) {
+		negative = 1;
+		num = (-(n));
+	} else {
+		num = n;
+		negative = 0;
+	}
+	/* Build number (backwards) in outbuf            */
+	i = 0;
+	do {
 		outbuf[i] = digits[(num % base)];
 		i++;
 		num /= base;
-    } while (num > 0);
+	} while (num > 0);
 
-    if (negative != 0) {
+	if (negative != 0) {
 		outbuf[i] = '-';
 		i++;
 	}
 
-    outbuf[i] = 0;
-    i--;
+	outbuf[i] = 0;
+	i--;
 
-    /* Move the converted number to the buffer and   */
-    /* add in the padding where needed.              */
-    par->len = (s32)strlen(outbuf);
-    padding( !(par->left_flag), par);
-    while (&outbuf[i] >= outbuf) {
+	/* Move the converted number to the buffer and   */
+	/* add in the padding where needed.              */
+	par->len = (s32)strlen(outbuf);
+	padding(!(par->left_flag), par);
+	while (&outbuf[i] >= outbuf) {
 #ifdef STDOUT_BASEADDRESS
-	outbyte( outbuf[i] );
+		outbyte(outbuf[i]);
 #endif
 		i--;
-}
-    padding( par->left_flag, par);
+	}
+	padding(par->left_flag, par);
 }
 /*---------------------------------------------------*/
 /*                                                   */
 /* This routine moves a 64-bit number to the output  */
 /* buffer as directed by the padding and positioning */
-/* flags. 											 */
+/* flags.                                            */
 /*                                                   */
 
-static void outnum1( const s64 n, const s32 base, params_t *par)
+static void outnum1(const s64 n, const s32 base, params_t *par)
 {
-    s32 negative;
+	s32 negative;
 	s32 i;
-    char8 outbuf[64];
+	char8 outbuf[64];
 	const char8 digits[] = "0123456789ABCDEF";
 	u64 num;
-	for(i = 0; i<64; i++) {
+
+	for (i = 0; i < 64; i++)
 		outbuf[i] = '0';
+
+	/* Check if number is negative                   */
+	if ((par->unsigned_flag == 0) && (base == 10) && (n < 0L)) {
+		negative = 1;
+		num = (-(n));
+	} else {
+		num = (n);
+		negative = 0;
 	}
 
-    /* Check if number is negative                   */
-    if ((par->unsigned_flag == 0) && (base == 10) && (n < 0L)) {
-        negative = 1;
-		num =(-(n));
-    }
-    else{
-        num = (n);
-        negative = 0;
-    }
-
-    /* Build number (backwards) in outbuf            */
-    i = 0;
-    do {
+	/* Build number (backwards) in outbuf            */
+	i = 0;
+	do {
 		outbuf[i] = digits[(num % base)];
 		i++;
 		num /= base;
-    } while (num > 0);
+	} while (num > 0);
 
-    if (negative != 0) {
+	if (negative != 0) {
 		outbuf[i] = '-';
 		i++;
 	}
 
-    outbuf[i] = 0;
-    i--;
+	outbuf[i] = 0;
+	i--;
 
-    /* Move the converted number to the buffer and   */
-    /* add in the padding where needed.              */
-    par->len = (s32)strlen(outbuf);
-    padding( !(par->left_flag), par);
-     while (&outbuf[i] >= outbuf) {
-	outbyte( outbuf[i] );
+	/* Move the converted number to the buffer and   */
+	/* add in the padding where needed.              */
+	par->len = (s32)strlen(outbuf);
+	padding(!(par->left_flag), par);
+	while (&outbuf[i] >= outbuf) {
+		outbyte(outbuf[i]);
 		i--;
-}
-    padding( par->left_flag, par);
+	}
+	padding(par->left_flag, par);
 }
 /*---------------------------------------------------*/
 /*                                                   */
 /* This routine gets a number from the format        */
 /* string.                                           */
 /*                                                   */
-static s32 getnum( charptr* linep)
+static s32 getnum(charptr *linep)
 {
-    s32 n;
-    s32 ResultIsDigit = 0;
-    charptr cptr;
-    n = 0;
-    cptr = *linep;
-	if(cptr != NULL){
+	s32 n;
+	s32 ResultIsDigit = 0;
+	charptr cptr;
+
+	n = 0;
+	cptr = *linep;
+	if (cptr != NULL)
 		ResultIsDigit = isdigit(((s32)*cptr));
-	}
-    while (ResultIsDigit != 0) {
-		if(cptr != NULL){
+	while (ResultIsDigit != 0) {
+		if (cptr != NULL) {
 			n = ((n*10) + (((s32)*cptr) - (s32)'0'));
 			cptr += 1;
-			if(cptr != NULL){
+			if (cptr != NULL)
 				ResultIsDigit = isdigit(((s32)*cptr));
-			}
 		}
 		ResultIsDigit = isdigit(((s32)*cptr));
 	}
-    *linep = ((charptr )(cptr));
-    return(n);
+	*linep = ((charptr)(cptr));
+	return n;
 }
 
 /*---------------------------------------------------*/
@@ -235,213 +248,201 @@ static s32 getnum( charptr* linep)
 /* the supported formats.                            */
 /*                                                   */
 
-/* void esp_printf( const func_ptr f_ptr,
-   const charptr ctrl1, ...) */
+/* void esp_printf(const func_ptr f_ptr,
+ *const charptr ctrl1, ...)
+ */
 #if HYP_GUEST && EL1_NONSECURE && XEN_USE_PV_CONSOLE
-void xil_printf( const char8 *ctrl1, ...){
+void xil_printf(const char8 *ctrl1, ...)
+{
 	XPVXenConsole_Printf(ctrl1);
 }
 #else
-void xil_printf( const char8 *ctrl1, ...)
+void xil_printf(const char8 *ctrl1, ...)
 {
 	s32 Check;
-    s32 dot_flag;
-    s32 long_flag;
-    params_t par;
+	s32 dot_flag;
+	s32 long_flag;
+	params_t par;
 
-    char8 ch;
-    va_list argp;
-    char8 *ctrl = (char8 *)ctrl1;
+	char8 ch;
+	va_list argp;
+	char8 *ctrl = (char8 *)ctrl1;
 
-    va_start( argp, ctrl1);
+	va_start(argp, ctrl1);
 
-    while ((ctrl != NULL) && (*ctrl != (char8)0)) {
-
-        /* move format string chars to buffer until a  */
-        /* format control is found.                    */
-        if (*ctrl != '%') {
+	while ((ctrl != NULL) && (*ctrl != (char8)0)) {
+	/* move format string chars to buffer until a  */
+	/* format control is found.                    */
+		if (*ctrl != '%') {
 #ifdef STDOUT_BASEADDRESS
-            outbyte(*ctrl);
+			outbyte(*ctrl);
 #endif
 			ctrl += 1;
-            continue;
-        }
-
-        /* initialize all the flags for this format.   */
-        dot_flag = 0;
-		long_flag = 0;
-        par.unsigned_flag = 0;
-		par.left_flag = 0;
-		par.do_padding = 0;
-        par.pad_character = ' ';
-        par.num2=32767;
-		par.num1=0;
-		par.len=0;
-
- try_next:
-		if(ctrl != NULL) {
-			ctrl += 1;
-		}
-		if(ctrl != NULL) {
-			ch = *ctrl;
-		}
-		else {
-			ch = *ctrl;
+			continue;
 		}
 
-        if (isdigit((s32)ch) != 0) {
-            if (dot_flag != 0) {
-                par.num2 = getnum(&ctrl);
-			}
-            else {
-                if (ch == '0') {
-                    par.pad_character = '0';
-				}
-				if(ctrl != NULL) {
-			par.num1 = getnum(&ctrl);
-				}
-                par.do_padding = 1;
-            }
-            if(ctrl != NULL) {
+	/* initialize all the flags for this format.   */
+	dot_flag = 0;
+	long_flag = 0;
+	par.unsigned_flag = 0;
+	par.left_flag = 0;
+	par.do_padding = 0;
+	par.pad_character = ' ';
+	par.num2 = 32767;
+	par.num1 = 0;
+	par.len = 0;
+
+try_next:
+	if (ctrl != NULL)
+		ctrl += 1;
+	if (ctrl != NULL)
+		ch = *ctrl;
+	else
+		ch = *ctrl;
+
+	if (isdigit((s32)ch) != 0) {
+		if (dot_flag != 0) {
+			par.num2 = getnum(&ctrl);
+		} else {
+			if (ch == '0')
+				par.pad_character = '0';
+			if (ctrl != NULL)
+				par.num1 = getnum(&ctrl);
+			par.do_padding = 1;
+		}
+		if (ctrl != NULL)
 			ctrl -= 1;
-			}
-            goto try_next;
-        }
+		goto try_next;
+	}
 
-        switch (tolower((s32)ch)) {
-            case '%':
+	switch (tolower((s32)ch)) {
+	case '%':
 #ifdef STDOUT_BASEADDRESS
-                outbyte( '%');
+		outbyte('%');
 #endif
-                Check = 1;
-                break;
-
-            case '-':
-                par.left_flag = 1;
-                Check = 0;
-                break;
-
-            case '.':
-                dot_flag = 1;
-                Check = 0;
-                break;
-
-            case 'l':
-            #if defined (__aarch64__)
-		long_flag=1;
-            #else
-		long_flag++;
-            #endif
-                Check = 0;
-                break;
-
-            case 'u':
-                par.unsigned_flag = 1;
-                /* fall through */
-            case 'i':
-            case 'd':
-                #if defined (__aarch64__)
-                if (long_flag != 0){
-			        outnum1((s64)va_arg(argp, s64), 10L, &par);
-                }
-                else {
-                    outnum( va_arg(argp, s32), 10L, &par);
-                }
-                #else
-                if(long_flag >=2)
-			outnum1((s64)va_arg(argp, s64), 10L, &par);
-                else
-                    outnum( va_arg(argp, s32), 10L, &par);
-                #endif
-				Check = 1;
-                break;
-            case 'p':
-		#if defined (__aarch64__)
-                par.unsigned_flag = 1;
-			    outnum1((s64)va_arg(argp, s64), 16L, &par);
-			    Check = 1;
-                break;
-                #else
-                long_flag=0;
-                #endif
-            case 'X':
-            case 'x':
-                par.unsigned_flag = 1;
-                #if defined (__aarch64__)
-                if (long_flag != 0) {
-			outnum1((s64)va_arg(argp, s64), 16L, &par);
-				}
-				else {
-				    outnum((s32)va_arg(argp, s32), 16L, &par);
-                }
-                #else
-                if (long_flag >= 2)
-			outnum1((s64)va_arg(argp, s64), 16L, &par);
-                else
-			outnum((s32)va_arg(argp, s32), 16L, &par);
-
-                #endif
-                Check = 1;
-                break;
-
-            case 's':
-                outs( va_arg( argp, char *), &par);
-                Check = 1;
-                break;
-
-            case 'c':
-#ifdef STDOUT_BASEADDRESS
-                outbyte( va_arg( argp, s32));
-#endif
-                Check = 1;
-                break;
-
-            case '\\':
-                switch (*ctrl) {
-                    case 'a':
-#ifdef STDOUT_BASEADDRESS
-                        outbyte( ((char8)0x07));
-#endif
-                        break;
-                    case 'h':
-#ifdef STDOUT_BASEADDRESS
-                        outbyte( ((char8)0x08));
-#endif
-                        break;
-                    case 'r':
-#ifdef STDOUT_BASEADDRESS
-                        outbyte( ((char8)0x0D));
-#endif
-                        break;
-                    case 'n':
-#ifdef STDOUT_BASEADDRESS
-                        outbyte( ((char8)0x0D));
-                        outbyte( ((char8)0x0A));
-#endif
-                        break;
-                    default:
-#ifdef STDOUT_BASEADDRESS
-                        outbyte( *ctrl);
-#endif
-                        break;
-                }
-                ctrl += 1;
-                Check = 0;
-                break;
-
-            default:
 		Check = 1;
 		break;
-        }
-        if(Check == 1) {
-			if(ctrl != NULL) {
+
+	case '-':
+		par.left_flag = 1;
+		Check = 0;
+		break;
+
+	case '.':
+		dot_flag = 1;
+		Check = 0;
+		break;
+
+	case 'l':
+#if defined(__aarch64__)
+		long_flag = 1;
+#else
+		long_flag++;
+#endif
+		Check = 0;
+		break;
+
+	case 'u':
+			par.unsigned_flag = 1;
+		/* fall through */
+	case 'i':
+	case 'd':
+#if defined(__aarch64__)
+		if (long_flag != 0)
+			outnum1((s64)va_arg(argp, s64), 10L, &par);
+		else
+			outnum(va_arg(argp, s32), 10L, &par);
+#else
+		if (long_flag >= 2)
+			outnum1((s64)va_arg(argp, s64), 10L, &par);
+		else
+			outnum(va_arg(argp, s32), 10L, &par);
+#endif
+		Check = 1;
+		break;
+	case 'p':
+#if defined(__aarch64__)
+		par.unsigned_flag = 1;
+		outnum1((s64)va_arg(argp, s64), 16L, &par);
+		Check = 1;
+		break;
+#else
+		long_flag = 0;
+#endif
+	case 'X':
+	case 'x':
+		par.unsigned_flag = 1;
+
+#if defined(__aarch64__)
+		if (long_flag != 0)
+			outnum1((s64)va_arg(argp, s64), 16L, &par);
+		else
+			outnum((s32)va_arg(argp, s32), 16L, &par);
+#else
+		if (long_flag >= 2)
+			outnum1((s64)va_arg(argp, s64), 16L, &par);
+		else
+			outnum((s32)va_arg(argp, s32), 16L, &par);
+#endif
+		Check = 1;
+		break;
+
+	case 's':
+		outs(va_arg(argp, char *), &par);
+		Check = 1;
+		break;
+
+	case 'c':
+#ifdef STDOUT_BASEADDRESS
+		outbyte(va_arg(argp, s32));
+#endif
+		Check = 1;
+		break;
+
+	case '\\':
+		switch (*ctrl) {
+		case 'a':
+#ifdef STDOUT_BASEADDRESS
+			outbyte(((char8)0x07));
+#endif
+			break;
+		case 'h':
+#ifdef STDOUT_BASEADDRESS
+			outbyte(((char8)0x08));
+#endif
+			break;
+		case 'r':
+#ifdef STDOUT_BASEADDRESS
+			outbyte(((char8)0x0D));
+#endif
+			break;
+		case 'n':
+#ifdef STDOUT_BASEADDRESS
+			outbyte(((char8)0x0D));
+			outbyte(((char8)0x0A));
+#endif
+			break;
+		default:
+#ifdef STDOUT_BASEADDRESS
+			outbyte(*ctrl);
+#endif
+			break;
+		}
+		ctrl += 1;
+		Check = 0;
+		break;
+	default:
+		Check = 1;
+		break;
+		}
+		if (Check == 1) {
+			if (ctrl != NULL)
 				ctrl += 1;
-			}
-                continue;
-        }
-        goto try_next;
-    }
-    va_end( argp);
+			continue;
+		}
+		goto try_next;
+	}
+	va_end(argp);
 }
 #endif
 /*---------------------------------------------------*/
