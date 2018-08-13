7e13a6e7fa4e08d7d3e852cbaa7d63326d4d71a1
b.vikram@xilinx.com <b.vikram@xilinx.com>
2018-08-13 14:35:52 +0530

    sw_apps :zynq_fsbl: Add support for ISSI 8Mb, 16Mb, 32Mb and 64Mb flash parts in Zynq FSBL
    The support is for IS25LP080D (8M/3.3V), IS25WP080D (8M/1.8V), IS25LP016D (16M/3.3V), IS25WP016D (16M/1.8V),
    IS25LP032D (32M/3.3V), IS25WP032D (32M/1.8V), IS25LP064A (64M/3.3V), and IS25WP064A (64M/1.8V).
    
    Signed-off-by: Vikram Sreenivasa Batchali <bvikram@xilinx.com>
    Acked-by: Tejas Prajapati Rameshchandra <tejaspra@xilinx.com>
    
    Acked-by: Tejas Prajapati Rameshchandra <tejaspra@xilinx.com>

--- lib/sw_apps/zynq_fsbl/src/qspi.h
+++ lib/sw_apps/zynq_fsbl/src/qspi.h
@@ -86,7 +86,12 @@ extern "C" {
 #define SPANSION_ID		0x01
 #define WINBOND_ID		0xEF
 #define MACRONIX_ID		0xC2
+#define ISSI_ID			0x9D
 
+#define FLASH_SIZE_ID_8M		0x14
+#define FLASH_SIZE_ID_16M		0x15
+#define FLASH_SIZE_ID_32M		0x16
+#define FLASH_SIZE_ID_64M		0x17
 #define FLASH_SIZE_ID_128M		0x18
 #define FLASH_SIZE_ID_256M		0x19
 #define FLASH_SIZE_ID_512M		0x20
@@ -98,6 +103,10 @@ extern "C" {
 /*
  * Size in bytes
  */
+#define FLASH_SIZE_8M			0x0100000
+#define FLASH_SIZE_16M			0x0200000
+#define FLASH_SIZE_32M			0x0400000
+#define FLASH_SIZE_64M			0x0800000
 #define FLASH_SIZE_128M			0x1000000
 #define FLASH_SIZE_256M			0x2000000
 #define FLASH_SIZE_512M			0x4000000
