cfd960e15411a2e13f63d34e8097ef48dc7c1f6b
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2018-08-13 14:49:01 +0530

    dmaps: Update driver version and copyright year
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
    
    Acked-for-series: Harini Katakam <harini.katakam@xilinx.com>

--- XilinxProcessorIPLib/drivers/dmaps/src/xdmaps.h
+++ XilinxProcessorIPLib/drivers/dmaps/src/xdmaps.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2009 - 2016 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2009 - 2018 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal 
@@ -29,7 +29,7 @@
 /**
 *
 * @file xdmaps.h
-* @addtogroup dmaps_v2_3
+* @addtogroup dmaps_v2_4
 * @{
 * @details
 *
--- XilinxProcessorIPLib/drivers/dmaps/src/xdmaps_selftest.c
+++ XilinxProcessorIPLib/drivers/dmaps/src/xdmaps_selftest.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2009 - 2015 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2009 - 2018 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal 
@@ -29,7 +29,7 @@
 /**
 *
 * @file xdmaps_selftest.c
-* @addtogroup dmaps_v2_3
+* @addtogroup dmaps_v2_4
 * @{
 *
 * This file contains the self-test functions for the XDmaPs driver.
