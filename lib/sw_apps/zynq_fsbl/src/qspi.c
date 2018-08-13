89bc0218cb65e8918c65c6c8d21fa5860c89f377
b.vikram@xilinx.com <b.vikram@xilinx.com>
2018-08-13 14:35:52 +0530

    sw_apps :zynq_fsbl: Add support for ISSI 8Mb, 16Mb, 32Mb and 64Mb flash parts in Zynq FSBL
    The support is for IS25LP080D (8M/3.3V), IS25WP080D (8M/1.8V), IS25LP016D (16M/3.3V), IS25WP016D (16M/1.8V),
    IS25LP032D (32M/3.3V), IS25WP032D (32M/1.8V), IS25LP064A (64M/3.3V), and IS25WP064A (64M/1.8V).
    
    Signed-off-by: Vikram Sreenivasa Batchali <bvikram@xilinx.com>
    Acked-by: Tejas Prajapati Rameshchandra <tejaspra@xilinx.com>
    
    Acked-by: Tejas Prajapati Rameshchandra <tejaspra@xilinx.com>

--- lib/sw_apps/zynq_fsbl/src/qspi.c
+++ lib/sw_apps/zynq_fsbl/src/qspi.c
@@ -495,12 +495,27 @@ u32 FlashReadID(void)
 	} else if(ReadBuffer[1] == MACRONIX_ID) {
 		QspiFlashMake = MACRONIX_ID;
 		fsbl_printf(DEBUG_INFO, "MACRONIX ");
+	} else if(ReadBuffer[0] == ISSI_ID) {
+		QspiFlashMake = ISSI_ID;
+		fsbl_printf(DEBUG_INFO, "ISSI ");
 	}
 
 	/*
 	 * Deduce flash Size
 	 */
-	if (ReadBuffer[3] == FLASH_SIZE_ID_128M) {
+	if (ReadBuffer[2] == FLASH_SIZE_ID_8M) {
+		QspiFlashSize = FLASH_SIZE_8M;
+		fsbl_printf(DEBUG_INFO, "8M Bits\r\n");
+	} else if (ReadBuffer[2] == FLASH_SIZE_ID_16M) {
+		QspiFlashSize = FLASH_SIZE_16M;
+		fsbl_printf(DEBUG_INFO, "16M Bits\r\n");
+	} else if (ReadBuffer[2] == FLASH_SIZE_ID_32M) {
+		QspiFlashSize = FLASH_SIZE_32M;
+		fsbl_printf(DEBUG_INFO, "32M Bits\r\n");
+	} else if (ReadBuffer[2] == FLASH_SIZE_ID_64M) {
+		QspiFlashSize = FLASH_SIZE_64M;
+		fsbl_printf(DEBUG_INFO, "64M Bits\r\n");
+	} else if (ReadBuffer[3] == FLASH_SIZE_ID_128M) {
 		QspiFlashSize = FLASH_SIZE_128M;
 		fsbl_printf(DEBUG_INFO, "128M Bits\r\n");
 	} else if (ReadBuffer[3] == FLASH_SIZE_ID_256M) {
