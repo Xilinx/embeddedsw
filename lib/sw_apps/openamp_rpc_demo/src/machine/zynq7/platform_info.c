c9998b0652c7aac93a33826f68e17ede6b44239c
Ben Levinsky <ben.levinsky@xilinx.com>
2018-08-14 10:08:21 -0700

    openamp: rpc: generic: update to match upstream
    Update openamp rpc demo application for standalone to match
    upstreamup to commit e8355d5
    
    Signed-off-by: Ben Levinsky <ben.levinsky@xilinx.com>
    
    Acked-for-series: Wendy Liang <wendy.liang@xilinx.com>

--- lib/sw_apps/openamp_rpc_demo/src/machine/zynq7/platform_info.c
+++ lib/sw_apps/openamp_rpc_demo/src/machine/zynq7/platform_info.c
@@ -1,31 +1,8 @@
 /*
- * Copyright (c) 2014, Mentor Graphics Corporation
- * All rights reserved.
- * Copyright (c) 2017 Xilinx, Inc.
+ * Copyright (c) 2014, Mentor Graphics Corporation. All rights reserved.
+ * Copyright (c) 2017 - 2018 Xilinx, Inc. All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. Neither the name of Mentor Graphics Corporation nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 
 /**************************************************************************
@@ -40,51 +17,245 @@
  *
  **************************************************************************/
 
-#include "openamp/hil.h"
-#include "metal/atomic.h"
+#include <openamp/remoteproc.h>
+#include <openamp/rpmsg_virtio.h>
+#include <metal/atomic.h>
+#include <metal/device.h>
+#include <metal/io.h>
+#include <metal/irq.h>
+#include <metal/sys.h>
 #include "platform_info.h"
+#include "rsc_table.h"
+#include <xparameters.h>
 
-#define APU_CPU_ID     0 /* another APU core CPU index. We only talk
-			  * to one CPU in the example. We set the CPU
-			  * index to 0. */
+/* Another APU core ID. In this demo, the other APU core is 0. */
+#define A9_CPU_ID	0UL
+
+/* scugic device, used to raise soft irq */
+#define SCUGIC_DEV_NAME	"scugic_dev"
+#define SCUGIC_BUS_NAME	"generic"
+
+/* scugic base address */
+#define SCUGIC_PERIPH_BASE	0xF8F00000
+#define SCUGIC_DIST_BASE	(SCUGIC_PERIPH_BASE + 0x00001000)
+
+#define _rproc_wait() asm volatile("wfi")
 
 /* processor operations for hil_proc for A9. It defines
  * notification operation and remote processor management. */
-extern struct hil_platform_ops zynq_a9_proc_ops;
-
-/* Firmware_info and fw_table_size are required in current version (2017.3)
- * of OpenAMP library to pass compilation. Will be removed in next release
- * if the OpenAMP application is not used to boot the remote. */
-const struct firmware_info fw_table[] =
-{
-	{"unknown",
-	 0,
-	 0}
+extern struct remoteproc_ops zynq_a9_proc_ops;
+static metal_phys_addr_t scugic_phys_addr = SCUGIC_DIST_BASE;
+struct metal_device scugic_device = {
+	.name = SCUGIC_DEV_NAME,
+	.bus = NULL,
+	.num_regions = 1,
+	.regions = {
+		{
+			.virt = (void *)SCUGIC_DIST_BASE,
+			.physmap = &scugic_phys_addr,
+			.size = 0x1000,
+			.page_shift = -1UL,
+			.page_mask = -1UL,
+			.mem_flags = DEVICE_MEMORY,
+			.ops = {NULL},
+		},
+	},
+	.node = {NULL},
+	.irq_num = 0,
+	.irq_info = NULL,
 };
-const int fw_table_size = sizeof(fw_table)/sizeof(struct firmware_info);
 
-struct hil_proc *platform_create_proc(int proc_index)
+/* Remoteproc private data */
+static struct remoteproc_priv rproc_priv = {
+	.gic_name = SCUGIC_DEV_NAME,
+	.gic_bus_name = SCUGIC_BUS_NAME,
+	.gic_dev = NULL,
+	.gic_io = NULL,
+	.irq_to_notify = SGI_TO_NOTIFY,
+	.irq_notification = SGI_NOTIFICATION,
+	/* toggle CPU ID, there is only two CPUs in PS */
+	.cpu_id = (~XPAR_CPU_ID) & ZYNQ_CPU_ID_MASK,
+};
+
+/* Remoteproc instance */
+static struct remoteproc rproc_inst;
+
+/* External functions */
+extern int init_system(void);
+extern void cleanup_system(void);
+
+static struct remoteproc *
+platform_create_proc(int proc_index, int rsc_index)
 {
+	void *rsc_table;
+	int rsc_size;
+	int ret;
+	metal_phys_addr_t pa;
+
 	(void) proc_index;
-	/* structure to represent a remote processor. It encapsulates the
-	 * shared memory and notification info required for inter processor
-	 * communication. */
-	struct hil_proc *proc;
-	proc = hil_create_proc(&zynq_a9_proc_ops, APU_CPU_ID, NULL);
-	if (!proc)
+	rsc_table = get_resource_table(rsc_index, &rsc_size);
+
+	/* Register IPI device */
+	(void)metal_register_generic_device(&scugic_device);
+	/* Initialize remoteproc instance */
+	if (!remoteproc_init(&rproc_inst, &zynq_a9_proc_ops, &rproc_priv))
 		return NULL;
 
-	/*************************************************************
-	 * Set vrings notification private data to hil_proc.
-	 * We don't set VirtIO device notification because we don't
-	 * support VirtIO status changes nofication kick on Zynq7.
-	 *************************************************************/
-	/* Set vring 0 nofication private data. */
-	hil_set_vring_ipi(proc, 0, VRING0_IPI_INTR_VECT, NULL);
-	/* Set vring 1 nofication private data. */
-	hil_set_vring_ipi(proc, 1, VRING1_IPI_INTR_VECT, NULL);
-	/* Set name of RPMsg channel 0 */
-	hil_set_rpmsg_channel(proc, 0, RPMSG_CHAN_NAME);
+	/*
+	 * Mmap shared memories
+	 * Or shall we constraint that they will be set as carved out
+	 * in the resource table?
+	 */
+	/* mmap resource table */
+	pa = (metal_phys_addr_t)rsc_table;
+	(void *)remoteproc_mmap(&rproc_inst, &pa,
+				NULL, rsc_size,
+				NORM_NONCACHE | STRONG_ORDERED,
+				&rproc_inst.rsc_io);
+	/* mmap shared memory */
+	pa = SHARED_MEM_PA;
+	(void *)remoteproc_mmap(&rproc_inst, &pa,
+				NULL, SHARED_MEM_SIZE,
+				NORM_NONCACHE | STRONG_ORDERED,
+				NULL);
 
-	return proc;
+	/* parse resource table to remoteproc */
+	ret = remoteproc_set_rsc_table(&rproc_inst, rsc_table, rsc_size);
+	if (ret) {
+		xil_printf("Failed to intialize remoteproc\r\n");
+		remoteproc_remove(&rproc_inst);
+		return NULL;
+	}
+	xil_printf("Initialize remoteproc successfully.\r\n");
+
+	return &rproc_inst;
+}
+
+int platform_init(int argc, char *argv[], void **platform)
+{
+	unsigned long proc_id = 0;
+	unsigned long rsc_id = 0;
+	struct remoteproc *rproc;
+
+	if (!platform) {
+		xil_printf("Failed to initialize platform,"
+			   "NULL pointer to store platform data.\n");
+		return -EINVAL;
+	}
+	/* Initialize HW system components */
+	init_system();
+
+	if (argc >= 2) {
+		proc_id = strtoul(argv[1], NULL, 0);
+	}
+
+	if (argc >= 3) {
+		rsc_id = strtoul(argv[2], NULL, 0);
+	}
+
+	rproc = platform_create_proc(proc_id, rsc_id);
+	if (!rproc) {
+		xil_printf("Failed to create remoteproc device.\n");
+		return -EINVAL;
+	}
+	*platform = rproc;
+	return 0;
+}
+
+/* RPMsg virtio shared buffer pool */
+static struct rpmsg_virtio_shm_pool shpool;
+
+struct  rpmsg_device *
+platform_create_rpmsg_vdev(void *platform, unsigned int vdev_index,
+			   unsigned int role,
+			   void (*rst_cb)(struct virtio_device *vdev),
+			   rpmsg_ns_bind_cb ns_bind_cb)
+{
+	struct remoteproc *rproc = platform;
+	struct rpmsg_virtio_device *rpmsg_vdev;
+	struct virtio_device *vdev;
+	void *shbuf;
+	struct metal_io_region *shbuf_io;
+	int ret;
+
+	rpmsg_vdev = metal_allocate_memory(sizeof(*rpmsg_vdev));
+	if (!rpmsg_vdev)
+		return NULL;
+	shbuf_io = remoteproc_get_io_with_pa(rproc, SHARED_MEM_PA);
+	if (!shbuf_io)
+		return NULL;
+	shbuf = metal_io_phys_to_virt(shbuf_io,
+				      SHARED_MEM_PA + SHARED_BUF_OFFSET);
+
+	xil_printf("creating remoteproc virtio\r\n");
+	/* TODO: can we have a wrapper for the following two functions? */
+	vdev = remoteproc_create_virtio(rproc, vdev_index, role, rst_cb);
+	if (!vdev) {
+		xil_printf("failed remoteproc_create_virtio\r\n");
+		goto err1;
+	}
+
+	xil_printf("initializing rpmsg vdev\r\n");
+	if (role == VIRTIO_DEV_MASTER) {
+		/* Only RPMsg virtio master needs to initialize the
+		 * shared buffers pool
+		 */
+		rpmsg_virtio_init_shm_pool(&shpool, shbuf,
+					   (SHARED_MEM_SIZE -
+					    SHARED_BUF_OFFSET));
+
+		/* RPMsg virtio slave can set shared buffers pool
+		 * argument to NULL
+		 */
+		ret =  rpmsg_init_vdev(rpmsg_vdev, vdev, ns_bind_cb,
+				       shbuf_io, &shpool);
+	} else {
+		ret =  rpmsg_init_vdev(rpmsg_vdev, vdev, ns_bind_cb,
+				       shbuf_io, NULL);
+	}
+	if (ret) {
+		xil_printf("failed rpmsg_init_vdev\r\n");
+		goto err2;
+	}
+	xil_printf("initializing rpmsg vdev\r\n");
+	return rpmsg_virtio_get_rpmsg_device(rpmsg_vdev);
+err2:
+	remoteproc_remove_virtio(rproc, vdev);
+err1:
+	metal_free_memory(rpmsg_vdev);
+	return NULL;
+}
+
+int platform_poll(void *priv)
+{
+	struct remoteproc *rproc = priv;
+	struct remoteproc_priv *prproc;
+	unsigned int flags;
+
+	prproc = rproc->priv;
+	while(1) {
+		flags = metal_irq_save_disable();
+		if (!(atomic_flag_test_and_set(&prproc->nokick))) {
+			metal_irq_restore_enable(flags);
+			remoteproc_get_notification(rproc, RSC_NOTIFY_ID_ANY);
+			break;
+		}
+		_rproc_wait();
+		metal_irq_restore_enable(flags);
+	}
+	return 0;
+}
+
+void platform_release_rpmsg_vdev(struct rpmsg_device *rpdev)
+{
+	(void)rpdev;
+}
+
+void platform_cleanup(void *platform)
+{
+	struct remoteproc *rproc = platform;
+
+	if (rproc)
+		remoteproc_remove(rproc);
+	cleanup_system();
 }
--- lib/sw_apps/openamp_rpc_demo/src/system/generic/rpc_demo.c
+++ lib/sw_apps/openamp_rpc_demo/src/system/generic/rpc_demo.c
@@ -1,83 +1,17 @@
-/*
- * Copyright (c) 2014, Mentor Graphics Corporation
- * All rights reserved.
- *
- * Copyright (C) 2017 Xilinx, Inc.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. Neither the name of Mentor Graphics Corporation nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+/* This is a sample demonstration application that showcases usage of proxy from the remote core.
+ This application is meant to run on the remote CPU running baremetal.
+ This applicationr can print to to master console and perform file I/O using proxy mechanism. */
 
-/**************************************************************************************
-* This is a sample demonstration application that showcases usage of proxy
-* from the remote core. This application is meant to run on the remote CPU running
-* bare-metal. It can print to master console and perform file I/O
-* using proxy mechanism.
-*
-* The init_system is called from main function which defines a shared memory region in
-* MPU settings for the communication between master and remote using
-* zynqMP_r5_map_mem_region API,it also initializes interrupt controller
-* GIC and register the interrupt service routine for IPI using
-* zynqMP_r5_gic_initialize API.
-*
-* The remoteproc_resource_init API is being called to create the virtio/RPMsg devices
-* required for IPC with the master context. Invocation of this API causes remoteproc
-* on the bare-metal to use the rpmsg name service announcement feature to advertise
-* the rpmsg channels served by the application.
-*
-* The master receives the advertisement messages and performs the following tasks:
-* 	1. Invokes the channel created callback registered by the master application
-* 	2. Responds to remote context with a name service acknowledgement message
-* After the acknowledgement is received from master, remoteproc on the bare-metal
-* invokes the RPMsg channel-created callback registered by the remote application.
-* The RPMsg channel is established at this point. All RPMsg APIs can be used subsequently
-* on both sides for run time communications between the master and remote software contexts.
-*
-* Upon running the master application, this application will use the console to display
-* print statements and execute file I/O on master by communicating with a proxy application
-* running on the Linux master. Once the application is ran and task by the
-* bare-metal application is done, master needs to properly shut down the remote
-* processor
-*
-* To shut down the remote processor, the following steps are performed:
-* 	1. The master application sends an application-specific shutdown message
-* 	   to the remote context
-* 	2. This bare-metal application cleans up application resources,
-* 	   sends a shutdown acknowledge to master, and invokes remoteproc_resource_deinit
-* 	   API to de-initialize remoteproc on the bare-metal side.
-* 	3. On receiving the shutdown acknowledge message, the master application invokes
-* 	   the remoteproc_shutdown API to shut down the remote processor and de-initialize
-* 	   remoteproc using remoteproc_deinit on its side.
-*
-**************************************************************************************/
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
-#include "xil_printf.h"
-#include "openamp/open_amp.h"
-#include "openamp/rpmsg_retarget.h"
+#include <openamp/open_amp.h>
+#include <openamp/rpmsg_retarget.h>
 #include "rsc_table.h"
 #include "platform_info.h"
+#include "rpmsg-rpc-demo.h"
 
 #define REDEF_O_CREAT   0000100
 #define REDEF_O_EXCL    0000200
@@ -87,58 +21,23 @@
 #define REDEF_O_APPEND  0002000
 #define REDEF_O_ACCMODE 0000003
 
-#define RPC_CHANNEL_READY_TO_CLOSE "rpc_channel_ready_to_close"
-
-/* xil_printf goes directly to serial port */
 #define LPRINTF(format, ...) xil_printf(format, ##__VA_ARGS__)
+//#define LPRINTF(format, ...)
 #define LPERROR(format, ...) LPRINTF("ERROR: " format, ##__VA_ARGS__)
 
-/* Global functions and variables */
-extern int init_system(void);
-extern void cleanup_system(void);
-
-/* Local variables */
-static struct rpmsg_channel *app_rp_chnl;
-static volatile int chnl_is_alive = 0;
-static struct remote_proc *proc = NULL;
-static struct rsc_table_info rsc_info;
-
-/*-----------------------------------------------------------------------------*
- *  RPMSG callbacks setup by remoteproc_resource_init()
- *-----------------------------------------------------------------------------*/
-static void rpmsg_read_cb(struct rpmsg_channel *rp_chnl, void *data, int len,
-			  void *priv, unsigned long src)
+static void rpmsg_rpc_shutdown(struct rpmsg_rpc_data *rpc)
 {
-	(void)rp_chnl;
-	(void)data;
-	(void)len;
-	(void)priv;
-	(void)src;
-}
-
-static void rpmsg_channel_created(struct rpmsg_channel *rp_chnl)
-{
-	app_rp_chnl = rp_chnl;
-	chnl_is_alive = 1;
-}
-
-static void rpmsg_channel_deleted(struct rpmsg_channel *rp_chnl)
-{
-	(void)rp_chnl;
-	app_rp_chnl = NULL;
-}
-
-static void shutdown_cb(struct rpmsg_channel *rp_chnl)
-{
-	(void)rp_chnl;
-	chnl_is_alive = 0;
+	(void)rpc;
+	LPRINTF("RPMSG RPC is shutting down.\n");
 }
 
 /*-----------------------------------------------------------------------------*
  *  Application specific
  *-----------------------------------------------------------------------------*/
-int app (struct hil_proc *hproc)
+int app(struct rpmsg_device *rdev, void *priv)
 {
+	struct rpmsg_rpc_data rpc;
+	struct rpmsg_rpc_syscall rpccall;
 	int fd, bytes_written, bytes_read;
 	char fname[] = "remote.file";
 	char wbuff[50];
@@ -147,29 +46,17 @@ int app (struct hil_proc *hproc)
 	float fdata;
 	int idata;
 	int ret;
-	int status;
-
-	/* Initialize framework */
-	LPRINTF("Try to init remoteproc resource\n");
-	status = remoteproc_resource_init(&rsc_info, hproc,
-					  rpmsg_channel_created,
-					  rpmsg_channel_deleted, rpmsg_read_cb,
-					  &proc, 0);
-	if (RPROC_SUCCESS != status) {
-		LPERROR("Failed  to initialize remoteproc resource.\n");
-		return -1;
-	}
-
-	LPRINTF("Init remoteproc resource done\n");
-
-	LPRINTF("Waiting for channel creation...\n");
-	while (!chnl_is_alive) {
-		hil_poll(proc->proc, 0);
-	}
 
 	/* redirect I/Os */
 	LPRINTF("Initializating I/Os redirection...\n");
-	rpmsg_retarget_init(app_rp_chnl, shutdown_cb);
+	ret = rpmsg_rpc_init(&rpc, rdev, RPMSG_SERVICE_NAME,
+			     RPMSG_ADDR_ANY, RPMSG_ADDR_ANY,
+			     priv, platform_poll, rpmsg_rpc_shutdown);
+	rpmsg_set_default_rpc(&rpc);
+	if (ret) {
+		LPRINTF("Failed to intialize rpmsg rpc\n");
+		return -1;
+	}
 
 	printf("\r\nRemote>Baremetal Remote Procedure Call (RPC) Demonstration\r\n");
 	printf("\r\nRemote>***************************************************\r\n");
@@ -236,62 +123,47 @@ int app (struct hil_proc *hproc)
 		}
 	}
 
-	printf("\r\nRemote> Firmware's rpmsg-openamp-demo-channel going down! \r\n");
+	printf("\r\nRemote> Firmware's rpmsg-rpc-channel going down! \r\n");
+	rpccall.id = TERM_SYSCALL_ID;
+	(void)rpmsg_rpc_send(&rpc, &rpccall, sizeof(rpccall), NULL, 0);
 
-	sprintf(wbuff, RPC_CHANNEL_READY_TO_CLOSE);
-	rpmsg_retarget_send(wbuff, sizeof (RPC_CHANNEL_READY_TO_CLOSE) + 1);
-
-	LPRINTF("Waiting for channel deletion...\n");
-	while (chnl_is_alive) {
-		hil_poll(proc->proc, 0);
-	}
-
-	LPRINTF("De-initializating rpmsg_retarget\n");
-	rpmsg_retarget_deinit(app_rp_chnl);
-	/* disable interrupts and free resources */
-	LPRINTF("De-initializating remoteproc resource\n");
-	remoteproc_resource_deinit(proc);
-
+	LPRINTF("Release remoteproc procedure call\n");
+	rpmsg_rpc_release(&rpc);
 	return 0;
 }
 
 /*-----------------------------------------------------------------------------*
  *  Application entry point
  *-----------------------------------------------------------------------------*/
-int main(void)
+int main(int argc, char *argv[])
 {
-	unsigned long proc_id = 0;
-	unsigned long rsc_id = 0;
-	struct hil_proc *hproc;
-	int status = -1;
+	void *platform;
+	struct rpmsg_device *rpdev;
+	int ret;
 
 	LPRINTF("Starting application...\n");
 
-	/* Initialize HW system components */
-	init_system();
-
-	/* Create HIL proc */
-	hproc = platform_create_proc(proc_id);
-	if (!hproc) {
-		LPERROR("Failed to create hil proc.\n");
+	/* Initialize platform */
+	ret = platform_init(argc, argv, &platform);
+	if (ret) {
+		LPERROR("Failed to initialize platform.\n");
+		ret = -1;
 	} else {
-		rsc_info.rsc_tab =
-			get_resource_table((int)rsc_id, &rsc_info.size);
-		if (!rsc_info.rsc_tab) {
-			LPERROR("Failed to get resource table data.\n");
+		rpdev = platform_create_rpmsg_vdev(platform, 0,
+						   VIRTIO_DEV_SLAVE,
+						   NULL, NULL);
+		if (!rpdev) {
+			LPERROR("Failed to create rpmsg virtio device.\n");
+			ret = -1;
 		} else {
-			status = app(hproc);
+			app(rpdev, platform);
+			platform_release_rpmsg_vdev(rpdev);
+			ret = 0;
 		}
 	}
 
 	LPRINTF("Stopping application...\n");
-	cleanup_system();
+	platform_cleanup(platform);
 
-	/* Suspend processor execution */
-	while (1) {
-		__asm__("wfi\n\t");
-	}
-
-	return status;
+	return ret;
 }
-
