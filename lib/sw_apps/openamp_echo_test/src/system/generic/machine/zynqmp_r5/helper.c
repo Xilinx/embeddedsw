9e37720f28b51c99db721b1a48eb616691215c12
Ben Levinsky <ben.levinsky@xilinx.com>
2018-08-14 10:08:18 -0700

    openamp: echo test: generic: update to match upstream
    Update openamp echo test application for standalone to match upstream
    up to commit e8355d5
    
    Signed-off-by: Ben Levinsky <ben.levinsky@xilinx.com>
    
    Acked-for-series: Wendy Liang <wendy.liang@xilinx.com>

--- lib/sw_apps/openamp_echo_test/src/system/generic/machine/zynqmp_r5/helper.c
+++ lib/sw_apps/openamp_echo_test/src/system/generic/machine/zynqmp_r5/helper.c
@@ -4,37 +4,15 @@
  *
  * Copyright (c) 2015 Xilinx, Inc. All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. Neither the name of the <ORGANIZATION> nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include "xparameters.h"
 #include "xil_exception.h"
 #include "xscugic.h"
 #include "xil_cache.h"
-#include "metal/sys.h"
-#include "metal/irq.h"
+#include <metal/sys.h>
+#include <metal/irq.h>
 #include "platform_info.h"
 
 #define INTC_DEVICE_ID		XPAR_SCUGIC_0_DEVICE_ID
@@ -60,11 +38,11 @@ static int app_gic_initialize(void)
 	 */
 	int_ctrl_config = XScuGic_LookupConfig(INTC_DEVICE_ID);
 	if (NULL == int_ctrl_config) {
-	        return XST_FAILURE;
+		return XST_FAILURE;
 	}
 
 	status = XScuGic_CfgInitialize(&xInterruptController, int_ctrl_config,
-					int_ctrl_config->CpuBaseAddress);
+				       int_ctrl_config->CpuBaseAddress);
 	if (status != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
@@ -73,10 +51,10 @@ static int app_gic_initialize(void)
 	for (int_id = 32U; int_id<XSCUGIC_MAX_NUM_INTR_INPUTS;int_id=int_id+4U) {
 		target_cpu = XScuGic_DistReadReg(&xInterruptController,
 						XSCUGIC_SPI_TARGET_OFFSET_CALC(int_id));
-	/* Remove current CPU from interrupt target register */
-	target_cpu &= ~mask_cpu_id;
-	XScuGic_DistWriteReg(&xInterruptController,
-				XSCUGIC_SPI_TARGET_OFFSET_CALC(int_id), target_cpu);
+		/* Remove current CPU from interrupt target register */
+		target_cpu &= ~mask_cpu_id;
+		XScuGic_DistWriteReg(&xInterruptController,
+					XSCUGIC_SPI_TARGET_OFFSET_CALC(int_id), target_cpu);
 	}
 	XScuGic_InterruptMaptoCpu(&xInterruptController, XPAR_CPU_ID, IPI_IRQ_VECT_ID);
 
@@ -98,11 +76,22 @@ static int app_gic_initialize(void)
 	return 0;
 }
 
+static void system_metal_logger(enum metal_log_level level,
+			   const char *format, ...)
+{
+	(void)level;
+	(void)format;
+}
+
+
 /* Main hw machinery initialization entry point, called from main()*/
 /* return 0 on success */
 int init_system(void)
 {
-	struct metal_init_params metal_param = METAL_INIT_DEFAULTS;
+	struct metal_init_params metal_param = {
+		.log_handler = system_metal_logger,
+		.log_level = METAL_LOG_INFO,
+	};
 
 	/* Low level abstraction layer for openamp initialization */
 	metal_init(&metal_param);
