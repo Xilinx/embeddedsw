b1ef7f2d11fc0ec5aed7997c5c1048cbdd36fa50
Ben Levinsky <ben.levinsky@xilinx.com>
2018-08-14 10:08:16 -0700

    openamp: update to upstream
    update up to b4aaf58c860b14869590ddf2229872aa397c141a
    
    Signed-off-by: Ben Levinsky <ben.levinsky@xilinx.com>
    
    Acked-for-series: Wendy Liang <wendy.liang@xilinx.com>

--- ThirdParty/sw_services/openamp/src/open-amp/lib/include/openamp/virtio.h
+++ ThirdParty/sw_services/openamp/src/open-amp/lib/include/openamp/virtio.h
@@ -1,29 +1,5 @@
-/*-
- * This header is BSD licensed so anyone can use the definitions to implement
- * compatible drivers/servers.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of IBM nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL IBM OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
  *
  * $FreeBSD$
  */
@@ -31,18 +7,29 @@
 #ifndef _VIRTIO_H_
 #define _VIRTIO_H_
 
-#include "openamp/virtqueue.h"
+#include <openamp/virtqueue.h>
+#include <metal/spinlock.h>
+
+#if defined __cplusplus
+extern "C" {
+#endif
+
+/* TODO: define this as compiler flags */
+#ifndef VIRTIO_MAX_NUM_VRINGS
+#define VIRTIO_MAX_NUM_VRINGS 2
+#endif
 
 /* VirtIO device IDs. */
-#define VIRTIO_ID_NETWORK    0x01
-#define VIRTIO_ID_BLOCK      0x02
-#define VIRTIO_ID_CONSOLE    0x03
-#define VIRTIO_ID_ENTROPY    0x04
-#define VIRTIO_ID_BALLOON    0x05
-#define VIRTIO_ID_IOMEMORY   0x06
-#define VIRTIO_ID_RPMSG		 0x07	/* virtio remote remote_proc messaging */
-#define VIRTIO_ID_SCSI       0x08
-#define VIRTIO_ID_9P         0x09
+#define VIRTIO_ID_NETWORK    0x01UL
+#define VIRTIO_ID_BLOCK      0x02UL
+#define VIRTIO_ID_CONSOLE    0x03UL
+#define VIRTIO_ID_ENTROPY    0x04UL
+#define VIRTIO_ID_BALLOON    0x05UL
+#define VIRTIO_ID_IOMEMORY   0x06UL
+#define VIRTIO_ID_RPMSG	     0x07UL /* remote processor messaging */
+#define VIRTIO_ID_SCSI       0x08UL
+#define VIRTIO_ID_9P         0x09UL
+#define VIRTIO_DEV_ANY_ID    (-1)UL
 
 /* Status byte for guest to report progress. */
 #define VIRTIO_CONFIG_STATUS_ACK       0x01
@@ -50,6 +37,15 @@
 #define VIRTIO_CONFIG_STATUS_DRIVER_OK 0x04
 #define VIRTIO_CONFIG_STATUS_NEEDS_RESET 0x40
 #define VIRTIO_CONFIG_STATUS_FAILED    0x80
+
+/* Virtio device role */
+#define VIRTIO_DEV_MASTER	0UL
+#define VIRTIO_DEV_SLAVE	1UL
+
+struct virtio_device_id {
+	uint32_t device;
+	uint32_t vendor;
+};
 
 /*
  * Generate interrupt when the virtqueue ring is
@@ -71,45 +67,67 @@
 #define VIRTIO_TRANSPORT_F_START      28
 #define VIRTIO_TRANSPORT_F_END        32
 
-typedef struct _virtio_dispatch_ virtio_dispatch;
+typedef void (*virtio_dev_reset_cb)(struct virtio_device *vdev);
+
+struct virtio_dispatch;
 
 struct virtio_feature_desc {
 	uint32_t vfd_val;
 	const char *vfd_str;
 };
 
+/**
+ * struct proc_shm
+ *
+ * This structure is maintained by hardware interface layer for
+ * shared memory information. The shared memory provides buffers
+ * for use by the vring to exchange messages between the cores.
+ *
+ */
+struct virtio_buffer_info {
+	/* Start address of shared memory used for buffers. */
+	void *vaddr;
+	/* Start physical address of shared memory used for buffers. */
+	metal_phys_addr_t paddr;
+	/* sharmed memory I/O region */
+	struct metal_io_region *io;
+	/* Size of shared memory. */
+	unsigned long size;
+};
+
+/**
+ * struct remoteproc_vring - remoteproc vring structure
+ * @vq virtio queue
+ * @va logical address
+ * @notifyid vring notify id
+ * @num_descs number of descriptors
+ * @align vring alignment
+ * @io metal I/O region of the vring memory, can be NULL
+ */
+struct virtio_vring_info {
+	struct virtqueue *vq;
+	struct vring_alloc_info info;
+	uint32_t notifyid;
+	struct metal_io_region *io;
+};
+
 /*
  * Structure definition for virtio devices for use by the
  * applications/drivers
- *
  */
 
 struct virtio_device {
-	/*
-	 * Since there is no generic device structure so
-	 * keep its type as void. The driver layer will take
-	 * care of it.
-	 */
-	void *device;
-
-	/* Device name */
-	char *name;
-
-	/* List of virtqueues encapsulated by virtio device. */
-	//TODO : Need to implement a list service for ipc stack.
-	void *vq_list;
-
-	/* Virtio device specific features */
-	uint32_t features;
-
-	/* Virtio dispatch table */
-	virtio_dispatch *func;
-
-	/*
-	 * Pointer to hold some private data, useful
-	 * in callbacks.
-	 */
-	void *data;
+	uint32_t index; /**< unique position on the virtio bus */
+	struct virtio_device_id id; /**< the device type identification
+				      *  (used to match it with a driver
+				      */
+	uint64_t features; /**< the features supported by both ends. */
+	unsigned int role; /**< if it is virtio backend or front end. */
+	virtio_dev_reset_cb reset_cb; /**< user registered device callback */
+	const struct virtio_dispatch *func; /**< Virtio dispatch table */
+	void *priv; /**< TODO: remove pointer to virtio_device private data */
+	unsigned int vrings_num; /**< number of vrings */
+	struct virtio_vring_info *vrings_info;
 };
 
 /*
@@ -121,34 +139,38 @@ void virtio_describe(struct virtio_device *dev, const char *msg,
 		     struct virtio_feature_desc *feature_desc);
 
 /*
- * Functions for virtio device configuration as defined in Rusty Russell's paper.
+ * Functions for virtio device configuration as defined in Rusty Russell's
+ * paper.
  * Drivers are expected to implement these functions in their respective codes.
- *
  */
 
-struct _virtio_dispatch_ {
-	int (*create_virtqueues) (struct virtio_device * dev, int flags,
-				  int nvqs, const char *names[],
-				  vq_callback * callbacks[],
-				  struct virtqueue * vqs[]);
-	 uint8_t(*get_status) (struct virtio_device * dev);
-	void (*set_status) (struct virtio_device * dev, uint8_t status);
-	 uint32_t(*get_features) (struct virtio_device * dev);
-	void (*set_features) (struct virtio_device * dev, uint32_t feature);
-	 uint32_t(*negotiate_features) (struct virtio_device * dev,
-					uint32_t features);
+struct virtio_dispatch {
+	uint8_t (*get_status)(struct virtio_device *dev);
+	void (*set_status)(struct virtio_device *dev, uint8_t status);
+	uint32_t (*get_features)(struct virtio_device *dev);
+	void (*set_features)(struct virtio_device *dev, uint32_t feature);
+	uint32_t (*negotiate_features)(struct virtio_device *dev,
+				       uint32_t features);
 
 	/*
 	 * Read/write a variable amount from the device specific (ie, network)
 	 * configuration region. This region is encoded in the same endian as
 	 * the guest.
 	 */
-	void (*read_config) (struct virtio_device * dev, uint32_t offset,
-			     void *dst, int length);
-	void (*write_config) (struct virtio_device * dev, uint32_t offset,
-			      void *src, int length);
-	void (*reset_device) (struct virtio_device * dev);
-
+	void (*read_config)(struct virtio_device *dev, uint32_t offset,
+			    void *dst, int length);
+	void (*write_config)(struct virtio_device *dev, uint32_t offset,
+			     void *src, int length);
+	void (*reset_device)(struct virtio_device *dev);
+	void (*notify)(struct virtqueue *vq);
 };
+
+int virtio_create_virtqueues(struct virtio_device *vdev, unsigned int flags,
+			     unsigned int nvqs, const char *names[],
+			     vq_callback *callbacks[]);
+
+#if defined __cplusplus
+}
+#endif
 
 #endif				/* _VIRTIO_H_ */
--- ThirdParty/sw_services/openamp/src/open-amp/lib/virtio/virtio.c
+++ ThirdParty/sw_services/openamp/src/open-amp/lib/virtio/virtio.c
@@ -2,34 +2,15 @@
  * Copyright (c) 2011, Bryan Venteicher <bryanv@FreeBSD.org>
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice unmodified, this list of conditions, and the following
- *    disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * SPDX-License-Identifier: BSD-2-Clause
  */
-#include "openamp/virtio.h"
+#include <openamp/virtio.h>
 
 static const char *virtio_feature_name(unsigned long feature,
-				       struct virtio_feature_desc *);
+				       const struct virtio_feature_desc *);
 
 //TODO : This structure may change depending on the types of devices we support.
-static struct virtio_ident {
+static const struct virtio_ident {
 	unsigned short devid;
 	const char *name;
 } virtio_ident_table[] = {
@@ -46,7 +27,7 @@ static struct virtio_ident {
 };
 
 /* Device independent features. */
-static struct virtio_feature_desc virtio_common_feature_desc[] = {
+static const struct virtio_feature_desc virtio_common_feature_desc[] = {
 	{VIRTIO_F_NOTIFY_ON_EMPTY, "NotifyOnEmpty"},
 	{VIRTIO_RING_F_INDIRECT_DESC, "RingIndirect"},
 	{VIRTIO_RING_F_EVENT_IDX, "EventIdx"},
@@ -57,7 +38,7 @@ static struct virtio_feature_desc virtio_common_feature_desc[] = {
 
 const char *virtio_dev_name(unsigned short devid)
 {
-	struct virtio_ident *ident;
+	const struct virtio_ident *ident;
 
 	for (ident = virtio_ident_table; ident->name != NULL; ident++) {
 		if (ident->devid == devid)
@@ -68,15 +49,15 @@ const char *virtio_dev_name(unsigned short devid)
 }
 
 static const char *virtio_feature_name(unsigned long val,
-				       struct virtio_feature_desc *desc)
+				       const struct virtio_feature_desc *desc)
 {
 	int i, j;
-	struct virtio_feature_desc *descs[2] = { desc,
+	const struct virtio_feature_desc *descs[2] = { desc,
 		virtio_common_feature_desc
 	};
 
 	for (i = 0; i < 2; i++) {
-		if (descs[i] == NULL)
+		if (!descs[i])
 			continue;
 
 		for (j = 0; descs[i][j].vfd_val != 0; j++) {
@@ -97,4 +78,43 @@ void virtio_describe(struct virtio_device *dev, const char *msg,
 
 	// TODO: Not used currently - keeping it for future use
 	virtio_feature_name(0, desc);
+}
+
+int virtio_create_virtqueues(struct virtio_device *vdev, unsigned int flags,
+			     unsigned int nvqs, const char *names[],
+			     vq_callback *callbacks[])
+{
+	struct virtio_vring_info *vring_info;
+	struct vring_alloc_info *vring_alloc;
+	unsigned int num_vrings, i;
+	int ret;
+	(void)flags;
+
+	num_vrings = vdev->vrings_num;
+	if (nvqs > num_vrings)
+		return -ERROR_VQUEUE_INVLD_PARAM;
+	/* Initialize virtqueue for each vring */
+	for (i = 0; i < nvqs; i++) {
+		vring_info = &vdev->vrings_info[i];
+
+		vring_alloc = &vring_info->info;
+#ifndef VIRTIO_SLAVE_ONLY
+		if (vdev->role == VIRTIO_DEV_MASTER) {
+			size_t offset;
+			struct metal_io_region *io = vring_info->io;
+
+			offset = metal_io_virt_to_offset(io,
+							 vring_alloc->vaddr);
+			metal_io_block_set(io, offset, 0,
+					   vring_size(vring_alloc->num_descs,
+						      vring_alloc->align));
+		}
+#endif
+		ret = virtqueue_create(vdev, i, names[i], vring_alloc,
+				       callbacks[i], vdev->func->notify,
+				       vring_info->vq);
+		if (ret)
+			return ret;
+	}
+	return 0;
 }
