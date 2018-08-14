1d94a9d6e2ba262423cb36473103c0985b04675e
Ben Levinsky <ben.levinsky@xilinx.com>
2018-08-14 10:08:20 -0700

    openamp: matrix multiply: freertos: update to match upstream
    Update openamp matrix multiplication application for freertos
    to match upstreamup to commit e8355d5
    
    Signed-off-by: Ben Levinsky <ben.levinsky@xilinx.com>
    
    Acked-for-series: Wendy Liang <wendy.liang@xilinx.com>

--- lib/sw_apps/openamp_matrix_multiply/src/system/freertos/machine/zynq7/helper.c
+++ lib/sw_apps/openamp_matrix_multiply/src/system/freertos/machine/zynq7/helper.c
@@ -4,56 +4,58 @@
  *
  * Copyright (c) 2015 Xilinx, Inc. All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. Neither the name of the <ORGANIZATION> nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include "xparameters.h"
 #include "xil_exception.h"
 #include "xscugic.h"
 #include "xil_cache.h"
-#include "metal/sys.h"
-#include "metal/irq.h"
+#include <metal/sys.h>
+#include <metal/irq.h>
 #include "platform_info.h"
 
 
 #define INTC_DEVICE_ID		XPAR_SCUGIC_0_DEVICE_ID
 
-extern XScuGic xInterruptController;
+static XScuGic xInterruptController;
 
-/* Complete Interrupt Controller setup, FreeRTOS is doing pre-init */
+/* Interrupt Controller setup */
 static int app_gic_initialize(void)
 {
-	/* Connect IPI0 Interrupt ID with ISR */
-	XScuGic_Connect(&xInterruptController, VRING0_IPI_INTR_VECT,
-			(Xil_ExceptionHandler)metal_irq_isr,
-			(void *)VRING0_IPI_INTR_VECT);
+	u32 Status;
+	XScuGic_Config *IntcConfig;	/* The configuration parameters of the interrupt controller */
 
-	/* Connect IPI1 Interrupt ID with ISR */
-	XScuGic_Connect(&xInterruptController, VRING1_IPI_INTR_VECT,
+	Xil_ExceptionDisable();
+
+	/*
+	 * Initialize the interrupt controller driver
+	 */
+	IntcConfig = XScuGic_LookupConfig(INTC_DEVICE_ID);
+	if (NULL == IntcConfig) {
+		return XST_FAILURE;
+	}
+
+	Status = XScuGic_CfgInitialize(&xInterruptController, IntcConfig,
+				       IntcConfig->CpuBaseAddress);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Register the interrupt handler to the hardware interrupt handling
+	 * logic in the ARM processor.
+	 */
+	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_IRQ_INT,
+			(Xil_ExceptionHandler)XScuGic_InterruptHandler,
+			&xInterruptController);
+
+	Xil_ExceptionEnable();
+
+	/* Connect notificaiton interrupt ID with ISR */
+	XScuGic_Connect(&xInterruptController, SGI_NOTIFICATION,
 			(Xil_ExceptionHandler)metal_irq_isr,
-			(void *)VRING1_IPI_INTR_VECT);
+			(void *)SGI_NOTIFICATION);
 
 	return 0;
 }
