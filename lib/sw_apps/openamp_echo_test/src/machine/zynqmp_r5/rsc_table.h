e08770c55a317f382d8091e8de6f86cfad6a0a28
Ben Levinsky <ben.levinsky@xilinx.com>
2018-08-14 10:08:17 -0700

    openamp: echo test: freertos: update to match upstream
    Update openamp echo test application for FreeRTOS to match upstream
    up to commit e8355d5
    
    Signed-off-by: Ben Levinsky <ben.levinsky@xilinx.com>
    
    Acked-for-series: Wendy Liang <wendy.liang@xilinx.com>

--- lib/sw_apps/openamp_echo_test/src/machine/zynqmp_r5/rsc_table.h
+++ lib/sw_apps/openamp_echo_test/src/machine/zynqmp_r5/rsc_table.h
@@ -4,29 +4,7 @@
  *
  * Copyright (C) 2015 Xilinx, Inc.  All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. Neither the name of Mentor Graphics Corporation nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 
 /* This file populates resource table for BM remote
@@ -36,7 +14,11 @@
 #define RSC_TABLE_H_
 
 #include <stddef.h>
-#include "openamp/open_amp.h"
+#include <openamp/open_amp.h>
+
+#if defined __cplusplus
+extern "C" {
+#endif
 
 #define NO_RESOURCE_ENTRIES         8
 
@@ -57,5 +39,9 @@ struct remote_resource_table {
 }__attribute__((packed, aligned(0x100)));
 
 void *get_resource_table (int rsc_id, int *len);
+
+#if defined __cplusplus
+}
+#endif
 
 #endif /* RSC_TABLE_H_ */
