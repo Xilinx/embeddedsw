a2a5afb6b80e8751541f35446af3f94027a12b5b
Eric Bong <nyong.nyiew.bong@xilinx.com>
2018-08-24 17:39:50 +0800

    hdcp22_cipher, hdcp22_common, hdcp22_mmult, hdcp22_rng, hdcp22_rx, hdcp22_tx: Updated copyright messages
    Signed-off-by: Eric Bong <nyongnyi@xilinx.com>
    
    Acked-by:Ilias Ibrahim <mmo@xilinx.com>

--- XilinxProcessorIPLib/drivers/hdcp22_mmult/src/xhdcp22_mmult_g.c
+++ XilinxProcessorIPLib/drivers/hdcp22_mmult/src/xhdcp22_mmult_g.c
@@ -1,7 +1,7 @@
 /*******************************************************************
 *
 * Copyright (C) 2010-2016 Xilinx, Inc. All rights reserved.
-* 
+*
 *Permission is hereby granted, free of charge, to any person obtaining a copy
 *of this software and associated documentation files (the "Software"), to deal
 *in the Software without restriction, including without limitation the rights
