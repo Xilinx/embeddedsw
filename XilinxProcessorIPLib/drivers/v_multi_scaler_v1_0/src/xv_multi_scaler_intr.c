2c22a1232cc9bcc52165132477b8d1f382d4246f
Vivek Veenam <vivek.veenam@xilinx.com>
2018-08-24 14:51:32 +0530

    v_multi_scaler: Add driver for multiscaler IP
    Add driver for multiscaler IP.
    
    Signed-off-by: Vivek Veenam <vveenam@xilinx.com>
    Acked-by: Vishal Sagar <vishal.sagar@xilinx.com>
    
    Acked-by: Vishal Sagar <vishal.sagar@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/v_multi_scaler_v1_0/src/xv_multi_scaler_intr.c
@@ -0,0 +1,108 @@
+/******************************************************************************
+ *
+ * Copyright (C) 2018 Xilinx, Inc.  All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * XILINX CONSORTIUM BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+ * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * Except as contained in this notice, the name of the Xilinx shall not be used
+ * in advertising or otherwise to promote the sale, use or other dealings in
+ * this Software without prior written authorization from Xilinx.
+ *
+ *****************************************************************************/
+/*****************************************************************************/
+/**
+ *
+ * @file xv_multi_scaler_intr.c
+ * @addtogroup v_multi_scaler_v1_0
+ * @{
+ *
+ * The functions in this file provides interrupt handler and associated
+ * functions that are consumed by layer-2
+ *
+ ******************************************************************************/
+
+/***************************** Include Files *********************************/
+#include "xil_printf.h"
+#include "xv_multi_scaler_hw.h"
+#include "xv_multi_scaler.h"
+
+/*****************************************************************************/
+/**
+ **
+ ** This function installs an asynchronous callback function:
+ **
+ ** @param    InstancePtr is a pointer to the MultiScaler IP instance.
+ ** @param    CallbackFunc is the address of the callback function.
+ ** @param    CallbackRef is a user data item that will be passed to the
+ **       callback function when it is invoked.
+ **
+ ** @return      None.
+ **
+ ** @note     Invoking this function for a handler that already has been
+ **       installed replaces it with the new handler.
+ **
+ ******************************************************************************/
+void XVMultiScaler_SetCallback(XV_multi_scaler *InstancePtr, void *CallbackFunc,
+	void *CallbackRef)
+{
+	/* Verify arguments. */
+	Xil_AssertVoid(InstancePtr != NULL);
+	Xil_AssertVoid(CallbackFunc != NULL);
+	Xil_AssertVoid(CallbackRef != NULL);
+
+	InstancePtr->FrameDoneCallback = (XVMultiScaler_Callback)CallbackFunc;
+	InstancePtr->CallbackRef = CallbackRef;
+}
+
+/*****************************************************************************/
+/**
+ **
+ ** This function is the interrupt handler for the MultiScaler core driver.
+ **
+ ** This handler clears the pending interrupt and determines if the source is
+ ** frame done signal. If yes, calls the registered callback function.
+ **
+ ** The application is responsible for connecting this function to the interrupt
+ ** system.
+ **
+ ** @param    InstancePtr is a pointer to the core instance that just
+ **           interrupted.
+ **
+ ** @return   None.
+ **
+ ** @note     None.
+ **
+ *****************************************************************************/
+void *XV_MultiScalerIntrHandler(void *InstancePtr)
+{
+	XV_multi_scaler *MscPtr = (XV_multi_scaler *) InstancePtr;
+
+	/* Verify arguments */
+	Xil_AssertVoid(MscPtr != NULL);
+	Xil_AssertVoid(MscPtr->IsReady == XIL_COMPONENT_IS_READY);
+
+	if (XV_multi_scaler_InterruptGetStatus(MscPtr)) {
+		XV_multi_scaler_InterruptClear(MscPtr,
+			XV_MULTI_SCALER_ISR_DONE_BIT_MASK |
+			XV_MULTI_SCALER_ISR_READY_BIT_MASK);
+		if (MscPtr->FrameDoneCallback)
+			MscPtr->FrameDoneCallback(MscPtr);
+	}
+}
+/** @} */
