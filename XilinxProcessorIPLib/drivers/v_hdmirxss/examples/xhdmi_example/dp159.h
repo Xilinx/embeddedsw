7a5eaf4a676671aecfc1be4dca146c9b68f515b3
Eric Bong <nyong.nyiew.bong@xilinx.com>
2018-08-30 11:25:03 +0800

    v_hdmirxss: Updated applications and driver
    Signed-off-by: Eric Bong <nyongnyi@xilinx.com>
    Acked-by: Yunhai Qiao <yunhaiq@xilinx.com>
    
    Acked-by: Yunhai Qiao <yunhaiq@xilinx.com>

--- XilinxProcessorIPLib/drivers/v_hdmirxss/examples/xhdmi_example/dp159.h
+++ XilinxProcessorIPLib/drivers/v_hdmirxss/examples/xhdmi_example/dp159.h
@@ -1,38 +1,28 @@
 /******************************************************************************
-*
-* Copyright (C) 2018 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
-*
+* Copyright (C) 2018 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 
 #ifndef DP159_H		 /* prevent circular inclusions */
 #define DP159_H		 /* by using protection macros */
 
 #include "xil_types.h"
+#include "xparameters.h"
+#ifndef versal
 #include "xvphy.h"
+#else
+#include "xhdmiphy1.h"
+#endif
 
 // Function prototypes
+#ifndef versal
 u32 i2c_dp159(XVphy *VphyPtr, u8 QuadId, u64 TxLineRate);
+#else
+u32 i2c_dp159(XHdmiphy1 *Hdmiphy1Ptr, u8 QuadId, u64 TxLineRate);
+#endif
 u32 i2c_dp159_write(u8 dev, u8 addr, u8 dat);
+u8 i2c_dp159_read(u8 dev, u8 addr);
 void i2c_dp159_dump(void);
 
 #endif
