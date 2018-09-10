f3bb34ea80dc5faaa23cfd9f98b65712da0faa2c
Mirela Simonovic <mirela.simonovic@aggios.com>
2018-09-10 17:02:00 +0530

    xilpm: Add clock models and function helpers in pm_clock.h/c
    Function calls exposed by the xilpm library to the users have to
    be mapped to function calls that are passed to the PMU-FW. This
    is required because the arguments do not match (PMU-FW/system-level
    interface was driven by the linux clock driver implementation rather
    than the EEMI API spec).
    The mapping has to be done with respect to:
    1) Multiplexers (set/get parent) - PMU-FW receives mux select values
     as arguments instead parent clock IDs. Mux select values are hardware
     implementation details that EEMI should abstract. Therefore, the xilpm
     library indeed receives parent clock ID and has to map that ID to a
     select value for a particular clock. To do that, some basic clock models
     are required in xilpm. These basic clock models should contain mapping of
     clock parent IDs to mux select values for each clock mux.
    2) Dividers (set/get divider) - PMU-FW interface receives divider ID as
     the argument (DIV0 or DIV1) and the divider value. xilpm interface
     receives the effective divider value that needs to be mapped onto 2x
     physical dividers (DIV0 and DIV1). To implement the mapping, the models
     have to represent which clocks have DIV0/1.
    
    Clocks that do not have mux or divider should not be modeled.
    The respective clock helper functions that implement the mapping
    described above are implemented and added in pm_clock.h/c.
    
    Signed-off-by: Mirela Simonovic <mirela.simonovic@aggios.com>
    
    Acked-for-series: Jolly Shah <jollys@xilinx.com>

---
+++ lib/sw_services/xilpm/src/common/pm_clock.h
@@ -0,0 +1,58 @@
+/******************************************************************************
+*
+* Copyright (C) 2015-2018 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+
+/*****************************************************************************/
+/**
+ * @file pm_clock.h
+ *
+ * PM Definitions of clocks - for xilpm internal purposes only
+ *****************************************************************************/
+
+#ifndef PM_CLOCKS_H_
+#define PM_CLOCKS_H_
+
+#include <xil_types.h>
+#include <xstatus.h>
+#include "pm_defs.h"
+
+XStatus XPm_GetClockParentBySelect(const enum XPmClock clock,
+				   const u32 select,
+				   enum XPmClock* const parent);
+
+XStatus XPm_GetSelectByClockParent(const enum XPmClock clock,
+				   const enum XPmClock parent,
+				   u32* const select);
+
+u8 XPm_GetClockDivType(const enum XPmClock clock);
+
+u8 XPm_MapDivider(const enum XPmClock clock,
+		  const u32 div,
+		  u32* const div0,
+		  u32* const div1);
+
+#endif
