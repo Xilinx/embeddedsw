3d3af22f4044d40bd8b717d1a59f24ad7be4593a
eddydebaere <debaere.eddy@gmail.com>
2018-09-11 09:00:13 +0200

    Update xavbuf_clk.h
    solved linking problems in standaone C++ projects

--- XilinxProcessorIPLib/drivers/avbuf/src/xavbuf_clk.h
+++ XilinxProcessorIPLib/drivers/avbuf/src/xavbuf_clk.h
@@ -58,6 +58,10 @@
 #include "xstatus.h"
 #include "sleep.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /****************************** Type Definitions ******************************/
 /**
  * This enum enumerates various PLL
@@ -94,4 +98,9 @@ typedef struct {
 /**************************** Function Prototypes *****************************/
 int XAVBuf_SetPixelClock(u64 FreqHz);
 int XAVBuf_SetAudioClock(u64 FreqHz);
+
+#ifdef __cplusplus
+}
+#endif
+	
 #endif /* XAVBUF_CLK_H_ */
