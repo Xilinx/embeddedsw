8765e9f40c7a0caad51220eb3ab00584cdaf6114
Manish Narani <manish.narani@xilinx.com>
2018-09-11 22:15:07 +0530

    xilffs: Add Word Access support in latest FatFS source
    The latest FatFS source does not have support for word aligned
    read/write accesses to memory. Word Access support is required in SD
    Boot while FSBL loads PMUFW from SD Card to PMU RAM. In PMU RAM,
    Accesses by the external masters should be 32-bit wide and word-aligned.
    
    Signed-off-by: Manish Narani <manish.narani@xilinx.com>
    
    Acked-for-series: Srinivas Goud <sgoud@xilinx.com>

--- lib/sw_services/xilffs/src/include/ffconf.h
+++ lib/sw_services/xilffs/src/include/ffconf.h
@@ -341,6 +341,33 @@ extern "C" {
 
 /* #include <windows.h>	// O/S definitions  */
 
+#ifdef FILE_SYSTEM_WORD_ACCESS
+#define FF_WORD_ACCESS	1
+#else
+#define FF_WORD_ACCESS	0
+#endif
+/* The FF_WORD_ACCESS option is an only platform dependent option. It defines
+/  which access method is used to the word data on the FAT volume.
+/
+/   0: Byte-by-byte access. Always compatible with all platforms.
+/   1: Word access. Do not choose this unless under both the following conditions.
+/
+/  * Address misaligned memory access is always allowed for ALL instructions.
+/  * Byte order on the memory is little-endian.
+/
+/  If it is the case, FF_WORD_ACCESS can also be set to 1 to improve performance and
+/  reduce code size. Following table shows an example of some processor types.
+/
+/   ARM7TDMI    0           ColdFire    0           V850E       0
+/   Cortex-M3   0           Z80         0/1         V850ES      0/1
+/   Cortex-M0   0           RX600(LE)   0/1         TLCS-870    0/1
+/   AVR         0/1         RX600(BE)   0           TLCS-900    0/1
+/   AVR32       0           RL78        0           R32C        0
+/   PIC18       0/1         SH-2        0           M16C        0/1
+/   PIC24       0           H8S         0           MSP430      0
+/   PIC32       0           H8/300H     0           x86         0/1
+*/
+
 #ifdef __cplusplus
 }
 #endif
