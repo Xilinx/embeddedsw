9dbea42995aa1559c3f48aeefff39eb95ef39a2b
Harini Katakam <harini.katakam@xilinx.com>
2018-09-14 17:00:31 +0530

    lwip: Update copyright content and years
    This patch fixes the following:
    - Copyright text for Xilinx files in lwip adapter
    - Copyright years for all source and example files
    - Extra spaces and typos in older copyright text in a few places
    - Adds missing copyright for autogenerated lwipopts file through tcl
    
    Signed-off-by: Harini Katakam <harini.katakam@xilinx.com>
    Acked-by: Ajay Yugalkishore Pandey <APANDEY@xilinx.com>
    
    Acked-by: Ajay Yugalkishore Pandey <APANDEY@xilinx.com>

--- ThirdParty/sw_services/lwip202/data/lwip202.tcl
+++ ThirdParty/sw_services/lwip202/data/lwip202.tcl
@@ -1,3 +1,31 @@
+###############################################################################
+#
+# Copyright (C) 2007 - 2018 Xilinx, Inc.  All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# XILINX BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+# OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Except as contained in this notice, the name of the Xilinx shall not be used
+# in advertising or otherwise to promote the sale, use or other dealings in
+# this Software without prior written authorization from Xilinx.
+#
+###############################################################################
+
 set use_axieth_on_zynq 0
 set use_emaclite_on_zynq 0
 
@@ -414,6 +442,38 @@ proc generate_lwip_opts {libhandle} {
 	file delete $lwipopts_file
 	set lwipopts_fd [open $lwipopts_file w]
 
+	puts $lwipopts_fd "/*"
+	puts $lwipopts_fd " * Copyright (c) 2001, 2002 Swedish Institute of Computer Science."
+	puts $lwipopts_fd " * All rights reserved."
+	puts $lwipopts_fd " *"
+	puts $lwipopts_fd " * Redistribution and use in source and binary forms, with or without modification,"
+	puts $lwipopts_fd " * are permitted provided that the following conditions are met:"
+	puts $lwipopts_fd " *"
+	puts $lwipopts_fd " * 1. Redistributions of source code must retain the above copyright notice,"
+	puts $lwipopts_fd " *    this list of conditions and the following disclaimer."
+	puts $lwipopts_fd " * 2. Redistributions in binary form must reproduce the above copyright notice,"
+	puts $lwipopts_fd " *    this list of conditions and the following disclaimer in the documentation"
+	puts $lwipopts_fd " *    and/or other materials provided with the distribution."
+	puts $lwipopts_fd " * 3. The name of the author may not be used to endorse or promote products"
+	puts $lwipopts_fd " *    derived from this software without specific prior written permission."
+	puts $lwipopts_fd " *"
+	puts $lwipopts_fd " * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED"
+	puts $lwipopts_fd " * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF"
+	puts $lwipopts_fd " * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT"
+	puts $lwipopts_fd " * SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,"
+	puts $lwipopts_fd " * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT"
+	puts $lwipopts_fd " * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS"
+	puts $lwipopts_fd " * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN"
+	puts $lwipopts_fd " * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING"
+	puts $lwipopts_fd " * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY"
+	puts $lwipopts_fd " * OF SUCH DAMAGE."
+	puts $lwipopts_fd " *"
+	puts $lwipopts_fd " * This file is part of the lwIP TCP/IP stack."
+	puts $lwipopts_fd " *"
+	puts $lwipopts_fd " * Author: Adam Dunkels <adam@sics.se>"
+	puts $lwipopts_fd " *"
+	puts $lwipopts_fd " */"
+	xprint_generated_header $lwipopts_fd "LWIP Configuration parameters"
 	puts $lwipopts_fd "\#ifndef __LWIPOPTS_H_"
 	puts $lwipopts_fd "\#define __LWIPOPTS_H_"
 	puts $lwipopts_fd ""
@@ -1350,6 +1410,7 @@ proc generate_adapterconfig_include {libhandle} {
 	set adapterconfig "src/contrib/ports/xilinx/include/xlwipconfig.h"
 	file delete $adapterconfig
 	set fd [open $adapterconfig w]
+	xprint_generated_header $fd "LWIP adapter configuration"
 
 	puts $fd "\#ifndef __XLWIPCONFIG_H_"
 	puts $fd "\#define __XLWIPCONFIG_H_\n\n"
--- ThirdParty/sw_services/lwip202/examples/lwip_example_igmp_app.c
+++ ThirdParty/sw_services/lwip202/examples/lwip_example_igmp_app.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- *
-* Copyright (C) 2017 Xilinx, Inc.  All rights reserved.
- *
+*
+* Copyright (C) 2017 - 2018 Xilinx, Inc.  All rights reserved.
+*
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
 * in the Software without restriction, including without limitation the rights
@@ -15,7 +15,7 @@
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* XILINX BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
 * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
 * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
