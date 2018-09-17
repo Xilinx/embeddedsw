f5362d001af888556df1b272a5f457aa04a15e0d
Venkateshwar Rao G <venkateshwar.rao.gannavarapu@xilinx.com>
2018-09-17 15:15:45 +0530

    Adding applications to display port 1.4 receiver subsystem
    This patch removed the display port 1.2 specific applications
    and adds the following applications to display port 1.4 rx subsystem
    - VCU118-RX : RX-only application on  VCU118 platform
    - ZCU102-PT : Framebuffer based pass through application on ZCU102
    - ZCU102-PT-MST: Framebuffer based pass through application with
    MST mode on ZCU102 platform
    - ZCU102-PT-HDCP: Framebuffer based pass through application with
    HDCP support on ZCU102 platform.
    
    Signed-off-by: Venkateshwar Rao Gannavarapu <venkateshwar.rao.gannavarapu@xilinx.com>
    
    Acked-by : Kapil Usgaonkar <kapilu@xilinx.com>

--- XilinxProcessorIPLib/drivers/dp14rxss/examples/src/vcu118_dp14_rx/xdprxss_vcu118_dp14_rx.c
+++ XilinxProcessorIPLib/drivers/dp14rxss/examples/src/vcu118_dp14_rx/xdprxss_vcu118_dp14_rx.c
@@ -15,14 +15,12 @@
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* XILINX BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
-* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
 *
-* Except as contained in this notice, the name of the Xilinx shall not be used
-* in advertising or otherwise to promote the sale, use or other dealings in
-* this Software without prior written authorization from Xilinx.
+*
 *
 ******************************************************************************/
 /*****************************************************************************/
@@ -1355,11 +1353,6 @@ void DpRxSs_LinkBandwidthHandler(void *InstancePtr)
 			    ONBOARD_REF_CLK, ONBOARD_REF_CLK,
 			    XVPHY_PLL_TYPE_QPLL1, XVPHY_PLL_TYPE_CPLL);
 	XVphy_ClkInitialize(&VPhyInst, 0, XVPHY_CHANNEL_ID_CHA, XVPHY_DIR_RX);
-
-	/*Overriding MC programming to work in DP mode always:
-	 * Paranoid operation to ensure MC is in good state*/
-        Status = XDpRxSs_MCDP6000_SetRegister(XPAR_IIC_0_BASEADDR, I2C_MCDP6000_ADDR,
-	                                                      0x0504, 0x0000705E);
 
 }
 
