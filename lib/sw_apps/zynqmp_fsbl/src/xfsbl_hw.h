f12baee2d844d711d211ebc925f637858996b145
Tejas Patel <tejas.patel@xilinx.com>
2018-09-17 06:11:54 -0700

    zynqmp_fsbl: Mark RPU core as usable
    Mark RPU core as usable if RPU application is loaded in SD boot
    mode. In JTAG boot mode FSBL don't know whether RPU application
    is going to be loaded or not. So, mark RPU cores as usable.
    
    Signed-off-by: Tejas Patel <tejas.patel@xilinx.com>
    
    Acked-for-series: Will Wong <WILLW@xilinx.com>

--- lib/sw_apps/zynqmp_fsbl/src/xfsbl_hw.h
+++ lib/sw_apps/zynqmp_fsbl/src/xfsbl_hw.h
@@ -339,6 +339,7 @@ extern "C" {
 #define PMU_GLOBAL_GLOB_GEN_STORAGE4 	( ( PMU_GLOBAL_BASEADDR ) + 0X40U )
 #define PMU_GLOBAL_GLOB_GEN_STORAGE1    ( ( PMU_GLOBAL_BASEADDR ) + 0X34U )
 #define PMU_GLOBAL_GLOB_GEN_STORAGE2    ( ( PMU_GLOBAL_BASEADDR ) + 0X38U )
+#define PMU_GLOBAL_GLOB_GEN_STORAGE7    ( ( PMU_GLOBAL_BASEADDR ) + 0X4CU )
 
 /**
  * Register: PMU_GLOBAL_PERS_GLOB_GEN_STORAGE4
@@ -783,6 +784,15 @@ extern "C" {
  */
 #define XFSBL_R5_0				(0x1U)
 #define XFSBL_R5_L				(0x2U)
+
+/**
+ * To indicate usage of RPU cores to PMU, PMU_GLOBAL_GLOB_GEN_STORAGE7 is used
+ */
+#define XFSBL_R5_USAGE_STATUS_REG		(PMU_GLOBAL_GLOB_GEN_STORAGE7)
+/* Bit 0 of rpu uasge status register is used for R50 status */
+#define XFSBL_R5_0_STATUS_MASK			(1U << 0)
+/* Bit 1 of rpu usage status register is used for R51 status */
+#define XFSBL_R5_1_STATUS_MASK			(1U << 1)
 
 /**
  * TCM address for R5
