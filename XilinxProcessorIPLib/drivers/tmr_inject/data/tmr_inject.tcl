b9d8f931410b4c065f0ab0758f9916525e32e4e7
Mubin Sayyed <mubin.usman.sayyed@xilinx.com>
2018-10-26 19:38:06 +0530

    tmr_inject: Add support for 64 bit fault address
    This patch exports "C_INJECT_LMB_AWIDTH" parameter
    to driver config structure, through LMBAddrWidth
    variable.
    Fault address will be written to newly introduced
    "EAIR" register if C_INJECT_LMB_AWIDTH is greater
    than 32, else it will be written to the "AIR"
    register.
    
    Signed-off-by: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>
    
    Acked-for-series: Anirudha Sarangi <anirudh@xilinx.com>

--- XilinxProcessorIPLib/drivers/tmr_inject/data/tmr_inject.tcl
+++ XilinxProcessorIPLib/drivers/tmr_inject/data/tmr_inject.tcl
@@ -38,11 +38,11 @@
 proc generate {drv_handle} {
     ::hsi::utils::define_include_file $drv_handle "xparameters.h" \
         "XTMR_Inject" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" \
-        "C_MASK" "C_MAGIC" "C_CPU_ID"
+        "C_MASK" "C_MAGIC" "C_CPU_ID" "C_INJECT_LMB_AWIDTH"
     ::hsi::utils::define_config_file  $drv_handle "xtmr_inject_g.c" \
-        "XTMR_Inject" "DEVICE_ID" "C_BASEADDR"  "C_MAGIC" "C_CPU_ID"
+        "XTMR_Inject" "DEVICE_ID" "C_BASEADDR"  "C_MAGIC" "C_CPU_ID" "C_INJECT_LMB_AWIDTH"
 
     ::hsi::utils::define_canonical_xpars $drv_handle "xparameters.h" \
         "TMR_Inject" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" "C_MASK" \
-        "C_MAGIC" "C_CPU_ID"
+        "C_MAGIC" "C_CPU_ID" "C_INJECT_LMB_AWIDTH"
 }
