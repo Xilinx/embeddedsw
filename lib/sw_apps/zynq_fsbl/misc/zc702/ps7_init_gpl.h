461f8f6a629fc34cd7142fdfdca6064cdbea34c6
b.vikram@xilinx.com <b.vikram@xilinx.com>
2018-11-02 17:16:37 +0530

    sw_apps :zynq_fsbl: Changes for zc702 in misc folder for Zynq versionless release
    This patch contains changes for zc702 board in misc folder for 2018.3 Zynq versionless release
    
    Signed-off-by: Vikram Sreenivasa Batchali <bvikram@xilinx.com>
    
    Acked-for-series: Krishna Chaitanya Patakamuri <krishna.chaitanya.patakamuri@xilinx.com>

--- lib/sw_apps/zynq_fsbl/misc/zc702/ps7_init_gpl.h
+++ lib/sw_apps/zynq_fsbl/misc/zc702/ps7_init_gpl.h
@@ -1,25 +1,26 @@
 /******************************************************************************
 *
-* Copyright (C) 2012 - 2014  Xilinx Inc.
+* Copyright (C) 2018 Xilinx, Inc.  All rights reserved.
 *
-*    This program is free software; you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation; either version 2 of the License, or
-*    (at your option) any later version.
+*  This program is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
 *
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
 *
-*    You should have received a copy of the GNU General Public License 
-*    along   with this program. If not, see <http://www.gnu.org/licenses/>.
+*  You should have received a copy of the GNU General Public License along
+*  with this program; if not, see <http://www.gnu.org/licenses/>
+*
 *
 ******************************************************************************/
 /****************************************************************************/
 /**
 *
-* @file ps7_init.h
+* @file ps7_init_gpl.h
 *
 * This file can be included in FSBL code
 * to get prototype of ps7_init() function
