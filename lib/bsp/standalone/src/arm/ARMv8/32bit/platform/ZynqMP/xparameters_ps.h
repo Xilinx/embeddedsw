b03cac9cfcbe028622247aaf563687e83c46b020
Siva Addepalli <saddepal@xilinx.com>
2018-11-26 05:06:23 -0500

    Updated BSP, freertos to add support for versal
    Signed-off-by: Siva Addepalli <saddepal@xilinx.com>

--- lib/bsp/standalone/src/arm/ARMv8/32bit/platform/ZynqMP/xparameters_ps.h
+++ lib/bsp/standalone/src/arm/ARMv8/32bit/platform/ZynqMP/xparameters_ps.h
@@ -1,8 +1,30 @@
 /******************************************************************************
-* Copyright (c) 2015 - 2020 Xilinx, Inc.  All rights reserved.
-* SPDX-License-Identifier: MIT
+*
+* Copyright (C) 2015 - 2017 Xilinx, Inc. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
 ******************************************************************************/
-
 /*****************************************************************************/
 /**
 * @file xparameters_ps.h
@@ -146,7 +168,7 @@ extern "C" {
 
 /*
  * This block contains constant declarations for the peripherals
- * within the hardblock. These have been put for backwards compatibility
+ * within the hardblock. These have been put for backwards compatibilty
  */
 
 
@@ -284,7 +306,7 @@ extern "C" {
 #define XPAR_XADCPS_0_BASEADDR	  (0xF8007000U)
 #define XPAR_XADCPS_INT_ID		XPS_SYSMON_INT_ID
 
-/* For backwards compatibility */
+/* For backwards compatibilty */
 #define XPAR_XUARTPS_0_CLOCK_HZ		XPAR_XUARTPS_0_UART_CLK_FREQ_HZ
 #define XPAR_XUARTPS_1_CLOCK_HZ		XPAR_XUARTPS_1_UART_CLK_FREQ_HZ
 #define XPAR_XTTCPS_0_CLOCK_HZ		XPAR_XTTCPS_0_TTC_CLK_FREQ_HZ
