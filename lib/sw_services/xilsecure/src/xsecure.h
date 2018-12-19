5f8e8806edb63cf46ddf506a8ba872f1e9c3dc4f
Archanao <archana.obannagari@xilinx.com>
2018-12-19 14:02:30 -0500

    Xilsecure: MISRA-C: Fix MISRA-C violations in xsecure.c
    Fix MISRA-C violations in xsecure.c and xsecure.h
    
    Signed-off-by: Archana Obannagari <archanao@xilinx.com>
    
    Acked-for-series: Durga Challa <vnsldurg@xilinx.com>

--- lib/sw_services/xilsecure/src/xsecure.h
+++ lib/sw_services/xilsecure/src/xsecure.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2017 - 18 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2017 - 2018 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -48,6 +48,7 @@
 *       ka   04/18/18 Added support for Zeroization of the memory in case of
 *                     Gcm-Tag mismatch
 * 3.2   ka   08/03/18 Added XSecure_Aes Api's to encrypt or decrypt data-blobs.
+* 4.0	arc  18/12/18 Fixed MISRA-C violation.
 * </pre>
 *
 * @note
@@ -66,69 +67,69 @@
 
 /************************** Constant Definitions *****************************/
 
-#define XSECURE_AES		1
-#define XSECURE_RSA		2
-#define XSECURE_RSA_AES		3
+#define XSECURE_AES		1U
+#define XSECURE_RSA		2U
+#define XSECURE_RSA_AES		3U
 
-#define XSECURE_SHA3_INIT	1
-#define XSECURE_SHA3_UPDATE	2
-#define XSECURE_SHA3_FINAL	4
+#define XSECURE_SHA3_INIT	1U
+#define XSECURE_SHA3HASH_UPDATE	2U
+#define XSECURE_SHA3_FINAL	4U
 #define XSECURE_SHA3_MASK	(XSECURE_SHA3_INIT | \
-				XSECURE_SHA3_UPDATE | XSECURE_SHA3_FINAL)
+				XSECURE_SHA3HASH_UPDATE | XSECURE_SHA3_FINAL)
 
 #define XSECURE_SPKID_EFUSE				1U /**< SPK ID eFUSE */
 #define XSECURE_USER_EFUSE				2U /**< User eFuse */
 #define XSECURE_USER_EFUSE_MIN_VALUE	1U
 #define XSECURE_USER_EFUSE_MAX_VALUE	256U
 
-#define XSECURE_ENC		1
-#define XSECURE_DEC		0
-#define XSECURE_AES_KUP_KEY	0
+#define XSECURE_ENC		1U
+#define XSECURE_DEC		0U
+#define XSECURE_AES_KUP_KEY	0U
 
-#define XSECURE_RSA_OPERATION	1
-#define XSECURE_RSA_KEY_SELECT  2
+#define XSECURE_RSA_CORE_OPERATION	1U
+#define XSECURE_RSA_KEY_SELECT  2U
 
 #define XSECURE_MASK		(XSECURE_AES | XSECURE_RSA)
 
-#define XSECURE_KEY_STR_LEN		64	/* String length */
-#define XSECURE_IV_STR_LEN		24	/* String length */
-#define XSECURE_KEY_LEN			8
-#define XSECURE_IV_LEN			3
-#define XSECURE_GCM_TAG_LEN		128
-#define XSECURE_WORD_LEN		4
-#define XSECURE_MAX_NIBBLES		8
+#define XSECURE_KEY_STR_LEN		64U	/* String length */
+#define XSECURE_IV_STR_LEN		24U	/* String length */
+#define XSECURE_KEY_LEN			8U
+#define XSECURE_IV_LEN			3U
+#define XSECURE_GCM_TAG_LEN		128U
+#define XSECURE_WORD_LEN		4U
+#define XSECURE_MAX_NIBBLES		8U
 
-#define XSECURE_WORD_SHIFT		32
+#define XSECURE_WORD_SHIFT		32U
 
 #define XSECURE_ARRAY_LENGTH(array) (sizeof((array))/sizeof((array)[0]))
 
-#define XSECURE_ERROR_CSUDMA_INIT_FAIL	0x1
-#define XSECURE_STRING_INVALID_ERROR	0x2
-#define XSECURE_INVALID_FLAG		0x3
-#define XSECURE_ISNOT_SECURE_IMAGE	0x4
-#define XSECURE_SHA3_INIT_FAIL		0x5
-#define XSECURE_SIZE_ERR		0x6
+#define XSECURE_ERROR_CSUDMA_INIT_FAIL	0x1U
+#define XSECURE_STRING_INVALID_ERROR	0x2U
+#define XSECURE_INVALID_FLAG		0x3U
+#define XSECURE_ISNOT_SECURE_IMAGE	0x4U
+#define XSECURE_SHA3_INIT_FAIL		0x5U
+#define XSECURE_SIZE_ERR		0x6U
 
-#define XSECURE_SEL_ERR			0x7
-#define XSECURE_REVOKE_ERR		0x8
-#define XSECURE_VERIFY_ERR		0x9
-#define XSECURE_RSA_INIT_ERR		0xA
-#define XSECURE_RSA_ENCRYPT_ERR		0xB
-#define XSECURE_SHA3_PADSELECT_ERR	0xC
-#define XSECURE_IMAGE_WITH_MUL_PARTITIONS 0xD
-#define XSECURE_AUTH_ISCOMPULSORY	0xE
-#define XSECURE_ENC_ISCOMPULSORY	0xF
+#define XSECURE_SEL_ERR			0x7U
+#define XSECURE_REVOKE_ERR		0x8U
+#define XSECURE_VERIFY_ERR		0x9U
+#define XSECURE_RSA_INIT_ERR		0xAU
+#define XSECURE_RSA_ENCRYPT_ERR		0xBU
+#define XSECURE_SHA3_PADSELECT_ERR	0xCU
+#define XSECURE_IMAGE_WITH_MUL_PARTITIONS 0xDU
+#define XSECURE_AUTH_ISCOMPULSORY	0xEU
+#define XSECURE_ENC_ISCOMPULSORY	0xFU
 
-#define XSECURE_BHDR_AUTH_NOT_ALLOWED	0x10
-#define	XSECURE_ONLY_BHDR_AUTH_ALLOWED	0x11
-#define XSECURE_HDR_NOAUTH_PART_AUTH	0x12
-#define XSECURE_DEC_WRONG_KEY_SOURCE	0x13
-#define XSECURE_KUP_KEY_NOT_PROVIDED	0x14
-#define XSECURE_KUP_KEY_NOT_REQUIRED	0x15
-#define XSECURE_AES_GCM_TAG_NOT_MATCH	0x16
-#define XSECURE_INVALID_EFUSE_SELECT 	0x17
-#define XSECURE_OUT_OF_RANGE_USER_EFUSE_ERROR	0x18
-#define XSECURE_AES_ZEROIZATION_ERR		0x19
+#define XSECURE_BHDR_AUTH_NOT_ALLOWED	0x10U
+#define	XSECURE_ONLY_BHDR_AUTH_ALLOWED	0x11U
+#define XSECURE_HDR_NOAUTH_PART_AUTH	0x12U
+#define XSECURE_DEC_WRONG_KEY_SOURCE	0x13U
+#define XSECURE_KUP_KEY_NOT_PROVIDED	0x14U
+#define XSECURE_KUP_KEY_NOT_REQUIRED	0x15U
+#define XSECURE_AES_GCM_TAG_NOT_MATCH	0x16U
+#define XSECURE_INVALID_EFUSE_SELECT 	0x17U
+#define XSECURE_OUT_OF_RANGE_USER_EFUSE_ERROR	0x18U
+#define XSECURE_AES_ZEROIZATION_ERR		0x19U
 #define XSECURE_AES_KEY_CLEAR_ERROR		XSECURE_CSU_AES_KEY_CLEAR_ERROR
 						/**< 0x20 */
 #define XSECURE_AES_KEYCLR_AND_GCMTAG_NOTMATCH	(XSECURE_AES_KEY_CLEAR_ERROR \
@@ -138,29 +139,29 @@
 					| XSECURE_CSU_AES_ZEROIZATION_ERROR)
 						/**< 0x24*/
 
-#define XSECURE_AUTH_NOT_ENABLED 	0xFF
+#define XSECURE_AUTH_NOT_ENABLED 	0xFFU
 
-#define XSECURE_PPK_ERR			0x100
-#define XSECURE_SPK_ERR			0x200
-#define XSECURE_AUTH_FAILURE		0x300
-#define XSECURE_AES_DECRYPTION_FAILURE	0x400
+#define XSECURE_PPK_ERR			0x100U
+#define XSECURE_SPK_ERR			0x200U
+#define XSECURE_AUTH_FAILURE		0x300U
+#define XSECURE_AES_DECRYPTION_FAILURE	0x400U
 
-#define XSECURE_BOOT_HDR_FAIL		0x1000
-#define XSECURE_IMG_HDR_FAIL		0x2000
-#define XSECURE_PARTITION_FAIL		0x3000
+#define XSECURE_BOOT_HDR_FAIL		0x1000U
+#define XSECURE_IMG_HDR_FAIL		0x2000U
+#define XSECURE_PARTITION_FAIL		0x3000U
 
-#define XSECURE_CSUDMA_DEVICEID		0
+#define XSECURE_CSUDMA_DEVICEID		0U
 
-#define XSECURE_MOD_LEN			512
+#define XSECURE_MOD_LEN			512U
 
 #define XSECURE_PH_ATTR_AUTH_ENABLE	0x8000U
 #define XSECURE_PH_ATTR_ENC_ENABLE	0x0080U
 
 #define XSECURE_AH_ATTR_PPK_SEL_MASK   	0x30000U
-#define XSECURE_AH_ATTR_PPK_SEL_SHIFT	16
+#define XSECURE_AH_ATTR_PPK_SEL_SHIFT	16U
 
 #define XSECURE_AH_ATTR_SPK_ID_FUSE_SEL_MASK 0xC0000U
-#define XSECURE_AH_ATTR_SPK_ID_FUSE_SEL_SHIFT 18
+#define XSECURE_AH_ATTR_SPKID_FUSESEL_SHIFT 18U
 
 #define XSECURE_AC_SPKID_OFFSET		0x04U
 #define XSECURE_AC_PPK_OFFSET		0x40U
@@ -169,8 +170,8 @@
 
 #define XSECURE_KEY_SIZE		(512U+512U+64U)
 
-#define XSECURE_ENABLED			0xFF
-#define XSECURE_NOTENABLED		0
+#define XSECURE_ENABLED			0xFFU
+#define XSECURE_NOTENABLED		0U
 
 
 #define XSECURE_EFUSE_BASEADDR		(0XFFCC0000U)
@@ -218,12 +219,12 @@
 					+ XSECURE_BHDR_SIG_SIZE 	\
 					+ XSECURE_PARTITION_SIG_SIZE)
 
-#define XSECURE_AUTH_CERT_BHDRSIG_OFFSET	0xAC0
-#define XSECURE_AUTH_CERT_PARTSIG_OFFSET	0xCC0
+#define XSECURE_AUTH_CERT_BHDRSIG_OFFSET	0xAC0U
+#define XSECURE_AUTH_CERT_PARTSIG_OFFSET	0xCC0U
 
 #define XSECURE_BOOT_HDR_MIN_SIZE	(0x000008B8U)
 #define XSECURE_BOOT_HDR_MAX_SIZE	(XSECURE_BOOT_HDR_MIN_SIZE + \
-					(0x00000182U * 4))
+					(0x00000182U * 4U))
 			/**< When boot header contains PUF helper data */
 #define XSECURE_BUFFER_SIZE		(0x00001080U)
 #define XSECURE_IV_SIZE			(4U)
@@ -322,17 +323,17 @@ u32 Key[XSECURE_KEY_LEN];
 
 /************************** Function Prototypes ******************************/
 
-u32 XSecure_RsaAes(u32 SrcAddrHigh, u32 SrcAddrLow, u32 WrSize, u32 flags);
+u32 XSecure_RsaAes(u32 SrcAddrHigh, u32 SrcAddrLow, u32 WrSize, u32 Flags);
 u32 XSecure_Sha3Hash(u32 SrcAddrHigh, u32 SrcAddrLow, u32 SrcSize, u32 Flags);
 u32 XSecure_RsaCore(u32 SrcAddrHigh, u32 SrcAddrLow, u32 SrcSize, u32 Flags);
-u32 XSecure_DataAuth(u8 *Signature, XSecure_RsaKey *Key, u8 *Hash);
+u32 XSecure_DataAuth(u8 *Signature, XSecure_RsaKey *KeyInst, u8 *Hash);
 u32 XSecure_AesOperation(u32 AddrHigh, u32 AddrLow);
 
 /* Memory copy */
 u32 XSecure_MemCopy(void * DestPtr, void * SrcPtr, u32 Size);
-
+u32 XSecure_CsuDmaInit(void);
 /* Keys verification */
-u32 XSecure_VerifySpk(u8 *Acptr, u32 EfuseRsaenable);
+u32 XSecure_VerifySpk(u8 *AcPtr, u32 EfuseRsaenable);
 u32 XSecure_PpkVerify(XCsuDma *CsuDmaInstPtr, u8 *AuthCert);
 u32 XSecure_SpkAuthentication(XCsuDma *CsuDmaInstPtr, u8 *AuthCert, u8 *Ppk);
 u32 XSecure_SpkRevokeCheck(u8 *AuthCert);
@@ -343,8 +344,8 @@ u32 XSecure_PartitionAuthentication(XCsuDma *CsuDmaInstPtr, u8 *Data,
 u32 XSecure_AuthenticationHeaders(u8 *StartAddr, XSecure_ImageInfo *ImageInfo);
 
 /* eFUSE read functions */
-u32 XSecure_IsRsaEnabled();
-u32 XSecure_IsEncOnlyEnabled();
+u32 XSecure_IsRsaEnabled(void);
+u32 XSecure_IsEncOnlyEnabled(void);
 
 /* For single partition secure image */
 u32 XSecure_SecureImage(u32 AddrHigh, u32 AddrLow,
