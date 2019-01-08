df04a394ad5ef3a101474c9b343816839ee30254
makula <mounika.grace.akula@xilinx.com>
2019-01-08 17:58:59 +0530

    sw_apps: zynqmp_pmufw: Make CSU DMA functionality common
    This patch makes the CSU DMA initialization functionality common for
    both DDR SR feature and APU-only restart feature.
    
    Signed-off-by: Mounika Grace Akula <makula@xilinx.com>
    
    Acked-for-series: Rajan Vaja <rajan.vaja@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/pm_csudma.h
+++ lib/sw_apps/zynqmp_pmufw/src/pm_csudma.h
@@ -1,26 +1,8 @@
 /*
- * Copyright (C) 2018 - 2019 Xilinx, Inc.  All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- *
+* Copyright (c) 2018 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
  */
+
 
 #ifndef PM_CSUDMA_H_
 #define PM_CSUDMA_H_
@@ -39,6 +21,12 @@ extern "C" {
  */
 /* CSU DMA device Id */
 #define CSUDMA_DEVICE_ID	XPAR_XCSUDMA_0_DEVICE_ID
+/* CSU DMA Source control */
+#define CSUDMA_SRC_CTRL		(XPAR_XCSUDMA_0_BASEADDR + 0xCU)
+/* CSU DMA Destination control */
+#define CSUDMA_DEST_CTRL	(XPAR_XCSUDMA_0_BASEADDR + 0x80CU)
+/*CSU DMA APB error response mask */
+#define CSUDMA_APB_ERR_RESP_MASK	0x01000000U
 /* CSU SSS_CFG Offset */
 #define CSU_SSS_CONFIG_OFFSET	0x00000008U
 /* LOOP BACK configuration macro */
