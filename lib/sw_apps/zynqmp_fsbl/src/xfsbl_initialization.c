526b4dc3e4aaac7df59afdf5cb130436e1bb2db8
makula <mounika.grace.akula@xilinx.com>
2019-01-08 17:58:58 +0530

    sw_apps: zynqmp_fsbl: Update PMU with FSBL running status
    This patch is to update PMU with running status of FSBL and on which
    processor it is running. For this, FSBL is using bits 1 and 2 from PMU
    global general purpose register 5. Based on this status, PMU will store
    the FSBL image to DDR to support APU-only warm-restart and free-up OCM
    memory for other purposes.
    
    Signed-off-by: Mounika Grace Akula <makula@xilinx.com>
    
    Acked-for-series: Rajan Vaja <rajan.vaja@xilinx.com>

--- lib/sw_apps/zynqmp_fsbl/src/xfsbl_initialization.c
+++ lib/sw_apps/zynqmp_fsbl/src/xfsbl_initialization.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2015 - 18 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2015 - 19 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -439,6 +439,7 @@ static u32 XFsbl_ProcessorInit(XFsblPs * FsblInstancePtr)
 	u32 RegValue;
 	u32 Index=0U;
 	char DevName[PART_NAME_LEN_MAX];
+	u32 FsblProcType = XFSBL_RUNNING_STATUS;
 
 	/**
 	 * Read the cluster ID and Update the Processor ID
@@ -475,6 +476,7 @@ static u32 XFsbl_ProcessorInit(XFsblPs * FsblInstancePtr)
 		XFsbl_Printf(DEBUG_GENERAL,"Running on A53-0 ");
 		FsblInstancePtr->ProcessorID =
 				XIH_PH_ATTRB_DEST_CPU_A53_0;
+		FsblProcType |= XFSBL_RUNNING_ON_A53;
 #ifdef __aarch64__
 		/* Running on A53 64-bit */
 		XFsbl_Printf(DEBUG_GENERAL,"(64-bit) Processor");
@@ -521,6 +523,14 @@ static u32 XFsbl_ProcessorInit(XFsblPs * FsblInstancePtr)
 				"XFSBL_ERROR_UNSUPPORTED_CLUSTER_ID\n\r");
 		goto END;
 	}
+
+	/*
+	 * Update FSBL running status and running processor to PMU Global Reg5
+	 * as PMU require this during boot for APU only warm-restart feature.
+	 */
+	FsblProcType |= (XFsbl_In32(PMU_GLOBAL_GLOB_GEN_STORAGE5) &
+					~XFSBL_STATE_PROC_INFO_MASK);
+	XFsbl_Out32(PMU_GLOBAL_GLOB_GEN_STORAGE5, FsblProcType);
 
 	/* Build Device name and print it */
 	(void)XFsbl_Strcpy(DevName, "XCZU");
