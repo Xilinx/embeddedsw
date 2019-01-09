6b8a9e74c00862c3985684d5d6b591a64f7c398a
Sai Krishna Potthuri <lakshmi.sai.krishna.potthuri@xilinx.com>
2019-01-09 16:58:16 +0530

    ospipsv: Added interrupt mode support
    This patch updates the ospipsv driver
    1. Added interrupt mode support.
    2. Added unaligned byte count read.
    3. Added support for INDAC<->DAC mode switching.
    4. Removed STIG/DMA mode selection selection by the user, driver will
    take care of selecting DMA/STIG based on command.
    5. Fixed MISRAC warnings.
    
    Signed-off-by: Sai Krishna Potthuri <lakshmi.sai.krishna.potthuri@xilinx.com>

--- XilinxProcessorIPLib/drivers/ospipsv/src/xospipsv_sinit.c
+++ XilinxProcessorIPLib/drivers/ospipsv/src/xospipsv_sinit.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2018-2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@
 /**
 *
 * @file xospipsv_sinit.c
-* @addtogroup ospips_v1_0
+* @addtogroup ospipsv_v1_0
 * @{
 *
 * The implementation of the XOspiPsv component's static initialization
