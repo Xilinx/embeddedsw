c5e6b965a6003006cdb6350055bbfde8b926430b
Harini Katakam <harini.katakam@xilinx.com>
2019-01-17 14:56:03 +0530

    lwip: Update SW apps to use the new version
    Update SW apps to switch from lwip202 to lwip211.
    
    Signed-off-by: Harini Katakam <harini.katakam@xilinx.com>

--- lib/sw_apps/lwip_echo_server/data/lwip_echo_server.tcl
+++ lib/sw_apps/lwip_echo_server/data/lwip_echo_server.tcl
@@ -164,8 +164,8 @@ proc swapp_is_supported_sw {} {
     # check for stdout being set
     check_stdout_sw;
 
-    # make sure lwip202 is available
-    set librarylist [hsi::get_libs -filter "NAME==lwip202"];
+    # make sure lwip211 is available
+    set librarylist [hsi::get_libs -filter "NAME==lwip211"];
 
     if { [llength $librarylist] == 0 } {
         error "This application requires lwIP library in the Board Support Package.";
@@ -336,8 +336,8 @@ proc swapp_generate {} {
     generate_stdout_config $fid;
     puts $fid "";
 
-    set use_softeth_on_zynq [common::get_property CONFIG.use_axieth_on_zynq [hsi::get_libs lwip202]];
-    set use_ethernetlite_on_zynq [common::get_property CONFIG.use_emaclite_on_zynq [hsi::get_libs lwip202]];
+    set use_softeth_on_zynq [common::get_property CONFIG.use_axieth_on_zynq [hsi::get_libs lwip211]];
+    set use_ethernetlite_on_zynq [common::get_property CONFIG.use_emaclite_on_zynq [hsi::get_libs lwip211]];
     # figure out the emac baseaddr
     generate_emac_config $fid;
     puts $fid "";
