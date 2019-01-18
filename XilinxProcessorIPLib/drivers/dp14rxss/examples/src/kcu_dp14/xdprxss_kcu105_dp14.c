5edb078cebed005f25fdb1657c0075e7b181a421
Kapil Usgaonkar <kapil.usgaonkar@xilinx.com>
2019-01-18 17:02:24 +0530

    dp14:All the passthrough examples updated to set MISC0[0] to 0
    Signed-off-by: Kapil Usgaonkar <kapilu@xilinx.com>
    
    Acked by: Kapil Usgaonkar<kapilu@xilinx.com>

--- XilinxProcessorIPLib/drivers/dp14rxss/examples/src/kcu_dp14/xdprxss_kcu105_dp14.c
+++ XilinxProcessorIPLib/drivers/dp14rxss/examples/src/kcu_dp14/xdprxss_kcu105_dp14.c
@@ -1,30 +1,8 @@
 /*******************************************************************************
- *
- * Copyright (C) 2018 Xilinx, Inc.  All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
- * OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- * Except as contained in this notice, the name of the Xilinx shall not be used
- * in advertising or otherwise to promote the sale, use or other dealings in
- * this Software without prior written authorization from Xilinx.
- *
+* Copyright (C) 2018 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 *******************************************************************************/
+
 /******************************************************************************/
 /**
  *
@@ -3161,10 +3139,11 @@ void DpPt_TxSetMsaValuesImmediate(void *InstancePtr)
 		     XDP_TX_MAIN_STREAM_VSTART + StreamOffsetAddr[0],
 		     XDp_ReadReg(DpRxSsInst.DpPtr->Config.BaseAddr,
 				 XDP_RX_MSA_VSTART));
-	XDp_WriteReg(DpTxSsInst.DpPtr->Config.BaseAddr,
-		     XDP_TX_MAIN_STREAM_MISC0 + StreamOffsetAddr[0],
-		     XDp_ReadReg(DpRxSsInst.DpPtr->Config.BaseAddr,
-				 XDP_RX_MSA_MISC0));
+        XDp_WriteReg(DpTxSsInst.DpPtr->Config.BaseAddr,
+                     XDP_TX_MAIN_STREAM_MISC0 + StreamOffset[0],
+                    ((XDp_ReadReg(DpRxSsInst.DpPtr->Config.BaseAddr,
+                                        XDP_RX_MSA_MISC0)) & 0xFFFFFFFE));
+
 	XDp_WriteReg(DpTxSsInst.DpPtr->Config.BaseAddr,
 		     XDP_TX_MAIN_STREAM_MISC1 + StreamOffsetAddr[0],
 		     XDp_ReadReg(DpRxSsInst.DpPtr->Config.BaseAddr,
