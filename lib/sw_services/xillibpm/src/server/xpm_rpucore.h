02536c06b1aa463e1b9e9278cc1699f2eddc7170
Saeed Nowshadi <saeed.nowshadi@xilinx.com>
2019-01-18 13:28:55 -0800

    xillibpm: Implement Ioctls used to setup RPUs.
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@xilinx.com>
    
    Acked-by: Jolly Shah <jollys@xilinx.com>

--- lib/sw_services/xillibpm/src/server/xpm_rpucore.h
+++ lib/sw_services/xillibpm/src/server/xpm_rpucore.h
@@ -57,7 +57,9 @@ XStatus XPmRpuCore_Init(XPm_RpuCore *RpuCore, u32 Id, u32 Ipi, u32 *BaseAddress,
 			XPm_Power *Power, XPm_ClockNode *Clock,
 			XPm_ResetNode *Reset);
 
+void XPm_RpuGetOperMode(const u32 DeviceId, u32 *Mode);
 void XPm_RpuSetOperMode(const u32 DeviceId, const u32 Mode);
 XStatus XPm_RpuBootAddrConfig(const u32 DeviceId, const u32 BootAddr);
+XStatus XPm_RpuTcmCombConfig(const u32 DeviceId, const u32 Config);
 /** @} */
 #endif /* XPM_RPUCORE_H_ */
