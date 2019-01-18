f90f10e49355c2c08b340c7e4106f507a15416a9
Kapil Usgaonkar <kapil.usgaonkar@xilinx.com>
2019-01-18 17:02:24 +0530

    dp14:All the passthrough examples updated to set MISC0[0] to 0
    Signed-off-by: Kapil Usgaonkar <kapilu@xilinx.com>
    
    Acked by: Kapil Usgaonkar<kapilu@xilinx.com>

--- XilinxProcessorIPLib/drivers/dp14rxss/examples/src/kcu_dp14/xdprxss_kcu105_dp14.c
+++ XilinxProcessorIPLib/drivers/dp14rxss/examples/src/kcu_dp14/xdprxss_kcu105_dp14.c
@@ -3161,10 +3161,11 @@ void DpPt_TxSetMsaValuesImmediate(void *InstancePtr)
 		     XDP_TX_MAIN_STREAM_VSTART + StreamOffsetAddr[0],
 		     XDp_ReadReg(DpRxSsInst.DpPtr->Config.BaseAddr,
 				 XDP_RX_MSA_VSTART));
-	XDp_WriteReg(DpTxSsInst.DpPtr->Config.BaseAddr,
-		     XDP_TX_MAIN_STREAM_MISC0 + StreamOffsetAddr[0],
-		     XDp_ReadReg(DpRxSsInst.DpPtr->Config.BaseAddr,
-				 XDP_RX_MSA_MISC0));
+        XDp_WriteReg(DpTxSsInst.DpPtr->Config.BaseAddr,
+                     XDP_TX_MAIN_STREAM_MISC0 + StreamOffset[0],
+                    ((XDp_ReadReg(DpRxSsInst.DpPtr->Config.BaseAddr,
+                                        XDP_RX_MSA_MISC0)) & 0xFFFFFFFE));
+
 	XDp_WriteReg(DpTxSsInst.DpPtr->Config.BaseAddr,
 		     XDP_TX_MAIN_STREAM_MISC1 + StreamOffsetAddr[0],
 		     XDp_ReadReg(DpRxSsInst.DpPtr->Config.BaseAddr,
