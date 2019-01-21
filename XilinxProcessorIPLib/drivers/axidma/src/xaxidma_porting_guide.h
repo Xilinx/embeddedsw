e5b66774806837868d9270e7cba9addaa0efde64
Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
2019-01-21 17:15:12 +0530

    axidma: Update doxygen tags and copyright year
    Update doxygen tags and copyright year.
    
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    
    Acked-for-series: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

--- XilinxProcessorIPLib/drivers/axidma/src/xaxidma_porting_guide.h
+++ XilinxProcessorIPLib/drivers/axidma/src/xaxidma_porting_guide.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2010 - 2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@
 /**
 *
 * @file xaxidma_porting_guide.h
-* @addtogroup axidma_v9_8
+* @addtogroup axidma_v9_9
 * @{
 *
 * This is a guide on how to move from using the xlldma driver to use xaxidma
