16b44b07eb5c943b1568898bf5d9159081e62e28
Manish Narani <manish.narani@xilinx.com>
2019-01-22 11:02:21 +0530

    xilffs: Add support to enable use of CHMOD functionality
    Add option to enable or disable the use of CHMOD functionality in xilffs
    for changing the attributes in the file system.
    
    Signed-off-by: Manish Narani <manish.narani@xilinx.com>
    
    Acked-for-series: Srinivas Goud <sgoud@xilinx.com>

--- lib/sw_services/xilffs/src/include/ffconf.h
+++ lib/sw_services/xilffs/src/include/ffconf.h
@@ -97,7 +97,11 @@ extern "C" {
 /* This option switches f_expand function. (0:Disable or 1:Enable) */
 
 
-#define FF_USE_CHMOD	0
+#ifdef FILE_SYSTEM_USE_CHMOD
+#define FF_USE_CHMOD	1	/* 1:Enable */
+#else
+#define FF_USE_CHMOD	0	/* 0:Disable */
+#endif
 /* This option switches attribute manipulation functions, f_chmod() and f_utime().
 /  (0:Disable or 1:Enable) Also FF_FS_READONLY needs to be 0 to enable this option. */
 
