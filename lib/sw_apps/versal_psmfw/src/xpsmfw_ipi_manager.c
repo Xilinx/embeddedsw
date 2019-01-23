f5ac42ea1bf5187a7dc9772818a6ac741ffe01b7
Rajan Vaja <rajan.vaja@xilinx.com>
2019-01-23 04:59:34 -0800

    versal_psmfw: Add Support to send message/response through IPI
    Add support for sending message and response through IPI.
    
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    
    Acked-for-series: Jolly Shah <jollys@xilinx.com>

--- lib/sw_apps/versal_psmfw/src/xpsmfw_ipi_manager.c
+++ lib/sw_apps/versal_psmfw/src/xpsmfw_ipi_manager.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2018-2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -117,5 +117,101 @@ int XPsmFw_DispatchIpiHandler(u32 SrcMask)
 		}
 	}
 	return Status;
+}
+
+/****************************************************************************/
+/**
+ * @brief	Sends IPI request to the target.
+ *
+ * @param IpiMask	IPI interrupt mask of target
+ * @param Payload	API ID and call arguments to be written in
+ *			IPI buffer
+ *
+ * @return	XST_SUCCESS if successful else XST_FAILURE or an error code
+ *		or a reason code
+ *
+ * @note	None
+ *
+ ****************************************************************************/
+XStatus XPsmFw_IpiSend(u32 IpiMask, u32 *Payload)
+{
+	XStatus Status;
+
+	/* Wait until current IPI interrupt is handled by target */
+	Status = XIpiPsu_PollForAck(IpiInstPtr, IpiMask, XPSMFW_IPI_TIMEOUT);
+	if (XST_SUCCESS != Status) {
+		XPsmFw_Printf(DEBUG_PRINT_ALWAYS, "%s: ERROR: Timeout expired\n",
+			      __func__);
+		goto done;
+	}
+
+	Status = XIpiPsu_WriteMessage(IpiInstPtr, IpiMask, Payload,
+				      PAYLOAD_ARG_CNT, XIPIPSU_BUF_TYPE_MSG);
+	if (XST_SUCCESS != Status) {
+		XPsmFw_Printf(DEBUG_PRINT_ALWAYS, "%s: ERROR writing to IPI request buffer\n", __func__);
+		goto done;
+	}
+
+	Status = XIpiPsu_TriggerIpi(IpiInstPtr, IpiMask);
+
+done:
+	return Status;
+}
+
+/****************************************************************************/
+/**
+ * @brief	Sends IPI response to the target.
+ *
+ * @param IpiMask	IPI interrupt mask of target
+ * @param Payload	IPI response
+ *
+ * @return	XST_SUCCESS if successful else XST_FAILURE or an error code
+ *		or a reason code
+ *
+ * @note	None
+ *
+ ****************************************************************************/
+XStatus XPsmFw_IpiSendResponse(u32 IpiMask, u32 *Payload)
+{
+	XStatus Status;
+
+	Status = XIpiPsu_WriteMessage(IpiInstPtr, IpiMask, Payload,
+				      PAYLOAD_ARG_CNT, XIPIPSU_BUF_TYPE_RESP);
+	if (XST_SUCCESS != Status) {
+		XPsmFw_Printf(DEBUG_PRINT_ALWAYS, "%s: ERROR writing to IPI request buffer\n", __func__);
+		goto done;
+	}
+
+done:
+	return Status;
+}
+#else
+
+s32 XPsmfw_IpiManagerInit(void)
+{
+	return XST_FAILURE;
+}
+
+int XPsmFw_DispatchIpiHandler(u32 SrcMask)
+{
+	(void)SrcMask;
+
+	return XST_FAILURE;
+}
+
+XStatus XPsmFw_IpiSend(u32 IpiMask, u32 *Payload)
+{
+	(void)IpiMask;
+	(void)Payload;
+
+	return XST_FAILURE;
+}
+
+XStatus XPsmFw_IpiSendResponse(u32 IpiMask, u32 *Payload)
+{
+	(void)IpiMask;
+	(void)Payload;
+
+	return XST_FAILURE;
 }
 #endif
