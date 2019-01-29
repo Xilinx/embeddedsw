973d59ecd09ecc0969931c7b495675b8c4433697
Manish Narani <manish.narani@xilinx.com>
2019-01-29 19:17:34 +0530

    sdps: Update driver version to 3.7
    Signed-off-by: Manish Narani <manish.narani@xilinx.com>
    
    Acked-for-series: Sai Krishna Potthuri<lakshmis@xilinx.com>

--- XilinxProcessorIPLib/drivers/sdps/src/xsdps_sinit.c
+++ XilinxProcessorIPLib/drivers/sdps/src/xsdps_sinit.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2013 - 2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2013 - 2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@
 /**
 *
 * @file xsdps_sinit.c
-* @addtogroup sdps_v3_6
+* @addtogroup sdps_v3_7
 * @{
 *
 * The implementation of the XSdPs component's static initialization
