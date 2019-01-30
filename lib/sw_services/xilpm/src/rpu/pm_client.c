5a762491a430ee1c2380cd949163dc43b02f0691
Ramadevi Veggalam <rama.devi.veggalam@xilinx.com>
2019-01-30 10:10:35 +0530

    Xilpm: Fixes DU anomaly dead code in pm_client.c
    This patch fixes "DU anomaly dead code, variable value is unused on all
    paths: pwrdn_req" violation in pm_client.c
    
    Signed-off-by: Ramadevi Veggalam <rveggala@xilinx.com>
    
    Acked-for-series: Mounika Grace Akula <makula@xilinx.com>

--- lib/sw_services/xilpm/src/rpu/pm_client.c
+++ lib/sw_services/xilpm/src/rpu/pm_client.c
@@ -134,11 +134,17 @@ void XPm_ClientSuspend(const struct XPm_Master *const master)
 	/* Disable interrupts at processor level */
 	pm_disable_int();
 	/* Set powerdown request */
+<<<<<<< HEAD
 	if (NULL != master) {
 		pwrdn_req = pm_read(master->pwrctl);
 		pwrdn_req |= master->pwrdn_mask;
 		pm_write(master->pwrctl, pwrdn_req);
 	}
+=======
+	pwrdn_req = pm_read(master->pwrctl);
+	pwrdn_req |= master->pwrdn_mask;
+	pm_write(master->pwrctl, pwrdn_req);
+>>>>>>> Xilpm: Fixes DU anomaly dead code in pm_client.c
 }
 
 void XPm_ClientAbortSuspend(void)
