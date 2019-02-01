1eef68666acf0a48609ac90d62f2b7a0767153cd
Wendy Liang <wendy.liang@xilinx.com>
2019-01-31 23:28:48 -0800

    libmetal: examples: freertos: zynqmp_r5: remove dev from irq registration
    Remove metal device from IRQ registration.
    
    Signed-off-by: Wendy Liang <wendy.liang@xilinx.com>

--- lib/sw_apps/libmetal_echo_demo/src/system/freertos/zynqmp_r5/zynqmp_amp_demo/shmem_latency_demod.c
+++ lib/sw_apps/libmetal_echo_demo/src/system/freertos/zynqmp_r5/zynqmp_amp_demo/shmem_latency_demod.c
@@ -268,7 +268,7 @@ int shmem_latency_demod()
 	ipi_irq = (intptr_t)ipi_dev->irq_info;
 
 	/* Register IPI irq handler */
-	metal_irq_register(ipi_irq, ipi_irq_handler, ipi_dev, &ch);
+	metal_irq_register(ipi_irq, ipi_irq_handler, &ch);
 	metal_irq_enable(ipi_irq);
 	/* initialize remote_nkicked */
 	atomic_init(&ch.remote_nkicked, 1);
