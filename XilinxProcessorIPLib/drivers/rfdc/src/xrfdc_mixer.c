2dd29b080aa4066e2eca2cfc855e7ca71580c01f
Conall O'Griofa <conall.o'griofa@xilinx.com>
2019-02-01 11:07:32 +0000

    rfdc: Upversioning Housekeeping to 5.1
    Signed-off-by: Conall O'Griofa <conall.o'griofa@xilinx.com>
    
    Acked-for-Series: Sai Krishna Potthuri<lakshmis@xilinx.com>

--- XilinxProcessorIPLib/drivers/rfdc/src/xrfdc_mixer.c
+++ XilinxProcessorIPLib/drivers/rfdc/src/xrfdc_mixer.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2018-2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@
 /**
 *
 * @file xrfdc_mixer.c
-* @addtogroup xrfdc_v5_0
+* @addtogroup xrfdc_v5_1
 * @{
 *
 * Contains the interface functions of the Mixer Settings in XRFdc driver.
