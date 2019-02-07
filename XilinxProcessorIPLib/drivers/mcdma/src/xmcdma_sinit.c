5f77f0e5ea491d7f55cd61974c0232833137507b
Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
2019-02-07 18:25:40 +0530

    mcdma: Update copyright year and doxygen tags
    Update copyright year and doxygen tags.
    
    Signed-off-by: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>
    Acked-for-series: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

--- XilinxProcessorIPLib/drivers/mcdma/src/xmcdma_sinit.c
+++ XilinxProcessorIPLib/drivers/mcdma/src/xmcdma_sinit.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2017 - 2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2017 - 2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@
 /**
 *
 * @file xmcdma_sinit.c
-* @addtogroup mcdma_v1_2
+* @addtogroup mcdma_v1_3
 * @{
 *
 * This file contains static initialization methods for Xilinx MCDMA core.
