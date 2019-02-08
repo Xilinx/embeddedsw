1e3466daa2b76b4450f1d690335591312536e6c6
Nava kishore Manne <nava.manne@xilinx.com>
2019-02-08 19:35:33 +0530

    lib: xilfpga: Update the examples for the latest xilfpga version
    This patch modify the examples to sync with the latest xilfpga
    version 5.0 changes.
    
    Signed-off-by: Nava kishore Manne <nava.manne@xilinx.com>
    
    Acked-for-series: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

--- lib/sw_services/xilfpga/examples/xfpga_reg_readback_example.c
+++ lib/sw_services/xilfpga/examples/xfpga_reg_readback_example.c
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- * Copyright (C) 2017 - 2018 Xilinx, Inc.  All rights reserved.
+ * Copyright (C) 2017 - 2019 Xilinx, Inc.  All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -46,6 +46,7 @@
  * 2.1   Nava  05/10/17  First Release
  * 4.2   Nava    30/05/18  Refactor the xilfpga library to support
  *                         different PL programming Interfaces.
+ * 5.0 	 Nava   06/02/19 Updated the example to sync with 5.0 version API's
  *</pre>
  ******************************************************************************/
 
@@ -91,7 +92,7 @@
 
 /************************** Function Prototypes ******************************/
 
-static int Xfpga_RegReadExample(void);
+static int Xfpga_RegReadExample(XFpga *InstancePtr);
 /************************** Variable Definitions *****************************/
 
 
@@ -106,12 +107,13 @@ static int Xfpga_RegReadExample(void);
  *		- XST_SUCCESS if successful
  *		- XST_FAILURE if unsuccessful
  *
- * @note		None.
+ * @note: This example supports only Zynq UltraScale+ MPSoC platform.
  *
  *****************************************************************************/
 int main(void)
 {
 	int Status;
+	XFpga XFpgaInstance = {0U};
 
 	xil_printf("Register Read back example\r\n");
 
@@ -119,7 +121,14 @@ int main(void)
 	 * Call the example , specify the device ID that is generated in
 	 * xparameters.h.
 	 */
-	Status = Xfpga_RegReadExample();
+	Status = XFpga_Initialize(&XFpgaInstance);
+	if (Status != XST_SUCCESS) {
+		goto done;
+	}
+
+	Status = Xfpga_RegReadExample(&XFpgaInstance);
+
+ done:
 	if (Status != XST_SUCCESS) {
 		xil_printf("Register Read back example Failed\r\n");
 		return XST_FAILURE;
@@ -135,7 +144,7 @@ int main(void)
  *
  * This function reads the configuration registers inside the FPGA.
  *
- * @param	DeviceId is the unique device id of the device.
+ * @param	InstancePtr Pointer to the XFpga structure.
  *
  * @return
  *		- XST_SUCCESS if successful
@@ -144,110 +153,105 @@ int main(void)
  * @note		None.
  *
  *****************************************************************************/
-static int Xfpga_RegReadExample(void)
+static int Xfpga_RegReadExample(XFpga *InstancePtr)
 {
 	u32 CfgReg[64];
 
 	xil_printf("Value of the Configuration Registers. \r\n\n");
 
-	if (XFpga_GetPlConfigReg(CRC, (UINTPTR)CfgReg) != XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, CRC) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" CRC -> \t %x \t\r\n", CfgReg[0]);
 
-	if (XFpga_GetPlConfigReg(FAR, (UINTPTR)CfgReg) != XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, FAR) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" FAR -> \t %x \t\r\n", CfgReg[0]);
 
-	if (XFpga_GetPlConfigReg(FDRI, (UINTPTR)CfgReg) !=
-		XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, FDRI) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" FDRI -> \t %x \t\r\n", CfgReg[0]);
 
-	if (XFpga_GetPlConfigReg(FDRO, (UINTPTR)CfgReg) !=
-		XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, FDRO) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" FDRO -> \t %x \t\r\n",  CfgReg[0]);
 
-	if (XFpga_GetPlConfigReg(CMD, (UINTPTR)CfgReg) !=
-		XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, CMD) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" CMD -> \t %x \t\r\n",  CfgReg[0]);
 
-	if (XFpga_GetPlConfigReg(CTL0, (UINTPTR)CfgReg) !=
-		XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, CTL0) !=	XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" CTL0 -> \t %x \t\r\n",  CfgReg[0]);
 
-	if (XFpga_GetPlConfigReg(MASK, (UINTPTR)CfgReg) !=
-		XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, MASK) !=	XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" MASK -> \t %x \t\r\n",  CfgReg[0]);
 
-	if (XFpga_GetPlConfigReg(STAT, (UINTPTR)CfgReg) != XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, STAT) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" STAT -> \t %x \t\r\n",  CfgReg[0]);
 
-	if (XFpga_GetPlConfigReg(LOUT, (UINTPTR)CfgReg) != XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, LOUT) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" LOUT -> \t %x \t\r\n",  CfgReg[0]);
 
-	if (XFpga_GetPlConfigReg(COR0, (UINTPTR)CfgReg) != XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, COR0) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" COR0 -> \t %x \t\r\n",  CfgReg[0]);
 
-	if (XFpga_GetPlConfigReg(MFWR, (UINTPTR)CfgReg) != XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, MFWR) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" MFWR -> \t %x \t\r\n",  CfgReg[0]);
 
-	if (XFpga_GetPlConfigReg(CBC, (UINTPTR)CfgReg) != XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, CBC) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" CBC -> \t %x \t\r\n",  CfgReg[0]);
 
 
-	if (XFpga_GetPlConfigReg(IDCODE, (UINTPTR)CfgReg) != XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, IDCODE) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" IDCODE -> \t %x \t\r\n",  CfgReg[0] & IDCODE_MASK);
 
 
-	if (XFpga_GetPlConfigReg(AXSS, (UINTPTR)CfgReg) != XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, AXSS) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" AXSS -> \t %x \t\r\n",  CfgReg[0]);
 
-	if (XFpga_GetPlConfigReg(COR1, (UINTPTR)CfgReg) != XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, COR1) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" COR1 -> \t %x \t\r\n",  CfgReg[0]);
 
-	if (XFpga_GetPlConfigReg(WBSTAR, (UINTPTR)CfgReg) != XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, WBSTAR) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" WBSTAR -> \t %x \t\r\n",  CfgReg[0]);
 
-	if (XFpga_GetPlConfigReg(TIMER, (UINTPTR)CfgReg) != XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, TIMER) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" TIMER -> \t %x \t\r\n",  CfgReg[0]);
 
-	if (XFpga_GetPlConfigReg(BOOTSTS, (UINTPTR)CfgReg) != XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, BOOTSTS) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" BOOTSTS -> \t %x \t\r\n",  CfgReg[0]);
 
-	if (XFpga_GetPlConfigReg(CTL1, (UINTPTR)CfgReg) != XST_SUCCESS) {
+	if (XFpga_GetPlConfigReg(InstancePtr, (UINTPTR)CfgReg, CTL1) != XST_SUCCESS) {
 		return XST_FAILURE;
 	}
 	xil_printf(" CTL1 -> \t %x \t\r\n",  CfgReg[0]);
