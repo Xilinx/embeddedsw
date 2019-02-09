3d4cd6b15c101e03f4818f709ed9840a3f1b4045
Nava kishore Manne <nava.manne@xilinx.com>
2019-02-09 14:11:52 +0530

    hwicap: Remove .o reference function prototypes from hwicap header file
    The source code for the XHwIcap_SetClbBits and XHwIcap_GetClbBits
    functions  are not included. These functions are delivered as .o
    files for families older than 7 series.
    
    The current version of the driver is not supported for families
    older than 7 series.So this patch remove .o referenced function
    prototypes from the header file.
    
    Signed-off-by: Nava kishore Manne <nava.manne@xilinx.com>
    Acked-for-series: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

--- XilinxProcessorIPLib/drivers/hwicap/src/xhwicap_selftest.c
+++ XilinxProcessorIPLib/drivers/hwicap/src/xhwicap_selftest.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2007 - 2016 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2007 - 2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@
 /**
 *
 * @file xhwicap_selftest.c
-* @addtogroup hwicap_v11_1
+* @addtogroup hwicap_v11_2
 * @{
 *
 * This file contains a diagnostic self test function for the XHwIcap driver.
