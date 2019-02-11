c7db2f7fd5a6022f0df706b3fd49fe0ee5f2231f
Jolly Shah <jolly.shah@xilinx.com>
2019-02-10 23:45:38 -0800

    xillibpm: Add support to bringup RPU and ecc initialize TCM
    Whenever tcm state goes from OFF to ON, RPu should be brought up if
    not running already and tcm banks should be ecc intialized
    
    Signed-off-by: Jolly Shah <jollys@xilinx.com>
    
    Acked-for-series: Rajan Vaja <rajan.vaja@xilinx.com>

--- lib/sw_services/xillibpm/src/server/xpm_mem.c
+++ lib/sw_services/xillibpm/src/server/xpm_mem.c
@@ -28,6 +28,109 @@
 #include "xillibpm_defs.h"
 #include "xpm_device.h"
 #include "xpm_mem.h"
+#include "xpm_rpucore.h"
+
+#define XPM_TCM_BASEADDRESS_MODE_OFFSET	0x80000U
+
+static const u32 XPmTcmDeviceStates[] = {
+	[XPM_DEVSTATE_UNUSED] = XPM_MIN_CAPABILITY,
+	[XPM_DEVSTATE_RUNNING] = XPM_MAX_CAPABILITY,
+};
+
+static const XPm_StateTran XPmTcmDevTransitions[] = {
+	{
+		.FromState = XPM_DEVSTATE_RUNNING,
+		.ToState = XPM_DEVSTATE_UNUSED,
+		.Latency = XPM_DEF_LATENCY,
+	}, {
+		.FromState = XPM_DEVSTATE_UNUSED,
+		.ToState = XPM_DEVSTATE_RUNNING,
+		.Latency = XPM_DEF_LATENCY,
+	},
+};
+
+static void TcmEccInit(XPm_MemDevice *Tcm, u32 Mode)
+{
+	u32 Size = Tcm->EndAddress - Tcm->StartAddress;
+	u32 Id = Tcm->Device.Node.Id;
+	u32 Base = Tcm->StartAddress;
+
+	if (XPM_DEVID_TCM_1_A == Id || XPM_DEVID_TCM_1_B == Id) {
+		if (XPM_RPU_MODE_LOCKSTEP == Mode)
+			Base -= XPM_TCM_BASEADDRESS_MODE_OFFSET;
+	}
+	if (Size) {
+		memset((void*)Base, 0U, Size);
+	}
+	return;
+}
+
+static XStatus HandleTcmDeviceState(XPm_Device* Device, u32 NextState)
+{
+	XStatus Status = XST_SUCCESS;
+	XPm_Device *Rpu0Device = XPmDevice_GetById(XPM_DEVID_R50_0);
+	XPm_Device *Rpu1Device = XPmDevice_GetById(XPM_DEVID_R50_1);
+	u32 Id = Device->Node.Id;
+	u32 Mode;
+
+	switch (Device->Node.State) {
+	case XPM_DEVSTATE_UNUSED:
+		if (XPM_DEVSTATE_RUNNING == NextState) {
+			Status = XPmDevice_BringUp(&Device->Node);
+			if (XST_SUCCESS != Status) {
+				goto done;
+			}
+			/* TCM is only accessible when the RPU is powered on and out of reset and is in halted state
+			 * so bring up RPU too when TCM is requested*/
+			XPm_RpuGetOperMode(XPM_DEVID_R50_0, &Mode);
+			 if (XPM_RPU_MODE_SPLIT == Mode)
+			 {
+				if (XPM_DEVID_TCM_0_A == Id || XPM_DEVID_TCM_0_B == Id) {
+					Status = XPmDevice_BringUp(&Rpu0Device->Node);
+					if (XST_SUCCESS != Status) {
+						goto done;
+					}
+				}
+				if (XPM_DEVID_TCM_1_A == Id || XPM_DEVID_TCM_1_B == Id) {
+					Status = XPmDevice_BringUp(&Rpu1Device->Node);
+					if (XST_SUCCESS != Status) {
+						goto done;
+					}
+				}
+			 }
+			 if (XPM_RPU_MODE_LOCKSTEP == Mode)
+			 {
+				if (XPM_DEVID_TCM_0_A == Id || XPM_DEVID_TCM_0_B == Id || XPM_DEVID_TCM_1_A == Id || XPM_DEVID_TCM_1_B == Id) {
+					Status = XPmDevice_BringUp(&Rpu0Device->Node);
+					if (XST_SUCCESS != Status) {
+						goto done;
+					}
+				}
+			}
+			/* Tcm should be ecc initialized */
+			TcmEccInit((XPm_MemDevice *)Device, Mode);
+		}
+		break;
+	case XPM_DEVSTATE_RUNNING:
+		if (XPM_DEVSTATE_UNUSED == NextState) {
+			Status = Device->Node.HandleEvent((XPm_Node *)Device,
+							  XPM_DEVEVENT_SHUTDOWN);
+		}
+		break;
+	default:
+		Status = XST_FAILURE;
+		break;
+	}
+
+done:
+	return Status;
+}
+
+static const XPm_DeviceFsm XPmTcmDeviceFsm = {
+	DEFINE_DEV_STATES(XPmTcmDeviceStates),
+	DEFINE_DEV_TRANS(XPmTcmDevTransitions),
+	.EnterState = HandleTcmDeviceState,
+};
 
 XStatus XPmMemDevice_Init(XPm_MemDevice *MemDevice,
 		u32 Id,
@@ -46,6 +149,10 @@ XStatus XPmMemDevice_Init(XPm_MemDevice *MemDevice,
 
 	MemDevice->StartAddress = MemStartAddress;
 	MemDevice->EndAddress = MemEndAddress;
+
+	if (XPM_NODETYPE_DEV_TCM == Type) {
+		MemDevice->Device.DeviceFsm = &XPmTcmDeviceFsm;
+	}
 
 done:
 	return Status;
