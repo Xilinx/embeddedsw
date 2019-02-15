7452b2f5f48d72179447ff104c657ac1495abb94
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2019-02-15 16:45:39 +0530

    sw_services: xilmbox: Add library for ZynqMP/Versal IPI
    This patch adds a new library for mailbox handling,
    currenlty it supports ZYNQMP and Versal IPI h/w.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
    
    Acked-for-series: Anirudha Sarangi <anirudh@xilinx.com>
    
    Acked-for-series: Anirudha Sarangi <anirudh@xilinx.com>

---
+++ lib/sw_services/xilmbox/data/xilmbox.tcl
@@ -0,0 +1,96 @@
+#/******************************************************************************
+#*
+#* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
+#*
+#* Permission is hereby granted, free of charge, to any person obtaining a copy
+#* of this software and associated documentation files (the "Software"), to deal
+#* in the Software without restriction, including without limitation the rights
+#* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#* copies of the Software, and to permit persons to whom the Software is
+#* furnished to do so, subject to the following conditions:
+#*
+#* The above copyright notice and this permission notice shall be included in
+#* all copies or substantial portions of the Software.
+#*
+#* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+#* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+#* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#* SOFTWARE.
+#*
+#* Except as contained in this notice, the name of the Xilinx shall not be used
+#* in advertising or otherwise to promote the sale, use or other dealings in
+#* this Software without prior written authorization from Xilinx.
+#*
+#******************************************************************************/
+#
+# Modification History
+#
+# Ver   Who  Date     Changes
+# ----- ---- -------- -----------------------------------------------
+# 1.0  adk   12/02/19 Initial Release
+##############################################################################
+
+#---------------------------------------------
+# mbox_drc
+#---------------------------------------------
+proc mbox_drc {libhandle} {
+	# check processor type
+	set proc_instance [hsi::get_sw_processor];
+	set hw_processor [common::get_property HW_INSTANCE $proc_instance]
+	set proc_type [common::get_property IP_NAME [hsi::get_cells -hier $hw_processor]]
+	if {$proc_type == "microblaze"} {
+		error "ERROR: This library is not supported for the selected processor"
+	}
+        set gic [hsi::get_cells -hier -filter {IP_NAME=="psu_scugic" || IP_NAME=="psu_acpu_gic" || IP_NAME=="psu_rcpu_gic"}]
+	if {[llength $gic] == 0} {
+		error "ERROR: This library is requires a scugic instance in the design"
+	}
+}
+
+proc generate {libhandle} {
+
+}
+
+
+#-------
+# post_generate: called after generate called on all libraries
+#-------
+proc post_generate {libhandle} {
+	xgen_opts_file $libhandle
+}
+
+#-------
+# execs_generate: called after BSP's, libraries and drivers have been compiled
+#-------
+proc execs_generate {libhandle} {
+
+}
+
+proc xgen_opts_file {libhandle} {
+
+	# Copy the include files to the include directory
+	set srcdir src
+	set dstdir [file join .. .. include]
+	set psdir "src/PS/"
+
+	# Create dstdir if it does not exist
+	if { ! [file exists $dstdir] } {
+		file mkdir $dstdir
+	}
+
+	foreach entry [glob -nocomplain [file join $psdir *]] {
+            file copy -force $entry "./src"
+        }
+
+	# Get list of files in the srcdir
+	set sources [glob -join $srcdir *.h]
+
+	# Copy each of the files in the list to dstdir
+	foreach source $sources {
+		file copy -force $source $dstdir
+	}
+	file delete -force $psdir
+}
