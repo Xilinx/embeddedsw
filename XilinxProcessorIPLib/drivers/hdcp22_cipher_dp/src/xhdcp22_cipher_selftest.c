cbae9c97579965bf6ebb08f8c88c46d1f9c0149e
Jagadeesh Banisetti <jagadeesh.banisetti@xilinx.com>
2019-02-25 16:59:30 +0530

    hdcp22_cipher_dp: initial check-in
    initial checkin of hdcp22_cipher_dp driver
    This has been taken from following driver and added dp related changes:
    commit id: b666b5ca1bd47201d91b147d164cd224083d7ee3
    driver: hdcp22_cipher
    updated mdd and tcl files accordingly.
    
    Signed-off-by: Jagadeesh Banisetti <jagadeesh.banisetti@xilinx.com>
    
    Acked-for-series: Vishal Sagar <vishal.sagar@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/hdcp22_cipher_dp/src/xhdcp22_cipher_selftest.c
@@ -0,0 +1,100 @@
+/******************************************************************************
+*
+* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+/*****************************************************************************/
+/**
+*
+* @file xhdcp22_cipher_selftest.c
+* @addtogroup hdcp22_cipher_v1_1
+* @{
+* @details
+*
+* This file contains self test function for the Xilinx HDCP 2.2 Cipher core.
+* The self test function reads the version register.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who    Date     Changes
+* ----- ------ -------- --------------------------------------------------
+* 1.00  JB     02/19/19 Initial Release.
+* </pre>
+*
+******************************************************************************/
+
+
+/***************************** Include Files *********************************/
+#include "xhdcp22_cipher.h"
+
+/************************** Constant Definitions *****************************/
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+/**************************** Type Definitions *******************************/
+
+/************************** Function Prototypes ******************************/
+
+/************************** Variable Definitions *****************************/
+
+/************************** Function Definitions *****************************/
+
+/*****************************************************************************/
+/**
+*
+* This function reads ID of the peripheral.
+*
+* @param  InstancePtr is a pointer to the HDCP22 Cipher core instance.
+*
+* @return
+*     - XST_SUCCESS if ID was matched.
+*     - XST_FAILURE if ID was mismatched.
+*
+* @note None.
+*
+******************************************************************************/
+int XHdcp22Cipher_SelfTest(XHdcp22_Cipher *InstancePtr)
+{
+	int Status = (XST_SUCCESS);
+	u32 RegValue;
+
+	/* Verify argument. */
+	Xil_AssertNonvoid(InstancePtr != NULL);
+
+	/* Read PIO ID */
+	RegValue = XHdcp22Cipher_ReadReg(InstancePtr->Config.BaseAddress,
+					(XHDCP22_CIPHER_VER_ID_OFFSET));
+
+	RegValue = ((RegValue) >> (XHDCP22_CIPHER_SHIFT_16)) & (XHDCP22_CIPHER_MASK_16);
+
+	if (RegValue != (XHDCP22_CIPHER_VER_ID)) {
+		Status = (XST_FAILURE);
+	}
+
+	return Status;
+}
+
+/** @} */
