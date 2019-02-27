68e56b8c6963649c85abb6163f4cc3ab4d5a9ad1
Sivaji Boddupilli <sivaji.bodupalli@xilinx.com>
2019-02-27 14:35:00 +0530

    sw_apps :zynqmp_fsbl: Changed SDK RELEASE YEAR and SDK RELEASE QUARTER to 2019.1 for ZynqMP FSBL
    This is done by changing the macros in xfsbl_main.h file.
    
    Signed-off-by: Sivaji Boddupilli <sivajib@xilinx.com>
    Acked-by: Vikram Sreenivasa Batchali <bvikram@xilinx.com>
    
    Acked-by: Vikram Sreenivasa Batchali <bvikram@xilinx.com>

--- lib/sw_apps/zynqmp_fsbl/src/xfsbl_main.h
+++ lib/sw_apps/zynqmp_fsbl/src/xfsbl_main.h
@@ -112,8 +112,8 @@ typedef struct {
 /***************** Macros (Inline Functions) Definitions *********************/
 
 /* SDK release version */
-#define SDK_RELEASE_YEAR	2018
-#define SDK_RELEASE_QUARTER	3
+#define SDK_RELEASE_YEAR	2019
+#define SDK_RELEASE_QUARTER	1
 
 #define XFSBL_RUNNING			(0xFFFFU)
 #define XFSBL_COMPLETED			(0x0U)
