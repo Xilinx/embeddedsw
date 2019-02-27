7ea7ab2f822fa088cde956c47d2e3542e29ee91f
Jolly Shah <jolly.shah@xilinx.com>
2019-02-27 04:29:08 -0800

    xillibpm: Add LPD house cleaning functions
    Added house cleaning handlers for LPD
    
    Signed-off-by: Jolly Shah <jollys@xilinx.com>
    Signed-off-by: Ravi Patel <ravi.patel@xilinx.com>
    
    Acked-for-series: Jolly Shah <jollys@xilinx.com>

--- lib/sw_services/xillibpm/src/server/xpm_pslpdomain.c
+++ lib/sw_services/xillibpm/src/server/xpm_pslpdomain.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2018-2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -28,151 +28,67 @@
 
 #include "xpm_common.h"
 #include "xpm_pslpdomain.h"
+#include "xpm_domain_iso.h"
+#include "xpm_reset.h"
 
-static XStatus (*HandlePowerEvent)(XPm_Node *Node, u32 Event);
-
-static XStatus HandlePsLpDomainEvent(XPm_Node *Node, u32 Event)
+static XStatus LpdPreHouseclean()
 {
-	u32 Status = XST_FAILURE;
-	XPm_Power *Power = (XPm_Power *)Node;
-	XPm_PsLpDomain *PsLpd = (XPm_PsLpDomain *)Node;
+	XStatus Status = XST_SUCCESS;
+//#ifdef SPP_HACK
+	/* TODO: Check vccint_pslp first to make sure power is on */
 
-	PmDbg("State=%s, Event=%s\n\r",
-		PmPowerStates[Node->State], PmPowerEvents[Event]);
+	/* Remove PS_PL isolation */
+	Status = XPm_DomainIsoDisable(XPM_DOMAIN_ISO_LPD_PL_TEST);
+	if (Status != XST_SUCCESS)
+		goto done;
 
-	switch (Node->State)
-	{
-		case XPM_POWER_STATE_PWR_UP_SELF:
-			if (XPM_POWER_EVENT_TIMER == Event) {
-				Status = XST_SUCCESS;
-				/* Todo: Read PSM status register */
-				if (1 /* Hack: Power node is up */) {
-					Node->State = XPM_POWER_STATE_LBIST;
-					/* Todo: Write to PSM request register */
-					/* Todo: Start timer to poll PSM status register */
-					/* Hack */
-					Status = Node->HandleEvent(Node, XPM_POWER_EVENT_TIMER);
-				} else {
-					/* Todo: Restart timer to poll PSM */
-				}
-			}
-			break;
-		case XPM_POWER_STATE_LBIST:
-			if (XPM_POWER_EVENT_TIMER == Event) {
-				Status = XST_SUCCESS;
-				/* Todo: Read PSM status register */
-				if (1 /* Hack: LBIST is done */) {
-					Node->State = XPM_POWER_STATE_SCAN_CLEAR;
-					/* Todo: Write to PSM request register */
-					/* Todo: Start timer to poll PSM status register */
-					/* Hack */
-					Node->HandleEvent(Node, XPM_POWER_EVENT_TIMER);
-				} else {
-					/* Todo: Restart timer to poll PSM */
-				}
-			}
-			break;
-		case XPM_POWER_STATE_SCAN_CLEAR:
-			if (XPM_POWER_EVENT_TIMER == Event) {
-				Status = XST_SUCCESS;
-				/* Todo: Read PSM status register */
-				if (1 /* Hack: Scan Clear is done */) {
-					Node->State = XPM_POWER_STATE_BISR;
-					/* Todo: Write to PSM request register */
-					/* Todo: Start timer to poll PSM status register */
-					/* Hack */
-					Status = Node->HandleEvent(Node, XPM_POWER_EVENT_TIMER);
-				} else {
-					/* Todo: Restart timer to poll PSM */
-				}
-			}
-			break;
-		case XPM_POWER_STATE_BISR:
-			if (XPM_POWER_EVENT_TIMER == Event) {
-				Status = XST_SUCCESS;
-				/* Todo: Read PSM status register */
-				if (1 /* Hack: BISR is done */) {
-					Node->State = XPM_POWER_STATE_MIST;
-					/* Todo: Write to PSM request register */
-					/* Todo: Start timer to poll PSM status register */
-					/* Hack */
-					Status = Node->HandleEvent(Node, XPM_POWER_EVENT_TIMER);
-				} else {
-					/* Todo: Restart timer to poll PSM */
-				}
-			}
-			break;
-		case XPM_POWER_STATE_MIST:
-			if (XPM_POWER_EVENT_TIMER == Event) {
-				Status = XST_SUCCESS;
-				/* Todo: Read PSM status register */
-				if (1 /* Hack: MIST is done */) {
-					Node->State = XPM_POWER_STATE_ON;
-					PsLpd->Domain.Power.UseCount++;
-				} else {
-					/* Todo: Restart timer to poll PSM */
-				}
-			}
-			break;
-		case XPM_POWER_STATE_ON:
-			if (XPM_POWER_EVENT_PWR_UP == Event) {
-				/* Use base class handler */
-				Status = HandlePowerEvent(Node, Event);
-			} else if (XPM_POWER_EVENT_PWR_DOWN == Event) {
-				Status = XST_SUCCESS;
-				if (1 == Power->UseCount) {
-					Node->State = XPM_POWER_STATE_PWR_DOWN_SELF;
-					/* Todo: Power down LPD */
-					/* Todo: Start timer to poll PSM status register */
-					/* Hack */
-					Status = Node->HandleEvent(Node, XPM_POWER_EVENT_TIMER);
-				} else {
-					/* Use base class handler */
-					Status = HandlePowerEvent(Node, Event);
-				}
-			} else {
-				/* Required by MISRA */
-			}
-			break;
-		case XPM_POWER_STATE_PWR_DOWN_SELF:
-			if (XPM_POWER_EVENT_TIMER == Event) {
-				Status = XST_SUCCESS;
-				/* Todo: Check LPD power state */
-				if (1 /* Hack: LPD is down */) {
-					Power->UseCount--;
-					if (NULL != Power->Parent) {
-						Node->State = XPM_POWER_STATE_PWR_DOWN_PARENT;
-						Power->WfParentUseCnt = Power->Parent->UseCount - 1;
-						Status = Power->Parent->Node.HandleEvent(
-							&Power->Parent->Node, XPM_POWER_EVENT_PWR_DOWN);
-						/* Todo: Start timer to poll the parent node */
-						/* Hack */
-						Status = Node->HandleEvent(Node, XPM_POWER_EVENT_TIMER);
-					} else {
-						Node->State = XPM_POWER_STATE_OFF;
-					}
-				} else {
-					/* Todo: Restart timer to poll parent node */
-				}
-			}
-			break;
-		default:
-			Status = HandlePowerEvent(Node, Event);
-			break;
-	}
+	Status = XPm_DomainIsoDisable(XPM_DOMAIN_ISO_LPD_PL);
+	if (Status != XST_SUCCESS)
+		goto done;
 
+	/* Remove PS_CPM domains isolation */
+	Status = XPm_DomainIsoDisable(XPM_DOMAIN_ISO_LPD_CPM_DFX);
+	if (Status != XST_SUCCESS)
+		goto done;
+
+	Status = XPm_DomainIsoDisable(XPM_DOMAIN_ISO_LPD_CPM);
+	if (Status != XST_SUCCESS)
+		goto done;
+
+	/* Remove FP-SOC isolation */
+	Status = XPm_DomainIsoDisable(XPM_DOMAIN_ISO_FPD_SOC);
+	if (Status != XST_SUCCESS)
+		goto done;
+
+	/* Remove LP-SoC isolation */
+	Status = XPm_DomainIsoDisable(XPM_DOMAIN_ISO_LPD_SOC);
+	if (Status != XST_SUCCESS)
+		goto done;
+
+	/* Remove PS_PMC domains isolation */
+	Status = XPm_DomainIsoDisable(XPM_DOMAIN_ISO_PMC_LPD_DFX);
+	if (Status != XST_SUCCESS)
+		goto done;
+
+	Status = XPm_DomainIsoDisable(XPM_DOMAIN_ISO_PMC_LPD);
+	if (Status != XST_SUCCESS)
+		goto done;
+
+	/*
+	 * Release POR for PS-LPD
+	 */
+	Status = XPmReset_AssertbyId(POR_RSTID(XPM_NODEIDX_RST_PS_POR),
+				     PM_RESET_ACTION_RELEASE);
+done:
+//#endif
 	return Status;
 }
 
-XStatus XPmPsLpDomain_Init(XPm_PsLpDomain *PsLpd, u32 Id, u32 BaseAddress,
-			   XPm_Power *Parent)
+static XStatus LpdPostHouseclean()
 {
-	XPmPowerDomain_Init(&PsLpd->Domain, Id, BaseAddress, Parent, NULL);
+	XStatus Status = XST_SUCCESS;
 
-	HandlePowerEvent = PsLpd->Domain.Power.Node.HandleEvent;
-	PsLpd->Domain.Power.Node.HandleEvent = HandlePsLpDomainEvent;
-
-	return XST_SUCCESS;
+	return Status;
 }
 
 /****************************************************************************/
@@ -182,10 +98,10 @@ XStatus XPmPsLpDomain_Init(XPm_PsLpDomain *PsLpd, u32 Id, u32 BaseAddress,
  * @return XST_SUCCESS if successful else XST_FAILURE
  *
  ****************************************************************************/
-int XPmPsLpDomain_ScanClearLpd(void)
+static XStatus LpdScanClear()
 {
-	int Status = XST_FAILURE;
-
+	XStatus Status = XST_SUCCESS;
+#ifdef SPP_HACK
 	/* Trigger Scan clear on LPD/LPD_IOU */
 	PmRmw32(PMC_ANALOG_SCAN_CLEAR_TRIGGER,
 		(PMC_ANALOG_SCAN_CLEAR_TRIGGER_LPD_MASK |
@@ -194,7 +110,6 @@ int XPmPsLpDomain_ScanClearLpd(void)
 		(PMC_ANALOG_SCAN_CLEAR_TRIGGER_LPD_MASK |
 		 PMC_ANALOG_SCAN_CLEAR_TRIGGER_LPD_IOU_MASK |
 		 PMC_ANALOG_SCAN_CLEAR_TRIGGER_LPD_RPU_MASK));
-
 	Status = XPm_PollForMask(PMC_ANALOG_SCAN_CLEAR_DONE,
 				 (PMC_ANALOG_SCAN_CLEAR_DONE_LPD_IOU_MASK |
 				  PMC_ANALOG_SCAN_CLEAR_DONE_LPD_MASK |
@@ -209,8 +124,8 @@ int XPmPsLpDomain_ScanClearLpd(void)
 				  PMC_ANALOG_SCAN_CLEAR_PASS_LPD_MASK |
 				  PMC_ANALOG_SCAN_CLEAR_PASS_LPD_RPU_MASK),
 				 XPM_POLL_TIMEOUT);
-
 done:
+#endif
 	return Status;
 }
 
@@ -221,10 +136,10 @@ done:
  * @return XST_SUCCESS if successful else XST_FAILURE
  *
  ****************************************************************************/
-int XPmPsLpDomain_LpdLBIST(void)
+static XStatus LpdLbist()
 {
-	int Status = XST_FAILURE;
-
+	XStatus Status = XST_SUCCESS;
+#ifdef SPP_HACK
 	/* Trigger LBIST on LPD */
 	PmRmw32(PMC_ANALOG_LBIST_ENABLE,
 		(PMC_ANALOG_LBIST_ENABLE_LPD_MASK |
@@ -236,7 +151,7 @@ int XPmPsLpDomain_LpdLBIST(void)
 				 (PMC_ANALOG_LBIST_DONE_LPD_MASK |
 				  PMC_ANALOG_LBIST_DONE_LPD_RPU_MASK),
 				 XPM_POLL_TIMEOUT);
-
+#endif
 	return Status;
 }
 
@@ -247,10 +162,11 @@ int XPmPsLpDomain_LpdLBIST(void)
  * @return XST_SUCCESS if successful else XST_FAILURE
  *
  ****************************************************************************/
-int XPmPsLpDomain_LpdBISR(void)
+static XStatus LpdBisr()
 {
-	int Status = XST_FAILURE;
+	XStatus Status = XST_SUCCESS;
 
+#ifdef SPP_HACK
 	PmRmw32(LPD_SLCR_BISR_CACHE_CTRL_0, LPD_SLCR_BISR_TRIGGER_MASK,
 		LPD_SLCR_BISR_TRIGGER_MASK);
 
@@ -268,8 +184,8 @@ int XPmPsLpDomain_LpdBISR(void)
 				  LPD_SLCR_BISR_PASS_1_MASK |
 				  LPD_SLCR_BISR_PASS_0_MASK),
 				 XPM_POLL_TIMEOUT);
-
 done:
+#endif
 	return Status;
 }
 
@@ -280,9 +196,10 @@ done:
  * @return XST_SUCCESS if successful else XST_FAILURE
  *
  ****************************************************************************/
-int XPmPsLpDomain_LpdMBIST(void)
+static XStatus LpdMbist()
 {
-	int Status = XST_FAILURE;
+	XStatus Status = XST_SUCCESS;
+#ifdef SPP_HACK
 	u32 RegValue;
 
 	PmRmw32(PMC_ANALOG_OD_MBIST_RST,
@@ -325,7 +242,24 @@ int XPmPsLpDomain_LpdMBIST(void)
 	     PMC_ANALOG_OD_MBIST_GOOD_LPD_MASK) != RegValue) {
 		Status = XST_FAILURE;
 	}
-
 done:
+#endif
 	return Status;
+}
+
+struct XPm_PowerDomainOps LpdOps = {
+	.PreHouseClean = LpdPreHouseclean,
+	.PostHouseClean = LpdPostHouseclean,
+	.ScanClear = LpdScanClear,
+	.Mbist = LpdMbist,
+	.Lbist = LpdLbist,
+	.Bisr = LpdBisr,
+};
+
+XStatus XPmPsLpDomain_Init(XPm_PsLpDomain *PsLpd, u32 Id, u32 BaseAddress,
+			   XPm_Power *Parent)
+{
+	XPmPowerDomain_Init(&PsLpd->Domain, Id, BaseAddress, Parent, &LpdOps);
+
+	return XST_SUCCESS;
 }
