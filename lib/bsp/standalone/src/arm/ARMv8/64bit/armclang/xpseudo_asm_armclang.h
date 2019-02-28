bbf8120a2aae8a70f99a419d6a0af26302604306
Mubin Sayyed <mubin.usman.sayyed@xilinx.com>
2019-02-28 12:40:21 +0530

    BSP:ARMv8: Add armclang suport
    Updated ARMv8 BSP files and tcl to support armclang
    compiler.
    
    Signed-off-by: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>
    
    Acked-for-series: Anirudha Sarangi <anirudh@xilinx.com>

--- lib/bsp/standalone/src/arm/ARMv8/64bit/armclang/xpseudo_asm_armclang.h
+++ lib/bsp/standalone/src/arm/ARMv8/64bit/armclang/xpseudo_asm_armclang.h
@@ -1,28 +1,8 @@
 /******************************************************************************
-*
-* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
-*
+* Copyright (c) 2019 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 /*****************************************************************************/
 /**
 *
@@ -36,6 +16,7 @@
 * Ver   Who  Date     Changes
 * ----- ---- -------- -----------------------------------------------
 * 7.0   cjp  02/26/19 First Release
+* 7.2   asa  04/03/20 Renamed the str macro to strw.
 * </pre>
 *
 ******************************************************************************/
@@ -101,7 +82,7 @@ extern "C" {
 			    rval; \
 			})
 
-#define str(adr, val)	__asm__ __volatile__( \
+#define strw(adr, val)	__asm__ __volatile__( \
 			"str %x0, [%x1]\n" \
 			: : "r" (val), "r" (adr) \
 			)
