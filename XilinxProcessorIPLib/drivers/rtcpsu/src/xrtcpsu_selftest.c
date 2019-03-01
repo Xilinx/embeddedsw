e4981e49896db0e631f56aff62d073dbf8e42b02
Srinivas Neeli <srinivas.neeli@xilinx.com>
2019-03-01 20:29:33 +0530

    rtcpsu:Updated year and change version history
    Updated year and change version history with Versal support
    and MISRA-C changes.
    
    Signed-off-by: Srinivas Neeli <sneeli@xilinx.com>
    
    Acked-for-series: Srinivas Goud <sgoud@xilinx.com>

--- XilinxProcessorIPLib/drivers/rtcpsu/src/xrtcpsu_selftest.c
+++ XilinxProcessorIPLib/drivers/rtcpsu/src/xrtcpsu_selftest.c
@@ -40,6 +40,13 @@
 * Ver   Who    Date	Changes
 * ----- ------ -------- -----------------------------------------------
 * 1.00  kvn    04/21/15 First release.
+* 1.7   sne    03/01/19 Added Versal support.
+* 1.7   sne    03/01/19 Fixed violations according to MISRAC-2012 standards
+*                       modified the code such as
+*                       No brackets to loop body,Declared the poiner param
+*                       as Pointer to const,No brackets to then/else,
+*                       Literal value requires a U suffix,Casting operation to a pointer
+*                       Array has no bounds specified,Logical conjunctions need brackets.
 * </pre>
 *
 ******************************************************************************/
