66c21c1bfab9f09a3ff91e204756cb014b56673c
Ravi Patel <ravi.patel@xilinx.com>
2019-03-04 01:51:38 -0800

    xilplmi: Lower the debug level of unnecessary prints
    Lower the debug level of some unnecessary prints which is
    coming frequently during execution.
    
    Signed-off-by: Ravi Patel <ravi.patel@xilinx.com>
    Acked-by: Krishna Chaitanya Patakamuri <krishna.chaitanya.patakamuri@xilinx.com>
    
    Acked-by: Krishna Chaitanya Patakamuri <krishna.chaitanya.patakamuri@xilinx.com>

--- lib/sw_services/xilplmi/src/xplmi_task.c
+++ lib/sw_services/xilplmi/src/xplmi_task.c
@@ -1,4 +1,5 @@
 /******************************************************************************
+*
 * Copyright (C) 2019 Xilinx, Inc. All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -11,10 +12,6 @@
 * The above copyright notice and this permission notice shall be included in
 * all copies or substantial portions of the Software.
 *
-* Use of the Software is limited solely to applications:
-* (a) running on a Xilinx device, or
-* (b) that interact with a Xilinx device through a bus or interconnect.
-*
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
@@ -26,6 +23,7 @@
 * Except as contained in this notice, the name of the Xilinx shall not be used
 * in advertising or otherwise to promote the sale, use or other dealings in
 * this Software without prior written authorization from Xilinx.
+*
 ******************************************************************************/
 
 /*****************************************************************************/
