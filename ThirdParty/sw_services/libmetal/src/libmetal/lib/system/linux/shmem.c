b241d9cdafdc9ab49c6c1b1ed90c5608e1b5018b
Wendy Liang <wendy.liang@xilinx.com>
2019-03-06 15:23:54 -0800

    linux: shmem: init/deinit shmem ION provider
    Initialize and deinitialize shmem ION provider when
    initializing and deinitializing Linux environment.
    
    Signed-off-by: Wendy Liang <wendy.liang@xilinx.com>
    
    Acked-for-series: Hyun Kwon <hyun.kwon@xilinx.com>

--- ThirdParty/sw_services/libmetal/src/libmetal/lib/system/linux/shmem.c
+++ ThirdParty/sw_services/libmetal/src/libmetal/lib/system/linux/shmem.c
@@ -14,13 +14,24 @@
 #include <metal/utilities.h>
 #include "shmem.h"
 
+extern int metal_ion_shm_provider_init(void);
+extern void metal_ion_shm_provider_deinit(void);
+
 int metal_linux_init_shmem(void)
 {
+	int ret = 0;
+
 	metal_shm_provider_register(&linux_shm_provider_shm);
-	return 0;
+#ifdef HAVE_DMA_BUF_H
+	ret = metal_ion_shm_provider_init();
+#endif
+	return ret;
 }
 
 void metal_linux_deinit_shmem(void)
 {
 	metal_shm_provider_unregister(&linux_shm_provider_shm);
+#ifdef HAVE_DMA_BUF_H
+	metal_ion_shm_provider_deinit();
+#endif
 }
