8f06755e94d11f35c8ee1a11e7d34ef3f17968d9
Harini Katakam <harini.katakam@xilinx.com>
2019-03-11 12:19:50 +0530

    emacps: Update copyright years and doxygen version
    Update copyright year in all files and doxygen version in source.
    
    Signed-off-by: Harini Katakam <harini.katakam@xilinx.com>
    
    Acked-for-series: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>

--- XilinxProcessorIPLib/drivers/emacps/src/xemacps_hw.c
+++ XilinxProcessorIPLib/drivers/emacps/src/xemacps_hw.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2010 - 2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@
 /**
 *
 * @file xemacps_hw.c
-* @addtogroup emacps_v3_8
+* @addtogroup emacps_v3_9
 * @{
 *
 * This file contains the implementation of the ethernet interface reset sequence
