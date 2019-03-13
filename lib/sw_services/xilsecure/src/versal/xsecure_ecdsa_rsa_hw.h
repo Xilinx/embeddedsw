2fd309e577ec4bb1fe1404146197902e4eae0a04
VNSL Durga <vnsl.durga.challa@xilinx.com>
2019-03-14 00:05:09 +0530

    XilSecure: Refactoring h/w definitions
    As part of refactoring hardware offsets are placed
    into the corresponding platform folders.
    Splited common xsecure_hw.h file into xsecure_sha_hw.h,
    xsecure_aes_hw.h and xsecure_rsa_hw.h.
    Also common functionalities like SSS configurations and
    hardware read and write APIs are placed in utils
    
    Signed-off-by: VNSL Durga Challa <vnsl.durga.challa@xilinx.com>
    
    Acked-for-series: Mohan Dhanawade <mohan.dhanawade@xilinx.com>

---
+++ lib/sw_services/xilsecure/src/versal/xsecure_ecdsa_rsa_hw.h
@@ -0,0 +1,198 @@
+/******************************************************************************
+*
+* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+
+/*****************************************************************************/
+/**
+*
+* @file xecdsa_rsa_hw.h
+*
+* This header file contains RSA ECDSA core hardware register offsets of versal.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who     Date     Changes
+* ----- ------  -------- ------------------------------------------------------
+* 4.0   vns     02/14/19 First Release
+* </pre>
+*
+******************************************************************************/
+#ifndef XSECURE_ECDSA_RSA_H_
+#define XSECURE_ECDSA_RSA_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/***************************** Include Files *********************************/
+
+/************************** Constant Definitions *****************************/
+
+/* ECDSA RSA core's base Address */
+#define XSECURE_ECDSA_RSA_BASEADDR			(0xF1200000U)
+					/**< Base address of ECDSA RSA core */
+
+/* Register offsets */
+/**
+ * Register: RAM data register
+ */
+#define XSECURE_ECDSA_RSA_RAM_DATA_OFFSET		(0x00000000U)
+
+/**
+ * Register: RAM address register
+ */
+#define XSECURE_ECDSA_RSA_RAM_ADDR_OFFSET		(0x00000004U)
+#define XSECURE_ECDSA_RSA_RAM_ADDR_WRRD_B_MASK		(0x80000000U)
+#define XSECURE_ECDSA_RSA_RAM_ADDR_ADDR_MASK		(0x7FFFFFFFU)
+
+/**
+ * Register: Control register
+ */
+#define XSECURE_ECDSA_RSA_CTRL_OFFSET			(0x00000008U)
+
+#define XSECURE_ECDSA_RSA_CTRL_CLR_DATA_BUF_SHIFT	(7U)
+#define XSECURE_ECDSA_RSA_CTRL_CLR_DATA_BUF_MASK	(0x00000080U)
+
+#define XSECURE_ECDSA_RSA_CTRL_CLR_DONE_ABORT_SHIFT	(6U)
+#define XSECURE_ECDSA_RSA_CTRL_CLR_DONE_ABORT_MASK	(0x00000040U)
+
+#define XSECURE_ECDSA_RSA_CTRL_OPCODE_MASK		(0x00000007U)
+
+/**
+ * Register: Status Register
+ */
+#define XSECURE_ECDSA_RSA_STATUS_OFFSET			(0x0000000CU)
+
+#define XSECURE_ECDSA_RSA_STATUS_PROGRESS_CNT_SHIFT	(3U)
+#define XSECURE_ECDSA_RSA_STATUS_PROGRESS_CNT_MASK	(0x000000F8U)
+
+#define XSECURE_ECDSA_RSA_STATUS_ERROR_SHIFT		(2U)
+#define XSECURE_ECDSA_RSA_STATUS_ERROR_MASK		(0x00000004U)
+
+#define XSECURE_ECDSA_RSA_STATUS_BUSY_SHIFT		(1U)
+#define XSECURE_ECDSA_RSA_STATUS_BUSY_MASK		(0x00000002U)
+
+#define XSECURE_ECDSA_RSA_STATUS_DONE_MASK		(0x00000001U)
+
+/**
+ * Register: MINV value register
+ */
+#define XSECURE_ECDSA_RSA_MINV_OFFSET			(0x00000010U)
+
+/**
+ * Register: Key Length register
+ */
+#define XSECURE_ECDSA_RSA_KEY_LENGTH_OFFSET		(0x00000020U)
+
+#define XSECURE_ECDSA_RSA_KEY_LENGTH_BIN_PRIME_SHIFT	(15U)
+#define XSECURE_ECDSA_RSA_KEY_LENGTH_BIN_PRIME_MASK	(0x00008000U)
+
+#define XSECURE_ECDSA_RSA_KEY_LENGTH_LENGTH_MASK	(0x00007FFFU)
+
+/**
+ * Register: CFG 0
+ */
+#define XSECURE_ECDSA_RSA_CFG0_OFFSET			(0x00000028U)
+
+#define XSECURE_ECDSA_RSA_CFG0_QSEL_SHIFT		(6U)
+#define XSECURE_ECDSA_RSA_CFG0_QSEL_MASK		(0x000000C0U)
+
+#define XSECURE_ECDSA_RSA_CFG0_MULTI_PASS_MASK		(0x0000003FU)
+
+/**
+ * Register: CFG 1
+ */
+#define XSECURE_ECDSA_RSA_CFG1_OFFSET			(0x0000002CU)
+
+#define XSECURE_ECDSA_RSA_CFG1_MONT_DIGIT_MASK		(0x000000FFU)
+
+/**
+ * Register: CFG 2
+ */
+#define XSECURE_ECDSA_RSA_CFG2_OFFSET			(0x00000030U)
+#define XSECURE_ECDSA_RSA_CFG2_MEM_LOC_SIZE_MASK	(0x0000001FU)
+
+/**
+ * Register: CFG 3
+ */
+#define XSECURE_ECDSA_RSA_CFG3_OFFSET			(0x00000034U)
+
+#define XSECURE_ECDSA_RSA_CFG3_MONT_MOD_SHIFT		(4U)
+#define XSECURE_ECDSA_RSA_CFG3_MONT_MOD_MASK		(0x000000F0U)
+
+#define XSECURE_ECDSA_RSA_CFG3_SCRATCH_MASK		(0x0000000FU)
+
+/**
+ * Register: XSECURE_ECDSA_RSA_CFG4
+ */
+#define XSECURE_ECDSA_RSA_CFG4_OFFSET    ( 0x00000038 )
+
+#define XSECURE_ECDSA_RSA_CFG4_START_ADDR_SHIFT   0
+#define XSECURE_ECDSA_RSA_CFG4_START_ADDR_WIDTH   8
+#define XSECURE_ECDSA_RSA_CFG4_START_ADDR_MASK    0x000000FF
+
+/**
+ * Register: XSECURE_ECDSA_RSA_CFG5
+ */
+#define XSECURE_ECDSA_RSA_CFG5_OFFSET    ( 0x0000003C )
+
+#define XSECURE_ECDSA_RSA_CFG5_NO_GROUPS_SHIFT   0
+#define XSECURE_ECDSA_RSA_CFG5_NO_GROUPS_WIDTH   5
+#define XSECURE_ECDSA_RSA_CFG5_NO_GROUPS_MASK    0x0000001F
+
+/**
+ * Register: XSECURE_ECDSA_RSA_RESET
+ */
+#define XSECURE_ECDSA_RSA_RESET_OFFSET    ( 0x00000040 )
+
+/**
+ * Register: XSECURE_ECDSA_RSA_APB_SLAVE_ERR_CTRL
+ */
+#define XSECURE_ECDSA_RSA_APB_SLAVE_ERR_CTRL_OFFSET    ( 0x00000044 )
+
+/**
+ * Register: XSECURE_ECDSA_RSA_RSA_CFG
+ */
+#define XSECURE_ECDSA_RSA_CFG_OFFSET    ( 0x00000058 )
+
+#define XSECURE_ECDSA_RSA_CFG_RD_ENDIANNESS_MASK    0x00000002
+
+#define XSECURE_ECDSA_RSA_RSA_CFG_WR_ENDIANNESS_SHIFT   0
+#define XSECURE_ECDSA_RSA_RSA_CFG_WR_ENDIANNESS_WIDTH   1
+#define XSECURE_ECDSA_RSA_RSA_CFG_WR_ENDIANNESS_MASK    0x00000001
+
+/**
+ * Register: XSECURE_ECDSA_RSA_XSECURE_ECDSA_RSA_ECO
+ */
+#define XSECURE_ECDSA_RSA_XSECURE_ECDSA_RSA_ECO_OFFSET    ( 0x00000060U )
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* XSECURE_ECDSA_RSA_H_ */
---
+++ lib/sw_services/xilsecure/src/zynqmp/xsecure_aes_hw.h
@@ -0,0 +1,129 @@
+/******************************************************************************
+*
+* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+
+/*****************************************************************************/
+/**
+*
+* @file xsecure_aes_hw.h
+*
+* This is the header file which contains ZynqMP AES core hardware definitions.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who  Date        Changes
+* ----- ---- -------- -------------------------------------------------------
+* 4.0   vns  03/11/19 Initial release
+*
+* </pre>
+*
+* @note
+*
+******************************************************************************/
+
+#ifndef XSECURE_AES_HW_H
+#define XSECURE_AES_HW_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/***************************** Include Files *********************************/
+#include "xil_io.h"
+#include "xparameters.h"
+#include "xil_types.h"
+#include "sleep.h"
+#include "xsecure_utils.h"
+
+/************************** Constant Definitions *****************************/
+
+#define XSECURE_CSU_AES_BASE	(XSECURE_CSU_REG_BASE_ADDR + 0x1000U)
+					/**< CSU AES base address */
+#define XSECURE_CSU_PCAP_STATUS	(XSECURE_CSU_REG_BASE_ADDR + 0X00003010U)
+					/**< CSU PCAP Status reg. */
+#define XSECURE_CSU_PCAP_STATUS_PCAP_WR_IDLE_MASK	(0X00000001U)
+					/**< PCAP Write Idle */
+/** @name Register Map
+ *
+ * Register offsets for the AES module.
+ * @{
+ */
+#define XSECURE_CSU_AES_STS_OFFSET	(0x00U) /**< AES Status */
+#define XSECURE_CSU_AES_KEY_SRC_OFFSET	(0x04U) /**< AES Key Source */
+#define XSECURE_CSU_AES_KEY_LOAD_OFFSET	(0x08U) /**< AES Key Load Reg */
+#define XSECURE_CSU_AES_START_MSG_OFFSET (0x0CU) /**< AES Start Message */
+#define XSECURE_CSU_AES_RESET_OFFSET	(0x10U) /**< AES Reset Register */
+#define XSECURE_CSU_AES_KEY_CLR_OFFSET	(0x14U) /**< AES Key Clear */
+#define XSECURE_CSU_AES_CFG_OFFSET	(0x18U)/**< AES Operational Mode */
+#define XSECURE_CSU_AES_KUP_WR_OFFSET	(0x1CU)/**< AES KUP Write Control */
+
+#define XSECURE_CSU_AES_KUP_0_OFFSET	(0x20U)
+						/**< AES Key Update 0 */
+#define XSECURE_CSU_AES_KUP_1_OFFSET	(0x24U) /**< AES Key Update 1 */
+#define XSECURE_CSU_AES_KUP_2_OFFSET	(0x28U) /**< AES Key Update 2 */
+#define XSECURE_CSU_AES_KUP_3_OFFSET	(0x2CU) /**< AES Key Update 3 */
+#define XSECURE_CSU_AES_KUP_4_OFFSET	(0x30U) /**< AES Key Update 4 */
+#define XSECURE_CSU_AES_KUP_5_OFFSET	(0x34U) /**< AES Key Update 5 */
+#define XSECURE_CSU_AES_KUP_6_OFFSET	(0x38U) /**< AES Key Update 6 */
+#define XSECURE_CSU_AES_KUP_7_OFFSET	(0x3CU) /**< AES Key Update 7 */
+
+#define XSECURE_CSU_AES_IV_0_OFFSET	(0x40U) /**< AES IV 0 */
+#define XSECURE_CSU_AES_IV_1_OFFSET	(0x44U) /**< AES IV 1 */
+#define XSECURE_CSU_AES_IV_2_OFFSET	(0x48U) /**< AES IV 2 */
+#define XSECURE_CSU_AES_IV_3_OFFSET	(0x4CU) /**< AES IV 3 */
+/* @} */
+
+/**************************** Type Definitions *******************************/
+
+/*****************************************************************************/
+/**
+* Wait for writes to PL and hence PCAP write cycle to complete
+*
+* @param	None.
+*
+* @return	None.
+*
+* @note		C-Style signature:
+*			void XSecure_PcapWaitForDone(void)
+*
+******************************************************************************/
+static inline void XSecure_PcapWaitForDone()
+{
+	while ((Xil_In32(XSECURE_CSU_PCAP_STATUS) &
+			XSECURE_CSU_PCAP_STATUS_PCAP_WR_IDLE_MASK) !=
+			XSECURE_CSU_PCAP_STATUS_PCAP_WR_IDLE_MASK);
+}
+
+/************************** Function Prototypes ******************************/
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* XSECURE_AES_HW_H */
