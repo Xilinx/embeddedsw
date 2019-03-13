78db5d0e5af22e888de4223319e67ba3189c10a8
Srinivas Goud <srinivas.goud@xilinx.com>
2019-03-13 12:42:25 +0530

    iicps: Updated Copyright year and addtogroup
    Updated Copyright year and doxygen changes for iicps
    
    Signed-off-by: Srinivas Goud <srinivas.goud@xilinx.com>
    
    Acked-for-series: Anirudha Sarangi <anirudh@xilinx.com>

--- XilinxProcessorIPLib/drivers/iicps/src/xiicps_g.c
+++ XilinxProcessorIPLib/drivers/iicps/src/xiicps_g.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2010 - 2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@
 /**
 *
 * @file xiicps_g.c
-* @addtogroup iicps_v3_8
+* @addtogroup iicps_v3_9
 * @{
 *
 * This file contains a configuration table that specifies the configuration of
