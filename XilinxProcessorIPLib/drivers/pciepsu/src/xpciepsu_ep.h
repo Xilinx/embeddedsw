0caf8fbbb84d114fdb99325f6f5c060723ab6feb
Tirupathi <tirupathi.korla@xilinx.com>
2019-03-15 12:51:00 +0530

    pciepsu: Adds Endpoint code
    Adds Header and comments in ep_enable_example.c
    Adds xpciepsu_ep.h
    Adds xpciepsu_ep.c file
    Modifies tcl to add dma base addr
    Adds new variable in xpciepsu structure
    
    Signed-off-by: Tirupathi <tirupath@xilinx.com>
    
    Acked-for-series-by: Venkata Ravi Shankar Jonnalagadda  <venkata.ravi.jonnalagadda@xilinx.com>

--- XilinxProcessorIPLib/drivers/pciepsu/src/xpciepsu_ep.h
+++ XilinxProcessorIPLib/drivers/pciepsu/src/xpciepsu_ep.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -27,7 +27,7 @@
 *******************************************************************************/
 /******************************************************************************/
 /**
-* @file xpciepsu.h
+* @file xpciepsu_ep.h
 *
 * This file contains the software API definition of the Xilinx PSU PCI IP
 * (psu_pcie). This driver provides "C" function interface to application/upper
@@ -52,85 +52,92 @@
 *   static configuration structure defined in xpciepsu_g.c. This is setup by
 *   the tools.
 *
-*   - XPciePsu_CfgInitialize(InstancePtr, CfgPtr, EffectiveAddress) - Uses a
-*   configuration structure provided by the caller. If running in a system with
-*   address translation, the provided virtual memory base address replaces the
-*   physical address present in the configuration structure.
+*   - XPciePsu_EP_CfgInitialize(InstancePtr, CfgPtr, EffectiveAddress) -
+*   Uses a configuration structure provided by the caller. If running in a
+*   system with address translation, the provided virtual memory base address
+*   replaces the physical address present in the configuration structure.
 *
 * <pre>
 * MODIFICATION HISTORY:
 *
 * Ver   Who  Date     Changes
 * ----- ---- -------- ---------------------------------------------------
-* 1.0	bs	08/21/2018	First release
+* 1.0	tk	02/13/2019	First release
 * </pre>
 *
 *******************************************************************************/
-#ifndef XPCIEPSU_H_
-#define XPCIEPSU_H_
+#ifndef XPCIEPSU_EP_H_
+#define XPCIEPSU_EP_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /******************************** Include Files *******************************/
-#include "xil_types.h"
-#include "xstatus.h"
-#include "xil_io.h"
-#include "xpciepsu_hw.h"
+
+#include "xpciepsu.h"
 
 /**************************** Constant Definitions ****************************/
 
 /******************** Macros (Inline Functions) Definitions *******************/
-#define XPciePsu_ReadReg(BaseAddr, RegOffset) Xil_In32((BaseAddr) + (RegOffset))
-
-#define XPciePsu_WriteReg(BaseAddr, RegOffset, Val)                            \
-	Xil_Out32((BaseAddr) + (RegOffset), (Val))
 
 /****************************** Type Definitions ******************************/
+#define INGRESS_SIZE				0x20
 
-typedef struct {
-	u16 DeviceId; /**< Unique ID of PCIe IP */
-	u64 BrigReg;  /**< Bridge Register base address */
-	u64 PciReg;		/**< pcie Register base address */
-	u64 Ecam;		/**< Ecam space base address */
-	u32	NpMemBaseAddr;		/**< non prefetchable memory base address */
-	u64	PMemBaseAddr;		/**< prefetchable memory base address */
-	u32	NpMemMaxAddr;	/**< non prefetchable memory max base address*/
-	u64	PMemMaxAddr;	/**< prefetchable memory max base address */
-    u32 DmaBaseAddr;	/**< DMA base address */
-	u8	PcieMode;		/**< pcie mode rc or endpoint */
-} XPciePsu_Config;
+#define COMMAND_REG					(0x4U)
+#define BAR0_OFFSET_LO				(0x10U)
+#define BAR0_OFFSET_HI				(0x14U)
 
-typedef struct {
-	XPciePsu_Config Config; /**< Configuration data */
-	u32 IsReady;		/**< Is IP been initialized and ready */
-	u32 MaxSupportedBusNo;		/**< If this is RC IP, Max Number of  Buses */
-} XPciePsu;
+#define PCIE_ENUMERATED_STATUS		0x00000002U
+
+#define DREG_CONTROL				(0x288U)
+#define DREG_BASE_LO				(0x290U)
+#define DREG_BASE_HI				(0x294U)
+
+#define INGRESS0_CONTROL			(0x808U)
+#define INGRESS0_SRC_BASE_LO		(0x810U)
+#define INGRESS0_SRC_BASE_HI		(0x814U)
+#define INGRESS0_DST_BASE_LO		(0x818U)
+#define INGRESS0_DST_BASE_HI		(0x81CU)
+
+#define MSGF_DMA_MASK               (0x464)
+
+#define PCIE_LINK_UP				0x000000001U
+#define ECAM_ENABLE					0x000000001U
+#define INGRESS_ENABLE				0x000000001U
+#define INGRESS_SECURITY_ENABLE		0x000000004U
+#define DMA_ENABLE					0x000000001U
+#define BREG_SIZE					0x000000004U
+
+
+#define DMA0_CHAN_AXI_INTR              (0x68)
+#define AXI_INTR_ENABLE                 0x00000001U
+#define MSGF_DMA_INTR_ENABLE            0x00000001U
+
+#define BREG_SIZE_SHIFT					16
+#define INGRESS_SIZE_SHIFT				16
+
+#define BREG_SIZE_MASK					0x00030000U
+#define INGRESS_SIZE_MASK				0x001F0000U
+
+#define INGRESS_SIZE_ENCODING			0x00000008U /* 2^(12+8) = 1MB */
+
+#define INGRESS_RD_WR_ATTR				0xFF800000U
+
+#define ENUMERATION_WAIT_TIME			10
 
 /***************************** Function Prototypes ****************************/
+void XPciePsu_EP_BridgeInitialize(XPciePsu *PciePsuPtr);
+void XPciePsu_EP_CfgInitialize(XPciePsu *PciePsuPtr,
+		XPciePsu_Config *ConfigPtr);
+void XPciePsu_EP_WaitForLinkup(XPciePsu *PciePsuPtr);
+void XPciePsu_EP_WaitForEnumeration(XPciePsu *PciePsuPtr);
+int XPciePsu_EP_SetupIngress(XPciePsu *PciePsuPtr, u32 IngressNum,
+		u32 BarNum, u32 DstLo);
 
-XPciePsu_Config *XPciePsu_LookupConfig(u16 DeviceId);
-
-u32 XPciePsu_CfgInitialize(XPciePsu *InstancePtr, XPciePsu_Config *CfgPtr,
-			   UINTPTR EffectiveBrgAddress);
-u8 XPciePsu_EnumerateBus(XPciePsu *InstancePtr);
-u8 XPciePsu_ReadConfigSpace(XPciePsu *InstancePtr, u8 Bus, u8 Device,
-				    u8 Function, u16 Offset, u32 *DataPtr);
-u8 XPciePsu_WriteConfigSpace(XPciePsu *InstancePtr, u8 Bus, u8 Device,
-				     u8 Function, u16 Offset, u32 Data);
-u32 XPciePsu_ComposeExternalConfigAddress(u8 Bus, u8 Device, u8 Function,
-					  u16 Offset);
-
-u8 XPciePsu_HasCapability(XPciePsu *InstancePtr, u8 Bus, u8 Device,
-		u8 Function, u8 CapId);
-u64 XPciePsu_GetCapability(XPciePsu *InstancePtr, u8 Bus, u8 Device,
-		u8 Function, u8 CapId);
-u8 XPciePsu_PrintAllCapabilites(XPciePsu *InstancePtr, u8 Bus, u8 Device,
-		u8 Function);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* XPCIEPSU_H_ */
+#endif /* XPCIEPSU_EP_H_ */
