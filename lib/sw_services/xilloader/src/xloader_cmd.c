79b6d7520202a5d3a84fbffa6ea9182174cdf6a0
Krishna Chaitanya <krishna.chaitanya.patakamuri@xilinx.com>
2019-03-15 11:34:42 -0600

    sw_services:xilloader: Added support for loading partial PDIs
    Added support for loading partial / subsystem PDI files.
    
    Signed-off-by: Krishna Chaitanya <krishna.chaitanya.patakamuri@xilinx.com>
    
    Acked-for-series: Vikram Sreenivasa Batchali <b.vikram@xilinx.com>

---
+++ lib/sw_services/xilloader/src/xloader_cmd.c
@@ -0,0 +1,150 @@
+/******************************************************************************
+* Copyright (C) 2019 Xilinx, Inc. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+******************************************************************************/
+/*****************************************************************************/
+/**
+*
+* @file xloader_cmd.c
+*
+* This file contains the xloader commands implementation.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who  Date        Changes
+* ----- ---- -------- -------------------------------------------------------
+* 1.00  kc   03/12/2019 Initial release
+*
+* </pre>
+*
+* @note
+*
+******************************************************************************/
+
+/***************************** Include Files *********************************/
+#include "xloader.h"
+#include "xplmi_cmd.h"
+#include "xplmi_modules.h"
+/************************** Constant Definitions *****************************/
+
+/**************************** Type Definitions *******************************/
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+/************************** Function Prototypes ******************************/
+
+/************************** Variable Definitions *****************************/
+XilPdi SubsystemPdiIns;
+
+/*****************************************************************************/
+
+static int XLoader_Reserved(XPlmi_Cmd * Cmd)
+{
+	XPlmi_Printf(DEBUG_DETAILED, "%s %p\n\r", __func__, Cmd);
+	return XST_SUCCESS;
+}
+
+/*****************************************************************************/
+/**
+ * @brief This function provides load subsystem PDI command execution
+ *  Command payload parameters are
+ *	* PdiSrc - Boot Mode values, DDR, PCIe
+ *	* PdiAddr - 64bit PDI address located in the Source
+ *
+ * @param Pointer to the command structure
+ *
+ * @return Returns the Load PDI command
+ *****************************************************************************/
+static int XLoader_LoadSubsystemPdi(XPlmi_Cmd * Cmd)
+{
+	int Status;
+	u32 PdiSrc;
+	u64 PdiAddr;
+	XilPdi* PdiPtr = &SubsystemPdiIns;
+
+	XPlmi_Printf(DEBUG_DETAILED, "%s \n\r", __func__);
+
+	/** store the command fields in resume data */
+	PdiSrc = Cmd->Payload[0];
+	PdiAddr = (u64 )Cmd->Payload[1];
+	PdiAddr = ((u64 )Cmd->Payload[2] |
+		   (PdiAddr << 32));
+
+	XPlmi_Printf(DEBUG_INFO, "Subsytem PDI Load: Started\n\r");
+
+	PdiPtr->PdiType = XLOADER_PDI_TYPE_PARTIAL;
+	Status = XLoader_PdiInit(PdiPtr, PdiSrc, PdiAddr);
+	if (Status != XST_SUCCESS)
+	{
+		goto END;
+	}
+
+	Status = XLoader_LoadAndStartSubSystemPdi(PdiPtr);
+	if (Status != XST_SUCCESS)
+	{
+		goto END;
+	}
+
+	XPlmi_Printf(DEBUG_INFO, "Subsystem PDI Load: Done\n\r");
+END:
+	return Status;
+}
+
+/*****************************************************************************/
+/**
+ * @brief contains the array of PLM loader commands
+ *
+ *****************************************************************************/
+static XPlmi_ModuleCmd XLoader_Cmds[] =
+{
+	XPLMI_MODULE_COMMAND(XLoader_Reserved),
+	XPLMI_MODULE_COMMAND(XLoader_LoadSubsystemPdi),
+};
+
+/*****************************************************************************/
+/**
+ * @brief Contains the module ID and loader commands array
+ *
+ *****************************************************************************/
+static XPlmi_Module XPlmi_Loader =
+{
+	XPLMI_MODULE_LOADER_ID,
+	XLoader_Cmds,
+	sizeof XLoader_Cmds / sizeof *XLoader_Cmds,
+};
+
+/*****************************************************************************/
+/**
+ * @brief This function registers the PLM Loader commands to the PLMI
+ *
+ * @param none
+ *
+ * @return none
+ *
+ *****************************************************************************/
+void XLoader_CmdsInit(void)
+{
+	XPlmi_ModuleRegister(&XPlmi_Loader);
+}
--- lib/sw_services/xilloader/src/xloader_ddr.h
+++ lib/sw_services/xilloader/src/xloader_ddr.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2018-2019 Xilinx, Inc. All rights reserved.
+* Copyright (C) 2019 Xilinx, Inc. All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -10,10 +10,6 @@
 *
 * The above copyright notice and this permission notice shall be included in
 * all copies or substantial portions of the Software.
-*
-* Use of the Software is limited solely to applications:
-* (a) running on a Xilinx device, or
-* (b) that interact with a Xilinx device through a bus or interconnect.
 *
 * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -27,74 +23,53 @@
 * in advertising or otherwise to promote the sale, use or other dealings in
 * this Software without prior written authorization from Xilinx.
 ******************************************************************************/
-
 /*****************************************************************************/
 /**
 *
-* @file xplmi_modules.h
+* @file xloader_ddr.h
 *
-* This is the header file which contains definitions for the modules
+* This is the header file which contains DDR interface declarations
+* for the xilloader.
 *
 * <pre>
 * MODIFICATION HISTORY:
 *
 * Ver   Who  Date        Changes
 * ----- ---- -------- -------------------------------------------------------
-* 1.00  kc   08/20/2018 Initial release
-*
+* 1.00  kc   03/12/2019 Initial release
 * </pre>
 *
 * @note
 *
 ******************************************************************************/
 
-#ifndef XPLMI_MODULES_H
-#define XPLMI_MODULES_H
+#ifndef XLOADER_DDR_H
+#define XLOADER_DDR_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /***************************** Include Files *********************************/
-#include "xplmi_cmd.h"
-
+#include "xplmi_hw.h"
+#include "xplmi_dma.h"
+#include "xplmi_status.h"
+#include "xplmi_debug.h"
 /************************** Constant Definitions *****************************/
-#define XPLMI_MAX_MODULES				(10U)
-
-/* TODO add enum for declaring module ids */
-#define XPLMI_MODULE_GENERIC_ID			(1U)
-#define XPLMI_MODULE_XILPM_ID				(2U)
-#define XPLMI_MODULE_SEM_ID				(3U)
-#define XPLMI_MODULE_XILFPGA_ID			(4U)
-#define XPLMI_MODULE_XILSECURE_ID			(5U)
-#define XPLMI_MODULE_XILPSM_ID				(6U)
-#define XPLMI_MODULE_LOADER_ID				(7U)
-
-#define XPLMI_MODULE_COMMAND(FUNC)		{ (FUNC) }
-
 /**************************** Type Definitions *******************************/
-typedef struct {
-	int (*Handler)(XPlmi_Cmd *Cmd);
-} XPlmi_ModuleCmd;
-
-typedef struct {
-	u32 Id;
-	XPlmi_ModuleCmd * CmdAry;
-	u32 CmdCnt;
-} XPlmi_Module;
 
 /***************** Macros (Inline Functions) Definitions *********************/
 
 /************************** Function Prototypes ******************************/
-void XPlmi_ModuleRegister(XPlmi_Module * Module);
+
+int XLoader_DdrInit(u32 DeviceFlags);
+XStatus XLoader_DdrCopy(u32 SrcAddr, u64 DestAddress, u32 Length, u32 Flags);
+int XLoader_DdrRelease(void );
 
 /************************** Variable Definitions *****************************/
-extern XPlmi_Module * Modules[XPLMI_MAX_MODULES];
-
-/*****************************************************************************/
 
 #ifdef __cplusplus
-extern "C" {
+}
 #endif
 
-#endif /* XPLMI_MODULES_H */
+#endif  /* XLOADER_DDR_H */
