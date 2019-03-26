754ca7d7d7c3660b75bb18c0983d5b8656a7f0f1
Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
2019-03-26 18:23:53 +0530

    qspipsu: Fixed data alignment warnings in the examples for IAR compiler.
    This patch fixes the improper data alignment checks in the examples
    for IAR compiler.
    
    Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
    
    Acked-for-series: Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>

--- XilinxProcessorIPLib/drivers/qspipsu/examples/xqspipsu_generic_flash_interrupt_example.c
+++ XilinxProcessorIPLib/drivers/qspipsu/examples/xqspipsu_generic_flash_interrupt_example.c
@@ -80,6 +80,8 @@
 * 1.8	tjs 16/07/18 Added support for the low density ISSI flash parts.
 * 1.9	akm 02/27/19 Added support for IS25LP128, IS25WP128, IS25LP256,
 * 		     IS25WP256, IS25LP512, IS25WP512 Flash Devices
+* 1.9   akm 03/26/19 Fixed data alignment warnings on IAR compiler.
+*
 *</pre>
 *
 ******************************************************************************/
@@ -621,7 +623,6 @@ int Test = 1;
 #ifdef __ICCARM__
 #pragma data_alignment = 32
 u8 ReadBuffer[(PAGE_COUNT * MAX_PAGE_SIZE) + (DATA_OFFSET + DUMMY_SIZE)*8];
-#pragma data_alignment = 4
 #else
 u8 ReadBuffer[(PAGE_COUNT * MAX_PAGE_SIZE) + (DATA_OFFSET + DUMMY_SIZE)*8] __attribute__ ((aligned(64)));
 #endif
