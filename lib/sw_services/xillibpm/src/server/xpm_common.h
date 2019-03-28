f79b5032ede8e6786e26deed7d7f63bee54b9ecc
Tejas Patel <tejas.patel@xilinx.com>
2019-03-28 04:52:29 -0700

    sw_services: xillibpm: Fixed MISRA-C violation in xpm_device_idle files
    This patch fixes the following violation in xpm_device_idle files
     - Function call with no prior declaration.(MISRA-C:2012 R.17.3)
    
    Signed-off-by: Tejas Patel <tejas.patel@xilinx.com>
    Acked-by: Jolly Shah <jollys@xilinx.com>
    
    Acked-by: Jolly Shah <jollys@xilinx.com>

--- lib/sw_services/xillibpm/src/server/xpm_common.h
+++ lib/sw_services/xillibpm/src/server/xpm_common.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2018-2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -29,8 +29,8 @@
 #ifndef XPM_COMMON_H_
 #define XPM_COMMON_H_
 
-#include <xstatus.h>
-#include <xil_io.h>
+#include "xstatus.h"
+#include "xil_io.h"
 
 #include "xplmi_debug.h"
 
