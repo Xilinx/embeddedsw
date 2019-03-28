455c94e41337df075bde13aa600db264d2339375
VNSL Durga <vnsl.durga.challa@xilinx.com>
2019-03-28 09:42:22 -0400

    XilSecure: Added support for ECDSA
    This patch provides support for ECDSA in Versal
    Makefile is modified to handle pre-compiled ECDSA static
    library file
    
    Signed-off-by: VNSL Durga Challa <vnsl.durga.challa@xilinx.com>
    Acked-by: Kalyani Akula <Kalyani.akula@xilinx.com>
    
    Acked-by: Kalyani Akula <Kalyani.akula@xilinx.com>

--- lib/sw_services/xilsecure/src/versal/xsecure_ecdsa.h
+++ lib/sw_services/xilsecure/src/versal/xsecure_ecdsa.h
@@ -1,28 +1,8 @@
 /******************************************************************************
-*
-* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
-*
+* Copyright (c) 2019 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 *******************************************************************************/
+
 /*****************************************************************************/
 /**
 *
@@ -37,6 +17,7 @@
 * Ver   Who  Date        Changes
 * ----- ---- -------- -------------------------------------------------------
 * 4.0 	vns  03/27/19 First Release
+* 4.2   har  11/07/19 Typo correction to enable compilation in C++
 *
 * </pre>
 *
@@ -51,11 +32,9 @@ extern "C" {
 #endif
 
 /***************************** Include Files *********************************/
-
 #include "xsecure_utils.h"
-
 /************************** Constant Definitions ****************************/
-
+#define XSECURE_ECC_DATA_SIZE_WORDS (12U)
 /***************************** Type Definitions ******************************/
 
 /***************************** Function Prototypes ***************************/
@@ -95,8 +74,10 @@ int P384_validatekey(unsigned char *Qx, unsigned char *Qy);
 int P384_ecdsaverify(unsigned char *z, unsigned char *Qx,
 		unsigned char *Qy, unsigned char *r, unsigned char *s);
 
+u32 XSecure_EcdsaKat(void);
+
 #ifdef __cplusplus
-extern "C" }
+}
 #endif
 
 #endif
