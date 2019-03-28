c34b729c02c0500a9eec89f46229af7058061e4e
Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
2019-03-28 13:01:03 +0530

    qspips: Update copyright year and add group information
    This patch updates copyright year and add group information
    for qspips driver.
    
    Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
    
    Acked-for-series: Naga Sureshkumar Relli <nagasure@xilinx.com>

--- XilinxProcessorIPLib/drivers/qspips/src/xqspips_options.c
+++ XilinxProcessorIPLib/drivers/qspips/src/xqspips_options.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2010 - 2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -29,7 +29,7 @@
 /**
 *
 * @file xqspips_options.c
-* @addtogroup qspips_v3_5
+* @addtogroup qspips_v3_6
 * @{
 *
 * Contains functions for the configuration of the XQspiPs driver component.
