2205c0cf1281c2824fab6ad3f95357a99c304798
Md Jubaer Hossain Pantho <md.jubaer.hossain.pantho@xilinx.com>
2019-04-02 14:23:25 -0700

    aiengine: src: lib: Add NPI API
    APIs to reset Shim and AIE array through the NPI
    
    Signed-off-by: Md Jubaer Hossain Pantho <md.jubaer.hossain.pantho@xilinx.com>
    
    Reviewed-by: Hyun Kwon <hyun.kwon@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/aiengine/src/lib/xaielib_npi.c
@@ -0,0 +1,112 @@
+/*******************************************************************************
+*
+* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+*******************************************************************************/
+
+/******************************************************************************/
+/**
+* @file xaielib_npi.c
+* @{
+*
+* This file contains routines for NPI module
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who     Date     Changes
+* ----- ------  -------- -----------------------------------------------------
+* 1.0  Jubaer  03/08/2019  Initial creation
+* </pre>
+*
+*******************************************************************************/
+
+/***************************** Include Files **********************************/
+#include "xaiegbl.h"
+#include "xaiegbl_defs.h"
+#include "xaiegbl_reginit.h"
+#include "xaielib_npi.h"
+
+/***************************** Constant Definitions ***************************/
+/***************************** Macro Definitions ******************************/
+/************************** Variable Definitions ******************************/
+/************************** Function Definitions ******************************/
+/*****************************************************************************/
+/**
+*
+* This API is used to assert shim reset from NPI.
+*
+* @param	Reset - Reset/Unreset the Shim Tile (1-Reset,0-Unreset).
+*
+* @return	XAIE_SUCCESS if successful, else XAIE_FAILURE.
+*
+* @note		None.
+*
+*******************************************************************************/
+u8 XAieLib_NpiShimReset(u8 Reset)
+{
+	u32 RegVal;
+
+	XAie_AssertNonvoid(Reset == 0 || Reset == 1);
+
+	XAieGbl_NPIWrite32(XAIE_NPI_PCSR_MASK, 1U <<
+			   XAIE_NPI_PCSR_MASK_SHIM_RESET_LSB);
+
+	RegVal = XAie_SetField(Reset, XAIE_NPI_PCSR_CONTROL_SHIM_RESET_LSB,
+			       XAIE_NPI_PCSR_CONTROL_SHIM_RESET_MSK);
+	XAieGbl_NPIWrite32(XAIE_NPI_PCSR_CONTROL, RegVal);
+
+	return XAIE_SUCCESS;
+}
+
+/*****************************************************************************/
+/**
+*
+* This API is used to assert AIE array reset from NPI.
+*
+* @param	Reset - Reset/Unreset the AIE array.
+*
+* @return	XAIE_SUCCESS if successful, else XAIE_FAILURE.
+*
+* @note		None.
+*
+*******************************************************************************/
+u8 XAieLib_NpiAieArrayReset(u8 Reset)
+{
+	u32 RegVal;
+
+	XAie_AssertNonvoid(Reset == 0 || Reset == 1);
+
+	XAieGbl_NPIWrite32(XAIE_NPI_PCSR_MASK, 1U <<
+			   XAIE_NPI_PCSR_MASK_AIE_ARRAY_RESET_LSB);
+
+	RegVal = XAie_SetField(Reset, XAIE_NPI_PCSR_CONTROL_AIE_ARRAY_RESET_LSB,
+			       XAIE_NPI_PCSR_CONTROL_AIE_ARRAY_RESET_MASK);
+	XAieGbl_NPIWrite32(XAIE_NPI_PCSR_CONTROL, RegVal);
+
+	return XAIE_SUCCESS;
+}
+
+/** @} */
