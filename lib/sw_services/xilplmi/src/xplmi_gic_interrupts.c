5d0c5a46360c26e24ea8c2b2cac7dc1f48a3c426
Krishna Chaitanya <krishna.chaitanya.patakamuri@xilinx.com>
2019-04-08 23:04:02 -0600

    sw_services:xilplmi: Added register/enable/disable different interrupts
    Added support for register/enable/disable different interrupts as per
    PMC MB.
    Added SBI DATA RDY interrupt for PR support.
    
    Signed-off-by: Krishna Chaitanya <krishna.chaitanya.patakamuri@xilinx.com>

--- lib/sw_services/xilplmi/src/xplmi_gic_interrupts.c
+++ lib/sw_services/xilplmi/src/xplmi_gic_interrupts.c
@@ -1,28 +1,8 @@
 /******************************************************************************
-*
-* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
-*
+* Copyright (c) 2019 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 
 /*****************************************************************************/
 /**
@@ -37,6 +17,7 @@
 * Ver   Who  Date        Changes
 * ====  ==== ======== ======================================================-
 * 1.00  mg   10/08/2018 Initial release
+* 1.01  mg   04/02/2020 Remove defines which are already part of pmc_global.h
 *
 * </pre>
 *
@@ -45,11 +26,11 @@
 ******************************************************************************/
 
 /***************************** Include Files *********************************/
-#include "xpm_node.h"
-#include "xpm_api.h"
 #include "xplmi_gic_interrupts.h"
 #include "xplmi_hw.h"
 #include "xplmi_util.h"
+#include "xplmi_task.h"
+#include "xplmi_debug.h"
 
 /************************** Constant Definitions *****************************/
 
@@ -61,195 +42,121 @@
 
 /************************** Variable Definitions *****************************/
 static struct GicIntrHandlerTable
-   g_GicPInterruptTable[XPLMI_GICP_SOURCE_COUNT][XPLMI_NO_OF_BITS_IN_REG] = {
+	g_GicPInterruptTable[XPLMI_GICP_SOURCE_COUNT][XPLMI_NO_OF_BITS_IN_REG] = {
 #ifdef XPAR_XIPIPSU_0_DEVICE_ID
 	[XPLMI_PMC_GIC_IRQ_GICP0][XPLMI_GICP0_SRC27] =
 		{(void *)0U, XPlmi_IpiDispatchHandler},
 #endif
-	[XPLMI_PMC_GIC_IRQ_GICP0][XPLMI_GICP0_SRC13] =
-		{(void *)XPM_NODEIDX_DEV_GPIO, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP0][XPLMI_GICP0_SRC14] =
-		{(void *)XPM_NODEIDX_DEV_I2C_0, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP0][XPLMI_GICP0_SRC15] =
-		{(void *)XPM_NODEIDX_DEV_I2C_1, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP0][XPLMI_GICP0_SRC16] =
-		{(void *)XPM_NODEIDX_DEV_SPI_0, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP0][XPLMI_GICP0_SRC17] =
-		{(void *)XPM_NODEIDX_DEV_SPI_1, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP0][XPLMI_GICP0_SRC18] =
-		{(void *)XPM_NODEIDX_DEV_UART_0, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP0][XPLMI_GICP0_SRC19] =
-		{(void *)XPM_NODEIDX_DEV_UART_1, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP0][XPLMI_GICP0_SRC20] =
-		{(void *)XPM_NODEIDX_DEV_CAN_FD_0, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP0][XPLMI_GICP0_SRC21] =
-		{(void *)XPM_NODEIDX_DEV_CAN_FD_1, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP0][XPLMI_GICP0_SRC22] =
-		{(void *)XPM_NODEIDX_DEV_USB_0, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP0][XPLMI_GICP0_SRC23] =
-		{(void *)XPM_NODEIDX_DEV_USB_0, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP0][XPLMI_GICP0_SRC24] =
-		{(void *)XPM_NODEIDX_DEV_USB_0, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP0][XPLMI_GICP0_SRC25] =
-		{(void *)XPM_NODEIDX_DEV_USB_0, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP0][XPLMI_GICP0_SRC26] =
-		{(void *)XPM_NODEIDX_DEV_USB_0, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC5] =
-		{(void *)XPM_NODEIDX_DEV_TTC_0, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC6] =
-		{(void *)XPM_NODEIDX_DEV_TTC_0, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC7] =
-		{(void *)XPM_NODEIDX_DEV_TTC_0, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC8] =
-		{(void *)XPM_NODEIDX_DEV_TTC_1, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC9] =
-		{(void *)XPM_NODEIDX_DEV_TTC_1, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC10] =
-		{(void *)XPM_NODEIDX_DEV_TTC_1, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC11] =
-		{(void *)XPM_NODEIDX_DEV_TTC_2, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC12] =
-		{(void *)XPM_NODEIDX_DEV_TTC_2, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC13] =
-		{(void *)XPM_NODEIDX_DEV_TTC_2, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC14] =
-		{(void *)XPM_NODEIDX_DEV_TTC_3, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC15] =
-		{(void *)XPM_NODEIDX_DEV_TTC_3, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC16] =
-		{(void *)XPM_NODEIDX_DEV_TTC_3, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC24] =
-		{(void *)XPM_NODEIDX_DEV_GEM_0, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC25] =
-		{(void *)XPM_NODEIDX_DEV_GEM_0, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC26] =
-		{(void *)XPM_NODEIDX_DEV_GEM_1, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC27] =
-		{(void *)XPM_NODEIDX_DEV_GEM_1, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC28] =
-		{(void *)XPM_NODEIDX_DEV_ADMA_0, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC29] =
-		{(void *)XPM_NODEIDX_DEV_ADMA_1, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC30] =
-		{(void *)XPM_NODEIDX_DEV_ADMA_2, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP1][XPLMI_GICP1_SRC31] =
-		{(void *)XPM_NODEIDX_DEV_ADMA_3, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP2][XPLMI_GICP2_SRC0] =
-		{(void *)XPM_NODEIDX_DEV_ADMA_4, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP2][XPLMI_GICP2_SRC1] =
-		{(void *)XPM_NODEIDX_DEV_ADMA_5, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP2][XPLMI_GICP2_SRC2] =
-		{(void *)XPM_NODEIDX_DEV_ADMA_6, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP2][XPLMI_GICP2_SRC3] =
-		{(void *)XPM_NODEIDX_DEV_ADMA_7, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP2][XPLMI_GICP2_SRC10] =
-		{(void *)XPM_NODEIDX_DEV_USB_0, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP3][XPLMI_GICP3_SRC30] =
-		{(void *)XPM_NODEIDX_DEV_SDIO_0, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP3][XPLMI_GICP3_SRC31] =
-		{(void *)XPM_NODEIDX_DEV_SDIO_0, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP4][XPLMI_GICP4_SRC0] =
-		{(void *)XPM_NODEIDX_DEV_SDIO_1, XPlmi_DispatchWakeHandler},
-	[XPLMI_PMC_GIC_IRQ_GICP4][XPLMI_GICP4_SRC1] =
-		{(void *)XPM_NODEIDX_DEV_SDIO_1, XPlmi_DispatchWakeHandler},
-
-	[XPLMI_PMC_GIC_IRQ_GICP4][XPLMI_GICP4_SRC14] =
-		{(void *)XPM_NODEIDX_DEV_RTC, XPlmi_DispatchWakeHandler},
 };
 
 /*****************************************************************************/
 /**
- * @brief This will register the GIC handler
+ * @brief	This will register the GIC handler.
  *
- * @param	None
+ * @param	PlmIntrId is the interrupt ID
+ * @param	Handler is the interrupt handler
+ * @param	Data is the pointer to arguments to interrupt handler
+ *
  * @return	None
+ *
  *****************************************************************************/
 void XPlmi_GicRegisterHandler(u32 PlmIntrId, Function_t Handler, void *Data)
 {
 	u32 GicPVal;
 	u32 GicPxVal;
 
-	/** Get the GicP mask */
-	GicPVal = (PlmIntrId & XPLMI_GICP_MASK)>>8U;
-	GicPxVal = (PlmIntrId & XPLMI_GICPX_MASK)>>16U;
-
+	/* Get the GicP mask */
+	GicPVal = (PlmIntrId & XPLMI_GICP_MASK) >> 8U;
+	GicPxVal = (PlmIntrId & XPLMI_GICPX_MASK) >> 16U;
 	/** Register Handler */
 	g_GicPInterruptTable[GicPVal][GicPxVal].GicHandler = Handler;
 	g_GicPInterruptTable[GicPVal][GicPxVal].Data = Data;
-
 }
 
 /*****************************************************************************/
 /**
- * @brief This will enable the GIC interrupt
+ * @brief	This will clear the GIC interrupt.
  *
- * @param	None
+ * @param	PlmIntrId PLM interrupt Id
+ *
  * @return	None
+ *
+ *****************************************************************************/
+void XPlmi_GicIntrClearStatus(u32 PlmIntrId)
+{
+	u32 GicPVal;
+	u32 GicPxVal;
+
+	/* Get the GicP mask */
+	GicPVal = (PlmIntrId & XPLMI_GICP_MASK)>>8U;
+	GicPxVal = (PlmIntrId & XPLMI_GICPX_MASK)>>16U;
+	/* Enable interrupt */
+	XPlmi_UtilRMW(PMC_GLOBAL_GICP_PMC_IRQ_STATUS,
+		1U << GicPVal,
+		1U << GicPVal);
+
+	XPlmi_UtilRMW(PMC_GLOBAL_GICP0_IRQ_STATUS + (GicPVal * XPLMI_GICPX_LEN),
+		1U << GicPxVal,
+		1U << GicPxVal);
+}
+
+/*****************************************************************************/
+/**
+ * @brief	This will enable the GIC interrupt.
+ *
+ * @param	PlmIntrId is the interrupt Id
+ *
+ * @return	None
+ *
  *****************************************************************************/
 void XPlmi_GicIntrEnable(u32 PlmIntrId)
 {
 	u32 GicPVal;
 	u32 GicPxVal;
 
-	/** Get the GicP mask */
-	GicPVal = (PlmIntrId & XPLMI_GICP_MASK)>>8U;
-	GicPxVal = (PlmIntrId & XPLMI_GICPX_MASK)>>16U;
-
+	/* Get the GicP mask */
+	GicPVal = (PlmIntrId & XPLMI_GICP_MASK) >> 8U;
+	GicPxVal = (PlmIntrId & XPLMI_GICPX_MASK) >> 16U;
 	/* Enable interrupt */
 	XPlmi_UtilRMW(PMC_GLOBAL_GICP_PMC_IRQ_ENABLE,
-		1U<<GicPVal,
-		1U<<GicPVal);
+		1U << GicPVal,
+		1U << GicPVal);
 
-	XPlmi_UtilRMW(PMC_GLOBAL_GICP0_IRQ_ENABLE + (GicPVal*0x14),
-		1U<<GicPxVal,
-		1U<<GicPxVal);
+	XPlmi_UtilRMW(PMC_GLOBAL_GICP0_IRQ_ENABLE + (GicPVal * XPLMI_GICPX_LEN),
+		1U << GicPxVal,
+		1U << GicPxVal);
 }
 
 /*****************************************************************************/
 /**
- * @brief This will disable the GIC interrupt
+ * @brief	This will disable the GIC interrupt.
  *
- * @param	None
+ * @param	PlmIntrId  is the interrupt Id
+ *
  * @return	None
+ *
  *****************************************************************************/
 void XPlmi_GicIntrDisable(u32 PlmIntrId)
 {
 	u32 GicPVal;
 	u32 GicPxVal;
 
-	/** Get the GicP mask */
-	GicPVal = (PlmIntrId & XPLMI_GICP_MASK)>>8U;
-	GicPxVal = (PlmIntrId & XPLMI_GICPX_MASK)>>16U;
-
+	/* Get the GicP mask */
+	GicPVal = (PlmIntrId & XPLMI_GICP_MASK) >> 8U;
+	GicPxVal = (PlmIntrId & XPLMI_GICPX_MASK) >> 16U;
 	/* Disable interrupt */
-	XPlmi_UtilRMW(PMC_GLOBAL_GICP0_IRQ_DISABLE + (GicPVal*0x14),
+	XPlmi_UtilRMW(PMC_GLOBAL_GICP0_IRQ_DISABLE + (GicPVal * XPLMI_GICPX_LEN),
 		1U<<GicPxVal,
 		1U<<GicPxVal);
 }
 
 /*****************************************************************************/
 /**
- * @brief This is handler for GIC interrupts
+ * @brief	This is handler for GIC interrupts.
  *
  * @param	CallbackRef is presently the interrupt number that is received
  *
- * @return	void
+ * @return	None
  *
  *****************************************************************************/
 void XPlmi_GicIntrHandler(void *CallbackRef)
@@ -262,65 +169,100 @@ void XPlmi_GicIntrHandler(void *CallbackRef)
 
 	(void )CallbackRef;
 
-	GicPIntrStatus = XPlmi_In32(XPLMI_GICP_IRQ_STATUS);
+	GicPIntrStatus = XPlmi_In32(PMC_GLOBAL_GICP_PMC_IRQ_STATUS);
 	XPlmi_Printf(DEBUG_DETAILED,
-		     "GicPIntrStatus: 0x%x\r\n", GicPIntrStatus);
+			"GicPIntrStatus: 0x%x\r\n", GicPIntrStatus);
 
 	for (GicIndex = 0U; GicIndex < XPLMI_GICP_SOURCE_COUNT; GicIndex++) {
-
 		if (GicPIntrStatus & (1U << GicIndex)) {
 			GicPNIntrStatus =
-			  XPlmi_In32(XPLMI_GICP0_IRQ_STATUS + (GicIndex*0x14));
+				XPlmi_In32(PMC_GLOBAL_GICP0_IRQ_STATUS +
+				(GicIndex * XPLMI_GICPX_LEN));
 			GicPNIntrMask =
-			  XPlmi_In32(XPLMI_GICP0_IRQ_MASK + (GicIndex*0x14));
+				XPlmi_In32(PMC_GLOBAL_GICP0_IRQ_MASK +
+				(GicIndex * XPLMI_GICPX_LEN));
 			XPlmi_Printf(DEBUG_DETAILED,
-				     "GicP%d Intr Status: 0x%x\r\n",
-				     GicIndex, GicPNIntrStatus);
+				"GicP%u Intr Status: 0x%x\r\n",
+				GicIndex, GicPNIntrStatus);
 
-			for (GicPIndex = 0U; GicPIndex <
-			     XPLMI_NO_OF_BITS_IN_REG; GicPIndex++) {
-
-				if ((GicPNIntrStatus & (1U<<GicPIndex)) &&
-				   ((GicPNIntrMask & (1U<<GicPIndex)) == 0U) ) {
-
+			for (GicPIndex = 0U;
+				GicPIndex < XPLMI_NO_OF_BITS_IN_REG; GicPIndex++) {
+				if ((GicPNIntrStatus & (1U << GicPIndex)) &&
+					((GicPNIntrMask & (1U << GicPIndex)) == 0U) ) {
 					if(g_GicPInterruptTable[GicIndex][GicPIndex].GicHandler != NULL) {
-						g_GicPInterruptTable[GicIndex][GicPIndex].GicHandler(
-							g_GicPInterruptTable[GicIndex][GicPIndex].Data);
+						XPlmi_GicIntrAddTask((GicIndex << 8U) |
+							(GicPIndex << 16U));
+						XPlmi_Out32((PMC_GLOBAL_GICP0_IRQ_DISABLE +
+							(GicIndex * XPLMI_GICPX_LEN)),
+							(1U << GicPIndex));
+					} else {
+						XPlmi_Printf(DEBUG_GENERAL,
+						"%s: Error: Unhandled GIC"
+						"interrupt received\n\r", __func__);
 					}
-					else {
-						XPlmi_Printf(DEBUG_GENERAL, "%s: Error: Unhandled GIC interrupt received\n\r", __func__);
-					}
-
-					XPlmi_Out32((XPLMI_GICP0_IRQ_STATUS +
-						    (GicIndex*0x14U)),
-						    (1U<<GicPIndex));
-
+					XPlmi_Out32((PMC_GLOBAL_GICP0_IRQ_STATUS +
+						(GicIndex * XPLMI_GICPX_LEN)),
+						(1U << GicPIndex));
 				}
 			}
-			XPlmi_Out32(XPLMI_GICP_IRQ_STATUS, (1U << GicIndex));
+			XPlmi_Out32(PMC_GLOBAL_GICP_PMC_IRQ_STATUS, (1U << GicIndex));
 		}
 	}
 
+	return;
 }
 
 /*****************************************************************************/
 /**
- * @brief This is the handler for wake up interrupts
+ * @brief	This function adds the GiCTaskHandler to the TaskQueue.
  *
- * @param  DeviceIdx	Index of peripheral device
- *
- * @return Status	XST_SUCCESS if processor wake successfully
- *			XST_FAILURE or error code in case of failure
+ * @param	Index is the interrupt index with GicPx and its corresponding
+ *              bit details.
+ * @return	None
  *
  *****************************************************************************/
-int XPlmi_DispatchWakeHandler(void *DeviceIdx)
+void XPlmi_GicIntrAddTask(u32 Index)
 {
-	int Status;
+	XPlmi_TaskNode *Task;
 
-	Status = XPm_GicProxyWakeUp((int)DeviceIdx);
-	if (XST_SUCCESS != Status) {
-		XPlmi_Printf(DEBUG_GENERAL, "%s: Error: Unhandled Wake interrupt receieved\n\r", __func__);
+	Task = XPlmi_TaskCreate(XPLM_TASK_PRIORITY_0,
+			XPlmi_GicTaskHandler, (void *)Index);
+	if (Task == NULL) {
+		XPlmi_Printf(DEBUG_GENERAL, "GIC Intr Task Creation Err\n\r");
+		goto END;
 	}
+	XPlmi_TaskTriggerNow(Task);
+
+END:
+	return;
+}
+
+/*****************************************************************************/
+/**
+ * @brief	This is GicTaskhandler with executes the registered handler. In
+ * case of SUCCESS or error, IRQ status is cleared and enabled again.
+ * If the task is in still progress, then interrupt is not cleared and Task
+ * handler is called again from TaskDispatcher.
+ *
+ * @param	Arg is index of the interrupt handler.
+ *
+ * @return	None
+ *
+ *****************************************************************************/
+int XPlmi_GicTaskHandler(void *Arg)
+{
+	int Status = XST_FAILURE;
+	u32 Index = (u32)Arg;
+	u32 GicIndex = (Index & XPLMI_GICP_MASK) >> 8U;
+	u32 GicPIndex = (Index & XPLMI_GICPX_MASK) >> 16U;
+
+	/*
+	 * Call Gic interrupt handler. GIC interrupt handlers should follow
+	 * the return values as followed by task handler.
+	 * Interrupt clear and enable should be done by Handler only.
+	 */
+	Status = g_GicPInterruptTable[GicIndex][GicPIndex].GicHandler(
+				g_GicPInterruptTable[GicIndex][GicPIndex].Data);
 
 	return Status;
 }
--- lib/sw_services/xilplmi/src/xplmi_gic_interrupts.h
+++ lib/sw_services/xilplmi/src/xplmi_gic_interrupts.h
@@ -1,28 +1,8 @@
 /******************************************************************************
-*
-* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
-*
+* Copyright (c) 2019 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 
 /*****************************************************************************/
 /**
@@ -37,6 +17,7 @@
 * Ver   Who  Date        Changes
 * ----- ---- -------- -------------------------------------------------------
 * 1.00  mg   10/08/2018 Initial release
+* 1.01  mg   04/02/2020 Remove defines which are already part of pmc_global.h
 *
 * </pre>
 *
@@ -55,100 +36,98 @@ extern "C" {
 #include "xplmi_ipi.h"
 
 /************************** Constant Definitions *****************************/
-#define XPLMI_GICP_IRQ_STATUS				0xF11400A0
-#define XPLMI_GICP0_IRQ_STATUS				0xF1140000
-#define XPLMI_GICP0_IRQ_MASK				0xF1140004
-#define XPLMI_GICP_SOURCE_COUNT				0x5
-#define XPLMI_NO_OF_BITS_IN_REG				32U
+#define XPLMI_GICP_SOURCE_COUNT				(0x5U)
+#define XPLMI_NO_OF_BITS_IN_REG				(32U)
 
 #define XPLMI_GICP_MASK			(0xFF00U)
 #define XPLMI_GICPX_MASK		(0xFF0000U)
+#define XPLMI_GICPX_LEN			(0x14U)
 
-/**
+/*
  * PMC GIC interrupts
  */
 enum {
-	XPLMI_PMC_GIC_IRQ_GICP0 = 0,
-	XPLMI_PMC_GIC_IRQ_GICP1,
-	XPLMI_PMC_GIC_IRQ_GICP2,
-	XPLMI_PMC_GIC_IRQ_GICP3,
-	XPLMI_PMC_GIC_IRQ_GICP4,
+	XPLMI_PMC_GIC_IRQ_GICP0 = 0U,
+	XPLMI_PMC_GIC_IRQ_GICP1, /**< 1U */
+	XPLMI_PMC_GIC_IRQ_GICP2, /**< 2U */
+	XPLMI_PMC_GIC_IRQ_GICP3, /**< 3U */
+	XPLMI_PMC_GIC_IRQ_GICP4, /**< 4U */
 };
 
-/**
+/*
  * PMC GICP0 interrupts
  */
 enum {
-	XPLMI_GICP0_SRC13 = 13, /**< GPIO Interrupt */
-	XPLMI_GICP0_SRC14 = 14, /**< I2C_0 Interrupt */
-	XPLMI_GICP0_SRC15 = 15, /**< I2C_1 Interrupt */
-	XPLMI_GICP0_SRC16 = 16, /**< SPI_0 Interrupt */
-	XPLMI_GICP0_SRC17 = 17, /**< SPI_1 Interrupt */
-	XPLMI_GICP0_SRC18 = 18, /**< UART_0 Interrupt */
-	XPLMI_GICP0_SRC19 = 19, /**< UART_1 Interrupt */
-	XPLMI_GICP0_SRC20 = 20, /**< CAN_0 Interrupt */
-	XPLMI_GICP0_SRC21 = 21, /**< CAN_1 Interrupt */
-	XPLMI_GICP0_SRC22 = 22, /**< USB_0 Interrupt */
-	XPLMI_GICP0_SRC23 = 23, /**< USB_0 Interrupt */
-	XPLMI_GICP0_SRC24 = 24, /**< USB_0 Interrupt */
-	XPLMI_GICP0_SRC25 = 25, /**< USB_0 Interrupt */
-	XPLMI_GICP0_SRC26 = 26, /**< USB_0 Interrupt */
-	XPLMI_GICP0_SRC27 = 27, /**< IPI Interrupt */
+	XPLMI_GICP0_SRC13 = 13U, /**< GPIO Interrupt */
+	XPLMI_GICP0_SRC14 = 14U, /**< I2C_0 Interrupt */
+	XPLMI_GICP0_SRC15 = 15U, /**< I2C_1 Interrupt */
+	XPLMI_GICP0_SRC16 = 16U, /**< SPI_0 Interrupt */
+	XPLMI_GICP0_SRC17 = 17U, /**< SPI_1 Interrupt */
+	XPLMI_GICP0_SRC18 = 18U, /**< UART_0 Interrupt */
+	XPLMI_GICP0_SRC19 = 19U, /**< UART_1 Interrupt */
+	XPLMI_GICP0_SRC20 = 20U, /**< CAN_0 Interrupt */
+	XPLMI_GICP0_SRC21 = 21U, /**< CAN_1 Interrupt */
+	XPLMI_GICP0_SRC22 = 22U, /**< USB_0 Interrupt */
+	XPLMI_GICP0_SRC23 = 23U, /**< USB_0 Interrupt */
+	XPLMI_GICP0_SRC24 = 24U, /**< USB_0 Interrupt */
+	XPLMI_GICP0_SRC25 = 25U, /**< USB_0 Interrupt */
+	XPLMI_GICP0_SRC26 = 26U, /**< USB_0 Interrupt */
+	XPLMI_GICP0_SRC27 = 27U, /**< IPI Interrupt */
 };
 
-/**
+/*
  * PMC GICP1 interrupts
  */
 enum {
-	XPLMI_GICP1_SRC5 = 5, /**< TTC_0 Interrupt */
-	XPLMI_GICP1_SRC6 = 6, /**< TTC_0 Interrupt */
-	XPLMI_GICP1_SRC7 = 7, /**< TTC_0 Interrupt */
-	XPLMI_GICP1_SRC8 = 8, /**< TTC_1 Interrupt */
-	XPLMI_GICP1_SRC9 = 9, /**< TTC_1 Interrupt */
-	XPLMI_GICP1_SRC10 = 10, /**< TTC_1 Interrupt */
-	XPLMI_GICP1_SRC11 = 11, /**< TTC_2 Interrupt */
-	XPLMI_GICP1_SRC12 = 12, /**< TTC_2 Interrupt */
-	XPLMI_GICP1_SRC13 = 13, /**< TTC_2 Interrupt */
-	XPLMI_GICP1_SRC14 = 14, /**< TTC_3 Interrupt */
-	XPLMI_GICP1_SRC15 = 15, /**< TTC_3 Interrupt */
-	XPLMI_GICP1_SRC16 = 16, /**< TTC_3 Interrupt */
-	XPLMI_GICP1_SRC24 = 24, /**< GEM_0 Interrupt */
-	XPLMI_GICP1_SRC25 = 25, /**< GEM_0 Interrupt */
-	XPLMI_GICP1_SRC26 = 26, /**< GEM_1 Interrupt */
-	XPLMI_GICP1_SRC27 = 27, /**< GEM_1 Interrupt */
-	XPLMI_GICP1_SRC28 = 28, /**< ADMA_0 Interrupt */
-	XPLMI_GICP1_SRC29 = 29, /**< ADMA_1 Interrupt */
-	XPLMI_GICP1_SRC30 = 30, /**< ADMA_2 Interrupt */
-	XPLMI_GICP1_SRC31 = 31, /**< ADMA_3 Interrupt */
+	XPLMI_GICP1_SRC5 = 5U, /**< TTC_0 Interrupt */
+	XPLMI_GICP1_SRC6 = 6U, /**< TTC_0 Interrupt */
+	XPLMI_GICP1_SRC7 = 7U, /**< TTC_0 Interrupt */
+	XPLMI_GICP1_SRC8 = 8U, /**< TTC_1 Interrupt */
+	XPLMI_GICP1_SRC9 = 9U, /**< TTC_1 Interrupt */
+	XPLMI_GICP1_SRC10 = 10U, /**< TTC_1 Interrupt */
+	XPLMI_GICP1_SRC11 = 11U, /**< TTC_2 Interrupt */
+	XPLMI_GICP1_SRC12 = 12U, /**< TTC_2 Interrupt */
+	XPLMI_GICP1_SRC13 = 13U, /**< TTC_2 Interrupt */
+	XPLMI_GICP1_SRC14 = 14U, /**< TTC_3 Interrupt */
+	XPLMI_GICP1_SRC15 = 15U, /**< TTC_3 Interrupt */
+	XPLMI_GICP1_SRC16 = 16U, /**< TTC_3 Interrupt */
+	XPLMI_GICP1_SRC24 = 24U, /**< GEM_0 Interrupt */
+	XPLMI_GICP1_SRC25 = 25U, /**< GEM_0 Interrupt */
+	XPLMI_GICP1_SRC26 = 26U, /**< GEM_1 Interrupt */
+	XPLMI_GICP1_SRC27 = 27U, /**< GEM_1 Interrupt */
+	XPLMI_GICP1_SRC28 = 28U, /**< ADMA_0 Interrupt */
+	XPLMI_GICP1_SRC29 = 29U, /**< ADMA_1 Interrupt */
+	XPLMI_GICP1_SRC30 = 30U, /**< ADMA_2 Interrupt */
+	XPLMI_GICP1_SRC31 = 31U, /**< ADMA_3 Interrupt */
 };
 
-/**
+/*
  * PMC GICP2 interrupts
  */
 enum {
-	XPLMI_GICP2_SRC0 = 0, /**< ADMA_4 Interrupt */
-	XPLMI_GICP2_SRC1 = 1, /**< ADMA_5 Interrupt */
-	XPLMI_GICP2_SRC2 = 2, /**< ADMA_6 Interrupt */
-	XPLMI_GICP2_SRC3 = 3, /**< ADMA_7 Interrupt */
-	XPLMI_GICP2_SRC10 = 10, /**< USB_0 Interrupt */
+	XPLMI_GICP2_SRC0 = 0U, /**< ADMA_4 Interrupt */
+	XPLMI_GICP2_SRC1 = 1U, /**< ADMA_5 Interrupt */
+	XPLMI_GICP2_SRC2 = 2U, /**< ADMA_6 Interrupt */
+	XPLMI_GICP2_SRC3 = 3U, /**< ADMA_7 Interrupt */
+	XPLMI_GICP2_SRC10 = 10U, /**< USB_0 Interrupt */
 };
 
-/**
+/*
  * PMC GICP3 interrupts
  */
 enum {
-	XPLMI_GICP3_SRC30 = 30, /**< SD_0 Interrupt */
-	XPLMI_GICP3_SRC31 = 31, /**< SD_0 Interrupt */
+	XPLMI_GICP3_SRC30 = 30U, /**< SD_0 Interrupt */
+	XPLMI_GICP3_SRC31 = 31U, /**< SD_0 Interrupt */
 };
 
-/**
+/*
  * PMC GICP4 interrupts
  */
 enum {
-	XPLMI_GICP4_SRC0 = 0, /**< SD_1 Interrupt */
-	XPLMI_GICP4_SRC1 = 1, /**< SD_1 Interrupt */
-	XPLMI_GICP4_SRC8 = 8, /**< SBI interrupt */
-	XPLMI_GICP4_SRC14 = 14, /**< RTC interrupt */
+	XPLMI_GICP4_SRC0 = 0U, /**< SD_1 Interrupt */
+	XPLMI_GICP4_SRC1 = 1U, /**< SD_1 Interrupt */
+	XPLMI_GICP4_SRC8 = 8U, /**< SBI interrupt */
+	XPLMI_GICP4_SRC14 = 14U, /**< RTC interrupt */
 };
 
 /**************************** Type Definitions *******************************/
@@ -158,16 +137,19 @@ struct GicIntrHandlerTable {
 	void *Data;
 	Function_t GicHandler;
 };
+
 /***************** Macros (Inline Functions) Definitions *********************/
 
 /************************** Function Prototypes ******************************/
-
 /* Functions defined in xplm_main.c */
 void XPlmi_GicIntrHandler(void *CallbackRef);
 void XPlmi_GicRegisterHandler(u32 PlmIntrId, Function_t Handler, void *Data);
 void XPlmi_GicIntrEnable(u32 PlmIntrId);
 void XPlmi_GicIntrDisable(u32 PlmIntrId);
+void XPlmi_GicIntrClearStatus(u32 PlmIntrId);
 int XPlmi_DispatchWakeHandler(void *DeviceIdx);
+void XPlmi_GicIntrAddTask(u32 Index);
+int XPlmi_GicTaskHandler(void *Arg);
 
 /************************** Variable Definitions *****************************/
 
