eef360abf7dab556974c8d8e493240809b40228c
b.vikram@xilinx.com <b.vikram@xilinx.com>
2019-04-14 08:20:27 -0400

    sw_services :xilloader: Reset value of CpusRunning during Subsystem restart
    This is done by explicitly assigning CpusRunning variable to 0 in
    XLoader_RestartImage.
    
    Signed-off-by: Vikram Sreenivasa Batchali <b.vikram@xilinx.com>
    Acked-by: Krishna Chaitanya Patakamuri <krishna.chaitanya.patakamuri@xilinx.com>
    
    Acked-by: Krishna Chaitanya Patakamuri <krishna.chaitanya.patakamuri@xilinx.com>

--- lib/sw_services/xilloader/src/xloader.c
+++ lib/sw_services/xilloader/src/xloader.c
@@ -884,6 +884,7 @@ int XLoader_RestartImage(u32 ImageId)
 		goto END;
 	}
 
+	SubSystemInfo.PdiPtr->CpusRunning = 0U;
 	Status = XLoader_StartImage(SubSystemInfo.PdiPtr);
 	if (Status != XST_SUCCESS) {
 		goto END;
