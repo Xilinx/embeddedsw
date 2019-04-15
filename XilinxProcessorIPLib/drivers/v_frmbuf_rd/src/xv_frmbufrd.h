cd585f06b9992b8d4c4fe183732d7c2fddb3a85b
Vivek Veenam <vivek.veenam@xilinx.com>
2019-04-15 18:16:44 +0530

    v_frmbuf_rd: Correct the format order in config structure
    The order of formats defined in the config structure and the order in
    the xv_frmbufrd_g.c file are not same. Because of this mismatch some
    formats even though enabled from the GUI are not getting enabled in the
    driver. Corrected the order in the config structure.
    
    Signed-off-by: Vivek Veenam <vveenam@xilinx.com>
    
    Acked by : Siva Rajesh Jarugula <sivaraj@xilinx.com>

--- XilinxProcessorIPLib/drivers/v_frmbuf_rd/src/xv_frmbufrd.h
+++ XilinxProcessorIPLib/drivers/v_frmbuf_rd/src/xv_frmbufrd.h
@@ -74,12 +74,12 @@ typedef struct {
   u16 RGBX12En;             /**< RGBX12     support */
   u16 RGB16En;              /**< RGB16      support */
   u16 YUVX12En;             /**< YUVX12     support */
-  u16 YUV16En;              /**< YUV16      support */
   u16 Y_UV12En;             /**< Y_UV12     support */
-  u16 Y_UV16En;             /**< Y_UV16     support */
   u16 Y_UV12_420En;         /**< Y_UV12_420 support */
-  u16 Y_UV16_420En;         /**< Y_UV16_420 support */
   u16 Y12En;                /**< Y12      support */
+  u16 YUV16En;              /**< YUV16      support */
+  u16 Y_UV16En;             /**< Y_UV16     support */
+  u16 Y_UV16_420En;         /**< Y_UV16_420 support */
   u16 Y16En;                /**< Y16      support */
   u16 Interlaced;           /**< Interlaced support */
 } XV_frmbufrd_Config;
