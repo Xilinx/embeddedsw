bf87d26368c7b38771827431c2faa0af0f2d3dce
Jyotheeswar Reddy <jyotheeswar.reddy.mutthareddyvari@xilinx.com>
2019-04-25 16:05:18 -0700

    xillibpm:aie: Add AIE mem init implementation
    AIE MBIST needs to be followed by memory scrubbing for ecc enabled
    memories and zeroization for parity protected memories, to avoid
    ecc errors.
    
    Signed-off-by: Jyotheeswar Reddy Mutthareddyvari <jyotheeswar.reddy.mutthareddyvari@xilinx.com>
    Acked-by: Jolly Shah <jolly.shah@xilinx.com>
    
    Acked-by: Jolly Shah <jolly.shah@xilinx.com>

--- lib/sw_services/xillibpm/src/server/xpm_aie.c
+++ lib/sw_services/xillibpm/src/server/xpm_aie.c
@@ -31,6 +31,9 @@
 #include "xpm_regs.h"
 #include "xpm_bisr.h"
 
+#include "xplmi_util.h"
+#include "xplmi_dma.h"
+
 #define ME_PCSR_KEY 0xF9E8D7C6U
 #define AIE_POLL_TIMEOUT 0X1000000U
 
@@ -52,16 +55,221 @@
 #define ME_NPI_REG_PCSR_MASK_GATEREG_MASK    0x00000002U
 #define ME_NPI_REG_PCSR_MASK_PCOMPLETE_MASK    0x00000001U
 
+#define ME_NPI_REG_PCSR_MASK_MEM_CLEAR_EN_ALL_MASK 0x00800000U
+#define ME_NPI_REG_PCSR_MASK_OD_BIST_SETUP_1_MASK 0x00400000U
+#define ME_NPI_REG_PCSR_MASK_OD_MBIST_ASYNC_RESET_N_MASK 0x00200000U
+#define ME_NPI_REG_PCSR_MASK_MEM_CLEAR_TRIGGER_MASK 0x00040000U
+
+#define ME_NPI_REG_PCSR_STATUS_MEM_CLEAR_PASS_MASK    0x00000080U
+#define ME_NPI_REG_PCSR_STATUS_MEM_CLEAR_DONE_MASK    0x00000040U
+
 #define ME_NPI_REG_PCSR_MASK_ODISABLE_SHIFT   2U
 #define ME_NPI_REG_PCSR_MASK_ODISABLE_0_MASK  (1U << (ME_NPI_REG_PCSR_MASK_ODISABLE_SHIFT + 0))
 #define ME_NPI_REG_PCSR_MASK_ODISABLE_1_MASK  (1U << (ME_NPI_REG_PCSR_MASK_ODISABLE_SHIFT + 1))
 
+#define COL_SHIFT 23U
+#define ROW_SHIFT 18U
+#define TILE_BASEADDRESS(col, row) ((u64)0x20000000000U +\
+			((u64)col<<COL_SHIFT)+\
+			((u64)row<<ROW_SHIFT))
 
+#define AIE_CORE_CONTROL_OFFSET 0x00032000U
+#define AIE_CORE_STATUS_OFFSET 0x00032004U
+#define AIE_CORE_ECC_SCRUB_EVENT_OFFSET 0x00032110U
+
+#define AIE_CORE_CONTROL_ENABLE_MASK (1U<<0U)
+#define AIE_CORE_CONTROL_RESET_MASK (1U<<1U)
+#define AIE_CORE_STATUS_DONE_MASK   (1U<<20U)
+
+#define AIE_PROGRAM_MEM_OFFSET 0x00020000U
+
+
+#define AieWrite64(addr, val) swea(addr, val)
+
+
+
+/* Buffer to hold AIE data memory zeroization elf*/
+static u32 ProgramMem[] __attribute__ ((aligned(16))) = {
+	0x0600703fU,
+	0x0a000804U,
+	0x000018c0U,
+	0x603803f7U,
+	0x00000203U,
+	0x400c9803U,
+	0x13201803U,
+	0x31009803U,
+	0x200003f7U,
+	0x00000277U,
+	0x800003f7U,
+	0x00000257U,
+	0x00000000U,
+	0x39200000U,
+	0x0000003dU,
+	0x00000000U,
+	0x00000000U,
+	0x00000000U,
+	0x40000000U,
+	0x00001888U,
+	0x00000000U,
+	0x00000000U,
+	0x00000000U,
+	0x0000079aU,
+	0x00000000U,
+	0x00000000U,
+	0x00000000U,
+	0x00002614U,
+	0x00000000U,
+	0x00000000U,
+	0x07428800U,
+	0x00000000U,
+	0x00010001U,
+	0x00010001U,
+	0x00030001U,
+	0x00011000U,
+	0x00010001U,
+	0x00010001U,
+	0x00010001U,
+	0x00010001U,
+	};
+
+
+
+struct AieArray {
+	u32 NpiAddress;
+	u64 NocAddress;
+	u32 NumCols;
+	u32 NumRows;
+	u32 StartCol;
+	u32 StartRow;
+	u8 IsSecure;
+};
+
+static struct AieArray AieInst = {
+	.NpiAddress = 0xF70A0000U,
+	.NocAddress = 0x20000000000U,
+	.NumCols = 50U,
+	.NumRows = 8U,
+	.StartCol = 0U,
+	.StartRow = 1U,
+	.IsSecure = 0U,
+};
+
+/*****************************************************************************/
+/**
+ * This function is used to set/clear bits in AIE PCSR
+ *
+ * @param Mask Mask to be written into PCSR_MASK register
+ * @param Value Value to be written into PCSR_CONTROL register
+ * @return
+ *****************************************************************************/
 static void AiePcsrWrite(u32 Mask, u32 Value)
 {
 	PmOut32(ME_NPI_REG_PCSR_MASK, Mask);
 	PmOut32(ME_NPI_REG_PCSR_CONTROL, Value);
 }
+
+/*****************************************************************************/
+/**
+ * This function is used to enable AIE Core
+ *
+ * @param Col Column index of the Core
+ * @param Row Row index of the Core
+ * @return
+ *****************************************************************************/
+static void AieCoreEnable(u32 Col, u32 Row)
+{
+    /* Release reset to the Core */
+	AieWrite64(TILE_BASEADDRESS(Col, Row) + AIE_CORE_CONTROL_OFFSET, 0U);
+
+	/* Enable the Core */
+	AieWrite64(TILE_BASEADDRESS(Col, Row) + AIE_CORE_CONTROL_OFFSET, 1U);
+}
+
+/*****************************************************************************/
+/**
+ * This function waits for a Core's DONE bit to be set
+ *
+ * @param Col Column index of the Core
+ * @param Row Row index of the Core
+ * @return Status Code
+ *****************************************************************************/
+static XStatus AieWaitForCoreDone(u32 Col, u32 Row)
+{
+	u64 StatusRegAddr = TILE_BASEADDRESS(Col, Row) + AIE_CORE_STATUS_OFFSET;
+	XStatus Status;
+
+	Status = XPlmi_UtilPollForMask64((u32)(StatusRegAddr>>32),
+				(u32)(StatusRegAddr), AIE_CORE_STATUS_DONE_MASK, 10U);
+	if (Status != XST_SUCCESS) {
+		PmInfo("ERROR: Poll for Done timeout \r\n");
+	}
+
+	return Status;
+}
+
+/*****************************************************************************/
+/**
+ * This function loads a core's program memory with zeroization elf
+ *
+ * @param Col Column index of the Core
+ * @param Row Row index of the Core
+ * @return Status Code
+ *****************************************************************************/
+static XStatus ProgramCore(u32 Col, u32 Row, u32 *PrgData, u32 NumOfWords)
+{
+	u64 PrgAddr = TILE_BASEADDRESS(Col, Row) + AIE_PROGRAM_MEM_OFFSET;
+
+	return XPlmi_DmaXfr((u64)(0U)|(u32)PrgData, PrgAddr, NumOfWords, XPLMI_PMCDMA_0);
+}
+
+/*****************************************************************************/
+/**
+ * This function is used to cycle reset to the entire AIE array
+ *
+ * @return
+ *****************************************************************************/
+static void ArrayReset(void)
+{
+	AiePcsrWrite(ME_NPI_REG_PCSR_MASK_ME_ARRAY_RESET_MASK,
+					ME_NPI_REG_PCSR_MASK_ME_ARRAY_RESET_MASK);
+	AiePcsrWrite(ME_NPI_REG_PCSR_MASK_ME_ARRAY_RESET_MASK, 0U);
+}
+
+/*****************************************************************************/
+/**
+ * This function is used to scrub ECC enabled memories in the entire AIE array
+ *
+ * @return
+ *****************************************************************************/
+static void TriggerEccScrub(void)
+{
+	u32 row, col;
+
+	for (col = AieInst.StartCol; col < (AieInst.StartCol + AieInst.NumCols); col++) {
+		for (row = AieInst.StartRow; row < (AieInst.StartRow + AieInst.NumRows); row++) {
+			AieWrite64(TILE_BASEADDRESS(col, row) + AIE_CORE_ECC_SCRUB_EVENT_OFFSET, 1U);
+		}
+	}
+}
+
+
+static XStatus MemInit(void)
+{
+	u32 row = AieInst.StartRow;
+	u32 col = AieInst.StartCol;
+
+	for (col = AieInst.StartCol; col < (AieInst.StartCol + AieInst.NumCols); col++) {
+		for (row = AieInst.StartRow; row < (AieInst.StartRow + AieInst.NumRows); row++) {
+			PmDbg("---------- (%d, %d)----------\r\n", col, row);
+			ProgramCore(col, row, &ProgramMem[0], ARRAY_SIZE(ProgramMem));
+			AieCoreEnable(col, row);
+		}
+	}
+
+	return AieWaitForCoreDone(col-1, row-1);
+
+}
+
 
 
 static XStatus AiePreHouseclean(u32 *Args, u32 NumOfArgs)
@@ -179,25 +387,74 @@ static XStatus AieMbistClear(u32 *Args, u32 NumOfArgs)
 {
 	XStatus Status = XST_SUCCESS;
 
-	/* TODO: Implement MBIST  */
+	/* This function does not use the args */
+	(void)Args;
+	(void)NumOfArgs;
+	if (Platform == PLATFORM_VERSION_SILICON) {
+		/* Assert MEM_CLEAR_EN_ALL */
+		AiePcsrWrite(ME_NPI_REG_PCSR_MASK_MEM_CLEAR_EN_ALL_MASK,
+			ME_NPI_REG_PCSR_MASK_MEM_CLEAR_EN_ALL_MASK);
+		/* De-assert OD_MBIST_ASYNC_RESET_N */
+		AiePcsrWrite(ME_NPI_REG_PCSR_MASK_OD_MBIST_ASYNC_RESET_N_MASK,
+			ME_NPI_REG_PCSR_MASK_OD_MBIST_ASYNC_RESET_N_MASK);
+		/* Assert OD_BIST_SETUP_1 */
+		AiePcsrWrite(ME_NPI_REG_PCSR_MASK_OD_BIST_SETUP_1_MASK,
+			ME_NPI_REG_PCSR_MASK_OD_BIST_SETUP_1_MASK);
+		/* Assert MEM_CLEAR_TRIGGER */
+		AiePcsrWrite(ME_NPI_REG_PCSR_MASK_MEM_CLEAR_TRIGGER_MASK,
+			ME_NPI_REG_PCSR_MASK_MEM_CLEAR_TRIGGER_MASK);
+
+		/* Wait for Mem Clear DONE */
+		Status = XPm_PollForMask(ME_NPI_REG_PCSR_STATUS,
+					ME_NPI_REG_PCSR_STATUS_MEM_CLEAR_DONE_MASK,
+					AIE_POLL_TIMEOUT);
+		if (Status != XST_SUCCESS) {
+			goto done;
+		}
+
+		/* Check Mem Clear PASS */
+		if ((XPm_In32(ME_NPI_REG_PCSR_STATUS) &
+			ME_NPI_REG_PCSR_STATUS_MEM_CLEAR_PASS_MASK) !=
+			ME_NPI_REG_PCSR_STATUS_MEM_CLEAR_PASS_MASK) {
+			Status = XST_FAILURE;
+			goto done;
+		}
+		/* Assert OD_MBIST_ASYNC_RESET_N */
+		AiePcsrWrite(ME_NPI_REG_PCSR_MASK_OD_MBIST_ASYNC_RESET_N_MASK, 0U);
+		/* De-assert OD_BIST_SETUP_1 */
+		AiePcsrWrite(ME_NPI_REG_PCSR_MASK_OD_BIST_SETUP_1_MASK, 0U);
+		/* De-assert MEM_CLEAR_TRIGGER */
+		AiePcsrWrite(ME_NPI_REG_PCSR_MASK_MEM_CLEAR_TRIGGER_MASK, 0U);
+	}
+done:
+	return Status;
+}
+
+static XStatus AieMemInit(u32 *Args, u32 NumOfArgs)
+{
+	XStatus Status = XST_FAILURE;
 
 	/* This function does not use the args */
 	(void)Args;
 	(void)NumOfArgs;
 
-        return Status;
-}
+	PmDbg("---------- START ----------\r\n");
 
-static XStatus AieMemInit(u32 *Args, u32 NumOfArgs)
-{
-	XStatus Status = XST_SUCCESS;
-
-        /* This function does not use the args */
-	(void)Args;
-	(void)NumOfArgs;
-
-	/* TODO: Implement memory zeroization */
-        return Status;
+	/* Scrub ECC protected memories */
+	TriggerEccScrub();
+	/* Wait for scrubbing to finish */
+	XPlmi_UtilWait(1000U);
+	/* Reset Array */
+	ArrayReset();
+	/* Zeroize Data Memory */
+	Status = MemInit();
+	if (Status != XST_SUCCESS) {
+		PmInfo("ERROR: MemInit failed\r\n");
+	}
+	/* Reset Array */
+	ArrayReset();
+	PmDbg("---------- END ----------\r\n");
+	return Status;
 }
 
 struct XPm_PowerDomainOps AieOps = {
@@ -212,6 +469,13 @@ struct XPm_PowerDomainOps AieOps = {
 XStatus XPmAieDomain_Init(XPm_AieDomain *AieDomain, u32 Id, u32 BaseAddress,
 			   XPm_Power *Parent)
 {
+	/* For SPP and EMU,  setup the array size */
+	if (Platform != PLATFORM_VERSION_SILICON) {
+		AieInst.NumCols = 7U;
+		AieInst.NumRows = 5U;
+		AieInst.StartCol = 6U;
+		AieInst.StartRow = 1U;
+	}
 	XPmPowerDomain_Init(&AieDomain->Domain, Id, BaseAddress, Parent, &AieOps);
 	return XST_SUCCESS;
 }
