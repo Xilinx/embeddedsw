514161c9e960906e2b31d4cd4336021636b2e849
Mohan Dhanawade <mohan.dhanawade@xilinx.com>
2019-05-28 19:26:12 +0530

    zynqmp_pmufw: Fixed buffer overflow issue
    This patch fixes buffer overflow when ENABLE_IPI_CRC is enabled.
    Function XPfw_IpiWriteMessage assumes IPI message length is 8 words and
    puts the CRC at the end.
    
    Signed-off-by: Mohan Marutirao Dhanawade <mohan.dhanawade@xilinx.com>
    CR-1032330
     lib/sw_apps/zynqmp_pmufw/src/pm_callbacks.c | 29 ----------------------------
     lib/sw_apps/zynqmp_pmufw/src/pm_callbacks.h | 30 +++++++++++++++++++++++++++++
     2 files changed, 30 insertions(+), 29 deletions(-)
    Acked-by: Mounika Akula <mounika.akula@xilinx.com>
    
    Acked-by: Mounika Akula <mounika.akula@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/pm_callbacks.h
+++ lib/sw_apps/zynqmp_pmufw/src/pm_callbacks.h
@@ -38,6 +38,36 @@ extern "C" {
 #include "pm_master.h"
 #include "xil_types.h"
 
+#define IPI_REQUEST1(mask, arg0)				\
+{	\
+	u32 _ipi_req_data[] = {(arg0), 0U, 0U, 0U, 0U, 0U, 0U, 0U};	\
+	(void)XPfw_IpiWriteMessage(PmModPtr, (mask), &_ipi_req_data[0], ARRAY_SIZE(_ipi_req_data));	\
+}
+
+#define IPI_REQUEST2(mask, arg0, arg1)				\
+{	\
+	u32 _ipi_req_data[] = {(arg0), (arg1), 0U, 0U, 0U, 0U, 0U, 0U};	\
+	(void)XPfw_IpiWriteMessage(PmModPtr, (mask), &_ipi_req_data[0], ARRAY_SIZE(_ipi_req_data));	\
+}
+
+#define IPI_REQUEST3(mask, arg0, arg1, arg2)			\
+{	\
+	u32 _ipi_req_data[] = {(arg0), (arg1), (arg2), 0U, 0U, 0U, 0U, 0U};	\
+	(void)XPfw_IpiWriteMessage(PmModPtr, (mask), &_ipi_req_data[0], ARRAY_SIZE(_ipi_req_data));	\
+}
+
+#define IPI_REQUEST4(mask, arg0, arg1, arg2, arg3)		\
+{	\
+	u32 _ipi_req_data[] = {(arg0), (arg1), (arg2), (arg3), 0U, 0U, 0U, 0U};	\
+	(void)XPfw_IpiWriteMessage(PmModPtr, (mask), &_ipi_req_data[0], ARRAY_SIZE(_ipi_req_data));	\
+}
+
+#define IPI_REQUEST5(mask, arg0, arg1, arg2, arg3, arg4)	\
+{	\
+	u32 _ipi_req_data[] = {(arg0), (arg1), (arg2), (arg3), (arg4), 0U, 0U, 0U};	\
+	(void)XPfw_IpiWriteMessage(PmModPtr, (mask), &_ipi_req_data[0], ARRAY_SIZE(_ipi_req_data));	\
+}
+
 void PmAcknowledgeCb(const PmMaster* const master, const PmNodeId nodeId,
 		     const u32 status, const u32 oppoint);
 
