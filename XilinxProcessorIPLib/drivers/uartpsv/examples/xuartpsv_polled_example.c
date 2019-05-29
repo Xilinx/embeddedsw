0b71e470f471440f0530104fd35f334797230b52
Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
2019-05-29 12:29:34 +0530

    xuartpsv: Add a polled mode example
    Add a polled mode example for the uart.
    The test sends and recieves data in a local loopback.
    
    Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
    Acked-By: Srinivas Goud <srinivas.goud@xilinx.com>
    
    Acked-By: Srinivas Goud <srinivas.goud@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/uartpsv/examples/xuartpsv_polled_example.c
@@ -0,0 +1,210 @@
+/******************************************************************************
+*
+* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* XILINX  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+* WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF
+* OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*
+* Except as contained in this notice, the name of the Xilinx shall not be used
+* in advertising or otherwise to promote the sale, use or other dealings in
+* this Software without prior written authorization from Xilinx.
+*
+******************************************************************************/
+/****************************************************************************/
+/**
+*
+* @file	xuartpsv_polled_example.c
+*
+* This file contains a design example using the XUartPsv driver in polled
+* mode. It sends data and expects to receive the same data through the device
+* using the local loopback mode.
+*
+*
+* @note
+* If the device does not work properly, the example may hang.
+*
+* MODIFICATION HISTORY:
+* <pre>
+* Ver   Who   Date     Changes
+* ----- ----- -------- ----------------------------------------------
+* 1.0   sd    05/23/19 First Release
+*
+* </pre>
+****************************************************************************/
+
+/***************************** Include Files *******************************/
+
+#include "xparameters.h"
+#include "xplatform_info.h"
+#include "xuartpsv.h"
+#include "xil_printf.h"
+
+/************************** Constant Definitions **************************/
+
+/*
+ * The following constants map to the XPAR parameters created in the
+ * xparameters.h file. They are defined here such that a user can easily
+ * change all the needed parameters in one place.
+ */
+#define UARTPSV_DEVICE_ID		XPAR_XUARTPSV_0_DEVICE_ID
+/*
+ * The following constant controls the length of the buffers to be sent
+ * and received with the UART,
+ */
+#define TEST_BUFFER_SIZE	10
+
+
+/**************************** Type Definitions ******************************/
+
+/************************** Function Prototypes *****************************/
+
+int UartPsvPolledExample(u16 DeviceId);
+
+/************************** Variable Definitions ***************************/
+
+XUartPsv UartPsv;		/* Instance of the UART Device */
+
+/*
+ * The following buffers are used in this example to send and receive data
+ * with the UART.
+ */
+static u8 SendBuffer[TEST_BUFFER_SIZE];	/* Buffer for Transmitting Data */
+static u8 RecvBuffer[TEST_BUFFER_SIZE];	/* Buffer for Receiving Data */
+
+/*
+ * The following counters are used to determine when the entire buffer has
+ * been sent and received.
+ */
+volatile int TotalReceivedCount;
+volatile int TotalSentCount;
+int TotalErrorCount;
+
+/**************************************************************************/
+/**
+*
+* Main function to call the Uart Polled mode example.
+*
+* @param	None
+*
+* @return	XST_SUCCESS if successful, XST_FAILURE if unsuccessful
+*
+* @note		None
+*
+**************************************************************************/
+#ifndef TESTAPP_GEN
+
+int main(void)
+{
+	int Status;
+
+	/* Run the UartPsv Polled example, specify the the Device ID */
+	Status = UartPsvPolledExample(UARTPSV_DEVICE_ID);
+	if (Status != XST_SUCCESS) {
+		xil_printf("UartPsv Polled Example Test Failed\r\n");
+		return XST_FAILURE;
+	}
+
+	xil_printf("Successfully ran UartPsv Polling Example Test\r\n");
+	return XST_SUCCESS;
+}
+#endif
+
+/**************************************************************************/
+/**
+*
+* This function does a minimal test on the UartPsv device and driver as a
+* design example. The purpose of this function is to illustrate
+* how to use the XUartPsv driver.
+*
+* This function does a minimal test on the XUartPs device in polled mode.
+* This function sends data and expects to receive the same data through the
+* device using the local loopback mode.
+*
+*
+* @param	DeviceId is the device Id of the UART device and is typically
+*		XPAR_<UartPsv_instance>_DEVICE_ID value from xparameters.h.
+*
+* @return	XST_SUCCESS if successful, otherwise XST_FAILURE.
+*
+* @note
+* This function polls the UART, it may hang if the hardware is not
+* working correctly.
+*
+**************************************************************************/
+int UartPsvPolledExample(u16 DeviceId)
+{
+	int Status;
+	XUartPsv_Config *Config;
+	int Index;
+	int BadByteCount = 0;
+	u8 SentCount;
+	unsigned int ReceivedCount;
+
+	/*
+	 * Initialize the UART driver so that it's ready to use.
+	 * Look up the configuration in the config table, then initialize it.
+	 */
+	Config = XUartPsv_LookupConfig(DeviceId);
+	if (NULL == Config) {
+		return XST_FAILURE;
+	}
+
+	Status = XUartPsv_CfgInitialize(&UartPsv, Config, Config->BaseAddress);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/* Use local loopback mode. */
+	XUartPsv_SetOperMode(&UartPsv, XUARTPSV_OPER_MODE_LOCAL_LOOP);
+
+	/*
+	 * Initialize the send buffer bytes with a pattern and the
+	 * the receive buffer bytes to zero to allow the receive data to be
+	 * verified
+	 */
+	for (Index = 0; Index < TEST_BUFFER_SIZE; Index++) {
+		SendBuffer[Index] = (Index % 26) + 'A';
+		RecvBuffer[Index] = 0;
+	}
+
+	TotalSentCount = XUartPsv_Send(&UartPsv, SendBuffer, TEST_BUFFER_SIZE);
+
+	while (XUartPsv_IsTransmitbusy(UartPsv.Config.BaseAddress));
+	ReceivedCount = 0;
+	while (ReceivedCount < TEST_BUFFER_SIZE) {
+		ReceivedCount +=
+		XUartPsv_Recv(&UartPsv, &RecvBuffer[ReceivedCount],
+				      (TEST_BUFFER_SIZE - ReceivedCount));
+	}
+	/* Verify the entire receive buffer was successfully received */
+	for (Index = 0; Index < TEST_BUFFER_SIZE; Index++) {
+		if (RecvBuffer[Index] != SendBuffer[Index]) {
+			BadByteCount++;
+		}
+	}
+
+	/* Set the UART in Normal Mode */
+	XUartPsv_SetOperMode(&UartPsv, XUARTPSV_OPER_MODE_NORMAL);
+
+	/* If any bytes were not correct, return an error */
+	if (BadByteCount != 0) {
+		return XST_FAILURE;
+	}
+
+	return XST_SUCCESS;
+}
