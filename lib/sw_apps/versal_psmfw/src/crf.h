50dcdcde889f70bdedd7df8426267db785d17884
Rajan Vaja <rajan.vaja@xilinx.com>
2019-05-31 04:38:54 -0700

    versal_psmfw: Correct reset masks
    Correct reset masks for ACPUx POR, L2 cache reset, APU GIC reset.
    
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    
    Acked-for-series: Jolly Shah <jolly.shah@xilinx.com>

--- lib/sw_apps/versal_psmfw/src/crf.h
+++ lib/sw_apps/versal_psmfw/src/crf.h
@@ -64,10 +64,10 @@ extern "C" {
 
 #define CRF_RST_APU_ACPU0_MASK                        ((u32)0x00000001U)
 #define CRF_RST_APU_ACPU1_MASK                        ((u32)0x00000002U)
-#define CRF_RST_APU_GIC_RESET_MASK                    ((u32)0x00000100U)
-#define CRF_RST_APU_L2_RESET_MASK                     ((u32)0x00000200U)
-#define CRF_RST_APU_ACPU0_PWRON_MASK                  ((u32)0x00001000U)
-#define CRF_RST_APU_ACPU1_PWRON_MASK                  ((u32)0x00002000U)
+#define CRF_RST_APU_GIC_RESET_MASK                    ((u32)0x00000080U)
+#define CRF_RST_APU_L2_RESET_MASK                     ((u32)0x00000100U)
+#define CRF_RST_APU_ACPU0_PWRON_MASK                  ((u32)0x00000400U)
+#define CRF_RST_APU_ACPU1_PWRON_MASK                  ((u32)0x00000800U)
 
 #define CRF_RST_APU_ACPU0_SHIFT                       (0)
 #define CRF_RST_APU_ACPU1_SHIFT                       (1)
