c9abbdb4e71e22f61ac733997976991fe20aba79
Saeed Nowshadi <saeed.nowshadi@xilinx.com>
2019-07-03 12:57:34 -0700

    xilplmi: Enable SysMon's over-temperature interrupt
    Enable OT interrupt of Root SysMon to generate an interrupt when value of
    DEVICE_TEMP_MAX_MAX register goes above DEVICE_TEMP_TH_UPPER.  Enable
    GIC4 proxy to route the interrupt and invoke an handler that shuts down the
    system.
    
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@xilinx.com>
    
    Acked-for-series: Jolly Shah <jolly.shah@xilinx.com>

--- lib/sw_services/xilplmi/src/xplmi_sysmon.c
+++ lib/sw_services/xilplmi/src/xplmi_sysmon.c
@@ -1,28 +1,8 @@
 /******************************************************************************
-*
-* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
-*
+* Copyright (c) 2019 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 
 /*****************************************************************************/
 /**
@@ -64,11 +44,11 @@ static XSysMonPsv *SysMonInstPtr = &SysMonInst;
 
 /*****************************************************************************/
 /**
- * @brief This function initializes the SysMon
+ * @brief	This function initializes the SysMon.
  *
  * @param	void
  *
- * @return	Status	SysMon initialization status
+ * @return	XST_SUCCESS on success and error code on failure
  *
  *****************************************************************************/
 int XPlmi_SysMonInit(void)
@@ -77,11 +57,7 @@ int XPlmi_SysMonInit(void)
 	XSysMonPsv_Config *ConfigPtr;
 
 	ConfigPtr = XSysMonPsv_LookupConfig();
-	if (ConfigPtr == NULL) {
-		goto END;
-	}
-	else
-	{
+	if (ConfigPtr != NULL) {
 		XSysMonPsv_CfgInitialize(SysMonInstPtr, ConfigPtr);
 
 		/*
@@ -90,56 +66,59 @@ int XPlmi_SysMonInit(void)
 		*/
 		XPlmi_Out32(ConfigPtr->BaseAddress + XSYSMONPSV_PCSR_LOCK,
 			PCSR_UNLOCK_VAL);
-		XSysMonPsv_IntrEnable(SysMonInstPtr, XSYSMONPSV_IER0_OT_MASK, 0);
-		XPlmi_Out32(ConfigPtr->BaseAddress + XSYSMONPSV_PCSR_LOCK, 0);
+		XSysMonPsv_IntrEnable(SysMonInstPtr, XSYSMONPSV_IER0_OT_MASK, 0U);
+		XPlmi_Out32(ConfigPtr->BaseAddress + XSYSMONPSV_PCSR_LOCK, 0U);
 		Status = XST_SUCCESS;
 	}
-
-END:
 	XPlmi_Printf(DEBUG_DETAILED,
-		    "%s: SysMon init status: 0x%x\n\r", __func__, Status);
+		 "%s: SysMon init status: 0x%x\n\r", __func__, Status);
+
 	return Status;
 }
 
 /*****************************************************************************/
 /**
- * @brief This function detects if we are still in over-temperature condition
+ * @brief	This function detects if we are still in over-temperature condition.
  *
- * @param	void
+ * @param	None
  *
- * @return	void
+ * @return	None
  *
  *****************************************************************************/
 void XPlmi_SysMonOTDetect(void)
 {
-	u32 Val, Count;
+	u32 Val;
+	u32 Count;
 
 	/*
 	 * If Interrupt Status Register does not indicate an over-temperature
 	 * condition, we are done.
 	 */
 	Val = XPlmi_In32((XSYSMONPSV_BASEADDR + XSYSMONPSV_ISR_OFFSET));
-	if (0 == (Val & XSYSMONPSV_ISR_OT_MASK)) {
-		return;
+	if (0U == (Val & XSYSMONPSV_ISR_OT_MASK)) {
+		goto END;
 	}
 
 	XPlmi_Out32(XSYSMONPSV_BASEADDR + XSYSMONPSV_PCSR_LOCK,
-		    PCSR_UNLOCK_VAL);
+		PCSR_UNLOCK_VAL);
 
 	/* Wait until over-temperature condition is resolved. */
-	Count = 1000;
-	while (0 != (Val & XSYSMONPSV_ISR_OT_MASK)) {
+	Count = 1000U;
+	while (0U != (Val & XSYSMONPSV_ISR_OT_MASK)) {
 		XPlmi_Out32((XSYSMONPSV_BASEADDR + XSYSMONPSV_ISR_OFFSET),
-			    XSYSMONPSV_ISR_OT_MASK);
-		usleep(1000);
+			XSYSMONPSV_ISR_OT_MASK);
+		usleep(1000U);
 		Count--;
-		if (0 == Count) {
-			XPlmi_Printf(DEBUG_PRINT_ALWAYS,
-				     "Warning: Over-temperature condition!\r\n");
-			Count = 1000;
+		if (0U == Count) {
+			XPlmi_Printf(DEBUG_GENERAL,
+				"Warning: Over-temperature condition!\r\n");
+			Count = 1000U;
 		}
 		Val = XPlmi_In32((XSYSMONPSV_BASEADDR + XSYSMONPSV_ISR_OFFSET));
 	}
 
 	XPlmi_Out32(XSYSMONPSV_BASEADDR + XSYSMONPSV_PCSR_LOCK, 0);
+
+END:
+	return;
 }
