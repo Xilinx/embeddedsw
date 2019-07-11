991678a0656f85e0a27ee954e3d558e71ffcc5ce
Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
2019-07-11 20:16:49 +0530

    xuartpsv: Update the interrupt example to use correct interrupt id
    Instead of hardcoding the interrupt ids use the macro.
    Fixes the interrupt id for versal.
    
    Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@xilinx.com>

--- XilinxProcessorIPLib/drivers/uartpsv/src/xuartpsv_options.c
+++ XilinxProcessorIPLib/drivers/uartpsv/src/xuartpsv_options.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2017 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2017-2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -27,7 +27,7 @@
 /**
 *
 * @file xuartpsv_options.c
-* @addtogroup uartpsv_v1_0
+* @addtogroup uartpsv_v1_1
 * @{
 *
 * The implementation of the options functions for the XUartPsv driver.
