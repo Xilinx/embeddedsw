df3679184c013352e7b330c144d7a2c0d01fb2c3
Mohan Dhanawade <mohan.dhanawade@xilinx.com>
2019-07-18 15:48:02 +0530

    zynqmp_pmufw: Modifies watchdog timer to avoid trip during secure operation
    This patch modifies the watchdog timer before performing the secure
    operations like secure image/bitstream  loading, RSA, AES and SHA3
    operations. By default it is assumed that maximum time required to
    do each of these operations is 500ms.
    
    Signed-off-by: Mohan Marutirao Dhanawade <mohan.dhanawade@xilinx.com>
    Acked-by: Mounika Akula <mounika.akula@xilinx.com>
    
    Acked-by: Mounika Akula <mounika.akula@xilinx.com>

--- lib/sw_services/xilpm/src/common/pm_defs.h
+++ lib/sw_services/xilpm/src/common/pm_defs.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2015-2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2015-2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -183,6 +183,7 @@ enum XPmApiCbId {
 	PM_INIT_SUSPEND_CB = 30,
 	PM_ACKNOWLEDGE_CB,
 	PM_NOTIFY_CB,
+	PM_NOTIFY_STL_NO_OP
 };
 
 /**
