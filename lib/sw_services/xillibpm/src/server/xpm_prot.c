15b1b90cb1215445d367ed7b2db6f55546e0404a
Jolly Shah <jolly.shah@xilinx.com>
2019-07-26 13:11:48 -0700

    xillibpm: configure protection runtime if enabled
    This patch adds support to configure xppu runtime based on
    requirements added by user.
    
    Signed-off-by: Jolly Shah <jolly.shah@xilinx.com>
    
    Acked-for-series: Rajan Vaja <rajan.vaja@xilinx.com>

--- lib/sw_services/xillibpm/src/server/xpm_prot.c
+++ lib/sw_services/xillibpm/src/server/xpm_prot.c
@@ -103,17 +103,17 @@ XStatus XPmProt_XppuEnable(u32 NodeId, u32 ApertureInitVal)
 	XPm_ProtPpu *PpuNode = (XPm_ProtPpu *)XPmProt_GetById(NodeId);
 	u32 Address, BaseAddr, RegVal;
 
-	if(PpuNode == NULL) {
+	if (PpuNode == NULL) {
 		Status = XST_FAILURE;
 		goto done;
 	}
 
 	BaseAddr = PpuNode->ProtNode.Node.BaseAddress;
 
-	if((PLATFORM_VERSION_SILICON == Platform) && (PLATFORM_VERSION_SILICON_ES1 == PlatformVersion)) {
+	if ((PLATFORM_VERSION_SILICON == Platform) && (PLATFORM_VERSION_SILICON_ES1 == PlatformVersion)) {
 		/* Disable permission checks for all apertures */
 		Address = BaseAddr + XPPU_ENABLE_PERM_CHECK_REG00_OFFSET;
-		for(i=0;i<MAX_PERM_REGS;i++)
+		for (i = 0; i < MAX_PERM_REGS; i++)
 		{
 			PmOut32(Address, 0x0);
 			Address = Address + 0x4;
@@ -137,10 +137,10 @@ XStatus XPmProt_XppuEnable(u32 NodeId, u32 ApertureInitVal)
 			Address = Address + 0x4;
 	}
 
-	if((PLATFORM_VERSION_SILICON == Platform) && (PLATFORM_VERSION_SILICON_ES1 == PlatformVersion)) {
+	if ((PLATFORM_VERSION_SILICON == Platform) && (PLATFORM_VERSION_SILICON_ES1 == PlatformVersion)) {
 		/* Enable permission checks for all apertures */
 		Address = BaseAddr + XPPU_ENABLE_PERM_CHECK_REG00_OFFSET;
-		for(i=0;i<MAX_PERM_REGS;i++)
+		for (i = 0; i < MAX_PERM_REGS; i++)
 		{
 			PmOut32(Address, 0xFFFFFFFF);
 			Address = Address + 0x4;
@@ -179,15 +179,15 @@ XStatus XPmProt_XppuDisable(u32 NodeId)
 	XPm_ProtPpu *PpuNode = (XPm_ProtPpu *)XPmProt_GetById(NodeId);
 	u32 Address, idx;
 
-	if(PpuNode == NULL) {
+	if (PpuNode == NULL) {
 		Status = XST_FAILURE;
 		goto done;
 	}
 
-	if((PLATFORM_VERSION_SILICON == Platform) && (PLATFORM_VERSION_SILICON_ES1 == PlatformVersion)) {
+	if ((PLATFORM_VERSION_SILICON == Platform) && (PLATFORM_VERSION_SILICON_ES1 == PlatformVersion)) {
 		/* Disable permission checks for all apertures */
 		Address = PpuNode->ProtNode.Node.BaseAddress + XPPU_ENABLE_PERM_CHECK_REG00_OFFSET;
-		for(idx=0;idx<MAX_PERM_REGS;idx++)
+		for (idx = 0; idx < MAX_PERM_REGS; idx++)
 		{
 			PmOut32(Address, 0x0);
 			Address = Address + 0x4;
@@ -198,6 +198,135 @@ XStatus XPmProt_XppuDisable(u32 NodeId)
 	PmRmw32(PpuNode->ProtNode.Node.BaseAddress + XPPU_CTRL_OFFSET, 0x1, 0x0);
 
 	PpuNode->ProtNode.Node.State = XPM_PROT_DISABLED;
+
+done:
+	return Status;
+}
+
+static XStatus XPmProt_ConfigureXppu(XPm_Requirement *Reqm, u32 Enable)
+{
+	XStatus Status = XST_SUCCESS;
+	u32 DeviceBaseAddr = Reqm->Device->Node.BaseAddress;
+	XPm_ProtPpu *PpuNode = NULL;
+	u32 ApertureOffset, ApertureAddress = 0;
+	u32 Permissions = 0, i;
+	u32 DynamicReconfigAddrOffset, PermissionRegAddress, PermissionRegMask;
+
+        PmDbg("Xppu configure %x\r\n", Enable);
+	PmDbg("Device base %x\r\n", DeviceBaseAddr);
+
+	/* Find XPPU */
+	for (i = 0; i < XPM_NODEIDX_PROT_MAX; i++)
+	{
+		if (PmProtNodes[i] != NULL && (XPM_NODESUBCL_PROT_XPPU == NODESUBCLASS(PmProtNodes[i]->Node.Id))) {
+			PpuNode = PmProtNodes[i];
+			if ((DeviceBaseAddr >= PpuNode->Aperture_64k.StartAddress) && (DeviceBaseAddr <= PpuNode->Aperture_64k.EndAddress)) {
+				ApertureOffset =  (DeviceBaseAddr - PpuNode->Aperture_64k.StartAddress) / SIZE_64K;
+				ApertureAddress = (PpuNode->ProtNode.Node.BaseAddress + XPPU_APERTURE_0_OFFSET) + (ApertureOffset * 4);
+				DynamicReconfigAddrOffset = ApertureOffset;
+				PermissionRegAddress = PpuNode->ProtNode.Node.BaseAddress + XPPU_ENABLE_PERM_CHECK_REG00_OFFSET + (((APER_64K_START + ApertureOffset) / 32) * 4);
+				PermissionRegMask = 1 << ((APER_64K_START + ApertureOffset) % 32);
+			} else if ((DeviceBaseAddr >= PpuNode->Aperture_1m.StartAddress) && (DeviceBaseAddr <= PpuNode->Aperture_1m.EndAddress)) {
+				ApertureOffset =  (DeviceBaseAddr - PpuNode->Aperture_1m.StartAddress) / SIZE_1M;
+				ApertureAddress = (PpuNode->ProtNode.Node.BaseAddress + XPPU_APERTURE_384_OFFSET) + (ApertureOffset * 4);
+				DynamicReconfigAddrOffset = ApertureOffset;
+				PermissionRegAddress = PpuNode->ProtNode.Node.BaseAddress + XPPU_ENABLE_PERM_CHECK_REG00_OFFSET + (((APER_1M_START + ApertureOffset) / 32) * 4);
+				PermissionRegMask = 1 << ((APER_1M_START + ApertureOffset) % 32);
+			/*TODO: 512M start and end address need to be validated */
+			/*} else if ((DeviceBaseAddr >= PpuNode->Aperture_512m.StartAddress) && (DeviceBaseAddr <= PpuNode->Aperture_512m.EndAddress)) {
+				ApertureOffset =  (DeviceBaseAddr - PpuNode->Aperture_512m.StartAddress) / SIZE_512M;
+				ApertureAddress = (PpuNode->ProtNode.Node.BaseAddress + XPPU_APERTURE_400_OFFSET) + (ApertureOffset * 4);
+				DynamicReconfigAddrOffset = APER_512M_START;
+				PermissionRegAddress = PpuNode->ProtNode.Node.BaseAddress + XPPU_ENABLE_PERM_CHECK_REG00_OFFSET + (((APER_512M_START + ApertureOffset) / 32) * 4);
+				PermissionRegMask = 1 << ((APER_512M_START + ApertureOffset) % 32); */
+			} else {
+				continue;
+			}
+			break;
+		}
+	}
+	if ((i==XPM_NODEIDX_PROT_MAX) || (PpuNode==NULL) || (ApertureAddress==0)) {
+		goto done;
+	}
+
+	/* See if XPPU is enabled or not, if not, return */
+	if (XPM_PROT_DISABLED == PpuNode->ProtNode.Node.State)
+		goto done;
+
+	PmDbg("Aperoffset %x AperAddress %x DynamicReconfigAddrOffset %x\r\n",ApertureOffset, ApertureAddress, DynamicReconfigAddrOffset);
+
+	if (Enable) {
+		u32 UsagePolicy = Reqm->Flags & REG_FLAGS_USAGE_MASK;
+		u32 Security = (Reqm->Flags & REG_FLAGS_SECURITY_MASK) >> REG_FLAGS_SECURITY_OFFSET;
+
+		PmIn32(ApertureAddress, Permissions);
+
+		/* Configure XPPU Aperture */
+		if ((UsagePolicy == REQ_NONSHARED) || (UsagePolicy == REQ_TIME_SHARED)) {
+			Permissions = ((Security << XPPU_APERTURE_TRUSTZONE_OFFSET) | (Reqm->Params[0] & XPPU_APERTURE_PERMISSION_MASK));
+		} else if (UsagePolicy == REQ_SHARED) {
+			/* if device is shared, permissions need to be ored with existing */
+			Permissions |= ((Security << XPPU_APERTURE_TRUSTZONE_OFFSET) | (Reqm->Params[0] & XPPU_APERTURE_PERMISSION_MASK));
+		} else if (UsagePolicy == REQ_NO_RESTRICTION) {
+			Permissions = (XPPU_APERTURE_PERMISSION_MASK | XPPU_APERTURE_TRUSTZONE_MASK);
+		}
+	} else {
+		/* Configure XPPU to disable masters belonging to this subsystem */
+		Permissions = (Permissions | XPPU_APERTURE_TRUSTZONE_MASK);
+		Permissions = (Permissions & (~(Reqm->Params[0] & XPPU_APERTURE_PERMISSION_MASK)));
+	}
+
+	PmDbg("PermissionRegAddress %x Permissions %x RegMask %x \r\n",PermissionRegAddress, Permissions, PermissionRegMask);
+
+	if ((PLATFORM_VERSION_SILICON == Platform) && (PLATFORM_VERSION_SILICON_ES1 == PlatformVersion)) {
+		/* Set XPPU control to 0 */
+		PmRmw32(PpuNode->ProtNode.Node.BaseAddress + XPPU_CTRL_OFFSET, 0x1, 0x0);
+
+		/* Set Enable Permission check of the required apertures to 0 */
+		PmRmw32(PermissionRegAddress, PermissionRegMask, 0);
+
+		/* Program permissions of the apertures that need to be �reconfigured� */
+		PmOut32(ApertureAddress, Permissions);
+
+		/* Enable back permission check of the apertures */
+		PmRmw32(PermissionRegAddress, PermissionRegMask, PermissionRegMask);
+
+		/* Set XPPU control to 1 */
+		PmRmw32(PpuNode->ProtNode.Node.BaseAddress + XPPU_CTRL_OFFSET, 0x1, 0x1);
+
+	} else {
+		/* Configure Dynamic reconfig enable registers before changing XPPU config */
+		PmOut32(PpuNode->ProtNode.Node.BaseAddress + XPPU_DYNAMIC_RECONFIG_APER_ADDR_OFFSET, DynamicReconfigAddrOffset);
+		PmOut32(PpuNode->ProtNode.Node.BaseAddress + XPPU_DYNAMIC_RECONFIG_APER_PERM_OFFSET, Permissions);
+		PmOut32(PpuNode->ProtNode.Node.BaseAddress + XPPU_DYNAMIC_RECONFIG_EN_OFFSET, 1);
+
+		/* Write values to Aperture */
+		PmOut32(ApertureAddress, Permissions);
+
+		/* Disable dynamic reconfig enable once done */
+		PmOut32(PpuNode->ProtNode.Node.BaseAddress + XPPU_DYNAMIC_RECONFIG_EN_OFFSET, 0);
+	}
+
+done:
+	return Status;
+}
+
+XStatus XPmProt_Configure(XPm_Requirement *Reqm, u32 Enable)
+{
+	XStatus Status = XST_SUCCESS;
+
+	if (PLATFORM_VERSION_SILICON != Platform) {
+		goto done;
+	}
+
+	if (XPM_NODESUBCL_DEV_PERIPH == NODESUBCLASS(Reqm->Device->Node.Id)) {
+		/* TODO: Some of the FPD peripherals are protected by XMPU. so based on device's baseaddress,
+		we need to find whether to configure XMPu/XPPU */
+		/* TODO: Need to know which addresse range particular XMPU protects */
+		Status = XPmProt_ConfigureXppu(Reqm, Enable);
+	} else {
+		goto done;
+	}
 
 done:
 	return Status;
