aa19b7311eb7a8d17f34cb9dc9830db23800e507
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2019-07-30 10:58:23 +0530

    srio: Update driver version and copyright year
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
    
    Acked-for-series: Anirudha Sarangi <anirudha.sarangi@xilinx.com>

--- XilinxProcessorIPLib/drivers/srio/src/xsrio_hw.h
+++ XilinxProcessorIPLib/drivers/srio/src/xsrio_hw.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2014 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2014-2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -27,7 +27,7 @@
 /**
 *
 * @file xsrio_hw.h
-* @addtogroup srio_v1_1
+* @addtogroup srio_v1_2
 * @{
 *
 * This header file contains identifiers and macros that can be used to access
