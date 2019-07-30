16e9b75d4f10f8d0cc2ed320378267285a1101ad
Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>
2019-07-30 12:30:20 +0530

    BSP:ARMv8:32 bit: Added constant definitions for memory attributes
    Added missing constant definitions for memory attributes in
    ARMv8 32 bit BSP.
    
    Signed-off-by: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>

--- lib/bsp/standalone/src/arm/ARMv8/32bit/xil_mmu.h
+++ lib/bsp/standalone/src/arm/ARMv8/32bit/xil_mmu.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2015 - 2017 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2015 - 2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -38,6 +38,7 @@
 * Ver   Who  Date     Changes
 * ----- ---- -------- ---------------------------------------------------
 * 5.2	pkp  28/05/15 First release
+* 7.1	mus  30/07/19 Added constant definitions for memory attributes.
 * </pre>
 *
 * @note
@@ -62,6 +63,24 @@ extern "C" {
 /**************************** Type Definitions *******************************/
 
 /************************** Constant Definitions *****************************/
+/* Memory type */
+#define NORM_NONCACHE 0x11DE2 	/* Normal Non-cacheable */
+#define STRONG_ORDERED 0xC02	/* Strongly ordered */
+#define DEVICE_MEMORY 0xC06		/* Device memory */
+#define RESERVED 0x0			/* reserved memory */
+
+/* Normal write-through cacheable shareable */
+#define NORM_WT_CACHE 0x16DEA
+
+/* Normal write back cacheable shareable */
+#define NORM_WB_CACHE 0x15DE6
+
+/* shareability attribute */
+#define SHAREABLE (0x1 << 16)
+#define NON_SHAREABLE	(~(0x1 << 16))
+
+/* Execution type */
+#define EXECUTE_NEVER ((0x1 << 4) | (0x1 << 0))
 
 /************************** Variable Definitions *****************************/
 
