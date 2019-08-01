90d7ddbbbc49b4cf3bb38def60fc89c4ca32f98c
Krishna Chaitanya <krishna.chaitanya.patakamuri@xilinx.com>
2019-08-01 01:20:37 -0600

    xilplmi: Add initial error management framework
    Added initial error management code. It supports setting
    HW reponses for EAM errors of PMC and PSM.
    
    Signed-off-by: Krishna Chaitanya <krishna.chaitanya.patakamuri@xilinx.com>
    
    Acked-for-Series: Sarat Chand Savitala <sarat.chand.savitala@xilinx.com>

---
+++ lib/sw_services/xilplmi/src/xplmi_err.h
@@ -0,0 +1,104 @@
+/******************************************************************************
+* Copyright (C) 2019 Xilinx, Inc. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMANGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*
+*
+******************************************************************************/
+/*****************************************************************************/
+/**
+*
+* @file xplmi_err.h
+*
+* This is the file which contains .
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who  Date        Changes
+* ----- ---- -------- -------------------------------------------------------
+* 1.00  kc   05/23/2019 Initial release
+*
+* </pre>
+*
+* @note
+*
+******************************************************************************/
+#ifndef XPLMI_ERR_H
+#define XPLMI_ERR_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/***************************** Include Files *********************************/
+#include "xil_types.h"
+#include "xil_assert.h"
+#include "xplmi_status.h"
+#include "xplmi_debug.h"
+#include "xplmi_hw.h"
+
+/************************** Constant Definitions *****************************/
+/* Action to be taken when an error occurs */
+#define XPLMI_EM_ACTION_POR		1U
+#define XPLMI_EM_ACTION_SRST		2U
+#define XPLMI_EM_ACTION_CUSTOM		3U
+#define XPLMI_EM_ACTION_ERROUT		4U
+#define XPLMI_EM_ACTION_SUBSYS_SHUTDN	5U
+#define XPLMI_EM_ACTION_SUBSYS_RESTART	6U
+#define XPLMI_EM_ACTION_NOTIFY_AGENT	7U
+#define XPLMI_EM_ACTION_NONE		8U
+#define XPLMI_EM_ACTION_MAX		9U
+
+/* PLMI ERROR Management error codes */
+#define XPLMI_INVALID_ERROR_ID		(1U)
+#define XPLMI_INVALID_ERROR_TYPE	(2U)
+#define XPLMI_INVALID_ERROR_HANDLER	(3U)
+#define XPLMI_INVALID_ERROR_ACTION	(4U)
+#define XPLMI_INVALID_NODEID		(5U)
+
+/**************************** Type Definitions *******************************/
+/* Pointer to Error Handler Function */
+typedef void (*XPlmi_ErrorHandler_t) (u8 ErrorId);
+
+/* Data Structure to hold Error Info */
+struct XPlmi_Error_t {
+	XPlmi_ErrorHandler_t Handler;
+	u8 Action;
+};
+/***************** Macros (Inline Functions) Definitions *********************/
+
+/************************** Function Prototypes ******************************/
+void XPlmi_EmInit(void);
+void XPlmi_PsEmInit(void);
+int XPlmi_EmSetAction(u32 ErrorId, u8 ActionId,
+		XPlmi_ErrorHandler_t ErrorHandler);
+
+/* Functions defined in xplmi_err_cmd.c */
+void XPlmi_ErrModuleInit(void);
+
+/************************** Variable Definitions *****************************/
+
+/*****************************************************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* XPLMI_ERR_H */
---
+++ lib/sw_services/xilplmi/src/xplmi_err_cmd.c
@@ -0,0 +1,146 @@
+/******************************************************************************
+* Copyright (C) 2019 Xilinx, Inc. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMANGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*
+*
+******************************************************************************/
+/*****************************************************************************/
+/**
+*
+* @file xplmi_err_cmd.c
+*
+* This file contains error management commands code for the PLM.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who  Date        Changes
+* ====  ==== ======== ======================================================-
+* 1.00  kc   02/12/2019 Initial release
+*
+* </pre>
+*
+* @note
+*
+******************************************************************************/
+
+/***************************** Include Files *********************************/
+#include "xplmi_err.h"
+#include "xplmi_modules.h"
+#include "xplmi.h"
+#include "xplmi_debug.h"
+#include "xillibpm_node.h"
+
+/************************** Constant Definitions *****************************/
+
+/**************************** Type Definitions *******************************/
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+/************************** Function Prototypes ******************************/
+
+/************************** Variable Definitions *****************************/
+
+/*****************************************************************************/
+/**
+ * @brief This function is reserved to get the supported features for this
+ * module.
+ * @param Pointer to the command structure
+ *
+ * @return Returns XST_SUCCESS always for now
+ *****************************************************************************/
+static int XPlmi_CmdEmReserved(XPlmi_Cmd * Cmd)
+{
+	XPlmi_Printf(DEBUG_DETAILED, "%s %p\n\r", __func__, Cmd);
+	return XST_SUCCESS;
+}
+
+/*****************************************************************************/
+/**
+ * @brief This function sets the command action as pescribed by the command.
+ * command payload paramters are
+ *		* Error ID
+ *		* Error Action
+ *			1 - POR
+ *			2 - SRST
+ *			3 - Not supported
+ *			4 - ErrOut
+ *			5 - Subsystem Shutdown
+ *			6 -  Subsystem Restart
+ *			7 - Notify Agent
+ *		* Error ID Mask
+ *	Optional Params
+ *		* Subsystem Shutdown: Subsystem Node ID
+ *		* Subsystem Restart: Subsystem Node ID
+ *		* Notify Agent: Channel Node ID
+ * @param Pointer to the command structure
+ *
+ * @return Returns XST_SUCCESS on successful execution
+ *****************************************************************************/
+static int XPlmi_CmdEmSetAction(XPlmi_Cmd * Cmd)
+{
+	u32 NodeId = Cmd->Payload[0];
+	u32 ErrorAction = Cmd->Payload[1];
+	int Status;
+
+	XPlmi_Printf(DEBUG_DETAILED,
+	    "%s: NodeId: 0x%0x,  ErrorAction: 0x%0x, \n\r",
+		 __func__, NodeId, ErrorAction);
+
+	Status = XPlmi_EmSetAction(NodeId, (u8)ErrorAction, NULL);
+	return Status;
+}
+
+/*****************************************************************************/
+/**
+ * @brief contains the array of PLM error commands
+ *
+ *****************************************************************************/
+static XPlmi_ModuleCmd XPlmi_ErrCmds[] =
+{
+	XPLMI_MODULE_COMMAND(XPlmi_CmdEmReserved),
+	XPLMI_MODULE_COMMAND(XPlmi_CmdEmSetAction),
+};
+
+/*****************************************************************************/
+/**
+ * @brief Contains the module ID and PLM error commands array
+ *
+ *****************************************************************************/
+static XPlmi_Module XPlmi_ErrModule =
+{
+	XPLMI_MODULE_ERROR_ID,
+	XPlmi_ErrCmds,
+	sizeof XPlmi_ErrCmds / sizeof *XPlmi_ErrCmds,
+};
+
+/*****************************************************************************/
+/**
+ * @brief This function registers the PLM error commands to the PLMI
+ *
+ * @param none
+ *
+ * @return none
+ *
+ *****************************************************************************/
+void XPlmi_ErrModuleInit(void)
+{
+	XPlmi_ModuleRegister(&XPlmi_ErrModule);
+}
