28347eaa252dc0ada643863d4a8130d3f44e199b
Krishna Chaitanya <krishna.chaitanya.patakamuri@xilinx.com>
2019-08-01 01:20:37 -0600

    xilplmi: Add initial error management framework
    Added initial error management code. It supports setting
    HW reponses for EAM errors of PMC and PSM.
    
    Signed-off-by: Krishna Chaitanya <krishna.chaitanya.patakamuri@xilinx.com>
    
    Acked-for-Series: Sarat Chand Savitala <sarat.chand.savitala@xilinx.com>

--- lib/sw_services/xilplmi/src/xplmi_err.h
+++ lib/sw_services/xilplmi/src/xplmi_err.h
@@ -1,26 +1,8 @@
 /******************************************************************************
-* Copyright (C) 2019 Xilinx, Inc. All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
+* Copyright (c) 2019 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 /*****************************************************************************/
 /**
 *
@@ -53,48 +35,92 @@ extern "C" {
 #include "xplmi_status.h"
 #include "xplmi_debug.h"
 #include "xplmi_hw.h"
+#include "xplmi_error_node.h"
 
 /************************** Constant Definitions *****************************/
 /* Action to be taken when an error occurs */
-#define XPLMI_EM_ACTION_POR		1U
-#define XPLMI_EM_ACTION_SRST		2U
-#define XPLMI_EM_ACTION_CUSTOM		3U
-#define XPLMI_EM_ACTION_ERROUT		4U
-#define XPLMI_EM_ACTION_SUBSYS_SHUTDN	5U
-#define XPLMI_EM_ACTION_SUBSYS_RESTART	6U
-#define XPLMI_EM_ACTION_NOTIFY_AGENT	7U
-#define XPLMI_EM_ACTION_NONE		8U
-#define XPLMI_EM_ACTION_MAX		9U
+#define XPLMI_EM_ACTION_INVALID			(0U)
+#define XPLMI_EM_ACTION_POR				(1U)
+#define XPLMI_EM_ACTION_SRST			(2U)
+#define XPLMI_EM_ACTION_CUSTOM			(3U)
+#define XPLMI_EM_ACTION_ERROUT			(4U)
+#define XPLMI_EM_ACTION_SUBSYS_SHUTDN	(5U)
+#define XPLMI_EM_ACTION_SUBSYS_RESTART	(6U)
+#define XPLMI_EM_ACTION_NONE			(7U)
+#define XPLMI_EM_ACTION_MAX				(8U)
+
+/* Subsystem shutdown/restart related macros */
+#define XPLMI_SUBSYS_SHUTDN_TYPE_SHUTDN		(0U)
+#define XPLMI_SUBSYS_SHUTDN_TYPE_RESTART	(1U)
+#define XPLMI_RESTART_SUBTYPE_SUBSYS		(0U)
 
 /* PLMI ERROR Management error codes */
 #define XPLMI_INVALID_ERROR_ID		(1U)
 #define XPLMI_INVALID_ERROR_TYPE	(2U)
 #define XPLMI_INVALID_ERROR_HANDLER	(3U)
 #define XPLMI_INVALID_ERROR_ACTION	(4U)
-#define XPLMI_INVALID_NODEID		(5U)
+#define XPLMI_LPD_UNINITIALIZED		(5U)
+#define XPLMI_CANNOT_CHANGE_ACTION	(6U)
 
 /**************************** Type Definitions *******************************/
 /* Pointer to Error Handler Function */
-typedef void (*XPlmi_ErrorHandler_t) (u8 ErrorId);
+typedef void (*XPlmi_ErrorHandler_t) (u32 ErrorId, u32 ErrorMask);
+extern s32 (* PmSystemShutdown)(u32 SubsystemId, const u32 Type,
+		const u32 SubType);
 
 /* Data Structure to hold Error Info */
 struct XPlmi_Error_t {
 	XPlmi_ErrorHandler_t Handler;
 	u8 Action;
+	u32 SubsystemId;
 };
+
 /***************** Macros (Inline Functions) Definitions *********************/
 
+/*****************************************************************************/
+/**
+ * @brief	This function returns register mask value for the error id mask
+ * given.
+ *
+ * @param	ErrorMask  is the input.
+ *
+ * @return	Register mask value
+ *
+ *****************************************************************************/
+inline u32 XPlmi_ErrRegMask(u32 ErrorMask)
+{
+	return (0x1U << (ErrorMask & 0x1FU));
+}
+
+/*****************************************************************************/
+/**
+ * @brief	This function returns Error event ID for the given error node ID.
+ *
+ * @param	Id is the event ID.
+ *
+ * @return	Error event ID
+ *
+ *****************************************************************************/
+inline u32 XPlmi_EventNodeType(u32 Id)
+{
+	return ((Id & XPLMI_NODE_TYPE_MASK) >> XPLMI_NODE_TYPE_SHIFT);
+}
+
 /************************** Function Prototypes ******************************/
-void XPlmi_EmInit(void);
+void XPlmi_EmInit(s32 (* SystemShutdown)(u32 SubsystemId,
+		const u32 Type, const u32 SubType));
 int XPlmi_PsEmInit(void);
-int XPlmi_EmSetAction(u32 ErrorId, u8 ActionId,
+int XPlmi_EmSetAction(u32 ErrorNodeId, u32 ErrorMask, u8 ActionId,
 		XPlmi_ErrorHandler_t ErrorHandler);
+int XPlmi_EmDisable(u32 ErrorNodeId, u32 ErrorMask);
+void XPlmi_ErrIntrHandler(void *CallbackRef);
+void XPlmi_ErrPSMIntrHandler(u32 ErrorNodeId, u32 ErrorMask);
 
 /* Functions defined in xplmi_err_cmd.c */
 void XPlmi_ErrModuleInit(void);
 
 /************************** Variable Definitions *****************************/
-
+extern u32 EmSubsystemId;
 /*****************************************************************************/
 
 #ifdef __cplusplus
--- lib/sw_services/xilplmi/src/xplmi_err_cmd.c
+++ lib/sw_services/xilplmi/src/xplmi_err_cmd.c
@@ -1,26 +1,8 @@
 /******************************************************************************
-* Copyright (C) 2019 Xilinx, Inc. All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
+* Copyright (c) 2019 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 /*****************************************************************************/
 /**
 *
@@ -46,7 +28,6 @@
 #include "xplmi_modules.h"
 #include "xplmi.h"
 #include "xplmi_debug.h"
-#include "xpm_node.h"
 
 /************************** Constant Definitions *****************************/
 
@@ -57,90 +38,145 @@
 /************************** Function Prototypes ******************************/
 
 /************************** Variable Definitions *****************************/
-
 /*****************************************************************************/
 /**
- * @brief This function is reserved to get the supported features for this
- * module.
- * @param Pointer to the command structure
- *
- * @return Returns XST_SUCCESS always for now
- *****************************************************************************/
-static int XPlmi_CmdEmReserved(XPlmi_Cmd * Cmd)
-{
-	XPlmi_Printf(DEBUG_DETAILED, "%s %p\n\r", __func__, Cmd);
-	return XST_SUCCESS;
-}
-
-/*****************************************************************************/
-/**
- * @brief This function sets the command action as pescribed by the command.
- * command payload paramters are
- *		* Error ID
- *		* Error Action
- *			1 - POR
- *			2 - SRST
- *			3 - Not supported
- *			4 - ErrOut
- *			5 - Subsystem Shutdown
- *			6 -  Subsystem Restart
- *			7 - Notify Agent
- *		* Error ID Mask
- *	Optional Params
- *		* Subsystem Shutdown: Subsystem Node ID
- *		* Subsystem Restart: Subsystem Node ID
- *		* Notify Agent: Channel Node ID
- * @param Pointer to the command structure
- *
- * @return Returns XST_SUCCESS on successful execution
- *****************************************************************************/
-static int XPlmi_CmdEmSetAction(XPlmi_Cmd * Cmd)
-{
-	u32 NodeId = Cmd->Payload[0];
-	u32 ErrorAction = Cmd->Payload[1];
-	int Status;
-
-	XPlmi_Printf(DEBUG_DETAILED,
-	    "%s: NodeId: 0x%0x,  ErrorAction: 0x%0x, \n\r",
-		 __func__, NodeId, ErrorAction);
-
-	Status = XPlmi_EmSetAction(NodeId, (u8)ErrorAction, NULL);
-	return Status;
-}
-
-/*****************************************************************************/
-/**
- * @brief contains the array of PLM error commands
- *
- *****************************************************************************/
-static XPlmi_ModuleCmd XPlmi_ErrCmds[] =
-{
-	XPLMI_MODULE_COMMAND(XPlmi_CmdEmReserved),
-	XPLMI_MODULE_COMMAND(XPlmi_CmdEmSetAction),
-};
-
-/*****************************************************************************/
-/**
- * @brief Contains the module ID and PLM error commands array
+ * @brief	Contains the module ID and PLM error commands array
  *
  *****************************************************************************/
 static XPlmi_Module XPlmi_ErrModule;
 
 /*****************************************************************************/
 /**
- * @brief This function registers the PLM error commands to the PLMI
+ * @brief	This function is reserved to get the supported features for this
+ * module.
  *
- * @param none
+ * @param	Cmd is pointer to the command structure
  *
- * @return none
+ * @return	Returns XST_SUCCESS always for now
+ *
+ *****************************************************************************/
+static int XPlmi_CmdEmFeatures(XPlmi_Cmd * Cmd)
+{
+	int Status = XST_FAILURE;
+
+	XPlmi_Printf(DEBUG_DETAILED, "%s %p\n\r", __func__, Cmd);
+
+	if (Cmd->Payload[0U] < XPlmi_ErrModule.CmdCnt) {
+		Cmd->Response[1U] = (u32)XST_SUCCESS;
+	} else {
+		Cmd->Response[1U] = (u32)XST_FAILURE;
+	}
+	Status = XST_SUCCESS;
+	Cmd->Response[0U] = (u32)Status;
+	return Status;
+}
+
+/*****************************************************************************/
+/**
+ * @brief	This function sets the error action as prescribed by the command.
+ *			Command payload parameters are
+ *				* Error Node ID
+ *				* Error Action
+ *					0 - Invalid
+ *					1 - POR
+ *					2 - SRST
+ *					3 - Custom(Not supported)
+ *					4 - ErrOut
+ *					5 - Subsystem Shutdown
+ *					6 - Subsystem Restart
+ *					7 - None
+ *			* Error ID Mask
+ * @param	Cmd is pointer to the command structure
+ *
+ * @return	XST_SUCCESS on success and error code on failure
+ *
+ *****************************************************************************/
+static int XPlmi_CmdEmSetAction(XPlmi_Cmd * Cmd)
+{
+	int Status = XST_FAILURE;
+	u32 NodeId = Cmd->Payload[0U];
+	u32 ErrorAction = Cmd->Payload[1U];
+	u32 ErrorMask = Cmd->Payload[2U];
+
+	XPlmi_Printf(DEBUG_DETAILED,
+		"%s: NodeId: 0x%0x,  ErrorAction: 0x%0x, ErrorMask: 0x%0x\n\r",
+		 __func__, NodeId, ErrorAction, ErrorMask);
+
+	/* Do not allow CUSTOM error action as it is not supported */
+	if ((XPLMI_EM_ACTION_CUSTOM == ErrorAction) ||
+		(ErrorAction >= XPLMI_EM_ACTION_MAX) ||
+		(XPLMI_EM_ACTION_INVALID == ErrorAction)) {
+		XPlmi_Printf(DEBUG_GENERAL,
+				"Error: XPlmi_CmdEmSetAction: Invalid/unsupported error "
+				"action %d received for error 0x%x", ErrorAction, ErrorMask);
+		Status = XPLMI_INVALID_ERROR_ACTION;
+		goto END;
+	}
+
+	/* PMC's PSM CR and NCR error actions must not be changed */
+	if ((ErrorMask == XPLMI_NODEIDX_ERROR_PMC_PSM_CR) ||
+		(ErrorMask == XPLMI_NODEIDX_ERROR_PMC_PSM_NCR)) {
+		XPlmi_Printf(DEBUG_GENERAL,
+				"Error: XPlmi_CmdEmSetAction: Error Action "
+				"cannot be changed for error 0x%x\r\n", ErrorMask);
+		Status = XPLMI_CANNOT_CHANGE_ACTION;
+		goto END;
+	}
+
+	/*
+	 * Allow error action setting for PSM errors only if LPD is initialized
+	 */
+	if ((ErrorMask >= XPLMI_NODEIDX_ERROR_PS_SW_CR) &&
+		(ErrorMask < XPLMI_NODEIDX_ERROR_PSMERR2_MAX) &&
+		((LpdInitialized & LPD_INITIALIZED) != LPD_INITIALIZED)) {
+		XPlmi_Printf(DEBUG_GENERAL, "LPD is not initialized to configure "
+				"PSM errors and actions\n\r");
+		Status = XPLMI_LPD_UNINITIALIZED;
+		goto END;
+	}
+
+	Status = XPlmi_EmSetAction(NodeId, ErrorMask, (u8)ErrorAction, NULL);
+	if(Status != XST_SUCCESS) {
+		goto END;
+	}
+
+END:
+	return Status;
+}
+
+/*****************************************************************************/
+/**
+ * @brief	Contains the array of PLM error commands
+ *
+ *****************************************************************************/
+static XPlmi_ModuleCmd XPlmi_ErrCmds[] =
+{
+	XPLMI_MODULE_COMMAND(XPlmi_CmdEmFeatures),
+	XPLMI_MODULE_COMMAND(XPlmi_CmdEmSetAction),
+};
+
+/*****************************************************************************/
+/**
+ * @brief	Contains the module ID and PLM error commands array
+ *
+ *****************************************************************************/
+static XPlmi_Module XPlmi_ErrModule =
+{
+	XPLMI_MODULE_ERROR_ID,
+	XPlmi_ErrCmds,
+	XPLMI_ARRAY_SIZE(XPlmi_ErrCmds),
+};
+
+/*****************************************************************************/
+/**
+ * @brief	This function registers the PLM error commands to the PLMI.
+ *
+ * @param	None
+ *
+ * @return	None
  *
  *****************************************************************************/
 void XPlmi_ErrModuleInit(void)
 {
-
-	XPlmi_ErrModule.Id = XPLMI_MODULE_ERROR_ID;
-	XPlmi_ErrModule.CmdAry = XPlmi_ErrCmds;
-	XPlmi_ErrModule.CmdCnt = sizeof XPlmi_ErrCmds / sizeof *XPlmi_ErrCmds;
-
 	XPlmi_ModuleRegister(&XPlmi_ErrModule);
 }
