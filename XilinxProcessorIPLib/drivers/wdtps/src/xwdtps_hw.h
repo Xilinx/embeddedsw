d42f0afc81372738e1e5a7205cb24d3dbc692656
Srinivas Neeli <srinivas.neeli@xilinx.com>
2019-08-06 15:17:50 +0530

    wdtps: Updated copyright year and addtogroup number
    Updated copyright year and addtogroup number for wdtps driver
    
    Signed-off-by: Srinivas Neeli <srinivas.neeli@xilinx.com>
    
    Acked-for-series: Naga Sureshkumar Relli <naga.sureshkumar.relli@xilinx.com>

--- XilinxProcessorIPLib/drivers/wdtps/src/xwdtps_hw.h
+++ XilinxProcessorIPLib/drivers/wdtps/src/xwdtps_hw.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2010 - 2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -27,7 +27,7 @@
 /**
 *
 * @file xwdtps_hw.h
-* @addtogroup wdtps_v3_1
+* @addtogroup wdtps_v3_2
 * @{
 *
 * This file contains the hardware interface to the System Watch Dog Timer (WDT).
