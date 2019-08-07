4e775a77007b6e706ab4b4e9262832a5e9c7b8bc
Kalyani Akula <kalyani.akula@xilinx.com>
2019-08-07 15:59:12 -0400

    XilPuf: New Versal library to handle PUF functionality.
    This patch adds versal PUF support and covers below API's
    - Puf provisioning
    - Puf Regeneration - On-demand with helper data stored in
    			external memory
    And also provides basic examples for PUF.
    
    Signed-off-by: Kalyani Akula <kalyani.akula@xilinx.com>
    Acked-by: Mohan Dhanawade <mohan.dhanawade@xilinx.com>
    
    Acked-by: Mohan Dhanawade <mohan.dhanawade@xilinx.com>

---
+++ lib/sw_services/xilpuf/data/xilpuf.tcl
@@ -0,0 +1,91 @@
+###############################################################################
+#
+# Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMANGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+#
+#
+###############################################################################
+#
+# Modification History
+#
+# Ver   Who  Date     Changes
+# ----- ---- -------- -----------------------------------------------
+# 1.0   kal  08/01/19 Initial Release
+##############################################################################
+
+#---------------------------------------------
+# puf_drc
+#---------------------------------------------
+proc puf_drc {libhandle} {
+	# check processor type
+	set proc_instance [hsi::get_sw_processor];
+	set hw_processor [common::get_property HW_INSTANCE $proc_instance]
+	set compiler [common::get_property CONFIG.compiler $proc_instance]
+	set proc_type [common::get_property IP_NAME [hsi::get_cells -hier $hw_processor]];
+	set os_type [hsi::get_os];
+
+	if {$proc_type != "psv_pmc" && $proc_type != "psv_cortexa72" &&
+		$proc_type != "psv_r5"} {
+		error "ERROR: XilPuf library is supported only for Versal";
+		return;
+	}
+}
+
+proc generate {libhandle} {
+
+}
+
+#-------
+# post_generate: called after generate called on all libraries
+#-------
+proc post_generate {libhandle} {
+
+	xgen_opts_file $libhandle
+}
+
+#-------
+# execs_generate: called after BSP's, libraries and drivers have been compiled
+#	This procedure builds the libxilpuf.a library
+#-------
+proc execs_generate {libhandle} {
+
+}
+
+proc xgen_opts_file {libhandle} {
+
+	# Copy the include files to the include directory
+	set srcdir src
+	set dstdir [file join .. .. include]
+
+	# Create dstdir if it does not exist
+	if { ! [file exists $dstdir] } {
+		file mkdir $dstdir
+	}
+
+	# Get list of files in the srcdir
+	set sources [glob -join $srcdir *.h]
+
+	# Copy each of the files in the list to dstdir
+	foreach source $sources {
+		file copy -force $source $dstdir
+	}
+
+}
