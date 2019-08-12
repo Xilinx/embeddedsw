7f5e6208dfcea134d41086f5e6787a75087e31a9
Ravi Patel <ravi.patel@xilinx.com>
2019-08-12 04:34:49 -0700

    xillibpm: client: Add API to get the chip ID
    Add GetChipID API in LibPM client library to return the
    ID and version of a chip.
    
    Signed-off-by: Ravi Patel <ravi.patel@xilinx.com>
    
    Acked-for-series: Jolly Shah <jolly.shah@xilinx.com>

--- lib/sw_services/xillibpm/src/client/xpm_client_api.c
+++ lib/sw_services/xillibpm/src/client/xpm_client_api.c
@@ -126,6 +126,39 @@ done:
 
 /****************************************************************************/
 /**
+ * @brief  This function is used to request the version and ID code of a chip
+ *
+ * @param  IDCode  Returns the chip ID code.
+ * @param  Version Returns the chip version.
+ *
+ * @return XST_SUCCESS if successful else XST_FAILURE or an error code
+ * or a reason code
+ *
+ * @note   None
+ *
+ ****************************************************************************/
+XStatus XPm_GetChipID(u32* IDCode, u32 *Version)
+{
+	XStatus Status;
+	u32 Payload[PAYLOAD_ARG_CNT];
+
+	PACK_PAYLOAD0(Payload, PM_GET_CHIPID);
+
+	/* Send request to the target module */
+	Status = XPm_IpiSend(PrimaryProc, Payload);
+	if (XST_SUCCESS != Status) {
+		goto done;
+	}
+
+	/* Return result from IPI return buffer */
+	Status = Xpm_IpiReadBuff32(PrimaryProc, IDCode, Version, NULL);
+
+done:
+	return Status;
+}
+
+/****************************************************************************/
+/**
  * @brief  This function is used to request the version number of the API
  * running on the platform management controller.
  *
