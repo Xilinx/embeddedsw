1b9cb931cb2db219f06777d13232f083869e3334
Harini Katakam <harini.katakam@xilinx.com>
2019-08-14 14:55:21 +0530

    emacps: Move common variable definition to _util example file
    "Platform" is used in _util and _intr_dma example files now. Move
    the definition to _util.c to allow for compilation of both regular
    example and 1588 example on Zynq platform.
    
    Signed-off-by: Harini Katakam <harini.katakam@xilinx.com>
    
    Acked-by :  Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>

--- XilinxProcessorIPLib/drivers/emacps/examples/xemacps_example_util.c
+++ XilinxProcessorIPLib/drivers/emacps/examples/xemacps_example_util.c
@@ -67,6 +67,7 @@ XEmacPs EmacPsInstance;	/* XEmacPs instance used throughout examples */
  */
 char EmacPsMAC[] = { 0x00, 0x0a, 0x35, 0x01, 0x02, 0x03 };
 
+u32 Platform;
 
 /****************************************************************************/
 /**
