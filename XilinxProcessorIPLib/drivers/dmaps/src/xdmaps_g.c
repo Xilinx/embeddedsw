f60eced57e2bd1c78120a4f766e475af713533fc
Harini Katakam <harini.katakam@xilinx.com>
2019-08-16 18:56:46 +0530

    dmaps: Update copyright years and doxygen tags
    Update copyright years and doxygen tags.
    
    Signed-off-by: Harini Katakam <harini.katakam@xilinx.com>
    
    Acked-for-series: Anirudha Sarangi <anirudha.sarangi@xilinx.com>

--- XilinxProcessorIPLib/drivers/dmaps/src/xdmaps_g.c
+++ XilinxProcessorIPLib/drivers/dmaps/src/xdmaps_g.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2009 - 2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2009 - 2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal 
@@ -27,7 +27,7 @@
 /**
 *
 * @file xdmaps_g.c
-* @addtogroup dmaps_v2_4
+* @addtogroup dmaps_v2_5
 * @{
 *
 * This file contains a configuration table where each entry is a configuration
--- XilinxProcessorIPLib/drivers/dmaps/src/xdmaps_selftest.c
+++ XilinxProcessorIPLib/drivers/dmaps/src/xdmaps_selftest.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2009 - 2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2009 - 2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal 
@@ -27,7 +27,7 @@
 /**
 *
 * @file xdmaps_selftest.c
-* @addtogroup dmaps_v2_4
+* @addtogroup dmaps_v2_5
 * @{
 *
 * This file contains the self-test functions for the XDmaPs driver.
