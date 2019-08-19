29b47df867cbd0fedd8f89ce5fb35139fdcb98c0
Mohan Marutirao Dhanawade <mohan.dhanawade@xilinx.com>
2019-08-20 00:02:51 +0530

    standalone: Added generic functions
    This patch adds generic support functions required by XilNvm and other
    libraries - Xil_IsValidHexChar, Xil_ConvertStringToHexLE, Xil_WaitForEvents,
    Xil_ValidateHexStr. It also touches existing code for minor
    tabification and for improve readability.
    
    Signed-off-by: Mohan Marutirao Dhanawade <mohan.dhanawade@xilinx.com>

--- lib/bsp/standalone/src/common/xil_util.h
+++ lib/bsp/standalone/src/common/xil_util.h
@@ -43,7 +43,25 @@
 #ifndef XIL_UTIL_H_
 #define XIL_UTIL_H_
 
-/****************************************************************************/
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "xil_types.h"
+#include "xil_io.h"
+#include "xstatus.h"
+
+/*************************** Constant Definitions *****************************/
+#define XIL_SIZE_OF_NIBBLE_IN_BITS	4U
+#define XIL_SIZE_OF_BYTE_IN_BITS	8U
+
+/* Maximum string length handled by Xil_ValidateHexStr function */
+#define XIL_MAX_HEX_STR_LEN	512U
+
+
+/****************** Macros (Inline Functions) Definitions *********************/
+
+/******************************************************************************/
 /**
 * This API ceils the provided float value.
 *
@@ -54,18 +72,38 @@
 *
 * @note		one.
 *
-*****************************************************************************/
+*******************************************************************************/
 #define Xil_Ceil(Value) \
 	(((Value > (u32)Value) || ((u32)Value == 0U)) ? \
 					(u32)((u32)Value + 1U) : (u32)Value)
 
+
+/*************************** Function Prototypes ******************************/
+
 /* Converts input character to nibble */
 u32 Xil_ConvertCharToNibble(u8 InChar, u8 *Num);
 
-/* Conver input hex string to array of 32-bits integers */
+/* Convert input hex string to array of 32-bits integers */
 u32 Xil_ConvertStringToHex(const char *Str, u32 *buf, u8 Len);
 
 /* Waits for specified event */
 u32 Xil_WaitForEvent(u32 RegAddr, u32 EventMask, u32 Event, u32 Timeout);
+
+/* Waits for specified events */
+u32 Xil_WaitForEvents(u32 EventsRegAddr, u32 EventsMask, u32 WaitEvents,
+			 u32 Timeout, u32* Events);
+
+/* Validate input hex character */
+u32 Xil_IsValidHexChar(const char Ch);
+
+/* Validate the input string contains only hexadecimal characters */
+u32 Xil_ValidateHexStr(const char *HexStr);
+
+/* Convert string to hex numbers in little enidian format */
+u32 Xil_ConvertStringToHexLE(const char *Str, u8 *Buf, u32 Len);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif	/* XIL_UTIL_H_ */
