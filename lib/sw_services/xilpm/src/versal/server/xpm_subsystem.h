94a0f9555d06f579e5495cf172a2e8db115a9a24
Rajan Vaja <rajan.vaja@xilinx.com>
2019-08-28 00:14:12 -0700

    xilpm: server_versal: Remove subsystem IDs from xpm_subsystem.h
    Subsystem IDs are defined and used from common node ID header
    xilpm_nodeid.h. So remove definitions of subsystem IDs from
    xpm_subsystem.h.
    
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    Acked-for-series: Jolly Shah <jolly.shah@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_subsystem.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_subsystem.h
@@ -1,44 +1,25 @@
 /******************************************************************************
-*
-* Copyright (C) 2018-2019 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
-*
+* Copyright (c) 2018 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 
 #ifndef XPM_SUBSYSTEM_H_
 #define XPM_SUBSYSTEM_H_
 
-#include "xpm_requirement.h"
+#include "xpm_defs.h"
+#include "xstatus.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define	INVALID_SUBSYSID	-1U
-#define SUBSYSTEMCLASS_MASK \
-	((XPM_NODECLASS_SUBSYSTEM & NODE_CLASS_MASK_BITS) << NODE_CLASS_SHIFT)
-#define ISVALIDSUBSYSTEM(id) \
-	( (((id & SUBSYSTEMCLASS_MASK) == SUBSYSTEMCLASS_MASK) && \
-	((id & NODE_INDEX_MASK_BITS) < XPM_NODEIDX_SUBSYS_MAX)) ? 1 : 0)
+#define	INVALID_SUBSYSID			(0xFFFFFFFFU)
+
+/**
+ * Subsystem specific flags.
+ */
+#define SUBSYSTEM_INIT_FINALIZED		(1U << 0U)
 
 /**
  * Subsystem creation states.
@@ -48,10 +29,11 @@ typedef enum {
 	RESERVED,
 	ONLINE,
 	SUSPENDING,
+	SUSPENDED,
+	POWERED_OFF,
 	MAX_STATE
 } XPm_SubsysState;
 
-typedef struct XPm_Requirement XPm_Requirement;
 typedef struct XPm_Subsystem XPm_Subsystem;
 
 /**
@@ -59,11 +41,13 @@ typedef struct XPm_Subsystem XPm_Subsystem;
  */
 struct XPm_Subsystem {
 	u32 Id; /**< Subsystem ID */
-	XPm_SubsysState State; /**< Subsystem state */
+	u8 State; /**< Subsystem state */
+	u8 Flags; /**< Subsystem specific flags */
 	u32 IpiMask;
-	XPm_Requirement *Requirements;
+	struct XPm_Reqm *Requirements;
 		/**< Head of the requirement list for all devices. */
 	void (*NotifyCb)(u32 SubsystemId, const u32 EventId);
+	XPm_Subsystem *NextSubsystem;
 };
 
 /************************** Function Prototypes ******************************/
@@ -78,14 +62,16 @@ XStatus XPmSubsystem_Destroy(u32 SubsystemId);
 XStatus XPmSubsystem_IsAllProcDwn(u32 SubsystemId);
 XStatus XPm_IsForcePowerDownAllowed(u32 SubsystemId, u32 NodeId);
 XStatus XPmSubsystem_ForceDownCleanup(u32 SubsystemId);
+int XPmSubsystem_InitFinalize(const u32 SubsystemId);
 int XPmSubsystem_Idle(u32 SubsystemId);
 XPm_Subsystem *XPmSubsystem_GetById(u32 SubsystemId);
-XPm_Subsystem *XPmSubsystem_GetByIndex(u32 SubsystemIndex);
+XPm_Subsystem *XPmSubsystem_GetByIndex(u32 SubSysIdx);
 XStatus XPmSubsystem_SetCurrent(u32 SubsystemId);
 u32 XPmSubsystem_GetCurrent(void);
 XStatus XPmSubsystem_Restart(u32 SubsystemId);
 XStatus XPmSubsystem_GetStatus(const u32 SubsystemId, const u32 DeviceId,
 			       XPm_DeviceStatus *const DeviceStatus);
+u32 XPmSubsystem_GetMaxSubsysIdx(void);
 
 #ifdef __cplusplus
 }
