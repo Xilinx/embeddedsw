d7d63b8394e3ead5ff6a1a3118f2e32a1e8a9b40
Ravi Patel <ravi.patel@xilinx.com>
2019-08-27 22:53:19 -0700

    xilpm: server_versal: Merge xillibpm_node.h to xpm_node.h
    Move the content of xillibpm_node.h to xpm_node.h and remove the
    xillibpm_node.h file.
    Also update the other files to include xpm_node.h instead of
    xillibpm_node.h.
    
    Signed-off-by: Ravi Patel <ravi.patel@xilinx.com>
    
    Acked-for-series: Jolly Shah <jolly.shah@xilinx.com>

--- lib/sw_services/xilplmi/src/xplmi_gic_interrupts.c
+++ lib/sw_services/xilplmi/src/xplmi_gic_interrupts.c
@@ -45,7 +45,7 @@
 ******************************************************************************/
 
 /***************************** Include Files *********************************/
-#include "xillibpm_node.h"
+#include "xpm_node.h"
 #include "xillibpm_api.h"
 #include "xplmi_gic_interrupts.h"
 #include "xplmi_hw.h"
