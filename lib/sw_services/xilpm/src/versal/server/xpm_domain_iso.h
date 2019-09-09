8e8d38f23ac3677724de436570e407d57f91a45a
Jolly Shah <jolly.shah@xilinx.com>
2019-09-09 16:32:48 -0700

    xilpm: versal: Add iso control command support for pl gem clocks
    This patch adds iso control command support for controlling
    pl gem clocks.
    
    Signed-off-by: Jolly Shah <jolly.shah@xilinx.com>
    Acked-by: Rajan Vaja <rajan.vaja@xilinx.com>
    
    Acked-by: Rajan Vaja <rajan.vaja@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_domain_iso.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_domain_iso.h
@@ -1,28 +1,8 @@
 /******************************************************************************
-*
-* Copyright (C) 2018-2019 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
-*
+* Copyright (c) 2018 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 
 #ifndef XPM_DOMAIN_ISO_H
 #define XPM_DOMAIN_ISO_H
@@ -34,11 +14,14 @@
 extern "C" {
 #endif
 
-#define FALSE_IMMEDIATE		2
+#define FALSE_VALUE		(0U)
+#define TRUE_VALUE		(1U)
+#define FALSE_IMMEDIATE		(2U)	/* Remove isolation immediately */
+#define TRUE_PENDING_REMOVE	(3U)	/* Set isolation, but pending removal */
 
 typedef struct XPm_Iso {
 	XPm_Node Node; /**< Node: Node base class */
-	u8 Offset;
+	u32 Mask;
 	u8 Polarity;
 	u32 DependencyNodeHandles[2];
 }XPm_Iso;
@@ -49,7 +32,7 @@ typedef enum {
         PM_ACTIVE_HIGH,
 }XPm_IsoPolarity;
 
-/* Isoaltion states */
+/* Isolation states */
 typedef enum {
 	PM_ISOLATION_ON,
 	PM_ISOLATION_OFF,
