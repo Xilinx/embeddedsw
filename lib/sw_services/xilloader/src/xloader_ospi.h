28540e1d929163695babd0c2f95b1854fa4e1b14
b.vikram@xilinx.com <b.vikram@xilinx.com>
2019-09-18 06:54:10 -0400

    sw_services :xilloader: Fix build issues in OSPI
    The patch declares FLASH_SIZE_2G macro and OSPI_WRITE_CMD_ENABLE
    to avoid clashing with the macros defined in xloader_qspi.h.
    This fixes build errors when only OSPI is enabled in design and
    multiple declaration warnings when both OSPI and QSPI are enabled in
    designs.
    
    Signed-off-by: Vikram Sreenivasa Batchali <b.vikram@xilinx.com>
    Acked-by: Mounika Akula <mounika.akula@xilinx.com>
    
    Acked-by: Mounika Akula <mounika.akula@xilinx.com>

--- lib/sw_services/xilloader/src/xloader_ospi.h
+++ lib/sw_services/xilloader/src/xloader_ospi.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2018 Xilinx, Inc. All rights reserved.
+* Copyright (C) 2018 - 2019 Xilinx, Inc. All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -33,7 +33,7 @@
 *
 * Ver   Who  Date        Changes
 * ----- ---- -------- -------------------------------------------------------
-* 1.00  kc   02/21/2017 Initial release
+* 1.00  kc   02/21/2018 Initial release
 * </pre>
 *
 * @note
@@ -64,11 +64,12 @@ extern "C" {
 #define MICRON_ID				(0x20U)
 #define FLASH_SIZE_ID_512M			(0x20U)
 #define FLASH_SIZE_512M				(0x4000000U)
+#define FLASH_SIZE_2G                   	(0x10000000U)
 #define READ_CMD_OCTAL_4B    			(0x7CU)
 #define READ_ID					(0x9FU)
 #define MICRON_INDEX_START			(0x0U)
 #define WRITE_DISABLE_CMD			(0x4U)
-#define WRITE_ENABLE_CMD			(0x6U)
+#define OSPI_WRITE_ENABLE_CMD			(0x6U)
 #define ENTER_4B_ADDR_MODE      0xB7
 #define EXIT_4B_ADDR_MODE       0xE9
 #define READ_FLAG_STATUS_CMD	0X70
@@ -80,9 +81,9 @@ extern "C" {
  * Byte 2 is second byte of Device ID describes flash size:
  * 512Mbit : 0x1A
  */
-#define	MICRON_OCTAL_ID_BYTE0	0x2C
-#define MICRON_OCTAL_ID_BYTE2_512	0x1A
-
+#define	MICRON_OCTAL_ID_BYTE0		(0x2CU)
+#define MICRON_OCTAL_ID_BYTE2_512	(0x1AU)
+#define MICRON_OCTAL_ID_BYTE2_2G	(0x1CU)
 /**************************** Type Definitions *******************************/
 
 /***************** Macros (Inline Functions) Definitions *********************/
@@ -90,7 +91,7 @@ extern "C" {
 /************************** Function Prototypes ******************************/
 
 int XLoader_OspiInit(u32 DeviceFlags);
-int XLoader_OspiCopy(u32 SrcAddr, u64 DestAddress, u32 Length, u32 Flags);
+XStatus XLoader_OspiCopy(u32 SrcAddr, u64 DestAddress, u32 Length, u32 Flags);
 int XLoader_OspiRelease(void );
 int XLoader_FlashEnterExit4BAddMode(XOspiPsv *OspiPsvPtr, u32 Enable);
 /************************** Variable Definitions *****************************/
