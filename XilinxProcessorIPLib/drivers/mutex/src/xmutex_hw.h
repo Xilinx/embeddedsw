293b92ff8a9707b673b148b53d7907241d9c80c2
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2019-09-19 10:36:33 +0530

    mutex: Update copyright year and doxygen tags
    Update copyright year and doxygen tags.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
    
    Acked-for-series: Shubhrajyoti Datta <Shubhrajyoti.datta@xilinx.com>
    
    Acked-for-series: Shubhrajyoti Datta <Shubhrajyoti.datta@xilinx.com>

--- XilinxProcessorIPLib/drivers/mutex/src/xmutex_hw.h
+++ XilinxProcessorIPLib/drivers/mutex/src/xmutex_hw.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2007 - 2014 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2007 - 2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -27,7 +27,7 @@
 /**
 *
 * @file xmutex_hw.h
-* @addtogroup mutex_v4_3
+* @addtogroup mutex_v4_4
 * @{
 *
 * This header file contains identifiers/definitions and macros that can be used
