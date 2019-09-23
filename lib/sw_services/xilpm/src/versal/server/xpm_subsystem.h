743e21478e16a99bd1932bee5d2f5a4f23640083
Ravi Patel <ravi.patel@xilinx.com>
2019-09-22 23:18:33 -0700

    xilpm: versal: Set appropriate state of subsystem at different places
    Existing implementation sets subsystem to OFFLINE after force
    powerdown, system shutdown and suspend finalize.
    
    Instead of that, set subsystem to SUSPENDED after suspend finalize
    and POWERED_OFF after force powerdown and system shutdown.
    
    Signed-off-by: Ravi Patel <ravi.patel@xilinx.com>
    
    Acked-for-series: Jolly Shah <jolly.shah@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_subsystem.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_subsystem.h
@@ -19,7 +19,16 @@ extern "C" {
 /**
  * Subsystem specific flags.
  */
-#define SUBSYSTEM_INIT_FINALIZED		(1U << 0U)
+#define SUBSYSTEM_INIT_FINALIZED		((u8)1U << 0U)
+#define SUBSYSTEM_IS_CONFIGURED			((u8)1U << 1U)
+
+/**
+ * Helper macros to check subsystem specific flags.
+ */
+#define IS_SUBSYS_CONFIGURED(Flags)	(SUBSYSTEM_IS_CONFIGURED ==	\
+					 ((Flags) & SUBSYSTEM_IS_CONFIGURED))
+#define IS_SUBSYS_INIT_FINALIZED(Flags)	(SUBSYSTEM_INIT_FINALIZED ==	\
+					 ((Flags) & SUBSYSTEM_INIT_FINALIZED))
 
 /**
  * Subsystem creation states.
@@ -57,9 +66,9 @@ u32 XPmSubsystem_GetSubSysIdByIpiMask(u32 IpiMask);
 XStatus XPm_IsWakeAllowed(u32 SubsystemId, u32 NodeId);
 XStatus XPm_IsAccessAllowed(u32 SubsystemId, u32 NodeId);
 XStatus XPmSubsystem_SetState(const u32 SubsystemId, const u32 State);
+XStatus XPmSubsystem_Configure(u32 SubsystemId);
 XStatus XPmSubsystem_Add(u32 SubsystemId);
 XStatus XPmSubsystem_Destroy(u32 SubsystemId);
-XStatus XPmSubsystem_IsAllProcDwn(u32 SubsystemId);
 XStatus XPm_IsForcePowerDownAllowed(u32 SubsystemId, u32 NodeId);
 XStatus XPmSubsystem_ForceDownCleanup(u32 SubsystemId);
 int XPmSubsystem_InitFinalize(const u32 SubsystemId);
