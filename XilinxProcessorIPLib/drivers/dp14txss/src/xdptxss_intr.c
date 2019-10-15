cc5111d1f6adc8518fa1f3e8026e091cc08613f2
Jagadeesh Banisetti <jagadeesh.banisetti@xilinx.com>
2019-10-15 18:47:13 +0530

    Revert "dp14txss: Added multi instance support."
    This reverts commit 0717fc014fa8238fec940da8fd5bccff81ebf975.
    
    For MST mode, generation of xdptxss_g.c file is not correct, and causing
    no display for streams 2, 3 and 4.
    
    Signed-off-by: Jagadeesh Banisetti <jagadeesh.banisetti@xilinx.com>
    
    Acked-for-series: Sandip Kothari <Sandip.kothari@xilinx.com>

--- XilinxProcessorIPLib/drivers/dp14txss/src/xdptxss_intr.c
+++ XilinxProcessorIPLib/drivers/dp14txss/src/xdptxss_intr.c
@@ -109,7 +109,7 @@ void XDpTxSs_DpIntrHandler(void *InstancePtr)
 	XDp_InterruptHandler(XDpTxSsPtr->DpPtr);
 }
 
-#if (XPAR_XHDCP_NUM_INSTANCES > 0)
+#if (XPAR_DPTXSS_0_HDCP_ENABLE > 0)
 /*****************************************************************************/
 /**
 *
@@ -139,7 +139,7 @@ void XDpTxSs_HdcpIntrHandler(void *InstancePtr)
 }
 #endif
 
-#if (XPAR_XHDCP_NUM_INSTANCES > 0) || (XPAR_XHDCP22_TX_NUM_INSTANCES > 0)
+#if (XPAR_DPTXSS_0_HDCP_ENABLE > 0) || (XPAR_XHDCP22_TX_NUM_INSTANCES > 0)
 /*****************************************************************************/
 /**
 *
@@ -450,7 +450,7 @@ u32 XDpTxSs_SetCallBack(XDpTxSs *InstancePtr, u32 HandlerType,
 			Status = XST_SUCCESS;
 			break;
 
-#if (XPAR_XHDCP_NUM_INSTANCES > 0)
+#if (XPAR_DPTXSS_0_HDCP_ENABLE > 0)
 		case XDPTXSS_HANDLER_HDCP_RPTR_EXCHG:
 			XHdcp1x_SetCallBack(InstancePtr->Hdcp1xPtr,
 				XHDCP1X_RPTR_HDLR_REPEATER_EXCHANGE,
