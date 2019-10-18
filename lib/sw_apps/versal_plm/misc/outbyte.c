5d23a1468e709a39716b683da5e8cddec564ccb0
Mounika Akula <mounika.akula@xilinx.com>
2019-10-18 19:56:31 +0530

    sw_apps: versal_plm: Versionless build support for PLM
    This patch adds support for versionless build for PLM for 2019.2
    release.
    
    Signed-off-by: Mounika Akula <mounika.akula@xilinx.com>
    Acked-by: Sarat Chand Savitala <sarat.chand.savitala@xilinx.com>

---
+++ lib/sw_apps/versal_plm/misc/outbyte.c
@@ -0,0 +1,15 @@
+#include "xparameters.h"
+#include "xuartpsv_hw.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void outbyte(char c); 
+
+#ifdef __cplusplus
+}
+#endif 
+
+void outbyte(char c) {
+	 XUartPsv_SendByte(STDOUT_BASEADDRESS, c);
+}
