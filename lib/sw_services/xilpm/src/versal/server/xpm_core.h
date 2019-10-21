6951759e21e2209c902a16202fd5769ad889cc8b
Rajan Vaja <rajan.vaja@xilinx.com>
2019-10-21 03:43:48 -0700

    xilpm: versal: Add support of latency parameter in GetOpCharacteristic
    Add support for getting Latency of power, device and core using
    GetOpCharacteristic() API.
    
    Also add power up and power down latency in core's structure.
    
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    Acked-by: Jolly Shah <jolly.shah@xilinx.com>
    
    Acked-by: Jolly Shah <jolly.shah@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_core.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_core.h
@@ -1,28 +1,8 @@
 /******************************************************************************
-*
-* Copyright (C) 2018-2019 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
-*
+* Copyright (c) 2018 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 
 #ifndef XPM_CORE_H_
 #define XPM_CORE_H_
@@ -52,6 +32,8 @@ struct XPm_CoreOps {
 struct XPm_Core {
 	XPm_Device Device; /**< Device: Base class */
 	u32 ImageId; /**< ImageId: Image ID */
+	u16 PwrUpLatency;
+	u16 PwrDwnLatency;
 	u64 ResumeAddr;
 	struct XPm_CoreOps *CoreOps; /**< Core operations */
 	u8 DebugMode; /**< DebugMode: Debugger is connected */
@@ -66,6 +48,7 @@ XStatus XPmCore_Init(XPm_Core *Core, u32 Id, XPm_Power *Power,
 		     struct XPm_CoreOps *Ops);
 XStatus XPmCore_PwrDwn(XPm_Core *Core);
 XStatus XPmCore_WakeUp(XPm_Core *Core);
+int XPmCore_GetWakeupLatency(const u32 DeviceId, u32 *Latency);
 
 #ifdef __cplusplus
 }
