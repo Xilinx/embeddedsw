cba72b764050aa2f5229c45cec7fed7627f2739d
Rajan Vaja <rajan.vaja@xilinx.com>
2019-10-21 03:43:48 -0700

    xilpm: versal: Add support of latency parameter in GetOpCharacteristic
    Add support for getting Latency of power, device and core using
    GetOpCharacteristic() API.
    
    Also add power up and power down latency in core's structure.
    
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    Acked-by: Jolly Shah <jolly.shah@xilinx.com>
    
    Acked-by: Jolly Shah <jolly.shah@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_core.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_core.h
@@ -8,6 +8,7 @@
 #define XPM_CORE_H_
 
 #include "xpm_api.h"
+#include "xpm_psm_api.h"
 #include "xpm_device.h"
 
 #ifdef __cplusplus
@@ -17,11 +18,11 @@ extern "C" {
 #define MAX_CORE_REGS 3
 
 typedef struct XPm_Core XPm_Core;
+extern volatile struct PsmToPlmEvent_t *PsmToPlmEvent;
+extern u32 ProcDevList[PROC_DEV_MAX];
 
 /* Core Operations */
 struct XPm_CoreOps {
-	int (*RestoreResumeAddr)(XPm_Core *Core);
-	int (*HasResumeAddr) (XPm_Core *Core);
 	XStatus (*RequestWakeup)(XPm_Core *Core, u32 SetAddress, u64 Address);
 	XStatus (*PowerDown) (XPm_Core *Core);
 };
@@ -34,20 +35,27 @@ struct XPm_Core {
 	u32 ImageId; /**< ImageId: Image ID */
 	u16 PwrUpLatency;
 	u16 PwrDwnLatency;
-	u64 ResumeAddr;
 	struct XPm_CoreOps *CoreOps; /**< Core operations */
 	u8 DebugMode; /**< DebugMode: Debugger is connected */
 	u8 Ipi; /**< IPI channel */
 	u8 SleepMask;
 	u8 PwrDwnMask;
+	u8 PsmToPlmEvent_ProcIdx; /**< Processor index in the PsmToPlmEvent structure */
+	u8 isCoreUp;
 };
 
 /************************** Function Prototypes ******************************/
 XStatus XPmCore_Init(XPm_Core *Core, u32 Id, XPm_Power *Power,
 		     XPm_ClockNode *Clock, XPm_ResetNode *Reset, u8 IpiCh,
 		     struct XPm_CoreOps *Ops);
+int XPmCore_StoreResumeAddr(XPm_Core *Core, u64 Address);
+int XPmCore_HasResumeAddr(XPm_Core *Core);
+int XPmCore_SetCPUIdleFlag(XPm_Core *Core, u32 CpuIdleFlag);
+int XPmCore_GetCPUIdleFlag(XPm_Core *Core, u32 *CpuIdleFlag);
 XStatus XPmCore_PwrDwn(XPm_Core *Core);
-XStatus XPmCore_WakeUp(XPm_Core *Core);
+XStatus XPmCore_WakeUp(XPm_Core *Core, u32 SetAddress, u64 Address);
+int XPmCore_AfterDirectWakeUp(XPm_Core *Core);
+int XPmCore_AfterDirectPwrDwn(XPm_Core *Core);
 int XPmCore_GetWakeupLatency(const u32 DeviceId, u32 *Latency);
 
 #ifdef __cplusplus
