Xilinx/embeddedsw

commit 0035385954f6b914f4c0f7422dc24e4d77aef468

intc: Fix total_intr_srcs calculation when interrupt pin is connected to a slice
Commit 4fe22a378d03 ("intc: Calculate total_source_intrs based on width
of interrupt port/pin") added support for updation of interrupt sources
based on the interrupt pin port width, but it didn't consider the use case
where the pin is connected to a slice, due to which slice based interrupt
designs are failing during bsp generation.

This patch fixes this issue.

Fixes: 4fe22a378d03 ("intc: Calculate total_source_intrs based on width of interrupt port/pin")
Signed-off-by: Adithya Balaji Boda <aboda@xilinx.com>
Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
Acked-by:  Mubin Sayyed <mubin.usman.sayyed@xilinx.com>

Acked-by:  Mubin Sayyed <mubin.usman.sayyed@xilinx.com>

XilinxProcessorIPLib/drivers/intc/data/intc.tcl
@@ -81,7 +81,8 @@
 ##     04/10/19 mus Updated intc_update_source_array proc to consider
 ##                  interrupt port width, while calculating
 ##                  total_source_intrs. It fixes CR#1020269
-##
+##     12/09/19 adk When interrupt source pin is connected to slice consider
+##		    slice width, while calculating the total interrupt sources.
 ##
 ##
 ## @END_CHANGELOG
@@ -89,6 +90,7 @@
 ############################################################
 # Global interrupt handlers array, default handler routine
 ############################################################
+source [file join [file dirname [info script]] slice_width.tcl]
 array set interrupt_handlers ""
 set default_interrupt_handler "XNullHandler"
 set cascade 0
@@ -754,7 +756,10 @@ proc intc_update_source_array {periph} {
                 incr j
             }
         } else {
-            set width [expr [common::get_property LEFT $source_pin] + 1]
+            set width [::hsi::utils::get_intr_connected_slice_width $periph $source_pin]
+            if {$width == 0 || $width == ""} {
+                set width [expr [common::get_property LEFT $source_pin] + 1]
+            }
             for {set count 0} {$count != $width} {incr count} {
                 set source_port_name($intr_cnt)         "${t_source_port_name}"
                 set source_name($intr_cnt)              $t_source_name

XilinxProcessorIPLib/drivers/intc/data/slice_width.tcl
@@ -1,0 +1,156 @@
+###############################################################################
+#
+# Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#
+#
+###############################################################################
+##############################################################################
+#
+# Modification History
+#
+# Ver   Who  Date     Changes
+# ----- ---- -------- -----------------------------------------------
+# 3.10   adk  13/09/19 First release
+#
+##############################################################################
+
+#uses "xillib.tcl"
+#
+# Get handles for all ports driving the interrupt pin of a peripheral
+#
+proc ::hsi::utils::get_intr_connected_slice_width {periph_handle intr_src_pin} {
+   set slice_width 0
+   lappend interrupt_pins
+   set interrupt_pins [::hsi::get_pins -of_objects $periph_handle -filter {TYPE==INTERRUPT && DIRECTION==I}]
+   foreach interrupt_pin $interrupt_pins {
+       set slice_width [::hsi::utils::get_intr_src_pins_for_slice $interrupt_pin $intr_src_pin]
+   }
+   return $slice_width
+}
+#
+# Get the interrupt source pins of a periph pin object
+#
+proc ::hsi::utils::get_intr_src_pins_for_slice {interrupt_pin intr_src_pin} {
+    lappend slice_width
+    set source_pins [::hsi::utils::get_source_pins $interrupt_pin]
+    foreach source_pin $source_pins {
+        set source_cell [::hsi::get_cells -of_objects $source_pin]
+        if { [llength $source_cell ] } {
+            #For concat IP, we need to bring pin source for other end
+            set ip_name [common::get_property IP_NAME $source_cell]
+            if { [string match -nocase $ip_name "xlconcat" ] } {
+                set slice_width [::hsi::__internal::get_concat_interrupt_sources_for_slice $source_cell $intr_src_pin]
+            } elseif { [string match -nocase $ip_name "xlslice"] } {
+                set slice_width [::hsi::__internal::get_slice_interrupt_sources_for_slice $source_cell $intr_src_pin]
+            } elseif { [string match -nocase $ip_name "util_reduced_logic"] } {
+                set slice_width [::hsi::__internal::get_util_reduced_logic_interrupt_sources_for_slice $source_cell $intr_src_pin]
+            }
+        }
+    }
+    return $slice_width
+}
+
+
+#It assume that XLCONCAT IP cell object is passed to this function
+proc ::hsi::__internal::get_concat_interrupt_sources_for_slice { concat_ip_obj intr_src_pin {lsb -1} {msb -1} } {
+    lappend slice_width
+    if {$lsb == -1 } {
+        set i 0
+        set num_ports [common::get_property CONFIG.NUM_PORTS $concat_ip_obj]
+    } else {
+        set i $lsb
+        set num_ports $msb
+    }
+    for { $i } { $i < $num_ports } { incr i } {
+        set in_pin [::hsi::get_pins -of_objects $concat_ip_obj "In$i"]
+        set pins [::hsi::utils::get_source_pins $in_pin]
+        foreach pin $pins {
+            set source_cell [::hsi::get_cells -of_objects $pin]
+            if { [llength $source_cell] } {
+                set ip_name [common::get_property IP_NAME $source_cell]
+                #Cascading case of concat IP
+                if { [string match -nocase $ip_name "xlconcat"] } {
+                    set slice_width [::hsi::__internal::get_concat_interrupt_sources_for_slice $source_cell $intr_src_pin]
+                } elseif { [string match -nocase $ip_name "xlslice"] } {
+                    set slice_width [::hsi::__internal::get_slice_interrupt_sources_for_slice $source_cell $intr_src_pin]
+                } elseif { [string match -nocase $ip_name "util_reduced_logic"] } {
+                    set slice_width [::hsi::__internal::get_util_reduced_logic_interrupt_sources_for_slice $source_cell $intr_src_pin]
+                }
+            }
+        }
+    }
+    return $slice_width
+}
+
+proc ::hsi::__internal::get_slice_interrupt_sources_for_slice { slice_ip_obj intr_src_pin } {
+    lappend slice_width
+    set in_pin [::hsi::get_pins -of_objects $slice_ip_obj "Din"]
+    set pins [::hsi::utils::get_source_pins $in_pin]
+    foreach pin $pins {
+        set source_cell [::hsi::get_cells -of_objects $pin]
+        if { [llength $source_cell] } {
+            set ip_name [common::get_property IP_NAME $source_cell]
+            #Cascading case of xlslice IP
+            if { [string match -nocase $ip_name "xlslice"] } {
+                set source_pins [::hsi::__internal::get_slice_interrupt_sources_for_slice $source_cell $intr_src_pin]
+            } elseif { [string match -nocase $ip_name "xlconcat"] } {
+                set from [::common::get_property CONFIG.DIN_FROM $slice_ip_obj]
+                set to [::common::get_property CONFIG.DIN_TO $slice_ip_obj]
+                set lsb [expr $from < $to ? $from : $to]
+                set msb [expr $from > $to ? $from : $to]
+                incr msb
+                set slice_width [::hsi::__internal::get_concat_interrupt_sources_for_slice $source_cell $intr_src_pin $lsb $msb ]
+            } elseif { [string match -nocase $ip_name "util_reduced_logic"] } {
+                set slice_width [::hsi::__internal::get_util_reduced_logic_interrupt_sources_for_slice $intr_src_pin]
+            } else {
+                if { $pin eq $intr_src_pin } {
+                  set slice_width [ common::get_property CONFIG.DOUT_WIDTH $slice_ip_obj ]
+                }
+            }
+
+        }
+    }
+    return $slice_width
+}
+
+proc ::hsi::__internal::get_util_reduced_logic_interrupt_sources_for_slice { url_ip_obj intr_src_pin } {
+    lappend slice_width
+    set in_pin [::hsi::get_pins -of_objects $url_ip_obj "Op1"]
+    set pins [::hsi::utils::get_source_pins $in_pin]
+    foreach pin $pins {
+        set source_cell [::hsi::get_cells -of_objects $pin]
+        if { [llength $source_cell] } {
+            set ip_name [common::get_property IP_NAME $source_cell]
+
+            if { [string match -nocase $ip_name "xlslice"] } {
+                set source_pins [::hsi::__internal::get_slice_interrupt_sources_for_slice $source_cell $intr_src_pin]
+            } elseif { [string match -nocase $ip_name "xlconcat"] } {
+                set source_pins [::hsi::__internal::get_concat_interrupt_sources_for_slice $source_cell $intr_src_pin]
+            } elseif { [string match -nocase $ip_name "util_reduced_logic"] } {
+        #Cascading case of util_reduced_logic IP
+                set source_pins [::hsi::__internal::get_util_reduced_logic_interrupt_sources_for_slice $source_cell $intr_src_pin]
+            }
+
+        }
+    }
+    return $slice_width
+}

commit e059f55997d93c5a96c333e02a88580ba451a1b5

intc: Fix total_intr_srcs calculation when external interrupt pin is connected to a slice
Commit 0035385954f6 ("intc: Fix total_intr_srcs calculation when
interrupt pin is connected to a slice") is not handling the use case
where external interrupt pin connected to a slice, due to which external
interrupt pin connected to a slice based interrupt designs are failing during
bsp generation.

This patch fixes this issue.

Fixes: 0035385954f6 ("intc: Fix total_intr_srcs calculation when interrupt pin is connected to a slice")
Signed-off-by: Adithya Balaji Boda <aboda@xilinx.com>
Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>

XilinxProcessorIPLib/drivers/intc/data/intc.mdd
@@ -50,7 +50,7 @@ OPTION DRC = intc_drc;
 OPTION supported_peripherals = (axi_intc);
 OPTION driver_state = ACTIVE;
 OPTION copyfiles = all;
-OPTION VERSION = 3.10;
+OPTION VERSION = 3.11;
 OPTION NAME = intc;
 OPTION INTC_TYPE = XIntc;
 

XilinxProcessorIPLib/drivers/intc/data/intc.tcl
@@ -83,6 +83,8 @@
 ##                  total_source_intrs. It fixes CR#1020269
 ##     12/09/19 adk When interrupt source pin is connected to slice consider
 ##		    slice width, while calculating the total interrupt sources.
+##     18/10/19 adk Updated the get_slice_interrupt_sources_for_slice proc to consider
+##		    external interrupt source pin connected to slice use case.
 ##
 ##
 ## @END_CHANGELOG

XilinxProcessorIPLib/drivers/intc/data/slice_width.tcl
@@ -127,7 +127,12 @@ proc ::hsi::__internal::get_slice_interrupt_sources_for_slice { slice_ip_obj int
                 }
             }
 
+        } else {
+              if { $pin eq $intr_src_pin } {
+                  set slice_width [ common::get_property CONFIG.DOUT_WIDTH $slice_ip_obj ]
+              }
         }
+
     }
     return $slice_width
 }

XilinxProcessorIPLib/drivers/intc/src/xintc.h
@@ -158,6 +158,9 @@
 * 3.10  mus   04/10/19 Updated intc_update_source_array proc to
 * 		      calculate total_source_intrs, based on the width of
 *		      interrupt port/pins.
+* 3.11  adk   18/10/19 Updated the get_slice_interrupt_sources_for_slice proc
+*		       to consider external interrupt source pin connected to
+*		       slice use case.
 *
 * @note
 *		For Cascade mode, Interrupt IDs are generated in xparameters.h