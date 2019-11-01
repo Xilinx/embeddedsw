e6b3ada2ba9a76134e66c4b8295b54aedaa013ce
Izhar Ameer Shaikh <izhar.ameer.shaikh@xilinx.com>
2019-11-01 10:50:45 -0700

    xilpm: versal: Fix unused parameters warnings
    This warning is generated when CUSTOM_PMBUS compiler definition is not
    provided during compilation. To suppress it, cast the respected
    variables to void.
    
    This patch fixes unused parameters warnings for the following variables:
    
    xpm_board.c: In function 'XPmBoard_ControlRail':
    xpm_board.c:236:61: warning: unused parameter 'Function' [-Wunused-parameter]
     XStatus XPmBoard_ControlRail(const enum power_rail_function Function,
                                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~
    xpm_board.c:237:35: warning: unused parameter 'PowerRegulatorId' [-Wunused-parameter]
              const enum power_rail_id PowerRegulatorId)
              ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~
    
    Signed-off-by: Izhar Ameer Shaikh <izhar.ameer.shaikh@xilinx.com>
    
    Acked-for-series: Jyotheeswar Reddy Mutthareddyvari <jyotheeswar.reddy.mutthareddyvari@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_board.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_board.h
@@ -39,23 +39,23 @@ extern "C" {
 #endif /* XPAR_XIICPS_1_DEVICE_ID */
 
 /************************** I2C Configurations *******************************/
-#define IIC_DEVICE_ID               0
-#define IIC_CLK_FREQ_HZ				100000000
-#define IIC_SCLK_RATE               100000
-#define IIC_BASE_ADDR				PMC_I2C_ADDR
+#define IIC_DEVICE_ID			0
+#define IIC_CLK_FREQ_HZ			100000000
+#define IIC_SCLK_RATE			100000
+#define IIC_BASE_ADDR			PMC_I2C_ADDR
 
 /************************** Address Definitions ******************************/
 #define PMC_I2C_ADDR			0xF1000000
-#define I2C0_MUX_ADDR           0x74
-#define PSFP_REGULATOR_ADDR     0x0A
+#define I2C0_MUX_ADDR			0x74
+#define PSFP_REGULATOR_ADDR		0x0A
 
 /************************ MUX Channel Definitions ****************************/
 /***************** Channels 5-7 are currently not configured *****************/
-#define MUX_SEL_CHANNEL_0       0x01
-#define MUX_SEL_CHANNEL_1       0x02
-#define MUX_SEL_CHANNEL_2       0x04
-#define MUX_SEL_CHANNEL_3       0x08
-#define MUX_SEL_CHANNEL_4       0x10
+#define MUX_SEL_CHANNEL_0		0x01
+#define MUX_SEL_CHANNEL_1		0x02
+#define MUX_SEL_CHANNEL_2		0x04
+#define MUX_SEL_CHANNEL_3		0x08
+#define MUX_SEL_CHANNEL_4		0x10
 
 /************************** Variable Definitions *****************************/
 
@@ -71,7 +71,7 @@ enum power_rail_function {
 
 /************************** Function Prototypes ******************************/
 XStatus XPmBoard_ControlRail(const enum power_rail_function Function,
-				     const enum power_rail_id PowerRegulatorId);
+			const enum power_rail_id PowerRegulatorId);
 
 #ifdef __cplusplus
 }
