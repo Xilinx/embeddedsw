c141d99e54bf84dbfbbd960dabf35963ccac1745
Ben Levinsky <ben.levinsky@xilinx.com>
2019-11-15 09:36:50 -0800

    openamp: run_cmake.bat: HDI_APPROOT parsing
    Previously, run_cmake had issue running on Windows when trying to
    parse the value of HDI_APPROOT. Update so that this will run successfully.
    
    Signed-off-by: Ben Levinsky <ben.levinsky@xilinx.com>
    
    Acked-for-series: Ed Mooring <ed.mooring@xilinx.com>

--- ThirdParty/sw_services/openamp/src/run_cmake.bat
+++ ThirdParty/sw_services/openamp/src/run_cmake.bat
@@ -1,5 +1,5 @@
 if not "%HDI_APPROOT%" == "" (
-${HDI_APPROOT}/tps/lnx64/cmake-3.3.2/bin/cmake $@ 2>&1
+%HDI_APPROOT%\tps\win64\cmake-3.3.2\bin\cmake.exe %* 2>&1
 ) else (
 if "%XILINX_SDK%"== "" (
 %XILINX_VITIS%\tps\win64\cmake-3.3.2\bin\cmake.exe %* 2>&1
