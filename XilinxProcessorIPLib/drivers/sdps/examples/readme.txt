e66fce922175bb69d7cce40451276f5f27a9a39b
Manish Narani <manish.narani@xilinx.com>
2019-12-06 00:00:36 -0700

    sdps: Add SD/eMMC Raw Test Example
    Add an example to test SD raw transfers to and from SD/eMMC.
    
    Signed-off-by: Manish Narani <manish.narani@xilinx.com>
    
    Acked-for-series: Sai Krishna Potthuri <lakshmi.sai.krishna.potthuri@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/sdps/examples/readme.txt
@@ -0,0 +1,9 @@
+/** \page example Examples
+You can refer to the below stated example applications for more details on how to use sdps driver.
+
+@section ex1 xsdps_raw_example.c
+Contains an example on how to use the XSdps driver directly.
+This example shows the usage of the driver in raw mode.
+
+For details, see xsdps_raw_example.c.
+*/
---
+++ XilinxProcessorIPLib/drivers/sdps/examples/xsdps_raw_example.c
@@ -0,0 +1,189 @@
+/******************************************************************************
+*
+* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*
+*
+*
+******************************************************************************/
+/*****************************************************************************/
+/**
+*
+* @file xsdps_raw_example.c
+*
+* This example is used to test read and write transfers on SD/eMMC interface.
+*
+* Please note that running this example will modify the card contents and
+* file system information will be erased in the card. Card will need to be
+* re-formatted.
+*
+* Modify the offset and Size macros to test different SD memory offset and
+* size.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver	Who	Date     Changes
+* ----- --- -------- ---------------------------------------------
+* 3.9 	mn  12/02/19 First release
+*
+*</pre>
+*
+******************************************************************************/
+
+/***************************** Include Files *********************************/
+
+#include "xsdps.h"		/* SD device driver */
+
+/************************** Constant Definitions *****************************/
+
+/**************************** Type Definitions *******************************/
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+/************************** Function Prototypes ******************************/
+static int SdpsRawTest(void);
+
+/************************** Variable Definitions *****************************/
+
+#ifdef __ICCARM__
+#pragma data_alignment = 32
+u8 DestinationAddress[10*1024];
+#pragma data_alignment = 32
+u8 SourceAddress[10*1024];
+#else
+u8 DestinationAddress[10*1024] __attribute__ ((aligned(32)));
+u8 SourceAddress[10*1024] __attribute__ ((aligned(32)));
+#endif
+
+#define TEST 7
+/* Number of SD blocks to test */
+#define NUM_BLOCKS 16
+/* Sector offset to test */
+#define SECTOR_OFFSET 204800
+
+/*****************************************************************************/
+/**
+*
+* Main function to call the SD example.
+*
+* @param	None
+*
+* @return	XST_SUCCESS if successful, otherwise XST_FAILURE.
+*
+* @note		None
+*
+******************************************************************************/
+int main(void)
+{
+	int Status;
+
+	xil_printf("SD Raw Read/ Write Test \r\n");
+
+	Status = SdpsRawTest();
+	if (Status != XST_SUCCESS) {
+		xil_printf("SD Raw Read/ Write Test failed \r\n");
+		return XST_FAILURE;
+	}
+
+	xil_printf("Successfully ran SD Raw Read/ Write Test \r\n");
+
+	return XST_SUCCESS;
+
+}
+
+/*****************************************************************************/
+/**
+*
+* This function performs the SD Raw Read/ Write Test.
+*
+* @param	None
+*
+* @return	XST_SUCCESS if successful, otherwise XST_FAILURE.
+*
+* @note		None
+*
+******************************************************************************/
+static int SdpsRawTest(void)
+{
+	static XSdPs SdInstance;
+	XSdPs_Config *SdConfig;
+	int Status;
+	u32 BuffCnt;
+	/*
+	 * Since block size is 512 bytes. File Size is 512*BlockCount.
+	 */
+	u32 FileSize = (512*NUM_BLOCKS); /* File Size is only up to 2MB */
+	u32 Sector = SECTOR_OFFSET;
+
+	for(BuffCnt = 0; BuffCnt < FileSize; BuffCnt++){
+		SourceAddress[BuffCnt] = TEST + BuffCnt;
+	}
+
+	/*
+	 * Initialize the host controller
+	 */
+	SdConfig = XSdPs_LookupConfig(XPAR_XSDPS_0_DEVICE_ID);
+	if (NULL == SdConfig) {
+		return XST_FAILURE;
+	}
+
+	Status = XSdPs_CfgInitialize(&SdInstance, SdConfig,
+					SdConfig->BaseAddress);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	Status = XSdPs_CardInitialize(&SdInstance);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Write data to SD/eMMC.
+	 */
+	if (!(SdInstance.HCS)) Sector *= XSDPS_BLK_SIZE_512_MASK;
+	Status = XSdPs_WritePolled(&SdInstance, Sector, NUM_BLOCKS,
+				   SourceAddress);
+	if (Status != XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Read data from SD/eMMC.
+	 */
+	if (!(SdInstance.HCS)) Sector *= XSDPS_BLK_SIZE_512_MASK;
+	Status  = XSdPs_ReadPolled(&SdInstance, Sector, NUM_BLOCKS,
+				   DestinationAddress);
+	if (Status!=XST_SUCCESS) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Data verification
+	 */
+	for(BuffCnt = 0; BuffCnt < FileSize; BuffCnt++){
+		if(SourceAddress[BuffCnt] != DestinationAddress[BuffCnt]){
+			return XST_FAILURE;
+		}
+	}
+
+	return XST_SUCCESS;
+}
