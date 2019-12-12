78b136a7390bdf88b5264705c5e3d6c61963dd8b
Ravi Patel <ravi.patel@xilinx.com>
2019-12-11 22:51:05 -0800

    versal: server: Make PSM->PLM IPI as non-blocking
    Existing PSM->PLM IPI communication is blocking which may leads to
    dead lock condition if PSM and PLM sends IPI to each other at
    nearly same time and waiting for each other to process IPI.
    
    To avoid this, make PSM->PLM IPI as non-blocking call by using
    shared PSM RAM address to log the power up/down events and act
    on those events at PLM side.
    When any direct power up/down interrupt occurs, PSM sets
    corresponding event at shared address and triggers IPI interrupt
    (without waiting for IPI to free) to notify PLM that PSM event is
    arrived. On the other side, PLM will process all the events
    (which is set by PSM) and clears it when processed.
    
    Signed-off-by: Ravi Patel <ravi.patel@xilinx.com>
    
    Acked-for-series: Felix Burton <fburton@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_psm_api.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_psm_api.h
@@ -1,28 +1,8 @@
 /******************************************************************************
- *
- * Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- *
- *
- ******************************************************************************/
+* Copyright (c) 2019 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
 
 #ifndef XPM_PSM_API_H_
 #define XPM_PSM_API_H_
@@ -34,14 +14,26 @@
 extern "C" {
 #endif
 
-#define PM_PWR_DWN_EVENT			(1U)
-#define PM_WAKE_UP_EVENT			(2U)
-#define PSM_API_MIN				PM_PWR_DWN_EVENT
-#define PSM_API_MAX				PM_WAKE_UP_EVENT
+#define PM_PSM_TO_PLM_EVENT			(1U)
+#define PSM_API_MIN				PM_PSM_TO_PLM_EVENT
+#define PSM_API_MAX				PM_PSM_TO_PLM_EVENT
 
 #define PSM_API_DIRECT_PWR_DWN			(1U)
 #define PSM_API_DIRECT_PWR_UP			(2U)
 #define PSM_API_FPD_HOUSECLEAN			(3U)
+
+enum ProcDeviceId {
+	ACPU_0,
+	ACPU_1,
+	RPU0_0,
+	RPU0_1,
+	PROC_DEV_MAX,
+};
+
+struct PsmToPlmEvent_t {
+	u32 Version;	/* Version of the event structure */
+	u32 Event[PROC_DEV_MAX];
+};
 
 void XPm_PsmModuleInit(void);
 XStatus XPm_PwrDwnEvent(const u32 DeviceId);
