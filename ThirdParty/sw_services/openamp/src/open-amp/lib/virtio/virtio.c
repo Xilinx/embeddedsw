535021cf6761c99076782b600f108a54f3593e5e
Arnaud Pouliquen <arnaud.pouliquen@st.com>
2020-01-06 17:59:23 +0100

    virtio: fix virtqueue callback typedef
    Fix declaration of the the function pointer type reported
    by checkpatch tool.
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>

--- ThirdParty/sw_services/openamp/src/open-amp/lib/virtio/virtio.c
+++ ThirdParty/sw_services/openamp/src/open-amp/lib/virtio/virtio.c
@@ -85,7 +85,7 @@ void virtio_describe(struct virtio_device *dev, const char *msg,
 
 int virtio_create_virtqueues(struct virtio_device *vdev, unsigned int flags,
 			     unsigned int nvqs, const char *names[],
-			     vq_callback *callbacks[])
+			     vq_callback callbacks[])
 {
 	struct virtio_vring_info *vring_info;
 	struct vring_alloc_info *vring_alloc;
