590b5f37bc35e4024882d87dc2669d06a951753b
Saeed Nowshadi <saeed.nowshadi@xilinx.com>
2020-01-10 13:03:25 -0800

    xilpm: versal: server: Check whether a device requirement is exclusive to one subsystem
    This routine checks all subsystems that could request a device to determine whether only one subsystem has allocated the device at a time.
    
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@xilinx.com>
    
    Acked-for-series: Jolly Shah <jolly.shah@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_requirement.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_requirement.h
@@ -1,28 +1,8 @@
 /******************************************************************************
-*
-* Copyright (C) 2018-2019 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
-*
+* Copyright (c) 2018 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 
 #ifndef XPM_REQUIREMENT_H_
 #define XPM_REQUIREMENT_H_
@@ -35,6 +15,7 @@ extern "C" {
 #endif
 
 typedef struct XPm_ReqmInfo XPm_ReqmInfo;
+typedef struct XPm_Reqm XPm_Requirement;
 
 /**
  * Specific requirement information.
@@ -45,8 +26,6 @@ struct XPm_ReqmInfo {
 	u32 QoS:7; /**< QoS requirement */
 };
 
-typedef struct XPm_Device XPm_Device;
-typedef struct XPm_Requirement XPm_Requirement;
 typedef enum {
         RELEASE_ONE,
         RELEASE_ALL,
@@ -63,10 +42,10 @@ enum XPm_ReqUsageFlags{
 
 enum XPm_ReqSecurityFlags{
         REQ_ACCESS_SECURE,
-		REQ_ACCESS_SECURE_NONSECURE,
+	REQ_ACCESS_SECURE_NONSECURE,
 };
 
-#define MAX_REQ_PARAMS 1
+#define MAX_REQ_PARAMS 		1U
 #define REG_FLAGS_USAGE_MASK	0x3U
 #define REG_FLAGS_SECURITY_MASK	0x4U
 #define REG_FLAGS_SECURITY_OFFSET 0x2U
@@ -74,15 +53,16 @@ enum XPm_ReqSecurityFlags{
 /**
  * The requirement class.
  */
-struct XPm_Requirement {
-	XPm_Subsystem *Subsystem; /**< Subsystem imposing this requirement on the device */
+struct XPm_Reqm {
+	struct XPm_Subsystem *Subsystem; /**< Subsystem imposing this requirement on the device */
 	XPm_Device *Device; /**< Device used by the subsystem */
 	XPm_Requirement *NextDevice; /**< Requirement on the next device from this subsystem */
 	XPm_Requirement *NextSubsystem; /**< Requirement from the next subsystem on this device */
 	u8 Allocated; /**< Device has been allocated to the subsystem */
 	u8 SetLatReq; /**< Latency has been set from the subsystem */
 	u8 Flags;	  /** Flags */
-	u32 Params[MAX_REQ_PARAMS];
+	u8 NumParams; /**< Params count */
+	u32 Params[MAX_REQ_PARAMS]; /**< Params */
 	XPm_ReqmInfo Curr; /**< Current requirements */
 	XPm_ReqmInfo Next; /**< Pending requirements */
 };
@@ -94,6 +74,7 @@ void XPm_RequiremntUpdate(XPm_Requirement *Reqm);
 XStatus XPmRequirement_Release(XPm_Requirement *Reqm, XPm_ReleaseScope Scope);
 void XPmRequirement_Clear(XPm_Requirement* Reqm);
 XStatus XPmRequirement_UpdateScheduled(XPm_Subsystem *Subsystem, u32 Swap);
+XStatus XPmRequirement_IsExclusive(XPm_Requirement *Reqm);
 
 #ifdef __cplusplus
 }
