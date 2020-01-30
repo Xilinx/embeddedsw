e9a5b395b27e4e9ee987d51dc6c3204aeda202b3
Rajan Vaja <rajan.vaja@xilinx.com>
2020-01-30 04:02:40 -0800

    versal_psmfw: Fix MISRA violations 10.3
    Fix below MISRA-C violation:
     - MISRA C-2012 Rule 10.3: The value of an expression shall not be
    			   assigned to an object with a narrower
    			   essential type or of a different essential
    			   type category
    
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    
    Acked-for-series: Jyotheeswar Reddy Mutthareddyvari <jyotheeswar.reddy.mutthareddyvari@xilinx.com>

--- lib/sw_apps/versal_psmfw/src/xpsmfw_ipi_manager.c
+++ lib/sw_apps/versal_psmfw/src/xpsmfw_ipi_manager.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2018-2019 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2018-2020 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -108,7 +108,7 @@ int XPsmFw_DispatchIpiHandler(u32 SrcMask)
 			} else {
 				Status = XPsmFw_ProcessIpi(&Payload[0]);
 
-				Response[0] = Status;
+				Response[0] = (u32)Status;
 				XPsmFw_IpiSendResponse(IPI_PSM_IER_PMC_MASK,
 						       Response);
 			}
