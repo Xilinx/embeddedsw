1bdf4c2ef5f6de3806a30e11c96cc211a4f6fdfa
Nicole Baze <nicole.baze@xilinx.com>
2020-01-30 00:10:55 -0800

    versal: server: Add APIs to unlock and lock AIE PCSR registers
    This commit adds APIs for locking and unlocking the AIE PCSR
    registers to allow writing to the PCSR mask and control registers.
    Also, register definitions are moved from the AIE file to the general
    register definition file.
    
    Signed-off-by: Nicole Baze <nicole.baze@xilinx.com>
    
    Acked-for-series: Jyotheeswar Reddy Mutthareddyvari <jyotheeswar.reddy.mutthareddyvari@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_aie.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_aie.h
@@ -1,35 +1,15 @@
 /******************************************************************************
-*
-* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
-*
+* Copyright (c) 2019 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 
 #ifndef XPM_AIE_H_
 #define XPM_AIE_H_
 
 #include "xpm_powerdomain.h"
-#include "xillibpm_defs.h"
-#include "xillibpm_psm_api.h"
+#include "xpm_defs.h"
+#include "xpm_psm_api.h"
 #include "xpm_ipi.h"
 
 #ifdef __cplusplus
@@ -44,6 +24,8 @@ typedef struct XPm_AieDomain {
 } XPm_AieDomain;
 
 /************************** Function Prototypes ******************************/
+void XPmAieDomain_UnlockPcsr(u32 BaseAddress);
+void XPmAieDomain_LockPcsr(u32 BaseAddress);
 XStatus XPmAieDomain_Init(XPm_AieDomain *AieDomain, u32 Id, u32 BaseAddress,
 			   XPm_Power *Parent);
 
