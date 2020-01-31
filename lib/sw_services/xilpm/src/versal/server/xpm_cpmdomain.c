cf3e95429fe99c0d6c4400e33e7e5951d474e7cb
Amit Sunil Dhamne <amit.sunil.dhamne@xilinx.com>
2020-01-30 17:31:16 -0800

    xilpm: versal: server: Add housecleaning support for CPM5_GTYP
    Add housecleaning support to CPM5_GTYP. Housecleaning sequences
    for CPM5_GTYP includes Memclear and BISR
    
    Signed-off-by: Amit Sunil Dhamne <amit.sunil.dhamne@xilinx.com>
    
    Acked-for-series: Jyotheeswar Reddy Mutthareddyvari <jyotheeswar.reddy.mutthareddyvari@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_cpmdomain.c
+++ lib/sw_services/xilpm/src/versal/server/xpm_cpmdomain.c
@@ -30,10 +30,18 @@
 #include "xpm_bisr.h"
 #include "xpm_power.h"
 #include "sleep.h"
+#include "xpm_device.h"
 
 #define XPM_HC_CPM_OPS			0U
 #define XPM_HC_CPM5_OPS			1U
 #define XPM_CPM_OPS_MAX			2U
+
+/* Define CPM5_GTYP device */
+#define XPM_NODEIDX_DEV_GTYP_CPM5_MIN		XPM_NODEIDX_DEV_GTYP_CPM5_0
+#define XPM_NODEIDX_DEV_GTYP_CPM5_MAX		XPM_NODEIDX_DEV_GTYP_CPM5_3
+
+u32 GtyAddresses[XPM_NODEIDX_DEV_GTYP_CPM5_MAX -
+		XPM_NODEIDX_DEV_GTYP_CPM5_MIN + 1] = {0};
 
 static XStatus CpmInitStart(u32 *Args, u32 NumOfArgs)
 {
@@ -77,6 +85,8 @@ static XStatus Cpm5InitStart(u32 *Args, u32 NumofArgs)
 {
 	XStatus Status = XPM_ERR_INIT_START;
 	XPm_CpmDomain *Cpm;
+	u32 i;
+	XPm_Device* Device = NULL;
 
 	/* This function does not use any args */
 	(void)Args;
@@ -97,6 +107,12 @@ static XStatus Cpm5InitStart(u32 *Args, u32 NumofArgs)
 	if (XST_SUCCESS != Status)
 		goto done;
 
+	/* Initialize Array with GTYP Base Addresses*/
+	for (i = 0; i < ARRAY_SIZE(GtyAddresses); ++i) {
+		Device = XPmDevice_GetById((u32)GT_DEVID(XPM_NODEIDX_DEV_GTYP_CPM5_MIN + i));
+		if(NULL != Device)
+			GtyAddresses[i] = Device->Node.BaseAddress;
+	}
 
 done:
 	return Status;
@@ -225,10 +241,6 @@ static XStatus Cpm5ScanClear(u32 *Args, u32 NumOfArgs)
 	/* Lock PCSR */
 	PmOut32(Cpm->CpmPcsrBaseAddr + CPM_PCSR_LOCK_OFFSET, 1);
 
-	/* TODO: Initiate Housecleaning on GTYP. Since functional isolation,
-	 * and de-assertion of clock gating + removal of reset happens through
-	 * CDO. This step will have to be pushed out to later steps*/
-
 done:
 	return Status;
 }
@@ -267,8 +279,15 @@ static XStatus Cpm5Bisr(u32 *Args, u32 NumOfArgs)
 		goto done;
 	}
 
-	/* Bisr */
+	/* Bisr on CPM5 PD*/
 	Status = XPmBisr_Repair(CPM5_TAG_ID);
+	if (XST_SUCCESS != Status)
+		goto done;
+
+	/* Bisr on GTYP_CPM5 */
+	Status = XPmBisr_Repair(CPM5_GTYP_TAG_ID);
+	if (XST_SUCCESS != Status)
+		goto done;
 
 done:
 	return Status;
@@ -337,11 +356,36 @@ done:
         return Status;
 }
 
+static XStatus Cpm5GtypMbist(u32 BaseAddress)
+{
+	XStatus Status = XST_FAILURE;
+
+	PmOut32(BaseAddress + GTY_PCSR_MASK_OFFSET, GTY_PCSR_MEM_CLEAR_TRIGGER_MASK);
+	PmOut32(BaseAddress + GTY_PCSR_CONTROL_OFFSET,
+	GTY_PCSR_MEM_CLEAR_TRIGGER_MASK);
+	Status = XPm_PollForMask(BaseAddress + GTY_PCSR_STATUS_OFFSET,
+	GTY_PCSR_STATUS_MEM_CLEAR_DONE_MASK, XPM_POLL_TIMEOUT);
+	if (XST_SUCCESS != Status)
+		goto done;
+
+	Status = XPm_PollForMask(BaseAddress + GTY_PCSR_STATUS_OFFSET,
+	GTY_PCSR_STATUS_MEM_CLEAR_PASS_MASK, XPM_POLL_TIMEOUT);
+	if (XST_SUCCESS != Status)
+		goto done;
+
+	/* Unwrite Trigger bits */
+	PmOut32(BaseAddress + GTY_PCSR_MASK_OFFSET, GTY_PCSR_MEM_CLEAR_TRIGGER_MASK);
+	PmOut32(BaseAddress + GTY_PCSR_CONTROL_OFFSET, 0);
+
+done:
+	return Status;
+}
+
 static XStatus Cpm5MbistClear(u32 *Args, u32 NumOfArgs)
 {
 	XStatus Status = XPM_ERR_MBIST_CLR;
 	XPm_CpmDomain *Cpm;
-	u32 RegValue;
+	u32 RegValue, i;
 
 	/* This function does not use the args */
 	(void)Args;
@@ -387,6 +431,19 @@ static XStatus Cpm5MbistClear(u32 *Args, u32 NumOfArgs)
 		0x0);
 	PmOut32(Cpm->CpmSlcrSecureBaseAddr + CPM5_SLCR_SECURE_WPROTS_OFFSET,
 		0x1);
+
+	for (i = 0; ARRAY_SIZE(GtyAddresses) && (0 != GtyAddresses[i]); ++i) {
+		PmOut32(GtyAddresses[i] + GTY_PCSR_LOCK_OFFSET, PCSR_UNLOCK_VAL);
+		/* Mbist */
+		Status = Cpm5GtypMbist(GtyAddresses[i]);
+		if (Status != XST_SUCCESS) {
+			PmErr("ERROR: CPM5 GTYP Mem clear failed\n\r");
+			PmOut32(GtyAddresses[i] + GTY_PCSR_LOCK_OFFSET, 1);
+			goto done;
+		}
+
+		PmOut32(GtyAddresses[i] + GTY_PCSR_LOCK_OFFSET, 1);
+	}
 
 done:
 	return Status;
