bde2c9acef26c7115bff96b81ccf940afd8355a3
Ravi Patel <ravi.patel@xilinx.com>
2020-02-03 01:31:24 -0800

    xilpm: versal: server: Fix MISRA 7.2 violation
    Fix below MISRA-C violation:
    
    - misra_c_2012_rule_7_2_violation:
    	A "u" or "U" suffix shall be applied to all integer
    	constants that are represented in an unsigned type
    
    Signed-off-by: Ravi Patel <ravi.patel@xilinx.com>
    
    Acked-for-series: Jyotheeswar Reddy Mutthareddyvari <jyotheeswar.reddy.mutthareddyvari@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_board.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_board.h
@@ -1,26 +1,8 @@
 /******************************************************************************
-*
-* Copyright (C) 2019 Xilinx, Inc. All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*
+* Copyright (c) 2019 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 
 #ifndef XPM_BOARD_H_
 #define XPM_BOARD_H_
@@ -40,9 +22,10 @@ extern "C" {
 #define IIC_BASE_ADDR			PMC_I2C_ADDR
 
 /************************** Address Definitions ******************************/
-#define PMC_I2C_ADDR			0xF1000000
+#define PMC_I2C_ADDR			0xF1000000U
 #define I2C0_MUX_ADDR			0x74
 #define PSFP_REGULATOR_ADDR		0x0A
+#define PSLP_REGULATOR_ADDR		0x09
 
 /************************ MUX Channel Definitions ****************************/
 /***************** Channels 5-7 are currently not configured *****************/
