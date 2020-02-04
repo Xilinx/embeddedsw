4d12b6a0b2cd706dea9db0434f86a7e4dd38c7d0
Rajan Vaja <rajan.vaja@xilinx.com>
2020-02-04 06:00:50 -0800

    xilpm: versal: server: Add Support of Subsystem Request Wakeup by subsystemId
    Currently request wakeup of processors are supported.
    Add support for the request wakeup of subsystem by subsystemId.
    
    During request wakeup of subsystem by subsystemId, restart subsystem
    image, which will load subsystem's CDOs and elfs for respective
    subsystem.
    
    Request Wakeup of self subsystem by subsystemId is not allowed.
    
    If subsystem was force powerdown and in powered off state then it should
    wakeup by subsystemId only, If some one tries to wakeup by core ID
    then return error of XPM_ERR_SUBSYS_NOTFOUND (error code: 0x2038).
    
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    
    Acked-for-series: Jyotheeswar Reddy Mutthareddyvari <jyotheeswar.reddy.mutthareddyvari@xilinx.com>

--- lib/sw_services/xilpm/src/versal/common/xpm_err.h
+++ lib/sw_services/xilpm/src/versal/common/xpm_err.h
@@ -1,28 +1,8 @@
 /******************************************************************************
-*
-* Copyright (C) 2019 Xilinx, Inc. All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*
-*
-*
+* Copyright (c) 2019 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 
 /*****************************************************************************/
 /**
@@ -132,6 +112,8 @@ extern "C" {
 #define XPM_INVALID_SUBSYSID                     0x2036 /* Invalid subsystem id
 							   passed to func */
 #define XPM_ERR_SUBSYS_IDLE                      0x2037 /* Unable to idle subs-
+							   ystem */
+#define XPM_ERR_SUBSYS_NOTFOUND			 0x2038 /* Unable to Find subs-
 							   ystem */
 
 /******************************* Device Errors **************************/
