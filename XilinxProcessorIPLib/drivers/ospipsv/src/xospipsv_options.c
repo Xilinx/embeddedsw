f861db51bc96acfea52e7d7507d3a1a7daf6836a
P L Sai Krishna <lakshmi.sai.krishna.potthuri@xilinx.com>
2020-02-05 16:04:13 +0530

    ospipsv: Updated copyright year and add_to_group tags
    This patch updates the copyright year and driver version
    in add to group tags.
    
    Signed-off-by: Sai Krishna Potthuri <lakshmi.sai.krishna.potthuri@xilinx.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@xilinx.com>

--- XilinxProcessorIPLib/drivers/ospipsv/src/xospipsv_options.c
+++ XilinxProcessorIPLib/drivers/ospipsv/src/xospipsv_options.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2018-2019 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2018-2020 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -27,7 +27,7 @@
 /**
 *
 * @file xospipsv_options.c
-* @addtogroup ospipsv_v1_1
+* @addtogroup ospipsv_v1_2
 * @{
 *
 * This file implements functions to configure the OSPIPSV component,
--- XilinxProcessorIPLib/drivers/ospipsv/src/xospipsv_sinit.c
+++ XilinxProcessorIPLib/drivers/ospipsv/src/xospipsv_sinit.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2018-2019 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2018-2020 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -27,7 +27,7 @@
 /**
 *
 * @file xospipsv_sinit.c
-* @addtogroup ospipsv_v1_1
+* @addtogroup ospipsv_v1_2
 * @{
 *
 * The implementation of the XOspiPsv component's static initialization
