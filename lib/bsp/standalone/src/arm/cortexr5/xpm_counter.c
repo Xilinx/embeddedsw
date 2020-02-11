Xilinx/embeddedsw

commit 4da13542868db1e907957394681ca97125e2315c

BSP: arm: cortexr5: Support PMU cycle counter for sleep routines
Updated sleep routines for CortexR5 BSP to use PMU cylce counter
for delay generation, in case ttc2 and ttc3 is not present in HW design
If user dont want to use PMU cycle counter in sleep routines,
-DDONT_USE_PMU_FOR_SLEEP_ROUTINES flag needs to be added in BSP
compiler flags. In that case machine cycles would be used
for delay generation.

Signed-off-by: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>

Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

lib/bsp/standalone/src/arm/cortexr5/xpm_counter.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2014 - 2019 Xilinx, Inc. All rights reserved.
+* Copyright (C) 2014 - 2020 Xilinx, Inc. All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -40,6 +40,13 @@
 * 5.00  pkp  02/10/14 Initial version
 * 6.2   mus  01/27/17 Updated to support IAR compiler
 * 7.1   aru  04/15/19 Updated the events correctly
+* 7.2   mus  01/29/20 Added helper function Xpm_SleepPerfCounter for sleep
+*                     routines. It would be consumed by sleep routines for
+*                     delay generation through CortexR5 PMU cycle counter,if
+*                     TTC2 as well as TTC3 is not present in HW design. User
+*                     can add compiler flag "DONT_USE_PMU_FOR_SLEEP_ROUTINES"
+*                     in BSP compiler flags to avoid using PMU cycle counter
+*                     for sleep routines.
 * </pre>
 *
 ******************************************************************************/
@@ -47,6 +54,8 @@
 /***************************** Include Files *********************************/
 
 #include "xpm_counter.h"
+#include "xil_sleeptimer.h"
+#include "xtime_l.h"
 
 /************************** Constant Definitions ****************************/
 
@@ -275,4 +284,39 @@ void Xpm_GetEventCounters(u32 *PmCtrValue)
 		  PmCtrValue[Counter] = Cp15Reg; }
 #endif
 	}
+}
+
+/****************************************************************************/
+/**
+*
+* @brief    This is helper function used by sleep/usleep APIs to generate
+*           delay in sec/usec
+*
+* @param    delay - delay time in sec/usec
+* @param    frequency - Number of countes in second/micro second
+*
+* @return       None.
+*
+*****************************************************************************/
+
+void Xpm_SleepPerfCounter(u32 delay, u64 frequency)
+{
+        u64 tEnd = 0U;
+        u64 tCur = 0U;
+        XCntrVal TimeHighVal = 0U;
+        XCntrVal TimeLowVal1 = 0U;
+        XCntrVal TimeLowVal2 = 0U;
+
+        TimeLowVal1 = Xpm_ReadCycleCounterVal();
+        tEnd = (u64)TimeLowVal1 + ((u64)(delay) * frequency);
+        do
+        {
+                TimeLowVal2 = Xpm_ReadCycleCounterVal();
+                if (TimeLowVal2 < TimeLowVal1) {
+                        TimeHighVal++;
+                }
+                TimeLowVal1 = TimeLowVal2;
+                tCur = (((u64) TimeHighVal) << XSLEEP_TIMER_REG_SHIFT) |
+                                                                (u64)TimeLowVal2;
+        }while (tCur < tEnd);
 }

lib/bsp/standalone/src/arm/cortexr5/xpm_counter.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2014 - 2019 Xilinx, Inc. All rights reserved.
+* Copyright (C) 2014 - 2020 Xilinx, Inc. All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -20,7 +20,7 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 * THE SOFTWARE.
 *
-* 
+*
 *
 ******************************************************************************/
 /*****************************************************************************/
@@ -49,6 +49,8 @@
 * ----- ---- -------- -----------------------------------------------
 * 5.00  pkp  02/10/14 Initial version
 * 7.1   aru  04/15/19 Updated the events correctly
+* 7.2   mus  01/29/20 Added new macro Xpm_ReadCycleCounterVal, to
+*                     read PMU cycle counter value
 * </pre>
 *
 ******************************************************************************/
@@ -510,6 +512,7 @@ extern "C" {
 /**************************** Type Definitions ******************************/
 
 /***************** Macros (Inline Functions) Definitions ********************/
+#define Xpm_ReadCycleCounterVal()	mfcp(XREG_CP15_PERF_CYCLE_COUNTER)
 
 /************************** Variable Definitions ****************************/
 
@@ -519,6 +522,8 @@ extern "C" {
 void Xpm_SetEvents(s32 PmcrCfg);
 void Xpm_GetEventCounters(u32 *PmCtrValue);
 
+/* This is helper function for sleep/usleep APIs */
+void Xpm_SleepPerfCounter(u32 delay, u64 frequency);
 #ifdef __cplusplus
 }
 #endif

lib/bsp/standalone/src/arm/cortexr5/xtime_l.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2014 - 2017 Xilinx, Inc. All rights reserved.
+* Copyright (C) 2014 - 2020 Xilinx, Inc. All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -50,6 +50,9 @@
 *                        TTC counter value register is read only.
 * 6.6   srm    10/18/17 Removed XTime_StartTimer API and made XTime_GetTime,
 *                       XTime_SetTime applicable for all the instances of TTC
+* 7.2   mus    01/29/20 Updated XTime_GetTime to use CortexR5 PMU cycle
+*                       counter, in case sleep timer (ttc3/ttc2) is not
+*                       present in the HW design. It fixes CR#1051591.
 *
 * </pre>
 *
@@ -63,10 +66,8 @@
 #include "xil_assert.h"
 #include "xil_io.h"
 #include "xdebug.h"
-
-#if defined SLEEP_TIMER_BASEADDR
+#include "xpm_counter.h"
 #include "xil_sleeptimer.h"
-#endif
 
 /***************** Macros (Inline Functions) Definitions *********************/
 
@@ -78,8 +79,6 @@
 
 /************************** Function Prototypes ******************************/
 
-/* Function definitions are applicable only when TTC is present*/
-#ifdef SLEEP_TIMER_BASEADDR
 
 /****************************************************************************/
 /**
@@ -116,7 +115,10 @@ void XTime_SetTime(XTime Xtime_Global)
 ****************************************************************************/
 void XTime_GetTime(XTime *Xtime_Global)
 {
+#if defined (SLEEP_TIMER_BASEADDR)
 	*Xtime_Global = Xil_In32(SLEEP_TIMER_BASEADDR +
 				      XSLEEP_TIMER_TTC_COUNT_VALUE_OFFSET);
-}
+#elif !defined (DONT_USE_PMU_FOR_SLEEP_ROUTINES)
+	*Xtime_Global = Xpm_ReadCycleCounterVal();
 #endif
+}

lib/bsp/standalone/src/arm/cortexr5/sleep.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2014 - 2017 Xilinx, Inc. All rights reserved.
+* Copyright (C) 2014 - 2020 Xilinx, Inc. All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -48,6 +48,13 @@
 * 6.6	srm      10/18/17 Updated sleep routines to support user configurable
 *			  implementation. Now sleep routines will use TTC
 *                         instance specified by user.
+* 7.2   mus      01/29/20 Updated sleep routines to use CortexR5 PMU for delay
+*                         generation, in case TTC3/TTC2 instances are not
+*                         present in HW design. If user dont want to use PMU
+*                         counter for sleep routines, BSP needs to be compiled
+*                         with "DONT_USE_PMU_FOR_SLEEP_ROUTINES" flag. It fixes
+*                         CR#1051591.
+*
 * </pre>
 *
 ******************************************************************************/
@@ -56,10 +63,8 @@
 #include "sleep.h"
 #include "xtime_l.h"
 #include "xparameters.h"
-
-#if defined (SLEEP_TIMER_BASEADDR)
 #include "xil_sleeptimer.h"
-#endif
+#include "xpm_counter.h"
 
 /*****************************************************************************/
 /*
@@ -89,12 +94,15 @@ unsigned sleep_R5(unsigned int seconds)
 {
 #if defined (SLEEP_TIMER_BASEADDR)
 	Xil_SleepTTCCommon(seconds, COUNTS_PER_SECOND);
+#elif !defined (DONT_USE_PMU_FOR_SLEEP_ROUTINES)
+	Xpm_SleepPerfCounter(seconds, COUNTS_PER_SECOND);
 #else
 #if defined (__GNUC__)
 	__asm__ __volatile__ (
 #elif defined (__ICCARM__)
 	__asm volatile (
 #endif
+
 		"push {r0,r1,r3} \n"
 		"mov r0, %[sec] \n"
 		"mov r1, %[iter] \n"

lib/bsp/standalone/src/arm/cortexr5/usleep.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2014 - 2017 Xilinx, Inc. All rights reserved.
+* Copyright (C) 2014 - 2020 Xilinx, Inc. All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -49,6 +49,12 @@
 * 6.6	srm      10/18/17 Updated sleep routines to support user configurable
 *			  implementation. Now sleep routines will use TTC
 *                         instance specified by user.
+* 7.2   mus      01/29/20 Updated sleep routines to use CortexR5 PMU for delay
+*                         generation, in case TTC3/TTC2 instances are not
+*                         present in HW design. If user dont want to use PMU
+*                         counter for sleep routines, BSP needs to be compiled
+*                         with "DONT_USE_PMU_FOR_SLEEP_ROUTINES" flag. It fixes
+*                         CR#1051591.
 *
 * </pre>
 *
@@ -61,6 +67,7 @@
 #include "xil_types.h"
 #include "xpseudo_asm.h"
 #include "xreg_cortexr5.h"
+#include "xpm_counter.h"
 
 #if defined (SLEEP_TIMER_BASEADDR)
 #include "xil_sleeptimer.h"
@@ -94,6 +101,8 @@ int usleep_R5(unsigned long useconds)
 {
 #if defined (SLEEP_TIMER_BASEADDR)
 	Xil_SleepTTCCommon(useconds, COUNTS_PER_USECOND);
+#elif !defined (DONT_USE_PMU_FOR_SLEEP_ROUTINES)
+	Xpm_SleepPerfCounter(useconds, COUNTS_PER_USECOND);
 #else
 #if defined (__GNUC__)
 	__asm__ __volatile__ (

lib/bsp/standalone/src/arm/cortexr5/xtime_l.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2014 - 2017 Xilinx, Inc. All rights reserved.
+* Copyright (C) 2014 - 2020 Xilinx, Inc. All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -43,6 +43,8 @@
 *						and mask
 * 6.6   srm    10/22/17 Added a warning message for the user configurable sleep
 *                       implementation when default timer is selected by the user
+* 7.2   mus    01/29/20 Updated #defines to support PMU cycle counter for sleep
+*                       routines.
 * </pre>
 *
 ******************************************************************************/
@@ -69,11 +71,25 @@ extern "C" {
 #else
 #define ITERS_PER_SEC  (XPAR_CPU_CORTEXR5_0_CPU_CLK_FREQ_HZ / 4)
 #define ITERS_PER_USEC  (XPAR_CPU_CORTEXR5_0_CPU_CLK_FREQ_HZ / 4000000)
-#define IRQ_FIQ_MASK 	0xC0	/* Mask IRQ and FIQ interrupts in cpsr */
+
+/*
+ * These constants are applicable for the CortexR5 PMU cycle counter.
+ * As boot code is setting up "D" bit in PMCR, cycle counter increments
+ * on every 64th bit of processor cycle
+ */
+
+#define COUNTS_PER_SECOND	(XPAR_CPU_CORTEXR5_0_CPU_CLK_FREQ_HZ / 64)
+#define COUNTS_PER_USECOND	(COUNTS_PER_SECOND/1000000)
 #endif
 
-#if defined (XSLEEP_TIMER_IS_DEFAULT_TIMER)
-#pragma message ("For the sleep routines, TTC3 is used if present else the assembly instructions are called")
+#define IRQ_FIQ_MASK 	0xC0	/* Mask IRQ and FIQ interrupts in cpsr */
+
+#if defined (SLEEP_TIMER_BASEADDR)
+#pragma message ("For the sleep routines, TTC3/TTC2 is used")
+#elif !defined (DONT_USE_PMU_FOR_SLEEP_ROUTINES)
+#pragma message ("For the sleep routines, CortexR5 PMU cycle counter is used")
+#else
+#pragma message ("For the sleep routines, machine cycles are used")
 #endif
 
 /*
@@ -83,14 +99,10 @@ extern "C" {
 #define IOU_SLCR_TZ_MASK	0x2U
 /**************************** Type Definitions *******************************/
 
-/* The following definitions are applicable only when TTC3 is present*/
-#ifdef SLEEP_TIMER_BASEADDR
 typedef u32 XTime;
 
 void XTime_SetTime(XTime Xtime_Global);
 void XTime_GetTime(XTime *Xtime_Global);
-#endif
-
 
 #ifdef __cplusplus
 }

commit 1ecc27efaede9029b17133cbc586fc8b374857c9

BSP: cortexr5: Fix Xpm_ReadCycleCounterVal for IAR compiler
Fixes IAR build broken by 4da13542868db1e907957394681ca97125e2315c

Signed-off-by: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>
Acked-by: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

lib/bsp/standalone/src/arm/cortexr5/xpm_counter.c
@@ -307,11 +307,21 @@ void Xpm_SleepPerfCounter(u32 delay, u64 frequency)
         XCntrVal TimeLowVal1 = 0U;
         XCntrVal TimeLowVal2 = 0U;
 
+	#if defined (__GNUC__)
         TimeLowVal1 = Xpm_ReadCycleCounterVal();
+	#elif defined (__ICCARM__)
+        Xpm_ReadCycleCounterVal(TimeLowVal1);
+	#endif
+
         tEnd = (u64)TimeLowVal1 + ((u64)(delay) * frequency);
         do
         {
+		#if defined (__GNUC__)
                 TimeLowVal2 = Xpm_ReadCycleCounterVal();
+		#elif defined (__ICCARM__)
+		Xpm_ReadCycleCounterVal(TimeLowVal2);
+		#endif
+
                 if (TimeLowVal2 < TimeLowVal1) {
                         TimeHighVal++;
                 }

lib/bsp/standalone/src/arm/cortexr5/xpm_counter.h
@@ -512,8 +512,11 @@ extern "C" {
 /**************************** Type Definitions ******************************/
 
 /***************** Macros (Inline Functions) Definitions ********************/
+#if defined(__GNUC__)
 #define Xpm_ReadCycleCounterVal()	mfcp(XREG_CP15_PERF_CYCLE_COUNTER)
-
+#elif defined (__ICCARM__)
+#define Xpm_ReadCycleCounterVal(val)       mfcp(XREG_CP15_PERF_CYCLE_COUNTER,val)
+#endif
 /************************** Variable Definitions ****************************/
 
 /************************** Function Prototypes *****************************/

lib/bsp/standalone/src/arm/cortexr5/xtime_l.c
@@ -119,6 +119,10 @@ void XTime_GetTime(XTime *Xtime_Global)
 	*Xtime_Global = Xil_In32(SLEEP_TIMER_BASEADDR +
 				      XSLEEP_TIMER_TTC_COUNT_VALUE_OFFSET);
 #elif !defined (DONT_USE_PMU_FOR_SLEEP_ROUTINES)
+	#if defined (__GNUC__)
 	*Xtime_Global = Xpm_ReadCycleCounterVal();
+	#elif defined (__ICCARM__)
+	Xpm_ReadCycleCounterVal(*Xtime_Global);
+	#endif
 #endif
 }