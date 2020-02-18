c77825d07992d86b33b9113f0cf3229c37215ccc
Ravi Patel <ravi.patel@xilinx.com>
2020-02-18 04:53:25 -0800

    xilpm: versal: client: Fix MISRA 10.1 violation
    Fix below MISRA-C violation:
    - misra_c_2012_rule_10_1_violation:
    	Operands shall not be of an inappropriate essential type
    
    Signed-off-by: Ravi Patel <ravi.patel@xilinx.com>
    
    Acked-for-series: Jolly Shah <jolly.shah@xilinx.com>

--- lib/sw_services/xilpm/src/versal/client/pm_client.h
+++ lib/sw_services/xilpm/src/versal/client/pm_client.h
@@ -1,28 +1,8 @@
 /******************************************************************************
-*
-* Copyright (C) 2018-2019 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
-*
+* Copyright (c) 2018 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 
 #ifndef PM_CLIENT_H_
 #define PM_CLIENT_H_
@@ -40,11 +20,10 @@ extern "C" {
 
 #define DEBUG_MODE
 
-#ifndef bool
-	#define bool	u8
-	#define true	1U
-	#define false	0U
-#endif
+#define PAYLOAD_ARG_CNT			(6U)	/* 1 for API ID + 5 for API arguments */
+#define RESPONSE_ARG_CNT		(4U)	/* 1 for status + 3 for values */
+#define PM_IPI_TIMEOUT			(~0U)
+#define TARGET_IPI_INT_MASK		XPAR_XIPIPS_TARGET_PSV_PMC_0_CH0_MASK
 
 /**
  * XPm_Proc - Processor structure
@@ -76,6 +55,11 @@ extern char ProcName[5];
 #else
 	#define XPm_Dbg(MSG, ...)	{}
 #endif
+
+#define pm_print		XPm_Dbg
+#define pm_dbg			XPm_Dbg
+#define pm_read			XPm_Read
+#define pm_write		XPm_Write
 
 void XPm_SetPrimaryProc(void);
 struct XPm_Proc *XPm_GetProcByDeviceId(u32 DeviceId);
