09e36a73ec2d8de82ad696be28d7573e0dcc82a3
Krishna Chaitanya <krishna.chaitanya.patakamuri@xilinx.com>
2020-02-19 03:03:17 -0700

    plm: Moved PLM banner print to XilPlmi
    After recent changes in Vivado to remove LPD initialization from PMC
    CDO, PLM banner print is not coming. So, moved the PLM banner print
    after LPD initialization and moved code to XilPLMI.
    Also removed the HookAfterLpdInit as LPD is not part of PMC CDO now.
    
    Signed-off-by: Krishna Chaitanya <krishna.chaitanya.patakamuri@xilinx.com>
    Acked-by: Vikram Sreenivasa Batchali <b.vikram@xilinx.com>

--- lib/sw_services/xilpm/src/zynqmp/client/common/pm_clock.h
+++ lib/sw_services/xilpm/src/zynqmp/client/common/pm_clock.h
@@ -1,28 +1,8 @@
 /******************************************************************************
-*
-* Copyright (C) 2015-2018 Xilinx, Inc.  All rights reserved.
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in
-* all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-* THE SOFTWARE.
-*
-*
-*
+* Copyright (c) 2015 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
 ******************************************************************************/
+
 
 /*****************************************************************************/
 /**
@@ -42,18 +22,18 @@
 extern "C" {
 #endif
 
-XStatus XPm_GetClockParentBySelect(const enum XPmClock clock,
+XStatus XPm_GetClockParentBySelect(const enum XPmClock clockId,
 				   const u32 select,
-				   enum XPmClock* const parent);
+				   enum XPmClock* const parentId);
 
-XStatus XPm_GetSelectByClockParent(const enum XPmClock clock,
-				   const enum XPmClock parent,
+XStatus XPm_GetSelectByClockParent(const enum XPmClock clockId,
+				   const enum XPmClock parentId,
 				   u32* const select);
 
-u8 XPm_GetClockDivType(const enum XPmClock clock);
+u8 XPm_GetClockDivType(const enum XPmClock clockId);
 
-u8 XPm_MapDivider(const enum XPmClock clock,
-		  const u32 div,
+u8 XPm_MapDivider(const enum XPmClock clockId,
+		  const u32 div_val,
 		  u32* const div0,
 		  u32* const div1);
 
