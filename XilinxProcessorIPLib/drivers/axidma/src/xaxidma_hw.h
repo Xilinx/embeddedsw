62f63dcafef789c20f4d300e33014c1780cc3a3e
Shravya Kumbham <shravya.kumbham@xilinx.com>
2020-03-06 05:26:29 -0700

    axidma: Update doxygen tags and copyright year
    Increment version. Update doxygen tags and copyright year.
    
    Signed-off-by: Shravya Kumbham <shravya.kumbham@xilinx.com>

--- XilinxProcessorIPLib/drivers/axidma/src/xaxidma_hw.h
+++ XilinxProcessorIPLib/drivers/axidma/src/xaxidma_hw.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2010 - 2019 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2020 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -26,7 +26,7 @@
 /*****************************************************************************/
 /**
  *  @file xaxidma_hw.h
-* @addtogroup axidma_v9_10
+* @addtogroup axidma_v9_11
 * @{
  *
  * Hardware definition file. It defines the register interface and Buffer
--- XilinxProcessorIPLib/drivers/axidma/src/xaxidma_selftest.c
+++ XilinxProcessorIPLib/drivers/axidma/src/xaxidma_selftest.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2015 - 2019 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2015 - 2020 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -27,7 +27,7 @@
 /**
 *
 * @file xaxidma_selftest.c
-* @addtogroup axidma_v9_10
+* @addtogroup axidma_v9_11
 * @{
 *
 * Contains diagnostic/self-test functions for the XAxiDma component.
