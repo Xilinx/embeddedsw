c0d490ec23323dcb981cbbfd285b3f21fa97f860
Mounika Akula <mounika.akula@xilinx.com>
2020-03-19 11:49:08 +0530

    sw_apps: zynqmp_fsbl: Update the status of FSBL image encryption in PMU Global register
    This patch updates PMU Global General Storage register 5 bit 3 with FSBL
    encryption status. FSBL sets this bit to 1 if boot header indicates that
    FSBL is encrypted or ENC_ONLY eFuse bit is programmed.
    PMU checks this information and skips copying FSBL to DDR if this bit is set.
    
    Signed-off-by: Mounika Akula <mounika.akula@xilinx.com>
    
    Acked-for-series: Rajan Vaja <rajan.vaja@xilinx.com>

--- lib/sw_apps/zynqmp_fsbl/src/xfsbl_main.h
+++ lib/sw_apps/zynqmp_fsbl/src/xfsbl_main.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2015 - 19 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2015 - 2020 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -183,6 +183,7 @@ typedef struct {
 #define XFSBL_STATE_PROC_SHIFT			(0x1U)
 
 #define XFSBL_STATE_PROC_INFO_MASK		(0x3U << XFSBL_STATE_PROC_SHIFT)
+#define XFSBL_FSBL_ENCRYPTED_MASK		(0x8U)
 
 
 /************************** Function Prototypes ******************************/
