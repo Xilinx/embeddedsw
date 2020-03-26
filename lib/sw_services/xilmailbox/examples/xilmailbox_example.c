3b44baeec4c26131ec556590ee33168bf51bd356
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2020-03-26 15:20:09 +0530

    lib: sw_services: xilmailbox: Update library version and copyright year
    This patch updates the library version number and copyright year
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

--- lib/sw_services/xilmailbox/examples/xilmailbox_example.c
+++ lib/sw_services/xilmailbox/examples/xilmailbox_example.c
@@ -1,7 +1,7 @@
 
 /******************************************************************************
 *
-* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2019-2020 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
