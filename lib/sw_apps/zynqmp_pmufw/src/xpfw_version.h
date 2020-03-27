627b013b1d825dc706da81cad5dd623bd96988af
Sharath Kumar Dasari <sharath.kumar.dasari@xilinx.com>
2020-03-27 22:17:55 +0530

    sw_apps :zynqmp_pmufw: Release version updated
    Updated release version to 2020.2 on master branch
    
    Signed-off-by: Sharath Kumar Dasari <sharath.kumar.dasari@xilinx.com>
    Acked-by: Mounika Akula <mounika.akula@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2019 - 2020 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -28,7 +28,7 @@
 extern "C" {
 #endif
 
-#define ZYNQMP_XPFW_VERSION	"2020.1"
+#define ZYNQMP_XPFW_VERSION	"2020.2"
 
 #ifdef __cplusplus
 }
