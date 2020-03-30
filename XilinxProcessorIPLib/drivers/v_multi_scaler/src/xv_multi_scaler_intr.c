40330e814bf0140ace3a43d41ce89b1b8f3ecfaa
Prasad Gutti <prasad.gutti@xilinx.com>
2020-03-30 02:39:36 -0600

    v_multi_scaler: Updated copy right year and addtogroup
    Updated copyright year to 2020 and addtogroup to v_multi_scaler_v1_1
    
    Signed-off-by: Prasad Gutti <prasad.gutti@xilinx.com>
    
    Acked-for-series: Sandip Kothari <sandipk@xilinx.com>

--- XilinxProcessorIPLib/drivers/v_multi_scaler/src/xv_multi_scaler_intr.c
+++ XilinxProcessorIPLib/drivers/v_multi_scaler/src/xv_multi_scaler_intr.c
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- * Copyright (C) 2018 Xilinx, Inc.  All rights reserved.
+ * Copyright (C) 2020 Xilinx, Inc.  All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,7 +27,7 @@
 /**
  *
  * @file xv_multi_scaler_intr.c
- * @addtogroup v_multi_scaler_v1_0
+ * @addtogroup v_multi_scaler_v1_1
  * @{
  *
  * The functions in this file provides interrupt handler and associated
--- XilinxProcessorIPLib/drivers/v_multi_scaler/src/xv_multi_scaler_l2.c
+++ XilinxProcessorIPLib/drivers/v_multi_scaler/src/xv_multi_scaler_l2.c
@@ -1,6 +1,6 @@
 /******************************************************************************
  *
- * Copyright (C) 2018 Xilinx, Inc.	All rights reserved.
+ * Copyright (C) 2020 Xilinx, Inc.	All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,7 +27,7 @@
 /**
 *
 * @file xv_multi_scaler_l2.c
-* @addtogroup v_multi_scaler_v1_0
+* @addtogroup v_multi_scaler_v1_1
 * @{
 *
 * The Multi Scaler Layer-2 Driver.
