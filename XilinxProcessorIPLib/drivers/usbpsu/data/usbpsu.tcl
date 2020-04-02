acef22a166a3d546e79ebfb0b901a868b137f016
Piyush Mehta <piyush.mehta@xilinx.com>
2020-04-02 12:16:43 +0530

    usb: usbpsu: Add clock support
    This patch adds clock support in usbpsu driver.
    
    Signed-off-by: Piyush Mehta <piyush.mehta@xilinx.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@xilinx.com>

--- XilinxProcessorIPLib/drivers/usbpsu/data/usbpsu.tcl
+++ XilinxProcessorIPLib/drivers/usbpsu/data/usbpsu.tcl
@@ -1,6 +1,6 @@
 ###############################################################################
 #
-# Copyright (C) 2016 - 2019 Xilinx, Inc.  All rights reserved.
+# Copyright (C) 2016 - 2020 Xilinx, Inc.  All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -34,6 +34,7 @@
 # 1.4	vak 24/09/18 Added SUPER_SPEED parameter
 # 1.5	vak 13/02/19 Correct the logic for setting SUPER_SPEED parameter
 # 1.6	mus 07/30/19 Added CCI support for Versal at EL1 NS
+# 1.7	pm  03/14/20 Added Clock support
 #
 ##############################################################################
 
@@ -44,10 +45,30 @@ proc generate {drv_handle} {
 
     generate_usb_params $drv_handle "xparameters.h"
 
+	set clocking [common::get_property CONFIG.clocking [hsi::get_os]]
+	set is_zynqmp_fsbl_bsp [common::get_property CONFIG.ZYNQMP_FSBL_BSP [hsi::get_os]]
+	set cortexa53proc [hsi::get_cells -hier -filter {IP_NAME=="psu_cortexa53"}]
+	set isclocking [check_clocking]
+
+	if { $isclocking == 1 && $is_zynqmp_fsbl_bsp != true   &&  [llength $cortexa53proc] > 0 && [string match -nocase $clocking "true"] > 0} {
+
+    ::hsi::utils::define_zynq_config_file $drv_handle "xusbpsu_g.c" "XUsbPsu" "DEVICE_ID" "C_S_AXI_BASEADDR" "IS_CACHE_COHERENT" "SUPER_SPEED" "REF_CLK"
+	} else {
     ::hsi::utils::define_zynq_config_file $drv_handle "xusbpsu_g.c" "XUsbPsu" "DEVICE_ID" "C_S_AXI_BASEADDR" "IS_CACHE_COHERENT" "SUPER_SPEED"
+	}
 
     ::hsi::utils::define_zynq_canonical_xpars $drv_handle "xparameters.h" "XUsbPsu" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR"
+}
 
+proc check_clocking { } {
+	set sw_proc_handle [hsi::get_sw_processor]
+	set slaves [common::get_property   SLAVES [  hsi::get_cells -hier $sw_proc_handle]]
+	foreach slave $slaves {
+		if {[string compare -nocase "psu_crf_apb" $slave] == 0 } {
+			return 1
+		}
+	}
+	return 0
 }
 
 proc generate_usb_params {drv_handle file_name} {
@@ -58,14 +79,21 @@ proc generate_usb_params {drv_handle file_name} {
 	set sw_processor [hsi::get_sw_processor]
 	set processor [hsi::get_cells -hier [common::get_property HW_INSTANCE $sw_processor]]
 	set processor_type [common::get_property IP_NAME $processor]
+	set isclocking [check_clocking]
 
 	foreach ip $ips {
 		set is_cc 0
+		set ref_tag 0xff
 		if {$processor_type == "psu_cortexa53"} {
 			set is_xen [common::get_property CONFIG.hypervisor_guest [hsi::get_os]]
 			if {$is_xen == "true"} {
 				set is_cc [common::get_property CONFIG.IS_CACHE_COHERENT $ip]
 			}
+			set ipname [common::get_property NAME $ip]
+			set pos [string length $ipname]
+			set num [ expr {$pos -1} ]
+			set index [string index $ipname $num]
+			set ref_tag [string toupper [format "USB%d_BUS_REF" $index ]]
 		} elseif {$processor_type == "psv_cortexa72"} {
 			set extra_flags [common::get_property CONFIG.extra_compiler_flags [hsi::get_sw_processor]]
 			set flagindex [string first {-DARMA72_EL3} $extra_flags 0]
@@ -74,7 +102,9 @@ proc generate_usb_params {drv_handle file_name} {
 			}
 		}
 		puts $file_handle "\#define [::hsi::utils::get_driver_param_name $ip "IS_CACHE_COHERENT"] $is_cc"
-
+		if { $isclocking == 1 } {
+			puts $file_handle "\#define [::hsi::utils::get_driver_param_name $ip "REF_CLK"] $ref_tag"
+		}
 		set val 0
 		set peripheral [get_cells -hier -filter {IP_NAME == zynq_ultra_ps_e}]
 
