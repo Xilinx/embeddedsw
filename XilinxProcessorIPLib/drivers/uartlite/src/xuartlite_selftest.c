26c35c6814fc75cc2cf04b2930d6f1373fa82db1
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2020-04-06 20:36:39 +0530

    uartlite: Update driver version and copyright year and doxygen tags
    This patch does the below
    --> Increments the driver version
    --> Updated copyright year
    --> Updated the doxygen tags as per latest driver version.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

--- XilinxProcessorIPLib/drivers/uartlite/src/xuartlite_selftest.c
+++ XilinxProcessorIPLib/drivers/uartlite/src/xuartlite_selftest.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2002 - 2019 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2002 - 2020 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -27,7 +27,7 @@
 /**
 *
 * @file xuartlite_selftest.c
-* @addtogroup uartlite_v3_3
+* @addtogroup uartlite_v3_4
 * @{
 *
 * This file contains the self-test functions for the UART Lite component
