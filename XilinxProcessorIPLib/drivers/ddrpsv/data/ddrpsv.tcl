dc013ae0dda3744a7ec020e8e7950bf69ca2e091
Shireesha Kothakonda <shireesha.kothakonda@xilinx.com>
2020-04-07 15:13:22 +0530

    ddrpsv: updating license content to SPDX based licensing
    updating license content to SPDX based licensing
    
    Signed-off-by: Shireesha Kothakonda <shireesha.kothakonda@xilinx.com>
    
    Acked-by:Siva Addepalli<saddepal@xilinx.com>

--- XilinxProcessorIPLib/drivers/ddrpsv/data/ddrpsv.tcl
+++ XilinxProcessorIPLib/drivers/ddrpsv/data/ddrpsv.tcl
@@ -1,36 +1,16 @@
-################################################################################
- #
- # Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
- #
- # Permission is hereby granted, free of charge, to any person obtaining a copy
- # of this software and associated documentation files (the "Software"), to deal
- # in the Software without restriction, including without limitation the rights
- # to use, copy, modify, merge, publish, distribute, sublicense, and#or sell
- # copies of the Software, and to permit persons to whom the Software is
- # furnished to do so, subject to the following conditions:
- #
- # The above copyright notice and this permission notice shall be included in
- # all copies or substantial portions of the Software.
- #
- # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
- #
-# 
- #
- #
- # MODIFICATION HISTORY:
- # Ver      Who    Date     Changes
- # -------- ------ -------- ------------------------------------
- # 1.0      mus    03/16/19 First Release.
- #          mn     09/05/19 Correct the variable name for C0_DDR_CH2 block
- #          mn     09/12/19 Check only for initial block name for DDR region
- #
- ################################################################################
+###############################################################################
+# Copyright (C) 2019 - 2020 Xilinx, Inc.  All rights reserved.
+# SPDX-License-Identifier: MIT
+#
+#
+# MODIFICATION HISTORY:
+# Ver      Who    Date     Changes
+# -------- ------ -------- ------------------------------------
+# 1.0      mus    03/16/19 First Release.
+#          mn     09/05/19 Correct the variable name for C0_DDR_CH2 block
+#          mn     09/12/19 Check only for initial block name for DDR region
+#
+###############################################################################
 
 proc generate {drv_handle} {
       define_addr_params $drv_handle "xparameters.h"
