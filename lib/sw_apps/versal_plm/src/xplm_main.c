1195e8e1f51258f44fdb4804ebad39229fb8f550
Jolly Shah <jolly.shah@xilinx.com>
2020-04-08 14:41:00 -0700

    plm: remove pl houseclean call in main
    Houseclean cant be performed before topology cdo is processed
    hence remove from main
    
    Signed-off-by: Jolly Shah <jolly.shah@xilinx.com>
    
    Acked-for-series: Jyotheeswar Reddy Mutthareddyvari <jyotheeswar.reddy.mutthareddyvari@xilinx.com>

--- lib/sw_apps/versal_plm/src/xplm_main.c
+++ lib/sw_apps/versal_plm/src/xplm_main.c
@@ -124,16 +124,6 @@ static int XPlm_Init()
 	 PMC_GLOBAL_DOMAIN_ISO_CNTRL_PMC_PL_CFRAME_MASK, 0U);
 
 	/**
-	 * There is a silicon problem where on 2-4% of Versal ES1 S80 devices
-	 * you can get 12A of VCCINT_PL current before CFI housecleaning is run.
-	 * The problem is eliminated when PL Vgg frame housecleaning is run
-	 * so we need to do that ASAP after PLM is loaded.
-	 * Otherwise also, PL housecleaning needs to be trigerred asap to reduce
-	 * boot time.
-	 */
-	XPmPlDomain_InitandHouseclean();
-
-	/**
 	 * Reset the wakeup signal set by ROM
 	 * Otherwise MB will always wakeup, irrespective of the sleep state
 	 */
