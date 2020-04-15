415cab30db3ca29a1f33914e71b94bbd40ce5335
msreeram <manikanta.sreeram@xilinx.com>
2020-04-15 12:59:23 +0530

    updating copyright line as per guidelines
    for few files (c) is not as per guidelines,
    this patch will update copyright line for all the incorect files in drivers folder.
    
    Signed-off-by: Manikanta Sreeram <manikanta.sreeram@xilinx.com>
    
    Acked-by: Ashok Kumar Madugula<ashok.kumar.madugula@xilinx.com>

--- XilinxProcessorIPLib/drivers/cresample/src/xcresample_g.c
+++ XilinxProcessorIPLib/drivers/cresample/src/xcresample_g.c
@@ -1,8 +1,9 @@
 
 /*******************************************************************
-* Copyright 1986-2020 Xilinx, Inc. All rights reserved.
+* Copyright (c) 1986-2020 Xilinx, Inc. All rights reserved.
 * SPDX-License-Identifier: MIT
 *******************************************************************/
+
 
 
 #include "xparameters.h"
