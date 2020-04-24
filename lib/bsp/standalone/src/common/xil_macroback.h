eb8c00d3de080ebeed5d08eba957a34865b4be55
Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
2020-04-24 14:43:54 +0530

    bsp: Add helpers for backward compatibility for pmonpsv
    Add the pmonpsv macros to xil_macroback.
    
    Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@xilinx.com>

--- lib/bsp/standalone/src/common/xil_macroback.h
+++ lib/bsp/standalone/src/common/xil_macroback.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2010 - 2015 Xilinx, Inc. All rights reserved.
+* Copyright (C) 2010 - 2020 Xilinx, Inc. All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -1047,6 +1047,77 @@ extern "C" {
 #define XUsb_mWriteReg XUsb_WriteReg
 #endif
 
+/*********************************************************************/
+/**
+ * Macros for Driver XPmonPsv
+ *
+ *********************************************************************/
+#ifndef XpsvPmon_CfgInitialize
+#define XpsvPmon_CfgInitialize  XPmonPsv_CfgInitialize
+#endif
+
+#ifndef XpsvPmon_LookupConfig
+#define XpsvPmon_LookupConfig  XPmonPsv_LookupConfig
+#endif
+
+#ifndef XpsvPmon_WriteReg
+#define XpsvPmon_WriteReg  XPmonPsv_WriteReg
+#endif
+
+#ifndef XpsvPmon_SetMetrics
+#define XpsvPmon_SetMetrics  XPmonPsv_SetMetrics
+#endif
+
+#ifndef XpsvPmon_GetMetrics
+#define XpsvPmon_GetMetrics  XPmonPsv_GetMetrics
+#endif
+
+#ifndef XpsvPmon_ResetCounter
+#define XpsvPmon_ResetCounter  XPmonPsv_ResetCounter
+#endif
+
+#ifndef XpsvPmon_GetWriteCounter
+#define XpsvPmon_GetWriteCounter  XPmonPsv_GetWriteCounter
+#endif
+
+#ifndef XpsvPmon_GetReadCounter
+#define XpsvPmon_GetReadCounter  XPmonPsv_GetReadCounter
+#endif
+
+#ifndef XpsvPmon_EnableCounters
+#define XpsvPmon_EnableCounters  XPmonPsv_EnableCounters
+#endif
+
+#ifndef XpsvPmon_StopCounter
+#define XpsvPmon_StopCounter  XPmonPsv_StopCounter
+#endif
+
+#ifndef XpsvPmon_SetPort
+#define XpsvPmon_SetPort  XPmonPsv_SetPort
+#endif
+
+#ifndef XpsvPmon_SetSrc
+#define XpsvPmon_SetSrc  XPmonPsv_SetSrc
+#endif
+
+#ifndef XpsvPmon_Unlock
+#define XpsvPmon_Unlock  XPmonPsv_Unlock
+#endif
+
+#ifndef XpsvPmon_Lock
+#define XpsvPmon_Lock  XPmonPsv_Lock
+#endif
+
+
+#ifndef XpsvPmon_RequestCounter
+#define XpsvPmon_RequestCounter  XPmonPsv_RequestCounter
+#endif
+
+#ifndef XPmonpsv_Config
+#define XPmonpsv_Config  XPmonPsv_Config
+#endif
+
+typedef  XPmonPsv XpsvPmon;
 #ifdef __cplusplus
 }
 #endif
