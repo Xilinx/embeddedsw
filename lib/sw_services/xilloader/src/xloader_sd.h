972ed223fc4f07b22551ebf20f13c9305a54b09a
b.vikram@xilinx.com <b.vikram@xilinx.com>
2020-04-28 12:04:47 +0530

    sw_services :xilloader: Change SD drive number to 5 when both SD0 and SD1 are enabled in design
    This is required as the number of partitions supported in xilffs is changed from 8 to 10.
    The change in xilffs was to support SD boot in file systems without a partition. The patch also
    changed the mask bits in secondary boot address that denote logical drive number to accommodate
    the extra drives added.
    
    Signed-off-by: Vikram Sreenivasa Batchali <b.vikram@xilinx.com>
    Acked-by: Krishna Chaitanya Patakamuri <krishna.chaitanya.patakamuri@xilinx.com>

--- lib/sw_services/xilloader/src/xloader_sd.h
+++ lib/sw_services/xilloader/src/xloader_sd.h
@@ -44,9 +44,14 @@ extern "C" {
 /***************** Macros (Inline Functions) Definitions *********************/
 #define XLOADER_BASE_FILE_NAME_LEN_SD_1 	(11U)
 #define XLOADER_NUM_DIGITS_IN_FILE_NAME 	(4U)
+#define XLOADER_SD_SBD_ADDR_SET_MASK		(0x100U)
+#define XLOADER_SD_SBD_ADDR_SHIFT			(0x9U)
+#define XLOADER_LOGICAL_DRV_MASK		(0xF0000U)
+#define XLOADER_LOGICAL_DRV_SHIFT		(16U)
+#define XLOADER_SD_SBD_ADDR_MASK		(0xFFFFU)
 #define XLOADER_SD_DRV_NUM_0			(0U)
 #define XLOADER_SD_DRV_NUM_1			(1U)
-#define XLOADER_SD_DRV_NUM_4			(4U)
+#define XLOADER_SD_DRV_NUM_5			(5U)
 #define XLOADER_SD_RAW_BLK_SIZE			(512U)
 #define XLOADER_SD_RAW_NUM_SECTORS		(128U)
 
