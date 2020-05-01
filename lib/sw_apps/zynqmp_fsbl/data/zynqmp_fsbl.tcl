49f966acb41adaf45a8df4213469570c463b5f15
b.vikram@xilinx.com <b.vikram@xilinx.com>
2020-05-01 17:38:35 +0530

    sw_apps :zynqmp_fsbl: FSBL project should not be created for ZCU104 and ZCU111 boards if I2C0 is not enabled in design
    This is because ZCU104 and ZCU111 boards have I2C0 interface on them.
    
    Signed-off-by: Vikram Sreenivasa Batchali <b.vikram@xilinx.com>
    Acked-by: Krishna Chaitanya Patakamuri <krishna.chaitanya.patakamuri@xilinx.com>

--- lib/sw_apps/zynqmp_fsbl/data/zynqmp_fsbl.tcl
+++ lib/sw_apps/zynqmp_fsbl/data/zynqmp_fsbl.tcl
@@ -201,7 +201,8 @@ proc swapp_is_supported_hw {} {
         set fields [split $boardname ":"]
         lassign $fields prefix board suffix
 
-        if { $board == "zcu102" || $board == "zcu106" } {
+        if { $board == "zcu102" || $board == "zcu106" || \
+		$board == "zcu104" || $board == "zcu111" } {
             set ip_list "psu_csudma psu_adma_0 psu_ipi_* psu_i2c_0"
         } else {
             set ip_list "psu_csudma psu_adma_0 psu_ipi_*"
