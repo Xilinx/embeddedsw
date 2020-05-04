804e3fc446fc20bbb0c796a6f625a4fa526c9772
meenap <meena.paleti@xilinx.com>
2020-05-04 13:48:51 +0530

    Updated src version with mdd version for v_scenechange
    Updated src version with mdd version for v_scenechange
    
    Signed-off-by: Meena Paleti <meena.paleti@xilinx.com>
    
    Acked-for-series:Siva Addepalli<saddepal@xilinx.com>

--- XilinxProcessorIPLib/drivers/v_scenechange/src/xv_scenechange_intr.c
+++ XilinxProcessorIPLib/drivers/v_scenechange/src/xv_scenechange_intr.c
@@ -7,7 +7,7 @@
 /**
  *
  * @file xv_scenechange_intr.c
- * @addtogroup v_scenechange_v1_0
+ * @addtogroup v_scenechange_v1_1
  * @{
  *
  * The functions in this file provides interrupt handler and associated
