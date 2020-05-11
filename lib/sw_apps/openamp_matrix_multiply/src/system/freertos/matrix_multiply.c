3bb04eb22f63658b2e81758caccfd2e21cca2ba0
Xiang Xiao <xiaoxiang@xiaomi.com>
2020-05-12 00:46:13 +0800

    apps: replace all zero address to RPMSG_ADDR_ANY
    the example should demo the best practice for the endpoint creation
    
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>
    Signed-off-by: Sergei Korneichuk <sergei.korneichuk@xilinx.com>

--- lib/sw_apps/openamp_matrix_multiply/src/system/freertos/matrix_multiply.c
+++ lib/sw_apps/openamp_matrix_multiply/src/system/freertos/matrix_multiply.c
@@ -102,7 +102,8 @@ int app(struct rpmsg_device *rdev, void *priv)
 	int ret;
 
 	ret = rpmsg_create_ept(&lept, rdev, RPMSG_SERVICE_NAME,
-				   0, RPMSG_ADDR_ANY, rpmsg_endpoint_cb,
+				   RPMSG_ADDR_ANY, RPMSG_ADDR_ANY,
+				   rpmsg_endpoint_cb,
 				   rpmsg_service_unbind);
 	if (ret) {
 		ML_ERR("Failed to create endpoint.\n");
