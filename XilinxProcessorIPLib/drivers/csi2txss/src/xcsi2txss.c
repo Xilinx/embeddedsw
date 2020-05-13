fdc8876c631f480d56747ee4409fdcfb3ca8b101
meenap <meena.paleti@xilinx.com>
2020-05-13 13:36:00 +0530

    Updated src version with mdd version for csi2txss component
    Updated src version with mdd version for csi2txss
    
    Signed-off-by: Meena Paleti <meena.paleti@xilinx.com>
    
    Acked-by: Siva Addepalli<saddepal@xilinx.com>

--- XilinxProcessorIPLib/drivers/csi2txss/src/xcsi2txss.c
+++ XilinxProcessorIPLib/drivers/csi2txss/src/xcsi2txss.c
@@ -7,7 +7,7 @@
 /**
 *
 * @file xcsi2txss.c
-* @addtogroup csi2txss_v1_1
+* @addtogroup csi2txss_v1_3
 * @{
 *
 * This is main code of Xilinx MIPI CSI Tx Subsystem device driver.
--- XilinxProcessorIPLib/drivers/csi2txss/src/xcsi2txss_hw.h
+++ XilinxProcessorIPLib/drivers/csi2txss/src/xcsi2txss_hw.h
@@ -7,7 +7,7 @@
 /**
 *
 * @file xcsi2txss_hw.h
-* @addtogroup csi2txss_v1_1
+* @addtogroup csi2txss_v1_3
 * @{
 *
 * This header file contains identifiers and register-level core functions (or
