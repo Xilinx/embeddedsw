27587031edb1e7ac112c88eb166337e132564107
praveen teja kundanala <praveen.teja.kundanala@xilinx.com>
2020-06-17 09:52:20 +0530

    sw_services:xilsecure: This patch modifies the xilsecure sha example
    This patch modifies the following contents in xilsecure sha example
    - String Xilinx is send to sha engine for hashing
    - Compares the expected hash with the output hash
    
    Signed-off-by: Praveen Teja Kundanala <praveen.teja.kundanala@xilinx.com>
    Acked-by: VNSL Durga Challa <vnsl.durga.challa@xilinx.com>

--- lib/sw_services/xilsecure/examples/xilsecure_sha_example.c
+++ lib/sw_services/xilsecure/examples/xilsecure_sha_example.c
@@ -17,7 +17,8 @@
 * ----- ------ -------- -------------------------------------------------
 * 1.00a bameta 11/05/14 First Release
 * 2.2   vns    07/06/16 Added doxygen tags
-*
+* 4.3   kpt    06/15/20 Added hash comparison and modified input data
+*                        string "hell" to "XILINX"
 * </pre>
 ******************************************************************************/
 
@@ -25,6 +26,7 @@
 
 #include "xparameters.h"
 #include "xsecure_sha.h"
+#include "xil_util.h"
 
 /************************** Constant Definitions *****************************/
 
@@ -38,14 +40,28 @@
 
 /***************** Macros (Inline Functions) Definitions *********************/
 
+#define SHA3_HASH_LEN_IN_BYTES  48U
+#define SHA3_INPUT_DATA_LEN     6U
+
 /************************** Function Prototypes ******************************/
 
-int SecureHelloWorldExample(void);
+u32 SecureSha3Example(void);
+u32 SecureSha3CompareHash(u8 *Hash, u8 *ExpectedHash);
+void SecureSha3PrintHash(u8 *Hash);
 
 /************************** Variable Definitions *****************************/
 
-XSecure_Sha3 Secure_Sha3;
-XCsuDma CsuDma;
+const char Data[SHA3_INPUT_DATA_LEN + 1U] = "XILINX";
+
+u8 ExpHash[SHA3_HASH_LEN_IN_BYTES] =
+				     {0x70,0x69,0x77,0x35,0x0b,0x93,
+				      0x92,0xa0,0x48,0x2c,0xd8,0x23,
+				      0x38,0x47,0xd2,0xd9,0x2d,0x1a,
+				      0x95,0x0c,0xad,0xa8,0x60,0xc0,
+				      0x9b,0x70,0xc6,0xad,0x6e,0xf1,
+				      0x5d,0x49,0x68,0xa3,0x50,0x75,
+				      0x06,0xbb,0x0b,0x9b,0x03,0x7d,
+				      0xd5,0x93,0x76,0x50,0xdb,0xd4};
 
 /*****************************************************************************/
 /**
@@ -64,12 +80,12 @@ int main(void)
 {
 	int Status;
 
-	Status = SecureHelloWorldExample();
+	Status = SecureSha3Example();
 	if(Status == XST_SUCCESS) {
 		xil_printf("Successfully ran SHA example");
 	}
 	else {
-		xil_printf("SHA example failed");
+		xil_printf("SHA Example failed");
 	}
 	return Status;
 }
@@ -77,36 +93,47 @@ int main(void)
 /****************************************************************************/
 /**
 *
-* This function sends 'Hello World' to SHA-3 module for hashing.
+* This function sends 'XILINX' to SHA-3 module for hashing.
 * The purpose of this function is to illustrate how to use the XSecure_Sha3
 * driver.
 *
 *
 * @return
-*		- XST_FAILURE if the SHA-3 hashing failed.
+*		- XST_SUCCESS - SHA-3 hash successfully generated for given
+*				input data string.
+*		- XST_FAILURE - if the SHA-3 hash failed.
 *
 * @note		None.
 *
 ****************************************************************************/
 /** //! [SHA3 example] */
-int SecureHelloWorldExample()
+u32 SecureSha3Example()
 {
-	u8 HelloWorld[4] = {'h','e','l','l'};
-	u32 Size = sizeof(HelloWorld);
-	u8 Out[384/8];
+	XSecure_Sha3 Secure_Sha3;
+	XCsuDma CsuDma;
 	XCsuDma_Config *Config;
+	u8 Out[SHA3_HASH_LEN_IN_BYTES];
+	u32 Status = XST_FAILURE;
+	u32 Size = 0U;
 
-	int Status;
+	Size = Xil_Strnlen(Data, SHA3_INPUT_DATA_LEN);
+	if (Size != SHA3_INPUT_DATA_LEN) {
+		xil_printf("Provided data length is Invalid\n\r");
+		Status = XST_FAILURE;
+		goto END;
+	}
 
 	Config = XCsuDma_LookupConfig(0);
 	if (NULL == Config) {
-		xil_printf("config  failed\n\r");
-		return XST_FAILURE;
+		xil_printf("config failed\n\r");
+		Status = XST_FAILURE;
+		goto END;
 	}
 
 	Status = XCsuDma_CfgInitialize(&CsuDma, Config, Config->BaseAddress);
 	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
+		Status = XST_FAILURE;
+		goto END;
 	}
 
 	/*
@@ -114,17 +141,61 @@ int SecureHelloWorldExample()
 	 */
 	XSecure_Sha3Initialize(&Secure_Sha3, &CsuDma);
 
-	XSecure_Sha3Digest(&Secure_Sha3, HelloWorld, Size, Out);
+	XSecure_Sha3Digest(&Secure_Sha3, (const u8*)Data, Size, Out);
 
-	xil_printf(" Calculated Digest \r\n ");
-	int i= 0;
-	for(i=0; i< (384/8); i++)
-	{
-		xil_printf(" %0x ", Out[i]);
+	xil_printf(" Calculated Hash \r\n ");
+	SecureSha3PrintHash(Out);
+
+	Status = SecureSha3CompareHash(Out, ExpHash);
+END:
+	return Status;
+}
+
+/****************************************************************************/
+/**
+*
+* This function compares the given hash with the expected Hash
+*
+* @return
+*		- XST_SUCCESS - if the expected hash is equal to the
+*                               given hash
+*		- XST_FAILURE - if the comparison fails.
+*
+****************************************************************************/
+u32 SecureSha3CompareHash(u8 *Hash, u8 *ExpectedHash)
+{
+	u32 Index;
+	u32 Status = XST_FAILURE;
+
+	for (Index = 0U; Index < SHA3_HASH_LEN_IN_BYTES; Index++) {
+		if (Hash[Index] != ExpectedHash[Index]) {
+			xil_printf("Expected Hash \r\n");
+			SecureSha3PrintHash(ExpectedHash);
+			xil_printf("SHA Example Failed at Hash Comparison \r\n");
+			break;
+		}
+	}
+	if (Index == SHA3_HASH_LEN_IN_BYTES) {
+		Status = XST_SUCCESS;
+	}
+
+	return Status;
+}
+
+/****************************************************************************/
+/**
+*
+* This function prints the given hash on the console
+*
+****************************************************************************/
+void SecureSha3PrintHash(u8 *Hash)
+{
+	u32 Index;
+
+	for (Index = 0U; Index < SHA3_HASH_LEN_IN_BYTES; Index++) {
+		xil_printf(" %0x ", Hash[Index]);
 	}
 	xil_printf(" \r\n ");
-
-	return XST_SUCCESS;
-}
+ }
 /** //! [SHA3 example] */
 /** @} */
