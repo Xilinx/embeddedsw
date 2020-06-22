a6c814ed4580f9952771c8d71e7631795cda91c4
Rajan Vaja <rajan.vaja@xilinx.com>
2020-06-22 02:50:10 -0700

    xilpm: versal: server: Move Ioctl related APIs to xpm_ioctl.c
    Move IOCTL related APIs to xpm_ioctl.c file and call XPm_Ioctl()
    from xpm_api.c file.
    
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    
    Acked-for-series: Jolly Shah <jolly.shah@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_api.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_api.h
@@ -15,53 +15,10 @@
 extern "C" {
 #endif
 
-#define MAX_BASEADDR_LEN	3
-
-/* Global general storage register base address */
-#define GGS_BASEADDR	(0xF1110030U)
-#define GGS_NUM_REGS	(4U)
-
-#define GGS_4_OFFSET	(0x10U)
-
 /* Persistent global general storage register base address */
 #define PGGS_BASEADDR	(0xF1110050U)
-#define PGGS_NUM_REGS	(4U)
 
-/* Tap delay bypass */
-#define TAPDLY_BYPASS_OFFSET			(0x0000003CU)
-#define XPM_TAP_DELAY_MASK			(0x4U)
-
-/* SD DLL control */
-#define SD0_CTRL_OFFSET				(0x00000404U)
-#define SD1_CTRL_OFFSET				(0x00000484U)
-#define XPM_SD_DLL_RST_MASK			(0x4U)
-
-/* SD ITAPDLY */
-#define ITAPDLY_OFFSET				(0x0000F0F8U)
-#define XPM_SD_ITAPCHGWIN_MASK			(0x200U)
-#define XPM_SD_ITAPDLYENA_MASK			(0x100U)
-#define XPM_SD_ITAPDLYSEL_MASK			(0xFFU)
-
-/* SD OTAPDLY */
-#define OTAPDLY_OFFSET				(0x0000F0FCU)
-#define XPM_SD_OTAPDLYENA_MASK			(0x40U)
-#define XPM_SD_OTAPDLYSEL_MASK			(0x3FU)
-
-/* Probe Counter Register related macros */
-#define PROBE_COUNTER_REQ_TYPE_SHIFT		(16U)
-#define PROBE_COUNTER_REQ_TYPE_MASK		(0xFFU)
-#define PROBE_COUNTER_TYPE_SHIFT		(8U)
-#define PROBE_COUNTER_TYPE_MASK			(0xFFU)
-#define PROBE_COUNTER_IDX_SHIFT			(0U)
-#define PROBE_COUNTER_IDX_MASK			(0xFFU)
-
-#define PROBE_COUNTER_CPU_R5_MAX_IDX		(9U)
-#define PROBE_COUNTER_LPD_MAX_IDX		(5U)
-#define PROBE_COUNTER_FPD_MAX_IDX		(15U)
-
-#define PROBE_COUNTER_CPU_R5_MAX_REQ_TYPE	(3U)
-#define PROBE_COUNTER_LPD_MAX_REQ_TYPE		(7U)
-#define PROBE_COUNTER_FPD_MAX_REQ_TYPE		(3U)
+#define MAX_BASEADDR_LEN	3
 
 /* Extern Variable and Function */
 extern int XLoader_RestartImage(u32 SubsystemId);
