6e659f7f3eb18cfdc717c12f41f4fd4598fc4a83
Tejas Patel <tejas.patel@xilinx.com>
2020-06-25 05:31:02 -0700

    Revert "xilpm: versel: server: Enhance error prints for fpd domain"
    This reverts commit 0491387914355342294c3fef3e32ee87d41ee015.
    
    Revert this commit as it is consuming more memory. Debug prints
    will be added in optimized way in upcoming changes
    
    Signed-off-by: Tejas Patel <tejas.patel@xilinx.com>
    
    Acked-for-series: Jolly Shah <jolly.shah@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_psfpdomain.c
+++ lib/sw_services/xilpm/src/versal/server/xpm_psfpdomain.c
@@ -22,12 +22,10 @@ static XStatus FpdInitStart(u32 *Args, u32 NumOfArgs)
 
 	/* Check vccint_fpd first to make sure power is on */
 	if (XST_SUCCESS != XPmPower_CheckPower(PMC_GLOBAL_PWR_SUPPLY_STATUS_VCCINT_FPD_MASK)) {
-		PmErr("FPD power is OFF\r\n");
 		goto done;
 	}
 
 	if (1U != XPmPsm_FwIsPresent()) {
-		PmErr("FPD is not enabled\r\n");
 		Status = XST_NOT_ENABLED;
 		goto done;
 	}
@@ -37,24 +35,16 @@ static XStatus FpdInitStart(u32 *Args, u32 NumOfArgs)
 
 	Status = XPm_IpiSend(PSM_IPI_INT_MASK, Payload);
 	if (XST_SUCCESS != Status) {
-		PmErr("IpiSend failed\r\n");
 		goto done;
 	}
 
 	Status = XPm_IpiReadStatus(PSM_IPI_INT_MASK);
 	if (XST_SUCCESS != Status) {
-		PmErr("IpiReadStatus failed\r\n");
 		goto done;
 	}
 	/* Release POR for PS-FPD */
 	Status = XPmReset_AssertbyId(PM_RST_FPD_POR, (u32)PM_RESET_ACTION_RELEASE);
-	if (XST_SUCCESS != Status) {
-		PmErr("Failed to reset release of FPD_POR\r\n", Status);
-	}
 done:
-	if (XST_SUCCESS != Status) {
-		PmErr("Return: 0x%x\r\n", Status);
-	}
 	return Status;
 }
 
@@ -80,41 +70,29 @@ static XStatus FpdHcComplete(u32 *Args, u32 NumOfArgs)
 
 	/* Release SRST for PS-FPD - in case Bisr and Mbist are skipped */
 	Status = XPmReset_AssertbyId(PM_RST_FPD, (u32)PM_RESET_ACTION_RELEASE);
-	if (XST_SUCCESS != Status) {
-		PmErr("Failed to reset release of FPD\r\n");
-	}
 
 	Payload[0] = PSM_API_FPD_HOUSECLEAN;
 	Payload[1] = (u32)FUNC_INIT_FINISH;
 
 	Status = XPm_IpiSend(PSM_IPI_INT_MASK, Payload);
 	if (XST_SUCCESS != Status) {
-		PmErr("IpiSend failed for FPD\r\n");
 		goto done;
 	}
 
 	Status = XPm_IpiReadStatus(PSM_IPI_INT_MASK);
 	if (XST_SUCCESS != Status) {
-		PmErr("IpiReadStatus failed for FPD\r\n");
 		goto done;
 	}
 
 	/* Remove FPD SOC domains isolation */
 	Status = XPmDomainIso_Control((u32)XPM_NODEIDX_ISO_FPD_SOC, FALSE_VALUE);
 	if (XST_SUCCESS != Status) {
-		PmErr("Failed to remove FPD SOC domains isolation\r\n");
 		goto done;
 	}
 
 	/* Copy sysmon data */
 	Status = XPmPowerDomain_ApplyAmsTrim(SysmonAddresses[XPM_NODEIDX_MONITOR_SYSMON_PS_FPD], PM_POWER_FPD, 0);
-	if (XST_SUCCESS != Status) {
-		PmErr("Failed to copy sysmon data\r\n\r\n");
-	}
 done:
-	if (XST_SUCCESS != Status) {
-		PmErr("Return: 0x%x\r\n", Status);
-	}
 	return Status;
 }
 
@@ -133,7 +111,6 @@ static XStatus FpdScanClear(u32 *Args, u32 NumOfArgs)
 
 	Psm = (XPm_Psm *)XPmDevice_GetById(PM_DEV_PSM_PROC);;
 	if (NULL == Psm) {
-		PmErr("Invalid PSM_PROC\r\n");
 		Status = XST_FAILURE;
 		goto done;
 	}
@@ -145,14 +122,12 @@ static XStatus FpdScanClear(u32 *Args, u32 NumOfArgs)
         Status = XPm_PollForMask(Psm->PsmGlobalBaseAddr + PSM_GLOBAL_SCAN_CLEAR_FPD_OFFSET,
 				 PSM_GLOBAL_SCAN_CLEAR_DONE_STATUS, 0x10000U);
         if (XST_SUCCESS != Status) {
-		PmErr("Timeout during ScanClear_Done for FPD\r\n");
                 goto done;
         }
 
         Status = XPm_PollForMask(Psm->PsmGlobalBaseAddr + PSM_GLOBAL_SCAN_CLEAR_FPD_OFFSET,
 				 PSM_GLOBAL_SCAN_CLEAR_PASS_STATUS, 0x10000U);
         if (XST_SUCCESS != Status) {
-		PmErr("Timeout during ScanClear_Pass for FPD\r\n");
                 goto done;
         }
 
@@ -161,9 +136,6 @@ static XStatus FpdScanClear(u32 *Args, u32 NumOfArgs)
 		PSM_GLOBAL_SCAN_CLEAR_TRIGGER, 0);
 
 done:
-	if (XST_SUCCESS != Status) {
-		PmErr("Return: 0x%x\r\n", Status);
-	}
         return Status;
 }
 
@@ -184,26 +156,18 @@ static XStatus FpdBisr(u32 *Args, u32 NumOfArgs)
 
 	Status = XPm_IpiSend(PSM_IPI_INT_MASK, Payload);
 	if (XST_SUCCESS != Status) {
-		PmErr("IpiSend failed for FPD\r\n");
 		goto done;
 	}
 
 	Status = XPm_IpiReadStatus(PSM_IPI_INT_MASK);
 	if (XST_SUCCESS != Status) {
-		PmErr("IpiReadStatus failed for FPD\r\n");
 		goto done;
 	}
 
 	/* Trigger Bisr repair */
 	Status = XPmBisr_Repair(FPD_TAG_ID);
-	if (XST_SUCCESS != Status) {
-		PmErr("BisrRep failed for FPD_TAG_ID\r\n");
-	}
 
 done:
-	if (XST_SUCCESS != Status) {
-		PmErr("Return: 0x%x\r\n", Status);
-        }
 	return Status;
 }
 
@@ -218,30 +182,22 @@ static XStatus FpdMbistClear(u32 *Args, u32 NumOfArgs)
 
 	Psm = (XPm_Psm *)XPmDevice_GetById(PM_DEV_PSM_PROC);;
 	if (NULL == Psm) {
-		PmErr("Invalid PSM_PROC\r\n");
 		Status = XST_FAILURE;
 		goto done;
 	}
 
 	/* Release SRST for PS-FPD */
 	Status = XPmReset_AssertbyId(PM_RST_FPD, (u32)PM_RESET_ACTION_RELEASE);
-        if (XST_SUCCESS != Status) {
-		PmErr("Failed to reset release of FPD\r\n");
-        }
 
         Payload[0] = PSM_API_FPD_HOUSECLEAN;
         Payload[1] = (u32)FUNC_MBIST_CLEAR;
 
         Status = XPm_IpiSend(PSM_IPI_INT_MASK, Payload);
         if (XST_SUCCESS != Status) {
-		PmErr("IpiSend faild for FPD\r\n");
                 goto done;
         }
 
         Status = XPm_IpiReadStatus(PSM_IPI_INT_MASK);
-        if (XST_SUCCESS != Status) {
-		PmErr("IpiReadStatus faild for FPD\r\n");
-        }
 
         if (PLATFORM_VERSION_SILICON != XPm_GetPlatform()) {
                 Status = XST_SUCCESS;
@@ -260,14 +216,12 @@ static XStatus FpdMbistClear(u32 *Args, u32 NumOfArgs)
         Status = XPm_PollForMask(Psm->PsmGlobalBaseAddr + PSM_GLOBAL_MBIST_DONE_OFFSET,
 				 PSM_GLOBAL_MBIST_DONE_FPD_MASK, 0x10000U);
         if (XST_SUCCESS != Status) {
-		PmErr("Timeout during Mbist_Done for FPD\r\n");
                 goto done;
         }
 
         if (PSM_GLOBAL_MBIST_GO_FPD_MASK !=
             (XPm_In32(Psm->PsmGlobalBaseAddr + PSM_GLOBAL_MBIST_GO_OFFSET) &
              PSM_GLOBAL_MBIST_GO_FPD_MASK)) {
-		PmErr("MBIST_GO_FPD failed\r\n");
                 Status = XST_FAILURE;
         }
 
@@ -282,7 +236,6 @@ static XStatus FpdMbistClear(u32 *Args, u32 NumOfArgs)
 		PSM_GLOBAL_MBIST_PG_EN_FPD_MASK, 0);
 
 	if (XST_SUCCESS != Status) {
-		PmErr("MBIST_GO_FPD failed\r\n");
 		goto done;
 	}
 
@@ -290,19 +243,14 @@ static XStatus FpdMbistClear(u32 *Args, u32 NumOfArgs)
 	so pulse gic reset as a work around to fix it */
 	Status = XPmReset_AssertbyId(PM_RST_ACPU_GIC, (u32)PM_RESET_ACTION_ASSERT);
 	if (XST_SUCCESS != Status) {
-		PmErr("Failed to reset assert of ACPU_GIC\r\n");
 		goto done;
 	}
 	Status = XPmReset_AssertbyId(PM_RST_ACPU_GIC, (u32)PM_RESET_ACTION_RELEASE);
 	if (XST_SUCCESS != Status) {
-		PmErr("Failed to reset release of ACPU_GIC\r\n");
 		goto done;
 	}
 
 done:
-	if (XST_SUCCESS != Status) {
-		PmErr("Return: 0x%x\r\n", Status);
-	}
         return Status;
 }
 
@@ -315,12 +263,7 @@ done:
  ****************************************************************************/
 static XStatus FpdXmpuCtrl(u32 *Args, u32 NumOfArgs)
 {
-	XStatus Status;
-	Status = XPmProt_CommonXmpuCtrl(Args, NumOfArgs);
-	if (XST_SUCCESS != Status) {
-		PmErr("Return: 0x%x\r\n", Status);
-	}
-        return Status;
+	return XPmProt_CommonXmpuCtrl(Args, NumOfArgs);
 }
 
 static struct XPm_PowerDomainOps FpdOps = {
