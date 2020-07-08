b602079875c8c9ae336788ac95252b5315f53185
Ramakrishna Ganeshu Poolla <ramakrishna.ganeshu.poolla@xilinx.com>
2020-07-08 02:17:06 -0700

    sw_service:xilsecure:Added tamper response process
    APIs added for the following features
     - API to request a tamper response
     - Place holder for secure lock down process
    
    Signed-off-by: Ramakrishna Ganeshu Poolla <ramakrishna.ganeshu.poolla@xilinx.com>
    
    Acked-for-series: Vikram Sreenivasa Batchali <b.vikram@xilinx.com>

---
+++ lib/sw_services/xilsecure/src/versal/xsecure_tamper.h
@@ -0,0 +1,48 @@
+/******************************************************************************
+* Copyright (c) 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+/*****************************************************************************/
+/**
+ *
+ * @file xsecure_tamper.h
+ *
+ * This file contains APIs for tamper processing routines
+ *
+ * <pre>
+ * MODIFICATION HISTORY:
+ *
+ * Ver   Who  Date        Changes
+ * ----- ---- ---------- -------------------------------------------------------
+ * 1.0   rpo  06/25/2020 Initial release
+ * </pre>
+ *
+ * @note
+ *
+ ******************************************************************************/
+#ifndef XSECURE_TAMPER_H
+#define XSECURE_TAMPER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/***************************** Include Files *********************************/
+#include "xil_util.h"
+
+/************************** Constant Definitions *****************************/
+
+/**************************** Type Definitions *******************************/
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+/************************** Function Prototypes ******************************/
+u32 XSecure_ProcessTamperResponse(void);
+void XSecure_SecureLockDown(void);
+void XSecure_EnableTamperInterrupt(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /** XSECURE_TAMPER_H */
