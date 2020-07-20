0d5ed43f45bdfa471372fa9d83ebedb0fdb40501
Simon Leiner <simon@leiner.me>
2020-07-20 12:40:00 +0200

    Cast allocated memory to target type
    Although omitting the cast to the target type is idiomatic C, the
    omission is actually forbidden in ISO C++. This patch enables the header
    to be used in C++ code.
    
    Signed-off-by: Simon Leiner <simon@leiner.me>

--- ThirdParty/sw_services/openamp/src/open-amp/lib/include/openamp/virtqueue.h
+++ ThirdParty/sw_services/openamp/src/open-amp/lib/include/openamp/virtqueue.h
@@ -205,7 +205,7 @@ static inline struct virtqueue *virtqueue_allocate(unsigned int num_desc_extra)
 	uint32_t vq_size = sizeof(struct virtqueue) +
 		 num_desc_extra * sizeof(struct vq_desc_extra);
 
-	vqs = metal_allocate_memory(vq_size);
+	vqs = (struct virtqueue *)metal_allocate_memory(vq_size);
 	if (vqs) {
 		memset(vqs, 0x00, vq_size);
 	}
