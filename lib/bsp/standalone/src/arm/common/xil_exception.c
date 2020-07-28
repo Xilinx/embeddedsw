Xilinx/embeddedsw

commit be3f792d05a9d68ab04e7308e522446fb3a6abd8

BSP:arm: Tweak exception registeration API's
Cortexa72 processor in versal is coupled with GIC-500,
and GIC-500 supports only FIQ at EL3. Hence, tweaking
API's Xil_ExceptionRegisterHandler and
Xil_GetExceptionRegisterHandler to always act on FIQ
for cortexa72 EL3 BSP.

This change is being done to support legacy examples over
cortexa72 at EL3 exception level.

Signed-off-by: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>

Acked-by: Anirudha Sarangi<anirudh@xilinx.com>

lib/bsp/standalone/src/arm/common/xil_exception.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 *
-* Copyright (C) 2015 - 2018 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2015 - 2019 Xilinx, Inc.  All rights reserved.
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
@@ -47,6 +47,9 @@
 * 6.7   mna      26/04/18 Add an API to obtain a corresponding
 *                         Xil_ExceptionHandler entry from XExc_VectorTable.
 * 6.7  asa       18/05/18 Fix bugs in the API Xil_GetExceptionRegisterHandler.
+* 7.0  mus       07/03/19 Tweak Xil_ExceptionRegisterHandler and
+*                         Xil_GetExceptionRegisterHandler to support legacy
+*                         examples for Cortexa72 EL3 exception level.
 *
 * </pre>
 *
@@ -171,6 +174,14 @@ void Xil_ExceptionRegisterHandler(u32 Exception_id,
 				    Xil_ExceptionHandler Handler,
 				    void *Data)
 {
+#if defined (versal) && !defined(ARMR5) && EL3
+/*
+ * Cortexa72 processor in versal is coupled with GIC-500, and GIC-500 supports
+ * only FIQ at EL3. Hence, tweaking this API to always act on FIQ,
+ * ignoring argument passed by user.
+ */
+	Exception_id = XIL_EXCEPTION_ID_FIQ_INT;
+#endif
 	XExc_VectorTable[Exception_id].Handler = Handler;
 	XExc_VectorTable[Exception_id].Data = Data;
 }
@@ -196,6 +207,15 @@ void Xil_GetExceptionRegisterHandler(u32 Exception_id,
 					Xil_ExceptionHandler *Handler,
 					void **Data)
 {
+#if defined (versal) && !defined(ARMR5) && EL3
+/*
+ * Cortexa72 processor in versal is coupled with GIC-500, and GIC-500 supports
+ * only FIQ at EL3. Hence, tweaking this API to always act on FIQ,
+ * ignoring argument passed by user.
+ */
+	Exception_id = XIL_EXCEPTION_ID_FIQ_INT;
+#endif
+
 	*Handler = XExc_VectorTable[Exception_id].Handler;
 	*Data = XExc_VectorTable[Exception_id].Data;
 }

commit 9476d2ebd3fa2bcafab78a75e2ac83b757e416ee

BSP: Fix exception registration related API's
In case of CortexA72 EL3 BSP, existing exception registration
APIs are ignoring exception id passed by user, and always
acting on FIQ. Due to this, applications are not able to
register handler for Synchronous/SError exceptions.

This issue was introduced by
be3f792d05a9d68ab04e7308e522446fb3a6abd8. Fixed it by ignoring
exception id passed by user, only in case if exception id is
pointing to IRQ.

Signed-off-by: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>
Acked-by: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

lib/bsp/standalone/src/arm/common/xil_exception.c
@@ -28,6 +28,10 @@
 * 7.0  mus       07/03/19 Tweak Xil_ExceptionRegisterHandler and
 *                         Xil_GetExceptionRegisterHandler to support legacy
 *                         examples for Cortexa72 EL3 exception level.
+* 7.3  mus       07/27/20 Updated Xil_ExceptionRegisterHandler and
+*                         Xil_GetExceptionRegisterHandler to ignore
+*                         Exception_id, only if its pointing to IRQ.
+*                         It fixes CR#1069524
 *
 * </pre>
 *
@@ -153,12 +157,15 @@ void Xil_ExceptionRegisterHandler(u32 Exception_id,
 				    void *Data)
 {
 #if defined (versal) && !defined(ARMR5) && EL3
-/*
- * Cortexa72 processor in versal is coupled with GIC-500, and GIC-500 supports
- * only FIQ at EL3. Hence, tweaking this API to always act on FIQ,
- * ignoring argument passed by user.
- */
-	Exception_id = XIL_EXCEPTION_ID_FIQ_INT;
+	if ( XIL_EXCEPTION_ID_IRQ_INT == Exception_id )
+	{
+	/*
+	 * Cortexa72 processor in versal is coupled with GIC-500, and
+	 * GIC-500 supports only FIQ at EL3. Hence, tweaking this API
+	 * to act on IRQ, if Exception_id is pointing to IRQ
+	 */
+		Exception_id = XIL_EXCEPTION_ID_FIQ_INT;
+	}
 #endif
 	XExc_VectorTable[Exception_id].Handler = Handler;
 	XExc_VectorTable[Exception_id].Data = Data;
@@ -186,12 +193,16 @@ void Xil_GetExceptionRegisterHandler(u32 Exception_id,
 					void **Data)
 {
 #if defined (versal) && !defined(ARMR5) && EL3
-/*
- * Cortexa72 processor in versal is coupled with GIC-500, and GIC-500 supports
- * only FIQ at EL3. Hence, tweaking this API to always act on FIQ,
- * ignoring argument passed by user.
- */
-	Exception_id = XIL_EXCEPTION_ID_FIQ_INT;
+	if ( XIL_EXCEPTION_ID_IRQ_INT == Exception_id )
+	{
+	/*
+	 * Cortexa72 processor in versal is coupled with GIC-500, and
+	 * GIC-500 supports only FIQ at EL3. Hence, tweaking this API
+	 * to act on IRQ, if Exception_id is pointing to IRQ
+	 */
+
+		Exception_id = XIL_EXCEPTION_ID_FIQ_INT;
+	}
 #endif
 
 	*Handler = XExc_VectorTable[Exception_id].Handler;