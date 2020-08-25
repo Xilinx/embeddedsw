f8d9e67008c4074f7f71f2a58d841695a1a29c8f
Raviteja Narayanam <raviteja.narayanam@xilinx.com>
2020-08-25 19:21:41 +0530

    drivers: uartpsv: Fix MISRA-C_2012 8.5 rule violation
    Function is declared more than once.
    
    Fixed this violation by creating a header file to keep the prototypes
    of internal functions. Removed the prototypes at multiple places.
    
    Signed-off-by: Raviteja Narayanam <raviteja.narayanam@xilinx.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/uartpsv/src/xuartpsv_xfer.h
@@ -0,0 +1,52 @@
+/******************************************************************************
+* Copyright (C) 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+/*****************************************************************************/
+/**
+*
+* @file xuartpsv_xfer.h
+* @addtogroup uartpsv_v1_3
+* @{
+*
+* This header file contains the prototypes of objects used internally.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver  Who  Date      Changes
+* ---  ---  --------- -----------------------------------------------
+* 1.3  rna  05/18/20  First release
+* </pre>
+*
+******************************************************************************/
+#ifndef XUARTPSV_XFER_H		/* prevent circular inclusions */
+#define XUARTPSV_XFER_H		/* by using protection macros */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/***************************** Include Files *********************************/
+
+#include "xil_types.h"
+#include "xil_assert.h"
+#include "xil_io.h"
+#include "xuartpsv.h"
+
+/************************** Function Prototypes ******************************/
+
+
+u32  XUartPsv_SendBuffer(XUartPsv *InstancePtr);
+
+u32  XUartPsv_ReceiveBuffer(XUartPsv *InstancePtr);
+
+/************************** Variable Definitions *****************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* end of protection macro */
+/** @} */
