7231b5abe94d4795bb861a22e8039a7e56363863
Vikram Sreenivasa Batchali <b.vikram@xilinx.com>
2020-08-27 12:15:40 +0530

    sw_apps :img_rcvry: Update web_pages folder
    The patch checks in web.img file along with the folder structure
    of web_pages folder in the desired format.
    
    Signed-off-by: Vikram Sreenivasa Batchali <b.vikram@xilinx.com>
    
    Acked-by: Mohan Marutirao Dhanawade <mohan.dhanawade@xilinx.com>

--- lib/sw_apps/img_rcvry/misc/cfi.sh
+++ lib/sw_apps/img_rcvry/misc/cfi.sh
@@ -1,4 +1,4 @@
-rm -f web.img
+rm web.img
 
 # Create image file of 64K
 dd if=/dev/zero of=web.img bs=512 count=128
@@ -7,12 +7,12 @@ dd if=/dev/zero of=web.img bs=512 count=128
 /sbin/mkfs.vfat -F 12 web.img
 
 # Mount it
-mkdir /tmp/fs
+sudo mkdir /tmp/fs
 chmod 666 web.img
 sudo mount -t vfat -o loop,rw web.img /tmp/fs/
 
 # Copy webpages
-cp -r  * /tmp/fs/
+sudo cp -r  ./web_pages/* /tmp/fs/
 sudo umount /tmp/fs
-rm -R /tmp/fs
+sudo rm -R /tmp/fs
 chmod 444 web.img
