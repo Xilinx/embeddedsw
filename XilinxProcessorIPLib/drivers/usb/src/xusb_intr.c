6fe607c9636b94235b47cce7a73267fc46253a2f
Piyush Mehta <piyush.mehta@xilinx.com>
2020-08-30 13:26:02 +0530

    usb: usb: update addtogroup version
    Updated addtogroup version for usb driver.
    
    Signed-off-by: Piyush Mehta <piyush.mehta@xilinx.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@xilinx.com>

--- XilinxProcessorIPLib/drivers/usb/src/xusb_intr.c
+++ XilinxProcessorIPLib/drivers/usb/src/xusb_intr.c
@@ -7,7 +7,7 @@
 /******************************************************************************/
 /**
  * @file xusb_intr.c
-* @addtogroup usb_v5_4
+* @addtogroup usb_v5_5
 * @{
  *
  * This file contains the functions that are related to interrupt processing
