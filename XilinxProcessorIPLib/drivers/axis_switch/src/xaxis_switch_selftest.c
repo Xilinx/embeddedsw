6e51bee11889532ad4c5025f1ed1d1be4b1bcd44
Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
2020-09-08 15:29:35 +0530

    axis_switch: Update the driver version
    Update the driver version
    
    Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@xilinx.com>

--- XilinxProcessorIPLib/drivers/axis_switch/src/xaxis_switch_selftest.c
+++ XilinxProcessorIPLib/drivers/axis_switch/src/xaxis_switch_selftest.c
@@ -7,7 +7,7 @@
 /**
 *
 * @file xaxis_switch_selftest.c
-* @addtogroup axis_switch_v1_3
+* @addtogroup axis_switch_v1_4
 * @{
 *
 * This file contains self test function for the AXI4-Stream Source Control
