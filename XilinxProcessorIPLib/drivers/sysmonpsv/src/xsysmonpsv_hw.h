b66e3c97d82b87623193ba5681cce5c056190b75
Anand Ashok Dumbre <anand.ashok.dumbre@xilinx.com>
2020-09-11 09:41:51 +0100

    sysmonpsv: Update driver version from v1_3 to v1_4
    Update driver version and add to group from v1_3 to v1_4
    
    Signed-off-by: Anand Ashok Dumbre <anand.ashok.dumbre@xilinx.com>
    
    Acked-for-series: Dragan Cvetic <dragan.cvetic@xilinx.com>

--- XilinxProcessorIPLib/drivers/sysmonpsv/src/xsysmonpsv_hw.h
+++ XilinxProcessorIPLib/drivers/sysmonpsv/src/xsysmonpsv_hw.h
@@ -7,7 +7,7 @@
 /**
 *
 * @file xsysmonpsv_hw.h
-* @addtogroup sysmonpsv_v1_3
+* @addtogroup sysmonpsv_v1_4
 *
 * This header file contains the identifiers and basic driver functions (or
 * macros) that can be used to access the device. Other driver functions
