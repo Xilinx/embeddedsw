404f8e6cffb4b040b3627b9f61c305e8345beded
Tarun DTS <tarun.dts@xilinx.com>
2020-09-15 19:56:56 +0530

    isw_services: xilloader: Fixes for MISRA C Rule 8.4 in xilloader
    MISRA C Rule 8.4 states: A compatible declaration shall be visible when an object or function with external linkage is defined.
    
    This patch fixes MISRA C Rule 8.4 by using static if no extern is required & moving extern variables to respective header files if required.
    
    Signed-off-by: Tarun DTS <tarun.dts@xilinx.com>
    
    Acked-for-series: Vikram Sreenivasa Batchali <b.vikram@xilinx.com>

--- lib/sw_services/xilloader/src/xloader.h
+++ lib/sw_services/xilloader/src/xloader.h
@@ -349,8 +349,6 @@ static inline u8 XLoader_IsEncEnabled(XilPdi* PdiPtr)
 }
 
 /************************** Function Prototypes ******************************/
-extern XilPdi SubsystemPdiIns;
-
 int XLoader_Init(void);
 int XLoader_LoadPdi(XilPdi* PdiPtr, PdiSrc_t PdiSrc, u64 PdiAddr);
 int XLoader_LoadImage(XilPdi *PdiPtr);
