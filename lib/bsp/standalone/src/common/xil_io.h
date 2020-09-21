c40fc8b01140f138e0a963256937e6963c316d21
praveen teja kundanala <praveen.teja.kundanala@xilinx.com>
2020-09-21 05:39:01 -0700

    BSP:common: Make Xil_EndianSwap16 and Xil_EndianSwap32 as inline
    In Securtiy critical functions, Xil_Htonl(Xil_EndianSwap32/Xil_EndianSwap16) is
    used to reverse the endianess of security critical data i.e Key.So,to avoid
    copying of arguments on to stack these functions are made inline
    
    Signed-off-by: Praveen Teja Kundanala <praveen.teja.kundanala@xilinx.com>
    Acked-by: Mohan Marutirao Dhanawade <mohan.dhanawade@xilinx.com>

--- lib/bsp/standalone/src/common/xil_io.h
+++ lib/bsp/standalone/src/common/xil_io.h
@@ -25,6 +25,9 @@
 *                         ARM processors
 * 7.20  har      01/03/20 Added Xil_SecureOut32 for avoiding blindwrite for
 *                         CR-1049218
+* 7.30  kpt      09/21/20 Moved Xil_EndianSwap16 and Xil_EndianSwap32 to
+*                         xil_io.h and made them as static inline
+*
 * </pre>
 ******************************************************************************/
 
@@ -48,8 +51,6 @@ extern "C" {
 #endif
 
 /************************** Function Prototypes ******************************/
-u16 Xil_EndianSwap16(u16 Data);
-u32 Xil_EndianSwap32(u32 Data);
 #ifdef ENABLE_SAFETY
 extern u32 XStl_RegUpdate(u32 RegAddr, u32 RegVal);
 #endif
@@ -256,6 +257,51 @@ static INLINE u32 Xil_SecureOut32(UINTPTR Addr, u32 Value)
 	}
 
 	return Status;
+}
+
+/*****************************************************************************/
+/**
+*
+* @brief    Perform a 16-bit endian conversion.
+*
+* @param	Data: 16 bit value to be converted
+*
+* @return	16 bit Data with converted endianness
+*
+******************************************************************************/
+static INLINE __attribute__((always_inline)) u16 Xil_EndianSwap16(u16 Data)
+{
+	return (u16) (((Data & 0xFF00U) >> 8U) | ((Data & 0x00FFU) << 8U));
+}
+
+/*****************************************************************************/
+/**
+*
+* @brief    Perform a 32-bit endian conversion.
+*
+* @param	Data: 32 bit value to be converted
+*
+* @return	32 bit data with converted endianness
+*
+******************************************************************************/
+static INLINE __attribute__((always_inline)) u32 Xil_EndianSwap32(u32 Data)
+{
+	u16 LoWord;
+	u16 HiWord;
+
+	/* get each of the half words from the 32 bit word */
+
+	LoWord = (u16) (Data & 0x0000FFFFU);
+	HiWord = (u16) ((Data & 0xFFFF0000U) >> 16U);
+
+	/* byte swap each of the 16 bit half words */
+
+	LoWord = (((LoWord & 0xFF00U) >> 8U) | ((LoWord & 0x00FFU) << 8U));
+	HiWord = (((HiWord & 0xFF00U) >> 8U) | ((HiWord & 0x00FFU) << 8U));
+
+	/* swap the half words before returning the value */
+
+	return ((((u32)LoWord) << (u32)16U) | (u32)HiWord);
 }
 
 #if defined (__MICROBLAZE__)
