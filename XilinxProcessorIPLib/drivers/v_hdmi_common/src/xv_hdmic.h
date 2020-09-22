49ee02f000e1cb7b73cafbec6a97ef8319f22cf2
Subhransu S. Prusty <subhransu.sekhar.prusty@xilinx.com>
2020-09-22 12:17:44 +0530

    v_hdmi_common: add logic to get fs based on measured CTS
    TMDS rate can be different for deep color modes and the N/CTS
    table may not give the correct fs. So add equation to derive
    fs if matching N and TMDS rates are found from the table.
    
    Signed-off-by: Subhransu S. Prusty <subhransu.sekhar.prusty@xilinx.com>
    
    Acked-for-series: Vishal Sagar <vishal.sagar@xilinx.com>

--- XilinxProcessorIPLib/drivers/v_hdmi_common/src/xv_hdmic.h
+++ XilinxProcessorIPLib/drivers/v_hdmi_common/src/xv_hdmic.h
@@ -523,7 +523,7 @@ XHdmiC_SamplingFrequencyVal
 	XHdmiC_FRL_GetAudSampFreq(XHdmiC_FRLCharRate FRLCharRate,
 		u32 CTS, u32 N);
 XHdmiC_SamplingFrequency XHdmiC_TMDS_GetAudSampFreq(u32 TMDSCharRate,
-		u32 N);
+		u32 N, u32 CTSVal);
 XHdmiC_SamplingFrequency
 	XHdmiC_GetAudIFSampFreq (XHdmiC_SamplingFrequencyVal AudSampFreqVal);
 XHdmiC_SamplingFrequencyVal
