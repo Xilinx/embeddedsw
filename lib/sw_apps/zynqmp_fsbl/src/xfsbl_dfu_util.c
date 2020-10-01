a005db846dba2f096541f4d53433e8f8eabf216d
b.vikram@xilinx.com <b.vikram@xilinx.com>
2020-10-01 19:03:15 +0530

    sw_apps :zynqmp_fsbl: Reduce data section usage in USB related code
    The patch reduces data section usage by removing unused descriptors.
    
    Signed-off-by: Vikram Sreenivasa Batchali <b.vikram@xilinx.com>
    
    Acked-for-series: Manish Narani <manish.narani@xilinx.com>

--- lib/sw_apps/zynqmp_fsbl/src/xfsbl_dfu_util.c
+++ lib/sw_apps/zynqmp_fsbl/src/xfsbl_dfu_util.c
@@ -61,8 +61,8 @@ XFsbl_UsbCh9_Data Dfu_data = {
 };
 
 /* Device Descriptors */
-static XFsblPs_UsbStdDevDesc __attribute__ ((aligned(16))) DDesc[] = {
-	{/* USB 2.0 */
+static XFsblPs_UsbStdDevDesc __attribute__ ((aligned(16))) DDesc = {
+	/* USB 2.0 */
 		(u8)sizeof(XFsblPs_UsbStdDevDesc), /* bLength */
 		USB_DEVICE_DESC, /* bDescriptorType */
 		(0x0200U), /* bcdUSB 2.0 */
@@ -77,118 +77,17 @@ static XFsblPs_UsbStdDevDesc __attribute__ ((aligned(16))) DDesc[] = {
 		0x02U, /* iProduct */
 		0x03U, /* iSerialNumber */
 		0x01U /* bNumConfigurations */
-	},
-	{/* USB 3.0 */
-		(u8)sizeof(XFsblPs_UsbStdDevDesc), /* bLength */
-		USB_DEVICE_DESC, /* bDescriptorType */
-		(0x0300U), /* bcdUSB 3.0 */
-		0x00U, /* bDeviceClass */
-		0x00U, /* bDeviceSubClass */
-		0x00U, /* bDeviceProtocol */
-		0x09U, /* bMaxPackedSize0 */
-		(0x03FDU), /* idVendor */
-		(0x0050U), /* idProduct */
-		(0x0404U), /* bcdDevice */
-		0x01U, /* iManufacturer */
-		0x02U, /* iProduct */
-		0x03U, /* iSerialNumber */
-		0x01U /* bNumConfigurations */
-	}};
+	};
 
 /* String Descriptors */
-static char* StringList[USB_MODES_NUM][STRING_DESCRIPTORS_NUM] = {
-	{
+static char* StringList[STRING_DESCRIPTORS_NUM] = {
 		"UNUSED",
 		"XILINX INC",
 		"DFU 2.0 emulation v 1.1",
 		"2A49876D9CC1AA4",
 		"Xilinx DFU Downloader",
 		"7ABC7ABC7ABC7ABC7ABC7ABC"
-	},
-	{
-		"UNUSED",
-		"XILINX INC",
-		"DFU 3.0 emulation v 1.1",
-		"2A49876D9CC1AA4",
-		"Xilinx DFU Downloader",
-		"7ABC7ABC7ABC7ABC7ABC7ABC"
-	},
-};
-
-static XFsblPs_Usb30Config __attribute__ ((aligned(16))) Config3 = {
-	/* Std Config */
-	{	(u8)sizeof(XFsblPs_UsbStdCfgDesc), /* bLength */
-		USB_CONFIG_DESC, /* bDescriptorType */
-		(u16) sizeof(XFsblPs_Usb30Config), /* wTotalLength */
-		0x01U, /* bNumInterfaces */
-		0x01U, /* bConfigurationValue */
-		0x00U, /* iConfiguration */
-		0xC0U, /* bmAttribute */
-		0x00U}, /* bMaxPower  */
-
-	/* Interface Config */
-	{	(u8)sizeof(XFsblPs_UsbStdIfDesc), /* bLength */
-		USB_INTERFACE_CFG_DESC, /* bDescriptorType */
-		0x00U, /* bInterfaceNumber */
-		0x00U, /* bAlternateSetting */
-		0x02U, /* bNumEndPoints */
-		0xFFU, /* bInterfaceClass */
-		0xFFU, /* bInterfaceSubClass */
-		0xFFU, /* bInterfaceProtocol */
-		0x04U}, /* iInterface */
-
-	/* Bulk In Endpoint Config */
-	{	(u8)sizeof(XFsblPs_UsbStdEpDesc), /* bLength */
-		USB_ENDPOINT_CFG_DESC, /* bDescriptorType */
-		0x81U, /* bEndpointAddress */
-		0x02U, /* bmAttribute  */
-		0x00U, /* wMaxPacketSize - LSB */
-		0x04U, /* wMaxPacketSize - MSB */
-		0x00U}, /* bInterval */
-
-	/* SS Endpoint companion  */
-	{	(u8)sizeof(XFsblPs_UsbStdEpSsCompDesc), /* bLength */
-		0x30U, /* bDescriptorType */
-		0x0FU, /* bMaxBurst */
-		0x00U, /* bmAttributes */
-		0x00U}, /* wBytesPerInterval */
-
-	/* Bulk Out Endpoint Config */
-	{	(u8)sizeof(XFsblPs_UsbStdEpDesc), /* bLength */
-		USB_ENDPOINT_CFG_DESC, /* bDescriptorType */
-		0x01U, /* bEndpointAddress */
-		0x02U, /* bmAttribute  */
-		0x00U, /* wMaxPacketSize - LSB */
-		0x04U, /* wMaxPacketSize - MSB */
-		0x00U}, /* bInterval */
-
-	/* SS Endpoint companion  */
-	{	(u8)sizeof(XFsblPs_UsbStdEpSsCompDesc), /* bLength */
-		0x30U, /* bDescriptorType */
-		0x0FU, /* bMaxBurst */
-		0x00U, /* bmAttributes */
-		0x00U}, /* wBytesPerInterval */
-
-	/*** DFU Interface descriptor ***/
-	{	(u8)sizeof(XFsblPs_UsbStdIfDesc), /* bLength */
-		USB_INTERFACE_CFG_DESC, /* bDescriptorType */
-		0x00U, /* bInterfaceNumber */
-		0x01U, /* bAlternateSetting */
-		0x00U, /* bNumEndPoints */
-		0xFEU, /* bInterfaceClass DFU application specific class code */
-		0x01U, /* bInterfaceSubClass DFU device firmware upgrade code*/
-		0x02U, /* bInterfaceProtocol DFU mode protocol*/
-		0x04U}, /* iInterface DFU string descriptor*/
-
-	/**** DFU functional descriptor ****/
-	{	(u8)sizeof(XFsblPs_UsbDfuFuncDesc), /* bLength*/
-		DFUFUNC_DESCR, /* bDescriptorType DFU functional descriptor type */
-		0x3U, /* bmAttributes Device is only download/upload capable */
-		8192U, /* wDetatchTimeOut 8192 ms */
-		DFU_MAX_TRANSFER, /*wTransferSize DFU block size 1024*/
-		0x0110U /*bcdDfuVersion 1.1 */
-	}
-};
+	};
 
 static XFsblPs_UsbConfig __attribute__ ((aligned(16))) Config2 = {
 	/* Std Config */
@@ -307,22 +206,12 @@ u32 XFsbl_Ch9SetupStrDescReply(u8 *BufPtr, u32 BufferLen, u8 Index)
 		goto END;
 	}
 
-	SStatus = XUsbPsu_IsSuperSpeed(&UsbInstance);
-	if(SStatus != XST_SUCCESS) {
-		/* USB 2.0 */
-		String = StringList[0][Index];
-
-	} else {
-		/* USB 3.0 */
-		String = StringList[1][Index];
-	}
-
+	String = StringList[Index];
 	StringLen = strlen(String);
 
 	/* Index 0 is LangId which is special as we can not represent
 	 * the string required in the table above.Therefore we handle
-	 * index 0 as a special case.*/
-
+	 * index 0 as a special case. */
 	if (0U == Index) {
 		StringDesc.Length = 4U;
 		StringDesc.DescriptorType = 0x03U;
@@ -388,15 +277,7 @@ u32 XFsbl_Ch9SetupDevDescReply(u8 *BufPtr, u32 BufferLen)
 		goto END;
 	}
 
-	SStatus = XUsbPsu_IsSuperSpeed(&UsbInstance);
-	if(SStatus != XST_SUCCESS) {
-		/* USB 2.0 */
-		(void)memcpy(BufPtr, &DDesc[0], DevDescLength);
-
-	} else {
-		/* USB 3.0 */
-		(void)memcpy(BufPtr, &DDesc[1], DevDescLength);
-	}
+	(void)memcpy(BufPtr, &DDesc, DevDescLength);
 
 END:
 	return DevDescLength;
@@ -427,17 +308,8 @@ u32 XFsbl_Ch9SetupCfgDescReply(u8 *BufPtr, u32 BufferLen)
 		goto END;
 	}
 
-	SStatus = XUsbPsu_IsSuperSpeed(&UsbInstance);
-	if(SStatus != XST_SUCCESS) {
-		/* USB 2.0 */
-		Config = (u8 *)&Config2;
-		CfgDescLen = sizeof(XFsblPs_UsbConfig);
-	} else {
-		/* USB 3.0 */
-		Config = (u8 *)&Config3;
-		CfgDescLen = sizeof(XFsblPs_Usb30Config);
-	}
-
+	Config = (u8 *)&Config2;
+	CfgDescLen = sizeof(XFsblPs_UsbConfig);
 	if (BufferLen < CfgDescLen) {
 		CfgDescLen = 0U;
 		goto END;
