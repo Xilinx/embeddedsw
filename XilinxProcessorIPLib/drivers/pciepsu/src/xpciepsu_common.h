bea1c80ebb9b3d902ee3c499a764fc76178b32c1
Arjun V <arjun.v@xilinx.com>
2020-10-06 09:58:42 +0530

    pciepsu: Fix MISRA 10.1 violations
    MISRA 10.1 rule - Operands shall not be of an inappropriate essential type
    Fixing MISRA 10.1 violations with the following changes:
     - Prefix U after constants to make it unsigned type
     - Fix conditional check to return a boolean type
    
    Signed-off-by: Arjun V <arjun.v@xilinx.com>
    
    Acked-for-series: Bharat Kumar Gogada <bharat.kumar.gogada@xilinx.com>

--- XilinxProcessorIPLib/drivers/pciepsu/src/xpciepsu_common.h
+++ XilinxProcessorIPLib/drivers/pciepsu/src/xpciepsu_common.h
@@ -33,110 +33,111 @@ extern "C" {
 /* Command register offsets */
 
 /* Memory access enable */
-#define XPCIEPSU_CFG_CMD_MEM_EN 		0x00000002
+#define XPCIEPSU_CFG_CMD_MEM_EN		0x00000002U
 
 /* Bus master enable */
-#define XPCIEPSU_CFG_CMD_BUSM_EN 		0x00000004
+#define XPCIEPSU_CFG_CMD_BUSM_EN	0x00000004U
 
 /* PCIe Configuration registers offsets */
 
 /* Vendor ID/Device ID offset */
-#define XPCIEPSU_CFG_ID_REG 	0x0000
+#define XPCIEPSU_CFG_ID_REG		0x0000U
 
 /* Command/Status Register Offset */
-#define XPCIEPSU_CFG_CMD_STATUS_REG 	0x0001
+#define XPCIEPSU_CFG_CMD_STATUS_REG	0x0001U
 
 /* Primary/Sec.Bus Register Offset */
-#define XPCIEPSU_CFG_PRI_SEC_BUS_REG 	0x0006
+#define XPCIEPSU_CFG_PRI_SEC_BUS_REG 	0x0006U
 
 /* Cache Line/Latency Timer / Header Type / BIST Register Offset */
-#define XPCIEPSU_CFG_CAH_LAT_HD_REG 	0x0003
+#define XPCIEPSU_CFG_CAH_LAT_HD_REG	0x0003U
 
-#define XPCIEPSU_CFG_BAR_MEM_TYPE_MASK 	0x1	/* Memory or IO request */
+#define XPCIEPSU_CFG_BAR_MEM_TYPE_MASK	0x1U	/* Memory or IO request */
 
-#define XPCIEPSU_CFG_BAR_MEM_AS_MASK	0x6	/* 32b or 64b address space */
+#define XPCIEPSU_CFG_BAR_MEM_AS_MASK	0x6U	/* 32b or 64b address space */
 
 /* PCIe Base Addr */
-#define XPCIEPSU_CFG_BAR_BASE_OFFSET 	0x0004
+#define XPCIEPSU_CFG_BAR_BASE_OFFSET	0x0004U
 
 /* PCIe Base Addr 0 */
-#define XPCIEPSU_CFG_BAR_0_REG 			0x0004
+#define XPCIEPSU_CFG_BAR_0_REG	      0x0004U
 
 /* PCIe Base Addr 1 */
-#define XPCIEPSU_CFG_BAR_1_REG 			0x0005
+#define XPCIEPSU_CFG_BAR_1_REG	      0x0005U
 
 /* PCIe Base Addr 2 */
-#define XPCIEPSU_CFG_BAR_2_REG 			0x0006
+#define XPCIEPSU_CFG_BAR_2_REG	      0x0006U
 
 /* PCIe Base Addr 3 */
-#define XPCIEPSU_CFG_BAR_3_REG 			0x0007
+#define XPCIEPSU_CFG_BAR_3_REG	      0x0007U
 
 /* PCIe Base Addr 4 */
-#define XPCIEPSU_CFG_BAR_4_REG 			0x0008
+#define XPCIEPSU_CFG_BAR_4_REG	      0x0008U
 
-#define XPCIEPSU_CFG_BUS_NUMS_T1_REG 	0X0006
-#define XPCIEPSU_CFG_NP_MEM_T1_REG 		0X0008
-#define XPCIEPSU_CFG_P_MEM_T1_REG 		0X0009
-#define XPCIEPSU_CFG_P_UPPER_MEM_T1_REG 0X000A
-#define XPCIEPSU_CFG_P_LIMIT_MEM_T1_REG 0X000B
+#define XPCIEPSU_CFG_BUS_NUMS_T1_REG	0x0006U
+#define XPCIEPSU_CFG_NP_MEM_T1_REG 	0x0008U
+#define XPCIEPSU_CFG_P_MEM_T1_REG 	0x0009U
+#define XPCIEPSU_CFG_P_UPPER_MEM_T1_REG 0x000AU
+#define XPCIEPSU_CFG_P_LIMIT_MEM_T1_REG 0x000BU
 
-#define XPCIEPSU_CFG_FUN_NOT_IMP_MASK 	0xFFFF
-#define XPCIEPSU_CFG_HEADER_TYPE_MASK 	0x00EF0000
-#define XPCIEPSU_CFG_MUL_FUN_DEV_MASK 	0x00800000
+#define XPCIEPSU_CFG_FUN_NOT_IMP_MASK 	0xFFFFU
+#define XPCIEPSU_CFG_HEADER_TYPE_MASK 	0x00EF0000U
+#define XPCIEPSU_CFG_MUL_FUN_DEV_MASK 	0x00800000U
 
-#define XPCIEPSU_CFG_MAX_NUM_OF_BUS 	256
-#define XPCIEPSU_CFG_MAX_NUM_OF_DEV 	32
-#define XPCIEPSU_CFG_MAX_NUM_OF_FUN 	8
+#define XPCIEPSU_CFG_MAX_NUM_OF_BUS 	256U
+#define XPCIEPSU_CFG_MAX_NUM_OF_DEV 	32U
+#define XPCIEPSU_CFG_MAX_NUM_OF_FUN 	8U
 
-#define XPCIEPSU_CFG_HEADER_O_TYPE 		0x0000
+#define XPCIEPSU_CFG_HEADER_O_TYPE	0x0000U
 
-#define XPCIEPSU_BAR_IO_MEM 				1
-#define XPCIEPSU_BAR_ADDR_MEM 			0
+#define XPCIEPSU_BAR_IO_MEM 		1U
+#define XPCIEPSU_BAR_ADDR_MEM 		0U
 
-#define XPCIEPSU_BAR_MEM_TYPE_64 		0x4
-#define XPCIEPSU_BAR_MEM_TYPE_32 		0x0
+#define XPCIEPSU_BAR_MEM_TYPE_IO	0x1U
+#define XPCIEPSU_BAR_MEM_TYPE_64 	0x4U
+#define XPCIEPSU_BAR_MEM_TYPE_32 	0x0U
 
-#define XPCIEPSU_PRIMARY_BUS   			0x18
+#define XPCIEPSU_PRIMARY_BUS   		0x18U
 
-#define XPCIEPSU_ECAM_MEMSIZE			16*1024*1024
+#define XPCIEPSU_ECAM_MEMSIZE		16U * 1024U * 1024U
 
-#define MB_SHIFT 					20
-#define HEX_NIBBLE 					4
-#define TWO_HEX_NIBBLES 			8
-#define FOUR_HEX_NIBBLES 			16
-#define EIGHT_HEX_NIBBLES 			32
+#define MB_SHIFT 			20U
+#define HEX_NIBBLE 			4U
+#define TWO_HEX_NIBBLES 		8U
+#define FOUR_HEX_NIBBLES		16U
+#define EIGHT_HEX_NIBBLES 		32U
 
-#define XPCIEPSU_LINKUP_SUCCESS				1
-#define XPCIEPSU_LINKUP_FAIL				0
+#define XPCIEPSU_LINKUP_SUCCESS		1U
+#define XPCIEPSU_LINKUP_FAIL		0U
 
-#define DATA_MASK_32				(0xFFFFFFFF)
+#define DATA_MASK_32			(0xFFFFFFFFU)
 
 /* Capability pointer Doubleword and masks */
-#define XPCIEPSU_CFG_P_CAP_PTR_T1_REG 	0X000D
-#define XPCIEPSU_CFG_CAP_ID_LOC		 	GENMASK(7, 0)
-#define XPCIEPSU_CAP_PTR_LOC			GENMASK(7, 0)
-#define XPCIEPSU_CAP_SHIFT				8
-#define XPCIEPSU_DOUBLEWORD(x)			(x / 4)
-#define CAP_PRESENT						(1)
-#define CAP_NOT_PRESENT					(0)
+#define XPCIEPSU_CFG_P_CAP_PTR_T1_REG 	0X000DU
+#define XPCIEPSU_CFG_CAP_ID_LOC		GENMASK(7U, 0U)
+#define XPCIEPSU_CAP_PTR_LOC		GENMASK(7U, 0U)
+#define XPCIEPSU_CAP_SHIFT		8U
+#define XPCIEPSU_DOUBLEWORD(x)		(x / 4U)
+#define CAP_PRESENT			(1U)
+#define CAP_NOT_PRESENT			(0U)
 
 /* PCIe mode */
-#define XPCIEPSU_MODE_ENDPOINT		0X0
-#define XPCIEPSU_MODE_ROOTCOMPLEX	0X1
+#define XPCIEPSU_MODE_ENDPOINT		0x0U
+#define XPCIEPSU_MODE_ROOTCOMPLEX	0x1U
 
 /* Conditional debugging prints */
 #define XPciePsu_Err(...) \
 		do { \
 			xil_printf("pcie_psu: "); \
 			xil_printf(__VA_ARGS__);  \
-		} while (0)
+		} while (0U)
 
 #ifdef DEBUG_MODE
 #define XPciePsu_Dbg(...) \
 		do { \
 			xil_printf("pcie_psu: "); \
 			xil_printf(__VA_ARGS__);  \
-		} while (0)
+		} while (0U)
 #else
 	#define XPciePsu_Dbg(...)	{}
 #endif
--- XilinxProcessorIPLib/drivers/pciepsu/src/xpciepsu_hw.h
+++ XilinxProcessorIPLib/drivers/pciepsu/src/xpciepsu_hw.h
@@ -42,60 +42,61 @@ extern "C" {
 */
 
 /* helper mecros */
-#define BIT(x) 								(1 << (x))
+#define BIT(x) 					(((u32)1U) << (x))
 
 /* each bus required 1 MB ecam space */
-#define GET_MAX_BUS_NO(ecam_sz) 			(((ecam_sz) / (1024 * 1024)) - 1)
+#define GET_MAX_BUS_NO(ecam_sz) 		(((ecam_sz) / (1024U * 1024U)) - 1U)
 
-#define BITSPERLONG 						64
-#define GENMASK(h, l) (((~0ULL) << (l)) & (~0ULL) >> (BITSPERLONG - 1 - (h)))
+#define BITSPERLONGLONG				64U
+
+#define GENMASK(h, l) \
+	(((~0ULL) << (l)) & (~0ULL >> (BITSPERLONGLONG - 1U - (h))))
 
 /* Bridge core config registers */
-#define XPCIEPSU_BRCFG_RX0 							0x00000000
-#define XPCIEPSU_BRCFG_INTERRUPT 					0x00000010
-#define XPCIEPSU_BRCFG_RX_MSG_FILTER 				0x00000020
+#define XPCIEPSU_BRCFG_RX0			0x00000000U
+#define XPCIEPSU_BRCFG_RX_MSG_FILTER 		0x00000020U
 
 /* Egress - Bridge translation registers */
-#define XPCIEPSU_E_BREG_CAPABILITIES 					0x00000200
-#define XPCIEPSU_E_BREG_CONTROL 						0x00000208
-#define XPCIEPSU_E_BREG_BASE_LO 						0x00000210
-#define XPCIEPSU_E_BREG_BASE_HI 						0x00000214
-#define XPCIEPSU_E_ECAM_CAPABILITIES 					0x00000220
-#define XPCIEPSU_E_ECAM_CONTROL 						0x00000228
-#define XPCIEPSU_E_ECAM_BASE_LO 						0x00000230
-#define XPCIEPSU_E_ECAM_BASE_HI 						0x00000234
+#define XPCIEPSU_E_BREG_CAPABILITIES 		0x00000200U
+#define XPCIEPSU_E_BREG_CONTROL 		0x00000208U
+#define XPCIEPSU_E_BREG_BASE_LO 		0x00000210U
+#define XPCIEPSU_E_BREG_BASE_HI 		0x00000214U
+#define XPCIEPSU_E_ECAM_CAPABILITIES 		0x00000220U
+#define XPCIEPSU_E_ECAM_CONTROL 		0x00000228U
+#define XPCIEPSU_E_ECAM_BASE_LO 		0x00000230U
+#define XPCIEPSU_E_ECAM_BASE_HI 		0x00000234U
 
-#define XPCIEPSU_I_ISUB_CONTROL 						0x000003E8
-#define SET_ISUB_CONTROL 								BIT(0)
+#define XPCIEPSU_I_ISUB_CONTROL 		0x000003E8U
+#define SET_ISUB_CONTROL 			BIT(0U)
 /* Rxed msg fifo  - Interrupt status registers */
-#define XPCIEPSU_MSGF_MISC_MASK 						0x00000404
-#define XPCIEPSU_MSGF_LEG_MASK 							0x00000424
+#define XPCIEPSU_MSGF_MISC_MASK 		0x00000404U
+#define XPCIEPSU_MSGF_LEG_MASK 			0x00000424U
 
 
 /* Msg filter mask bits */
-#define CFG_ENABLE_PM_MSG_FWD 			BIT(1)
-#define CFG_ENABLE_INT_MSG_FWD 			BIT(2)
-#define CFG_ENABLE_ERR_MSG_FWD 			BIT(3)
+#define CFG_ENABLE_PM_MSG_FWD 			BIT(1U)
+#define CFG_ENABLE_INT_MSG_FWD 			BIT(2U)
+#define CFG_ENABLE_ERR_MSG_FWD 			BIT(3U)
 #define CFG_ENABLE_MSG_FILTER_MASK                                             \
 	(CFG_ENABLE_PM_MSG_FWD | CFG_ENABLE_INT_MSG_FWD                        \
 	 | CFG_ENABLE_ERR_MSG_FWD)
 
 /* Misc interrupt status mask bits */
-#define MSGF_MISC_SR_RXMSG_AVAIL 		BIT(0)
-#define MSGF_MISC_SR_RXMSG_OVER 		BIT(1)
-#define MSGF_MISC_SR_SLAVE_ERR 			BIT(4)
-#define MSGF_MISC_SR_MASTER_ERR 		BIT(5)
-#define MSGF_MISC_SR_I_ADDR_ERR 		BIT(6)
-#define MSGF_MISC_SR_E_ADDR_ERR 		BIT(7)
-#define MSGF_MISC_SR_FATAL_AER 			BIT(16)
-#define MSGF_MISC_SR_NON_FATAL_AER 		BIT(17)
-#define MSGF_MISC_SR_CORR_AER 			BIT(18)
-#define MSGF_MISC_SR_UR_DETECT 			BIT(20)
-#define MSGF_MISC_SR_NON_FATAL_DEV 		BIT(22)
-#define MSGF_MISC_SR_FATAL_DEV 			BIT(23)
-#define MSGF_MISC_SR_LINK_DOWN 			BIT(24)
-#define MSGF_MSIC_SR_LINK_AUTO_BWIDTH 	BIT(25)
-#define MSGF_MSIC_SR_LINK_BWIDTH 		BIT(26)
+#define MSGF_MISC_SR_RXMSG_AVAIL 		BIT(0U)
+#define MSGF_MISC_SR_RXMSG_OVER 		BIT(1U)
+#define MSGF_MISC_SR_SLAVE_ERR 			BIT(4U)
+#define MSGF_MISC_SR_MASTER_ERR 		BIT(5U)
+#define MSGF_MISC_SR_I_ADDR_ERR 		BIT(6U)
+#define MSGF_MISC_SR_E_ADDR_ERR 		BIT(7U)
+#define MSGF_MISC_SR_FATAL_AER 			BIT(16U)
+#define MSGF_MISC_SR_NON_FATAL_AER 		BIT(17U)
+#define MSGF_MISC_SR_CORR_AER 			BIT(18U)
+#define MSGF_MISC_SR_UR_DETECT 			BIT(20U)
+#define MSGF_MISC_SR_NON_FATAL_DEV 		BIT(22U)
+#define MSGF_MISC_SR_FATAL_DEV 			BIT(23U)
+#define MSGF_MISC_SR_LINK_DOWN 			BIT(24U)
+#define MSGF_MSIC_SR_LINK_AUTO_BWIDTH		BIT(25U)
+#define MSGF_MSIC_SR_LINK_BWIDTH 		BIT(26U)
 
 #define MSGF_MISC_SR_MASKALL                                                   \
 	(MSGF_MISC_SR_RXMSG_AVAIL | MSGF_MISC_SR_RXMSG_OVER                    \
@@ -109,59 +110,58 @@ extern "C" {
 
 
 /* Legacy interrupt status mask bits */
-#define MSGF_LEG_SR_INTA 			BIT(0)
-#define MSGF_LEG_SR_INTB 			BIT(1)
-#define MSGF_LEG_SR_INTC 			BIT(2)
-#define MSGF_LEG_SR_INTD 			BIT(3)
+#define MSGF_LEG_SR_INTA 			BIT(0U)
+#define MSGF_LEG_SR_INTB 			BIT(1U)
+#define MSGF_LEG_SR_INTC 			BIT(2U)
+#define MSGF_LEG_SR_INTD 			BIT(3U)
 #define MSGF_LEG_SR_MASKALL                                                    \
 	(MSGF_LEG_SR_INTA | MSGF_LEG_SR_INTB | MSGF_LEG_SR_INTC                \
 	 | MSGF_LEG_SR_INTD)
 
 /* Bridge config interrupt mask */
-#define BREG_PRESENT 				BIT(0)
-#define BREG_ENABLE 				BIT(0)
-#define BREG_ENABLE_FORCE 			BIT(1)
+#define BREG_PRESENT 				BIT(0U)
+#define BREG_ENABLE 				BIT(0U)
+#define BREG_ENABLE_FORCE 			BIT(1U)
 
 /* E_ECAM status mask bits */
-#define E_ECAM_PRESENT 				BIT(0)
-#define E_ECAM_CR_ENABLE 			BIT(0)
-#define E_ECAM_SIZE_LOC 			GENMASK(20, 16)
-#define E_ECAM_SIZE_MIN 			GENMASK(23, 16)
-#define E_ECAM_SIZE_SHIFT 			16
-#define PSU_ECAM_VALUE_DEFAULT 		12
+#define E_ECAM_PRESENT 				BIT(0U)
+#define E_ECAM_CR_ENABLE 			BIT(0U)
+#define E_ECAM_SIZE_LOC 			GENMASK(20U, 16U)
+#define E_ECAM_SIZE_MIN 			GENMASK(23U, 16U)
+#define E_ECAM_SIZE_SHIFT 			16U
+#define PSU_ECAM_VALUE_DEFAULT			12U
 
-#define CFG_DMA_REG_BAR 			GENMASK(2, 0)
+#define CFG_DMA_REG_BAR 			GENMASK(2U, 0U)
 
 /* Reading the PS_LINKUP */
-#define XPCIEPSU_PS_LINKUP_OFFSET 			0x00000238
-#define XPCIEPSU_PHY_LINKUP_BIT 		BIT(0)
-#define XPCIEPSU_XPHY_RDY_LINKUP_BIT 			BIT(1)
-
-/* Parameters for the waiting for link up routine */
-#define XPCIEPSU_LINK_WAIT_MAX_RETRIES 		10
-#define XPCIEPSU_LINK_WAIT_USLEEP_MIN 		90000
+#define XPCIEPSU_PS_LINKUP_OFFSET 		0x00000238U
+#define XPCIEPSU_PHY_LINKUP_BIT 		BIT(0U)
+#define XPCIEPSU_XPHY_RDY_LINKUP_BIT 		BIT(1U)
 
 #define XPciePsu_IsEcamBusy(InstancePtr)                                       \
 	((XPciePsu_ReadReg((InstancePtr)->Config.Ecam, PCIEPSU_BSC_OFFSET)     \
-	  & PCIEPSU_BSC_ECAM_BUSY_MASK)                                        \
-		 ? TRUE                                                        \
-		 : FALSE)
+	& PCIEPSU_BSC_ECAM_BUSY_MASK)                                        \
+		? TRUE                                                        \
+		: FALSE)
 
+/* Parameters for the waiting for link up routine */
+#define XPCIEPSU_LINK_WAIT_MAX_RETRIES 		10U
+#define XPCIEPSU_LINK_WAIT_USLEEP_MIN 		90000U
 
 /** @name ECAM Address Register bitmaps and masks
 *
 * @{
 */
-#define XPCIEPSU_ECAM_MASK 0x0FFFFFFF     /**< Mask of all valid bits */
-#define XPCIEPSU_ECAM_BUS_MASK 0x0FF00000 /**< Bus Number Mask */
-#define XPCIEPSU_ECAM_DEV_MASK 0x000F8000 /**< Device Number Mask */
-#define XPCIEPSU_ECAM_FUN_MASK 0x00007000 /**< Function Number Mask */
-#define XPCIEPSU_ECAM_REG_MASK 0x00000FFC /**< Register Number Mask */
+#define XPCIEPSU_ECAM_MASK	0x0FFFFFFFU     /**< Mask of all valid bits */
+#define XPCIEPSU_ECAM_BUS_MASK	0x0FF00000U /**< Bus Number Mask */
+#define XPCIEPSU_ECAM_DEV_MASK	0x000F8000U /**< Device Number Mask */
+#define XPCIEPSU_ECAM_FUN_MASK	0x00007000U /**< Function Number Mask */
+#define XPCIEPSU_ECAM_REG_MASK	0x00000FFCU /**< Register Number Mask */
 
-#define XPCIEPSU_ECAM_BUS_SHIFT 20 /**< Bus Number Shift Value */
-#define XPCIEPSU_ECAM_DEV_SHIFT 15 /**< Device Number Shift Value */
-#define XPCIEPSU_ECAM_FUN_SHIFT 12 /**< Function Number Shift Value */
-#define XPCIEPSU_ECAM_REG_SHIFT 2  /**< Register Number Shift Value */
+#define XPCIEPSU_ECAM_BUS_SHIFT	20U /**< Bus Number Shift Value */
+#define XPCIEPSU_ECAM_DEV_SHIFT	15U /**< Device Number Shift Value */
+#define XPCIEPSU_ECAM_FUN_SHIFT	12U /**< Function Number Shift Value */
+#define XPCIEPSU_ECAM_REG_SHIFT	2U  /**< Register Number Shift Value */
 /*@}*/
 /******************** Macros (Inline Functions) Definitions *******************/
 /**************************** Variable Definitions ****************************/
