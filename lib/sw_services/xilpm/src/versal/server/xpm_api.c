813b5187f426eb393d4610d898bc37ce042fd2fc
Tarun DTS <tarun.dts@xilinx.com>
2020-10-07 00:48:32 +0530

    sw_services: xilpm: Updating the Status return variable in XPm_ProcessCmd
    This patch updates the Status variable with CmdHandler Status in XPm_ProcessCmd
    
    Signed-off-by: Tarun DTS <tarun.dts@xilinx.com>
    Acked-by: Izhar Ameer Shaikh <izhar.ameer.shaikh@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_api.c
+++ lib/sw_services/xilpm/src/versal/server/xpm_api.c
@@ -355,7 +355,7 @@ static int XPm_ProcessCmd(XPlmi_Cmd * Cmd)
 					      Pload[3], Cmd->IpiMask);
 		break;
 	default:
-		PmErr("CMD: INVALID PARAM\n\r");
+		PmErr("CMD: INVALID PARAM\r\n");
 		Status = XST_INVALID_PARAM;
 		break;
 	}
@@ -376,9 +376,12 @@ static int XPm_ProcessCmd(XPlmi_Cmd * Cmd)
 		goto done;
 	}
 
+	/* Restore the CmdHandler status */
+	Status = (int)Cmd->Response[0];
+
 done:
-	if ((XST_SUCCESS != Status) || (XST_SUCCESS != Cmd->Response[0])) {
-		PmErr("Cmd status: 0x%x, Response copy status: 0x%x\n\r", Cmd->Response[0], Status);
+	if (XST_SUCCESS != Status) {
+		PmErr("Err Code: 0x%x\r\n", Status);
 	}
 	return Status;
 }
