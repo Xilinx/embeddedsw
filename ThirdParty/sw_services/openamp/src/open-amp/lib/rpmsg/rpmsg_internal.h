6143fc21940a75f2210ab3f8a3598944d24445cd
Arnaud Pouliquen <arnaud.pouliquen@st.com>
2020-10-06 10:32:53 +0200

    rpmsg: set rpmsg_init_ept API as deprecated
    The rpmsg_init_ept does not seem to be usable by application
    but is used internally. This patch mark this API deprecated
    and in at least two full release, to remove this API as the
    standard API to use is rpmsg_create_ept.
    In addition, move the rpmsg_init_ept to rpmsg_internal and rename it.
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@st.com>

--- ThirdParty/sw_services/openamp/src/open-amp/lib/rpmsg/rpmsg_internal.h
+++ ThirdParty/sw_services/openamp/src/open-amp/lib/rpmsg/rpmsg_internal.h
@@ -87,6 +87,33 @@ struct rpmsg_ns_msg {
 	uint32_t flags;
 } METAL_PACKED_END;
 
+/**
+ * rpmsg_initialize_ept - initialize rpmsg endpoint
+ *
+ * Initialize an RPMsg endpoint with a name, source address,
+ * remoteproc address, endpoint callback, and destroy endpoint callback.
+ *
+ * @ept: pointer to rpmsg endpoint
+ * @name: service name associated to the endpoint
+ * @src: local address of the endpoint
+ * @dest: target address of the endpoint
+ * @cb: endpoint callback
+ * @ns_unbind_cb: end point service unbind callback, called when remote ept is
+ *                destroyed.
+ */
+static inline void rpmsg_initialize_ept(struct rpmsg_endpoint *ept,
+					const char *name,
+					uint32_t src, uint32_t dest,
+					rpmsg_ept_cb cb,
+					rpmsg_ns_unbind_cb ns_unbind_cb)
+{
+	strncpy(ept->name, name ? name : "", sizeof(ept->name));
+	ept->addr = src;
+	ept->dest_addr = dest;
+	ept->cb = cb;
+	ept->ns_unbind_cb = ns_unbind_cb;
+}
+
 int rpmsg_send_ns_message(struct rpmsg_endpoint *ept, unsigned long flags);
 
 struct rpmsg_endpoint *rpmsg_get_endpoint(struct rpmsg_device *rvdev,
