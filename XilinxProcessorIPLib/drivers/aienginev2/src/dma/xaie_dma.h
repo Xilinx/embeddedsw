d26b60733de280597d2a6e1facf13bce6a95fee4
Dishita Vashi <dishita.vashi@xilinx.com>
2020-10-08 17:55:57 -0700

    aienginev2: src: dma: Add api configure repeat BD ID, repeat count and token status
    Add api to configure BD Id, repeat count - the number of times a task
    should be repeated and token issue status - whether or not issue a token
    at the end of a task.
    
    Signed-off-by: Dishita Vashi <dishitav@xilinx.com>
    
    Acked-for-series: Wendy Liang <wendy.liang@xilinx.com>

--- XilinxProcessorIPLib/drivers/aienginev2/src/dma/xaie_dma.h
+++ XilinxProcessorIPLib/drivers/aienginev2/src/dma/xaie_dma.h
@@ -89,6 +89,9 @@ AieRC XAie_DmaGetPendingBdCount(XAie_DevInst *DevInst, XAie_LocType Loc,
 		u8 ChNum, XAie_DmaDirection Dir, u8 *PendingBd);
 AieRC XAie_DmaGetMaxQueueSize(XAie_DevInst *DevInst, XAie_LocType Loc,
 		u8 *QueueSize);
+AieRC XAie_DmaChannelSetStartQueue(XAie_DevInst *DevInst, XAie_LocType Loc,
+		u8 ChNum, XAie_DmaDirection Dir, u8 BdNum, u32 RepeatCount,
+		u8 EnTokenIssue);
 AieRC XAie_DmaUpdateBdLen(XAie_DevInst *DevInst, XAie_LocType Loc, u32 Len,
 		u8 BdNum);
 AieRC XAie_DmaUpdateBdAddr(XAie_DevInst *DevInst, XAie_LocType Loc, u64 Addr,
