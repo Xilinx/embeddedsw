297280e9b46f57f646a627880957b13c7676153d
Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
2020-10-15 13:00:19 -0600

    lib: sw_apps: srec_spi_bootloader: Add srec spi bootloader source code to embeddedsw repo
    Add srec spi bootloader source code to embeddedsw repo and
    changed the license to SPDX.
    
    Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@xilinx.com>

---
+++ lib/sw_apps/srec_spi_bootloader/src/blconfig.h
@@ -0,0 +1,7 @@
+/******************************************************************************
+* Copyright (C) 2004 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+#warning "Please provide the correct address value for the definition FLASH_IMAGE_BASEADDR. Please give the flash offset @ which SREC application is programmed"
+
+#define FLASH_IMAGE_BASEADDR  0xF8000000
