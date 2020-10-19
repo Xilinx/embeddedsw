9f8716d672a942a5dfb8408a9694c0da8915bbb1
Shravya <shravya.kumbham@xilinx.com>
2020-10-19 02:31:29 -0600

    BSP: Consolidate and add the drivers xdebug.h data to common xdebug.h
    While building any driver which have the xdebug.h in the src path
    we are seeing the following failure with xdebug.h:
    
    cp:cannot create regular file ../../../include/xdebug.h :File exists
    make[2]: [Makefile:35: ../../../include/xdebug.h] Error 1
    
    This issue is because of using the same file name in standalone
    BSP and a few drivers(axidma, axicdma, axivdma, axiethernet and
    llfifo). There are intermittent errors from makefile because of
    the same file being overwritten. Fix this as there should not be
    multiple instances of the same file. So,consolidate all the macros
    from driver xdebug.h files and add them to the common xdebug.h file
    and delete the xdebug.h files from drivers and deprecated VxWorks
    defines are removed but the common xdebug.h now contains a common
    superset of definitions from all other files.
    
    Signed-off-by: Shravya Kumbham <shravya.kumbham@xilinx.com>
    
    Acked-for-series: Harini Katakam <harini.katakam@xilinx.com>

--- lib/bsp/standalone/src/common/xdebug.h
+++ lib/bsp/standalone/src/common/xdebug.h
@@ -18,7 +18,24 @@ int printf(const char *format, ...);
 #define XDBG_DEBUG_GENERAL           0x00000002U    /* general debug  messages */
 #define XDBG_DEBUG_ALL               0xFFFFFFFFU    /* all debugging data */
 
-#define xdbg_current_types (XDBG_DEBUG_GENERAL)
+#define XDBG_DEBUG_FIFO_REG          0x00000100U    /* display register reads/writes */
+#define XDBG_DEBUG_FIFO_RX           0x00000101U    /* receive debug messages */
+#define XDBG_DEBUG_FIFO_TX           0x00000102U    /* transmit debug messages */
+#define XDBG_DEBUG_FIFO_ALL          0x0000010FU    /* all fifo debug messages */
+
+#define XDBG_DEBUG_TEMAC_REG         0x00000400U    /* display register reads/writes */
+#define XDBG_DEBUG_TEMAC_RX          0x00000401U    /* receive debug messages */
+#define XDBG_DEBUG_TEMAC_TX          0x00000402U    /* transmit debug messages */
+#define XDBG_DEBUG_TEMAC_ALL         0x0000040FU    /* all temac  debug messages */
+
+#define XDBG_DEBUG_TEMAC_ADPT_RX     0x00000800U    /* receive debug messages */
+#define XDBG_DEBUG_TEMAC_ADPT_TX     0x00000801U    /* transmit debug messages */
+#define XDBG_DEBUG_TEMAC_ADPT_IOCTL  0x00000802U    /* ioctl debug messages */
+#define XDBG_DEBUG_TEMAC_ADPT_MISC   0x00000803U    /* debug msg for other routines */
+#define XDBG_DEBUG_TEMAC_ADPT_ALL    0x0000080FU    /* all temac adapter debug messages */
+
+
+#define xdbg_current_types (XDBG_DEBUG_GENERAL | XDBG_DEBUG_ERROR | XDBG_DEBUG_TEMAC_REG | XDBG_DEBUG_FIFO_RX | XDBG_DEBUG_FIFO_TX | XDBG_DEBUG_FIFO_REG)
 
 #define xdbg_stmnt(x)  x
 
