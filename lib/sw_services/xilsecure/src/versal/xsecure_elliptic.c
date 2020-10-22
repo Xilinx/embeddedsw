7b9df45b39a4269bc3adc59bac081a01d40a31e7
Harsha <harsha.harsha@xilinx.com>
2020-10-22 23:57:23 +0530

    sw_services:xilsecure:Replaced ecdsa with elliptic in names of files, functions and macros
    The ECC engine is capable of digital signature as well as encryption/decryption.
    ECDSA which is a digital signature scheme is just a subset of the usecases of ECC.
    Therefore it will be more appropriate to rename the files as xsecure_elliptic*.
    
    This patch replaces ecdsa with elliptic in the names of the files, functions and macros.
    It also adds temporal redundancy checks in XSecure_EllipticValidateKey
    and XSecure_EllipticGenerateSign.
    
    Signed-off-by: Harsha <harsha.harsha@xilinx.com>
    
    Acked-for-Series: Sarat Chand Savitala <sarat.chand.savitala@xilinx.com>

--- lib/sw_services/xilsecure/src/versal/xsecure_elliptic.c
+++ lib/sw_services/xilsecure/src/versal/xsecure_elliptic.c
@@ -6,10 +6,10 @@
 /*****************************************************************************/
 /**
 *
-* @file xsecure_ecdsa.c
+* @file xsecure_elliptic.c
 *
-* This file contains the implementation of the interface functions for ECDSA
-* driver.
+* This file contains the implementation of the interface functions for ECC
+* engine.
 *
 * <pre>
 * MODIFICATION HISTORY:
@@ -21,8 +21,10 @@
 * 4.3   har 08/24/20  Added APIs to generate and verify ECDSA public key and
 *                     signature
 *                     Added support for ECDSA P521 curve
-*	am  09/25/20  Resolved MISRA C violations
+*       am  09/25/20  Resolved MISRA C violations
 *       har 10/12/20  Addressed security review comments
+*       har 10/14/20  Replaced ecdsa with elliptic in names of function and
+*                     macros
 *
 * </pre>
 *
@@ -32,7 +34,7 @@
 
 /***************************** Include Files *********************************/
 #include "xsecure_error.h"
-#include "xsecure_ecdsa.h"
+#include "xsecure_elliptic.h"
 #include "xsecure_ecdsa_rsa_hw.h"
 #include "xsecure_utils.h"
 #include "xil_util.h"
@@ -73,7 +75,7 @@ static const u32 HashVal[XSECURE_ECC_P384_DATA_SIZE_WORDS] = {
 /***************** Macros (Inline Functions) Definitions *********************/
 
 /************************** Function Prototypes ******************************/
-static EcdsaCrvInfo* XSecure_EcdsaGetCrvData(XSecure_EcdsaCrvTyp CrvTyp);
+static EcdsaCrvInfo* XSecure_EllipticGetCrvData(XSecure_EllipticCrvTyp CrvTyp);
 
 /************************** Variable Definitions *****************************/
 
@@ -82,42 +84,42 @@ static EcdsaCrvInfo* XSecure_EcdsaGetCrvData(XSecure_EcdsaCrvTyp CrvTyp);
 /**
  * @brief	This function generates Public Key for a given curve type
  *
- * @param	CrvType - Is a type of ECDSA curve
- * @param	D 	   - Pointer to static private key
- * @param	Key    - Pointer to public key
+ * @param	CrvType - Is a type of elliptic curve
+ * @param	D       - Pointer to static private key
+ * @param	Key     - Pointer to public key
  *
  * @return
  *		- XST_SUCCESS - On success
- * 		- XSECURE_ECDSA_NON_SUPPORTED_CRV - When ECDSA Curve is not supported
- * 		- XSECURE_ECDSA_INVALID_PARAM 	  - On invalid argument
- * 		- XSECURE_ECDSA_GEN_KEY_ERR 	  - Error in generating Public key
+ * 		- XSECURE_ELLIPTIC_NON_SUPPORTED_CRV - When elliptic Curve is not supported
+ * 		- XSECURE_ELLIPTIC_INVALID_PARAM - On invalid argument
+ * 		- XSECURE_ELLIPTIC_GEN_KEY_ERR 	 Error in generating Public key
  *
  *****************************************************************************/
-int XSecure_EcdsaGenerateKey(XSecure_EcdsaCrvTyp CrvType, const u8* D,
-	XSecure_EcdsaKey *Key)
+int XSecure_EllipticGenerateKey(XSecure_EllipticCrvTyp CrvType, const u8* D,
+	XSecure_EllipticKey *Key)
 {
-	int Status = (int)XSECURE_ECDSA_NON_SUPPORTED_CRV;
+	int Status = (int)XSECURE_ELLIPTIC_NON_SUPPORTED_CRV;
 	EcdsaCrvInfo *Crv = NULL;
 
-	if ((CrvType != XSECURE_ECDSA_NIST_P384) &&
-			(CrvType != XSECURE_ECDSA_NIST_P521)) {
-		Status = (int)XSECURE_ECDSA_INVALID_PARAM;
+	if ((CrvType != XSECURE_ECC_NIST_P384) &&
+			(CrvType != XSECURE_ECC_NIST_P521)) {
+		Status = (int)XSECURE_ELLIPTIC_INVALID_PARAM;
 		goto END;
 	}
 
 	if ((D == NULL) || (Key == NULL)) {
-		Status = (int)XSECURE_ECDSA_INVALID_PARAM;
+		Status = (int)XSECURE_ELLIPTIC_INVALID_PARAM;
 		goto END;
 	}
 
 	XSecure_ReleaseReset(XSECURE_ECDSA_RSA_BASEADDR,
 		XSECURE_ECDSA_RSA_RESET_OFFSET);
 
-	Crv = XSecure_EcdsaGetCrvData(CrvType);
+	Crv = XSecure_EllipticGetCrvData(CrvType);
 	if(Crv != NULL) {
 		Status = Ecdsa_GeneratePublicKey(Crv, D, (EcdsaKey *)Key);
 		if (Status != XST_SUCCESS) {
-			Status = (int)XSECURE_ECDSA_GEN_KEY_ERR;
+			Status = (int)XSECURE_ELLIPTIC_GEN_KEY_ERR;
 		}
 	}
 
@@ -131,20 +133,20 @@ END:
 /**
  * @brief	This function generates signature for a given hash and curve type
  *
- * @param	CrvType  - Is a type of ECDSA curve
+ * @param	CrvType  -Type of elliptic curve
  * @param	Hash    - Pointer to the hash for which sign has to be generated
- * @param	HashLen - Is a length of the hash in bytes
+ * @param	HashLen - Length of the hash in bytes
  * @param	D       - Pointer to the static private key
- * @param	K       - Is an ephemeral private key
+ * @param	K       - Ephemeral private key
  * @param	Sign    - Pointer to the signature
  *
  * @return
  *	- XST_SUCCESS - On success
- * 	- XSECURE_ECDSA_INVALID_PARAM - On invalid argument
- * 	- XSECURE_ECDSA_GEN_SIGN_BAD_RAND_NUM - When Bad random number used
+ * 	- XSECURE_ELLIPTIC_INVALID_PARAM - On invalid argument
+ * 	- XSECURE_ELLIPTIC_GEN_SIGN_BAD_RAND_NUM - When Bad random number used
  *						for sign generation
- * 	- XSECURE_ECDSA_GEN_SIGN_INCORRECT_HASH_LEN - Incorrect hash length for sign
- *						      generation
+ * 	- XSECURE_ELLIPTIC_GEN_SIGN_INCORRECT_HASH_LEN - Incorrect hash length for sign
+ *							generation
  *	- XST_FAILURE - On failure
  *
  * @note
@@ -154,46 +156,48 @@ END:
  * the security of ECDSA
  *
  *****************************************************************************/
-int XSecure_EcdsaGenerateSign(XSecure_EcdsaCrvTyp CrvType, const u8* Hash,
-	const u32 HashLen, const u8* D, const u8* K, XSecure_EcdsaSign *Sign)
+int XSecure_EllipticGenerateSignature(XSecure_EllipticCrvTyp CrvType, const u8* Hash,
+	const u32 HashLen, const u8* D, const u8* K, XSecure_EllipticSign *Sign)
 {
-	int Status = (int)XSECURE_ECDSA_NON_SUPPORTED_CRV;
+	int Status = (int)XSECURE_ELLIPTIC_NON_SUPPORTED_CRV;
 	int GenStatus = XST_FAILURE;
 	EcdsaCrvInfo *Crv = NULL;
-	u8 PaddedHash[XSECURE_ECDSA_P521_SIZE_IN_BYTES] = {0U};
+	u8 PaddedHash[XSECURE_ECC_P521_SIZE_IN_BYTES] = {0U};
 
-	if ((CrvType != XSECURE_ECDSA_NIST_P384) &&
-			(CrvType != XSECURE_ECDSA_NIST_P521)) {
-		Status = (int)XSECURE_ECDSA_INVALID_PARAM;
+	if ((CrvType != XSECURE_ECC_NIST_P384) &&
+			(CrvType != XSECURE_ECC_NIST_P521)) {
+		Status = (int)XSECURE_ELLIPTIC_INVALID_PARAM;
 		goto END;
 	}
 
 	if ((D == NULL) || (K == NULL) || (Hash == NULL) || (Sign == NULL)) {
-		Status = (int)XSECURE_ECDSA_INVALID_PARAM;
+		Status = (int)XSECURE_ELLIPTIC_INVALID_PARAM;
 		goto END;
 	}
 
-	if (HashLen > XSECURE_ECDSA_P521_SIZE_IN_BYTES) {
-		Status = (int)XSECURE_ECDSA_INVALID_PARAM;
+	if (HashLen > XSECURE_ECC_P521_SIZE_IN_BYTES) {
+		Status = (int)XSECURE_ELLIPTIC_INVALID_PARAM;
 		goto END;
 	}
 
 	XSecure_ReleaseReset(XSECURE_ECDSA_RSA_BASEADDR,
 		XSECURE_ECDSA_RSA_RESET_OFFSET);
 
-	Crv = XSecure_EcdsaGetCrvData(CrvType);
+	Crv = XSecure_EllipticGetCrvData(CrvType);
 	if(Crv != NULL) {
 		Xil_MemCpy(PaddedHash, Hash, HashLen);
-		GenStatus = Ecdsa_GenerateSign(Crv, PaddedHash, Crv->Bits, D,
-			K, (EcdsaSign *)Sign);
-		if ((GenStatus == ECDSA_GEN_SIGN_BAD_R) ||
-				(GenStatus == ECDSA_GEN_SIGN_BAD_S)) {
-			Status = (int)XSECURE_ECDSA_GEN_SIGN_BAD_RAND_NUM;
+		XSECURE_TEMPORAL_CHECK(SIG_ERR, GenStatus, Ecdsa_GenerateSign,
+			Crv, PaddedHash, Crv->Bits, D, K, (EcdsaSign *)Sign);
+
+SIG_ERR:
+		if ((GenStatus == ELLIPTIC_GEN_SIGN_BAD_R) ||
+				(GenStatus == ELLIPTIC_GEN_SIGN_BAD_S)) {
+			Status = (int)XSECURE_ELLIPTIC_GEN_SIGN_BAD_RAND_NUM;
 		}
-		else if (GenStatus == ECDSA_GEN_SIGN_INCORRECT_HASH_LEN) {
-			Status = (int)XSECURE_ECDSA_GEN_SIGN_INCORRECT_HASH_LEN;
+		else if (GenStatus == ELLIPTIC_GEN_SIGN_INCORRECT_HASH_LEN) {
+			Status = (int)XSECURE_ELLIPTIC_GEN_SIGN_INCORRECT_HASH_LEN;
 		}
-		else if (GenStatus != ECDSA_SUCCESS) {
+		else if (GenStatus != ELLIPTIC_SUCCESS) {
 			Status = XST_FAILURE;
 		}
 		else {
@@ -211,50 +215,55 @@ END:
 /**
  * @brief	This function validates the public key for a given curve type
  *
- * @param	CrvType - Is a type of ECDSA curve
- * @param	Key    - Pointer to the public key
+ * @param	CrvType - Type of elliptic curve
+ * @param	Key     - Pointer to the public key
  *
  * @return
  *		- XST_SUCCESS - On success
- * 		- XSECURE_ECDSA_INVALID_PARAM   - On invalid argument
- * 		- XSECURE_ECDSA_KEY_ZERO        - When Public key is zero
- *		- XSECURE_ECDSA_KEY_WRONG_ORDER - Wrong order of Public key
- * 		- XSECURE_ECDSA_KEY_NOT_ON_CRV  - When Key is not found on the curve
+ * 		- XSECURE_ELLIPTIC_INVALID_PARAM   - On invalid argument
+ * 		- XSECURE_ELLIPTIC_KEY_ZERO        - When Public key is zero
+ *		- XSECURE_ELLIPTIC_KEY_WRONG_ORDER - Wrong order of Public key
+ * 		- XSECURE_ELLIPTIC_KEY_NOT_ON_CRV  - When Key is not found on the curve
  *		- XST_FAILURE                   - On failure
  *
  *****************************************************************************/
-int XSecure_EcdsaValidateKey(XSecure_EcdsaCrvTyp CrvType, XSecure_EcdsaKey *Key)
+int XSecure_EllipticValidateKey(XSecure_EllipticCrvTyp CrvType,
+	XSecure_EllipticKey *Key)
 {
-	int Status = (int)XSECURE_ECDSA_NON_SUPPORTED_CRV;
+	int Status = (int)XSECURE_ELLIPTIC_NON_SUPPORTED_CRV;
 	int ValidateStatus = XST_FAILURE;
 	EcdsaCrvInfo *Crv = NULL;
 
-	if ((CrvType != XSECURE_ECDSA_NIST_P384) && (CrvType != XSECURE_ECDSA_NIST_P521)) {
-		Status = (int)XSECURE_ECDSA_INVALID_PARAM;
+	if ((CrvType != XSECURE_ECC_NIST_P384) &&
+		(CrvType != XSECURE_ECC_NIST_P521)) {
+		Status = (int)XSECURE_ELLIPTIC_INVALID_PARAM;
 		goto END;
 	}
 
 	if (Key == NULL) {
-		Status = (int)XSECURE_ECDSA_INVALID_PARAM;
+		Status = (int)XSECURE_ELLIPTIC_INVALID_PARAM;
 		goto END;
 	}
 
 	XSecure_ReleaseReset(XSECURE_ECDSA_RSA_BASEADDR,
 		XSECURE_ECDSA_RSA_RESET_OFFSET);
 
-	Crv = XSecure_EcdsaGetCrvData(CrvType);
+	Crv = XSecure_EllipticGetCrvData(CrvType);
 	if(Crv != NULL) {
-		ValidateStatus = Ecdsa_ValidateKey(Crv, (EcdsaKey *)Key);
-		if (ValidateStatus == ECDSA_KEY_ZERO) {
-			Status = (int)XSECURE_ECDSA_KEY_ZERO;
+		XSECURE_TEMPORAL_CHECK(KEY_ERR, ValidateStatus, Ecdsa_ValidateKey,
+			Crv, (EcdsaKey *)Key);
+
+KEY_ERR:
+		if (ValidateStatus == ELLIPTIC_KEY_ZERO) {
+			Status = (int)XSECURE_ELLIPTIC_KEY_ZERO;
 		}
-		else if (ValidateStatus == ECDSA_KEY_WRONG_ORDER) {
-			Status = (int)XSECURE_ECDSA_KEY_WRONG_ORDER;
+		else if (ValidateStatus == ELLIPTIC_KEY_WRONG_ORDER) {
+			Status = (int)XSECURE_ELLIPTIC_KEY_WRONG_ORDER;
 		}
-		else if (ValidateStatus == ECDSA_KEY_NOT_ON_CRV) {
-			Status = (int)XSECURE_ECDSA_KEY_NOT_ON_CRV;
+		else if (ValidateStatus == ELLIPTIC_KEY_NOT_ON_CRV) {
+			Status = (int)XSECURE_ELLIPTIC_KEY_NOT_ON_CRV;
 		}
-		else if (ValidateStatus != ECDSA_SUCCESS) {
+		else if (ValidateStatus != ELLIPTIC_SUCCESS) {
 			Status = XST_FAILURE;
 		}
 		else {
@@ -271,9 +280,9 @@ END:
 /*****************************************************************************/
 /**
  * @brief	This function verifies the signature for a given hash, key and
- *			curve type
+ *		curve type
  *
- * @param	CrvType - Type of ECDSA curve
+ * @param	CrvType - Type of elliptic curve
  * @param	Hash    - Pointer to the hash for which sign has to be generated
  * @param	HashLen - Length of hash in bytes
  * @param	Key     - Pointer to the public key
@@ -281,69 +290,69 @@ END:
  *
  * @return
  *	- XST_SUCCESS - On success
- * 	- XSECURE_ECDSA_INVALID_PARAM - On invalid argument
- * 	- XSECURE_ECDSA_BAD_SIGN - When signature provided for verification is bad
- * 	- XSECURE_ECDSA_VER_SIGN_INCORRECT_HASH_LEN - Incorrect hash length for sign
- *						      verification
- *      - XSECURE_ECDSA_VER_SIGN_R_ZERO - R set to zero
- *      - XSECURE_ECDSA_VER_SIGN_S_ZERO - S set to zero
- *      - XSECURE_ECDSA_VER_SIGN_R_ORDER_ERROR - R is not within ECC order
- *      - XSECURE_ECDSA_VER_SIGN_S_ORDER_ERROR - S is not within ECC order
+ *	- XSECURE_ELLIPTIC_INVALID_PARAM - On invalid argument
+ *	- XSECURE_ELLIPTIC_BAD_SIGN - When signature provided for verification is bad
+ *	- XSECURE_ELLIPTIC_VER_SIGN_INCORRECT_HASH_LEN - Incorrect hash length
+ *						for sign verification
+ *	- XSECURE_ELLIPTIC_VER_SIGN_R_ZERO - R set to zero
+ *	- XSECURE_ELLIPTIC_VER_SIGN_S_ZERO - S set to zero
+ *	- XSECURE_ELLIPTIC_VER_SIGN_R_ORDER_ERROR - R is not within ECC order
+ *	- XSECURE_ELLIPTIC_VER_SIGN_S_ORDER_ERROR - S is not within ECC order
  *	- XST_FAILURE - On failure
  *
  *****************************************************************************/
-int XSecure_EcdsaVerifySign(XSecure_EcdsaCrvTyp CrvType, const u8 *Hash,
-	const u32 HashLen, XSecure_EcdsaKey *Key, XSecure_EcdsaSign *Sign)
+int XSecure_EllipticVerifySign(XSecure_EllipticCrvTyp CrvType, const u8 *Hash,
+	const u32 HashLen, XSecure_EllipticKey *Key, XSecure_EllipticSign *Sign)
 {
-	int Status = (int)XSECURE_ECDSA_NON_SUPPORTED_CRV;
+	int Status = (int)XSECURE_ELLIPTIC_NON_SUPPORTED_CRV;
 	volatile int VerifyStatus = XST_FAILURE;
 	EcdsaCrvInfo *Crv = NULL;
-	u8 PaddedHash[XSECURE_ECDSA_P521_SIZE_IN_BYTES] = {0U};
+	u8 PaddedHash[XSECURE_ECC_P521_SIZE_IN_BYTES] = {0U};
 
-	if ((CrvType != XSECURE_ECDSA_NIST_P384) && (CrvType != XSECURE_ECDSA_NIST_P521)) {
-		Status = (int)XSECURE_ECDSA_INVALID_PARAM;
+	if ((CrvType != XSECURE_ECC_NIST_P384) && (CrvType != XSECURE_ECC_NIST_P521)) {
+		Status = (int)XSECURE_ELLIPTIC_INVALID_PARAM;
 		goto END;
 	}
 
 	if ((Hash == NULL) || (Key == NULL) || (Sign == NULL)) {
-		Status = (int)XSECURE_ECDSA_INVALID_PARAM;
+		Status = (int)XSECURE_ELLIPTIC_INVALID_PARAM;
 		goto END;
 	}
 
-	if (HashLen > XSECURE_ECDSA_P521_SIZE_IN_BYTES) {
-		Status = (int)XSECURE_ECDSA_INVALID_PARAM;
+	if (HashLen > XSECURE_ECC_P521_SIZE_IN_BYTES) {
+		Status = (int)XSECURE_ELLIPTIC_INVALID_PARAM;
 		goto END;
 	}
 
 	XSecure_ReleaseReset(XSECURE_ECDSA_RSA_BASEADDR,
 		XSECURE_ECDSA_RSA_RESET_OFFSET);
 
-	Crv = XSecure_EcdsaGetCrvData(CrvType);
+	Crv = XSecure_EllipticGetCrvData(CrvType);
 	if(Crv != NULL) {
 		Xil_MemCpy(PaddedHash, Hash, HashLen);
 		XSECURE_TEMPORAL_CHECK(SIG_ERR, VerifyStatus, Ecdsa_VerifySign,
 			Crv, PaddedHash, Crv->Bits, (EcdsaKey *)Key, (EcdsaSign *)Sign);
 
 SIG_ERR:
-		if (ECDSA_BAD_SIGN == VerifyStatus) {
-			Status = (int)XSECURE_ECDSA_BAD_SIGN;
+		if ((int)ELLIPTIC_BAD_SIGN == VerifyStatus) {
+			Status = (int)XSECURE_ELLIPTIC_BAD_SIGN;
 		}
-		else if (ECDSA_VER_SIGN_INCORRECT_HASH_LEN == VerifyStatus) {
-			Status = (int)XSECURE_ECDSA_VER_SIGN_INCORRECT_HASH_LEN;
+		else if ((int)ELLIPTIC_VER_SIGN_INCORRECT_HASH_LEN == VerifyStatus) {
+			Status = (int)XSECURE_ELLIPTIC_VER_SIGN_INCORRECT_HASH_LEN;
 		}
-		else if (ECDSA_VER_SIGN_R_ZERO == VerifyStatus) {
-                        Status = (int)XSECURE_ECDSA_VER_SIGN_R_ZERO;
-                }
-		else if (ECDSA_VER_SIGN_S_ZERO == VerifyStatus) {
-                        Status = (int)XSECURE_ECDSA_VER_SIGN_S_ZERO;
-                }
-		else if (ECDSA_VER_SIGN_R_ORDER_ERROR == VerifyStatus) {
-                        Status = (int)XSECURE_ECDSA_VER_SIGN_R_ORDER_ERROR;
-                }
-		else if (ECDSA_VER_SIGN_S_ORDER_ERROR == VerifyStatus) {
-                        Status = (int)XSECURE_ECDSA_VER_SIGN_S_ORDER_ERROR;
-                }
-		else if (ECDSA_SUCCESS != VerifyStatus) {
+		else if (ELLIPTIC_VER_SIGN_R_ZERO == VerifyStatus) {
+			Status = (int)XSECURE_ELLIPTIC_VER_SIGN_R_ZERO;
+		}
+		else if (ELLIPTIC_VER_SIGN_S_ZERO == VerifyStatus) {
+			Status = (int)XSECURE_ELLIPTIC_VER_SIGN_S_ZERO;
+		}
+		else if (ELLIPTIC_VER_SIGN_R_ORDER_ERROR == VerifyStatus) {
+			Status = (int)XSECURE_ELLIPTIC_VER_SIGN_R_ORDER_ERROR;
+		}
+		else if (ELLIPTIC_VER_SIGN_S_ORDER_ERROR == VerifyStatus) {
+			Status = (int)XSECURE_ELLIPTIC_VER_SIGN_S_ORDER_ERROR;
+		}
+		else if (ELLIPTIC_SUCCESS != VerifyStatus) {
 			Status = XST_FAILURE;
 		}
 		else {
@@ -359,41 +368,40 @@ END:
 
 /*****************************************************************************/
 /**
- * @brief	This function performs known answer test(KAT) on ECDSA core
+ * @brief	This function performs known answer test(KAT) on ECC core
  *
  * @param	None
  *
  * @return
  *		- XST_SUCCESS - On success
- * 		- XSECURE_ECC_KAT_KEY_NOTVALID_ERROR - When ECC key is not valid
- * 		- XSECURE_ECC_KAT_FAILED_ERROR       - When ECC KAT fails
+ * 		- XSECURE_ELLIPTIC_KAT_KEY_NOTVALID_ERROR - When elliptic key is not valid
+ * 		- XSECURE_ELLIPTIC_KAT_FAILED_ERROR       - When elliptic KAT fails
  *
  *****************************************************************************/
-int XSecure_EcdsaKat(void)
+int XSecure_EllipticKat(void)
 {
-	volatile int Status = (int)XSECURE_ECC_KAT_FAILED_ERROR;
+	volatile int Status = (int)XSECURE_ELLIPTIC_KAT_FAILED_ERROR;
 
-	XSecure_EcdsaKey Key;
-	XSecure_EcdsaSign ExpectedSign;
+	XSecure_EllipticKey Key;
+	XSecure_EllipticSign ExpectedSign;
 
 	Key.Qx = (u8 *)QxCord;
 	Key.Qy = (u8 *)QyCord;
 	ExpectedSign.SignR = (u8*)KatSignR;
 	ExpectedSign.SignS = (u8*)KatSignS;
 
-	Status = XSecure_EcdsaValidateKey(XSECURE_ECDSA_NIST_P384, &Key);
+	Status = XSecure_EllipticValidateKey(XSECURE_ECC_NIST_P384, &Key);
 	if(Status != XST_SUCCESS) {
-		Status = (int)XSECURE_ECC_KAT_KEY_NOTVALID_ERROR;
+		Status = (int)XSECURE_ELLIPTIC_KAT_KEY_NOTVALID_ERROR;
 		goto END;
 	}
 
-	Status = (int)XSECURE_ECC_KAT_FAILED_ERROR;
+	Status = (int)XSECURE_ELLIPTIC_KAT_FAILED_ERROR;
 
-	Status = XSecure_EcdsaVerifySign(XSECURE_ECDSA_NIST_P384,
-				(const u8 *)HashVal, XSECURE_SHA3_LEN_BYTES,
-				&Key, &ExpectedSign);
+	Status = XSecure_EllipticVerifySign(XSECURE_ECC_NIST_P384,
+		(const u8 *)HashVal, XSECURE_SHA3_LEN_BYTES, &Key, &ExpectedSign);
 	if(Status != XST_SUCCESS) {
-		Status = (int)XSECURE_ECC_KAT_FAILED_ERROR;
+		Status = (int)XSECURE_ELLIPTIC_KAT_FAILED_ERROR;
 		goto END;
 	}
 
@@ -405,20 +413,20 @@ END:
 /**
  * @brief	This function gets the curve related information
  *
- * @param	CrvTyp - Is a type of the ECDSA curve
+ * @param	CrvTyp - Type of the elliptic curve
  *
- * @return	- Crv - Which contains the curve information
+ * @return	Crv    - Curve information
  *
  *****************************************************************************/
-static EcdsaCrvInfo* XSecure_EcdsaGetCrvData(XSecure_EcdsaCrvTyp CrvTyp)
+static EcdsaCrvInfo* XSecure_EllipticGetCrvData(XSecure_EllipticCrvTyp CrvTyp)
 {
 	u32 Index;
 	EcdsaCrvInfo *Crv = NULL;
-	u32 TotalCurves = XSecure_EcdsaCrvsGetCount();
+	u32 TotalCurves = XSecure_EllipticCrvsGetCount();
 
 	for(Index = 0U; Index < TotalCurves; Index++) {
-		if (XSecure_EcdsaCrvsDb[Index].CrvType == (EcdsaCrvTyp)CrvTyp) {
-			Crv = &XSecure_EcdsaCrvsDb[Index];
+		if (XSecure_EllipticCrvsDb[Index].CrvType == (EcdsaCrvTyp)CrvTyp) {
+			Crv = &XSecure_EllipticCrvsDb[Index];
 			break;
 		}
 	}
