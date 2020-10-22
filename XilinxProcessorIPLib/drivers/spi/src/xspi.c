Xilinx/embeddedsw

commit eb8d819ef7979b59e0e03b03e868bd947d14f068

spi: Removed master inhibit dependency while writing to DTR
In case of multiple transfers, for each transfer the driver
was setting master inhibit bit before filling the DTR and
then reseting master inhibit bit after filling the DTR.
This hampered the transfer throughput.

This patch fixes this issue by setting and resetting the master
inhibit bit only once.

Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>

Acked-for-series: Srinivas Goud <srinivas.goud@xilinx.com>

XilinxProcessorIPLib/drivers/spi/src/xspi.c
@@ -96,6 +96,8 @@
 * 4.4	tjs  11/28/17 When receive fifo exists, we need to check for status
 *                     register rx fifo empty flag. If clear we can proceed for
 *                     read. Otherwise we will hit execption. CR# 989938
+* 4.5	akm  05/29/19 Removed master inhibit dependency while writing DTR
+*		      in between multiple transfers.
 * </pre>
 *
 ******************************************************************************/
@@ -617,6 +619,12 @@ int XSpi_Transfer(XSpi *InstancePtr, u8 *SendBufPtr,
 
 	DataWidth = InstancePtr->DataWidth;
 
+	/* Inhibit the transmitter while the transmit register/FIFO is
+	 * being filled.
+	 */
+	ControlReg = XSpi_GetControlReg(InstancePtr);
+	XSpi_SetControlReg(InstancePtr, ControlReg |
+			   XSP_CR_TRANS_INHIBIT_MASK);
 	/*
 	 * Fill the DTR/FIFO with as many bytes as it will take (or as many as
 	 * we have to send). We use the tx full status bit to know if the device
@@ -705,16 +713,6 @@ int XSpi_Transfer(XSpi *InstancePtr, u8 *SendBufPtr,
 
 			XSpi_IntrClear(InstancePtr,XSP_INTR_TX_EMPTY_MASK);
 
-			/*
-			 * A transmit has just completed. Process received data
-			 * and check for more data to transmit. Always inhibit
-			 * the transmitter while the transmit register/FIFO is
-			 * being filled, or make sure it is stopped if we're
-			 * done.
-			 */
-			ControlReg = XSpi_GetControlReg(InstancePtr);
-			XSpi_SetControlReg(InstancePtr, ControlReg |
-						XSP_CR_TRANS_INHIBIT_MASK);
 
 			/*
 			 * First get the data received as a result of the
@@ -819,13 +817,6 @@ int XSpi_Transfer(XSpi *InstancePtr, u8 *SendBufPtr,
 							InstancePtr);
 				}
 
-				/*
-				 * Start the transfer by not inhibiting the
-				 * transmitter any longer.
-				 */
-				ControlReg = XSpi_GetControlReg(InstancePtr);
-				ControlReg &= ~XSP_CR_TRANS_INHIBIT_MASK;
-				XSpi_SetControlReg(InstancePtr, ControlReg);
 			}
 		}
 
@@ -1103,7 +1094,6 @@ void XSpi_InterruptHandler(void *InstancePtr)
 	u32 IntrStatus;
 	unsigned int BytesDone;	/* number of bytes done so far */
 	u32 Data = 0;
-	u32 ControlReg;
 	u32 StatusReg;
 	u8  DataWidth;
 
@@ -1156,15 +1146,6 @@ void XSpi_InterruptHandler(void *InstancePtr)
 	if ((IntrStatus & XSP_INTR_TX_EMPTY_MASK) ||
 	    (IntrStatus & XSP_INTR_TX_HALF_EMPTY_MASK)) {
 
-		/*
-		 * A transmit has just completed. Process received data and
-		 * check for more data to transmit. Always inhibit the
-		 * transmitter while the Isr re-fills the transmit
-		 * register/FIFO, or make sure it is stopped if we're done.
-		 */
-		ControlReg = XSpi_GetControlReg(SpiPtr);
-		XSpi_SetControlReg(SpiPtr, ControlReg |
-					XSP_CR_TRANS_INHIBIT_MASK);
 
 		/*
 		 * First get the data received as a result of the transmit that
@@ -1249,11 +1230,6 @@ void XSpi_InterruptHandler(void *InstancePtr)
 				StatusReg = XSpi_GetStatusReg(SpiPtr);
 			}
 
-			/*
-			 * Start the transfer by not inhibiting the transmitter
-			 * any longer.
-			 */
-			XSpi_SetControlReg(SpiPtr, ControlReg);
 		} else {
 
 			/*

XilinxProcessorIPLib/drivers/spi/src/xspi.h
@@ -295,6 +295,8 @@
 * 4.4	tjs  11/28/17 When receive fifo exists, we need to check for status
 *                     register rx fifo empty flag. If clear we can proceed for
 *                     read. Otherwise we will hit execption. CR# 989938
+* 4.5	akm  05/29/19 Removed master inhibit dependency while writing DTR
+*		      in between multiple transfers.
 * </pre>
 *
 ******************************************************************************/

commit a4acb9ecba683b6d7cb3d251bfb8c3ee832935c1

spi: Fill txfifo with data less than or equal to fifo depth
In between multiple transfers when txfifo empty Interrupt occurs, The driver
first reads the rxfifo till it is empty, then the driver starts filling the
txfifo with remaining data bytes and simultaneously keeps on checking for the
tx_full flag. But as the master inhibit bit is already set so as soon as the
data is written to the txfifo, it gets pushed out on to the lines and the
tx_full never gets set. So when the number of bytes written to the txfifo goes
beyond the fifo depth then rxfifo overrun error occurs and the Transfer fails.

This bug was introduced in commit eb8d819ef7979b59e0e03b03e868bd947d14f068

This patch removes the dependency of tx_full flag while writing data to the
txfifo and fills the txfifo with data byte either less than or equal to the
fifo depth.

Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>

Acked-for-series: Srinivas Goud <srinivas.goud@xilinx.com>

XilinxProcessorIPLib/drivers/spi/src/xspi.c
@@ -76,6 +76,8 @@
 *                     read. Otherwise we will hit exception. CR# 989938
 * 4.5	akm  05/29/19 Removed master inhibit dependency while writing DTR
 *		      in between multiple transfers.
+* 4.7	akm  10/22/20 Removed dependency of Tx_Full flag while writing DTR
+*		      in between multiple transfers.
 * </pre>
 *
 ******************************************************************************/
@@ -172,6 +174,7 @@ int XSpi_CfgInitialize(XSpi *InstancePtr, XSpi_Config *Config,
 	InstancePtr->RemainingBytes = 0;
 	InstancePtr->BaseAddr = EffectiveAddr;
 	InstancePtr->HasFifos = Config->HasFifos;
+	InstancePtr->FifosDepth = Config->FifosDepth;
 	InstancePtr->SlaveOnly = Config->SlaveOnly;
 	InstancePtr->NumSlaveBits = Config->NumSlaveBits;
 	if (Config->DataWidth == 0) {
@@ -523,6 +526,8 @@ int XSpi_Transfer(XSpi *InstancePtr, u8 *SendBufPtr,
 	u32 StatusReg;
 	u32 Data = 0;
 	u8  DataWidth;
+	u32 DataLen;
+	u32 Index;
 
 	/*
 	 * The RecvBufPtr argument can be NULL.
@@ -755,10 +760,10 @@ int XSpi_Transfer(XSpi *InstancePtr, u8 *SendBufPtr,
 				 * The downside is that the status must be read
 				 * each loop iteration.
 				 */
-				StatusReg = XSpi_GetStatusReg(InstancePtr);
+				DataLen = (InstancePtr->RemainingBytes > InstancePtr->FifosDepth) ?
+						InstancePtr->FifosDepth : InstancePtr->RemainingBytes;
 
-				while(((StatusReg & XSP_SR_TX_FULL_MASK)== 0) &&
-					(InstancePtr->RemainingBytes > 0)) {
+				for(Index = 0; Index < DataLen; Index += (DataWidth / 8)) {
 					if (DataWidth == XSP_DATAWIDTH_BYTE) {
 						/*
 						 * Data Transfer Width is Byte
@@ -789,11 +794,8 @@ int XSpi_Transfer(XSpi *InstancePtr, u8 *SendBufPtr,
 							XSP_DTR_OFFSET, Data);
 					InstancePtr->SendBufferPtr +=
 							(DataWidth >> 3);
-					InstancePtr->RemainingBytes -=
-							(DataWidth >> 3);
-					StatusReg = XSpi_GetStatusReg(
-							InstancePtr);
 				}
+				InstancePtr->RemainingBytes -= DataLen;
 
 			}
 		}
@@ -1074,6 +1076,8 @@ void XSpi_InterruptHandler(void *InstancePtr)
 	u32 Data = 0;
 	u32 StatusReg;
 	u8  DataWidth;
+	u32 DataLen;
+	u32 Index;
 
 	Xil_AssertVoid(InstancePtr != NULL);
 
@@ -1177,9 +1181,9 @@ void XSpi_InterruptHandler(void *InstancePtr)
 			 * The downside is that the status must be read each
 			 * loop iteration.
 			 */
-			StatusReg = XSpi_GetStatusReg(SpiPtr);
-			while (((StatusReg & XSP_SR_TX_FULL_MASK) == 0) &&
-				(SpiPtr->RemainingBytes > 0)) {
+			DataLen = (SpiPtr->RemainingBytes > SpiPtr->FifosDepth) ?
+					SpiPtr->FifosDepth : SpiPtr->RemainingBytes;
+			for(Index = 0; Index < DataLen; Index += (DataWidth / 8)) {
 				if (DataWidth == XSP_DATAWIDTH_BYTE) {
 					/*
 					 * Data Transfer Width is Byte (8 bit).
@@ -1204,9 +1208,8 @@ void XSpi_InterruptHandler(void *InstancePtr)
 				XSpi_WriteReg(SpiPtr->BaseAddr, XSP_DTR_OFFSET,
 						Data);
 				SpiPtr->SendBufferPtr += (DataWidth >> 3);
-				SpiPtr->RemainingBytes -= (DataWidth >> 3);
-				StatusReg = XSpi_GetStatusReg(SpiPtr);
 			}
+			SpiPtr->RemainingBytes -= DataLen;
 
 		} else {
 

XilinxProcessorIPLib/drivers/spi/src/xspi.h
@@ -279,6 +279,8 @@
 *		       passing the correct interrupt controller instance to
 *		       SpiIntrExample() function (CR-1035793).
 * 4.7	akm  09/02/20 Updated the Makefile to support parallel make execution.
+* 4.7   akm  10/22/20 Removed dependency of Tx_Full flag while writing DTR
+*                     in between multiple transfers.
 * </pre>
 *
 ******************************************************************************/
@@ -424,6 +426,7 @@ typedef struct {
 	void *StatusRef;	/**< Callback reference for status handler */
 	u32 FlashBaseAddr;    	/**< Used in XIP Mode */
 	u8 XipMode;             /**< 0 if Non-XIP, 1 if XIP Mode */
+	u16 FifosDepth;		/**< TX and RX FIFO Depth */
 } XSpi;
 
 /***************** Macros (Inline Functions) Definitions *********************/