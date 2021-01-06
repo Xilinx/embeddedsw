b72af205e777cc78e9e508d9380d71e94281242a
Mubin Sayyed <mubin.usman.sayyed@xilinx.com>
2021-01-06 13:06:41 +0530

    intc: Update doxygen tags
    Updated doxygen tag to latest version 3.13.
    
    Signed-off-by: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

--- XilinxProcessorIPLib/drivers/intc/src/xintc.c
+++ XilinxProcessorIPLib/drivers/intc/src/xintc.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2002 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2002 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xintc.c
-* @addtogroup intc_v3_12
+* @addtogroup intc_v3_13
 * @{
 *
 * Contains required functions for the XIntc driver for the Xilinx Interrupt
--- XilinxProcessorIPLib/drivers/intc/src/xintc_options.c
+++ XilinxProcessorIPLib/drivers/intc/src/xintc_options.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2002 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2002 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xintc_options.c
-* @addtogroup intc_v3_12
+* @addtogroup intc_v3_13
 * @{
 *
 * Contains option functions for the XIntc driver. These functions allow the
