b4b3a31b31e7e50fc50c301d981433473c68b9b9
Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>
2021-01-06 18:40:56 -0800

    aienginev2: src: common: move io operations from header
    move low level io operation functions from header file.
    
    Signed-off-by: Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>
    
    Acked-for-series: Wendy Liang <wendy.liang@xilinx.com>

--- XilinxProcessorIPLib/drivers/aienginev2/src/common/xaie_helper.h
+++ XilinxProcessorIPLib/drivers/aienginev2/src/common/xaie_helper.h
@@ -72,72 +72,6 @@ static inline u64 _XAie_GetTileAddr(XAie_DevInst *DevInst, int R, int C)
 	return (R << DevInst->DevProp.RowShift) | (C << DevInst->DevProp.ColShift);
 }
 
-static inline void XAie_Write32(XAie_DevInst *DevInst, u64 RegOff, u32 Value)
-{
-	const XAie_Backend *Backend = DevInst->Backend;
-
-	Backend->Ops.Write32((void*)(DevInst->IOInst), RegOff, Value);
-}
-
-static inline u32 XAie_Read32(XAie_DevInst *DevInst, u64 RegOff)
-{
-	const XAie_Backend *Backend = DevInst->Backend;
-
-	return Backend->Ops.Read32((void*)(DevInst->IOInst), RegOff);
-}
-
-static inline void XAie_MaskWrite32(XAie_DevInst *DevInst, u64 RegOff, u32 Mask,
-		u32 Value)
-{
-	const XAie_Backend *Backend = DevInst->Backend;
-
-	Backend->Ops.MaskWrite32((void *)(DevInst->IOInst), RegOff, Mask,
-			Value);
-}
-
-static inline u32 XAie_MaskPoll(XAie_DevInst *DevInst, u64 RegOff, u32 Mask,
-		u32 Value, u32 TimeOutUs)
-{
-	const XAie_Backend *Backend = DevInst->Backend;
-
-	return Backend->Ops.MaskPoll((void*)(DevInst->IOInst), RegOff, Mask,
-			Value, TimeOutUs);
-}
-
-static inline void XAie_BlockWrite32(XAie_DevInst *DevInst, u64 RegOff,
-		u32 *Data, u32 Size)
-{
-	const XAie_Backend *Backend = DevInst->Backend;
-
-	Backend->Ops.BlockWrite32((void *)(DevInst->IOInst), RegOff, Data,
-			Size);
-}
-
-static inline void XAie_BlockSet32(XAie_DevInst *DevInst, u64 RegOff, u32 Data,
-		u32 Size)
-{
-	const XAie_Backend *Backend = DevInst->Backend;
-
-	Backend->Ops.BlockSet32((void *)(DevInst->IOInst), RegOff, Data, Size);
-}
-
-static inline void XAie_CmdWrite(XAie_DevInst *DevInst, u8 Col, u8 Row,
-		u8 Command, u32 CmdWd0, u32 CmdWd1, const char *CmdStr)
-{
-	const XAie_Backend *Backend = DevInst->Backend;
-
-	Backend->Ops.CmdWrite((void *)(DevInst->IOInst), Col, Row, Command,
-			CmdWd0, CmdWd1, CmdStr);
-}
-
-static inline AieRC XAie_RunOp(XAie_DevInst *DevInst, XAie_BackendOpCode Op,
-		void *Arg)
-{
-	const XAie_Backend *Backend = DevInst->Backend;
-
-	return Backend->Ops.RunOp(DevInst->IOInst, DevInst, Op, Arg);
-}
-
 void XAie_Log(FILE *Fd, const char* prefix, const char *Format, ...);
 u8 _XAie_GetTileTypefromLoc(XAie_DevInst *DevInst, XAie_LocType Loc);
 AieRC _XAie_CheckModule(XAie_DevInst *DevInst, XAie_LocType Loc,
@@ -150,5 +84,16 @@ u32 _XAie_GetFatalGroupErrors(XAie_DevInst *DevInst, XAie_LocType Loc,
 		XAie_ModuleType Module);
 u32 _XAie_GetTileBitPosFromLoc(XAie_DevInst *DevInst, XAie_LocType Loc);
 void _XAie_SetBitInBitmap(u32 *Bitmap, u32 StartSetBit, u32 NumSetBit);
+void XAie_Write32(XAie_DevInst *DevInst, u64 RegOff, u32 Value);
+u32 XAie_Read32(XAie_DevInst *DevInst, u64 RegOff);
+void XAie_MaskWrite32(XAie_DevInst *DevInst, u64 RegOff, u32 Mask, u32 Value);
+u32 XAie_MaskPoll(XAie_DevInst *DevInst, u64 RegOff, u32 Mask, u32 Value,
+		u32 TimeOutUs);
+void XAie_BlockWrite32(XAie_DevInst *DevInst, u64 RegOff, u32 *Data, u32 Size);
+void XAie_BlockSet32(XAie_DevInst *DevInst, u64 RegOff, u32 Data, u32 Size);
+void XAie_CmdWrite(XAie_DevInst *DevInst, u8 Col, u8 Row, u8 Command,
+		u32 CmdWd0, u32 CmdWd1, const char *CmdStr);
+AieRC XAie_RunOp(XAie_DevInst *DevInst, XAie_BackendOpCode Op, void *Arg);
+
 #endif		/* end of protection macro */
 /** @} */
