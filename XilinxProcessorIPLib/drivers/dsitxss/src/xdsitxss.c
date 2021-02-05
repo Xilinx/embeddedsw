Xilinx/embeddedsw

commit f97be9b8669677df09c98a059ba1cb4b75a21469

dsitxss: Updated XDsiTxSs_Activate API to enable subcores
This patch introduces new argument to XDsiTxSs_Activate API
to enable/disable subsytem cores DSI & DPHY separately.
This change is to accomodate new init sequence prescribed by
hardware version 2.1

Signed-off-by: Venkateshwar Rao Gannavarapu <venkateshwar.rao.gannavarapu@xilinx.com>

Acked-for-series: Sandip Kothari <sandipk@xilinx.com>

XilinxProcessorIPLib/drivers/dsitxss/src/xdsitxss.c
@@ -27,7 +27,7 @@
 /**
 *
 * @file xdsitxss.c
-* @addtogroup dsitxss_v1_1
+* @addtogroup dsitxss_v2_0
 * @{
 *
 * This is main code of Xilinx MIPI DSI Tx Subsystem device driver.
@@ -185,29 +185,33 @@ u32 XDsiTxSs_DefaultConfigure(XDsiTxSs *InstancePtr)
 * the DPHY and DSI. Enable/Disable IP core to start processing
 *
 * @param	InstancePtr is a pointer to the Subsystem instance to be worked on.
-* @param	Flag is used to denote whether to enable or disable the subsystem
+* @param	core is used to denote the subcore of subsystem
+* @param        Flag is used to denote whether to enable or disable the subsystem
 *
-* @return	None
+* @return	XST_SUCCESS is returned if subcore(DSI/DPHY) was
+*			successfully enabled or disabled
+*		XST_INVALID_PARAM is returned if subsystem core is not found
 *
 * @note		None.
 *
 ******************************************************************************/
-void XDsiTxSs_Activate(XDsiTxSs *InstancePtr, u8 Flag)
+int XDsiTxSs_Activate(XDsiTxSs *InstancePtr, XDsiSS_Subcore core, u8 Flag)
 {
 	/* Verify arguments */
 	Xil_AssertVoid(InstancePtr != NULL);
 	Xil_AssertVoid(Flag <= XDSITXSS_ENABLE);
 	Xil_AssertVoid(InstancePtr->DsiPtr != NULL);
+	Xil_AssertVoid(InstancePtr->DphyPtr != NULL);
 
-	XDsi_Activate(InstancePtr->DsiPtr, Flag);
-
-#if (XPAR_XDPHY_NUM_INSTANCES > 0)
-	if (InstancePtr->Config.IsDphyRegIntfcPresent && InstancePtr->DphyPtr) {
+	if (core == XDSITXSS_DSI)
+		XDsi_Activate(InstancePtr->DsiPtr, Flag);
+	else if (core == XDSITXSS_PHY)
 		XDphy_Activate(InstancePtr->DphyPtr, Flag);
-	}
-#endif
-}
+	else
+		return XST_INVALID_PARAM;
 
+	return XST_SUCCESS;
+}
 /*****************************************************************************/
 /**
 * This function is used to reset the DSI Subsystem. Internally it resets

XilinxProcessorIPLib/drivers/dsitxss/src/xdsitxss.h
@@ -27,7 +27,7 @@
 /**
 *
 * @file xdsitxss.h
-* @addtogroup dsitxss_v1_1
+* @addtogroup dsitxss_v2_0
 * @{
 * @details
 *
@@ -175,6 +175,14 @@ typedef enum {
 } XDsiSS_Selection;
 
 /**
+ * Sub-Core Enable/Disable
+ */
+typedef enum {
+	XDSITXSS_DSI,	/* DSI Core */
+	XDSITXSS_PHY	/* DPHY */
+} XDsiSS_Subcore;
+
+/**
  * MIPI DSI Tx Subsystem configuration structure.
  * Each subsystem device should have a configuration structure associated
  * that defines the MAX supported sub-cores within subsystem
@@ -251,7 +259,7 @@ XDsiTxSs_Config* XDsiTxSs_LookupConfig(u32 DeviceId);
 s32 XDsiTxSs_CfgInitialize(XDsiTxSs *InstancePtr, XDsiTxSs_Config *CfgPtr,
 							UINTPTR EffectiveAddr);
 u32 XDsiTxSs_DefaultConfigure(XDsiTxSs *InstancePtr);
-void XDsiTxSs_Activate(XDsiTxSs *InstancePtr, u8 Flag);
+int XDsiTxSs_Activate(XDsiTxSs *InstancePtr, XDsiSS_Subcore core, u8 Flag);
 void XDsiTxSs_Reset(XDsiTxSs *InstancePtr);
 void XDsiTxSs_ReportCoreInfo(XDsiTxSs *InstancePtr);
 u32 XDsiTxSs_SelfTest(XDsiTxSs *InstancePtr);

commit a191f4bc3ee22a078f068542efcb627ea33e3249

dsitxss: Fix DPHY compilation issue
Fixes compilation issue introduced by commit
f97be9b8669677df09c98a059ba1cb4b75a21469 dsitxss: Updated
XDsiTxSs_Activate API to enable subcores

When DPHY register interface isn't enabled, the Dphy related APIs
shouldn't be compiled. This patch fixes this. It also fixes the
assertions to Nonvoid.

Signed-off-by: Vishal Sagar <vishal.sagar@xilinx.com>
Acked-by: Sandip Kothari <sandipk@xilinx.com>

XilinxProcessorIPLib/drivers/dsitxss/src/xdsitxss.c
@@ -63,7 +63,9 @@ XDsiTxSs_SubCores DsiTxSsSubCores[XPAR_XDSITXSS_NUM_INSTANCES]; /**< Define Driv
 
 static void XDsiTxSs_GetIncludedSubCores(XDsiTxSs *DsiTxSsPtr);
 static s32 XDsiTxSs_SubCoreInitDsi(XDsiTxSs *DsiTxSsPtr);
+#if (XPAR_XDPHY_NUM_INSTANCES > 0)
 static s32 XDsiTxSs_SubCoreInitDphy(XDsiTxSs *DsiTxSsPtr);
+#endif
 static s32 ComputeSubCoreAbsAddr(UINTPTR SsBaseAddr, UINTPTR SsHighAddr,
 					u32 Offset, UINTPTR *BaseAddr);
 
@@ -178,15 +180,18 @@ u32 XDsiTxSs_DefaultConfigure(XDsiTxSs *InstancePtr)
 int XDsiTxSs_Activate(XDsiTxSs *InstancePtr, XDsiSS_Subcore core, u8 Flag)
 {
 	/* Verify arguments */
-	Xil_AssertVoid(InstancePtr != NULL);
-	Xil_AssertVoid(Flag <= XDSITXSS_ENABLE);
-	Xil_AssertVoid(InstancePtr->DsiPtr != NULL);
-	Xil_AssertVoid(InstancePtr->DphyPtr != NULL);
-
+	Xil_AssertNonvoid(InstancePtr != NULL);
+	Xil_AssertNonvoid(Flag <= XDSITXSS_ENABLE);
+	Xil_AssertNonvoid(InstancePtr->DsiPtr != NULL);
+#if (XPAR_XDPHY_NUM_INSTANCES > 0)
+	Xil_AssertNonvoid(InstancePtr->DphyPtr != NULL);
+#endif
 	if (core == XDSITXSS_DSI)
 		XDsi_Activate(InstancePtr->DsiPtr, Flag);
+#if (XPAR_XDPHY_NUM_INSTANCES > 0)
 	else if (core == XDSITXSS_PHY)
 		XDphy_Activate(InstancePtr->DphyPtr, Flag);
+#endif
 	else
 		return XST_INVALID_PARAM;
 