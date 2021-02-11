aacef63c6779b4b152b4849ba26ca9fafa3f8ecb
Izhar Ameer Shaikh <izhar.ameer.shaikh@xilinx.com>
2021-02-11 09:55:20 -0800

    xilpm: versal: server: Remove protection InitNode Ops from PMC Powerdomain
    As Protection nodes now support InitNode operations, remove the existing
    protection node control operations from PMC Powerdomain.
    
    Signed-off-by: Izhar Ameer Shaikh <izhar.ameer.shaikh@xilinx.com>
    
    Acked-for-series: Ravi Patel <ravi.patel@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_pmcdomain.c
+++ lib/sw_services/xilpm/src/versal/server/xpm_pmcdomain.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (c) 2018 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (c) 2018 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -39,42 +39,13 @@ static XStatus HandlePmcDomainEvent(XPm_Node *Node, u32 Event)
 	return Status;
 }
 
-/****************************************************************************/
-/**
- * @brief  This function configures xppu for PMC or PMC_NPI
- *
- * @return XST_SUCCESS if successful else XST_FAILURE
- *
- ****************************************************************************/
-static XStatus PmcXppuCtrl(u32 *Args, u32 NumOfArgs)
-{
-	return XPmProt_CommonXppuCtrl(Args, NumOfArgs);
-}
-
-/****************************************************************************/
-/**
- * @brief  This function configures xmpu for PMC (used for DDR memory)
- *
- * @return XST_SUCCESS if successful else XST_FAILURE
- *
- ****************************************************************************/
-static XStatus PmcXmpuCtrl(u32 *Args, u32 NumOfArgs)
-{
-	return XPmProt_CommonXmpuCtrl(Args, NumOfArgs);
-}
-
-static struct XPm_PowerDomainOps PmcOps = {
-	.XppuCtrl = PmcXppuCtrl,
-	.XmpuCtrl = PmcXmpuCtrl,
-};
-
 XStatus XPmPmcDomain_Init(XPm_PmcDomain *PmcDomain, u32 Id, XPm_Power *Parent)
 {
 	XStatus Status = XST_FAILURE;
 	u16 DbgErr = XPM_INT_ERR_UNDEFINED;
 
 	Status = XPmPowerDomain_Init(&PmcDomain->Domain, Id, 0x00000000, Parent,
-				     &PmcOps);
+				     NULL);
 	if (XST_SUCCESS != Status) {
 		PmErr("Status: 0x%x\r\n", Status);
 		goto done;
