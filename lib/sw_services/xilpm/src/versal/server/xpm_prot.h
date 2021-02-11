5e0f7193d9745e12853296d66b35bda52a85ab0b
Izhar Ameer Shaikh <izhar.ameer.shaikh@xilinx.com>
2021-02-11 09:55:13 -0800

    xilpm: versal: server: Make xpm_prot module compliant with xilpm coding style
    Fix various xpm_prot module coding style issues, such as:
     - Use XPm_Node (aliased to XPm_Prot) as the base class directly for
     protection derived classed for xppu and xmpu
     - Fix/Shorten variable/struct names according xilpm coding style
     - Add comments at a few applicable places in header
     - Fix various formatting issues
     - Use of memset() in XPmProt_Init() for initializing apertures for
     better readability
    
    Signed-off-by: Izhar Ameer Shaikh <izhar.ameer.shaikh@xilinx.com>
    
    Acked-for-series: Ravi Patel <ravi.patel@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_prot.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_prot.h
@@ -14,40 +14,37 @@ extern "C" {
 #endif
 
 /* Protection node states */
-typedef enum {
+enum XPm_ProtState {
 	XPM_PROT_DISABLED,
 	XPM_PROT_ENABLED,
-} XPm_ProtState;
-
-typedef struct XPm_Prot XPm_Prot;
-typedef struct XPm_ProtMpu XPm_ProtMpu;
-typedef struct XPm_ProtPpu XPm_ProtPpu;
-
-/**
- * The processor core class.  This is the base class for all processor cores.
- */
-struct XPm_Prot {
-	XPm_Node Node; /**< Node: Base class */
 };
 
-typedef struct Aperture {
-	u32 NumSupported;	/**< Number of Supported Apertures */
-	u32 StartAddress;	/**< Aperture Base Address */
-	u32 EndAddress;		/**< Aperture End Address */
-}Aperture;
+typedef struct XPm_Node XPm_Prot;
+typedef struct XPm_ProtMpu XPm_ProtMpu;
+typedef struct XPm_ProtPpu XPm_ProtPpu;
+typedef struct XPm_PpuAperture XPm_PpuAperture;
 
+/* Aperture information of an XPPU instance */
+struct XPm_PpuAperture {
+	u32 Total;		/**< Number of Supported Apertures */
+	u32 Start;		/**< Aperture Base Address */
+	u32 End;		/**< Aperture End Address */
+};
+
+/* Protection - XPPU Class */
 struct XPm_ProtPpu {
-	XPm_Prot ProtNode;	/**< Node: Base Class */
-	Aperture Aperture_64k;	/**< Aperture 64k */
-	Aperture Aperture_1m;	/**< Aperture 1m */
-	Aperture Aperture_512m;	/**< Aperture 512m */
+	XPm_Prot Node;		/**< Node: Base Class */
+	XPm_PpuAperture A64k;	/**< Aperture 64k */
+	XPm_PpuAperture A1m;	/**< Aperture 1m */
+	XPm_PpuAperture A512m;	/**< Aperture 512m */
 	u32 AperPermInitMask;	/**< Default aperture permission mask */
 	u8 MIDParityEn;		/**< Parity error checking status for Master IDs */
 	u8 AperParityEn;	/**< Parity error checking status for Aperture entries */
 };
 
+/* Protection - XMPU Class */
 struct XPm_ProtMpu {
-	XPm_Prot ProtNode;	/**< Node: Base Class */
+	XPm_Prot Node;		/**< Node: Base Class */
 	u8 AlignCfg;		/**< Region alignment: 4k or 1m aligned */
 };
 
