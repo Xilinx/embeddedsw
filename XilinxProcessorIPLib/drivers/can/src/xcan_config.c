3b1955c89be4ebba72d7eaaba091760ea772d543
Srinivas Neeli <srinivas.neeli@xilinx.com>
2021-02-12 10:26:44 +0530

    can: Update copyright year and addtogroup version number
    Updated copyright year to 2021 and addtogroup version number to 3_6.
    
    Signed-off-by: Srinivas Neeli <srinivas.neeli@xilinx.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@xilinx.com>

--- XilinxProcessorIPLib/drivers/can/src/xcan_config.c
+++ XilinxProcessorIPLib/drivers/can/src/xcan_config.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2005 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2005 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xcan_config.c
-* @addtogroup can_v3_5
+* @addtogroup can_v3_6
 * @{
 *
 * Functions in this file are CAN Configuration Register access related.
