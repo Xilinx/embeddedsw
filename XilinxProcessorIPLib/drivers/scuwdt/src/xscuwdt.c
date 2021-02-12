fc1474d9c697695016e32a50bb5cf3af85484c6d
Srinivas Neeli <srinivas.neeli@xilinx.com>
2021-02-12 10:26:12 +0530

    scuwdt: Update copyright year and addtogroup version number
    updated copyright year to 2021 and addtogroup version number to 2_4.
    
    Signed-off-by: Srinivas Neeli <srinivas.neeli@xilinx.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@xilinx.com>

--- XilinxProcessorIPLib/drivers/scuwdt/src/xscuwdt.c
+++ XilinxProcessorIPLib/drivers/scuwdt/src/xscuwdt.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2010 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xscuwdt.c
-* @addtogroup scuwdt_v2_3
+* @addtogroup scuwdt_v2_4
 * @{
 *
 * Contains the implementation of interface functions of the XScuWdt driver.
--- XilinxProcessorIPLib/drivers/scuwdt/src/xscuwdt_hw.h
+++ XilinxProcessorIPLib/drivers/scuwdt/src/xscuwdt_hw.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2010 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xscuwdt_hw.h
-* @addtogroup scuwdt_v2_3
+* @addtogroup scuwdt_v2_4
 * @{
 *
 * This file contains the hardware interface to the Xilinx SCU private Watch Dog
