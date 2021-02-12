4566c642ba71094d1cb28d3c96d9030c2540b5db
Srinivas Neeli <srinivas.neeli@xilinx.com>
2021-02-12 10:26:19 +0530

    uartlite: Update copyright year and addtogroup version number
    updated copyright year to 2021 and addtogroup version number to 3_6.
    
    Signed-off-by: Srinivas Neeli <srinivas.neeli@xilinx.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@xilinx.com>

--- XilinxProcessorIPLib/drivers/uartlite/src/xuartlite_intr.c
+++ XilinxProcessorIPLib/drivers/uartlite/src/xuartlite_intr.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2002 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2002 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xuartlite_intr.c
-* @addtogroup uartlite_v3_5
+* @addtogroup uartlite_v3_6
 * @{
 *
 * This file contains interrupt-related functions for the UART Lite component
--- XilinxProcessorIPLib/drivers/uartlite/src/xuartlite_l.c
+++ XilinxProcessorIPLib/drivers/uartlite/src/xuartlite_l.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2002 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2002 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xuartlite_l.c
-* @addtogroup uartlite_v3_5
+* @addtogroup uartlite_v3_6
 * @{
 *
 * This file contains low-level driver functions that can be used to access the
