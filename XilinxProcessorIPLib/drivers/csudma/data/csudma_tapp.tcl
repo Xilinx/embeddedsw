743238e7b0b865254e897eefc2c24077c23571ff
Shravya <shravya.kumbham@xilinx.com>
2021-02-18 05:38:40 -0700

    csudma: Remove double quote to fix the doxygen warning
    Remove the extra double quote at the closing brace in
    csudma_tapp.tcl to fix the below doxygen warning.
    
    Doxygen Warning : Error 1301 embeddedsw/XilinxProcessorIPLib/drivers/
    csudma/data/csudma_tapp.tcl() at line 130! expected word separator:
    
    Signed-off-by: Shravya Kumbham <shravya.kumbham@xilinx.com>
    Acked-by: Harini Katakam<harini.katakam@xilinx.com>

--- XilinxProcessorIPLib/drivers/csudma/data/csudma_tapp.tcl
+++ XilinxProcessorIPLib/drivers/csudma/data/csudma_tapp.tcl
@@ -127,7 +127,7 @@ proc gen_testfunc_call {swproj mhsinst} {
       Status = XCsuDma_IntrExample(&${intcvar}, &${ipname}, \\
                                  ${deviceid}, \\
                                  ${intr_id});
-   }"
+   }
 
    }
 } else {
