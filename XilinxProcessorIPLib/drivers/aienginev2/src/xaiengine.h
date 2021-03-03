dd72e5bf497fc099fe910b0c81d3b6bfe743f6e8
Dishita Vashi <dishita.vashi@xilinx.com>
2021-03-03 08:51:13 -0800

    aienginev2: src: Add rsc header to the main header
    Add rsc header to the main header file.
    
    Signed-off-by: Dishita Vashi <dishita.vashi@xilinx.com>
    
    Acked-for-series: Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>

--- XilinxProcessorIPLib/drivers/aienginev2/src/xaiengine.h
+++ XilinxProcessorIPLib/drivers/aienginev2/src/xaiengine.h
@@ -19,6 +19,7 @@ extern "C" {
 #include <xaiengine/xaie_perfcnt.h>
 #include <xaiengine/xaie_plif.h>
 #include <xaiengine/xaie_reset.h>
+#include <xaiengine/xaie_rsc.h>
 #include <xaiengine/xaie_ss.h>
 #include <xaiengine/xaie_timer.h>
 #include <xaiengine/xaie_trace.h>
