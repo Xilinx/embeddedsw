425a6c6fedfb686fcd46cb033cee7d8b850cfcd1
Mubin Sayyed <mubin.usman.sayyed@xilinx.com>
2021-03-12 16:09:45 +0530

    intc: Update copyright year and doxygen tag
    Copyright year and doxygen tag was not updated in few
    files.
    
    Signed-off-by: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

--- XilinxProcessorIPLib/drivers/intc/data/intc_tapp.tcl
+++ XilinxProcessorIPLib/drivers/intc/data/intc_tapp.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-# Copyright (C) 2004 - 2020 Xilinx, Inc.  All rights reserved.
+# Copyright (C) 2004 - 2021 Xilinx, Inc.  All rights reserved.
 # SPDX-License-Identifier: MIT
 #
 #
