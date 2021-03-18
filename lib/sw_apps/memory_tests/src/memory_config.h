6affbd4bc4ce9aa7ac1bc50a64fd2fa571903fc5
Mubin Sayyed <mubin.usman.sayyed@xilinx.com>
2021-03-18 12:22:57 +0530

    sw_apps: memory_tests: Support regions mapped at extnded addresses for 32 bit microblaze
    Updated application to support memory regions mapped
    at extended addresses (addresses > 4 GB), in case of
    32 bit Microblaze processor.
    
    Also replaced data types used in application with
    Xilinx standard data types.
    
    Signed-off-by: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

--- lib/sw_apps/memory_tests/src/memory_config.h
+++ lib/sw_apps/memory_tests/src/memory_config.h
@@ -6,13 +6,16 @@
 #ifndef __MEMORY_CONFIG_H_
 #define __MEMORY_CONFIG_H_
 
+#include "xil_types.h"
 struct memory_range_s {
-    char *name;
-    char *ip;
-    unsigned long base;
-    unsigned long size;
+    char8 *name;
+    char8 *ip;
+    u64 base;
+    u32 size;
 };
 
+#define UPPER_4BYTES_MASK	0xFFFFFFFF00000000LL
+#define LOWER_4BYTES_MASK	0xFFFFFFFFUL
 /* generated memory ranges defined in memory_ranges_g.c */
 extern struct memory_range_s memory_ranges[];
 extern int n_memory_ranges;
