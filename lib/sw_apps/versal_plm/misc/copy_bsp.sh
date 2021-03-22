464e450135951b5f97fd9cec06a37c27efd36904
Tarun DTS <tarun.dts@xilinx.com>
2021-03-22 18:57:03 +0530

    sw_apps: versal_plm: Fix PLM versionless build
    This patch fixes versionless PLM build by pointing correct xilsem files to bsp
    during build.
    
    Signed-off-by: Tarun DTS <tarun.dts@xilinx.com>
    Acked-by: Vikram Sreenivasa Batchali <b.vikram@xilinx.com>

--- lib/sw_apps/versal_plm/misc/copy_bsp.sh
+++ lib/sw_apps/versal_plm/misc/copy_bsp.sh
@@ -87,8 +87,9 @@ rm $BSP_DIR/libsrc/xilsecure/src/libxilsecure_*.a
 BSP_SEQUENTIAL_MAKEFILES="$BSP_SEQUENTIAL_MAKEFILES $BSP_DIR/libsrc/xilsecure/src/Makefile"
 
 mkdir -p $BSP_DIR/libsrc/xilsem
-cp -r $SERVICES_DIR/xilsem/src $BSP_DIR/libsrc/xilsem/
-cp -r $SERVICES_DIR/xilsem/src/*.h $BSP_DIR/include/
+cp -r $SERVICES_DIR/xilsem/src/ $BSP_DIR/libsrc/xilsem/
+cp -r $SERVICES_DIR/xilsem/src/server/*.h $BSP_DIR/include/
+cp -r $SERVICES_DIR/xilsem/src/server/libxilsem.a $BSP_DIR/lib
 
 # copy bsp standalone code
 cp -r $STANDALONE_DIR/common/*  $BSP_DIR/libsrc/standalone/src/
