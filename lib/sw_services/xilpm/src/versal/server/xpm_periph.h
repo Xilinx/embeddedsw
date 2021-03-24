9c87887b2c1e9b7b03f27829a834a44ac69658bf
Pallav Joshi <pallav.joshi@xilinx.com>
2021-03-24 13:31:32 -0700

    xilpm: versal: server: Add FSM for HealthyBoot Mon
    Add FSM and handler for Health Boot monitor.
    Also Add the init API to override the fsm for the device. This will
    be called while the device node is added.
    
    Signed-off-by: Pallav Joshi <pallav.joshi@xilinx.com>
    
    Reviewed-for-series: Izhar Ameer Shaikh <izhar.ameer.shaikh@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_periph.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_periph.h
@@ -37,6 +37,7 @@ XStatus XPmPeriph_Init(XPm_Periph *Periph, u32 Id, u32 BaseAddress,
 		       XPm_ResetNode *Reset, u32 GicProxyMask,
 		       u32 GicProxyGroup);
 XStatus XPmVirtDev_DeviceInit(XPm_Device *Device, u32 Id, XPm_Power *Power);
+XStatus XPmHbMonDev_Init(XPm_Device *Device, u32 Id, XPm_Power *Power);
 
 #ifdef __cplusplus
 }
