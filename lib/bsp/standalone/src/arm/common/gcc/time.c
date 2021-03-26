1cd21a371cef347dfa7867086088843c8e90b73d
Mubin Sayyed <mubin.usman.sayyed@xilinx.com>
2021-03-26 10:56:08 +0530

    BSP: arm: Add weak implementation for clock function
    clock is C library function used to get time elapsed by
    processor.
    
    As timekeeping is tightly coupled with system, we are not
    implementing this function, default implementation would
    always return 0. However, users can overrride it with
    their own implementation based on the system.
    
    Also updated the existing implementaion for IAR compiler,
    to align with other compilers.
    
    Signed-off-by: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>
    Acked-by: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

---
+++ lib/bsp/standalone/src/arm/common/gcc/time.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+* Copyright (c) 2009 - 2021 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+#include <time.h>
+
+#ifdef __cplusplus
+extern "C" {
+	__attribute__((weak)) clock_t clock(void);
+}
+#endif
+/*
+ * clock -- It supposed to return processor time. We are not implementing
+ *          this function, as timekeeping is tightly coupled with system, hence
+ *          always returning 0. Users can override this with their system
+ *          specific implementation.
+ *
+ */
+__attribute__((weak)) clock_t clock(void)
+{
+    return (0);
+}
