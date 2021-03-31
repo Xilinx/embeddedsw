ac1c9b895edabb87464a65aa3c37f2cc67b49237
Sergei Korneichuk <sergei.korneichuk@xilinx.com>
2021-03-31 04:15:46 -0700

    openamp: apps: zynqmp_r5: set up ISR via FreeRTOS API
    Use FreeRTOS API to install and enable the ISR for the interrupt
    controller instance initialized by the FreeRTOS porting layer.
    Remove re-initialization of the interrupt controller because it
    disables the FreeRTOS tick handler and the scheduler:
    https://github.com/Xilinx/embeddedsw/issues/75
    
    Signed-off-by: Sergei Korneichuk <sergei.korneichuk@xilinx.com>
    
    Acked-for-series: Ed Mooring <ed.mooring@xilinx.com>

--- lib/sw_apps/openamp_matrix_multiply/src/system/freertos/machine/zynqmp_r5/helper.c
+++ lib/sw_apps/openamp_matrix_multiply/src/system/freertos/machine/zynqmp_r5/helper.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2014, Mentor Graphics Corporation
  * All rights reserved.
  *
- * Copyright (c) 2015 Xilinx, Inc. All rights reserved.
+ * Copyright (c) 2021 Xilinx, Inc. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -10,74 +10,27 @@
 #include "xparameters.h"
 #include "xil_exception.h"
 #include "xil_printf.h"
-#include "xscugic.h"
+#include "FreeRTOS.h"
 #include "xil_cache.h"
 #include <metal/sys.h>
 #include <metal/irq.h>
 #include "platform_info.h"
 
-#define INTC_DEVICE_ID		XPAR_SCUGIC_0_DEVICE_ID
-
-static XScuGic xInterruptController;
-
 /* Interrupt Controller setup */
 static int app_gic_initialize(void)
 {
-	uint32_t status;
-	XScuGic_Config *int_ctrl_config; /* interrupt controller configuration params */
-	uint32_t int_id;
-	uint32_t mask_cpu_id = ((u32)0x1 << XPAR_CPU_ID);
-	uint32_t target_cpu;
-
-	mask_cpu_id |= mask_cpu_id << 8U;
-	mask_cpu_id |= mask_cpu_id << 16U;
-
-	Xil_ExceptionDisable();
-
 	/*
-	 * Initialize the interrupt controller driver
+	 * Register the ISR with the interrupt controller instance
+	 * initialized by porting layer.
 	 */
-	int_ctrl_config = XScuGic_LookupConfig(INTC_DEVICE_ID);
-	if (NULL == int_ctrl_config) {
-		return XST_FAILURE;
-	}
-
-	status = XScuGic_CfgInitialize(&xInterruptController, int_ctrl_config,
-				       int_ctrl_config->CpuBaseAddress);
-	if (status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
-
-	/* Only associate interrupt needed to this CPU */
-	for (int_id = 32U; int_id<XSCUGIC_MAX_NUM_INTR_INPUTS;int_id=int_id+4U) {
-		target_cpu = XScuGic_DistReadReg(&xInterruptController,
-						XSCUGIC_SPI_TARGET_OFFSET_CALC(int_id));
-		/* Remove current CPU from interrupt target register */
-		target_cpu &= ~mask_cpu_id;
-		XScuGic_DistWriteReg(&xInterruptController,
-					XSCUGIC_SPI_TARGET_OFFSET_CALC(int_id), target_cpu);
-	}
-	XScuGic_InterruptMaptoCpu(&xInterruptController, XPAR_CPU_ID, IPI_IRQ_VECT_ID);
-
+	xPortInstallInterruptHandler(IPI_IRQ_VECT_ID,
+				     (Xil_ExceptionHandler)metal_xlnx_irq_isr,
+				     (void *)IPI_IRQ_VECT_ID);
 	/*
-	 * Register the interrupt handler to the hardware interrupt handling
-	 * logic in the ARM processor.
+	 * Enable interrupt for IPI_IRQ_VECT_ID.
+	 * This calls XScuGic_InterruptMaptoCpu() via XScuGic_Enable()
 	 */
-	Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_IRQ_INT,
-			(Xil_ExceptionHandler)XScuGic_InterruptHandler,
-			&xInterruptController);
-
-	/* Disable the interrupt before enabling exception to avoid interrupts
-	 * received before exception is enabled.
-	 */
-	XScuGic_Disable(&xInterruptController, IPI_IRQ_VECT_ID);
-
-	Xil_ExceptionEnable();
-
-	/* Connect Interrupt ID with ISR */
-	XScuGic_Connect(&xInterruptController, IPI_IRQ_VECT_ID,
-			(Xil_ExceptionHandler)metal_xlnx_irq_isr,
-			(void *)IPI_IRQ_VECT_ID);
+	vPortEnableInterrupt(IPI_IRQ_VECT_ID);
 
 	return 0;
 }
