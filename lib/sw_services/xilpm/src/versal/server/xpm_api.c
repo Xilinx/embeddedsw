58423f13ff69af524e3b2882b96ee36a676ea78c
Ben Levinsky <ben.levinsky@xilinx.com>
2021-04-01 08:28:19 -0700

    xilpm: versal: server: Check IPI Request type for subsystem permissions
    For subsystem-subsystem permissions, use IPI request type information when
    checking if a subsystem can enact a secure operation
    
    Signed-off-by: Ben Levinsky <ben.levinsky@xilinx.com>
    
    Reviewed-for-series: Izhar Ameer Shaikh <izhar.ameer.shaikh@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_api.c
+++ lib/sw_services/xilpm/src/versal/server/xpm_api.c
@@ -1455,7 +1455,6 @@ XStatus XPm_RequestSuspend(const u32 SubsystemId, const u32 TargetSubsystemId,
 	u32 Payload[5] = {0};
 	/* Warning Fix */
 	(void) (Ack);
-	(void) (CmdType);
 
 	IpiMask = XPmSubsystem_GetIPIMask(TargetSubsystemId);
 	if (0U == IpiMask) {
@@ -1471,7 +1470,8 @@ XStatus XPm_RequestSuspend(const u32 SubsystemId, const u32 TargetSubsystemId,
 	}
 
 	Status = XPmSubsystem_IsOperationAllowed(SubsystemId, TargetSubsystemId,
-						 SUB_PERM_SUSPEND_MASK);
+						 SUB_PERM_SUSPEND_MASK,
+						 CmdType);
 	if (XST_SUCCESS != Status) {
 		Status = XPM_PM_NO_ACCESS;
 		PmErr("Subsystem %x not allowed to suspend Target %x\n", SubsystemId, TargetSubsystemId);
@@ -1583,10 +1583,9 @@ XStatus XPm_RequestWakeUp(u32 SubsystemId, const u32 DeviceId,
 
 	/* Warning Fix */
 	(void) (Ack);
-	(void) CmdType;
 
 	/*Validate access first */
-	Status = XPm_IsWakeAllowed(SubsystemId, DeviceId);
+	Status = XPm_IsWakeAllowed(SubsystemId, DeviceId, CmdType);
 	if (XST_SUCCESS != Status) {
 		Status = XPM_PM_NO_ACCESS;
 		goto done;
@@ -1689,7 +1688,7 @@ XStatus XPm_ForcePowerdown(u32 SubsystemId, const u32 NodeId, const u32 Ack,
 	(void) (Ack);
 
 	/*Validate access first */
-	Status = XPm_IsForcePowerDownAllowed(SubsystemId, NodeId);
+	Status = XPm_IsForcePowerDownAllowed(SubsystemId, NodeId, CmdType);
 	if (XST_SUCCESS != Status) {
 		goto done;
 	}
