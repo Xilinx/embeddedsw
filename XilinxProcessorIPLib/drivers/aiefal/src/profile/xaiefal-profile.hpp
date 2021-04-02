626046e9d453321ee892b808cbcbd350c53a41f5
Wendy Liang <wendy.liang@xilinx.com>
2021-04-02 11:08:15 -0700

    aiefal: src: update profile to use group event reference
    Update the profiling class to use group event reference.
    
    Signed-off-by: Wendy Liang <wendy.liang@xilinx.com>
    
    Acked-for-series: Pramod Peethambaran <pramodp@xilinx.com>

--- XilinxProcessorIPLib/drivers/aiefal/src/profile/xaiefal-profile.hpp
+++ XilinxProcessorIPLib/drivers/aiefal/src/profile/xaiefal-profile.hpp
@@ -5,6 +5,7 @@
 #include <functional>
 #include <vector>
 #include <xaiengine.h>
+#include <xaiefal/rsc/xaiefal-groupevent.hpp>
 #include <xaiefal/rsc/xaiefal-perf.hpp>
 
 #pragma once
@@ -27,22 +28,67 @@ namespace xaiefal {
 	public:
 		XAieStallCycles() = delete;
 		XAieStallCycles(std::shared_ptr<XAieDevHandle> DevHd,
-			XAie_LocType L, bool CrossM = false):
-			XAiePerfCounter(DevHd, L, XAIE_CORE_MOD, CrossM) {
+			XAie_LocType L,
+			std::shared_ptr<XAieGroupEventHandle> StallG,
+			std::shared_ptr<XAieGroupEventHandle> FlowG,
+			bool CrossM = false):
+			XAiePerfCounter(DevHd, L, XAIE_CORE_MOD, CrossM),
+			StallGroupEvent(StallG),
+			FlowGroupEvent(FlowG) {
 			initialize(XAIE_CORE_MOD, XAIE_EVENT_GROUP_CORE_STALL_CORE,
 				XAIE_CORE_MOD, XAIE_EVENT_GROUP_CORE_PROGRAM_FLOW_CORE);
+			StallGroupEvent->setGroupEvents(0x19F);
 		}
 		XAieStallCycles(XAieDev &Dev,
-			XAie_LocType L, bool CrossM = false):
-			XAieStallCycles(Dev.getDevHandle(), L, CrossM) {}
-		AieRC _startPrepend() {
-			XAie_Write32(dev(),
-				     _XAie_GetTileAddr(dev(),
-						       Loc.Row,
-						       Loc.Col) + 0x00034508,
-				     0x19F);
-			return XAIE_OK;
+			XAie_LocType L,
+			bool CrossM = false):
+			XAiePerfCounter(Dev, L, XAIE_CORE_MOD, CrossM) {
+			initialize(XAIE_CORE_MOD, XAIE_EVENT_GROUP_CORE_STALL_CORE,
+				XAIE_CORE_MOD, XAIE_EVENT_GROUP_CORE_PROGRAM_FLOW_CORE);
+			StallGroupEvent = Dev.tile(L).module(XAIE_CORE_MOD).groupEvent(XAIE_EVENT_GROUP_CORE_STALL_CORE);
+			FlowGroupEvent = Dev.tile(L).module(XAIE_CORE_MOD).groupEvent(XAIE_EVENT_GROUP_CORE_PROGRAM_FLOW_CORE);
+			StallGroupEvent->setGroupEvents(0x19F);
+			}
+	protected:
+		AieRC _reserveAppend() {
+			AieRC RC;
+
+			RC = StallGroupEvent->reserve();
+			if (RC == XAIE_OK) {
+				RC = FlowGroupEvent->reserve();
+			}
+			return RC;
 		}
+		AieRC _releaseppend() {
+			AieRC RC;
+
+			RC = StallGroupEvent->release();
+			if (RC == XAIE_OK) {
+				RC = FlowGroupEvent->release();
+			}
+			return RC;
+		}
+		AieRC _startPrepend() {
+			AieRC RC;
+
+			RC = StallGroupEvent->start();
+			if (RC == XAIE_OK) {
+				RC = FlowGroupEvent->start();
+			}
+			return RC;
+		}
+		AieRC _stopAppend() {
+			AieRC RC;
+
+			RC = StallGroupEvent->stop();
+			if (RC == XAIE_OK) {
+				RC = FlowGroupEvent->stop();
+			}
+			return RC;
+		}
+	private:
+		std::shared_ptr<XAieGroupEventHandle> StallGroupEvent;
+		std::shared_ptr<XAieGroupEventHandle> FlowGroupEvent;
 	};
 
 }
