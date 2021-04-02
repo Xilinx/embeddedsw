a62757cb01a9b6225414e170039cd92b113a03ff
Jyotheeswar Reddy <jyotheeswar.reddy.mutthareddyvari@xilinx.com>
2021-04-01 21:39:01 -0700

    sw_apps: zynqmp_pmufw: Add external WDT module
    Add external WDT servicing module. This module can be enabled by adding
    build flag ENABLE_MOD_EXTWDT. The default servicing interval is 1140 ms,
    and this can be changed by passing the flag EXTWDT_INTERVAL_MS set to
    desired interval in milliseconds.
    
    Signed-off-by: Jyotheeswar Reddy Mutthareddyvari <jyotheeswar.reddy.mutthareddyvari@xilinx.com>
    
    Acked-for-series: Rajan Vaja <rajan.vaja@xilinx.com>

---
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_mod_extwdt.h
@@ -0,0 +1,18 @@
+/******************************************************************************
+* Copyright (c) 2021 Xilinx, Inc. All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+#ifndef XPFW_MOD_EXTWDT_H_
+#define XPFW_MOD_EXTWDT_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ModExtWdtInit(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* XPFW_MOD_EXTWDT_H_ */
