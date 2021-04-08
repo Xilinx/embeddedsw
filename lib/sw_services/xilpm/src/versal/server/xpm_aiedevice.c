c595647d4197452e6fbd4c5386bd6eae90bf9d5c
Amit Sunil Dhamne <amit.sunil.dhamne@xilinx.com>
2021-04-08 13:35:17 -0700

    xilpm: versal: server: Add functional support for AIE Device InitNode Ops
    Add functional support Init Node Start for AIE Device. AIE Device Init Node
    Finish is a no-op and will returns success always.
    
    Signed-off-by: Amit Sunil Dhamne <amit.sunil.dhamne@xilinx.com>
    
    Reviewed-for-series: Izhar Ameer Shaikh <izhar.ameer.shaikh@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_aiedevice.c
+++ lib/sw_services/xilpm/src/versal/server/xpm_aiedevice.c
@@ -4,6 +4,8 @@
 ******************************************************************************/
 
 #include "xpm_aiedevice.h"
+#include "xpm_power.h"
+#include "xpm_debug.h"
 
 /****************************************************************************/
 /**
@@ -13,17 +15,49 @@
  * @param  Args: Arguments for AIE
  * @param  NumArgs: Number of arguments for AIE
  *
- * @return XST_SUCCESS
+ * @return XST_SUCCESS if successful else XST_FAILURE or error code
  *
  * @note Arguments consist of Power Domain Node Id that AIE depends on
  *
  ****************************************************************************/
 static XStatus AieInitStart(XPm_AieDevice *AieDevice, u32 *Args, u32 NumArgs)
 {
-	(void)AieDevice;
-	(void)Args;
-	(void)NumArgs;
-	return XST_SUCCESS;
+	XStatus Status = XST_FAILURE;
+	u16 DbgErr = XPM_INT_ERR_UNDEFINED;
+	const XPm_PlDevice *Parent;
+	XPm_Device *Device;
+
+	if ((1U != NumArgs) || (PM_POWER_ME != Args[0])) {
+		DbgErr = XPM_INT_ERR_INVALID_ARGS;
+		goto done;
+	}
+
+	/*
+	 * Check the implicit device, PM_DEV_AIE is running. Device represents the
+	 * AIE Device Array as whole (includes resets, clocks, power)
+	 */
+	Device = XPmDevice_GetById(PM_DEV_AIE);
+	if ((NULL == Device) ||
+	    ((u32)XPM_DEVSTATE_RUNNING != Device->Node.State)) {
+		DbgErr = XPM_INT_ERR_DEV_AIE;
+		goto done;
+	}
+
+	/*
+	 * AIE CDO cannot run if parent is assigned or in unused or initializing
+	 * state
+	 */
+	Parent = AieDevice->Parent;
+	if ((NULL == Parent)||
+	    ((u8)XPM_DEVSTATE_RUNNING != Parent->Device.Node.State)) {
+		DbgErr = XPM_INT_ERR_INVALID_AIE_PARENT;
+		goto done;
+	}
+
+	Status = XST_SUCCESS;
+done:
+	XPm_PrintDbgErr(Status, DbgErr);
+	return Status;
 }
 
 /****************************************************************************/
