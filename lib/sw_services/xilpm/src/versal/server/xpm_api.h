c02ca2d188d58797acc6311720af2de90291b58e
Siva Addepalli <sivaprasad.addepalli@xilinx.com>
2021-04-14 01:24:57 +0530

    Revert "xilpm: versal: server: pass IpiReqType to XPm_RequestSuspend"
    This reverts commit 33c4da76738c67667e0324b2f04457fea40a94e5.

--- lib/sw_services/xilpm/src/versal/server/xpm_api.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_api.h
@@ -129,8 +129,7 @@ XStatus XPm_SelfSuspend(const u32 SubsystemId, const u32 DeviceId,
 XStatus XPm_AbortSuspend(const u32 SubsystemId, const u32 Reason,
 			 const u32 DeviceId);
 XStatus XPm_RequestSuspend(const u32 SubsystemId, const u32 TargetSubsystemId,
-			   const u32 Ack, const u32 Latency, const u32 State,
-			   const u32 CmdType);
+			   const u32 Ack, const u32 Latency, const u32 State);
 XStatus XPm_InitNode(u32 NodeId, u32 Function, u32 *Args, u32 NumArgs);
 int XPm_FeatureCheck(const u32 ApiId, u32 *const Version);
 XStatus XPm_IsoControl(u32 NodeId, u32 Enable);
