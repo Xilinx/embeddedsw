cd0828d14248164131f9e208795cda55a6d207bf
Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>
2021-04-15 09:24:14 +0200

    apps: add libraries version information in echo example
    Add library information used for the demo. This used to provide
    an examples of the new version API use.
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>
    Signed-off-by: Sergei Korneichuk <sergei.korneichuk@xilinx.com>

--- lib/sw_apps/openamp_echo_test/src/system/generic/rpmsg-echo.c
+++ lib/sw_apps/openamp_echo_test/src/system/generic/rpmsg-echo.c
@@ -8,7 +8,9 @@ This application echoes back data that was sent to it by the master core. */
 
 #include "xil_printf.h"
 #include <openamp/open_amp.h>
+#include <openamp/version.h>
 #include <metal/alloc.h>
+#include <metal/version.h>
 #include "platform_info.h"
 #include "rpmsg-echo.h"
 
@@ -97,6 +99,16 @@ int main(int argc, char *argv[])
 	int ret;
 
 	/* can't use ML_INFO, metal_log setup is in init_system */
+	LPRINTF("openamp lib version: %s (", openamp_version());
+	LPRINTF("Major: %d, ", openamp_version_major());
+	LPRINTF("Minor: %d, ", openamp_version_minor());
+	LPRINTF("Patch: %d)\r\n", openamp_version_patch());
+
+	LPRINTF("libmetal lib version: %s (", metal_ver());
+	LPRINTF("Major: %d, ", metal_ver_major());
+	LPRINTF("Minor: %d, ", metal_ver_minor());
+	LPRINTF("Patch: %d)\r\n", metal_ver_patch());
+
 	LPRINTF("Starting application...\n");
 
 	/* Initialize platform */
