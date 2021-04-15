f467ce341d0afc97c24310377572c0292024e10e
nishantd <nishant.dhonde@xilinx.com>
2021-04-15 19:08:20 +0530

    dp14txss:tx_only: Moved global variables from .h to .c files. Updated the application for TX cts tests.
    Moved global variables from .h to .c files due to GCC compiler errors. tx_is_reconnected=0 and max_new_cap=8.1 set for TX cts test.
    
    Signed-off-by: nishantd <nishant.dhonde@xilinx.com>
    
    Acked-for-series: Kapil Usgaonkar<kapil.usgaonkar@xilinx.com>

--- XilinxProcessorIPLib/drivers/dp14txss/examples/src/tx_only/clk_set.h
+++ XilinxProcessorIPLib/drivers/dp14txss/examples/src/tx_only/clk_set.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2010 - 20209Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2020 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
