04d346e19ef567feffb53a4d745d08676e4f7186
Srinivas Neeli <srinivas.neeli@xilinx.com>
2021-04-26 11:11:30 +0530

    rtcpsu: Update copyright year and addtogroup version number
    updated copyright year to 2021 and addtogroup version number to 1_11.
    
    Signed-off-by: Srinivas Neeli <srinivas.neeli@xilinx.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@xilinx.com>

--- XilinxProcessorIPLib/drivers/rtcpsu/src/xrtcpsu_selftest.c
+++ XilinxProcessorIPLib/drivers/rtcpsu/src/xrtcpsu_selftest.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2015 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2015 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xrtcpsu_selftest.c
-* @addtogroup rtcpsu_v1_10
+* @addtogroup rtcpsu_v1_11
 * @{
 *
 * This file contains the self-test functions for the XRtcPsu driver.
