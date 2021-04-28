4b681e02dd0f610ff92c299df7a219be68bb9a54
Kapil Usgaonkar <kapil.usgaonkar@xilinx.com>
2021-04-28 18:35:02 +0530

    v_hdmiphy1: updated the revision to 2.2
    This patch updates the revision and addtogroup to 2_2
    
    Signed-off-by: Kapil Usgaonkar <kapil.usgaonkar@xilinx.com>
    
    Acked-for-series: Pankaj Kumbhare <pankaj.kumbhare@xilinx.com>

--- XilinxProcessorIPLib/drivers/v_hdmiphy1/src/xhdmiphy1.h
+++ XilinxProcessorIPLib/drivers/v_hdmiphy1/src/xhdmiphy1.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (C) 2015 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2015 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 *******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
  *
  * @file xhdmiphy1.h
- * @addtogroup xhdmiphy1_v2_1
+ * @addtogroup xhdmiphy1_v2_2
  * @{
  * @details
  * This is main header file of the Xilinx HDMI PHY Controller driver
