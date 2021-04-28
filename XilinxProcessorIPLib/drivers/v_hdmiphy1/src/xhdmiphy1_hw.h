296e532190d406e8e51e05d5fbcf169a5b531980
Kapil Usgaonkar <kapil.usgaonkar@xilinx.com>
2021-04-28 18:35:02 +0530

    v_hdmiphy1: updated the revision to 2.2
    This patch updates the revision and addtogroup to 2_2
    
    Signed-off-by: Kapil Usgaonkar <kapil.usgaonkar@xilinx.com>
    
    Acked-for-series: Pankaj Kumbhare <pankaj.kumbhare@xilinx.com>

--- XilinxProcessorIPLib/drivers/v_hdmiphy1/src/xhdmiphy1_hw.h
+++ XilinxProcessorIPLib/drivers/v_hdmiphy1/src/xhdmiphy1_hw.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (C) 2015 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2015 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 *******************************************************************************/
 
@@ -24,7 +24,7 @@
  * 1.0   gm   10/12/18 Initial release.
  * </pre>
  *
- * @addtogroup xhdmiphy1_v2_1
+ * @addtogroup xhdmiphy1_v2_2
  * @{
 *******************************************************************************/
 
