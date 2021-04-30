422aa8a2bc384377c6902b8e45458a5d242df797
Kalyani Akula <kalyani.akula@xilinx.com>
2021-04-30 14:27:06 +0530

    XilSecure: Add XilSecure Server side standalone examples
    This patch adds Server side examples support for XilSecure
    library.
    
    Signed-off-by: Kalyani Akula <kalyani.akula@xilinx.com>
    
    Acked-for-series: Mohan Marutirao Dhanawade <mohan.dhanawade@xilinx.com>

---
+++ lib/sw_services/xilsecure/examples/xilsecure_versal_aes_server_example.c
@@ -0,0 +1,312 @@
+/******************************************************************************
+* Copyright (c) 2021 Xilinx, Inc. All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+
+/*****************************************************************************/
+/**
+*
+* @file	xilsecure_versal_aes_server_example.c
+* @addtogroup xsecure_versal_aes_example XilSecure AES API Example Generic Usage
+* @{
+*
+* @note
+* This example illustrates the usage of Versal AES Server APIs,
+* by encrypting the data with provided key and IV and decrypts the encrypted data
+* and compares with original data and checks for GCM tag match.
+*
+* MODIFICATION HISTORY:
+* <pre>
+* Ver   Who    Date     Changes
+* ----- ------ -------- -------------------------------------------------
+* 1.0   kal    04/21/21 First Release
+*
+* </pre>
+******************************************************************************/
+
+/***************************** Include Files *********************************/
+
+#include "xparameters.h"
+#include "xsecure_aes.h"
+#include "xil_util.h"
+#include "xpmcdma.h"
+
+/************************** Constant Definitions *****************************/
+
+/* Harcoded KUP key for encryption of data */
+#define	XSECURE_AES_KEY	\
+	"F878B838D8589818E868A828C8488808F070B030D0509010E060A020C0408000"
+
+/* Hardcoded IV for encryption of data */
+#define	XSECURE_IV	"D2450E07EA5DE0426C0FA133"
+
+#define XSECURE_DATA	\
+	"1234567808F070B030D0509010E060A020C0408000A5DE08D85898A5A5FEDCA10134" \
+	"ABCDEF12345678900987654321123487654124456679874309713627463801AD1056"
+
+#define XSECURE_AAD			"67e21cf3cb29e0dcbc4d8b1d0cc5334b"
+
+#define XSECURE_DATA_SIZE		(68)
+#define XSECURE_DATA_SIZE_IN_BITS	(XSECURE_DATA_SIZE * 8U)
+#define XSECURE_IV_SIZE			(12)
+#define XSECURE_IV_SIZE_IN_BITS		(XSECURE_IV_SIZE * 8U)
+#define XSECURE_KEY_SIZE		(32)
+#define XSECURE_KEY_SIZE_IN_BITS	(XSECURE_KEY_SIZE * 8U)
+#define XSECURE_AAD_SIZE		(16)
+#define XSECURE_AAD_SIZE_IN_BITS	(XSECURE_AAD_SIZE * 8U)
+
+#define XSECURE_PMCDMA_DEVICEID		PMCDMA_0_DEVICE_ID
+
+/**************************** Type Definitions *******************************/
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+/************************** Function Prototypes ******************************/
+
+static int SecureAesExample(void);
+
+/************************** Variable Definitions *****************************/
+static u8 Iv[XSECURE_IV_SIZE];
+static u8 Key[XSECURE_KEY_SIZE];
+
+#if defined (__GNUC__)
+static u8 Data[XSECURE_DATA_SIZE]__attribute__ ((aligned (64)))
+				__attribute__ ((section (".data.Data")));
+static u8 DecData[XSECURE_DATA_SIZE]__attribute__ ((aligned (64)))
+				__attribute__ ((section (".data.DecData")));
+static u8 EncData[XSECURE_DATA_SIZE]__attribute__ ((aligned (64)))
+				__attribute__ ((section (".data.EncData")));
+static u8 GcmTag[XSECURE_SECURE_GCM_TAG_SIZE]__attribute__ ((aligned (64)))
+				__attribute__ ((section (".data.GcmTag")));
+static u8 Aad[XSECURE_AAD_SIZE]__attribute__ ((aligned (64)))
+				__attribute__ ((section (".data.Aad")));
+#elif defined (__ICCARM__)
+#pragma data_alignment = 64
+static u8 Data[XSECURE_DATA_SIZE];
+#pragma data_alignment = 64
+static u8 DecData[XSECURE_DATA_SIZE];
+#pragma data_alignment = 64
+static u8 EncData[XSECURE_DATA_SIZE ];
+#pragma data_alignment = 64
+static u8 GcmTag[XSECURE_SECURE_GCM_TAG_SIZE];
+#pragma data_alignment = 64
+static u8 Aad[XSECURE_AAD_SIZE];
+#endif
+
+/************************** Function Definitions ******************************/
+int main(void)
+{
+	int Status = XST_FAILURE;
+
+	Status = Xil_ConvertStringToHexBE(
+			(const char *) (XSECURE_AES_KEY),
+				Key, XSECURE_KEY_SIZE_IN_BITS);
+	if (Status != XST_SUCCESS) {
+		xil_printf(
+			"String Conversion error (KEY):%08x !!!\r\n", Status);
+		goto END;
+	}
+
+	Status = Xil_ConvertStringToHexBE(
+			(const char *) (XSECURE_IV),
+				Iv, XSECURE_IV_SIZE_IN_BITS);
+	if (Status != XST_SUCCESS) {
+		xil_printf(
+			"String Conversion error (IV):%08x !!!\r\n", Status);
+		goto END;
+	}
+
+	Status = Xil_ConvertStringToHexBE(
+			(const char *) (XSECURE_DATA),
+				Data, XSECURE_DATA_SIZE_IN_BITS);
+	if (Status != XST_SUCCESS) {
+		xil_printf(
+			"String Conversion error (Data):%08x !!!\r\n", Status);
+		goto END;
+	}
+
+	Status = Xil_ConvertStringToHexBE((const char *) (XSECURE_AAD), Aad,
+		XSECURE_AAD_SIZE_IN_BITS);
+	if (Status != XST_SUCCESS) {
+		xil_printf("String Conversion error (AAD):%08x !!!\r\n", Status);
+		goto END;
+	}
+
+	Xil_DCacheFlushRange((UINTPTR)Iv, XSECURE_IV_SIZE);
+	Xil_DCacheFlushRange((UINTPTR)Data, XSECURE_DATA_SIZE);
+	Xil_DCacheFlushRange((UINTPTR)Key, XSECURE_KEY_SIZE);
+	Xil_DCacheFlushRange((UINTPTR)Aad, XSECURE_AAD_SIZE);
+
+	/* Encryption and decryption of the data */
+	Status = SecureAesExample();
+	if(Status == XST_SUCCESS) {
+		xil_printf("\r\nSuccessfully ran Versal AES example\r\n");
+	}
+	else {
+		xil_printf("\r\nVersal AES example failed\r\n");
+	}
+
+END:
+	return Status;
+}
+
+/****************************************************************************/
+/**
+*
+* This function encrypts the data with provided AES key and IV and decrypts
+* the encrypted data also checks whether GCM tag is matched or not and finally
+* compares the decrypted data with the original data provided.
+*
+* @param	None
+*
+* @return
+*		- XST_FAILURE if the Aes example was failed.
+*		- XST_SUCCESS if the Aes example was successful
+*
+* @note		None.
+*
+****************************************************************************/
+/** //! [Generic AES example] */
+static int SecureAesExample(void)
+{
+	XPmcDma_Config *Config;
+	int Status = XST_FAILURE;
+	u32 Index;
+	XPmcDma PmcDmaInstance;
+	XSecure_Aes Secure_Aes;
+
+	/* Initialize PMC DMA driver */
+	Config = XPmcDma_LookupConfig(XSECURE_PMCDMA_DEVICEID);
+	if (NULL == Config) {
+		return XST_FAILURE;
+	}
+
+	Status = XPmcDma_CfgInitialize(&PmcDmaInstance, Config,
+					Config->BaseAddress);
+	if (Status != XST_SUCCESS) {
+		goto END;
+	}
+
+	/* Initialize the Aes driver so that it's ready to use */
+	Status = XSecure_AesInitialize(&Secure_Aes, &PmcDmaInstance);
+	if (Status != XST_SUCCESS) {
+		xil_printf("Failure at Aes initialize\r\n");
+		goto END;
+	}
+
+	/* Write AES key */
+	Status = XSecure_AesWriteKey(&Secure_Aes, XSECURE_AES_USER_KEY_0,
+				XSECURE_AES_KEY_SIZE_256, (u64)Key);
+	if (Status != XST_SUCCESS) {
+		xil_printf("Failure at key write\n\r");
+		goto END;
+	}
+
+	xil_printf("Data to be encrypted: \n\r");
+	for (Index = 0; Index < XSECURE_DATA_SIZE; Index++) {
+		xil_printf("%02x", Data[Index]);
+	}
+	xil_printf( "\r\n\n");
+
+
+	Xil_DCacheInvalidateRange((UINTPTR)EncData, XSECURE_DATA_SIZE);
+	Xil_DCacheInvalidateRange((UINTPTR)GcmTag, XSECURE_SECURE_GCM_TAG_SIZE);
+
+	/* Encryption of Data */
+	/*
+	 * If all the data to be encrypted is contiguous one can call
+	 * XSecure_AesEncryptData API directly after
+	 * XSecure_AesEncryptInit() call
+	 */
+	Status = XSecure_AesEncryptInit(&Secure_Aes, XSECURE_AES_USER_KEY_0,
+					XSECURE_AES_KEY_SIZE_256, (u64)Iv);
+	if (Status != XST_SUCCESS) {
+		xil_printf(" Aes encrypt init is failed\n\r");
+		goto END;
+	}
+
+	Status = XSecure_AesUpdateAad(&Secure_Aes, (u64)Aad, XSECURE_AAD_SIZE);
+	if (Status != XST_SUCCESS) {
+		xil_printf(" Aes update aad failed %x\n\r", Status);
+		goto END;
+	}
+	Status = XSecure_AesEncryptUpdate(&Secure_Aes, (u64)Data,(u64)EncData,
+						XSECURE_DATA_SIZE, TRUE);
+	if (Status != XST_SUCCESS) {
+		xil_printf(" Aes encrypt update is failed\n\r");
+		goto END;
+	}
+
+	Status = XSecure_AesEncryptFinal(&Secure_Aes, (u64)GcmTag);
+	if (Status != XST_SUCCESS) {
+		xil_printf("Failed at GCM tag generation\n\r");
+		goto END;
+	}
+
+	Xil_DCacheInvalidateRange((UINTPTR)EncData, XSECURE_DATA_SIZE);
+	Xil_DCacheInvalidateRange((UINTPTR)GcmTag, XSECURE_SECURE_GCM_TAG_SIZE);
+
+	xil_printf("Encrypted data: \n\r");
+	for (Index = 0; Index < XSECURE_DATA_SIZE; Index++) {
+		xil_printf("%02x", EncData[Index]);
+	}
+	xil_printf( "\r\n");
+
+	xil_printf("GCM tag: \n\r");
+	for (Index = 0; Index < XSECURE_SECURE_GCM_TAG_SIZE; Index++) {
+		xil_printf("%02x", GcmTag[Index]);
+	}
+	xil_printf( "\r\n\n");
+
+	Xil_DCacheInvalidateRange((UINTPTR)DecData, XSECURE_DATA_SIZE);
+
+	/* Decrypts the encrypted data */
+	/*
+	 * If data to be decrypted is contiguous one can also call
+	 * single API XSecure_AesDecryptData after
+	 * XSecure_AesDecryptInit() call
+	 */
+	Status = XSecure_AesDecryptInit(&Secure_Aes, XSECURE_AES_USER_KEY_0,
+					XSECURE_AES_KEY_SIZE_256, (u64)Iv);
+	if (Status != XST_SUCCESS) {
+		xil_printf("Error in decrypt initi %x\n\r", Status);
+		goto END;
+	}
+	Status = XSecure_AesUpdateAad(&Secure_Aes, (u64)Aad, XSECURE_AAD_SIZE);
+	if (Status != XST_SUCCESS) {
+		xil_printf(" Aes update aad failed %x\n\r", Status);
+		goto END;
+	}
+	Status = XSecure_AesDecryptUpdate(&Secure_Aes, (u64)EncData, (u64)DecData,
+						 XSECURE_DATA_SIZE, TRUE);
+	if (Status != XST_SUCCESS) {
+		xil_printf("Aes decrypt update failed %x\n\r", Status);
+		goto END;
+	}
+	Status = XSecure_AesDecryptFinal(&Secure_Aes, (u64)GcmTag);
+	if (Status != XST_SUCCESS) {
+		xil_printf("Decryption failure- GCM tag was not matched\n\r");
+		goto END;
+	}
+
+	Xil_DCacheInvalidateRange((UINTPTR)DecData, XSECURE_DATA_SIZE);
+
+	xil_printf("Decrypted data \n\r");
+	for (Index = 0; Index < XSECURE_DATA_SIZE; Index++) {
+		xil_printf("%02x", DecData[Index]);
+	}
+	xil_printf( "\r\n");
+
+	/* Comparison of Decrypted Data with original data */
+	Status = Xil_MemCmp(Data, DecData, XSECURE_DATA_SIZE);
+	if (Status != XST_SUCCESS) {
+		xil_printf("Failure during comparison of the data\n\r");
+		goto END;
+	}
+
+END:
+	return Status;
+}
+/** //! [Generic AES example] */
+/** @} */
---
+++ lib/sw_services/xilsecure/examples/xilsecure_versal_sha_server_example.c
@@ -0,0 +1,188 @@
+/******************************************************************************
+* Copyright (c) 2021 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+/*****************************************************************************/
+/**
+*
+* @file	xilsecure_versal_sha_server_example.c
+* @addtogroup xsecure_sha3_example_apis XilSecure SHA3 Versal API Example Usage
+* @{
+* This example illustrates the SHA3 hash calculation.
+*
+* MODIFICATION HISTORY:
+* <pre>
+* Ver   Who    Date     Changes
+* ----- ------ -------- -------------------------------------------------
+* 1.0  kal 21/04/2021 First Release
+*
+* </pre>
+******************************************************************************/
+
+/***************************** Include Files *********************************/
+
+#include "xparameters.h"
+#include "xsecure_sha.h"
+#include "xil_util.h"
+
+/************************** Constant Definitions *****************************/
+
+/*
+ * The following constants map to the XPAR parameters created in the
+ * xparameters.h file. They are defined here such that a user can easily
+ * change all the needed parameters in one place.
+ */
+
+/**************************** Type Definitions *******************************/
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+#define SHA3_HASH_LEN_IN_BYTES  48U
+#define SHA3_INPUT_DATA_LEN     6U
+
+/************************** Function Prototypes ******************************/
+
+static int SecureSha3Example(void);
+static int SecureSha3CompareHash(u8 *Hash, u8 *ExpectedHash);
+static void SecureSha3PrintHash(u8 *Hash);
+
+/************************** Variable Definitions *****************************/
+
+static const char Data[SHA3_INPUT_DATA_LEN + 1U] = "XILINX";
+
+static u8 ExpHash[SHA3_HASH_LEN_IN_BYTES] =
+				     {0x70,0x69,0x77,0x35,0x0b,0x93,
+				      0x92,0xa0,0x48,0x2c,0xd8,0x23,
+				      0x38,0x47,0xd2,0xd9,0x2d,0x1a,
+				      0x95,0x0c,0xad,0xa8,0x60,0xc0,
+				      0x9b,0x70,0xc6,0xad,0x6e,0xf1,
+				      0x5d,0x49,0x68,0xa3,0x50,0x75,
+				      0x06,0xbb,0x0b,0x9b,0x03,0x7d,
+				      0xd5,0x93,0x76,0x50,0xdb,0xd4};
+
+/*****************************************************************************/
+/**
+*
+* Main function to call the SecureShaExample
+*
+* @return
+*		- XST_FAILURE if the SHA calculation failed.
+*
+******************************************************************************/
+int main(void)
+{
+	int Status;
+
+	Status = SecureSha3Example();
+	if(Status == XST_SUCCESS) {
+		xil_printf("Successfully ran SHA example");
+	}
+	else {
+		xil_printf("SHA Example failed");
+	}
+	return Status;
+}
+
+/****************************************************************************/
+/**
+*
+* This function sends 'XILINX' to SHA-3 module for hashing.
+* The purpose of this function is to illustrate how to use the XSecure_Sha3
+* driver.
+*
+*
+* @return
+*		- XST_SUCCESS - SHA-3 hash successfully generated for given
+*				input data string.
+*		- XST_FAILURE - if the SHA-3 hash failed.
+*
+****************************************************************************/
+/** //! [SHA3 example] */
+static int SecureSha3Example()
+{
+	XSecure_Sha3 Secure_Sha3;
+	XCsuDma CsuDma;
+	XCsuDma_Config *Config;
+	u8 Out[SHA3_HASH_LEN_IN_BYTES];
+	int Status = XST_FAILURE;
+	u32 Size = 0U;
+
+	Size = Xil_Strnlen(Data, SHA3_INPUT_DATA_LEN);
+	if (Size != SHA3_INPUT_DATA_LEN) {
+		xil_printf("Provided data length is Invalid\n\r");
+		Status = XST_FAILURE;
+		goto END;
+	}
+
+	Config = XCsuDma_LookupConfig(0);
+	if (NULL == Config) {
+		xil_printf("config failed\n\r");
+		Status = XST_FAILURE;
+		goto END;
+	}
+
+	Status = XCsuDma_CfgInitialize(&CsuDma, Config, Config->BaseAddress);
+	if (Status != XST_SUCCESS) {
+		Status = XST_FAILURE;
+		goto END;
+	}
+
+	/*
+	 * Initialize the SHA-3 driver so that it's ready to use
+	 */
+	XSecure_Sha3Initialize(&Secure_Sha3, &CsuDma);
+
+	XSecure_Sha3Digest(&Secure_Sha3, (UINTPTR)Data, Size, (XSecure_Sha3Hash*)Out);
+
+
+	xil_printf(" Calculated Hash \r\n ");
+	SecureSha3PrintHash(Out);
+
+	Status = SecureSha3CompareHash(Out, ExpHash);
+END:
+	return Status;
+}
+
+/****************************************************************************/
+/**
+*
+* This function compares the given hash with the expected Hash
+*
+* @return
+*		- XST_SUCCESS - if the expected hash is equal to the
+*                               given hash
+*		- XST_FAILURE - if the comparison fails.
+*
+****************************************************************************/
+static int SecureSha3CompareHash(u8 *Hash, u8 *ExpectedHash)
+{
+	int Status = XST_FAILURE;
+
+	Status = Xil_MemCmp(Hash, ExpectedHash, SHA3_HASH_LEN_IN_BYTES);
+	if (Status != XST_SUCCESS) {
+		xil_printf("Expected Hash \r\n");
+		SecureSha3PrintHash(ExpectedHash);
+		xil_printf("SHA Example Failed at Hash Comparison \r\n");
+	}
+
+	return Status;
+}
+
+/****************************************************************************/
+/**
+*
+* This function prints the given hash on the console
+*
+****************************************************************************/
+static void SecureSha3PrintHash(u8 *Hash)
+{
+	u32 Index;
+
+	for (Index = 0U; Index < SHA3_HASH_LEN_IN_BYTES; Index++) {
+		xil_printf(" %0x ", Hash[Index]);
+	}
+	xil_printf(" \r\n ");
+ }
+/** //! [SHA3 example] */
+/** @} */
