66a5778973fd78678b29daeb656b2919e907bc6b
Alex Kiani <alex.kiani@xilinx.com>
2021-05-03 11:30:56 -0700

    aiefal: profile: added stall occurence class and API for stall event
    added class for stall occurences
    added API to set stall event
    created pointer to stall occurent in base
    Signed-off-by: Alex Kiani <alex.kiani@xilinx.com>
    
    Acked-by: Wendy Liang <wendy.liang@xilinx.com>

--- XilinxProcessorIPLib/drivers/aiefal/src/profile/xaiefal-profile.hpp
+++ XilinxProcessorIPLib/drivers/aiefal/src/profile/xaiefal-profile.hpp
@@ -49,6 +49,50 @@ namespace xaiefal {
 			FlowGroupEvent = Dev.tile(L).module(XAIE_CORE_MOD).groupEvent(XAIE_EVENT_GROUP_CORE_PROGRAM_FLOW_CORE);
 			StallGroupEvent->setGroupEvents(0x19F);
 			}
+
+		/**
+		 * This function sets the stall group events enabling bits.
+		 * Must be called before reserve.
+		 *
+		 * @param C group event
+		 * @return XAIE_OK for success, error code for failure
+		 */
+		AieRC stallGroupEventConfig(uint32_t C) {
+			AieRC RC = XAIE_OK;
+
+			if (State.Reserved == 0) {
+				Logger::log(LogLevel::ERROR) << "StallCycles " << __func__ << " (" <<
+					(uint32_t)Loc.Col << "," << (uint32_t)Loc.Row << ")" <<
+					" Expect Mod= " << Mod <<
+					" resource not allocated." << std::endl;
+				RC = XAIE_ERR;
+			} else {
+				StallGroupEvent->setGroupEvents(C);
+			}
+			return RC;
+		}
+
+		/**
+		 * This function sets the flow group events enabling bits.
+		 * Must be called before reserve.
+		 *
+		 * @param C group event
+		 * @return XAIE_OK for success, error code for failure
+		 */
+		AieRC flowGroupEventConfig(uint32_t C) {
+			AieRC RC = XAIE_OK;
+
+			if (State.Reserved == 0) {
+				Logger::log(LogLevel::ERROR) << "StallCycles " << __func__ << " (" <<
+					(uint32_t)Loc.Col << "," << (uint32_t)Loc.Row << ")" <<
+					" Expect Mod= " << Mod <<
+					" resource not allocated." << std::endl;
+				RC = XAIE_ERR;
+			} else {
+				FlowGroupEvent->setGroupEvents(C);
+			}
+			return RC;
+		}
 	protected:
 		AieRC _reserveAppend() {
 			AieRC RC;
@@ -59,7 +103,7 @@ namespace xaiefal {
 			}
 			return RC;
 		}
-		AieRC _releaseppend() {
+		AieRC _releaseAppend() {
 			AieRC RC;
 
 			RC = StallGroupEvent->release();
@@ -91,4 +135,80 @@ namespace xaiefal {
 		std::shared_ptr<XAieGroupEventHandle> FlowGroupEvent;
 	};
 
+	/**
+	 * @class XAieStallOccurrences
+	 * @brief class for number of stall occurences
+	 */
+	class XAieStallOccurrences: public XAiePerfCounter {
+	public:
+		XAieStallOccurrences() = delete;
+		XAieStallOccurrences(std::shared_ptr<XAieDevHandle> DevHd,
+			XAie_LocType L,
+			std::shared_ptr<XAieGroupEventHandle> StallG,
+			bool CrossM = false):
+			XAiePerfCounter(DevHd, L, XAIE_CORE_MOD, CrossM),
+			StallGroupEvent(StallG) {
+			initialize(XAIE_CORE_MOD, XAIE_EVENT_GROUP_CORE_STALL_CORE,
+				XAIE_CORE_MOD, XAIE_EVENT_GROUP_CORE_STALL_CORE);
+			StallGroupEvent->setGroupEvents(0x19F);
+		}
+		XAieStallOccurrences(XAieDev &Dev,
+			XAie_LocType L,
+			bool CrossM = false):
+			XAiePerfCounter(Dev, L, XAIE_CORE_MOD, CrossM) {
+			initialize(XAIE_CORE_MOD, XAIE_EVENT_GROUP_CORE_STALL_CORE,
+				XAIE_CORE_MOD, XAIE_EVENT_GROUP_CORE_STALL_CORE);
+			StallGroupEvent = Dev.tile(L).module(XAIE_CORE_MOD).groupEvent(XAIE_EVENT_GROUP_CORE_STALL_CORE);
+			StallGroupEvent->setGroupEvents(0x19F);
+			}
+
+		/**
+		 * This function sets the stall group events enabling bits
+		 * Must be called before reserve.
+		 *
+		 * @param C group event
+		 * @return XAIE_OK for success, error code for failure
+		 */
+		AieRC stallGroupEventConfig(uint32_t C) {
+			AieRC RC = XAIE_OK;
+
+			if (State.Reserved == 0) {
+				Logger::log(LogLevel::ERROR) << "StallCycles " << __func__ << " (" <<
+					(uint32_t)Loc.Col << "," << (uint32_t)Loc.Row << ")" <<
+					" Expect Mod= " << Mod <<
+					" resource not allocated." << std::endl;
+				RC = XAIE_ERR;
+			} else {
+				StallGroupEvent->setGroupEvents(C);
+			}
+			return RC;
+		}
+	protected:
+		AieRC _reserveAppend() {
+			AieRC RC;
+
+			RC = StallGroupEvent->reserve();
+			return RC;
+		}
+		AieRC _releaseAppend() {
+			AieRC RC;
+
+			RC = StallGroupEvent->release();
+			return RC;
+		}
+		AieRC _startPrepend() {
+			AieRC RC;
+
+			RC = StallGroupEvent->start();
+			return RC;
+		}
+		AieRC _stopAppend() {
+			AieRC RC;
+
+			RC = StallGroupEvent->stop();
+			return RC;
+		}
+	private:
+		std::shared_ptr<XAieGroupEventHandle> StallGroupEvent;
+	};
 }
