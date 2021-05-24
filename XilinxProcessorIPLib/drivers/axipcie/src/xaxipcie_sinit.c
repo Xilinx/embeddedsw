0c3b36bfd8034ee5f6e71243319832eb0f9b2739
meenap <meena.paleti@xilinx.com>
2021-05-24 19:15:44 +0530

    Updated src files addtogroup versions with mdd version for axipcie driver 2021.1
    Updated src files addtogroup version with mdd version for axipcie
    
    Signed-off-by: Meena Paleti <meena.paleti@xilinx.com>
    
    Acked-by:Siva Addepalli <sivaprasad.addepalli@xilinx.com>

--- XilinxProcessorIPLib/drivers/axipcie/src/xaxipcie_sinit.c
+++ XilinxProcessorIPLib/drivers/axipcie/src/xaxipcie_sinit.c
@@ -6,7 +6,7 @@
 /*****************************************************************************
 **
 * @file xaxipcie_sinit.c
-* @addtogroup axipcie_v3_2
+* @addtogroup axipcie_v3_3
 * @{
 *
 * This file contains the implementation of AXI PCIe driver's static
