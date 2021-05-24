a721554f23366608345b41f29b0827b28eb962f6
Anirudha Sarangi <anirudha.sarangi@xilinx.com>
2021-05-24 20:24:03 +0530

    sw_apps: Add Dhrystone application
    This patch adds Dhrystone application support into sw_apps folder to be
    accessible to users through Vitis/xsct. The dhrystone being added does
    not support Microblaze. Support for the same will be added later. The
    Dhrystone app can only be used using GNU toolchain. Also it is targeted
    for baremetal environment.
    
    Signed-off-by: Anirudha Sarangi <anirudha.sarangi@xilinx.com>
    Acked-by: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

---
+++ lib/sw_apps/dhrystone/src/platform.c
@@ -0,0 +1,39 @@
+#/******************************************************************************
+#* Copyright (c) 2010 - 2021 Xilinx, Inc.  All rights reserved.
+#* SPDX-License-Identifier: MIT
+#******************************************************************************/
+
+#include "xparameters.h"
+#include "xil_cache.h"
+#include "platform_config.h"
+#ifdef STDOUT_IS_16550
+#include "xuartns550_l.h"
+#endif
+
+void enable_caches()
+{
+	/* Empty function for ARM platforms */
+	/* Once support for MB is added, this function will be populated */
+}
+
+void disable_caches()
+{
+	Xil_DCacheDisable();
+	Xil_ICacheDisable();
+}
+
+void init_platform()
+{
+	enable_caches();
+	/* if we have a uart 16550, then that needs to be initialized */
+#ifdef STDOUT_IS_16550
+	XUartNs550_SetBaud(STDOUT_BASEADDR, XPAR_XUARTNS550_CLOCK_HZ, 9600);
+	XUartNs550_SetLineControlReg(STDOUT_BASEADDR, XUN_LCR_8_DATA_BITS);
+#endif
+}
+
+void cleanup_platform()
+{
+	disable_caches();
+}
+
---
+++ lib/sw_apps/dhrystone/src/platform_config.h
@@ -0,0 +1,17 @@
+#/******************************************************************************
+#* Copyright (c) 2010 - 2021 Xilinx, Inc.  All rights reserved.
+#* SPDX-License-Identifier: MIT
+#******************************************************************************/
+
+#ifndef __PLATFORM_CONFIG_H_
+#define __PLATFORM_CONFIG_H_
+
+/* declare strcpy */
+#include <string.h>
+
+
+/* declare functions in platform.c */
+void init_platform();
+void cleanup_platform();
+
+#endif
