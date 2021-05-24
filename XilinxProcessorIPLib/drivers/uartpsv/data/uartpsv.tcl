bf5ca4012cdcd4ea9e39d4e97e76e209e5fc9a37
Tarun DTS <tarun.dts@xilinx.com>
2021-05-24 08:37:30 +0530

    sw_services: xilplmi: Support PLM to use configurable uart baudrate
    This patch adds support for PLM to use configurable uart baudrate during build
    based on design
    
    Signed-off-by: Tarun DTS <tarun.dts@xilinx.com>
    Acked-by: Vikram Sreenivasa Batchali <b.vikram@xilinx.com>

--- XilinxProcessorIPLib/drivers/uartpsv/data/uartpsv.tcl
+++ XilinxProcessorIPLib/drivers/uartpsv/data/uartpsv.tcl
@@ -16,10 +16,10 @@
 #uses "xillib.tcl"
 
 proc generate {drv_handle} {
-    ::hsi::utils::define_zynq_include_file $drv_handle "xparameters.h" "XUartPsv" "NUM_INSTANCES" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR" "C_UART_CLK_FREQ_HZ" "C_HAS_MODEM"
+    ::hsi::utils::define_zynq_include_file $drv_handle "xparameters.h" "XUartPsv" "NUM_INSTANCES" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR" "C_UART_CLK_FREQ_HZ" "C_HAS_MODEM" "C_BAUDRATE"
 
-    ::hsi::utils::define_zynq_config_file $drv_handle "xuartpsv_g.c" "XUartPsv"  "DEVICE_ID" "C_S_AXI_BASEADDR" "C_UART_CLK_FREQ_HZ" "C_HAS_MODEM"
+    ::hsi::utils::define_zynq_config_file $drv_handle "xuartpsv_g.c" "XUartPsv"  "DEVICE_ID" "C_S_AXI_BASEADDR" "C_UART_CLK_FREQ_HZ" "C_HAS_MODEM" "C_BAUDRATE"
 
-    ::hsi::utils::define_zynq_canonical_xpars $drv_handle "xparameters.h" "XUartPsv" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR" "C_UART_CLK_FREQ_HZ" "C_HAS_MODEM"
+    ::hsi::utils::define_zynq_canonical_xpars $drv_handle "xparameters.h" "XUartPsv" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR" "C_UART_CLK_FREQ_HZ" "C_HAS_MODEM" "C_BAUDRATE"
 
 }
