9d10a804e222b419cf393d5eab31fb078e04dcf3
Shravya <shravya.kumbham@xilinx.com>
2021-05-25 05:53:32 +0000

    axivdma: Update version and copyright year
    Update axivdma version to 6.9 and copyright year to 2021.
    
    Signed-off-by: Shravya Kumbham <shravya.kumbham@xilinx.com>
    
    Acked-for-series: Radhey Shyam Pandey <radhey.shyam.pandey@xilinx.com>

--- XilinxProcessorIPLib/drivers/axivdma/src/xaxivdma_sinit.c
+++ XilinxProcessorIPLib/drivers/axivdma/src/xaxivdma_sinit.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2012 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2012 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xaxivdma_sinit.c
-* @addtogroup axivdma_v6_8
+* @addtogroup axivdma_v6_9
 * @{
 *
 * Look up the hardware settings using device ID. The hardware setting is inside
