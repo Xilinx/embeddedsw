98aef1abe886cb989f2b5f6bd829b744f011286a
Mubin Sayyed <mubin.usman.sayyed@xilinx.com>
2021-05-26 10:58:42 +0530

    cpu: Add -fno-tree-loop-distribute-patterns flag
    GCC 10.2 version is enabling -ftree-loop- distribute-patterns flag by default
    at O2 optimization level. Due to this flag, certain for loops would be
    converted into memset.
    
    "for" loop to memset conversions can result into incorrect behavior.
    For Microblaze processor unaligned access is not allowed and
    it would result into HW exception.
    
    memset is standard C library function, and it does not take care of alignment.
    So, replacing for loop with memset could potentially result into exceptions
    for Microblaze processor
    
    Disabling for loop to memset conversions by using
    -fno-tree-loop-distribute-patterns flags to avoid incorrect behavior.
    
    Signed-off-by: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

--- XilinxProcessorIPLib/drivers/cpu/src/xio.c
+++ XilinxProcessorIPLib/drivers/cpu/src/xio.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2007 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2007 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xio.c
-* @addtogroup cpu_v2_12
+* @addtogroup cpu_v2_13
 * @{
 *
 * Contains I/O functions for memory-mapped or non-memory-mapped I/O
