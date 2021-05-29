e6bb04e844036ca6100a7cb230ac1ff9aee1e1fc
Wendy Liang <wendy.liang@xilinx.com>
2021-05-28 17:43:55 -0700

    aienginev2: src: dev: add partition isolation after reset op for aieml
    Add partition isolation after reset operation for aieml device
    
    Signed-off-by: Wendy Liang <wendy.liang@xilinx.com>
    
    Acked-for-series: Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>

--- XilinxProcessorIPLib/drivers/aienginev2/src/device/xaie_device_aieml.h
+++ XilinxProcessorIPLib/drivers/aienginev2/src/device/xaie_device_aieml.h
@@ -28,6 +28,7 @@
 u8 _XAieMl_GetTTypefromLoc(XAie_DevInst *DevInst, XAie_LocType Loc);
 AieRC _XAieMl_SetPartColShimReset(XAie_DevInst *DevInst, u8 Enable);
 AieRC _XAieMl_SetPartColClockAfterRst(XAie_DevInst *DevInst, u8 Enable);
+AieRC _XAieMl_SetPartIsolationAfterRst(XAie_DevInst *DevInst);
 
 #endif /* XAIE_DEVICE_AIEML */
 /** @} */
