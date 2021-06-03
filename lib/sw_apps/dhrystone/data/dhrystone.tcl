3a350377082b8c0296ecb89c42d203cf2316033c
Anirudha Sarangi <anirudha.sarangi@xilinx.com>
2021-06-03 13:45:21 +0530

    sw_apps: dhrystone: Add support for Microblaze
    Microblaze CPU support is being added into the dhrystone application
    through this patch. For Microblaze Dhrystone to work, it is mandatory
    that the design has an Axi Timer.
    
    Signed-off-by: Anirudha Sarangi <anirudha.sarangi@xilinx.com>
    Acked-by: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

--- lib/sw_apps/dhrystone/data/dhrystone.tcl
+++ lib/sw_apps/dhrystone/data/dhrystone.tcl
@@ -8,7 +8,7 @@ proc swapp_get_name {} {
 }
 
 proc swapp_get_description {} {
-    return "Dhrystone synthetic benchmark program for baremetal environment. Not supported for Microblaze in 2021.1.";
+    return "Dhrystone synthetic benchmark program for baremetal environment.";
 }
 
 proc check_standalone_os {} {
@@ -64,23 +64,40 @@ proc check_stdout_hw {} {
 	set hw_processor [common::get_property HW_INSTANCE $proc_instance]
 	set proc_type [common::get_property IP_NAME [hsi::get_cells -hier $hw_processor]];
 
-	if {($proc_type == "psu_microblaze")} {
-		error "This application is not supported for PMU Microblaze processor (psu_microblaze).";
-	}
-	
-	if {($proc_type == "microblaze")} {
-		error "This application is not supported for Microblaze processor as of now.";
+	if {$proc_type == "psu_pmu" || $proc_type == "psu_pmc" || $proc_type == "psu_psm" || $proc_type == "psv_pmc" || $proc_type == "psv_psm" } {
+		#error "This application is not supported for non-standard and hardened Microblaze configurations.";
 	}
 	
 	set slaves [common::get_property SLAVES [hsi::get_cells -hier [hsi::get_sw_processor]]]
 	foreach slave $slaves {
 		set slave_type [common::get_property IP_NAME [hsi::get_cells -hier $slave]];
-		if { $slave_type == "ps7_uart" || $slave_type == "psu_uart"  || $slave_type == "psv_sbsauart" } {
+		if { $slave_type == "ps7_uart" || $slave_type == "psu_uart"  || $slave_type == "psv_sbsauart" || $slave_type == "axi_uartlite" ||
+			$slave_type == "axi_uart16550" || $slave_type == "iomodule" ||
+			$slave_type == "mdm" } {
 			return;
 		}
 	}
 	
-	error "This application requires a Uart IP in the hardware."	
+	error "This application requires a Uart IP in the hardware."
+}
+
+proc check_axi_timer_hw {} {
+	set proc_instance [hsi::get_sw_processor];
+	set hw_processor [common::get_property HW_INSTANCE $proc_instance]
+	set proc_type [common::get_property IP_NAME [hsi::get_cells -hier $hw_processor]];
+
+        if {($proc_type == "microblaze")} {
+	    set slaves [common::get_property SLAVES [hsi::get_cells -hier [hsi::get_sw_processor]]]
+	    foreach slave $slaves {
+		set slave_type [common::get_property IP_NAME [hsi::get_cells -hier $slave]];
+		    if { $slave_type == "axi_timer" } {
+			return;
+		     }
+	     }
+         } else {
+	     return;
+	 }
+        error "This application requires a Axi Timer IP in the hardware."
 }
 
 proc check_stdout_sw {} {
@@ -124,6 +141,9 @@ proc swapp_is_supported_hw {} {
     # check for uart peripheral
     check_stdout_hw;
 
+    # check for axi timer
+    check_axi_timer_hw;
+
     # we require atleast 30k memory
     require_memory "30000";
 }
@@ -137,7 +157,29 @@ proc swapp_is_supported_sw {} {
 }
 
 proc swapp_generate {} {
-# To be filled up once Microblaze support is added
+    # cleanup this file for writing
+    set fid [open "platform_config.h" "w+"];
+    puts $fid "/******************************************************************************";
+    puts $fid "* Copyright (c) 2010 - 2021 Xilinx, Inc.  All rights reserved.";
+    puts $fid "* SPDX-License-Identifier: MIT";
+    puts $fid "******************************************************************************/";
+    puts $fid "#ifndef __PLATFORM_CONFIG_H_";
+    puts $fid "#define __PLATFORM_CONFIG_H_\n";
+
+    puts $fid "/* declare strcpy */";
+    puts $fid "#include <string.h>";
+
+    puts $fid "\n";
+
+    puts $fid "/* declare functions in platform.c */";
+    puts $fid "void init_platform();";
+    puts $fid "void cleanup_platform();";
+
+    # if we have a uart16550 as stdout, then generate some config for that
+    generate_stdout_config $fid;
+
+    puts $fid "#endif";
+    close $fid;
 }
 
 proc swapp_get_linker_constraints {} {
@@ -146,7 +188,7 @@ proc swapp_get_linker_constraints {} {
 }
 
 proc swapp_get_supported_processors {} {
-    return "psu_cortexa53 ps7_cortexa9 psv_cortexa72 psu_cortexr5 psv_cortexr5";
+    return "microblaze psu_cortexa53 ps7_cortexa9 psv_cortexa72 psu_cortexr5 psv_cortexr5";
 }
 
 proc swapp_get_supported_os {} {
