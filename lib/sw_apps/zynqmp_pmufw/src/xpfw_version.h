ffbfcbd3ebefb4d3205319ca948046de673faea0
Sharath Kumar Dasari <sharath.kumar.dasari@xilinx.com>
2021-06-15 22:37:50 +0530

    sw_apps: zynqmp_pmufw: ZynqMp PMU software version update
    This patch updates software version to 2021.2 for ZynqMp PMUFW
    
    Signed-off-by: Sharath Kumar Dasari <sharath.kumar.dasari@xilinx.com>
    Acked-by: Mounika Akula <mounika.akula@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (c) 2019 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (c) 2019 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -10,7 +10,7 @@
 extern "C" {
 #endif
 
-#define ZYNQMP_XPFW_VERSION	"2021.1"
+#define ZYNQMP_XPFW_VERSION	"2021.2"
 
 #ifdef __cplusplus
 }
