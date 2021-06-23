73870cf541bc6df4eff1dd6d9a457e3aa4a2d031
Wendy Liang <wendy.liang@xilinx.com>
2021-06-23 10:17:20 -0700

    aienginev2: src: device: add set isolation after reset operation
    Add device operation to set partition isolation after reset.
    
    Signed-off-by: Wendy Liang <wendy.liang@xilinx.com>
    
    Acked-for-series: Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>

--- XilinxProcessorIPLib/drivers/aienginev2/src/global/xaiegbl_reginit.c
+++ XilinxProcessorIPLib/drivers/aienginev2/src/global/xaiegbl_reginit.c
@@ -3057,6 +3057,7 @@ XAie_DeviceOps AieDevOps =
 	.GetTTypefromLoc = &_XAie_GetTTypefromLoc,
 	.SetPartColShimReset = &_XAie_SetPartColShimReset,
 	.SetPartColClockAfterRst = &_XAie_SetPartColClockAfterRst,
+	.SetPartIsolationAfterRst = &_XAie_SetPartIsolationAfterRst,
 };
 
 /** @} */
