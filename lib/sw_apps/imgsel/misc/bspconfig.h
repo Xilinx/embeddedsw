19cde5dfc13c44ae9f4b6b9d1159f0cb60967ce1
b.vikram@xilinx.com <b.vikram@xilinx.com>
2021-07-15 17:07:05 +0530

    sw_apps: imgsel: Create makefile to support versionless build of imgsel
    This patch adds support for versionless build for imgsel app.
    
    Signed-off-by: Vikram Sreenivasa Batchali <b.vikram@xilinx.com>
    Signed-off-by: Tarun DTS <tarun.dts@xilinx.com>
    Acked-by: Sharath Kumar Dasari <Sharath.Kumar.Dasari@xilinx.com>

---
+++ lib/sw_apps/imgsel/misc/bspconfig.h
@@ -0,0 +1,13 @@
+/******************************************************************************
+* Copyright (c) 2021 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+ ******************************************************************************/
+
+#ifndef BSPCONFIG_H  /* prevent circular inclusions */
+#define BSPCONFIG_H  /* by using protection macros */
+
+#define MICROBLAZE_PVR_NONE
+#define EL3 1
+#define EL1_NONSECURE 0
+#define HYP_GUEST 0
+#endif /*end of __BSPCONFIG_H_*/
