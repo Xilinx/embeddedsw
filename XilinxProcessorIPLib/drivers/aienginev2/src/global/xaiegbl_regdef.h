e5dcca05e926996e463a5d17876432d1468518c9
Alex Kiani <alex.kiani@xilinx.com>
2021-07-19 12:29:38 -0700

    aienginev2: src: add field to check if aie tile checkerboard
    add field in aie device op struct to check checkerboard
    initialize aie devop struct to contain checkerboard field
    
    Signed-off-by: Alex Kiani <alex.kiani@xilinx.com>
    
    Acked-for-series: Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>

--- XilinxProcessorIPLib/drivers/aienginev2/src/global/xaiegbl_regdef.h
+++ XilinxProcessorIPLib/drivers/aienginev2/src/global/xaiegbl_regdef.h
@@ -815,6 +815,7 @@ typedef struct XAie_TileMod {
 
 
 typedef struct XAie_DeviceOps {
+	u8 IsCheckerBoard;
 	u8 (*GetTTypefromLoc)(XAie_DevInst *DevInst, XAie_LocType Loc);
 	AieRC (*SetPartColShimReset)(XAie_DevInst *DevInst, u8 Enable);
 	AieRC (*SetPartColClockAfterRst)(XAie_DevInst *DevInst, u8 Enable);
