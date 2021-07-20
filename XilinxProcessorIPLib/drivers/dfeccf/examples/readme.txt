771ab84a0fcd1666e981d9792ab1e13a8fcb5fee
Dragan Cvetic <dragan.cvetic@xilinx.com>
2021-07-20 15:15:06 +0100

    dfeccf: Update to common latency requirements
    Add latency registers.
    Add latency handler APIs.
    Improve comments in driver code.
    Organise custom device macro for bare metal.
    Aligne example with the other DFE examples.
    Update examples.
    Update example comments.
    Align si570 code with the other drivers
    Update driver version number in all files.
    Fix assertions in nonvoid functions.
    Remove read/write registers calls from examples.
    Pass Shift as argument to Load Coefficient API.
    Also, remove Shift parametar automatic calculation.
    
    Signed-off-by: Dragan Cvetic <dragan.cvetic@xilinx.com>
    
    Acked-for-series: Conall O'Griofa <conallo@xilinx.com>

--- XilinxProcessorIPLib/drivers/dfeccf/examples/readme.txt
+++ XilinxProcessorIPLib/drivers/dfeccf/examples/readme.txt
@@ -2,9 +2,11 @@
 You can refer to the below stated example applications for more details on how to use the driver.
 
 @section ex1 xdfeccf_selftest_example.c
-Contains an example on how to initialise XDfeCcf driver.
-daughter board on zcu208.
-This example does the following:
+Contains an example:
+- how to initialise two instances of XDfeCcf driver.
+- how to initialise and set one instance of  XDfeCcf driver in pass through mode.
 
+The example code is written and tested for zcu670 board.
+Running example in a Bare metal environment includes setting an output clock on si570.
 For details, see xdfeccf_selftest_example.c.
 */
--- XilinxProcessorIPLib/drivers/dfeccf/examples/xdfeccf_selftest_example.c
+++ XilinxProcessorIPLib/drivers/dfeccf/examples/xdfeccf_selftest_example.c
@@ -1,21 +1,23 @@
 /******************************************************************************
-* Copyright (C) 2021 Xilinx, Inc.  All rights reserved.
-* SPDX-License-Identifier: MIT
-******************************************************************************/
+ * Copyright (C) 2021 Xilinx, Inc.  All rights reserved.
+ * SPDX-License-Identifier: MIT
+ ******************************************************************************/
 
 /*****************************************************************************/
 /**
 *
-* @file xdfeccf_selftest_examplep.c
+* @file xdfeccf_selftest_example.c
 *
-* This file contains a selftest example for using the Channel filter hardware
-* and CfFilter driver.
-* This example does some writes to the hardware to do some sanity checks.
+* This file contains a selftest examples for using the Channel Filter hardware
+* and Channel Filter driver.
+* The examples are:
+*     - initialise two instances of Channel Filter driver.
+*     - add CCs to a sequence.
 *
 * Note: MGT si570 oscillator is set to 152.25MHz by default. The DFE IP wrapper
 *       requires MGT clock to be set to 122.88MHz (some IP use 61.44MHz).
 *       Prerequisite is to set the MGT si570 oscillator to the required IP
-*       before running the example code. This is for the ZCU208 prodaction
+*       before running the example code. This is for the ZCU670 production
 *       platform.
 *
 * <pre>
@@ -32,6 +34,7 @@
 *       dc     04/06/21 Register with full node name
 *       dc     04/07/21 Fix bare metal initialisation
 *       dc     04/08/21 Set sequence length only once
+* 1.1   dc     07/13/21 Update to common latency requirements
 *
 * </pre>
 *
@@ -44,6 +47,7 @@
 #endif
 #include "xdfeccf.h"
 #include "xdfeccf_hw.h"
+#include <math.h>
 
 /************************** Constant Definitions ****************************/
 
@@ -54,19 +58,18 @@
 #define printf xil_printf
 #define XDFECCF_NODE1_NAME XPAR_XDFECCF_0_DEV_NAME
 #define XDFECCF_NODE2_NAME XPAR_XDFECCF_1_DEV_NAME
+#define XDFESI570_CURRENT_FREQUENCY 156.25
+#define XDFESI570_NEW_FREQUENCY 122.88
 #else
 #define XDFECCF_NODE1_NAME "a7c00000.xdfe_cc_filter"
 #define XDFECCF_NODE2_NAME "a7d00000.xdfe_cc_filter"
 #endif
 
-#define XDFESI570_CURRENT_FREQUENCY 156.25
-#define XDFESI570_NEW_FREQUENCY 122.88
-
 /************************** Function Prototypes *****************************/
 extern int XDfeSi570_SetMgtOscillator(double CurrentFrequency,
 				      double NewFrequency);
 static int XDfeCcf_MultiInstancesExample();
-static int XDfeCcf_PassThroughTestExample();
+static int XDfeCcf_PassThroughExample();
 
 /************************** Variable Definitions ****************************/
 #ifdef __BAREMETAL__
@@ -75,40 +78,8 @@ metal_phys_addr_t metal_phys[XDFECCF_MAX_NUM_INSTANCES] = {
 	XPAR_XDFECCF_1_BASEADDR,
 };
 struct metal_device CustomDevice[XDFECCF_MAX_NUM_INSTANCES] = {
-	{
-		.name = XPAR_XDFECCF_0_DEV_NAME,
-		.bus = NULL,
-		.num_regions = 1,
-		.regions = { {
-			.virt = (void *)XPAR_XDFECCF_0_BASEADDR,
-			.physmap = &metal_phys[0],
-			.size = 0x10000,
-			.page_shift = (u32)(-1),
-			.page_mask = (u32)(-1),
-			.mem_flags = 0x0,
-			.ops = { NULL },
-		} },
-		.node = { NULL },
-		.irq_num = 0,
-		.irq_info = NULL,
-	},
-	{
-		.name = XPAR_XDFECCF_1_DEV_NAME,
-		.bus = NULL,
-		.num_regions = 1,
-		.regions = { {
-			.virt = (void *)XPAR_XDFECCF_1_BASEADDR,
-			.physmap = &metal_phys[0],
-			.size = 0x10000,
-			.page_shift = (u32)(-1),
-			.page_mask = (u32)(-1),
-			.mem_flags = 0x0,
-			.ops = { NULL },
-		} },
-		.node = { NULL },
-		.irq_num = 0,
-		.irq_info = NULL,
-	},
+	XDFECCF_CUSTOM_DEV(XPAR_XDFECCF_0_DEV_NAME, XPAR_XDFECCF_0_BASEADDR, 0),
+	XDFECCF_CUSTOM_DEV(XPAR_XDFECCF_1_DEV_NAME, XPAR_XDFECCF_1_BASEADDR, 1),
 };
 #endif
 
@@ -123,12 +94,10 @@ struct metal_device CustomDevice[XDFECCF_MAX_NUM_INSTANCES] = {
 *		- XST_SUCCESS if the example has completed successfully.
 *		- XST_FAILURE if the example has failed.
 *
-* @note		None.
-*
 *****************************************************************************/
 int main(void)
 {
-	printf("DFE Channel Filter (CCF) Example Test\r\n");
+	printf("DFE Channel Filter (CCF) Example\r\n");
 
 #ifdef __BAREMETAL__
 	if (XST_SUCCESS !=
@@ -140,46 +109,44 @@ int main(void)
 #endif
 
 	/*
-	 * Run the DFE Channel Filter init/close example, specify the Device
-	 * ID that is generated in xparameters.h.
+	 * Run the DFE Channel Filter init/close example. For bare metal
+	 * specify the Device ID that is generated in xparameters.h.
 	 */
 	if (XST_SUCCESS != XDfeCcf_MultiInstancesExample()) {
-		printf("Multi Instances Example Test failed\r\n");
+		printf("Multi Instances Example failed\r\n");
 		return XST_FAILURE;
 	}
 
 	/*
-	 * Run the DFE Channel Filter pass through example, specify the Device
-	 * ID that is generated in xparameters.h.
+	 * Run the DFE Channel Filter pass through example. For bare metal
+	 * specify the Device ID that is generated in xparameters.h.
 	 */
-	if (XST_SUCCESS != XDfeCcf_PassThroughTestExample()) {
-		printf("Pass through Example Test failed\r\n");
+	if (XST_SUCCESS != XDfeCcf_PassThroughExample()) {
+		printf("Pass through Example failed\r\n");
 		return XST_FAILURE;
 	}
 
-	printf("Successfully ran Multi Instances and Pass Through Example Test\r\n");
+	printf("\r\nSuccessfully run Multi Instances and Pass Through Example\r\n");
 	return XST_SUCCESS;
 }
 
 /****************************************************************************/
 /**
 *
-* This function runs a test on the DFE Channel Filter device using the
-* driver APIs.
+* This function runs two instances of the DFE Channel Filter using the driver
+* APIs.
 * This function does the following tasks:
 *	- Create and system initialize the device driver instance.
+*	- Read SW and HW version numbers.
 *	- Reset the device.
 *	- Configure the device.
 *	- Initialize the device.
 *	- Activate the device.
-*	- Write and read coefficient.
 *	- DeActivate the device.
 *
 * @return
 *		- XST_SUCCESS if the example has completed successfully.
 *		- XST_FAILURE if the example has failed.
-*
-* @note   	None
 *
 ****************************************************************************/
 static int XDfeCcf_MultiInstancesExample()
@@ -188,103 +155,155 @@ static int XDfeCcf_MultiInstancesExample()
 	XDfeCcf_Cfg Cfg;
 	XDfeCcf *InstancePtr1 = NULL;
 	XDfeCcf *InstancePtr2 = NULL;
+	XDfeCcf_Version SwVersion;
+	XDfeCcf_Version HwVersion;
 	XDfeCcf_Init Init;
 
+	printf("\r\nChannel Filter: Multi instances initialisation example\n");
+
 	/* Initialize libmetal */
-	if (0 != metal_init(&init_param)) {
-		(void)printf("ERROR: Failed to run metal initialization\n");
+	if (XST_SUCCESS != metal_init(&init_param)) {
+		(void)printf("ERROR: Failed to run metal initialization\r\n");
 		return XST_FAILURE;
 	}
 
 	/* Initialize the instance of channel filter driver */
 	InstancePtr1 = XDfeCcf_InstanceInit(XDFECCF_NODE1_NAME);
 	InstancePtr2 = XDfeCcf_InstanceInit(XDFECCF_NODE2_NAME);
+
+	/* Get SW and HW version numbers */
+	XDfeCcf_GetVersions(InstancePtr1, &SwVersion, &HwVersion);
+	printf("SW Version: Major %d, Minor %d\r\n", SwVersion.Major,
+	       SwVersion.Minor);
+	printf("HW Version: Major %d, Minor %d, Revision %d, Patch %d\r\n",
+	       HwVersion.Major, HwVersion.Minor, HwVersion.Revision,
+	       HwVersion.Patch);
+
 	/* Go through initialization states of the state machine */
 	XDfeCcf_Reset(InstancePtr1);
 	XDfeCcf_Configure(InstancePtr1, &Cfg);
 	XDfeCcf_Initialize(InstancePtr1, &Init);
-	XDfeCcf_Activate(InstancePtr1, true);
+	XDfeCcf_Activate(InstancePtr1, false);
 
 	XDfeCcf_Reset(InstancePtr2);
 	XDfeCcf_Configure(InstancePtr2, &Cfg);
 	XDfeCcf_Initialize(InstancePtr2, &Init);
-	XDfeCcf_Activate(InstancePtr2, true);
-
-	/* Write and read a dummy Coefficient[0] value */
-	XDfeCcf_WriteReg(InstancePtr1, XDFECCF_COEFF_VALUE, 0x1234);
-	if (0x1234 != XDfeCcf_ReadReg(InstancePtr1, XDFECCF_COEFF_VALUE)) {
-		return XST_FAILURE;
-	}
-
-	/* Write and read a dummy Coefficient[0] value */
-	XDfeCcf_WriteReg(InstancePtr2, XDFECCF_COEFF_VALUE, 0x4321);
-	if (0x4321 != XDfeCcf_ReadReg(InstancePtr2, XDFECCF_COEFF_VALUE)) {
-		return XST_FAILURE;
-	}
+	XDfeCcf_Activate(InstancePtr2, false);
 
 	XDfeCcf_Deactivate(InstancePtr1);
 	XDfeCcf_Deactivate(InstancePtr2);
 	XDfeCcf_InstanceClose(InstancePtr1);
 	XDfeCcf_InstanceClose(InstancePtr2);
+
+	printf("Channel Filter: Multi Instances Example pass\r\n");
+
 	return XST_SUCCESS;
 }
 
 /****************************************************************************/
 /**
 *
-* This function runs a test on the DFE Channel Filter device using the
-* driver APIs.
+* This function runs the DFE Channel Filter device using the driver APIs.
 * This function does the following tasks:
 *	- Create and system initialize the device driver instance.
+*	- Read SW and HW version numbers.
 *	- Reset the device.
 *	- Configure the device.
 *	- Initialize the device.
-	- Set the triggers
+*	- Set the triggers
 *	- Activate the device.
 *	- Load a channel filter coefficients.
-	- Add Component Channel.
+*	- Add Component Channel.
 *	- DeActivate the device.
 *
 * @return
 *		- XST_SUCCESS if the example has completed successfully.
 *		- XST_FAILURE if the example has failed.
 *
-* @note   	None
-*
 ****************************************************************************/
-static int XDfeCcf_PassThroughTestExample()
+static int XDfeCcf_PassThroughExample()
 {
 	struct metal_init_params init_param = METAL_INIT_DEFAULTS;
 	XDfeCcf_Cfg Cfg;
 	XDfeCcf *InstancePtr = NULL;
-	XDfeCcf_Init Init = { { 4, { 0 } }, 1 };
+	XDfeCcf_Init Init;
+	u32 Shift;
+	u32 CCID;
+	u32 BitSequence;
 	XDfeCcf_TriggerCfg TriggerCfg;
 	XDfeCcf_Coefficients Coeffs = {
-		7, 1, { 0, 0, 0, (2 << 15) - 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+		0, 0, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
 	};
 	XDfeCcf_CarrierCfg CarrierCfg = { 0, 0, 0, 1, 0, 0, 0 };
+	XDfeCcf_Version SwVersion;
+	XDfeCcf_Version HwVersion;
+
+	printf("\r\nChannel Filter: Pass through example\r\n");
 
 	/* Initialize libmetal */
-	if (0 != metal_init(&init_param)) {
-		(void)printf("ERROR: Failed to run metal initialization\n");
+	if (XST_SUCCESS != metal_init(&init_param)) {
+		(void)printf("ERROR: Failed to run metal initialization\r\n");
 		return XST_FAILURE;
 	}
 
 	/* Initialize the instance of channel filter driver */
 	InstancePtr = XDfeCcf_InstanceInit(XDFECCF_NODE1_NAME);
 
+	/* Get SW and HW version numbers */
+	XDfeCcf_GetVersions(InstancePtr, &SwVersion, &HwVersion);
+	printf("SW Version: Major %d, Minor %d\r\n", SwVersion.Major,
+	       SwVersion.Minor);
+	printf("HW Version: Major %d, Minor %d, Revision %d, Patch %d\r\n",
+	       HwVersion.Major, HwVersion.Minor, HwVersion.Revision,
+	       HwVersion.Patch);
+
 	/* Go through initialization states of the state machine */
+	/* Reset */
 	XDfeCcf_Reset(InstancePtr);
+	/* Configure */
 	XDfeCcf_Configure(InstancePtr, &Cfg);
+	/* Initialise */
+	Init.GainStage = 1;
+	Init.Sequence.Length = 16;
 	XDfeCcf_Initialize(InstancePtr, &Init);
+
+	/* Set trigger */
+	TriggerCfg.Activate.Mode = 1U;
+	TriggerCfg.Activate.TuserEdgeLevel = 0;
+	TriggerCfg.Activate.TUSERBit = 0;
+	TriggerCfg.CCUpdate.Mode = 1U;
+	TriggerCfg.CCUpdate.TuserEdgeLevel = 1U;
+	TriggerCfg.CCUpdate.TUSERBit = 0;
 	XDfeCcf_SetTriggersCfg(InstancePtr, &TriggerCfg);
-	XDfeCcf_Activate(InstancePtr, true);
 
-	/* Set coefficents and add channel */
-	XDfeCcf_LoadCoefficients(InstancePtr, 0, &Coeffs);
-	XDfeCcf_AddCC(InstancePtr, 0, 0x7, &CarrierCfg);
+	/* Activate - disable low power */
+	XDfeCcf_Activate(InstancePtr, false);
 
+	/* Set coefficents */
+	Shift = 5;
+	Coeffs.Num = 7U;
+	Coeffs.Symmetric = 1U;
+	Coeffs.Value[1] = 1 << 15;
+	Coeffs.Value[2] = 1 << 13;
+	XDfeCcf_LoadCoefficients(InstancePtr, 1, Shift, &Coeffs);
+
+	/* Clear events */
+	XDfeCcf_ClearEventStatus(InstancePtr);
+	usleep(10000U); /* Give trigger time to finish */
+
+	/* Add component carrier */
+	CCID = 0;
+	BitSequence = 0xffff;
+	CarrierCfg.Rate = 16U;
+	CarrierCfg.Gain = round(1024*8);
+	CarrierCfg.ImagCoeffSet = 0;
+	CarrierCfg.RealCoeffSet = 0;
+	XDfeCcf_AddCC(InstancePtr, CCID, BitSequence, &CarrierCfg);
+
+	/* Close and exit */
 	XDfeCcf_Deactivate(InstancePtr);
 	XDfeCcf_InstanceClose(InstancePtr);
+
+	printf("Pass Through Example pass\r\n");
 	return XST_SUCCESS;
 }
