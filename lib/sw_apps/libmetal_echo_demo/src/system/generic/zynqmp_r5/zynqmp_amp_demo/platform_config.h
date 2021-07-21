d0e1b3fdf0cd139f2e72a71364463ff4d493b4a9
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2021-07-21 12:22:49 +0530

    sw_apps: libmetal_echo_demo: Add copyright for the missing files
    This commit adds copyright for the missing files.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

--- lib/sw_apps/libmetal_echo_demo/src/system/generic/zynqmp_r5/zynqmp_amp_demo/platform_config.h
+++ lib/sw_apps/libmetal_echo_demo/src/system/generic/zynqmp_r5/zynqmp_amp_demo/platform_config.h
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2021 Xilinx, Inc. and Contributors. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 #ifndef __PLATFORM_CONFIG_H_
 #define __PLATFORM_CONFIG_H_
 
