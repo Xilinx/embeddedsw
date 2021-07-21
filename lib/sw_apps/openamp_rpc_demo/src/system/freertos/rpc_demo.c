117fdc6763f6b5f216636f01132ba2f446e4f8ee
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2021-07-21 12:22:50 +0530

    sw_apps: openamp_rpc_demo: Add copyright for the missing files
    This commit adds copyright for the missing files.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

--- lib/sw_apps/openamp_rpc_demo/src/system/freertos/rpc_demo.c
+++ lib/sw_apps/openamp_rpc_demo/src/system/freertos/rpc_demo.c
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2021 Xilinx, Inc. and Contributors. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 /* This is a sample demonstration application that showcases usage of proxy from the remote core.
  This application is meant to run on the remote CPU running baremetal.
  This applicationr can print to to master console and perform file I/O using proxy mechanism. */
