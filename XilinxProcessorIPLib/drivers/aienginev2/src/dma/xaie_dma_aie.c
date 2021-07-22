dc5adea5a2e791de6b6095edeed350133ae642fa
Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>
2021-07-22 08:48:19 -0700

    aienginev2: src: fix dma iteration stepsize data type
    shim dma and memtile dma modules have iteration stepsize greater than 16bits.
    change data type to u32 in apis to handle this.
    
    Signed-off-by: Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>
    
    Acked-by: Nishad Saraf <nishad.saraf@xilinx.com>
    
    Acked-by: Wendy Liang <wendy.liang@xilinx.com>

--- XilinxProcessorIPLib/drivers/aienginev2/src/dma/xaie_dma_aie.c
+++ XilinxProcessorIPLib/drivers/aienginev2/src/dma/xaie_dma_aie.c
@@ -754,7 +754,7 @@ AieRC _XAie_ShimDmaUpdateBdAddr(XAie_DevInst *DevInst,
 * @note		Internal only.
 *
 ******************************************************************************/
-AieRC _XAie_DmaSetBdIteration(XAie_DmaDesc *DmaDesc, u16 StepSize, u8 Wrap,
+AieRC _XAie_DmaSetBdIteration(XAie_DmaDesc *DmaDesc, u32 StepSize, u8 Wrap,
 		u8 IterCurr)
 {
 	(void)DmaDesc;
