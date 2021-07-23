c197c5ab1d88606f6213a2a25228889fe8deb732
Ronak Jain <ronak.jain@xilinx.com>
2021-07-22 23:34:51 -0700

    sw_apps: zynqmp_pmufw: Add runtime support of OT feature
    Add implementation of over temperature feature to support at runtime.
     The feature is macro protected due to memory constraint.
    
    Signed-off-by: Ronak Jain <ronak.jain@xilinx.com>
    
    Acked-for-series: Jyotheeswar Reddy Mutthareddyvari <jyotheeswar.reddy.mutthareddyvari@xilinx.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_mod_overtemp.c
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_mod_overtemp.c
@@ -39,7 +39,12 @@
  * Calculate the OT Register Value as per EQN 2-12 in UG580
  * LSB 4 bits should be equal to 0x3 to enable OT alarm
  */
+#ifdef ENABLE_RUNTIME_OVERTEMP
+#define OT_REG_VAL(value) (((u32)(( (float)(value) + 280.23087870f)* 4096.0f / 509.3140064f) <<4) | 0x3U)
+static u32 OverTempLimit = (u32)OVERTEMP_DEGC;
+#else
 #define OT_REG_VAL (((u32)(( (float)OVERTEMP_DEGC + 280.23087870f)* 4096.0f / 509.3140064f) <<4) | 0x3U)
+#endif /* ENABLE_RUNTIME_OVERTEMP */
 
 #ifndef ENABLE_RUNTIME_OVERTEMP
 static void InitOverTempAlarm(void)
@@ -62,24 +67,132 @@ static void OverTempHandler(u8 ErrorId)
 }
 
 #ifdef ENABLE_RUNTIME_OVERTEMP
+/****************************************************************************/
+/**
+ * @brief  Set OT register value at runtime using an IOCTL calls.
+ *
+ * @param  DegCel The OT value in degree Celsius
+ *
+ * @return None.
+ *
+ * @note   None.
+ *
+ ****************************************************************************/
 void SetOverTempLimit(u32 DegCel)
 {
-	(void)DegCel;
+	OverTempLimit = DegCel;
 }
 
+/****************************************************************************/
+/**
+ * @brief  Get OT register value.
+ *
+ * @param  None.
+ *
+ * @return OT reg value
+ *
+ * @note   None.
+ *
+ ****************************************************************************/
 u32 GetOverTempLimit(void)
 {
-	return 0;
+	return OverTempLimit;
 }
 
+static void DeInitOverTempAlarm(void)
+{
+	/* Set OT alarm disable bit to disable alarm */
+	XPfw_RMW32(AMS_PSSYSMON_CONFIG_REG1, AMS_CONFIG_REG1_OT_DIS_MASK, 0x1U);
+}
+
+static void InitOverTempAlarm(void)
+{
+	u32 OtLimit = GetOverTempLimit();
+
+	/* Clear OT alarm disable bit */
+	XPfw_RMW32(AMS_PSSYSMON_CONFIG_REG1, AMS_CONFIG_REG1_OT_DIS_MASK, 0x0U);
+	/* Disable hysteresis mode */
+	XPfw_Write32(AMS_PSSYSMON_ALARM_OT_LOWER, 0x1U);
+	/* Set OT alarm upper threshold */
+	XPfw_Write32(AMS_PSSYSMON_ALARM_OT_UPPER, OT_REG_VAL(OtLimit));
+}
+
+/****************************************************************************/
+/**
+ * @brief  This function registers over temperature handler.
+ *
+ * @param  None.
+ *
+ * @return XST_SUCCESS if successful else XST_FAILURE or an error
+ * 	   code or a reason code
+ *
+ * @note   None.
+ *
+ ****************************************************************************/
 s32 OverTempCfgInit(void)
 {
-	return XST_FAILURE;
+	s32 Status = XST_FAILURE;
+
+	/* Register Over Temperature handler for FPD event */
+	Status = XPfw_EmSetAction(EM_ERR_ID_FPD_TEMP,
+				  EM_ACTION_CUSTOM,
+				  OverTempHandler);
+	if (XST_SUCCESS != Status) {
+		XPfw_Printf(DEBUG_DETAILED,"Over-Temp: Failed to register FPD OT Handler\r\n");
+		goto done;
+	}
+
+	/* Register Over Temperature handler for LPD event */
+	Status = XPfw_EmSetAction(EM_ERR_ID_LPD_TEMP,
+				  EM_ACTION_CUSTOM,
+				  OverTempHandler);
+	if (XST_SUCCESS != Status) {
+		XPfw_Printf(DEBUG_DETAILED,"Over-Temp: Failed to register LPD OT Handler\r\n");
+		goto done;
+	}
+
+	/* Initialize SYSMON OT Alarm */
+	InitOverTempAlarm();
+
+done:
+	return Status;
 }
 
+/****************************************************************************/
+/**
+ * @brief  This function de-registers over temperature handler.
+ *
+ * @param  None.
+ *
+ * @return XST_SUCCESS if successful else XST_FAILURE or an error
+ * 	   code or a reason code
+ *
+ * @note   None.
+ *
+ ****************************************************************************/
 s32 OverTempCfgDeInit(void)
 {
-	return XST_FAILURE;
+	s32 Status = XST_FAILURE;
+
+	/* De-Initialize SYSMON OT Alarm */
+	DeInitOverTempAlarm();
+
+	/* De-Register Over Temperature handler for LPD event */
+	Status = XPfw_EmDisable(EM_ERR_ID_LPD_TEMP);
+	if (XST_SUCCESS != Status) {
+		XPfw_Printf(DEBUG_DETAILED,"Over-Temp: Failed to de-register LPD OT Handler\r\n");
+		goto done;
+	}
+
+	/* De-Register Over Temperature handler for FPD event */
+	Status = XPfw_EmDisable(EM_ERR_ID_FPD_TEMP);
+	if (XST_SUCCESS != Status) {
+		XPfw_Printf(DEBUG_DETAILED,"Over-Temp: Failed to de-register FPD OT Handler\r\n");
+		goto done;
+	}
+
+done:
+	return Status;
 }
 #else
 static void OverTempCfgInit(const XPfw_Module_t *ModPtr, const u32 *CfgData, u32 Len)
