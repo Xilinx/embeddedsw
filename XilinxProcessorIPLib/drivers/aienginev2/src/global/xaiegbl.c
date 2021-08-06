Xilinx/aie-rt

aienginev2: global: only includes required device gen modules
Add a macro to enable user to only build with single device generation
modules, so that when user build AI engine application, the data or
functions which are not related to the specified device generation
will not be included in the application.

Signed-off-by: Wendy Liang <wendy.liang@xilinx.com>
Acked-by: Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>
Acked-by: Pramod Peethambaran <pramod.peethambaran@xilinx.com>

driver/src/global/xaiegbl.c
@@ -54,6 +54,22 @@ extern XAie_TileMod Aie2IpuMod[XAIEGBL_TILE_TYPE_MAX];
 extern XAie_DeviceOps AieDevOps;
 extern XAie_DeviceOps AieMlDevOps;
 extern XAie_DeviceOps Aie2IpuDevOps;
+
+#if XAIE_DEV_SINGLE_GEN == XAIE_DEV_GEN_AIE2IPU
+#define XAIE_DEV_SINGLE_MOD Aie2IpuMod
+#define XAIE_DEV_SINGLE_DEVOPS Aie2IpuDevOps
+#elif XAIE_DEV_SINGLE_GEN == XAIE_DEV_GEN_AIE2
+#define XAIE_DEV_SINGLE_MOD Aie2Mod
+#define XAIE_DEV_SINGLE_DEVOPS AieMlDevOps
+#elif XAIE_DEV_SINGLE_GEN == XAIE_DEV_GEN_AIE
+#define XAIE_DEV_SINGLE_MOD AieMod
+#define XAIE_DEV_SINGLE_DEVOPS AieDevOps
+#else
+#ifdef XAIE_DEV_SINGLE_GEN
+#error "Unsupported device defined."
+#endif
+#endif
+
 /************************** Function Definitions *****************************/
 /*****************************************************************************/
 /**
@@ -88,6 +104,12 @@ AieRC XAie_CfgInitialize(XAie_DevInst *InstPtr, XAie_Config *ConfigPtr)
 		return XAIE_OK;
 
 	/* Initialize device property according to Device Type */
+#ifdef XAIE_DEV_SINGLE_GEN
+	if (ConfigPtr->AieGen == XAIE_DEV_SINGLE_GEN) {
+		InstPtr->DevProp.DevMod = XAIE_DEV_SINGLE_MOD;
+		InstPtr->DevProp.DevGen = XAIE_DEV_SINGLE_GEN;
+		InstPtr->DevOps = &XAIE_DEV_SINGLE_DEVOPS;
+#else
 	if(ConfigPtr->AieGen == XAIE_DEV_GEN_AIE2) {
 		InstPtr->DevProp.DevMod = Aie2Mod;
 		InstPtr->DevProp.DevGen = XAIE_DEV_GEN_AIE2;
@@ -100,6 +122,7 @@ AieRC XAie_CfgInitialize(XAie_DevInst *InstPtr, XAie_Config *ConfigPtr)
 		InstPtr->DevProp.DevMod = Aie2IpuMod;
 		InstPtr->DevProp.DevGen = XAIE_DEV_GEN_AIE2IPU;
 		InstPtr->DevOps = &Aie2IpuDevOps;
+#endif
 	} else {
 		XAIE_ERROR("Invalid device\n",
 				XAIE_INVALID_DEVICE);

Xilinx/embeddedsw

XilinxProcessorIPLib/drivers/aienginev2/src/global/xaiegbl.c
@@ -52,6 +52,19 @@ extern XAie_TileMod Aie2Mod[XAIEGBL_TILE_TYPE_MAX];
 
 extern XAie_DeviceOps AieDevOps;
 extern XAie_DeviceOps AieMlDevOps;
+
+#if XAIE_DEV_SINGLE_GEN == XAIE_DEV_GEN_AIE2
+#define XAIE_DEV_SINGLE_MOD Aie2Mod
+#define XAIE_DEV_SINGLE_DEVOPS AieMlDevOps
+#elif XAIE_DEV_SINGLE_GEN == XAIE_DEV_GEN_AIE
+#define XAIE_DEV_SINGLE_MOD AieMod
+#define XAIE_DEV_SINGLE_DEVOPS AieDevOps
+#else
+#ifdef XAIE_DEV_SINGLE_GEN
+#error "Unsupported device defined."
+#endif
+#endif
+
 /************************** Function Definitions *****************************/
 /*****************************************************************************/
 /**
@@ -86,6 +99,12 @@ AieRC XAie_CfgInitialize(XAie_DevInst *InstPtr, XAie_Config *ConfigPtr)
 		return XAIE_OK;
 
 	/* Initialize device property according to Device Type */
+#ifdef XAIE_DEV_SINGLE_GEN
+	if (ConfigPtr->AieGen == XAIE_DEV_SINGLE_GEN) {
+		InstPtr->DevProp.DevMod = XAIE_DEV_SINGLE_MOD;
+		InstPtr->DevProp.DevGen = XAIE_DEV_SINGLE_GEN;
+		InstPtr->DevOps = &XAIE_DEV_SINGLE_DEVOPS;
+#else
 	if(ConfigPtr->AieGen == XAIE_DEV_GEN_AIE2) {
 		InstPtr->DevProp.DevMod = Aie2Mod;
 		InstPtr->DevProp.DevGen = XAIE_DEV_GEN_AIE2;
@@ -94,6 +113,7 @@ AieRC XAie_CfgInitialize(XAie_DevInst *InstPtr, XAie_Config *ConfigPtr)
 		InstPtr->DevProp.DevMod = AieMod;
 		InstPtr->DevProp.DevGen = XAIE_DEV_GEN_AIE;
 		InstPtr->DevOps = &AieDevOps;
+#endif
 	} else {
 		XAIE_ERROR("Invalid device\n",
 				XAIE_INVALID_DEVICE);