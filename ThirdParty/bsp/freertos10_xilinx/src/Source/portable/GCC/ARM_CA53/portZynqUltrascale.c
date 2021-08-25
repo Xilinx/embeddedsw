3fef3630bfe80bbb693c2b4d6028f5a4c62336f5
Shravya <shravya.kumbham@xilinx.com>
2021-08-25 12:13:22 +0530

    freertos10_xilinx: Fix compilation errors with freertos os
    XScuGic_ConfigTable is declared as extern in xscugic.h to fix
    misra-c violations. This fix generates compilation error for
    versal, zynqmp and zynq platforms when built with freertos
    as XScuGic_ConfigTable is declared as extern const in freertos.
    Remove const to fix the compilation errors.
    
    portZynqUltrascale.c:133:29: error: conflicting types for
    'XScuGic_ConfigTable'.
    
    Signed-off-by: Shravya Kumbham <shravya.kumbham@xilinx.com>
    Acked-by:  Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>

--- ThirdParty/bsp/freertos10_xilinx/src/Source/portable/GCC/ARM_CA53/portZynqUltrascale.c
+++ ThirdParty/bsp/freertos10_xilinx/src/Source/portable/GCC/ARM_CA53/portZynqUltrascale.c
@@ -108,7 +108,7 @@ void FreeRTOS_ClearTickInterrupt( void )
 
 void vApplicationIRQHandler( uint32_t ulICCIAR )
 {
-extern const XScuGic_Config XScuGic_ConfigTable[];
+extern XScuGic_Config XScuGic_ConfigTable[];
 static const XScuGic_VectorTableEntry *pxVectorTable = XScuGic_ConfigTable[ XPAR_SCUGIC_SINGLE_DEVICE_ID ].HandlerTable;
 uint32_t ulInterruptID;
 const XScuGic_VectorTableEntry *pxVectorEntry;
