c20b0fdd86b4bcba8cd43aca100b98a61997110e
bhawanas <bhawana.sahu@xilinx.com>
2021-09-02 14:04:38 +0530

    Updated src files addtogroup versions with mdd version for v_warp_init driver 2021.2
    Updated src files addtogroup version with mdd version for v_warp_init
    
    Signed-off-by: bhawanas <bhawana.sahu@xilinx.com>
    
    Acked-by: Siva Addepalli <sivaprasad.addepalli@xilinx.com>

--- XilinxProcessorIPLib/drivers/v_warp_init/src/xv_warp_init_intr.c
+++ XilinxProcessorIPLib/drivers/v_warp_init/src/xv_warp_init_intr.c
@@ -7,7 +7,7 @@
 /**
  *
  * @file xv_warp_init_intr.c
- * @addtogroup v_warp_init_v1_0
+ * @addtogroup v_warp_init_v1_1
  * @{
  *
  * The functions in this file provides interrupt handler and associated
