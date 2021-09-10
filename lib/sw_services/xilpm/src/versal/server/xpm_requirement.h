3c89bec699560fc914cdc7fd81db92b95f1fc577
Madhav Bhatt <madhav.bhatt@xilinx.com>
2021-09-09 19:56:16 -0700

    xilpm: versal: server: Resolve compile time warnings
    Resolve warnings, treated as errors after adding missing compiler
    options.
    
    Signed-off-by: Madhav Bhatt <madhav.bhatt@xilinx.com>
    
    Acked-for-series: Rajan Vaja <rajan.vaja@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_requirement.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_requirement.h
@@ -17,13 +17,18 @@ extern "C" {
 typedef struct XPm_ReqmInfo XPm_ReqmInfo;
 typedef struct XPm_Reqm XPm_Requirement;
 
+/* Size of bit fields for XPm_ReqmInfo structure */
+#define REQ_INFO_CAPS_BIT_FIELD_SIZE		4
+#define REQ_INFO_LATENCY_BIT_FIELD_SIZE		21
+#define REQ_INFO_RESERVED_BIT_FIELD_SIZE	7
+
 /**
  * Specific requirement information.
  */
 struct XPm_ReqmInfo {
-	u32 Capabilities:4; /**< Device capabilities (1-hot) */
-	u32 Latency:21; /**< Maximum device latency */
-	u32 Reserved:7; /**< Reserved for future use */
+	u32 Capabilities:REQ_INFO_CAPS_BIT_FIELD_SIZE; /**< Device capabilities (1-hot) */
+	u32 Latency:REQ_INFO_LATENCY_BIT_FIELD_SIZE; /**< Maximum device latency */
+	u32 Reserved:REQ_INFO_RESERVED_BIT_FIELD_SIZE; /**< Reserved for future use */
 	u32 QoS; /**< QoS requirement */
 };
 
