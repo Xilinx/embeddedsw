277bf8ec6fa1cae58159f3d93de1454abbc98885
Ben Levinsky <ben.levinsky@xilinx.com>
2021-10-05 07:45:11 -0700

    xilpm: versal: server: Remove default subsystem in permission schemes
    Presently there is special case handling for default subsystem for
    xGGS, subsystem and reset permissions schemes.
    
    Remove special cases of default subsystem for each.
    
    Signed-off-by: Ben Levinsky <ben.levinsky@xilinx.com>
    
    Acked-for-series: Izhar Ameer Shaikh <izhar.ameer.shaikh@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_subsystem.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_subsystem.h
@@ -17,8 +17,8 @@ extern "C" {
 
 #define	INVALID_SUBSYSID			(0xFFFFFFFFU)
 #define MAX_NUM_SUBSYSTEMS			(16U)
-#define SUBSYS_TO_NS_BITPOS(x)			(NODE_INDEX_MASK & ((x) - 2U))
-#define SUBSYS_TO_S_BITPOS(x)			(NODE_INDEX_MASK & ((x) - 2U + MAX_NUM_SUBSYSTEMS))
+#define SUBSYS_TO_NS_BITPOS(x)			(NODE_INDEX_MASK & ((x)))
+#define SUBSYS_TO_S_BITPOS(x)			(NODE_INDEX_MASK & ((x) + MAX_NUM_SUBSYSTEMS))
 
 /**
  * Subsystem specific flags.
