1f27260d19f8716de7f61d932b582ea07aa58f74
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2021-11-30 18:12:51 +0530

    drivers: scutimer: Add support for generic interrupt wrapper
    This commit adds support for generic interrupt wrapper in the driver.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
    
    Acked-for-series: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>

--- XilinxProcessorIPLib/drivers/scutimer/data/scutimer.tcl
+++ XilinxProcessorIPLib/drivers/scutimer/data/scutimer.tcl
@@ -17,9 +17,22 @@
 
 proc generate {drv_handle} {
     ::hsi::utils::define_zynq_include_file $drv_handle "xparameters.h" "XScuTimer" "NUM_INSTANCES" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR"
-
-    ::hsi::utils::define_zynq_config_file $drv_handle "xscutimer_g.c" "XScuTimer" "DEVICE_ID" "C_S_AXI_BASEADDR"
-
+    set intr_wrap [common::get_property CONFIG.xil_interrupt [hsi::get_os]]
+    if { [string match -nocase $intr_wrap "true"] > 0} {
+        ::hsi::utils::define_zynq_config_file $drv_handle "xscutimer_g.c" "XScuTimer" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_INTERRUPT" "C_INTR_PARENT"
+    } else {
+        ::hsi::utils::define_zynq_config_file $drv_handle "xscutimer_g.c" "XScuTimer" "DEVICE_ID" "C_S_AXI_BASEADDR"
+    }
     ::hsi::utils::define_zynq_canonical_xpars $drv_handle "xparameters.h" "XScuTimer" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR"
 
+    if { [string match -nocase $intr_wrap "true"] > 0} {
+        foreach i [get_sw_cores standalone*] {
+            set intr_wrapper_tcl_file "[get_property "REPOSITORY" $i]/data/intr_wrapper.tcl"
+            if {[file exists $intr_wrapper_tcl_file]} {
+                source $intr_wrapper_tcl_file
+                break
+            }
+        }
+	gen_intr $drv_handle "xparameters.h"
+    }
 }
