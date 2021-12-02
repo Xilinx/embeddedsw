27f1dbd760ca4a546dba16e17e9367be424f6203
Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
2021-12-02 13:03:40 +0530

    clockps: Update driver version and copyright year
    Update the resetps driver version to 1.4, update the copyright year and addtogroup to group with the latest information.
    
    Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@xilinx.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@xilinx.com>

--- XilinxProcessorIPLib/drivers/clockps/src/xclockps.c
+++ XilinxProcessorIPLib/drivers/clockps/src/xclockps.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2018 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2018 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xclockps.c
-* @addtogroup xclockps_v1_3
+* @addtogroup xclockps_v1_4
 * @{
 *
 * Contains the implementation of interface functions of the clock driver.
--- XilinxProcessorIPLib/drivers/clockps/src/xclockps_sinit.c
+++ XilinxProcessorIPLib/drivers/clockps/src/xclockps_sinit.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2018 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2018 - 2021 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xclockps_sinit.c
-* @addtogroup xclockps_v1_3
+* @addtogroup xclockps_v1_4
 * @{
 *
 * This file contains method for static initialization (compile-time) of the
