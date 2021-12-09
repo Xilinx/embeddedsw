71741cf5771136f540eddb5ea690ad37c615e92f
Nishad Saraf <nishad.saraf@xilinx.com>
2021-12-08 19:03:16 -0800

    aienginev2: src: Add support to implement lightweight APIs
    This patch implements a lightweight version of driver APIs optimized for code
    size and performance.
    
    Signed-off-by: Nishad Saraf <nishad.saraf@xilinx.com>
    Signed-off-by: Wendy Liang <wendy.liang@xilinx.com>
    
    Acked-for-series: Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/aienginev2/src/lite/xaie_lite.h
@@ -0,0 +1,51 @@
+/******************************************************************************
+* Copyright (C) 2021 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+
+/*****************************************************************************/
+/**
+* @file xaie_lite.h
+* @{
+*
+* This header file defines a lightweight version of AIE driver APIs.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who     Date     Changes
+* ----- ------  -------- -----------------------------------------------------
+* 1.0  Nishad  08/30/2021  Initial creation
+* </pre>
+*
+******************************************************************************/
+#ifndef XAIE_LITE_H
+#define XAIE_LITE_H
+
+#ifdef XAIE_FEATURE_LITE
+
+#include "xaiegbl_defs.h"
+
+#define __FORCE_INLINE__			__attribute__((always_inline))
+#define _XAie_LSetRegField(Val, Lsb, Mask)	(((Val) << (Lsb)) & Mask)
+
+#if XAIE_DEV_SINGLE_GEN == XAIE_DEV_GEN_AIE
+#include "xaie_lite_aie.h"
+#elif XAIE_DEV_SINGLE_GEN == XAIE_DEV_GEN_AIEML
+#include "xaie_lite_aieml.h"
+#else
+#include <xaie_custom_device.h>
+#endif
+
+#define XAie_LDeclareDevInst(DevInst, _BaseAddr, _StartCol, _NumCols) \
+	XAie_DevInst DevInst = { \
+		.BaseAddr = (_BaseAddr), \
+		.NumCols = (_NumCols), \
+	}
+
+#endif /* XAIE_FEATURE_LITE */
+
+#endif /* XAIE_LITE_H */
+
+/** @} */
