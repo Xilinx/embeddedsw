0a4faba4bd79d5802e051bf65bd22b0a6f7d4328
Alex Kiani <alex.kiani@xilinx.com>
2021-12-20 10:14:47 -0800

    aiefal: test: update user event utest
    update user event utest to increase line coverage
    
    Signed-off-by: Alex Kiani <alex.kiani@xilinx.com>
    
    Acked-for-series: Wendy Liang <wendy.liang@xilinx.com>

--- XilinxProcessorIPLib/drivers/aiefal/tests/utests/tc/test_userevent.cpp
+++ XilinxProcessorIPLib/drivers/aiefal/tests/utests/tc/test_userevent.cpp
@@ -15,7 +15,7 @@ TEST_GROUP(UserEvent)
 {
 };
 
-TEST(UserEvent, Basic)
+TEST(UserEvent, UserEventBasic)
 {
 	AieRC RC;
 	XAie_Events E;
@@ -32,7 +32,7 @@ TEST(UserEvent, Basic)
 	CHECK_EQUAL(RC, XAIE_OK);
 	XAieDev Aie(&DevInst, true);
 
-	auto UserEvent = Aie.tile(1,1).core().userEvent();
+	auto UserEvent = Aie.tile(1,3).core().userEvent();
 	RC = UserEvent->getEvent(E);
 	CHECK_EQUAL(RC, XAIE_INVALID_ARGS);
 	RC = UserEvent->reserve();
@@ -46,35 +46,35 @@ TEST(UserEvent, Basic)
 	RC = UserEvent->release();
 	CHECK_EQUAL(RC, XAIE_OK);
 
-	auto UserEvent0 = Aie.tile(1,1).core().userEvent();
+	auto UserEvent0 = Aie.tile(1,3).core().userEvent();
 	RC = UserEvent0->reserve();
 	CHECK_EQUAL(RC, XAIE_OK);
 	RC = UserEvent0->getEvent(E);
 	CHECK_EQUAL(RC, XAIE_OK);
 	CHECK_EQUAL(E, XAIE_EVENT_USER_EVENT_0_CORE);
-	auto UserEvent1 = Aie.tile(1,1).core().userEvent();
+	auto UserEvent1 = Aie.tile(1,3).core().userEvent();
 	RC = UserEvent1->reserve();
 	CHECK_EQUAL(RC, XAIE_OK);
 	RC = UserEvent1->getEvent(E);
 	CHECK_EQUAL(RC, XAIE_OK);
 	CHECK_EQUAL(E, XAIE_EVENT_USER_EVENT_1_CORE);
-	auto UserEvent2 = Aie.tile(1,1).core().userEvent();
+	auto UserEvent2 = Aie.tile(1,3).core().userEvent();
 	RC = UserEvent2->reserve();
 	CHECK_EQUAL(RC, XAIE_OK);
 	RC = UserEvent2->getEvent(E);
 	CHECK_EQUAL(RC, XAIE_OK);
 	CHECK_EQUAL(E, XAIE_EVENT_USER_EVENT_2_CORE);
-	auto UserEvent3 = Aie.tile(1,1).core().userEvent();
+	auto UserEvent3 = Aie.tile(1,3).core().userEvent();
 	RC = UserEvent3->reserve();
 	CHECK_EQUAL(RC, XAIE_OK);
 	RC = UserEvent3->getEvent(E);
 	CHECK_EQUAL(RC, XAIE_OK);
 	CHECK_EQUAL(E, XAIE_EVENT_USER_EVENT_3_CORE);
-	auto UserEvent4 = Aie.tile(1,1).core().userEvent();
+	auto UserEvent4 = Aie.tile(1,3).core().userEvent();
 	RC = UserEvent4->reserve();
 	CHECK_EQUAL(RC, XAIE_INVALID_ARGS);
 
-	UserEvent = Aie.tile(1,1).mem().userEvent();
+	UserEvent = Aie.tile(1,3).mem().userEvent();
 	RC = UserEvent->reserve();
 	CHECK_EQUAL(RC, XAIE_OK);
 	RC = UserEvent->getEvent(E);
