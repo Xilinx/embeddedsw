19c943d0e3b2121b2206f3268554e9c933b8abd1
Alex Kiani <alex.kiani@xilinx.com>
2021-12-20 10:14:45 -0800

    aiefal: test: update stream switch utest
    update stream switch utest to increase line coverage
    
    Signed-off-by: Alex Kiani <alex.kiani@xilinx.com>
    
    Acked-for-series: Wendy Liang <wendy.liang@xilinx.com>

--- XilinxProcessorIPLib/drivers/aiefal/tests/utests/tc/test_ss.cpp
+++ XilinxProcessorIPLib/drivers/aiefal/tests/utests/tc/test_ss.cpp
@@ -17,8 +17,12 @@ TEST_GROUP(StreamSelect)
 };
 
 TEST(StreamSelect, SSBasic) {
-  AieRC RC;
+	AieRC RC;
 	std::vector<XAie_LocType> vL, vL1;
+	XAie_Events SSEvent;
+
+	uint32_t SSRscType;
+
 	XAie_ModuleType StartM, EndM, StartM1, EndM1;
 
 	XAie_SetupConfig(ConfigPtr, HW_GEN, XAIE_BASE_ADDR,
@@ -32,41 +36,125 @@ TEST(StreamSelect, SSBasic) {
 	RC = XAie_CfgInitialize(&(DevInst), &ConfigPtr);
 	CHECK_EQUAL(RC, XAIE_OK);
 
-  auto Aie = std::make_shared<XAieDev>(&DevInst, true);
-  auto Stream = Aie->tile(1,1).sswitchPort();
+	auto Aie = std::make_shared<XAieDev>(&DevInst, true);
+	auto Stream = Aie->tile(1,3).sswitchPort();
 
-  RC = Stream->reserve();
-  CHECK_EQUAL(RC, XAIE_OK);
+	RC = Stream->getSSIdleEvent(SSEvent);
+	CHECK_EQUAL(RC, XAIE_ERR);
 
-  RC = Stream->setPortToSelect(XAIE_STRMSW_SLAVE, CORE, 0);
-  CHECK_EQUAL(RC, XAIE_OK);
+	RC = Stream->reserve();
+	CHECK_EQUAL(RC, XAIE_OK);
 
-  RC = Stream->start();
-  CHECK_EQUAL(RC, XAIE_OK);
+	RC = Stream->getSSIdleEvent(SSEvent);
+	CHECK_EQUAL(RC, XAIE_OK);
 
-  RC = Stream->stop();
-  CHECK_EQUAL(RC, XAIE_OK);
+	RC = Stream->getSSRunningEvent(SSEvent);
+	CHECK_EQUAL(RC, XAIE_OK);
 
-  RC = Stream->release();
-  CHECK_EQUAL(RC, XAIE_OK);
+	RC = Stream->getSSStalledEvent(SSEvent);
+	CHECK_EQUAL(RC, XAIE_OK);
 
-  /*Test reserve with preferred resource Id*/
-  RC = Stream->setPreferredId(1);
-  CHECK_EQUAL(RC, XAIE_OK);
+	RC = Stream->getSSTlastEvent(SSEvent);
+	CHECK_EQUAL(RC, XAIE_OK);
 
-  RC = Stream->reserve();
-  CHECK_EQUAL(RC, XAIE_OK);
+	RC = Stream->setPortToSelect(XAIE_STRMSW_SLAVE, CORE, 0);
+	CHECK_EQUAL(RC, XAIE_OK);
 
-  RC = Stream->setPortToSelect(XAIE_STRMSW_SLAVE, CORE, 0);
-  CHECK_EQUAL(RC, XAIE_OK);
+	 RC = Stream->start();
+	CHECK_EQUAL(RC, XAIE_OK);
 
-  RC = Stream->start();
-  CHECK_EQUAL(RC, XAIE_OK);
+	RC = Stream->stop();
+	CHECK_EQUAL(RC, XAIE_OK);
 
-  RC = Stream->stop();
-  CHECK_EQUAL(RC, XAIE_OK);
+	RC = Stream->release();
+	CHECK_EQUAL(RC, XAIE_OK);
 
-  RC = Stream->release();
-  CHECK_EQUAL(RC, XAIE_OK);
+	/*Test reserve with preferred resource Id*/
+	RC = Stream->setPreferredId(1);
+	CHECK_EQUAL(RC, XAIE_OK);
+
+	RC = Stream->reserve();
+	CHECK_EQUAL(RC, XAIE_OK);
+
+	SSRscType = Stream->getRscType();
+
+	RC = Stream->setPortToSelect(XAIE_STRMSW_SLAVE, CORE, 0);
+	CHECK_EQUAL(RC, XAIE_OK);
+
+	RC = Stream->start();
+	CHECK_EQUAL(RC, XAIE_OK);
+
+	RC = Stream->stop();
+	CHECK_EQUAL(RC, XAIE_OK);
+
+	RC = Stream->release();
+	CHECK_EQUAL(RC, XAIE_OK);
+
+}
+
+TEST(StreamSelect, SSNegative) {
+	AieRC RC;
+	std::vector<XAie_LocType> vL, vL1;
+	XAie_Events SSEvent;
+
+	uint32_t SSRscType;
+
+	XAie_ModuleType StartM, EndM, StartM1, EndM1;
+
+	XAie_SetupConfig(ConfigPtr, HW_GEN, XAIE_BASE_ADDR,
+			XAIE_COL_SHIFT, XAIE_ROW_SHIFT,
+			XAIE_NUM_COLS, XAIE_NUM_ROWS, XAIE_SHIM_ROW,
+			XAIE_MEM_TILE_ROW_START, XAIE_MEM_TILE_NUM_ROWS,
+			XAIE_AIE_TILE_ROW_START, XAIE_AIE_TILE_NUM_ROWS);
+
+	XAie_InstDeclare(DevInst, &ConfigPtr);
+
+	RC = XAie_CfgInitialize(&(DevInst), &ConfigPtr);
+	CHECK_EQUAL(RC, XAIE_OK);
+
+	auto Aie = std::make_shared<XAieDev>(&DevInst, true);
+	auto Stream = Aie->tile(1,3).sswitchPort();
+
+	RC = Stream->setPortToSelect(XAIE_STRMSW_SLAVE, CORE, 0);
+	CHECK_EQUAL(RC, XAIE_OK);
+
+	RC = Stream->getSSIdleEvent(SSEvent);
+	CHECK_EQUAL(RC, XAIE_ERR);
+
+	DevInst.IsReady = 0;
+
+	RC = Stream->reserve();
+	CHECK_EQUAL(RC, XAIE_INVALID_ARGS);
+
+	DevInst.IsReady = 1;
+
+	RC = Stream->reserve();
+	CHECK_EQUAL(RC, XAIE_OK);
+
+	DevInst.IsReady = 0;
+
+	RC = Stream->start();
+	CHECK_EQUAL(RC, XAIE_INVALID_ARGS);
+
+	DevInst.IsReady = 1;
+
+	RC = Stream->start();
+	CHECK_EQUAL(RC, XAIE_OK);
+
+	RC = Stream->setPortToSelect(XAIE_STRMSW_SLAVE, CORE, 0);
+	CHECK_EQUAL(RC, XAIE_ERR);
+
+	DevInst.IsReady = 0;
+
+	RC = Stream->stop();
+	CHECK_EQUAL(RC, XAIE_INVALID_ARGS);
+
+	DevInst.IsReady = 1;
+
+	RC = Stream->stop();
+	CHECK_EQUAL(RC, XAIE_OK);
+
+	RC = Stream->release();
+	CHECK_EQUAL(RC, XAIE_OK);
 
 }
