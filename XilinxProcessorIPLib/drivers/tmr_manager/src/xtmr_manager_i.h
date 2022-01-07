f664b4597cf89096c7fba7e2ac99ef54c648ffb9
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2022-01-07 15:49:35 +0530

    XilinxProcessorIPLib: drivers: tmr_manager: Increment driver version
    This commit does the below
    --> Increments the driver version number.
    --> Updates addtogroup tags.
    --> Updates copyright year.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

--- XilinxProcessorIPLib/drivers/tmr_manager/src/xtmr_manager_i.h
+++ XilinxProcessorIPLib/drivers/tmr_manager/src/xtmr_manager_i.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2017 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2017 - 2022 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xtmr_manager_i.h
-* @addtogroup tmr_manager_v1_2
+* @addtogroup tmr_manager_v1_3
 * @{
 *
 * Contains data which is shared between the files of the XTMR_Manager component.
