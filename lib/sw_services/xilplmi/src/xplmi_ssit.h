4753f3212edf47d5267feda56bfc8cfb85bf5a68
Trung Tran <trung.tran@xilinx.com>
2022-01-12 00:14:41 -0800

    xplmi_ssit: enable ssit_sync per slave
    - This patch allow ssit_sync_slaves will wait exactly one slave SLR.
    - This patch use extra interrupt lines from master to slave as sync ID.
    - The interrupt line that is currently use for Error update from Master
    to slave now re-used as sync ID for SLR_3
    
    Signed-off-by: Trung Tran <trung.tran@xilinx.com>
    Acked-by: Mounika Akula <mounika.akula@xilinx.com>

--- lib/sw_services/xilplmi/src/xplmi_ssit.h
+++ lib/sw_services/xilplmi/src/xplmi_ssit.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (c) 2019 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (c) 2019 - 2022 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -23,6 +23,7 @@
 * 1.01  bsv  04/04/2020 Code clean up
 * 1.02  bm   10/14/2020 Code clean up
 *       ana  10/19/2020 Added doxygen comments
+* 1.04  tnt  01/10/2022 Added more constants to support ssit_sync_slave
 *
 * </pre>
 *
@@ -50,12 +51,20 @@ extern "C" {
  * SSIT defines
  */
 #define PMC_GLOBAL_SSIT_ERR_IRQ_OUT_0_MASK	(1U)
+#define PMC_GLOBAL_SSIT_ERR_IRQ_OUT_1_MASK	(2U)
 #define PMC_GLOBAL_SSIT_ERR_IRQ_OUT_2_MASK	(4U)
 #define PMC_GLOBAL_SSIT_ERR_MASK		(0xE0000000U)
 
 #define SSIT_SLAVE_0_MASK			(1U)
 #define SSIT_SLAVE_1_MASK			(2U)
 #define SSIT_SLAVE_2_MASK			(4U)
+
+#define SLR_TYPE_SSIT_DEV_MASTER_SLR		(0x6U)
+#define SLR_TYPE_SSIT_DEV_SLAVE_1_SLR_TOP	(0x5U)
+#define SLR_TYPE_SSIT_DEV_SLAVE_1_SLR_NTOP	(0x4U)
+#define SLR_TYPE_SSIT_DEV_SLAVE_2_SLR_TOP	(0x3U)
+#define SLR_TYPE_SSIT_DEV_SLAVE_2_SLR_NTOP	(0x2U)
+#define SLR_TYPE_SSIT_DEV_SLAVE_3_SLR_TOP	(0x1U)
 /**
  * @}
  * @endcond
