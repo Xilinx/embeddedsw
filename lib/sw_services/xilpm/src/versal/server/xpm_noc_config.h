67d380cb4a9d35e1e771541c91258603e51a616e
Trung Tran <trung.tran@xilinx.com>
2022-01-26 14:47:13 -0800

    xilpm:versal:server Add NoC repair code
    - Add NoC repair code
    - Call NoC repair right after NoC POR asserting during System Reset
    state.
    
    Signed-off-by: Trung Tran <trung.tran@xilinx.com>
    Acked-by: Jyotheeswar Reddy Mutthareddyvari <jyotheeswar.reddy.mutthareddyvari@xilinx.com>

---
+++ lib/sw_services/xilpm/src/versal/server/xpm_noc_config.h
@@ -0,0 +1,32 @@
+/******************************************************************************
+* Copyright (c) 2018 - 2022 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+/*****************************************************************************/
+/**
+*
+* @file xpm_noc_config.h
+*
+* NOC configuration header file
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver	Who  Date		 Changes
+* ----- ---- -------- -------------------------------------------------------
+* 1.0  rpoolla	 23/01/18 Initial release
+* 2.0  tnt	 01/11/22 Porting to xilpm
+* </pre>
+*
+* @note
+*
+******************************************************************************/
+#ifndef SRC_XPMCR_NOC_H_
+#define SRC_XPMCR_NOC_H_
+
+#include "xil_util.h"
+
+XStatus XPm_NoCConfig(void);
+
+#endif /* SRC_XPMCR_NOC_H_ */
