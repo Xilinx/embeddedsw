fbcf1d6b70ab7459cce16edeb14b2f0b13b83e62
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2022-01-31 10:59:48 +0530

    XilinxProcessorIPLib: drivers: axiethernet: Increment driver version
    This commit does the below
    --> Increments the driver version number.
    --> Updates addtogroup tags.
    --> Updates copyright year.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

--- XilinxProcessorIPLib/drivers/axiethernet/src/xaxiethernet.h
+++ XilinxProcessorIPLib/drivers/axiethernet/src/xaxiethernet.h
@@ -1,12 +1,12 @@
 /******************************************************************************
-* Copyright (C) 2010 - 2021 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2022 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
 /**
 *
 * @file xaxiethernet.h
-* @addtogroup axiethernet_v5_13
+* @addtogroup axiethernet_v5_14
 * @{
 * @details
 *
