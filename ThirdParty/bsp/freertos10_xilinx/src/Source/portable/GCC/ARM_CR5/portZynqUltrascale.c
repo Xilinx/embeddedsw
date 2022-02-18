df8dfff4a43bc6a25ff561334e02217f25a2cbda
Anirudha Sarangi <anirudha.sarangi@xilinx.com>
2022-02-18 18:55:35 +0530

    freertos10_xilinx: Improve upon the existing handling of static allocation of data structures
    FreeRTOS provides a provision where users can provide static memory for
    various kernel data structures like TCBs, QCBs, task stacks etc instead
    of leaving it to FreeRTOS kernel to allocate the memory dynamically.
    
    This is controlled by the user option support_static_allocation. When
    set to 1, FreeRTOS kernel expects that memory for various kernel data
    structures are provided by the users through their application code. This
    includes memory needed for managing kernel based tasks like idle task or
    timer task.
    
    For kernel based idle and timer tasks, it may not be very clear for
    users that they need to pass on the memory for these. This can result in
    hard to understand linker errors with the above option.
    
    The new implementation allocates memory for timer and idle tasks as part
    of the port/kernel. This also marks the newly added functions as weak
    so that users can still override these and provide their own static
    memory for idle and timer tasks if they wish to.
    
    The existing freertos hello world example is modified to support static
    allocation use cases and to support the use case when someone wishes to
    use freertos hello world example with support_static_allocation set as
    1. The static allocation example currently available as part of Xilinx
    provided FreeRTOS examples is still present to let users know the ideal
    way of using support_static_allocation option. In this example users are
    shown on how to provide memory for idle and timer tasks by overriding
    the kernel allocated memory (being added through this patch).
    
    Signed-off-by: Anirudha Sarangi <anirudha.sarangi@xilinx.com>
    
    Acked-for-series: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>

--- ThirdParty/bsp/freertos10_xilinx/src/Source/portable/GCC/ARM_CR5/portZynqUltrascale.c
+++ ThirdParty/bsp/freertos10_xilinx/src/Source/portable/GCC/ARM_CR5/portZynqUltrascale.c
@@ -1,7 +1,7 @@
 /*
  * FreeRTOS Kernel V10.4.6
  * Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
- * Copyright (C) 2014 - 2021 Xilinx, Inc. All rights reserved.
+ * Copyright (C) 2014 - 2022 Xilinx, Inc. All rights reserved.
  *
  * SPDX-License-Identifier: MIT
  *
@@ -56,6 +56,18 @@ void vApplicationTickHook( void ) __attribute__((weak));
 void vApplicationIdleHook( void ) __attribute__((weak));
 void vApplicationMallocFailedHook( void ) __attribute((weak));
 void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName ) __attribute__((weak));
+
+#if (configSUPPORT_STATIC_ALLOCATION == 1)
+void vApplicationGetTimerTaskMemory( StaticTask_t **ppxTimerTaskTCBBuffer,
+                                     StackType_t **ppxTimerTaskStackBuffer,
+                                     uint32_t *pulTimerTaskStackSize )
+								__attribute__((weak));
+
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer,
+                                    StackType_t **ppxIdleTaskStackBuffer,
+                                    uint32_t *pulIdleTaskStackSize )
+								__attribute__((weak));
+#endif
 
 #ifndef XPAR_XILTIMER_ENABLED
 /* Timer used to generate the tick interrupt. */
@@ -268,3 +280,48 @@ volatile char *pcOverflowingTaskName = pcTaskName;
 	portDISABLE_INTERRUPTS();
 	for( ;; );
 }
+
+#if (configSUPPORT_STATIC_ALLOCATION == 1)
+/* Buffers below are used for static memory allocation for idle
+ * task. */
+static StaticTask_t xIdleTaskTCB;
+static StackType_t  xIdleTaskStack[ configMINIMAL_STACK_SIZE ];
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer,
+                                    StackType_t **ppxIdleTaskStackBuffer,
+                                    uint32_t *pulIdleTaskStackSize )
+{
+	/* Pass out a pointer to the StaticTask_t structure in which the Idle task's
+    state will be stored. */
+    *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
+
+    /* Pass out the array that will be used as the Idle task's stack. */
+    *ppxIdleTaskStackBuffer = xIdleTaskStack;
+
+    /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
+    Note that, as the array is necessarily of type StackType_t,
+    configMINIMAL_STACK_SIZE is specified in words, not bytes. */
+    *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+}
+
+/*-----------------------------------------------*/
+/* Buffers below are used for static memory allocation for timer
+ * task. */
+static StaticTask_t xTimerTaskTCB;
+static StackType_t  xTimerTaskStack[ configTIMER_TASK_STACK_DEPTH ];
+void vApplicationGetTimerTaskMemory( StaticTask_t **ppxTimerTaskTCBBuffer,
+                                     StackType_t **ppxTimerTaskStackBuffer,
+                                     uint32_t *pulTimerTaskStackSize )
+{
+	/* Pass out a pointer to the StaticTask_t structure in which the Timer
+    task's state will be stored. */
+    *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
+
+    /* Pass out the array that will be used as the Timer task's stack. */
+    *ppxTimerTaskStackBuffer = xTimerTaskStack;
+
+    /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
+    Note that, as the array is necessarily of type StackType_t,
+    configTIMER_TASK_STACK_DEPTH is specified in words, not bytes. */
+    *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
+}
+#endif
