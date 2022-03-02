13b8f357d751b12a1c99fe1a828eb915f089e016
Shravya <shravya.kumbham@xilinx.com>
2022-03-02 17:13:53 +0530

    BSP: Typecast with unsigned int to fix misrac violation
    Typecast signed variables with unsigned to fix below misrac
    violations.
    
    misra_c_2012_rule_10_4_violation: Essential type of the
    left hand operand val (signed) is not the same as that
    of the right operand 4U(unsigned).
    
    Signed-off-by: Shravya Kumbham <shravya.kumbham@xilinx.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

--- lib/bsp/standalone/src/arm/cortexr5/xtime_l.h
+++ lib/bsp/standalone/src/arm/cortexr5/xtime_l.h
@@ -31,6 +31,8 @@
 * 7.5   mus    04/30/21  Moved pragma message from xtime_l.h to xtime_l.c, to avoid
 *                        displaying same warnings multiple times. It fixes CR#1090562.
 * 8.0   mus    07/06/21 Added support for VERSAL NET.
+* 8.0	sk	03/02/22 Update COUNTS_PER_USECOND macro to fix misra_c_2012_rule_
+* 			 10_4 violation.
 * </pre>
 *
 ******************************************************************************/
@@ -80,7 +82,7 @@ static inline u64 arch_counter_get_cntvct(void)
 #define COUNTS_PER_SECOND	((XPAR_CPU_CORTEXR5_0_CPU_CLK_FREQ_HZ + 32)/ 64)
 #endif
 
-#define COUNTS_PER_USECOND	((COUNTS_PER_SECOND + 500000) / 1000000)
+#define COUNTS_PER_USECOND	((COUNTS_PER_SECOND + 500000U) / 1000000U)
 
 #define IRQ_FIQ_MASK 	0xC0	/* Mask IRQ and FIQ interrupts in cpsr */
 
