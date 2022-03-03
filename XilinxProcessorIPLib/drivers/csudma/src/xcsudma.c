be87f8b50ffefc4be89f8de2d7e2fe3cfb7c8b6f
Shravya <shravya.kumbham@xilinx.com>
2022-03-03 12:59:12 +0530

    csudma: Update description under overview section
    Update file description under overview section based on
    review comments.
    
    Signed-off-by: Shravya Kumbham <shravya.kumbham@xilinx.com>
    
    Acked-for-series: Harini Katakam <harini.katakam@xilinx.com>

--- XilinxProcessorIPLib/drivers/csudma/src/xcsudma.c
+++ XilinxProcessorIPLib/drivers/csudma/src/xcsudma.c
@@ -11,8 +11,8 @@
 * @addtogroup Overview
 * @{
 *
-* This file contains the implementation of the interface functions for CSU_DMA
-* driver. Refer to the header file xcsudma.h for more detailed information.
+* This section contains the functions of the CSU_DMA driver.
+* Refer to the header file xcsudma.h for more detailed information.
 *
 * <pre>
 * MODIFICATION HISTORY:
@@ -37,6 +37,7 @@
 * 1.7	sk	08/26/20 Fix MISRA-C violations.
 * 1.7	sk	08/26/20 Remove busy check in SetConfig.
 * 1.11	sk	03/03/22 Replace driver version in addtogroup with Overview.
+* 1.11	sk	03/03/22 Update Overview section based on review comments.
 * </pre>
 *
 ******************************************************************************/
