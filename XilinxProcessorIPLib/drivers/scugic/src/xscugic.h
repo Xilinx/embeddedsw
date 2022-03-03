Xilinx/embeddedsw

commit f6978484de00d3714c4d8f45f2f47add67e6e889

scugic: Added support for VERSAL NET APU and RPU
-GIC600-AE is coupled with Cortex-A78.
-Cortex-R52 core has integrated interrupt controller (kind of
 similar to GIC500).
-Syntax to access RPU GIC CPU interface registers is different
 than APU counterpart

Signed-off-by: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>

Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

XilinxProcessorIPLib/drivers/scugic/src/xscugic.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2010 - 2021 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2022 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xscugic.h
-* @addtogroup scugic_v4_7
+* @addtogroup scugic_v5_0
 * @{
 * @details
 *
@@ -172,6 +172,7 @@
 * 4.6	sk   08/05/21 Fix scugic misrac violations.
 * 4.7   dp   11/22/21 Added new API XScuGic_IsInitialized() to check and return
 *                     the GIC initialization status.
+* 5.9   mus  22/02/22 Add support for VERSAL NET
 *
 * </pre>
 *
@@ -322,6 +323,7 @@ extern XScuGic_Config XScuGic_ConfigTable[];	/**< Config table */
 #define XScuGic_DistReadReg(InstancePtr, RegOffset) \
 (XScuGic_ReadReg(((InstancePtr)->Config->DistBaseAddress), (RegOffset)))
 
+#if defined (GICv3)
 /****************************************************************************/
 /**
 *
@@ -400,6 +402,30 @@ XSCUGIC_RDIST_OFFSET), (RegOffset)))
 (XScuGic_ReadReg((((InstancePtr)->Config->DistBaseAddress)+ \
 					XSCUGIC_RDIST_SGI_PPI_OFFSET), (RegOffset)))
 
+#if defined(ARMR52)
+#define XREG_ICC_SRE_EL1	"p15, 0, %0,  c12,  c12, 5"
+#define XREG_ICC_IGRPEN0_EL1	"p15, 0, %0,  c12,  c12, 6"
+#define XREG_ICC_IGRPEN1_EL1	"p15, 0, %0,  c12,  c12, 7"
+#define XREG_ICC_SGI0R_EL1	"p15, 2, %0,  %1,  c12"
+#define XREG_ICC_SGI1R_EL1	"p15, 0, %0,  %1,  c12"
+#define XREG_ICC_PMR_EL1	"p15, 0, %0,  c4,  c6, 0"
+#define XREG_ICC_IAR0_EL1	"p15, 0, %0,  c12,  c8, 0"
+#define XREG_ICC_EOIR0_EL1	"p15, 0, %0,  c12,  c8, 1"
+#define XREG_IMP_CBAR		"p15, 1, %0, c15, c3, 0"
+#else
+#define XREG_ICC_SRE_EL1	"S3_6_C12_C12_5"
+#define XREG_ICC_SRE_EL3	"S3_6_C12_C12_5"
+#define XREG_ICC_IGRPEN0_EL1	"S3_0_C12_C12_6"
+#define XREG_ICC_IGRPEN1_EL1	"S3_0_C12_C12_7"
+#define XREG_ICC_IGRPEN1_EL3	"S3_6_C12_C12_7"
+#define XREG_ICC_SGI0R_EL1	"S3_0_C12_C11_7"
+#define XREG_ICC_SGI1R_EL1	"S3_0_C12_C11_5"
+#define XREG_ICC_PMR_EL1	"S3_0_C4_C6_0"
+#define XREG_ICC_IAR0_EL1	"S3_0_C12_C8_0"
+#define XREG_ICC_IAR1_EL1	"S3_0_C12_C12_0"
+#define XREG_ICC_EOIR0_EL1	"S3_0_C12_C8_1"
+#define XREG_ICC_EOIR1_EL1	"S3_0_C12_C12_1"
+#endif
 /****************************************************************************/
 /**
 * This function enables system register interface for GIC CPU Interface
@@ -411,8 +437,12 @@ XSCUGIC_RDIST_OFFSET), (RegOffset)))
 * @note        None.
 *
 *****************************************************************************/
-#define XScuGic_Enable_SystemReg_CPU_Interface_EL3() mtcp(S3_6_C12_C12_5, 0xF);
-#define XScuGic_Enable_SystemReg_CPU_Interface_EL1() mtcp(S3_0_C12_C12_5, 0xF);
+#if defined (__aarch64__)
+#define XScuGic_Enable_SystemReg_CPU_Interface_EL3() mtcpnotoken(XREG_ICC_SRE_EL3, 0xF);
+#define XScuGic_Enable_SystemReg_CPU_Interface_EL1() mtcpnotoken(XREG_ICC_SRE_EL1, 0xF);
+#elif defined (ARMR52)
+#define XScuGic_Enable_SystemReg_CPU_Interface_EL1() mtcp(XREG_ICC_SRE_EL1, 0xF);
+#endif
 /****************************************************************************/
 /**
 * This function enables Grou0 interrupts
@@ -424,7 +454,11 @@ XSCUGIC_RDIST_OFFSET), (RegOffset)))
 * @note        None.
 *
 *****************************************************************************/
-#define XScuGic_Enable_Group0_Interrupts() mtcp(S3_0_C12_C12_6,0x1);
+#if defined(ARMR52)
+#define XScuGic_Enable_Group0_Interrupts() mtcp(XREG_ICC_IGRPEN0_EL1,0x1);
+#else
+#define XScuGic_Enable_Group0_Interrupts() mtcpnotoken(XREG_ICC_IGRPEN0_EL1,0x1);
+#endif
 /****************************************************************************/
 /**
 * This function enables Group1 interrupts
@@ -436,14 +470,16 @@ XSCUGIC_RDIST_OFFSET), (RegOffset)))
 * @note        None.
 *
 *****************************************************************************/
-#if defined (__aarch64__)
-#if EL1_NONSECURE
+#if defined (ARMR52)
+
 #define XScuGic_Enable_Group1_Interrupts() \
-		mtcp (S3_0_C12_C12_7, 0x1 | mfcp(S3_0_C12_C12_7) );
+		mtcp (XREG_ICC_IGRPEN1_EL1, 0x1 | mfcp(XREG_ICC_IGRPEN1_EL1) );
+#elif EL1_NONSECURE
+#define XScuGic_Enable_Group1_Interrupts() \
+                mtcpnotoken(XREG_ICC_IGRPEN1_EL1, 0x1 | mfcpnotoken(XREG_ICC_IGRPEN1_EL1) );
 #else
 #define XScuGic_Enable_Group1_Interrupts() \
-		mtcp (S3_6_C12_C12_7, 0x1 | mfcp(S3_6_C12_C12_7) );
-#endif
+		mtcpnotoken(XREG_ICC_IGRPEN1_EL3, 0x1 | mfcpnotoken(XREG_ICC_IGRPEN1_EL3) );
 #endif
 /****************************************************************************/
 /**
@@ -456,7 +492,11 @@ XSCUGIC_RDIST_OFFSET), (RegOffset)))
 * @note     None.
 *
 *****************************************************************************/
-#define XScuGic_WriteICC_SGI0R_EL1(val) mtcp(S3_0_C12_C11_7,val)
+#if defined(ARMR52)
+#define XScuGic_WriteICC_SGI0R_EL1(val) mtcp2(XREG_ICC_SGI0R_EL1,val)
+#else
+#define XScuGic_WriteICC_SGI0R_EL1(val) mtcpnotoken(XREG_ICC_SGI0R_EL1,val)
+#endif
 
 /****************************************************************************/
 /**
@@ -469,7 +509,11 @@ XSCUGIC_RDIST_OFFSET), (RegOffset)))
 * @note        None.
 *
 *****************************************************************************/
-#define XScuGic_WriteICC_SGI1R_EL1(val) mtcp(S3_0_C12_C11_5,val)
+#if defined(ARMR52)
+#define XScuGic_WriteICC_SGI1R_EL1(val) mtcp2(XREG_ICC_SGI1R_EL1,val)
+#else
+#define XScuGic_WriteICC_SGI1R_EL1(val) mtcpnotoken(XREG_ICC_SGI1R_EL1,val)
+#endif
 
 /****************************************************************************/
 /**
@@ -482,7 +526,11 @@ XSCUGIC_RDIST_OFFSET), (RegOffset)))
 * @note        None.
 *
 *****************************************************************************/
-#define XScuGic_ReadICC_SGI1R_EL1() mfcp(S3_0_C12_C11_5)
+#if defined (ARMR52)
+#define XScuGic_ReadICC_SGI1R_EL1() mfcp(XREG_ICC_SGI1R_EL1)
+#else
+#define XScuGic_ReadICC_SGI1R_EL1() mfcpnotoken(XREG_ICC_SGI1R_EL1)
+#endif
 /****************************************************************************/
 /**
 * This function sets interrupt priority filter
@@ -494,7 +542,11 @@ XSCUGIC_RDIST_OFFSET), (RegOffset)))
 * @note        None.
 *
 *****************************************************************************/
-#define XScuGic_set_priority_filter(val)  __asm__ __volatile__("msr  S3_0_C4_C6_0,%0"  : : "r" (val))
+#if defined (ARMR52)
+#define XScuGic_set_priority_filter(val)  mtcp(XREG_ICC_PMR_EL1, val)
+#else
+#define XScuGic_set_priority_filter(val)  mtcpnotoken(XREG_ICC_PMR_EL1, val)
+#endif
 /****************************************************************************/
 /**
 * This function returns interrupt id of highest priority pending interrupt
@@ -506,12 +558,12 @@ XSCUGIC_RDIST_OFFSET), (RegOffset)))
 * @note        None.
 *
 *****************************************************************************/
-#if defined (__aarch64__)
-#if EL3
-#define XScuGic_get_IntID()  mfcp(S3_0_C12_C8_0)
+#if defined(ARMR52)
+#define XScuGic_get_IntID()  mfcp(XREG_ICC_IAR0_EL1)
+#elif EL3
+#define XScuGic_get_IntID()  mfcpnotoken(XREG_ICC_IAR0_EL1)
 #else
-#define XScuGic_get_IntID()  mfcp(S3_0_C12_C12_0)
-#endif
+#define XScuGic_get_IntID()  mfcpnotoken(XREG_ICC_IAR1_EL1)
 #endif
 /****************************************************************************/
 /**
@@ -524,12 +576,12 @@ XSCUGIC_RDIST_OFFSET), (RegOffset)))
 * @note        None.
 *
 *****************************************************************************/
-#if  defined (__aarch64__)
-#if EL3
-#define XScuGic_ack_Int(val)   mtcp(S3_0_C12_C8_1,val)
+#if defined(ARMR52)
+#define XScuGic_ack_Int(val)   mtcp(XREG_ICC_EOIR0_EL1,val)
+#elif EL3
+#define XScuGic_ack_Int(val)   mtcpnotoken(XREG_ICC_EOIR0_EL1,val)
 #else
-#define XScuGic_ack_Int(val)   mtcp(S3_0_C12_C12_1,val)
-#endif
+#define XScuGic_ack_Int(val)   mtcpnotoken(XREG_ICC_EOIR1_EL1,val)
 #endif
 /****************************************************************************/
 /**
@@ -544,6 +596,7 @@ XSCUGIC_RDIST_OFFSET), (RegOffset)))
 *
 *****************************************************************************/
 #define XScuGic_Get_Rdist_Int_Trigger_Index(IntrId)  (((Int_Id%16) & 0x1f) << 2) +1
+#endif
 /************************** Function Prototypes ******************************/
 
 /*

XilinxProcessorIPLib/drivers/scugic/data/scugic.mdd
@@ -7,10 +7,10 @@ OPTION psf_version = 2.1;
 
 BEGIN driver scugic
 
-  OPTION supported_peripherals = (ps7_scugic psu_scugic psu_acpu_gic psu_rcpu_gic psv_scugic psv_acpu_gic psv_rcpu_gic);
+  OPTION supported_peripherals = (ps7_scugic psu_scugic psu_acpu_gic psu_rcpu_gic psv_scugic psv_acpu_gic psv_rcpu_gic psxl_acpu_gic psxl_rcpu_gic);
   OPTION driver_state = ACTIVE;
   OPTION copyfiles = all;
-  OPTION VERSION = 4.7;
+  OPTION VERSION = 5.0
   OPTION NAME = scugic;
   OPTION INTC_TYPE = XScuGic;
 END driver

XilinxProcessorIPLib/drivers/scugic/data/scugic.tcl
@@ -1,5 +1,5 @@
 ###############################################################################
-# Copyright (C) 2011 - 2021 Xilinx, Inc.  All rights reserved.
+# Copyright (C) 2011 - 2022 Xilinx, Inc.  All rights reserved.
 # SPDX-License-Identifier: MIT
 #
 ###############################################################################
@@ -87,6 +87,7 @@
 #                     Utility vector logic. Also defined get_interrupt_parent and
 #                     get_connected_intr_cntrl to handle utility vector logic and
 #                     return proper interrupt controller.
+# 5.0   mus  22/02/22 Added support for VERSAL NET.
 ##############################################################################
 
 #uses "xillib.tcl"
@@ -142,6 +143,8 @@ proc xdefine_zynq_include_file {drv_handle file_name drv_string args} {
 	if {([string compare -nocase $proctype "ps7_cortexa9"] == 0)|| ([string compare -nocase $proctype "psu_cortexa72"] == 0) ||
 	     (([string compare -nocase $proctype "psv_cortexa72"] == 0) && ([string compare -nocase $periph "psv_acpu_gic"] == 0)) ||
 	     (([string compare -nocase $proctype "psv_cortexr5"] == 0) && ([string compare -nocase $periph "psv_rcpu_gic"] == 0)) ||
+	     (([string compare -nocase $proctype "psxl_cortexa78"] == 0) && ([string compare -nocase $periph "psxl_acpu_gic"] == 0)) ||
+	     (([string compare -nocase $proctype "psxl_cortexr52"] == 0) && ([string compare -nocase $periph "psxl_rcpu_gic"] == 0)) ||
 	    (([string compare -nocase $proctype "psu_cortexa53"] == 0)&&([string compare -nocase $periph "psu_acpu_gic"] == 0))||
 	    (([string compare -nocase $proctype "psu_cortexr5"] == 0)&&([string compare -nocase $periph "psu_rcpu_gic"] == 0))} {
 		lappend newperiphs $periph_inst
@@ -193,6 +196,8 @@ proc xdefine_zynq_include_file {drv_handle file_name drv_string args} {
 		} elseif {[string compare -nocase "C_S_AXI_BASEADDR" $arg] == 0} {
 			if {([string compare -nocase $proctype "psu_cortexr5"] == 0) || ([string compare -nocase $proctype "psv_cortexr5"] == 0)} {
 				set value 0xF9001000
+			} elseif {([string compare -nocase $proctype "psxl_cortexr52"] == 0) || ([string compare -nocase $proctype "psxl_cortexa78"] == 0)} {
+				set value 0xE2001000
 			} elseif {[string compare -nocase $proctype "ps7_cortexa9"] == 0} {
 				set value [::hsi::utils::get_param_value $periph $arg]
 			} elseif {([string compare -nocase $proctype "psu_cortexa72"] == 0) || ([string compare -nocase $proctype "psv_cortexa72"] == 0) } {
@@ -207,6 +212,8 @@ proc xdefine_zynq_include_file {drv_handle file_name drv_string args} {
 		} elseif {[string compare -nocase "C_S_AXI_HIGHADDR" $arg] == 0} {
 			if {([string compare -nocase $proctype "psu_cortexr5"] == 0) || ([string compare -nocase $proctype "psv_cortexr5"] == 0) } {
 				set value 0xF9001FFF
+			} elseif {([string compare -nocase $proctype "psxl_cortexr52"] == 0) || ([string compare -nocase $proctype "psxl_cortexa78"] == 0)} {
+				set value 0xE2001FFF
 			} elseif {[string compare -nocase $proctype "ps7_cortexa9"] == 0} {
 				set value [::hsi::utils::get_param_value $periph $arg]
 			} elseif {([string compare -nocase $proctype "psu_cortexa72"] == 0) || ([string compare -nocase $proctype "psv_cortexa72"] == 0) } {
@@ -221,6 +228,8 @@ proc xdefine_zynq_include_file {drv_handle file_name drv_string args} {
 		   } elseif {[string compare -nocase "C_DIST_BASEADDR" $arg] == 0} {
 			if {([string compare -nocase $proctype "psu_cortexr5"] == 0) || ([string compare -nocase $proctype "psv_cortexr5"] == 0) } {
 				set value 0xF9000000
+			} elseif {([string compare -nocase $proctype "psxl_cortexr52"] == 0) || ([string compare -nocase $proctype "psxl_cortexa78"] == 0)} {
+				set value 0xE2000000
 			} elseif {[string compare -nocase $proctype "ps7_cortexa9"] == 0} {
 				set value 0xf8f01000
 			} elseif {([string compare -nocase $proctype "psu_cortexa72"] == 0) || ([string compare -nocase $proctype "psv_cortexa72"] == 0) } {
@@ -278,7 +287,9 @@ proc xdefine_zynq_canonical_xpars {drv_handle file_name drv_string args} {
 	if {([string compare -nocase $proctype "ps7_cortexa9"] == 0) || ([string compare -nocase $proctype "psu_cortexa72"] == 0) ||
 	     (([string compare -nocase $proctype "psv_cortexa72"] == 0) && ([string compare -nocase $periph "psv_acpu_gic"] == 0)) ||
 	     (([string compare -nocase $proctype "psv_cortexr5"] == 0) && ([string compare -nocase $periph "psv_rcpu_gic"] == 0)) ||
-	    (([string compare -nocase $proctype "psu_cortexa53"] == 0)&&([string compare -nocase $periph "psu_acpu_gic"] == 0))||
+	     (([string compare -nocase $proctype "psxl_cortexa78"] == 0) && ([string compare -nocase $periph "psxl_acpu_gic"] == 0)) ||
+	     (([string compare -nocase $proctype "psxl_cortexr52"] == 0) && ([string compare -nocase $periph "psxl_rcpu_gic"] == 0)) ||
+	     (([string compare -nocase $proctype "psu_cortexa53"] == 0)&&([string compare -nocase $periph "psu_acpu_gic"] == 0))||
 	    (([string compare -nocase $proctype "psu_cortexr5"] == 0)&&([string compare -nocase $periph "psu_rcpu_gic"] == 0))} {
 		lappend newperiphs $periph_inst
 		set valid_periph 1
@@ -338,6 +349,8 @@ proc xdefine_zynq_canonical_xpars {drv_handle file_name drv_string args} {
                 if {[string compare -nocase "C_S_AXI_BASEADDR" $arg] == 0} {
 			if {([string compare -nocase $proctype "psu_cortexr5"] == 0) || ([string compare -nocase $proctype "psv_cortexr5"] == 0)} {
 				set rvalue 0xF9001000
+			} elseif {([string compare -nocase $proctype "psxl_cortexr52"] == 0) || ([string compare -nocase $proctype "psxl_cortexa78"] == 0)} {
+				set rvalue 0xE2001000
 			} elseif {[string compare -nocase $proctype "ps7_cortexa9"] == 0} {
 				set rvalue [::hsi::utils::get_param_value $periph $arg]
 			} elseif {([string compare -nocase $proctype "psu_cortexa72"] == 0) || ([string compare -nocase $proctype "psv_cortexa72"] == 0) } {
@@ -352,6 +365,8 @@ proc xdefine_zynq_canonical_xpars {drv_handle file_name drv_string args} {
 		} elseif {[string compare -nocase "C_S_AXI_HIGHADDR" $arg] == 0} {
 			if {([string compare -nocase $proctype "psu_cortexr5"] == 0) || ([string compare -nocase $proctype "psv_cortexr5"] == 0)} {
 				set rvalue 0xF9001FFF
+			} elseif {([string compare -nocase $proctype "psxl_cortexr52"] == 0) || ([string compare -nocase $proctype "psxl_cortexa78"] == 0)} {
+				set rvalue 0xE2001FFF
 			} elseif {[string compare -nocase $proctype "ps7_cortexa9"] == 0} {
 				set rvalue [::hsi::utils::get_param_value $periph $arg]
 			} elseif {([string compare -nocase $proctype "psu_cortexa72"] == 0) || ([string compare -nocase $proctype "psv_cortexa72"] == 0)} {
@@ -366,6 +381,8 @@ proc xdefine_zynq_canonical_xpars {drv_handle file_name drv_string args} {
 		} elseif {[string compare -nocase "C_DIST_BASEADDR" $arg] == 0} {
 			if {([string compare -nocase $proctype "psu_cortexr5"] == 0) || ([string compare -nocase $proctype "psv_cortexr5"] == 0)} {
 				set rvalue 0xF9000000
+			} elseif {([string compare -nocase $proctype "psxl_cortexr52"] == 0) || ([string compare -nocase $proctype "psxl_cortexa78"] == 0)} {
+				set rvalue 0xE2000000
 			} elseif {[string compare -nocase $proctype "ps7_cortexa9"] == 0} {
 				set rvalue 0xf8f01000
                         } elseif {([string compare -nocase $proctype "psu_cortexa72"] == 0) || ([string compare -nocase $proctype "psv_cortexa72"] == 0)} {
@@ -431,6 +448,8 @@ proc xdefine_zynq_config_file {drv_handle file_name drv_string args} {
 	if {([string compare -nocase $proctype "ps7_cortexa9"] == 0)|| ([string compare -nocase $proctype "psu_cortexa72"] == 0)||
 	     (([string compare -nocase $proctype "psv_cortexa72"] == 0) && ([string compare -nocase $periph "psv_acpu_gic"] == 0)) ||
 	     (([string compare -nocase $proctype "psv_cortexr5"] == 0) && ([string compare -nocase $periph "psv_rcpu_gic"] == 0)) ||
+	     (([string compare -nocase $proctype "psxl_cortexa78"] == 0) && ([string compare -nocase $periph "psxl_acpu_gic"] == 0)) ||
+	     (([string compare -nocase $proctype "psxl_cortexr52"] == 0) && ([string compare -nocase $periph "psxl_rcpu_gic"] == 0)) ||
 	    (([string compare -nocase $proctype "psu_cortexa53"] == 0)&&([string compare -nocase $periph "psu_acpu_gic"] == 0))||
 	    (([string compare -nocase $proctype "psu_cortexr5"] == 0)&&([string compare -nocase $periph "psu_rcpu_gic"] == 0))} {
 		lappend newperiphs $periph_inst
@@ -507,6 +526,8 @@ proc xdefine_gic_params {drvhandle} {
 	if {([string compare -nocase $proctype "ps7_cortexa9"] == 0)|| ([string compare -nocase $proctype "psu_cortexa72"] == 0)||
 	     (([string compare -nocase $proctype "psv_cortexa72"] == 0) && ([string compare -nocase $periph "psv_acpu_gic"] == 0)) ||
 	     (([string compare -nocase $proctype "psv_cortexr5"] == 0) && ([string compare -nocase $periph "psv_rcpu_gic"] == 0)) ||
+	     (([string compare -nocase $proctype "psxl_cortexa78"] == 0) && ([string compare -nocase $periph "psxl_acpu_gic"] == 0)) ||
+	     (([string compare -nocase $proctype "psxl_cortexr52"] == 0) && ([string compare -nocase $periph "psxl_rcpu_gic"] == 0)) ||
 	   (([string compare -nocase $proctype "psu_cortexa53"] == 0)&&([string compare -nocase $periph "psu_acpu_gic"] == 0))||
 	   (([string compare -nocase $proctype "psu_cortexr5"] == 0)&&([string compare -nocase $periph "psu_rcpu_gic"] == 0))} {
 		lappend newperiphs $periph_inst
@@ -588,7 +609,7 @@ proc xdefine_gic_params {drvhandle} {
             set ip_name   $source_name($i)
             set port_name $source_port_name($i)
             set port_obj  [::hsi::get_ports $port_name]
-            if {([string compare -nocase $proctype "psu_cortexa53"] == 0) || ([string compare -nocase $proctype "psu_cortexr5"] == 0) || ([string compare -nocase $proctype "psv_cortexa72"] == 0) || ([string compare -nocase $proctype "psv_cortexr5"] == 0) } {
+            if {([string compare -nocase $proctype "psu_cortexa53"] == 0) || ([string compare -nocase $proctype "psu_cortexr5"] == 0) || ([string compare -nocase $proctype "psv_cortexa72"] == 0) || ([string compare -nocase $proctype "psv_cortexr5"] == 0) || ([string compare -nocase $proctype "psxl_cortexa78"] == 0) || ([string compare -nocase $proctype "psxl_cortexr52"] == 0)} {
 		set port_intr_id [get_psu_interrupt_id $ip_name $port_name]
                 if {[llength $port_intr_id] == 1} {
 		      set port_intr_id [expr $port_intr_id + 32]
@@ -608,7 +629,7 @@ proc xdefine_gic_params {drvhandle} {
                     set port_obj    [::hsi::get_pins -of_objects $ip_obj $port_name]
                 }
             } else {
-                if {([string compare -nocase $proctype "psu_cortexa53"] == 0) || ([string compare -nocase $proctype "psu_cortexr5"] == 0) || ([string compare -nocase $proctype "psv_cortexa72"] == 0) || ([string compare -nocase $proctype "psv_cortexr5"] == 0) } {
+                if {([string compare -nocase $proctype "psu_cortexa53"] == 0) || ([string compare -nocase $proctype "psu_cortexr5"] == 0) || ([string compare -nocase $proctype "psv_cortexa72"] == 0) || ([string compare -nocase $proctype "psv_cortexr5"] == 0) || ([string compare -nocase $proctype "psxl_cortexa78"] == 0)  || ([string compare -nocase $proctype "psxl_cortexr52"] == 0)} {
 			set port_intr_id [get_psu_interrupt_id $ip_name $port_name]
 			if {[llength $port_intr_id] == 1} {
 				set port_intr_id [expr $port_intr_id + 32]
@@ -1340,9 +1361,9 @@ proc get_psu_interrupt_id { ip_name port_name } {
 proc is_interrupt { IP_NAME } {
 		if { [string match -nocase $IP_NAME "ps7_scugic"] } {
 						return true
-		} elseif { ([string match -nocase $IP_NAME "psu_acpu_gic"]) || ([string match -nocase $IP_NAME "psv_acpu_gic"])   } {
+		} elseif { ([string match -nocase $IP_NAME "psu_acpu_gic"]) || ([string match -nocase $IP_NAME "psv_acpu_gic"] || ([string match -nocase $IP_NAME "psxl_acpu_gic"])} {
 						return true
-		} elseif { ([string match -nocase $IP_NAME "psu_rcpu_gic"]) || ([string match -nocase $IP_NAME "psv_rcpu_gic"])   } {
+`		} elseif { ([string match -nocase $IP_NAME "psu_rcpu_gic"]) || ([string match -nocase $IP_NAME "psv_rcpu_gic"] || ([string match -nocase $IP_NAME "psxl_rcpu_gic"])} {
 						return true
 		}
 		#puts "return $IP_NAME\n\r"

XilinxProcessorIPLib/drivers/scugic/src/xscugic.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2010 - 2021 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2022 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xscugic.c
-* @addtogroup scugic_v4_7
+* @addtogroup scugic_v5_0
 * @{
 *
 * Contains required functions for the XScuGic driver for the Interrupt
@@ -134,6 +134,7 @@
 * 		      when (Int_Id >= XSCUGIC_SPI_INT_ID_START).
 * 4.7   dp   11/22/21 Added new API XScuGic_IsInitialized() to check and return
 *                     the GIC initialization status.
+* 5.0   mus  22/02/22 Added support for VERSAL NET.
 * </pre>
 *
 ******************************************************************************/
@@ -401,6 +402,10 @@ s32  XScuGic_CfgInitialize(XScuGic *InstancePtr,
 
 		InstancePtr->IsReady = 0U;
 		InstancePtr->Config = ConfigPtr;
+		#if defined(ARMR52)
+		/* Read Distributor base address through IMP_CBAR register */
+		ConfigPtr->DistBaseAddress = mfcp(XREG_IMP_CBAR);
+		#endif
 
 
 		for (Int_Id = 0U; Int_Id < XSCUGIC_MAX_NUM_INTR_INPUTS;
@@ -423,6 +428,12 @@ s32  XScuGic_CfgInitialize(XScuGic *InstancePtr,
 		}
 #if defined (GICv3)
 	u32 Waker_State;
+	#if defined (GIC600)
+	XScuGic_ReDistWriteReg(InstancePtr,XSCUGIC_RDIST_PWRR_OFFSET,
+			(XScuGic_ReDistReadReg(InstancePtr, XSCUGIC_RDIST_PWRR_OFFSET) &
+			(~XSCUGIC_RDIST_PWRR_RDPD_MASK)));
+	#endif
+
 	Waker_State = XScuGic_ReDistReadReg(InstancePtr,XSCUGIC_RDIST_WAKER_OFFSET);
 	XScuGic_ReDistWriteReg(InstancePtr,XSCUGIC_RDIST_WAKER_OFFSET,
 							Waker_State & (~ XSCUGIC_RDIST_WAKER_LOW_POWER_STATE_MASK));

XilinxProcessorIPLib/drivers/scugic/src/xscugic_g.c
@@ -7,7 +7,7 @@
 /**
 *
 * @file xscugic_g.c
-* @addtogroup scugic_v4_7
+* @addtogroup scugic_v5_0
 * @{
 *
 * This file contains a configuration table that specifies the configuration of

XilinxProcessorIPLib/drivers/scugic/src/xscugic_hw.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2010 - 2021 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2022 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xscugic_hw.c
-* @addtogroup scugic_v4_7
+* @addtogroup scugic_v5_0
 * @{
 *
 * This file contains low-level driver functions that can be used to access the

XilinxProcessorIPLib/drivers/scugic/src/xscugic_hw.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2010 - 2021 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2022 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xscugic_hw.h
-* @addtogroup scugic_v4_7
+* @addtogroup scugic_v5_0
 * @{
 *
 * This header file contains identifiers and HW access functions (or
@@ -59,6 +59,7 @@
 * 4.6	sk   08/05/21 Fix Scugic Misrac violations.
 * 4.7	sk   12/10/21 Update XSCUGIC_SPI_INT_ID_START macro from signed to unsigned
 * 		      to fix misrac violation.
+* 5.0   mus  22/02/22 Added support for VERSAL NET
 *
 * </pre>
 *
@@ -80,15 +81,22 @@ extern "C" {
 #include "bspconfig.h"
 
 /************************** Constant Definitions *****************************/
-#if defined (versal) && !defined(ARMR5)
+#if (defined (versal) && !defined(ARMR5)) || defined (ARMR52)
 #define GICv3
 #endif
+
+#if defined (VERSAL_NET) && ! defined (ARMR52)
+#define GIC600
+#endif
+
 
 /*
  * The maximum number of interrupts supported by the hardware.
  */
 #ifdef PLATFORM_ZYNQ
 #define XSCUGIC_MAX_NUM_INTR_INPUTS    	95U /* Maximum number of interrupt defined by Zynq */
+#elif defined (VERSAL_NET)
+#define XSCUGIC_MAX_NUM_INTR_INPUTS    	256U /* Maximum number of interrupt sources in VERSAL NET */
 #elif defined (versal)
 #define XSCUGIC_MAX_NUM_INTR_INPUTS    	192U
 #else
@@ -481,9 +489,22 @@ extern "C" {
  *
  * @{
  */
+#if defined (VERSAL_NET) && ! defined (ARMR52)
+#define XSCUGIC_RDIST_OFFSET              0x60000U
+#elif defined (ARMR52)
+#define XSCUGIC_RDIST_OFFSET              0x100000U
+#else
 #define XSCUGIC_RDIST_OFFSET              0x80000U
+#endif
 #define XSCUGIC_RDIST_BASE_ADDRESS        (XPAR_SCUGIC_0_DIST_BASEADDR + XSCUGIC_RDIST_OFFSET)
+
+#if defined (VERSAL_NET) && ! defined (ARMR52)
+#define XSCUGIC_RDIST_SGI_PPI_OFFSET              0x70000U
+#elif defined (ARMR52)
+#define XSCUGIC_RDIST_SGI_PPI_OFFSET              0x110000U
+#else
 #define XSCUGIC_RDIST_SGI_PPI_OFFSET              0x90000U
+#endif
 #define XSCUGIC_RDIST_SGI_PPI_BASE_ADDRESS    (XPAR_SCUGIC_0_DIST_BASEADDR + XSCUGIC_RDIST_SGI_PPI_OFFSET)
 #define XSCUGIC_RDIST_ISENABLE_OFFSET     0x100U
 #define XSCUGIC_RDIST_IPRIORITYR_OFFSET   0x400U
@@ -493,10 +514,13 @@ extern "C" {
 #define XSCUGIC_RDIST_WAKER_OFFSET        0x14U
 #define XSCUGIC_SGIR_EL1_INITID_SHIFT    24U
 
+#if defined (GIC600)
+#define XSCUGIC_RDIST_PWRR_OFFSET	0x24U
+#endif
 /*
  * GICR_IGROUPR  register definitions
  */
-#if EL3
+#if (defined(ARMR52) || EL3)
 #define XSCUGIC_DEFAULT_SECURITY    0x0U
 #else
 #define XSCUGIC_DEFAULT_SECURITY    0xFFFFFFFFU
@@ -505,6 +529,7 @@ extern "C" {
  * GICR_WAKER  register definitions
  */
 #define XSCUGIC_RDIST_WAKER_LOW_POWER_STATE_MASK    0x7
+#define XSCUGIC_RDIST_PWRR_RDPD_MASK	0x1U
 #endif
 /***************** Macros (Inline Functions) Definitions *********************/
 

XilinxProcessorIPLib/drivers/scugic/src/xscugic_intr.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2010 - 2021 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2010 - 2022 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xscugic_intr.c
-* @addtogroup scugic_v4_7
+* @addtogroup scugic_v5_0
 * @{
 *
 * This file contains the interrupt processing for the driver for the Xilinx

commit e526520a28653dfb413bb7cdd965d5988107b24b

scugic: Fix XREG_ICC_SRE_EL1 macro
It fixes bug introduced by
f6978484de00d3714c4d8f45f2f47add67e6e889.

Signed-off-by: Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>
Acked-by: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

XilinxProcessorIPLib/drivers/scugic/src/xscugic.h
@@ -413,7 +413,7 @@ XSCUGIC_RDIST_OFFSET), (RegOffset)))
 #define XREG_ICC_EOIR0_EL1	"p15, 0, %0,  c12,  c8, 1"
 #define XREG_IMP_CBAR		"p15, 1, %0, c15, c3, 0"
 #else
-#define XREG_ICC_SRE_EL1	"S3_6_C12_C12_5"
+#define XREG_ICC_SRE_EL1	"S3_0_C12_C12_5"
 #define XREG_ICC_SRE_EL3	"S3_6_C12_C12_5"
 #define XREG_ICC_IGRPEN0_EL1	"S3_0_C12_C12_6"
 #define XREG_ICC_IGRPEN1_EL1	"S3_0_C12_C12_7"