54aab0b8d189376b3b9248c60d89f884e3ab0784
Shravya <shravya.kumbham@xilinx.com>
2022-03-03 12:59:10 +0530

    csudma: Update csudma driver version
    Update csudma version from 1.10 to 1.11 and copyright
    year to 2022.
    
    Signed-off-by: Shravya Kumbham <shravya.kumbham@xilinx.com>
    
    Acked-for-series: Harini Katakam <harini.katakam@xilinx.com>

--- XilinxProcessorIPLib/drivers/csudma/src/xcsudma_selftest.c
+++ XilinxProcessorIPLib/drivers/csudma/src/xcsudma_selftest.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2014 - 2021 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2014 - 2022 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
