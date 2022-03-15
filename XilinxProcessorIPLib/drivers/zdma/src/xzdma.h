d31f690a40a9b57d6ad7105f98e25452cf472015
Kedareswara rao Appana <appana.durga.rao@xilinx.com>
2022-03-15 16:16:05 +0530

    XilinxProcessorIPLib: drivers: zdma: Increment driver version
    This commit does the below
    --> Increments the driver version number.
    --> Updates addtogroup tags.
    --> Updates copyright year.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.rao@xilinx.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

--- XilinxProcessorIPLib/drivers/zdma/src/xzdma.h
+++ XilinxProcessorIPLib/drivers/zdma/src/xzdma.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2014 - 2021 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2014 - 2022 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xzdma.h
-* @addtogroup zdma_v1_13
+* @addtogroup zdma_v1_14
 * @{
 * @details
 *
