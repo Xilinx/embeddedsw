5b14fa3c1d99c35a3fde965b6aedc624c6c33da3
Jay Buddhabhatti <jay.buddhabhatti@xilinx.com>
2022-04-05 05:26:33 -0700

    xilpm: versal_net: add enums and macros for versal_net
    Add enums and macros in xpm_defs.h file which are required for
    xilpm client library.
    
    Signed-off-by: Jay Buddhabhatti <jay.buddhabhatti@xilinx.com>
    
    Acked-for-series: Trung Tran<trung.tran@xilinx.com>=0A=

--- lib/sw_services/xilpm/src/versal_net/common/xpm_defs.h
+++ lib/sw_services/xilpm/src/versal_net/common/xpm_defs.h
@@ -34,6 +34,35 @@ extern "C" {
 /** @} */
 
 /**
+ * PM abort reasons
+ */
+enum XPmAbortReason {
+	ABORT_REASON_WKUP_EVENT = 100,			/**< Wakeup Event */
+	ABORT_REASON_PU_BUSY,				/**< Processor Busy */
+	ABORT_REASON_NO_PWRDN,				/**< No Powerdown */
+	ABORT_REASON_UNKNOWN,				/**< Unknown Reason */
+};
+
+/**
+ * Boot status enumeration.
+ */
+enum XPmBootStatus {
+	PM_INITIAL_BOOT,				/**< boot is a fresh system startup */
+	PM_RESUME,					/**< boot is a resume */
+	PM_BOOT_ERROR,					/**< error, boot cause cannot be identified */
+};
+
+/**
+ *  PM Acknowledge Request Types
+ */
+enum XPmRequestAck {
+	REQUEST_ACK_NO = 1,				/**< No Ack */
+	REQUEST_ACK_BLOCKING,				/**< Blocking Ack */
+	REQUEST_ACK_NON_BLOCKING,			/**< Non blocking Ack */
+	REQUEST_ACK_CB_CERROR,				/**< Callback Error */
+};
+
+/**
  *  Device capability requirements enumeration.
  */
 enum XPmCapability {
@@ -45,9 +74,6 @@ enum XPmCapability {
 	PM_CAP_COHERENT	= 0x20U,			/**< Device Coherency */
 	PM_CAP_VIRTUALIZED = 0x40U,			/**< Device Virtualization */
 };
-#define XPM_MAX_QOS		(100U)
-#define XPM_DEF_QOS		XPM_MAX_QOS
-#define XPM_MAX_LATENCY		(0xFFFFU)
 
 /**
  * Usage status, returned by PmGetNodeStatus
@@ -56,6 +82,84 @@ enum XPmDeviceUsage {
 	PM_USAGE_CURRENT_SUBSYSTEM = 0x1U,		/**< Current subsystem is using */
 	PM_USAGE_OTHER_SUBSYSTEM   = 0x2U,		/**< Other subsystem is using */
 };
+
+/**
+ * Reset configuration argument
+ */
+enum XPmResetActions {
+	PM_RESET_ACTION_RELEASE,			/**< Reset action release */
+	PM_RESET_ACTION_ASSERT,				/**< Reset action assert */
+	PM_RESET_ACTION_PULSE,				/**< Reset action pulse */
+};
+
+/**
+ * Suspend reasons
+ */
+enum XPmSuspendReason {
+	SUSPEND_REASON_PU_REQ		= (201U),	/**< Processor request */
+	SUSPEND_REASON_ALERT		= (202U),	/**< Alert */
+	SUSPEND_REASON_SYS_SHUTDOWN	= (203U),	/**< System shutdown */
+};
+
+/**
+ * PM API callback IDs
+ */
+typedef enum {
+	PM_INIT_SUSPEND_CB		= (30),		/**< Suspend callback */
+	PM_ACKNOWLEDGE_CB		= (31),		/**< Acknowledge callback */
+	PM_NOTIFY_CB			= (32),		/**< Notify callback */
+} XPmApiCbId_t;
+
+/**
+ * Contains the device status information.
+ */
+typedef struct XPm_DeviceStatus {
+	u32 Status;					/**< Device power state */
+	u32 Requirement;				/**< Requirements placed on the device by the caller */
+	u32 Usage;					/**< Usage info (which subsystem is using the device) */
+} XPm_DeviceStatus;
+
+/**
+ * @name Requirement limits
+ * @{
+ */
+/**
+ * Requirement limits
+ */
+#define XPM_MAX_CAPABILITY	((u32)PM_CAP_ACCESS | (u32)PM_CAP_CONTEXT | (u32)PM_CAP_WAKEUP)
+#define XPM_MAX_LATENCY		(0xFFFFU)
+#define XPM_MAX_QOS		(100U)
+#define XPM_MIN_CAPABILITY	(0U)
+#define XPM_MIN_LATENCY		(0U)
+#define XPM_MIN_QOS		(0U)
+#define XPM_DEF_CAPABILITY	XPM_MAX_CAPABILITY
+#define XPM_DEF_LATENCY		XPM_MAX_LATENCY
+#define XPM_DEF_QOS		XPM_MAX_QOS
+/** @} */
+
+/**
+ * @name Device node status
+ * @{
+ */
+/**
+ * Device node status
+ */
+#define NODE_STATE_OFF			(0U)
+#define NODE_STATE_ON			(1U)
+/** @} */
+
+/**
+ * @name Processor node status
+ * @{
+ */
+/**
+ * Processor node status
+ */
+#define PROC_STATE_SLEEP		NODE_STATE_OFF
+#define PROC_STATE_ACTIVE		NODE_STATE_ON
+#define PROC_STATE_FORCEDOFF		(7U)
+#define PROC_STATE_SUSPENDING		(8U)
+/** @} */
 
 /**
  * Query IDs
@@ -78,6 +182,132 @@ enum pm_query_id {
 	XPM_QID_PLD_GET_PARENT,				/**< Get PLD parent */
 };
 
+/**
+ * Pin Function IDs
+ */
+enum PmPinFunIds {
+	PIN_FUNC_SPI0,					/**< Pin function ID of SPI0 */
+	PIN_FUNC_SPI0_SS,				/**< Pin function ID of SPI0_SS */
+	PIN_FUNC_SPI1,					/**< Pin function ID of SPI1 */
+	PIN_FUNC_SPI1_SS,				/**< Pin function ID of SPI1_SS */
+	PIN_FUNC_CAN0,					/**< Pin function ID of CAN0 */
+	PIN_FUNC_CAN1,					/**< Pin function ID of CAN1 */
+	PIN_FUNC_I2C0,					/**< Pin function ID of I2C0 */
+	PIN_FUNC_I2C1,					/**< Pin function ID of I2C1 */
+	PIN_FUNC_I2C_PMC,				/**< Pin function ID of I2C_PMC */
+	PIN_FUNC_TTC0_CLK,				/**< Pin function ID of TTC0_CLK */
+	PIN_FUNC_TTC0_WAV,				/**< Pin function ID of TTC0_WAV */
+	PIN_FUNC_TTC1_CLK,				/**< Pin function ID of TTC1_CLK */
+	PIN_FUNC_TTC1_WAV,				/**< Pin function ID of TTC1_WAV */
+	PIN_FUNC_TTC2_CLK,				/**< Pin function ID of TTC2_CLK */
+	PIN_FUNC_TTC2_WAV,				/**< Pin function ID of TTC2_WAV */
+	PIN_FUNC_TTC3_CLK,				/**< Pin function ID of TTC3_CLK */
+	PIN_FUNC_TTC3_WAV,				/**< Pin function ID of TTC3_WAV */
+	PIN_FUNC_WWDT0,					/**< Pin function ID of WWDT0 */
+	PIN_FUNC_WWDT1,					/**< Pin function ID of WWDT1 */
+	PIN_FUNC_SYSMON_I2C0,				/**< Pin function ID of SYSMON_I2C0 */
+	PIN_FUNC_SYSMON_I2C0_ALERT,			/**< Pin function ID of SYSMON_I2C0_AL */
+	PIN_FUNC_UART0,					/**< Pin function ID of UART0 */
+	PIN_FUNC_UART0_CTRL,				/**< Pin function ID of UART0_CTRL */
+	PIN_FUNC_UART1,					/**< Pin function ID of UART1 */
+	PIN_FUNC_UART1_CTRL,				/**< Pin function ID of UART1_CTRL */
+	PIN_FUNC_GPIO0,					/**< Pin function ID of GPIO0 */
+	PIN_FUNC_GPIO1,					/**< Pin function ID of GPIO1 */
+	PIN_FUNC_GPIO2,					/**< Pin function ID of GPIO2 */
+	PIN_FUNC_EMIO0,					/**< Pin function ID of EMIO0 */
+	PIN_FUNC_GEM0,					/**< Pin function ID of GEM0 */
+	PIN_FUNC_GEM1,					/**< Pin function ID of GEM1 */
+	PIN_FUNC_TRACE0,				/**< Pin function ID of TRACE0 */
+	PIN_FUNC_TRACE0_CLK,				/**< Pin function ID of TRACE0_CLK */
+	PIN_FUNC_MDIO0,					/**< Pin function ID of MDIO0 */
+	PIN_FUNC_MDIO1,					/**< Pin function ID of MDIO1 */
+	PIN_FUNC_GEM_TSU0,				/**< Pin function ID of GEM_TSU0 */
+	PIN_FUNC_PCIE0,					/**< Pin function ID of PCIE0 */
+	PIN_FUNC_SMAP0,					/**< Pin function ID of SMAP0 */
+	PIN_FUNC_USB0,					/**< Pin function ID of USB0 */
+	PIN_FUNC_SD0,					/**< Pin function ID of SD0 */
+	PIN_FUNC_SD0_PC,				/**< Pin function ID of SD0_PC */
+	PIN_FUNC_SD0_CD,				/**< Pin function ID of SD0_CD */
+	PIN_FUNC_SD0_WP,				/**< Pin function ID of SD0_WP */
+	PIN_FUNC_SD1,					/**< Pin function ID of SD1 */
+	PIN_FUNC_SD1_PC,				/**< Pin function ID of SD1_PC */
+	PIN_FUNC_SD1_CD,				/**< Pin function ID of SD1_CD */
+	PIN_FUNC_SD1_WP,				/**< Pin function ID of SD1_WP */
+	PIN_FUNC_OSPI0,					/**< Pin function ID of OSPI0 */
+	PIN_FUNC_OSPI0_SS,				/**< Pin function ID of OSPI0_SS */
+	PIN_FUNC_QSPI0,					/**< Pin function ID of QSPI0 */
+	PIN_FUNC_QSPI0_FBCLK,				/**< Pin function ID of QSPI0_FBCLK */
+	PIN_FUNC_QSPI0_SS,				/**< Pin function ID of QSPI0_SS */
+	PIN_FUNC_TEST_CLK,				/**< Pin function ID of TEST_CLK */
+	PIN_FUNC_TEST_SCAN,				/**< Pin function ID of TEST_SCAN */
+	PIN_FUNC_TAMPER_TRIGGER,			/**< Pin function ID of TAMPER_TRIGGER */
+	MAX_FUNCTION,					/**< Max Pin function */
+};
+
+/**
+ * Pin Control Configuration
+ */
+enum pm_pinctrl_config_param {
+	PINCTRL_CONFIG_SLEW_RATE,			/**< Pin config slew rate */
+	PINCTRL_CONFIG_BIAS_STATUS,			/**< Pin config bias status */
+	PINCTRL_CONFIG_PULL_CTRL,			/**< Pin config pull control */
+	PINCTRL_CONFIG_SCHMITT_CMOS,			/**< Pin config schmitt CMOS */
+	PINCTRL_CONFIG_DRIVE_STRENGTH,			/**< Pin config drive strength */
+	PINCTRL_CONFIG_VOLTAGE_STATUS,			/**< Pin config voltage status */
+	PINCTRL_CONFIG_TRI_STATE,			/**< Pin config tri state */
+	PINCTRL_CONFIG_MAX,				/**< Max Pin config */
+};
+
+/**
+ * Pin Control Slew Rate
+ */
+enum pm_pinctrl_slew_rate {
+	PINCTRL_SLEW_RATE_FAST,				/**< Fast slew rate */
+	PINCTRL_SLEW_RATE_SLOW,				/**< Slow slew rate */
+};
+
+/**
+ * Pin Control Bias Status
+ */
+enum pm_pinctrl_bias_status {
+	PINCTRL_BIAS_DISABLE,				/**< Bias disable */
+	PINCTRL_BIAS_ENABLE,				/**< Bias enable */
+};
+
+/**
+ * Pin Control Pull Control
+ */
+enum pm_pinctrl_pull_ctrl {
+	PINCTRL_BIAS_PULL_DOWN,				/**< Bias pull-down */
+	PINCTRL_BIAS_PULL_UP,				/**< Bias pull-up */
+};
+
+/**
+ * Pin Control Input Type
+ */
+enum pm_pinctrl_schmitt_cmos {
+	PINCTRL_INPUT_TYPE_CMOS,			/**< Input type CMOS */
+	PINCTRL_INPUT_TYPE_SCHMITT,			/**< Input type SCHMITT */
+};
+
+/**
+ * Pin Control Drive Strength
+ */
+enum pm_pinctrl_drive_strength {
+	PINCTRL_DRIVE_STRENGTH_TRISTATE,		/**< tri-state */
+	PINCTRL_DRIVE_STRENGTH_4MA,			/**< 4mA */
+	PINCTRL_DRIVE_STRENGTH_8MA,			/**< 8mA */
+	PINCTRL_DRIVE_STRENGTH_12MA,			/**< 12mA */
+	PINCTRL_DRIVE_STRENGTH_MAX,			/**< Max value */
+};
+
+/**
+ * Pin Control Tri State
+ */
+enum pm_pinctrl_tri_state {
+	PINCTRL_TRI_STATE_DISABLE,			/**< Tri state disable */
+	PINCTRL_TRI_STATE_ENABLE,			/**< Tri state enable */
+};
 /**
  * PM init node functions
  */
@@ -132,34 +362,222 @@ typedef enum {
 	IOCTL_AIE_ISR_CLEAR,				/**< AIE ISR clear */
 } pm_ioctl_id;
 
+/**
+ * PLL parameters
+ */
+enum XPm_PllConfigParams {
+	PM_PLL_PARAM_ID_DIV2,				/**< PLL param ID DIV2 */
+	PM_PLL_PARAM_ID_FBDIV,				/**< PLL param ID FBDIV */
+	PM_PLL_PARAM_ID_DATA,				/**< PLL param ID DATA */
+	PM_PLL_PARAM_ID_PRE_SRC,			/**< PLL param ID PRE_SRC */
+	PM_PLL_PARAM_ID_POST_SRC,			/**< PLL param ID POST_SRC */
+	PM_PLL_PARAM_ID_LOCK_DLY,			/**< PLL param ID LOCK_DLY */
+	PM_PLL_PARAM_ID_LOCK_CNT,			/**< PLL param ID LOCK_CNT */
+	PM_PLL_PARAM_ID_LFHF,				/**< PLL param ID LFHF */
+	PM_PLL_PARAM_ID_CP,				/**< PLL param ID CP */
+	PM_PLL_PARAM_ID_RES,				/**< PLL param ID RES */
+	PM_PLL_PARAM_MAX,				/**< PLL param ID max */
+};
 
 /**
- * Contains the device status information.
+ * PLL modes
  */
-typedef struct XPm_DeviceStatus {
-	u32 Status;					/**< Device power state */
-	u32 Requirement;				/**< Requirements placed on the device by the caller */
-	u32 Usage;					/**< Usage info (which subsystem is using the device) */
-} XPm_DeviceStatus;
+enum XPmPllMode {
+	PM_PLL_MODE_INTEGER		= (0U),		/**< PLL mode integer */
+	PM_PLL_MODE_FRACTIONAL		= (1U),		/**< PLL mode fractional */
+	PM_PLL_MODE_RESET		= (2U),		/**< PLL mode reset */
+};
+
 
 /**
- * PM API callback IDs
+ * PM operating characteristic types
  */
-typedef enum {
-	PM_INIT_SUSPEND_CB		= (30),		/**< Suspend callback */
-	PM_ACKNOWLEDGE_CB		= (31),		/**< Acknowledge callback */
-	PM_NOTIFY_CB			= (32),		/**< Notify callback */
-} XPmApiCbId_t;
+enum XPmOpCharType {
+	PM_OPCHAR_TYPE_POWER = 1,			/**< Operating characteristic ID power */
+	PM_OPCHAR_TYPE_TEMP,				/**< Operating characteristic ID temperature */
+	PM_OPCHAR_TYPE_LATENCY,				/**< Operating characteristic ID latency */
+};
 
+/**
+ * PM notify events
+ */
+enum XPmNotifyEvent {
+        EVENT_STATE_CHANGE = 1,				/**< State change event */
+        EVENT_ZERO_USERS = 2,				/**< Zero user event */
+	EVENT_CPU_IDLE_FORCE_PWRDWN = 4,		/**< CPU idle event during force power down */
+};
+
+/**
+ * @name System shutdown macros
+ * @{
+ */
+/**
+ * System shutdown macros
+ */
+#define PM_SHUTDOWN_TYPE_SHUTDOWN		(0U)
+#define PM_SHUTDOWN_TYPE_RESET			(1U)
+
+#define PM_SHUTDOWN_SUBTYPE_RST_SUBSYSTEM	(0U)
+#define PM_SHUTDOWN_SUBTYPE_RST_PS_ONLY		(1U)
+#define PM_SHUTDOWN_SUBTYPE_RST_SYSTEM		(2U)
+/** @} */
+
+/**
+ * @name State arguments of the self suspend
+ * @{
+ */
+/**
+ * State arguments of the self suspend
+ */
+#define PM_SUSPEND_STATE_CPU_IDLE		0x0U
+#define PM_SUSPEND_STATE_SUSPEND_TO_RAM		0xFU
+/** @} */
+
+/**
+ * @name RPU operation mode
+ * @{
+ */
 /**
  * RPU operation mode
  */
 #define XPM_RPU_MODE_LOCKSTEP	0U
 #define XPM_RPU_MODE_SPLIT	1U
+/** @} */
 
-#define XPM_RPU_SLSPLIT_MASK		BIT(3)
-#define XPM_RPU_TCM_COMB_MASK		BIT(6)
-#define XPM_RPU_SLCLAMP_MASK		BIT(4)
+/**
+ * @name RPU Boot memory
+ * @{
+ */
+/**
+ * RPU Boot memory
+ */
+#define XPM_RPU_BOOTMEM_LOVEC	(0U)
+#define XPM_RPU_BOOTMEM_HIVEC	(1U)
+/** @} */
+
+/**
+ * @name RPU TCM mode
+ * @{
+ */
+/**
+ * RPU TCM mode
+ */
+#define XPM_RPU_TCM_SPLIT	0U
+#define XPM_RPU_TCM_COMB	1U
+/** @} */
+
+/**
+ * @name Boot health status mask
+ * @{
+ */
+/**
+ * Boot health status mask
+ */
+#define XPM_BOOT_HEALTH_STATUS_MASK	(0x1U)
+/** @} */
+
+/**
+ * @name Tap delay signal type
+ * @{
+ */
+/**
+ * Tap delay signal type
+ */
+#define XPM_TAPDELAY_QSPI		(2U)
+/** @} */
+
+/**
+ * @name Tap delay bypass
+ * @{
+ */
+/**
+ * Tap delay bypass
+ */
+#define XPM_TAPDELAY_BYPASS_DISABLE	(0U)
+#define XPM_TAPDELAY_BYPASS_ENABLE	(1U)
+/** @} */
+
+/**
+ * @name Ospi AXI Mux select
+ * @{
+ */
+/**
+ * Ospi AXI Mux select
+ */
+#define XPM_OSPI_MUX_SEL_DMA		(0U)
+#define XPM_OSPI_MUX_SEL_LINEAR		(1U)
+#define XPM_OSPI_MUX_GET_MODE		(2U)
+/** @} */
+
+/**
+ * @name Tap delay type
+ * @{
+ */
+/**
+ * Tap delay type
+ */
+#define XPM_TAPDELAY_INPUT		(0U)
+#define XPM_TAPDELAY_OUTPUT		(1U)
+/** @} */
+
+/**
+ * @name Dll reset type
+ * @{
+ */
+/**
+ * Dll reset type
+ */
+#define XPM_DLL_RESET_ASSERT		(0U)
+#define XPM_DLL_RESET_RELEASE		(1U)
+#define XPM_DLL_RESET_PULSE		(2U)
+/** @} */
+
+/**
+ * @name Reset Reason
+ * @{
+ */
+/**
+ * Reset Reason
+ */
+#define XPM_RESET_REASON_EXT_POR	(0U)
+#define XPM_RESET_REASON_SW_POR		(1U)
+#define XPM_RESET_REASON_SLR_POR	(2U)
+#define XPM_RESET_REASON_ERR_POR	(3U)
+#define XPM_RESET_REASON_DAP_SRST	(7U)
+#define XPM_RESET_REASON_ERR_SRST	(8U)
+#define XPM_RESET_REASON_SW_SRST	(9U)
+#define XPM_RESET_REASON_SLR_SRST	(10U)
+#define XPM_RESET_REASON_INVALID	(0xFFU)
+/** @} */
+
+/**
+ * @name Probe Counter Type
+ * @{
+ */
+/**
+ * Probe Counter Type
+ */
+#define XPM_PROBE_COUNTER_TYPE_LAR_LSR		(0U)
+#define XPM_PROBE_COUNTER_TYPE_MAIN_CTL		(1U)
+#define XPM_PROBE_COUNTER_TYPE_CFG_CTL		(2U)
+#define XPM_PROBE_COUNTER_TYPE_STATE_PERIOD	(3U)
+#define XPM_PROBE_COUNTER_TYPE_PORT_SEL		(4U)
+#define XPM_PROBE_COUNTER_TYPE_SRC		(5U)
+#define XPM_PROBE_COUNTER_TYPE_VAL		(6U)
+/** @} */
+
+/**
+ * @name PM API versions
+ * @{
+ */
+/**
+ * PM API versions
+ */
+#define XST_API_BASE_VERSION		(1U)
+#define XST_API_QUERY_DATA_VERSION	(2U)
+#define XST_API_REG_NOTIFIER_VERSION	(2U)
+#define XST_API_PM_IOCTL_VERSION	(2U)
+/** @} */
 
 /**
  * PM API IDs
