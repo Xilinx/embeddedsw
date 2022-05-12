70c3e594c8094a4da0e7290f63e9b5020016d28c
Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
2022-05-12 18:54:46 +0530

    lib: sw_apps: Update memory and peripheral tests apps to support VERSA NET
    Update memory and peripheral tests apps to return A78 and R52 as supported
    processors.
    
    Signed-off-by: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
    Acked-by:  Mubin Usman Sayyed <mubin.usman.sayyed@xilinx.com>

--- lib/sw_apps/memory_tests/data/memory_tests.tcl
+++ lib/sw_apps/memory_tests/data/memory_tests.tcl
@@ -414,7 +414,7 @@ proc swapp_get_linker_constraints {} {
 }
 
 proc swapp_get_supported_processors {} {
-    return "microblaze ps7_cortexa9 psu_cortexa53 psu_cortexr5 psv_cortexa72 psv_cortexr5";
+    return "microblaze ps7_cortexa9 psu_cortexa53 psu_cortexr5 psv_cortexa72 psv_cortexr5 psxl_cortexa78 psxl_cortexr52 psx_cortexa78 psx_cortexr52";
 }
 
 proc swapp_get_supported_os {} {
