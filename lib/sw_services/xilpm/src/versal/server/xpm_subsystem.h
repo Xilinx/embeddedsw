1fc5fab150fc144bfa849a2519cb0e44f79112c6
Abhyuday Godhasara <abhyuday.godhasara@xilinx.com>
2022-06-17 02:39:32 -0700

    xilpm: versal: server: notify base on event sequence
    As per current implementation pending events gets notify in order of events
    was registered instead of event occurred. So there is an possibility of
    that certain events are repeatedly being skipped.
    
    So, notify subsystem base on event sequence where occurred event was stored
    in sequence of occurrence.
    
    Signed-off-by: Abhyuday Godhasara <abhyuday.godhasara@xilinx.com>
    
    Acked-for-series: Ronak Jain <ronak.jain@xilinx.com>=20

--- lib/sw_services/xilpm/src/versal/server/xpm_subsystem.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_subsystem.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (c) 2018 - 2021 Xilinx, Inc.  All rights reserved.
+* Copyright (c) 2018 - 2022 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -26,6 +26,7 @@ extern "C" {
 #define SUBSYSTEM_INIT_FINALIZED		((u8)1U << 0U)
 #define SUBSYSTEM_IS_CONFIGURED			((u8)1U << 1U)
 #define SUBSYSTEM_IDLE_SUPPORTED		((u8)1U << 2U)
+#define SUBSYSTEM_SUSCB_PRIORITIZE		((u8)1U << 3U)
 
 /**
  * Helper macros to check subsystem specific flags.
