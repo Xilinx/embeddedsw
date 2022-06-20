2694d5efb6c9c12bc5ea5fd6655b35135deddd18
Ben Levinsky <ben.levinsky@xilinx.com>
2022-06-20 09:27:15 -0700

    lib: sw_apps: openamp_echo_test: Add switch to create multiple endpoints
    Add macro ECHO_NUM_EPTS that, if defined, will create multiple endpoints
    with the endpoint name "rpmsg-openamp-demo-channelX" where X is
    0<N<1 for N endpoints as defined by the macro ECHO_NUM_EPTS.
    
    If the macro is not defined then one endpoint is defined with the name:
    "rpmsg-openamp-demo-channel".
    
    The default value for ECHO_NUM_EPTS is 1. Additionally there is a macro guard
    around the macro ECHO_NUM_EPTS so that if the symbol is defined with an integer
    value in the BSP, this can override the default value provided.
    
    Signed-off-by: Ben Levinsky <ben.levinsky@xilinx.com>
    Acked-by: Sergei Korneichuk < sergei.korneichuk@amd.com>

--- lib/sw_apps/openamp_echo_test/src/system/generic/rpmsg-echo.c
+++ lib/sw_apps/openamp_echo_test/src/system/generic/rpmsg-echo.c
@@ -4,7 +4,39 @@
 
 /* This is a sample demonstration application that showcases usage of rpmsg
 This application is meant to run on the remote CPU running baremetal code.
-This application echoes back data that was sent to it by the master core. */
+This application echoes back data that was sent to it by the master core.
+
+In addition this application supports multiple endpoints. If the
+macro ECHO_NUM_EPTS is set to a number >1, then this application
+echoes back via one of N endpoints that correspond 1:1 with endpoints
+on the host.
+
+This firmware is expected to be loaded via Xilinx R5 Remoteproc kernel driver.
+
+Usage on Linux to interact with this is as follows:
+
+1. Load and Start Firmware on RPU:
+echo image_multiple_echo_test > /sys/class/remoteproc/remoteproc0/firmware
+echo start > /sys/class/remoteproc/remoteproc0/state
+
+2. Start Daemon to create control interface:
+systemctl start ept-mgrd
+
+3. Start client and specify one of the two channel by RPMsg endpoint name:
+ept_client -e <channel name>
+
+4. Stop Daemon:
+systemctl stop ept-mgrd
+
+5. Stop and power down RPU
+echo stop > /sys/class/remoteproc/remoteproc0/state
+
+The 2 channel names are:
+"rpmsg-openamp-demo-channel0" and "rpmsg-openamp-demo-channel1"
+
+Note that in the case of multiple endpoints, the 0th endpoint has the
+original name of "rpmsg-openamp-demo-channel".
+*/
 
 #include "xil_printf.h"
 #include <openamp/open_amp.h>
@@ -13,13 +45,24 @@ This application echoes back data that was sent to it by the master core. */
 #include <metal/version.h>
 #include "platform_info.h"
 #include "rpmsg-echo.h"
+#include <stdio.h>
 
 #define SHUTDOWN_MSG	0xEF56A55A
 
 #define LPRINTF(fmt, ...) xil_printf("%s():%u " fmt, __func__, __LINE__, ##__VA_ARGS__)
 #define LPERROR(fmt, ...) LPRINTF("ERROR: " fmt, ##__VA_ARGS__)
 
-static struct rpmsg_endpoint lept;
+#define EPT_NAME_LEN 32
+
+#ifndef ECHO_NUM_EPTS
+#define ECHO_NUM_EPTS 1
+#endif /* !ECHO_NUM_EPTS */
+
+#if ECHO_NUM_EPTS < 1 || ECHO_NUM_EPTS > 5
+#error "ECHO_NUM_EPTS should be from 1 to 5"
+#endif /* ECHO_NUM_EPTS < 1 || ECHO_NUM_EPTS > 5 */
+
+static struct rpmsg_endpoint lept[ECHO_NUM_EPTS];
 static int shutdown_req = 0;
 
 /*-----------------------------------------------------------------------------*
@@ -57,18 +100,28 @@ static void rpmsg_service_unbind(struct rpmsg_endpoint *ept)
  *-----------------------------------------------------------------------------*/
 int app(struct rpmsg_device *rdev, void *priv)
 {
-	int ret;
+	int ret, i;
+	char ept_name[EPT_NAME_LEN] = RPMSG_SERVICE_NAME;
 
 	/* Initialize RPMSG framework */
 	ML_INFO("Try to create rpmsg endpoint.\r\n");
+	for (i = 0; i < ECHO_NUM_EPTS; i++) {
+		if (i != 0)
+			sprintf(ept_name, "%s%d", RPMSG_SERVICE_NAME, i);
 
-	ret = rpmsg_create_ept(&lept, rdev, RPMSG_SERVICE_NAME,
-			       RPMSG_ADDR_ANY, RPMSG_ADDR_ANY,
-			       rpmsg_endpoint_cb,
-			       rpmsg_service_unbind);
-	if (ret) {
-		ML_ERR("Failed to create endpoint.\r\n");
-		return -1;
+		/* Initialize RPMSG framework */
+		ML_INFO("Try to create rpmsg endpoint %s.\r\n", ept_name);
+
+		ret = rpmsg_create_ept(&lept[i], rdev, ept_name,
+				       RPMSG_ADDR_ANY, RPMSG_ADDR_ANY,
+				       rpmsg_endpoint_cb,
+				       rpmsg_service_unbind);
+		if (ret) {
+			ML_ERR("Failed to create endpoint.\r\n");
+			return -1;
+		}
+
+		ML_INFO("Successfully created rpmsg endpoint.\r\n");
 	}
 
 	ML_INFO("Successfully created rpmsg endpoint.\r\n");
@@ -80,7 +133,9 @@ int app(struct rpmsg_device *rdev, void *priv)
 		}
 	}
 	ML_DBG("done\r\n");
-	rpmsg_destroy_ept(&lept);
+
+	for (i = 0; i < ECHO_NUM_EPTS; i++)
+		rpmsg_destroy_ept(&lept[i]);
 
 	return 0;
 }
