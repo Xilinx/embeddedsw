bbc9ab4d312af4ebc42d024ce3a320e754288419
Harsha <harsha.harsha@xilinx.com>
2022-07-10 16:40:30 +0530

    sw_services: xilpuf: Added VERSAL_NET support in server APIs
    This patch adds the following support for VERSAL_NET in server APIs:
     - Added support for the new register PUF_RO_SWAP.
     - Added a new shutter value 0x01000080U for Versal_net
     - Currently a check is done to ensure that MSB of shutter value
       is in sync with Global Variation Filter option. As it is fixed
       in VERSAL_NET ROM, so this check should be done for Versal only.
    
    Signed-off-by: Harsha Harsha <harsha.harsha@xilinx.com>
    
    Acked-for-series: VNSL Durga Challa <vnsl.durga.challa@xilinx.com>

---
+++ lib/sw_services/xilpuf/src/server/xpuf_plat.h
@@ -0,0 +1,69 @@
+/******************************************************************************
+* Copyright (c) 2022 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+/*****************************************************************************/
+/**
+*
+* @file xpuf_plat.h
+* @addtogroup xpuf_plat_apis XilPuf Platform specific APIs
+* @{
+* @cond xpuf_internal
+* This file contains platform specific APIs for PUF
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who  Date        Changes
+* ----- ---- ---------- -------------------------------------------------------
+* 1.0   har  07/04/2022 Initial release
+*
+* </pre>
+*
+* @endcond
+*
+*******************************************************************************/
+#ifndef XPUF_PLAT_H
+#define XPUF_PLAT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/****************************** Include Files *********************************/
+#include "xparameters.h"
+#include "xil_types.h"
+
+/*************************** Constant Definitions *****************************/
+/** @cond xpuf_internal
+@{
+*/
+#if defined (VERSAL_NET)
+#define XPUF_PMX_GLOBAL_PUF_RO_SWP_OFFSET		(0x00000100U)
+#endif
+
+#if defined (VERSAL_NET)
+#define XPUF_SHUTTER_VALUE				(0x01000080U)
+		/**< PUF Shutter Value - Versal Net */
+#else
+#define XPUF_SHUTTER_VALUE				(0x81000100U)
+		/**< PUF Shutter Value - Versal */
+#endif
+
+/***************************** Type Definitions *******************************/
+typedef struct _XPuf_Data XPuf_Data;
+
+/***************** Macros (Inline Functions) Definitions *********************/
+
+/*************************** Function Prototypes ******************************/
+int XPuf_CheckGlobalVariationFilter(XPuf_Data *PufData);
+void XPuf_SetRoSwap(XPuf_Data *PufData);
+/** @}
+@endcond */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* XPUF_PLAT_H */
