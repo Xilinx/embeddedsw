3d1adca03b2a6cc7f2f30a90061ce07aff5003fa
lakshmip <lakshmi.prasanna.eachuri@xilinx.com>
2022-07-12 20:47:08 +0530

    dp21: Adding new folder structure for dp21 drivers
    This patch creates the new folder structure as follows:
    
    moves dp14 -> dp21
    
    Created new folder structure from the following source:
    Commit id : bb1f185
    
    new folder : dp21
    
    Signed-off-by: Lakshmi Prasanna Eachuri <lakshmi.prasanna.eachuri@xilinx.com>
    
    Acked-for-series: Jagadeesh Banisetti <jagadeesh.banisetti@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/dp21/data/dp14.tcl
@@ -0,0 +1,180 @@
+##******************************************************************************
+# Copyright (C) 2014 - 2020 Xilinx, Inc.  All rights reserved.
+# SPDX-License-Identifier: MIT
+#
+#
+##*****************************************************************************/
+##############################################################################
+#
+# Modification History
+#
+# Ver   Who  Date     Changes
+# ----- ---- -------- -----------------------------------------------
+# 5.3   ms   04/18/17 Modified tcl file to add suffix U for all macros
+#                     definitions of dp in xparameters.h
+##############################################################################
+proc generate {drv_handle} {
+        xdefine_include_file $drv_handle "xparameters.h" "XDp" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_LANE_COUNT" "C_LINK_RATE" "C_MAX_BITS_PER_COLOR" "C_QUAD_PIXEL_ENABLE" "C_DUAL_PIXEL_ENABLE" "C_YCRCB_ENABLE" "C_YONLY_ENABLE" "C_GT_DATAWIDTH" "C_SECONDARY_SUPPORT" "C_AUDIO_CHANNELS" "C_MST_ENABLE" "C_NUMBER_OF_MST_STREAMS" "C_PROTOCOL_SELECTION" "C_FLOW_DIRECTION" "S_AXI_ACLK"
+        ::hsi::utils::define_config_file $drv_handle "xdp_g.c" "XDp" "DEVICE_ID" "C_BASEADDR" "S_AXI_ACLK" "C_LANE_COUNT" "C_LINK_RATE" "C_MAX_BITS_PER_COLOR" "C_QUAD_PIXEL_ENABLE" "C_DUAL_PIXEL_ENABLE" "C_YCRCB_ENABLE" "C_YONLY_ENABLE" "C_GT_DATAWIDTH" "C_SECONDARY_SUPPORT" "C_AUDIO_CHANNELS" "C_MST_ENABLE" "C_NUMBER_OF_MST_STREAMS" "C_PROTOCOL_SELECTION" "C_FLOW_DIRECTION"
+        xdefine_canonical_xpars $drv_handle "xparameters.h" "XDp" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_LANE_COUNT" "C_LINK_RATE" "C_MAX_BITS_PER_COLOR" "C_QUAD_PIXEL_ENABLE" "C_DUAL_PIXEL_ENABLE" "C_YCRCB_ENABLE" "C_YONLY_ENABLE" "C_GT_DATAWIDTH" "C_SECONDARY_SUPPORT" "C_AUDIO_CHANNELS" "C_MST_ENABLE" "C_NUMBER_OF_MST_STREAMS" "C_PROTOCOL_SELECTION" "C_FLOW_DIRECTION" "S_AXI_ACLK"
+}
+
+#
+# Given a list of arguments, define them all in an include file.
+# Handles IP model/user parameters, as well as the special parameters NUM_INSTANCES,
+# DEVICE_ID
+# Will not work for a processor.
+#
+proc xdefine_include_file {drv_handle file_name drv_string args} {
+    set args [get_exact_arg_list $args]
+    # Open include file
+    set file_handle [xopen_include_file $file_name]
+
+    # Get all peripherals connected to this driver
+    set periphs [xget_sw_iplist_for_driver $drv_handle]
+
+    set uSuffix "U"
+    # Handle special cases
+    set arg "NUM_INSTANCES"
+    set posn [lsearch -exact $args $arg]
+    if {$posn > -1} {
+        puts $file_handle "/* Definitions for driver [string toupper [get_property name $drv_handle]] */"
+        # Define NUM_INSTANCES
+        puts $file_handle "#define [xget_dname $drv_string $arg] [llength $periphs]$uSuffix"
+        set args [lreplace $args $posn $posn]
+    }
+
+    # Check if it is a driver parameter
+    lappend newargs
+    foreach arg $args {
+        set value [get_property CONFIG.$arg $drv_handle]
+        if {[llength $value] == 0} {
+            lappend newargs $arg
+        } else {
+            puts $file_handle "#define [xget_dname $drv_string $arg] [get_property $arg $drv_handle]$uSuffix"
+        }
+    }
+    set args $newargs
+
+    # Print all parameters for all peripherals
+    set device_id 0
+    foreach periph $periphs {
+        puts $file_handle ""
+        puts $file_handle "/* Definitions for peripheral [string toupper [get_property NAME $periph]] */"
+
+        foreach arg $args {
+            if {[string compare -nocase "DEVICE_ID" $arg] == 0} {
+                set value $device_id
+                incr device_id
+            } elseif {[string compare -nocase "S_AXI_ACLK" $arg] == 0} {
+                set freq [xget_ip_clk_pin_freq $periph $arg]
+                if {[llength $freq] == 0} {
+                        set freq 0
+                        puts "WARNING: Clock frequency information is not available in the design, \
+                              for peripheral $periph_name. This will not work."
+                }
+                set value $freq
+            } else {
+                set value [get_property CONFIG.$arg $periph]
+            }
+            if {[llength $value] == 0} {
+                set value 0
+            }
+            set value [xformat_addr_string $value $arg]
+            if {[string compare -nocase "HW_VER" $arg] == 0} {
+                puts $file_handle "#define [xget_name $periph $arg] \"$value\""
+            } else {
+                puts $file_handle "#define [xget_name $periph $arg] $value$uSuffix"
+            }
+        }
+        puts $file_handle ""
+    }
+    puts $file_handle "\n/******************************************************************/\n"
+    close $file_handle
+}
+
+#
+# xdefine_canonical_xpars - Used to print out canonical defines for a driver.
+# Given a list of arguments, define each as a canonical constant name, using
+# the driver name, in an include file.
+#
+proc xdefine_canonical_xpars {drv_handle file_name drv_string args} {
+    set args [get_exact_arg_list $args]
+   # Open include file
+   set file_handle [xopen_include_file $file_name]
+
+   # Get all the peripherals connected to this driver
+   set periphs [xget_sw_iplist_for_driver $drv_handle]
+
+   # Get the names of all the peripherals connected to this driver
+   foreach periph $periphs {
+       set peripheral_name [string toupper [get_property NAME $periph]]
+       lappend peripherals $peripheral_name
+   }
+
+   # Get possible canonical names for all the peripherals connected to this
+   # driver
+   set device_id 0
+   foreach periph $periphs {
+       set canonical_name [string toupper [format "%s_%s" $drv_string $device_id]]
+       lappend canonicals $canonical_name
+
+       # Create a list of IDs of the peripherals whose hardware instance name
+       # doesn't match the canonical name. These IDs can be used later to
+       # generate canonical definitions
+       if { [lsearch $peripherals $canonical_name] < 0 } {
+           lappend indices $device_id
+       }
+       incr device_id
+   }
+
+   set i 0
+   foreach periph $periphs {
+       set periph_name [string toupper [get_property NAME $periph]]
+
+       # Generate canonical definitions only for the peripherals whose
+       # canonical name is not the same as hardware instance name
+       if { [lsearch $canonicals $periph_name] < 0 } {
+           puts $file_handle "/* Canonical definitions for peripheral $periph_name */"
+           set canonical_name [format "%s_%s" $drv_string [lindex $indices $i]]
+
+           foreach arg $args {
+               set lvalue [xget_dname $canonical_name $arg]
+
+               # The commented out rvalue is the name of the instance-specific constant
+               # set rvalue [xget_name $periph $arg]
+               # The rvalue set below is the actual value of the parameter
+                if {[string compare -nocase "S_AXI_ACLK" $arg] == 0} {
+                        set freq [xget_ip_clk_pin_freq $periph $arg]
+                        if {[llength $freq] == 0} {
+                            set freq 0
+                            puts "WARNING: Clock frequency information is not available in the design, \
+                                  for peripheral $periph_name. This will not work."
+                        }
+                        set rvalue $freq
+                } else {
+                    set rvalue [xget_param_value $periph $arg]
+                    if {[llength $rvalue] == 0} {
+                        set rvalue 0
+                    }
+                    set rvalue [xformat_addr_string $rvalue $arg]
+                }
+	       set uSuffix [xdefine_getSuffix $lvalue $rvalue]
+               puts $file_handle "#define $lvalue $rvalue$uSuffix"
+           }
+           puts $file_handle ""
+           incr i
+       }
+   }
+
+   puts $file_handle "\n/******************************************************************/\n"
+   close $file_handle
+}
+
+proc xdefine_getSuffix {arg_name value} {
+	set uSuffix ""
+	if { [string match "*DEVICE_ID" $value] == 0 } {
+		set uSuffix "U"
+	}
+	return $uSuffix
+}
---
+++ XilinxProcessorIPLib/drivers/dp21/src/xdp_selftest.c
@@ -0,0 +1,242 @@
+/*******************************************************************************
+* Copyright (C) 2015 - 2020 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+*******************************************************************************/
+
+/******************************************************************************/
+/**
+ *
+ * @file xdp_selftest.c
+ * @addtogroup dp_v7_6
+ * @{
+ *
+ * This file contains a diagnostic self-test function for the XDp driver. It
+ * will check many of the DisplayPort core's register values against the default
+ * reset values as a sanity-check that the core is ready to be used.
+ *
+ * @note	None.
+ *
+ * <pre>
+ * MODIFICATION HISTORY:
+ *
+ * Ver   Who  Date     Changes
+ * ----- ---- -------- -----------------------------------------------
+ * 1.0   als  01/20/15 Initial release. TX code merged from the dptx driver.
+ * 5.0   als  07/27/16 8B10B enable is now set by default in PHY_CONFIG reg.
+ * 6.0   tu   08/04/17 Changed Selftest regiter's default value.
+ * </pre>
+ *
+*******************************************************************************/
+
+/******************************* Include Files ********************************/
+
+#include "xdp.h"
+
+/**************************** Function Prototypes *****************************/
+
+#if XPAR_XDPTXSS_NUM_INSTANCES
+static u32 XDp_TxSelfTest(XDp *InstancePtr);
+#endif
+#if XPAR_XDPRXSS_NUM_INSTANCES
+static u32 XDp_RxSelfTest(XDp *InstancePtr);
+#endif
+
+/**************************** Variable Definitions ****************************/
+
+#if XPAR_XDPTXSS_NUM_INSTANCES
+/**
+ * This table contains the default values for the DisplayPort TX core's general
+ * usage registers.
+ */
+u32 TxResetValues[2][2] =
+{
+	{XDP_TX_VERSION, 0x07000000},
+	{XDP_TX_CORE_ID, 0x01020A00}
+};
+
+/**
+ * This table contains the default values for the DisplayPort TX core's main
+ * stream attribute (MSA) registers.
+ */
+u32 TxResetValuesMsa[20][2] =
+{
+	{XDP_TX_MAIN_STREAM_HTOTAL, 0},
+	{XDP_TX_MAIN_STREAM_VTOTAL, 0},
+	{XDP_TX_MAIN_STREAM_POLARITY, 0},
+	{XDP_TX_MAIN_STREAM_HSWIDTH, 0},
+	{XDP_TX_MAIN_STREAM_VSWIDTH, 0},
+	{XDP_TX_MAIN_STREAM_HRES, 0},
+	{XDP_TX_MAIN_STREAM_VRES, 0},
+	{XDP_TX_MAIN_STREAM_HSTART, 0},
+	{XDP_TX_MAIN_STREAM_VSTART, 0},
+	{XDP_TX_MAIN_STREAM_MISC0, 0},
+	{XDP_TX_MAIN_STREAM_MISC1, 0},
+	{XDP_TX_M_VID, 0},
+	{XDP_TX_TU_SIZE, 0},
+	{XDP_TX_N_VID, 0},
+	{XDP_TX_USER_PIXEL_WIDTH, 0},
+	{XDP_TX_USER_DATA_COUNT_PER_LANE, 0},
+	{XDP_TX_MAIN_STREAM_INTERLACED, 0},
+	{XDP_TX_MIN_BYTES_PER_TU, 0},
+	{XDP_TX_FRAC_BYTES_PER_TU, 0},
+	{XDP_TX_INIT_WAIT, 32}
+};
+#endif /* XPAR_XDPTXSS_NUM_INSTANCES */
+
+#if XPAR_XDPRXSS_NUM_INSTANCES
+/**
+ * This table contains the default values for the DisplayPort RX core's general
+ * usage registers.
+ */
+u32 RxResetValues[2][2] =
+{
+	{XDP_RX_VERSION, 0x07000000},
+	{XDP_RX_CORE_ID, 0x01020A01}
+};
+#endif /* XPAR_XDPRXSS_NUM_INSTANCES */
+
+/**************************** Function Definitions ****************************/
+
+/******************************************************************************/
+/**
+ * This function runs a self-test on the XDp driver/device depending on whether
+ * the core is operating in TX or RX mode. The sanity test checks whether or not
+ * all tested registers hold their default reset values.
+ *
+ * @param	InstancePtr is a pointer to the XDp instance.
+ *
+ * @return
+ *		- XST_SUCCESS if the self-test passed - all tested registers
+ *		  hold their default reset values.
+ *		- XST_FAILURE otherwise.
+ *
+ * @note	None.
+ *
+*******************************************************************************/
+u32 XDp_SelfTest(XDp *InstancePtr)
+{
+	u32 Status;
+
+	/* Verify arguments. */
+	Xil_AssertNonvoid(InstancePtr != NULL);
+	Xil_AssertNonvoid(InstancePtr->IsReady == XIL_COMPONENT_IS_READY);
+
+#if XPAR_XDPTXSS_NUM_INSTANCES
+	if (XDp_GetCoreType(InstancePtr) == XDP_TX) {
+		Status = XDp_TxSelfTest(InstancePtr);
+	} else
+#endif
+#if XPAR_XDPRXSS_NUM_INSTANCES
+	if (XDp_GetCoreType(InstancePtr) == XDP_RX)	{
+		Status = XDp_RxSelfTest(InstancePtr);
+	} else
+#endif
+	{
+		Status = XST_DEVICE_NOT_FOUND;
+	}
+
+	return Status;
+}
+
+#if XPAR_XDPTXSS_NUM_INSTANCES
+/******************************************************************************/
+/**
+ * This function runs a self-test on the XDp driver/device. The sanity test
+ * checks whether or not all tested registers hold their default reset values.
+ *
+ * @param	InstancePtr is a pointer to the XDp instance.
+ *
+ * @return
+ *		- XST_SUCCESS if the self-test passed - all tested registers
+ *		  hold their default reset values.
+ *		- XST_FAILURE otherwise.
+ *
+ * @note	None.
+ *
+*******************************************************************************/
+static u32 XDp_TxSelfTest(XDp *InstancePtr)
+{
+	u8 Index;
+	u8 StreamIndex;
+	u32 StreamOffset;
+	u32 Val;
+
+	/* Compare general usage registers with their default values. */
+	for (Index = 0; Index < 2; Index++) {
+		Val = XDp_ReadReg(InstancePtr->Config.BaseAddr,
+						TxResetValues[Index][0]);
+		/* Fail if register does not hold default value. */
+		if (Val != TxResetValues[Index][1]) {
+			return XST_FAILURE;
+		}
+	}
+
+	/* Compare main stream attribute (MSA) registers for all 4 streams with
+	 * their default values. */
+	for (StreamIndex = 0; StreamIndex < 4; StreamIndex++) {
+		/* Determine the MSA register offset for each stream. */
+		if (StreamIndex == 0) {
+			StreamOffset = 0;
+		}
+		else if (StreamIndex == 1) {
+			StreamOffset = XDP_TX_STREAM2_MSA_START_OFFSET;
+		}
+		else if (StreamIndex == 2) {
+			StreamOffset = XDP_TX_STREAM3_MSA_START_OFFSET;
+		}
+		else if (StreamIndex == 3) {
+			StreamOffset = XDP_TX_STREAM4_MSA_START_OFFSET;
+		}
+
+		for (Index = 0; Index < 20; Index++) {
+			Val = XDp_ReadReg(InstancePtr->Config.BaseAddr,
+				StreamOffset + TxResetValuesMsa[Index][0]);
+			/* Fail if register does not hold default value. */
+			if (Val != TxResetValuesMsa[Index][1]) {
+				return XST_FAILURE;
+			}
+		}
+	}
+
+	/* All tested registers hold their default reset values. */
+	return XST_SUCCESS;
+}
+#endif /* XPAR_XDPTXSS_NUM_INSTANCES */
+
+#if XPAR_XDPRXSS_NUM_INSTANCES
+/******************************************************************************/
+/**
+ * This function runs a self-test on the XDp driver/device running in RX mode.
+ * The sanity test checks whether or not all tested registers hold their default
+ * reset values.
+ *
+ * @param	InstancePtr is a pointer to the XDp instance.
+ *
+ * @return
+ *		- XST_SUCCESS if the self-test passed - all tested registers
+ *		  hold their default reset values.
+ *		- XST_FAILURE otherwise.
+ *
+ * @note	None.
+ *
+*******************************************************************************/
+static u32 XDp_RxSelfTest(XDp *InstancePtr)
+{
+	u8 Index;
+	u32 Val;
+
+	/* Compare general usage registers with their default values. */
+	for (Index = 0; Index < 2; Index++) {
+		Val = XDp_ReadReg(InstancePtr->Config.BaseAddr,
+						RxResetValues[Index][0]);
+		/* Fail if register does not hold default value. */
+		if (Val != RxResetValues[Index][1]) {
+			return XST_FAILURE;
+		}
+	}
+
+	/* All tested registers hold their default reset values. */
+	return XST_SUCCESS;
+}
+#endif /* XPAR_XDPRXSS_NUM_INSTANCES */
+/** @} */
