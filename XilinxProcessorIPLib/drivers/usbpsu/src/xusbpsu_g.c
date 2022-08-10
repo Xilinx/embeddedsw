abdc753166d1142d4c934513801c3e1baab2123e
Piyush Mehta <piyush.mehta@amd.com>
2022-08-10 15:32:20 +0530

    usb: usbpsu: src: update copyright year
    Updated copyright year with 2022.
    
    Signed-off-by: Piyush Mehta <piyush.mehta@amd.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

--- XilinxProcessorIPLib/drivers/usbpsu/src/xusbpsu_g.c
+++ XilinxProcessorIPLib/drivers/usbpsu/src/xusbpsu_g.c
@@ -1,5 +1,5 @@
 /*******************************************************************
-* Copyright (C) 2016 - 2021 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2016 - 2022 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 *******************************************************************/
 
