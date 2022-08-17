a8ce0e008b7ca08fd8da6e4e2032f8fb40241001
Ankush Mehtre <ankush.mehtre@amd.com>
2022-08-18 03:18:10 +0530

    VersalNet: xilsecure: Add Copyright section in Ecdsa.h
    This patch adds copyright section and addresses review
    comments in Ecdsa.h file for VersalNet.
    
    Signed-off-by: Ankush Mehtre <ankush.mehtre@xilinx.com>
    Acked-by: Kundanala, Praveen Teja <praveen.teja.kundanala@amd.com>

--- lib/sw_services/xilsecure/src/versal_net/server/Ecdsa.h
+++ lib/sw_services/xilsecure/src/versal_net/server/Ecdsa.h
@@ -1,26 +1,19 @@
-//
-// ECDSA compact code for RSA5X, "quiet" version
-// API definitions
-// (C) IP Cores, Inc. 2021-2022
-//
-// Version	1.8  Initial release (version number synchronized with regular Ecdsa)
-//			1.81 MISRA C changes applied, Xilinx-only changes
-//			1.82 Xilinx updates applied
-//			1.84 No changes, revision bumped to match the .c file
-//			1.86 Synchronized with Xilinx changes (except extern changed to externC
-//				 for Ecdsa_P384)
-//
-#ifndef RSA5X_H
-#define RSA5X_H
+/******************************************************************************
+* Copyright (C) 2022 IP Cores, Inc.  All rights reserved.
+* Copyright (C) 2022 Xilinx, Inc.  All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
 
-#include <stdint.h>
-#include <string.h>
-#include "xsecure_trng.h"
+/**<
+* ECDSA compact code for RSA5X, "quiet" version
+* API definitions
+*/
+#ifndef ECDSA_H
+#define ECSDA_H
 
-#define BYTE_SIZE_IN_BITS	8U
-#define TRNG_BURST_SIZE		128U
-#define RSA5X_GET_RANDOM(bytes, buf, bufsize) XTrng_Generate(bytes, buf, bufsize)
+//#define NO_COMPACT_ECDSA_SUPPORT // If defined, the compact ECDSA code is disabled
 
+/**< For compilation in C++ */
 #ifdef __cplusplus
 #define externC extern "C"
 #else
@@ -28,86 +21,93 @@
 #endif
 
 typedef struct {
-	u8* Qx;
-	u8* Qy;
+	u8* Qx;		/**< Public key curve point x */
+	u8* Qy;		/**< Public key curve point y */
 } EcdsaKey;
 
 typedef struct {
-	u8* r;
-	u8* s;
+	u8* r;		/**< The signature component r */
+	u8* s;		/**< The signature component s */
 } EcdsaSign;
 
 typedef enum {
-	ECDSA_INVALID_LOW = 0,
-	ECDSA_NIST_P192,
-	ECDSA_NIST_P224,
-	ECDSA_NIST_P256,
-	ECDSA_NIST_P384,
-	ECDSA_NIST_P521,
-	ECDSA_NIST_K163,
-	ECDSA_NIST_B163,
-	ECDSA_NIST_K233,
-	ECDSA_NIST_B233,
-	ECDSA_NIST_K283,
-	ECDSA_NIST_B283,
-	ECDSA_NIST_K409,
-	ECDSA_NIST_B409,
-	ECDSA_NIST_K571,
-	ECDSA_NIST_B571,
-	ECDSA_SECT_193R1,
-	ECDSA_SM2_FP256,
-	ECDSA_BRAINPOOL_P160,
-	ECDSA_BRAINPOOL_P192,
-	ECDSA_BRAINPOOL_P224,
-	ECDSA_BRAINPOOL_P256,
-	ECDSA_BRAINPOOL_P320,
-	ECDSA_BRAINPOOL_P384,
-	ECDSA_BRAINPOOL_P512,
-	ECDSA_INVALID_HIGH
+	ECDSA_INVALID_LOW = 0,	/**< Invalid value (min) */
+	ECDSA_NIST_P192,		/**< NIST P-192 curve */
+	ECDSA_NIST_P224,		/**< NIST P-224 curve */
+	ECDSA_NIST_P256,		/**< NIST P-256 curve */
+	ECDSA_NIST_P384,		/**< NIST P-384 curve */
+	ECDSA_NIST_P521,		/**< NIST P-521 curve */
+	ECDSA_NIST_K163,		/**< NIST K-163 curve */
+	ECDSA_NIST_B163,		/**< NIST B-163 curve */
+	ECDSA_NIST_K233,		/**< NIST K-233 curve */
+	ECDSA_NIST_B233,		/**< NIST B-233 curve */
+	ECDSA_NIST_K283,		/**< NIST K-283 curve */
+	ECDSA_NIST_B283,		/**< NIST B-283 curve */
+	ECDSA_NIST_K409,		/**< NIST K-409 curve */
+	ECDSA_NIST_B409,		/**< NIST B-409 curve */
+	ECDSA_NIST_K571,		/**< NIST K-571 curve */
+	ECDSA_NIST_B571,		/**< NIST B-571 curve */
+	ECDSA_SECT_193R1,		/**< SECT 193R1 curve */
+	ECDSA_SM2_FP256,		/**< SM2 FP 256 curve */
+	ECDSA_BRAINPOOL_P160,		/**< Brainpool P-160 curve */
+	ECDSA_BRAINPOOL_P192,		/**< Brainpool P-192 curve */
+	ECDSA_BRAINPOOL_P224,		/**< Brainpool P-224 curve */
+	ECDSA_BRAINPOOL_P256,		/**< Brainpool P-256 curve */
+	ECDSA_BRAINPOOL_P320,		/**< Brainpool P-320 curve */
+	ECDSA_BRAINPOOL_P384,		/**< Brainpool P-384 curve */
+	ECDSA_BRAINPOOL_P512,		/**< Brainpool P-512 curve */
+	ECDSA_INVALID_HIGH			/**< Invalid value (max) */
 } EcdsaCrvTyp;
 
 typedef enum {
-	ECDSA_PRIME = 0,
-	ECDSA_BINARY = 1,
+	ECDSA_PRIME = 0,	/**< Prime curve */
+	ECDSA_BINARY = 1,	/**< Binary curve */
 } EcdsaCrvClass;
 
-//
-// The structure below matches the more private structures of
-//    binary_curve and prime_curve in the SDK. The field names are
-//    obfuscated, as this API is intended for an end-user that does
-//    not necessarily have the rights to the source code of the SDK
-//
+/**
+ * @name Ecdsa curve info
+ * @{
+ */
+/**< The structure below matches the more private structures of
+ *   binary_curve and prime_curve in the SDK. The field names are
+ *   obfuscated, as this API is intended for an end-user that does
+ *   not necessarily have the rights to the source code of the SDK
+ */
 typedef struct {
-	EcdsaCrvTyp CrvType;
-	u16 Bits; EcdsaCrvClass Class;
-	const u8* const d0;
-	const u8* const d1;
-	const u8* const d2;
-	const u8* const d3;
-	const u8* const d4;
-	const u8* const d5;
-	const u8* const d6;
-	const u8* const d7;
-	const u8* const d8;
-	const u8* const d9;
-	const u8* const d10;
-	const u8* const d11;
-	const u8* const d12;
-	const u8* const d13;
-	const u8* const d14;
-	const u8* const d15;
+	EcdsaCrvTyp CrvType;	/**< Curve type */
+	u16 Bits;				/**< Curve bites */
+	EcdsaCrvClass Class;	/**< Curve class - Prime or Binary */
+	const u8* const d0;		/**< curve specific data field 0 */
+	const u8* const d1;		/**< curve specific data field 1 */
+	const u8* const d2;		/**< curve specific data field 2 */
+	const u8* const d3;		/**< curve specific data field 3 */
+	const u8* const d4;		/**< curve specific data field 4 */
+	const u8* const d5;		/**< curve specific data field 5 */
+	const u8* const d6;		/**< curve specific data field 6 */
+	const u8* const d7;		/**< curve specific data field 7 */
+	const u8* const d8;		/**< curve specific data field 8 */
+	const u8* const d9;		/**< curve specific data field 9 */
+	const u8* const d10;	/**< curve specific data field 10 */
+	const u8* const d11;	/**< curve specific data field 11 */
+	const u8* const d12;	/**< curve specific data field 12 */
+	const u8* const d13;	/**< curve specific data field 13 */
+	const u8* const d14;	/**< curve specific data field 14 */
+	const u8* const d15;	/**< curve specific data field 15 */
 } EcdsaCrvInfo;
+/** @} */
 
-externC const EcdsaCrvInfo Ecdsa_P384;
-
-//
-//	The following restrictions define the memory used for the ECC variables
-//  The design sometimes requires two extra digits
-//
+/**
+ * @name ECC variables size in bits
+ * @{
+ */
+/**< The following restrictions define the memory used for the ECC variables
+ *   The design sometimes requires two extra digits
+ */
 #define MAX_CURVE_BITS  571
 #define ECC_DIGIT_BITS	(RSA5X_DSIZE*8U)
 #define ECC_MAX_BITS	(((MAX_CURVE_BITS+ECC_DIGIT_BITS-1)/ECC_DIGIT_BITS+2)*ECC_DIGIT_BITS)
 #define ECC_P384_SIZE_IN_BYTES		48U
+/** @} */
 
 #define ELLIPTIC_SUCCESS					(0x0)
 						/**< Return value in case of success */
@@ -145,75 +145,26 @@ externC const EcdsaCrvInfo Ecdsa_P384;
 #define ELLIPTIC_GEN_SIGN_INCORRECT_HASH_LEN		(0x3)
 /** @} */
 
-
-//
-// Validate public key
-//
-// Returns: 0 if validated successfully, error otherwise
-// Input:	CrvInfo		- pointer to the curve data structure
-//			Key			- public key
-//
+/**
+ * @name IPCores API declarations
+ * @{
+ */
+/**< Prototype declarations for IPCores APIs for Elliptic curve related operations */
 externC s32 Ecdsa_ValidateKey(const EcdsaCrvInfo* CrvInfo, const EcdsaKey* Key);
 
-//
-// Verify the signature
-//
-// Returns: 0 if validated successfully, error otherwise
-// Input:	CrvInfo		- pointer to the curve data structure
-//			z		- pointer to hash of the signed data
-//			zlen		- length of the hash (bits)
-//			Key			- public key
-//			Sign		- signature
-//
 externC s32 Ecdsa_VerifySign(const EcdsaCrvInfo* CrvInfo, u8* z, u32 zlen,
 	const EcdsaKey* Key, const EcdsaSign* Sign);
 
-//
-// Generate the signature
-//
-// Returns: 0 if OK, error otherwise
-// Input:	CrvInfo		- pointer to the curve data structure
-//			z		- pointer to hash of the data to be signed
-//			zlen		- length of the hash (bits)
-//			D			- secret key
-//			K			- nonce
-// Output:  Sign		- signature
-//
 externC s32 Ecdsa_GenerateSign(const EcdsaCrvInfo* CrvInfo, const u8* z, u32 zlen,
 	const u8* D, const u8* K, const EcdsaSign* Sign);
 
-//
-// Generate the public key
-//
-// Returns: 0 if OK, error otherwise
-// Input:	CrvInfo		- pointer to the curve data structure
-//			D			- secret key
-// Output:  Key			- public key
-//
 externC s32 Ecdsa_GeneratePublicKey(const EcdsaCrvInfo* CrvInfo, const u8* D, const EcdsaKey* Key);
 
-//
-// SDK assert function
-//
 externC void sdk_assert(int cond);
 
-//
-// Performs mod operation result = a MOD ecc order of the curve
-//
-// Returns: 0 if OK, error otherwise
-// Input:	CrvInfo		- pointer to the curve data structure
-//		a		- input number
-//		alen		- length of input number
-//		out		- memory pointer where mod result is to be stored
-//		outlen		- size of out buffer
-// Output:  Key			- public key
-//
 externC s32 Ecdsa_ModEccOrder(const EcdsaCrvInfo* CrvInfo, const u8* In, u8* Out);
-//
-// Clearing the ECC RAMs
-//
-void Ecdsa_ClearEccRam(void);
 
-externC s32 XTrng_Generate(u32 RandBytes, u8 *RandBuf, u32 RandBufSize);
+void Ecdsa_ClearEccRam(void);
+/** @} */
 
 #endif
