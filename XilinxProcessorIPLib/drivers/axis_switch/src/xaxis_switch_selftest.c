e5ee626b2abf3969c8951820fcea48c36e201704
Anirudha Sarangi <anirudha.sarangi@xilinx.com>
2022-08-25 10:46:07 +0530

    axis_switch: Update copyright year and doxygen tags
    Update the copyright year to 2022 and doxygen tags to 1.5.
    
    Signed-off-by: Anirudha Sarangi <anirudha.sarangi@xilinx.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>

--- XilinxProcessorIPLib/drivers/axis_switch/src/xaxis_switch_selftest.c
+++ XilinxProcessorIPLib/drivers/axis_switch/src/xaxis_switch_selftest.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2015 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2015 - 2022 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +7,7 @@
 /**
 *
 * @file xaxis_switch_selftest.c
-* @addtogroup axis_switch_v1_4
+* @addtogroup axis_switch_v1_5
 * @{
 *
 * This file contains self test function for the AXI4-Stream Source Control
