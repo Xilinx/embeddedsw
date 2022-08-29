1d444024dc228ec6ef7d5f6be0fe03e7c7ccef60
Swati Agarwal <swati.agarwal@xilinx.com>
2022-08-29 17:16:23 +0530

    zdma: Revert zdma linear example infinite loop fix
    Replace timeout based Xil_WaitForEventSet with original while loop. This is a temporary
    workaround as the timeout fix causes intermittent failures in ZDMA linear example.
    
    Signed-off-by: Swati Agarwal <swati.agarwal@xilinx.com>
    Acked-by: Harini Katakam <harini.katakam@amd.com>

--- XilinxProcessorIPLib/drivers/zdma/examples/xzdma_linear_example.c
+++ XilinxProcessorIPLib/drivers/zdma/examples/xzdma_linear_example.c
@@ -43,7 +43,6 @@
 #include "xzdma.h"
 #include "xparameters.h"
 #include "xscugic.h"
-#include "xil_util.h"
 
 /************************** Function Prototypes ******************************/
 
@@ -70,8 +69,6 @@ static void ErrorHandler(void *CallBackRef, u32 Mask);
 
 #define DESCRIPTOR1_DATA_SIZE	1024 /**< Descriptor 1 data in bytes */
 #define DESCRIPTOR2_DATA_SIZE	64   /**< Descriptor 2 data in bytes */
-
-#define POLL_TIMEOUT_COUNTER    1000000U /* Wait for 1 sec */
 
 /**************************** Type Definitions *******************************/
 
@@ -266,11 +263,7 @@ int XZDma_LinearExample(u16 DeviceId)
 	/* Resuming the ZDMA core */
 	XZDma_Resume(&ZDma);
 
-	/* Wait till DMA done interrupt generated */
-	Status = Xil_WaitForEventSet(POLL_TIMEOUT_COUNTER, 1, (u32*)&Done);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
+	while (Done == 0); /* Wait till DMA done interrupt generated */
 
 	/* Disable relevant interrupts */
 	XZDma_DisableIntr(&ZDma, (XZDMA_IXR_DMA_DONE_MASK |
