f94c93b67eefed5cae0288eaaab85d14515373a5
Swati Agarwal <swati.agarwal@xilinx.com>
2022-09-06 14:12:43 +0530

    Revert "axicdma: Fix infinite loops in driver examples"
    This reverts commit 65d538bec048f8e665554127717f2e2ba10a725e.
    
    Reverting the fix due to intermittent failures in examples.
    
    Signed-off-by: Swati Agarwal <swati.agarwal@xilinx.com>
    
    Acked-for-series: Harini Katakam <harini.katakam@amd.com>

--- XilinxProcessorIPLib/drivers/axicdma/examples/xaxicdma_example_sg_intr.c
+++ XilinxProcessorIPLib/drivers/axicdma/examples/xaxicdma_example_sg_intr.c
@@ -63,7 +63,6 @@
 #include "xil_exception.h"
 #include "xil_cache.h"
 #include "xparameters.h"
-#include "xil_util.h"
 
 #ifdef XPAR_INTC_0_DEVICE_ID
 #include "xintc.h"
@@ -140,7 +139,7 @@ extern void xil_printf(const char *format, ...);
  */
 #define COALESCING_COUNT	5
 #define DELAY_COUNT		5
-#define POLL_TIMEOUT_COUNTER         1000000U /* Wait for 1 sec */
+
 /**************************** Type Definitions *******************************/
 
 
@@ -846,8 +845,12 @@ int XAxiCdma_SgIntrExample(XScuGic *IntcInstancePtr, XAxiCdma *InstancePtr,
 
 	/* Wait until the DMA transfer is done
 	 */
-	Status = Xil_WaitForEventSet(POLL_TIMEOUT_COUNTER, 1, (u32*)&Done);
-	if (Status != XST_SUCCESS) {
+
+	while ((Done < NUMBER_OF_BDS_TO_TRANSFER) && !Error) {
+		/* Wait */
+	}
+
+	if(Error) {
 		xdbg_printf(XDBG_DEBUG_ERROR, "Transfer has error %x\r\n",
 		    (unsigned int)XAxiCdma_GetError(InstancePtr));
 
--- XilinxProcessorIPLib/drivers/axicdma/examples/xaxicdma_example_simple_intr.c
+++ XilinxProcessorIPLib/drivers/axicdma/examples/xaxicdma_example_simple_intr.c
@@ -59,7 +59,6 @@
 #include "xil_exception.h"
 #include "xil_cache.h"
 #include "xparameters.h"
-#include "xil_util.h"
 
 #ifdef XPAR_INTC_0_DEVICE_ID
 #include "xintc.h"
@@ -101,7 +100,6 @@
 					 */
 
 #define NUMBER_OF_TRANSFERS	4	/* Number of simple transfers to do */
-#define POLL_TIMEOUT_COUNTER    1000000U /* Wait for 1 sec */
 
 /**************************** Type Definitions *******************************/
 
@@ -336,8 +334,11 @@ static int DoSimpleTransfer(XAxiCdma *InstancePtr, int Length, int Retries)
 
 	/* Wait until the DMA transfer is done
 	 */
-	Status = Xil_WaitForEventSet(POLL_TIMEOUT_COUNTER, 1, (u32*)&Done);
-	if (Status != XST_SUCCESS) {
+	while (!Done && !Error) {
+		/* Wait */
+	}
+
+	if (Error) {
 		xdbg_printf(XDBG_DEBUG_ERROR, "DMA transfer error\r\n");
 		return XST_FAILURE;
 	}
