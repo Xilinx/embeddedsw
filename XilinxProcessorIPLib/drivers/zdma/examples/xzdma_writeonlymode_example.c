e802545660a5ee4566e00bb2b167fc4d28969c51
Swati Agarwal <swati.agarwal@xilinx.com>
2022-09-06 14:12:46 +0530

    Revert "zdma: Fix infinite loops in driver examples"
    This reverts commit 426b65e087ecbe5cab833be2c813a09bd25b8632.
    
    Reverting the fix due to intermittent failures in examples.
    
    Signed-off-by: Swati Agarwal <swati.agarwal@xilinx.com>
    
    Acked-for-series: Harini Katakam <harini.katakam@amd.com>

--- XilinxProcessorIPLib/drivers/zdma/examples/xzdma_writeonlymode_example.c
+++ XilinxProcessorIPLib/drivers/zdma/examples/xzdma_writeonlymode_example.c
@@ -44,7 +44,6 @@
 #include "xzdma.h"
 #include "xparameters.h"
 #include "xscugic.h"
-#include "xil_util.h"
 
 /************************** Function Prototypes ******************************/
 
@@ -67,7 +66,6 @@ static void DoneHandler(void *CallBackRef);
 
 #define SIZE			1024 /* Size of the data to be written */
 
-#define POLL_TIMEOUT_COUNTER    1000000U /* Wait for 1 sec */
 /**************************** Type Definitions *******************************/
 
 
@@ -223,10 +221,7 @@ int XZDma_WriteOnlyExample(u16 DeviceId)
 	XZDma_Start(&ZDma, &Data, 1); /* Initiates the data transfer */
 
 	/* Wait till DMA destination done interrupt generated */
-	Status = Xil_WaitForEventSet(POLL_TIMEOUT_COUNTER, 1, (u32*)&Done);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
+	while (Done == 0);
 
 	XZDma_DisableIntr(&ZDma, XZDMA_IXR_DMA_DONE_MASK);
 
