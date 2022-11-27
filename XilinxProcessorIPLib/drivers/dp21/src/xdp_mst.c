e91d71bf860ade05e6aabb33b1d536cccb474045
Gugulothu, Rajesh <rajesh.gugulothu@amd.com>
2022-11-27 22:31:01 +0530

    dp21: Fix VIC payload Id table updation
    Writing payload data in to aux registers should be dependent on start
    timeslot aswell because the start timeslot is different to DP2.1 and DP1.4.
    
    Signed-off-by: Rajesh Gugulothu <rajesh.gugulothu@amd.com>
    Signed-off-by: Lakshmi Prasanna Eachuri <lakshmi.prasanna.eachuri@amd.com>
    
    Acked-for-series: Eachuri, Lakshmi Prasanna<lakshmi.prasanna.eachuri@amd.com>

--- XilinxProcessorIPLib/drivers/dp21/src/xdp_mst.c
+++ XilinxProcessorIPLib/drivers/dp21/src/xdp_mst.c
@@ -1510,8 +1510,10 @@ u32 XDp_TxAllocatePayloadVcIdTable(XDp *InstancePtr, u8 VcId, u8 Ts, u8 StartTs)
 	u32 Status;
 	u8 AuxData[3];
 	u8 Index;
+	u8 payloadIndex = 0;
 	u8 TimeoutCount = 0;
 	u8 PayloadCount = 0;
+	u8 vic_id = 0;
 	XDp_TxMainStreamAttributes *MsaConfig;
 
 	/* Verify arguments. */
@@ -1547,12 +1549,14 @@ u32 XDp_TxAllocatePayloadVcIdTable(XDp *InstancePtr, u8 VcId, u8 Ts, u8 StartTs)
 			PayloadCount++;
 		}
 	}
-	if (VcId != 0) {
-		for (int i = PayloadCount + 1; i < XDP_TX_NUM_PAYLOAD_TIMESLOTS; i++) {
-			int vicid = 0;
 
+	if (VcId != 0) {
+		for (payloadIndex =
+		     MsaConfig->StartTs + MsaConfig->TransferUnitSize;
+		     payloadIndex < XDP_TX_NUM_PAYLOAD_TIMESLOTS + 1;
+		     payloadIndex++) {
 			XDp_WriteReg(InstancePtr->Config.BaseAddr,
-				     (XDP_TX_VC_PAYLOAD_BUFFER_ADDR + (4 * i)), vicid);
+				(XDP_TX_VC_PAYLOAD_BUFFER_ADDR + (4 * payloadIndex)), vic_id);
 		}
 	}
 
