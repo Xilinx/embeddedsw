6c1e721498638c6882447b680c2d22f0ae3f7d02
praveen teja kundanala <praveen.teja.kundanala@xilinx.com>
2023-01-14 16:29:15 +0530

    sw_apps:versalnet:misc:Add macro VERSALNET_PLM
    Add macro VERSALNET_PLM in versionless flow
    
    Signed-off-by: Praveen Teja Kundanala <praveen.teja.kundanala@xilinx.com>
    
    Acked-for-series: Harsha <harsha.harsha@xilinx.com>=20

--- lib/sw_apps/versal_plm/misc/versal_net/xparameters.h
+++ lib/sw_apps/versal_plm/misc/versal_net/xparameters.h
@@ -1,5 +1,6 @@
 /******************************************************************************
 * Copyright (c) 2022 Xilinx, Inc.  All rights reserved.
+* Copyright (c) 2022 - 2023 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 #ifndef XPARAMETERS_H   /* prevent circular inclusions */
@@ -240,6 +241,7 @@
 
 /* Platform specific definitions */
 #define VERSAL_PLM
+#define VERSALNET_PLM
 #ifndef versal
 #define versal
 #endif
