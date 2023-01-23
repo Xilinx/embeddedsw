c6314342926108a1bc06ed4c44cf8ed7f054fc46
Amar Nath Baliyase <amarnath.baliyase@amd.com>
2023-01-23 12:44:23 +0530

    dma: update copyright for dma files
    Update copyright for the DMA driver files edited in this release:
    axicdma, axidma, axivdma, mcdma & zdma.
    
    Signed-off-by: Amar Nath Baliyase <amarnath.baliyase@amd.com>
    
    Acked-for-series: Harini Katakam <harini.katakam@amd.com>

--- XilinxProcessorIPLib/drivers/axidma/src/xaxidma_selftest.c
+++ XilinxProcessorIPLib/drivers/axidma/src/xaxidma_selftest.c
@@ -1,5 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2015 - 2022 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2022 - 2023 Advanced Micro Devices, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
--- XilinxProcessorIPLib/drivers/zdma/src/xzdma_g.c
+++ XilinxProcessorIPLib/drivers/zdma/src/xzdma_g.c
@@ -1,5 +1,6 @@
 /*******************************************************************
 * Copyright (C) 2010 - 2022 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2022 - 2023 Advanced Micro Devices, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 *******************************************************************/
 
