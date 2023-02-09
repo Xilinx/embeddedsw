77160d71591c3802b013468b1b5284276b08fed9
Srinivas Neeli <srinivas.neeli@amd.com>
2023-02-09 15:48:54 +0530

    axiethernet: Increment driver version, copyright and addtogroup version
    This commit does the below
    -> Increments the driver version number.
    -> Updates addtogroup tags.
    -> Updates copyright year.
    
    Signed-off-by: Srinivas Neeli <srinivas.neeli@amd.com>
    
    Acked-for-series: Harini Katakam <harini.katakam@amd.com>

--- XilinxProcessorIPLib/drivers/axiethernet/src/xaxiethernet_control.c
+++ XilinxProcessorIPLib/drivers/axiethernet/src/xaxiethernet_control.c
@@ -1,12 +1,13 @@
 /******************************************************************************
 * Copyright (C) 2010 - 2022 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2022 - 2023 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
 /**
 *
 * @file xaxiethernet_control.c
-* @addtogroup axiethernet_v5_14
+* @addtogroup axiethernet Overview
 * @{
 *
 * This file has driver APIs related to the controlling of the extended
