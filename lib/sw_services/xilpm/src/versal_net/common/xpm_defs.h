a824d6bdb018e4dbc0f6ae12ca707a86a98f3ae1
Ronak Jain <ronak.jain@xilinx.com>
2023-02-15 21:50:24 -0800

    xilpm: versal_common: server: bump up the version of versal EEMI APIs for feature check
    There are a some EEMI APIs in the versal which have some extra
    functionality as compared to the ZynqMP.
    
    So updated the version for such EEMI APIs so the user can differentiate
    the supported functionality by checking the appropriate version.
    
    list of EEMI APIs with updated version and it's extra functionality:
    
    - PM_SELF_SUSPEND - support for cpu idle feature
    - PM_FORCE_POWERDOWN - support for cpu idle feature
    - PM_REQUEST_NODE - support for security policy handling during request device
    - PM_RELEASE_NODE - support for security policy handling during release device
    
    Signed-off-by: Ronak Jain <ronak.jain@xilinx.com>
    Acked-by: Saeed Nowshadi <saeed.nowshadi@amd.com>

--- lib/sw_services/xilpm/src/versal_net/common/xpm_defs.h
+++ lib/sw_services/xilpm/src/versal_net/common/xpm_defs.h
@@ -1,5 +1,6 @@
 /******************************************************************************
 * Copyright (c) 2018 - 2022 Xilinx, Inc.  All rights reserved.
+* Copyright (c) 2022 - 2023, Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -601,11 +602,20 @@ enum XPmNotifyEvent {
 /**
  * PM API versions
  */
-#define XST_API_BASE_VERSION		(1U)
-#define XST_API_QUERY_DATA_VERSION	(2U)
-#define XST_API_REG_NOTIFIER_VERSION	(2U)
-#define XST_API_PM_IOCTL_VERSION	(2U)
+#define XST_API_BASE_VERSION			(1U)
+#define XST_API_QUERY_DATA_VERSION		(2U)
+#define XST_API_REG_NOTIFIER_VERSION		(2U)
+#define XST_API_PM_IOCTL_VERSION		(2U)
 #define XST_API_PM_FEATURE_CHECK_VERSION	(2U)
+/* Version 2 supports the CPU idling feature during force power down */
+#define XST_API_SELF_SUSPEND_VERSION		(2U)
+#define XST_API_FORCE_POWERDOWN_VERSION		(2U)
+/*
+ * Version 2 supports some extra security checks for REQ_ACCESS_SECURE
+ * and REQ_ACCESS_SECURE_NONSECURE policies.
+ */
+#define XST_API_REQUEST_NODE_VERSION		(2U)
+#define XST_API_RELEASE_NODE_VERSION		(2U)
 /** @} */
 
 /**
