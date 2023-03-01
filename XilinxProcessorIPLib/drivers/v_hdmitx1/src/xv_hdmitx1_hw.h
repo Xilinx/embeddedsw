81e082af86cce037413546f127ac5941ea8c885d
Siva Addepalli <sivaprasad.addepalli@xilinx.com>
2023-03-01 10:49:01 +0530

    Added AMD copyright under Xilinx copyright for all the modified files in this release
    Signed-off-by: Siva Addepalli <sivaprasad.addepalli@xilinx.com>

--- XilinxProcessorIPLib/drivers/v_hdmitx1/src/xv_hdmitx1_hw.h
+++ XilinxProcessorIPLib/drivers/v_hdmitx1/src/xv_hdmitx1_hw.h
@@ -1,5 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2018 – 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2022 - 2023 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 * Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 ******************************************************************************/
