88ba481c8f5828456d53175807c59a99684e2182
Buddhabhatti, Jay <jay.buddhabhatti@amd.com>
2023-03-28 13:26:07 +0530

    xilpm: versal_net: server: add power rail checks for LPD domain
    add power rail status check for PS-LPD domain.
    
    Signed-off-by: Jay Buddhabhatti <jay.buddhabhatti@amd.com>
    
    Acked-for-series: Trung Tran <trung.tran@amd.com>

--- lib/sw_services/xilpm/src/versal_net/server/xpm_pslpdomain.c
+++ lib/sw_services/xilpm/src/versal_net/server/xpm_pslpdomain.c
@@ -11,16 +11,31 @@
 #include "xpm_regs.h"
 #include "xpm_device.h"
 #include "xpm_debug.h"
+#include "xpm_rail.h"
 #include "xplmi.h"
 
 static XStatus LpdInitStart(XPm_PowerDomain *PwrDomain, const u32 *Args,
 		u32 NumOfArgs)
 {
 	XStatus Status = XST_FAILURE;
+	u16 DbgErr = XPM_INT_ERR_UNDEFINED;
+
 	(void)PwrDomain;
 	(void)Args;
 	(void)NumOfArgs;
-	Status = XST_SUCCESS;
+
+	const XPm_Rail *VccintPslpRail = (XPm_Rail *)XPmPower_GetById(PM_POWER_VCCINT_PSLP);
+
+	/* Check vccint_pslp first to make sure power is on */
+	Status = XPmPower_CheckPower(VccintPslpRail,
+						PMC_GLOBAL_PWR_SUPPLY_STATUS_VCCINT_LPD_MASK);
+	if (XST_SUCCESS != Status) {
+		DbgErr = XPM_INT_ERR_POWER_SUPPLY;
+		goto done;
+	}
+
+done:
+	XPm_PrintDbgErr(Status, DbgErr);
 	return Status;
 }
 static XStatus LpdInitFinish(const XPm_PowerDomain *PwrDomain, const u32 *Args,
