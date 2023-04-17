c2b4ad1bad7ec62448f5cfbafb16c178c5d96c7b
Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
2023-04-17 17:10:58 +0530

    lib: sw_apps: memory_tests: Add support for system device-tree flow
    Updated the application to support system device-tree flow
    1) Added a new metadata file (memory_tests.yaml) to describe application
    and supported processor and os etc.
    2) Added CMakeLists.txt to compile the application in the cmake flow
    3) Added cmake configure file (memory_config_g.c.in) to generate the
    memory_config_g.c file by consuming the lopper generated meta-data.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>

---
+++ lib/sw_apps/memory_tests/src/platform_config.h
@@ -0,0 +1,9 @@
+/******************************************************************************
+* Copyright (c) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+* SPDX-License-Identifier: MIT
+*******************************************************************************/
+
+#ifndef __PLATFORM_CONFIG_H_
+#define __PLATFORM_CONFIG_H_
+
+#endif
