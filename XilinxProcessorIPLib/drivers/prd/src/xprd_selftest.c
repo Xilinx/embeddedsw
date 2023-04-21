28768ef7d3f74c1160ef6de7299d3c22e0f761dc
Nava kishore Manne <nava.kishore.manne@amd.com>
2023-04-21 16:32:17 +0530

    prd: Increment driver version, copyright and addtogroup version
    This commit does the below
    -> Increments the driver version number.
    -> Updates addtogroup tags.
    -> Updates copyright year.
    
    Signed-off-by: Nava kishore Manne <nava.kishore.manne@amd.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>

--- XilinxProcessorIPLib/drivers/prd/src/xprd_selftest.c
+++ XilinxProcessorIPLib/drivers/prd/src/xprd_selftest.c
@@ -1,5 +1,6 @@
 /******************************************************************************
-* Copyright (C) 2016 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2016 - 2022 Xilinx, Inc.  All rights reserved.
+* Copyright (c) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +8,7 @@
 /**
 *
 * @file xprd_selftest.c
-* @addtogroup prd_v2_1
+* @addtogroup prd Overview
 * @{
 *
 * This file contains the self-test functions for the XPrd driver.
