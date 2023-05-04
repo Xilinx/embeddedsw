9aa9455a53eeb2801a0ff97c19b65b0eec62edc6
Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
2023-05-04 15:22:39 +0530

    csudma: examples: Add support for system device-tree flow
    1) Updated the examples LookupConfig() API to use baseaddress
    for system device-tree flow.
    2) Updated the interrupt example to use interrupt wrapper API
    for system device-tree flow.
    3) Added example meta-data in the csudma.yaml inorder to populate
    examples under import examples section in the vitis new gui when
    this driver is pulled.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
    
    Acked-for-series: Harini Katakam <harini.katakam@amd.com>

--- XilinxProcessorIPLib/drivers/csudma/examples/xcsudma_selftest_example.c
+++ XilinxProcessorIPLib/drivers/csudma/examples/xcsudma_selftest_example.c
@@ -22,6 +22,7 @@
 *       ms     04/10/17 Modified filename tag to include the file in doxygen
 *                       examples.
 * 1.2   adk    11/22/17 Added peripheral test app support.
+* 1.14  adk    05/04/23 Added support for system device-tree flow.
 * </pre>
 *
 ******************************************************************************/
@@ -38,7 +39,11 @@
  * xparameters.h file. They are defined here such that a user can easily
  * change all the needed parameters in one place.
  */
+#ifndef SDT
 #define CSUDMA_DEVICE_ID  XPAR_XCSUDMA_0_DEVICE_ID /* CSU DMA device Id */
+#else
+#define CSUDMA_BASEADDR	XPAR_XCSUDMA_0_BASEADDR /* CSU DMA base address */
+#endif
 
 /**************************** Type Definitions *******************************/
 
@@ -48,7 +53,11 @@
 /************************** Function Prototypes ******************************/
 
 
+#ifndef SDT
 int XCsuDma_SelfTestExample(u16 DeviceId);
+#else
+int XCsuDma_SelfTestExample(UINTPTR BaseAddress);
+#endif
 
 /************************** Variable Definitions *****************************/
 
@@ -72,7 +81,11 @@ int main(void)
 	int Status;
 
 	/* Run the selftest example */
+#ifndef SDT
 	Status = XCsuDma_SelfTestExample((u16)CSUDMA_DEVICE_ID);
+#else
+	Status = XCsuDma_SelfTestExample(CSUDMA_BASEADDR);
+#endif
 	if (Status != XST_SUCCESS) {
 		xil_printf("CSU_DMA Selftest Example Failed\r\n");
 		return XST_FAILURE;
@@ -99,7 +112,11 @@ int main(void)
 * @note		None.
 *
 ******************************************************************************/
+#ifndef SDT
 int XCsuDma_SelfTestExample(u16 DeviceId)
+#else
+int XCsuDma_SelfTestExample(UINTPTR BaseAddress)
+#endif
 {
 	int Status;
 	XCsuDma_Config *Config;
@@ -109,7 +126,11 @@ int XCsuDma_SelfTestExample(u16 DeviceId)
 	 * look up the configuration in the config table,
 	 * then initialize it.
 	 */
+#ifndef SDT
 	Config = XCsuDma_LookupConfig(DeviceId);
+#else
+	Config = XCsuDma_LookupConfig(BaseAddress);
+#endif
 	if (NULL == Config) {
 		return XST_FAILURE;
 	}
