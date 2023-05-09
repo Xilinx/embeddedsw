3eda3475e8cc480152029f93fe063e69829151c8
Anantha Umapathi <anantha.umapathi@xilinx.com>
2023-05-09 14:10:30 +0530

    sw_services: xilmailbox: Updated XIpiPs_PollforDone logic to improve AES client performance
    Updated XIpiPs_PollforDone logic to improve AES client performance
    
    Signed-off-by: Anantha Umapathi <anantha.umapathi@xilinx.com>
    Acked-by: Bharath Mulagondla <Bharath.mulagondla@xilinx.com>

--- lib/sw_services/xilmailbox/src/PS/xilmailbox_ipips.h
+++ lib/sw_services/xilmailbox/src/PS/xilmailbox_ipips.h
@@ -52,7 +52,8 @@ typedef struct {
 } XMailbox_Agent; /**< Xilmailbox agent */
 /************************** Constant Definitions *****************************/
 #define BIT(x)                 	(1 << (x)) /**< Bit position */
-#define XIPI_DONE_TIMEOUT_VAL	20000    /**< Timeout for IPI */
+#define XIPI_DONE_TIMEOUT_VAL	2000000    /**< Timeout for IPI */
+#define XIPI_IPI_DONE_BIT_SLEEP_IN_US	(1U)
 
 #ifdef versal
 #define XMAILBOX_IPIPSM		BIT(0)  /**< PSM channel */
