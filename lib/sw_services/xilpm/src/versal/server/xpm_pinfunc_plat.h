265fcc8dc0c9473c8dfe738bd9a8e542f6ca0132
Buddhabhatti, Jay <jay.buddhabhatti@amd.com>
2023-05-19 16:38:29 +0530

    xilpm: versal_net: server: Add support for OSPI pins
    Add below PMIO nodes and pin control functions which are required
    by OSPI driver:
     - PMIO 0-13
     - PMIO 26
    Pin functions:
     - PIN_FUNC_OSPI0
     - PIN_FUNC_OSPI0_SS
     - PIN_FUNC_OSPI0_RST_N
     - PIN_FUNC_OSPI0_ECC_FAIL
    
    Also move common pin related APIs in common directory for Versal and
    Versal NET and fix pin index validation for adding min index as invalid
    pin index in XPmPin_Init().
    
    Signed-off-by: Jay Buddhabhatti <jay.buddhabhatti@amd.com>
    
    Acked-for-series: Izhar Ameer Shaikh <izhar.ameer.shaikh@xilinx.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_pinfunc_plat.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_pinfunc_plat.h
@@ -1,25 +1,16 @@
 /******************************************************************************
 * Copyright (c) 2018 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2022 - 2023, Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
 
-#ifndef XPM_PINFUNC_H_
-#define XPM_PINFUNC_H_
-
-#include "xpm_device.h"
+#ifndef XPM_PINFUNC_PLAT_H_
+#define XPM_PINFUNC_PLAT_H_
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-#define FUNC_NAME_SIZE		(16U)		/* Function name string size */
-#define MAX_GROUPS_PER_RES	(6U)		/* Max No. of groups per query response */
-#define INVALID_FUNC_ID 	(0xFFFFU)
-#define END_OF_GRP		(0xFFU)		/* -1 */
-#define RESERVED_GRP		(0xFFFEU)	/* -2 */
-
-typedef struct XPm_PinFunc XPm_PinFunc;
 
 enum PmPinGrpIds {
 	PIN_GRP_SPI0_0,
@@ -557,31 +548,9 @@ enum PmPinGrpIds {
 	PIN_GRP_TEST_SCAN_0,
 	PIN_GRP_TAMPER_TRIGGER_0,
 };
-
-/**
- * The Pin Function class.
- */
-struct XPm_PinFunc {
-	char Name[FUNC_NAME_SIZE]; /**< Function name */
-	u8 Id; /**< Function ID */
-	u8 NumPins; /**< Number of pins needed for this function */
-	u8 NumGroups; /**< Number of groups for this function */
-	u16 DevIdx; /**< Device index for this function */
-	u16 LmioRegMask; /**< Register mask value to select this function */
-	u16 PmioRegMask; /**< Register mask value to select this function */
-	u16 *Groups; /**< Array of group identifier */
-};
-
-/************************** Function Prototypes ******************************/
-XPm_PinFunc *XPmPinFunc_GetById(u32 FuncId);
-XStatus XPmPinFunc_GetNumFuncs(u32 *NumFuncs);
-XStatus XPmPinFunc_GetFuncName(u32 FuncId, char *FuncName);
-XStatus XPmPinFunc_GetNumFuncGroups(u32 FuncId, u32 *NumGroups);
-XStatus XPmPinFunc_GetFuncGroups(u32 FuncId, u32 Index, u16 *Groups);
-
 #ifdef __cplusplus
 }
 #endif
 
 /** @} */
-#endif /* XPM_PINFUNC_H_ */
+#endif /* XPM_PINFUNC_PLAT_H_ */
