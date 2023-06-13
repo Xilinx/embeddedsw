cd9a1406ad6d8ef55092eab6ef7d8150bdc7f348
Nava kishore Manne <nava.kishore.manne@amd.com>
2023-06-13 17:42:48 +0530

    lib: sw_services: xilfpga: Add support for system device-tree flow
    Updated the library to support system device-tree flow
    1) Added meta-data file(xilfpga.yaml) which represents the h/w dependencies
       to describe library and supported processor and os etc.
    2) Added CMakeListst.txt files to compile the library in the cmake flow.
    
    Signed-off-by: Nava kishore Manne <nava.kishore.manne@amd.com>
    Acked-by: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>

---
+++ lib/sw_services/xilfpga/src/xilfpga.cmake
@@ -0,0 +1,39 @@
+# Copyright (C) 2023 Advanced Micro Devices, Inc.  All rights reserved.
+# SPDX-License-Identifier: MIT
+cmake_minimum_required(VERSION 3.3)
+
+option(XILFPGA_secure_mode "Enable secure Bitstream loading support" ON)
+option(XILFPGA_secure_environment "Which is used to Enable the secure PL configuration" OFF)
+option(XILFPGA_secure_readback "Which is used to Enable the secure PL configuration Read-back support" OFF)
+option(XILFPGA_debug_mode "Which is used to Enable the Debug messages in the library" OFF)
+SET(XILFPGA_ocm_address 0xfffc0000 CACHE STRING "OCM Address which is used for Bitstream Authentication")
+SET(XILFPGA_base_address 0x80000 CACHE STRING "Bitstream Image Base Address")
+
+set(zynqmp_secure_env 0)
+if("${CMAKE_MACHINE}" STREQUAL "Versal")
+    set(VERSAL_INCLUDE "#include <xilfpga_versal.h>")
+elseif("${CMAKE_MACHINE}" STREQUAL "ZynqMP")
+    set(PLATFORM_ZYNQMP " ")
+    if((NOT("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "pmu_microblaze")) AND
+       ${XILFPGA_secure_environment})
+        set(XFPGA_SECURE_IPI_MODE_EN " ")
+        set(zynqmp_secure_env 1)
+    else()
+        set(PCAP_INCLUDE "#include <xilfpga_pcap.h>")
+    endif()
+    set(PCAP_COMMON_INCLUDE "#include <xilfpga_pcap_common.h>")
+endif()
+
+set(XFPGA_OCM_ADDRESS ${XILFPGA_ocm_address})
+set(XFPGA_BASE_ADDRESS ${XILFPGA_base_address})
+if (${XILFPGA_secure_mode})
+	set(XFPGA_SECURE_MODE " ")
+endif()
+if (${XILFPGA_debug_mode})
+	set(XFPGA_DEBUG " ")
+endif()
+if (${XILFPGA_secure_readback})
+    set(XFPGA_SECURE_READBACK_MODE " ")
+endif()
+
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/xfpga_config.h.in ${CMAKE_BINARY_DIR}/include/xfpga_config.h)
