024bec3bf69b1e5f769029168e1f7cf0caf7de1b
Onkar Harsh <onkar.harsh@amd.com>
2023-06-22 15:39:45 +0530

    lib: bsp: Support for zynq specific BSP in System device-tree flow
    Adding below new files in the bsp directory to support bsp build for zynq
    1. cortexa9.yaml
    2. CMakeLists.txt
    3. cortexa9 specific header files
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
    Signed-off-by: Onkar Harsh <onkar.harsh@amd.com>
    
    Acked-for-series: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>

---
+++ lib/bsp/standalone/src/arm/cortexa9/xcortexa9.h
@@ -0,0 +1,18 @@
+/******************************************************************************
+* Copyright (c) 2021-2022 Xilinx, Inc.  All rights reserved.
+* Copyright (c) 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+#ifndef XIL_XCORTEXA9_H_ /* prevent circular inclusions */
+#define XIL_XCORTEXA9_H_ /* by using protection macros */
+
+/***************************** Include Files ********************************/
+
+#include "xil_types.h"
+
+/**************************** Type Definitions ******************************/
+typedef struct {
+		u32 CpuFreq;
+} XCortexa9_Config;
+
+#endif /* XIL_XCORTEXA9_H_ */
