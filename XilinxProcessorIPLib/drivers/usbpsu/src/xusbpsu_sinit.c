1b940ddf63183a33fc45111e7871456e0af97d45
Piyush Mehta <piyush.mehta@amd.com>
2023-06-22 12:21:26 +0530

    usb: usbpsu: src: update the driver to support SDT flow
    This patch does following,
    - Added new parameters interrupts and interrupt_parent
      to the config structure.
    - Updated the config pointer checking with 'Name' for
      hibernation feature.
    
    Signed-off-by: Piyush Mehta <piyush.mehta@amd.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>

--- XilinxProcessorIPLib/drivers/usbpsu/src/xusbpsu_sinit.c
+++ XilinxProcessorIPLib/drivers/usbpsu/src/xusbpsu_sinit.c
@@ -20,6 +20,7 @@
 * 1.0   sg   06/06/16 First release
 * 1.9   pm   03/15/21 Fixed doxygen warnings
 * 1.12   pm   08/10/22 Update doxygen tag and addtogroup version
+* 1.14	pm    21/06/23 Added support for system device-tree flow.
 *
 * </pre>
 *
@@ -27,8 +28,10 @@
 
 /***************************** Include Files *********************************/
 
-#include "xparameters.h"
 #include "xusbpsu.h"
+#ifndef SDT
+#include "xparameters.h"
+#endif
 
 /************************** Constant Definitions *****************************/
 
@@ -60,6 +63,7 @@
 * @note none
 *
 ******************************************************************************/
+#ifndef SDT
 XUsbPsu_Config *XUsbPsu_LookupConfig(u16 DeviceId)
 {
 	XUsbPsu_Config *CfgPtr = NULL;
@@ -74,4 +78,21 @@ XUsbPsu_Config *XUsbPsu_LookupConfig(u16 DeviceId)
 
 	return (XUsbPsu_Config *)(CfgPtr);
 }
+#else
+XUsbPsu_Config *XUsbPsu_LookupConfig(u32 BaseAddress)
+{
+	XUsbPsu_Config *CfgPtr = NULL;
+	u32 i;
+
+	for (i = 0U; XUsbPsu_ConfigTable[i].Name != NULL; i++) {
+		if ((XUsbPsu_ConfigTable[i].BaseAddress == BaseAddress) ||
+		    !BaseAddress) {
+			CfgPtr = &XUsbPsu_ConfigTable[i];
+			break;
+		}
+	}
+
+	return (XUsbPsu_Config *)(CfgPtr);
+}
+#endif
 /** @} */
