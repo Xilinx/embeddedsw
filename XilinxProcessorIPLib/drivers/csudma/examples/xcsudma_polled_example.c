72d974061b47c36d6bfb3a6201599d1933fa0c00
Abin Joseph <abin.joseph@amd.com>
2023-06-27 11:03:04 +0530

    csudma: Fixed code formatting issue using checkpatch tool
    Fix code formatting issues with checkpatch tool.
    It fixes the indentation, formats the code as per coding standards.
    
    Signed-off-by: Abin Joseph <abin.joseph@amd.com>
    
    Acked-for-series: Harini Katakam<harini.katakam@amd.com>

--- XilinxProcessorIPLib/drivers/csudma/examples/xcsudma_polled_example.c
+++ XilinxProcessorIPLib/drivers/csudma/examples/xcsudma_polled_example.c
@@ -185,13 +185,11 @@ int XCsuDma_PolledExample(UINTPTR BaseAddress)
 			  ((Xil_In32(XCSU_BASEADDRESS + CSU_SSS_CONFIG_OFFSET) & 0xF0000) |
 			   CSUDMA_LOOPBACK_CFG));
 #if defined (versal)
-	}
-	else if (Config->DmaType == XCSUDMA_DMATYPEIS_PMCDMA0) {
+	} else if (Config->DmaType == XCSUDMA_DMATYPEIS_PMCDMA0) {
 		Xil_Out32(XPS_PMC_GLOBAL_BASEADDRESS + PMC_SSS_CONFIG_OFFSET,
 			  ((Xil_In32(XPS_PMC_GLOBAL_BASEADDRESS + PMC_SSS_CONFIG_OFFSET) & 0xFF000000) |
 			   PMCDMA0_LOOPBACK_CFG));
-	}
-	else {
+	} else {
 		Xil_Out32(XPS_PMC_GLOBAL_BASEADDRESS + PMC_SSS_CONFIG_OFFSET,
 			  ((Xil_In32(XPS_PMC_GLOBAL_BASEADDRESS + PMC_SSS_CONFIG_OFFSET) & 0xFF000000) |
 			   PMCDMA1_LOOPBACK_CFG));
@@ -229,7 +227,7 @@ int XCsuDma_PolledExample(UINTPTR BaseAddress)
 	 * Perform cache operations on ARM64 and R5
 	 */
 #if defined(ARMR52)
-        Xil_DCacheInvalidateRange((INTPTR)DstPtr, SIZE * 4);
+	Xil_DCacheInvalidateRange((INTPTR)DstPtr, SIZE * 4);
 #elif defined(ARMR5)
 	Xil_DCacheFlushRange((INTPTR)DstPtr, SIZE * 4);
 #endif
@@ -240,8 +238,7 @@ int XCsuDma_PolledExample(UINTPTR BaseAddress)
 	for (Index = 0; Index < SIZE; Index++) {
 		if (*SrcPtr != *DstPtr) {
 			return XST_FAILURE;
-		}
-		else {
+		} else {
 			SrcPtr++;
 			DstPtr++;
 		}
