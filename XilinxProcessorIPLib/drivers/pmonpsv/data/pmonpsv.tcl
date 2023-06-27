8dcd97877ed3b2240bafdf0b33e0045eb392639d
Harini T <harini.t@amd.com>
2023-06-27 10:50:41 +0530

    pmonpsv: Driver is going to be deprecated as IP feature is not part of Versal TRM
    Add deprecation message in pmonpsv.tcl because the driver is going to be
    deprecated as IP feature is not part of Versal TRM.
    
    Signed-off-by: Harini T <harini.t@amd.com>
    Acked-by: Srinivas Goud <srinivas.goud@amd.com>

--- XilinxProcessorIPLib/drivers/pmonpsv/data/pmonpsv.tcl
+++ XilinxProcessorIPLib/drivers/pmonpsv/data/pmonpsv.tcl
@@ -1,5 +1,6 @@
 ###############################################################################
 # Copyright (C) 2019 - 2020 Xilinx, Inc.  All rights reserved.
+# Copyright (C) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
 # SPDX-License-Identifier: MIT
 #
 #
@@ -8,11 +9,13 @@
 # -------- ------ -------- ----------------------------------------------------
 # 1.0     sd    01/20/19   Initial release
 # 2.0     sd    04/22/20   Rename the APIs
+# 2.3     ht    06/27/23   Added message regarding deprecation of Pmonpsv
 ###############################################################################
 
 #uses "xillib.tcl"
 
 proc generate {drv_handle} {
+	puts "WARNING: Pmonpsv driver is being deprecated from 2024.1 release. It will be made obsolete in 2025.1 release."
     ::hsi::utils::define_include_file $drv_handle "xparameters.h" "XPmonPsv" "NUM_INSTANCES" "DEVICE_ID" "C_S_AXI_BASEADDR"
     ::hsi::utils::define_config_file $drv_handle "xpmonpsv_g.c" "XPmonPsv" "DEVICE_ID" "C_S_AXI_BASEADDR"
 
