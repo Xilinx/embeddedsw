7ac0a79daabfbec012534043636fc46b0f2274cc
Bhavana Jupalli <Bhavana.Jupalli@amd.com>
2023-06-27 10:33:50 +0530

    xdmapcie: Update copyright year
    Updated Xilinx copyright year and added AMD copyright information.
    
    Signed-off-by: Bhavana Jupalli <Bhavana.Jupalli@amd.com>
    
    Acked-for-series: Bharat Kumar Gogada <bharat.kumar.gogada@amd.com>

--- XilinxProcessorIPLib/drivers/xdmapcie/src/xdmapcie_g.c
+++ XilinxProcessorIPLib/drivers/xdmapcie/src/xdmapcie_g.c
@@ -1,5 +1,6 @@
 /******************************************************************************
-* Copyright (C) 2010 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2019 - 2022 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2022 - 2023 Advanced Micro Devices, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
