abdcdf5bd23e2ad2a3393267abb1e54ac085628f
Gutti, Prasad <prasad.gutti@amd.com>
2023-06-30 17:59:41 +0530

    FB_Wr Driver: Updated addtogroup in driver src directory
    Updated addtogroup from v4_6 to v4_7 in FBW driver src directory
    
    Signed-off-by: Prasad Gutti <prasad.gutti@amd.com>
    
    Acked-for-series: Gaddipati, Naveen <naveen.gaddipati@amd.com>

--- XilinxProcessorIPLib/drivers/v_frmbuf_wr/src/xv_frmbufwr_l2.c
+++ XilinxProcessorIPLib/drivers/v_frmbuf_wr/src/xv_frmbufwr_l2.c
@@ -8,7 +8,7 @@
 /**
 *
 * @file xv_frmbufwr_l2.c
-* @addtogroup v_frmbuf_wr_v4_6
+* @addtogroup v_frmbuf_wr_v4_7
 * @{
 *
 * Frame Buffer Write Layer-2 Driver. The functions in this file provides an
