61a63d990493da31053a119a17809868f9ebe902
Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
2023-07-04 09:04:31 +0530

    XilinxProcessorIPLib: drivers: tmr_manager: Add support for system device-tree flow
    Updated the driver to support system device-tree flow
    1) Added a new meta-data file (tmr_manager. yaml) to pull the driver and
    generate the required config data (xtmr_manager_g.c and xparameters. h defines)
    2) Updated LookConfig API to use base address instead of device id.
    3) Updated Config Structure to inline with meta-data file (tmr_manager.yaml
    required section) for the system device-tree flow.
    4) Added CMakeLists.txt to compile the driver source in cmake flow.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>

--- XilinxProcessorIPLib/drivers/tmr_manager/src/xtmr_manager_recover.c
+++ XilinxProcessorIPLib/drivers/tmr_manager/src/xtmr_manager_recover.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2017 - 2022 Xilinx, Inc.  All rights reserved.
-* Copyright (C) 2022 Advanced Micro Devices, Inc. All Rights Reserved.
+* Copyright (C) 2022 - 2023 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -33,7 +33,9 @@
 #include "xtmr_manager.h"
 #include "xtmr_manager_l.h"
 #include "xil_io.h"
+#ifndef SDT
 #include "xparameters.h"
+#endif
 
 /************************** Constant Definitions ****************************/
 
@@ -75,8 +77,13 @@ void XTMR_Manager_BreakHandler (XTMR_Manager *InstancePtr)
 	Xil_AssertVoid(InstancePtr != NULL);
 	Xil_AssertVoid(InstancePtr->IsReady == XIL_COMPONENT_IS_READY);
 	Xil_AssertVoid((InstancePtr->Cr & XTM_CR_RIR_MASK) != 0);
+#ifndef SDT
 	Xil_AssertVoid((InstancePtr->Cr & XTM_CR_MAGIC1_MASK) ==
 		       XPAR_TMR_MANAGER_0_MAGIC1);
+#else
+	Xil_AssertVoid((InstancePtr->Cr & XTM_CR_MAGIC1_MASK) ==
+		       InstancePtr->Config.Magic1);
+#endif
 
 	/* Call user defined pre-recovery handler, if any */
 	if (InstancePtr->PreResetHandler != NULL)
