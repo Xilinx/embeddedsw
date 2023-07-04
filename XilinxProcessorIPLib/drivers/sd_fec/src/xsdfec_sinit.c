a6498a2a8639cd3914115856339edf4b2f42de8a
O'Griofa, Conall <conall.ogriofa@amd.com>
2023-07-04 22:17:39 +0100

    sd_fec: Modifications for SDT flow
    This patch contains the modifications needed for the SDT flow
    for the SD_FEC driver
    
    Signed-off-by: Conall O'Griofa <conall.ogriofa@amd.com>
    Acked-by: Kadamathikuttiyil Karthikeyan Pillai, Anish <anish.kadamathikuttiyil-karthikeyan-pillai@amd.com>

--- XilinxProcessorIPLib/drivers/sd_fec/src/xsdfec_sinit.c
+++ XilinxProcessorIPLib/drivers/sd_fec/src/xsdfec_sinit.c
@@ -1,15 +1,19 @@
 /******************************************************************************
+* Copyright (C) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
 * Copyright (C) 2016 - 2020 Xilinx, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
 
 #include "xstatus.h"
+#ifndef SDT
 #include "xparameters.h"
+#endif
 #include "xsdfec.h"
 
 extern XSdFec_Config XSdFec_ConfigTable[];
 
+#ifndef SDT
 XSdFec_Config *XSdFecLookupConfig(u16 DeviceId) {
 	XSdFec_Config *ConfigPtr = NULL;
 
@@ -24,6 +28,24 @@ XSdFec_Config *XSdFecLookupConfig(u16 DeviceId) {
 
 	return ConfigPtr;
 }
+#else
+XSdFec_Config *XSdFec_LookupConfig(UINTPTR BaseAddress)
+{
+       XSdFec_Config *CfgPtr = NULL;
+       u32 Index;
+
+       /* Checks all the instances */
+       for (Index = (u32)0x0; XSdFec_ConfigTable[Index].Name != NULL; Index++) {
+               if ((XSdFec_ConfigTable[Index].BaseAddress == BaseAddress) ||
+                    !BaseAddress) {
+                       CfgPtr = &XSdFec_ConfigTable[Index];
+                       break;
+               }
+       }
+
+       return CfgPtr;
+}
+#endif
 
 int XSdFecInitialize(XSdFec *InstancePtr, u16 DeviceId) {
 	XSdFec_Config *ConfigPtr;
