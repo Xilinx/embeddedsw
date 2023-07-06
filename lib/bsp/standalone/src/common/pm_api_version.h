86b5da028323ef1f3839fea580b1d22ea1b4688e
Jain, Ronak <ronak.jain@amd.com>
2023-07-05 23:28:08 -1200

    lib: bsp: standalone: Fix grammatical issues
    There are some grammatical issues in the EEMI API version description
    hence fix these mistakes.
    
    Signed-off-by: Ronak Jain <ronak.jain@amd.com>
    
    Acked-for-series: Naman Trivedi Manojbhai <naman.trivedimanojbhai@amd.com>

--- lib/bsp/standalone/src/common/pm_api_version.h
+++ lib/bsp/standalone/src/common/pm_api_version.h
@@ -26,72 +26,72 @@
  * | PM_SET_CONFIGURATION	| 0x2	| ZynqMP	| 1	 | The API is used to configure the power management framework		     |
  * | ^				| ^	| ^		| ^	 | Note: Deprecated in Versal but supported in ZynqMP			     |
  * | PM_GET_NODE_STATUS		| 0x3	| Both		| 1	 | The API is used to obtain information about current status of a device    |
- * | PM_GET_OP_CHARACTERISTIC	| 0x4	| Both		| 2	 | V1 - The API is used to gets operating characteristics of a device	     |
- * | ^				| ^	| ^		| ^	 | V2 - Added support of bitmask functionality, user can check the supported\n									"type" first before performing the actual functionality			    |
- * | PM_REGISTER_NOTIFIER	| 0x5	| Both		| 2	 | V1 - The API is used to register a subsystem to be notified about the\n									device event								    |
+ * | PM_GET_OP_CHARACTERISTIC	| 0x4	| Both		| 2	 | V1 - The API is used to get operating characteristics of a device	     |
+ * | ^				| ^	| ^		| ^	 | V2 - Added support of bitmask functionality, user can check the supported\n									"type" first before performing the actual functionality				       |
+ * | PM_REGISTER_NOTIFIER	| 0x5	| Both		| 2	 | V1 - The API is used to register a subsystem to be notified about the\n									device event									       |
  * | ^				| ^	| ^		| ^	 | V2 - Added support of event management functionality			     |
- * | ^				| ^	| ^		| ^	 | Note: V2 is supported in Versal but for ZynqMP it supports only V1	     |
+ * | ^				| ^	| ^		| ^	 | Note: V2 is supported in Versal but ZynqMP supports only V1		     |
  * | PM_REQUEST_SUSPEND		| 0x6	| Both		| 1	 | The API is used to send suspend request to another subsystem		     |
  * | PM_SELF_SUSPEND		| 0x7	| Both		| 2	 | V1 - The API is used to suspend a child subsystem			     |
  * | ^				| ^	| ^		| ^	 | V2 - Added support of cpu idle functionality during force powerdown	     |
- * | ^				| ^	| ^		| ^	 | Note: V2 is supported in Versal but for ZynqMP it supports only V1	     |
+ * | ^				| ^	| ^		| ^	 | Note: V2 is supported in Versal but ZynqMP supports only V1		     |
  * | PM_FORCE_POWERDOWN		| 0x8	| Both		| 2	 | V1 - The API is used to Powerdown other processor or node		     |
  * | ^				| ^	| ^		| ^	 | V2 - Added support of cpu idle functionality	during force powerdown	     |
- * | ^				| ^	| ^		| ^	 | Note: V2 is supported in Versal but for ZynqMP it supports only V1	     |
- * | PM_ABORT_SUSPEND		| 0x9	| Both		| 1	 | The API is used to aborting suspend of a child subsystem		     |
+ * | ^				| ^	| ^		| ^	 | Note: V2 is supported in Versal but ZynqMP supports only V1		     |
+ * | PM_ABORT_SUSPEND		| 0x9	| Both		| 1	 | The API is used by a subsystem to abort suspend of a child subsystem	     |
  * | PM_REQUEST_WAKEUP		| 0xA	| Both		| 1	 | The API is used to start-up and wake-up a child subsystem		     |
  * | PM_SET_WAKEUP_SOURCE	| 0xB	| Both		| 1	 | The API is used to set wakeup source					     |
  * | PM_SYSTEM_SHUTDOWN		| 0xC	| Both		| 1	 | The API is used to shutdown or restart the system			     |
  * | PM_REQUEST_NODE		| 0xD	| Both		| 2	 | V1 - The API is used to request the usage of a device		     |
  * | ^				| ^	| ^		| ^	 | V2 - Added support of security policy handling during request device	     |
- * | ^				| ^	| ^		| ^	 | Note: V2 is supported in Versal but for ZynqMP it supports only V1	     |
+ * | ^				| ^	| ^		| ^	 | Note: V2 is supported in Versal but ZynqMP supports only V1		     |
  * | PM_RELEASE_NODE		| 0xE	| Both		| 2	 | V1 - The API is used to release the usage of a device		     |
  * | ^				| ^	| ^		| ^	 | V2 - Added support of security policy handling during request device      |
- * | ^				| ^	| ^		| ^	 | Note: V2 is supported in Versal but for ZynqMP it supports only V1	     |
- * | PM_SET_REQUIREMENT		| 0xF	| Both		| 1	 | The API is used to announce a change in requirement for a specific slave\n									node which is currently in use						    |
+ * | ^				| ^	| ^		| ^	 | Note: V2 is supported in Versal but ZynqMP supports only V1		     |
+ * | PM_SET_REQUIREMENT		| 0xF	| Both		| 1	 | The API is used to announce a change in requirement for a specific slave\n									node which is currently in use							       |
  * | PM_SET_MAX_LATENCY		| 0x10	| Both		| 1	 | The API is used to set maximum allowed latency for the device	     |
  * | PM_RESET_ASSERT		| 0x11	| Both		| 1	 | The API is used to reset or de-reset a device			     |
- * | PM_RESET_GET_STATUS	| 0x12	| Both		| 1	 | The API is used to reads the device reset state			     |
- * | PM_MMIO_WRITE		| 0x13	| ZynqMP	| 1	 | The API is used to writes a value into a register			     |
+ * | PM_RESET_GET_STATUS	| 0x12	| Both		| 1	 | The API is used to read the device reset state			     |
+ * | PM_MMIO_WRITE		| 0x13	| ZynqMP	| 1	 | The API is used to write a value into a register			     |
  * | ^				| ^	| ^		| ^	 | Note: Deprecated in Versal but supported in ZynqMP			     |
- * | PM_MMIO_READ		| 0x14	| ZynqMP	| 1	 | The API is used to reads a value from a register			     |
+ * | PM_MMIO_READ		| 0x14	| ZynqMP	| 1	 | The API is used to read a value from a register			     |
  * | ^				| ^	| ^		| ^	 | Note: Deprecated in Versal but supported in ZynqMP			     |
- * | PM_INIT_FINALIZE		| 0x15	| Both		| 1	 | The API is used to initializes subsystem and releases unused	devices	     |
+ * | PM_INIT_FINALIZE		| 0x15	| Both		| 1	 | The API is used to initialize subsystem and release unused devices	     |
  * | PM_GET_CHIPID		| 0x18	| Both		| 1	 | The API is used to request the version and ID code of a chip		     |
- * | PM_PINCTRL_REQUEST		| 0x1C	| Both		| 1	 | The API is used to requests the pin					     |
- * | PM_PINCTRL_RELEASE		| 0x1D	| Both		| 1	 | The API is used to releases the pin					     |
- * | PM_PINCTRL_GET_FUNCTION	| 0x1E	| Both		| 1	 | The API is used to reads the pin function				     |
- * | PM_PINCTRL_SET_FUNCTION	| 0x1F	| Both		| 1	 | The API is used to sets the pin function				     |
- * | PM_PINCTRL_CONFIG_PARAM_GET| 0x20	| Both		| 1	 | The API is used to reads the pin parameter value			     |
+ * | PM_PINCTRL_REQUEST		| 0x1C	| Both		| 1	 | The API is used to request the pin					     |
+ * | PM_PINCTRL_RELEASE		| 0x1D	| Both		| 1	 | The API is used to release the pin					     |
+ * | PM_PINCTRL_GET_FUNCTION	| 0x1E	| Both		| 1	 | The API is used to read the pin function				     |
+ * | PM_PINCTRL_SET_FUNCTION	| 0x1F	| Both		| 1	 | The API is used to set the pin function				     |
+ * | PM_PINCTRL_CONFIG_PARAM_GET| 0x20	| Both		| 1	 | The API is used to read the pin parameter value			     |
  * | PM_PINCTRL_CONFIG_PARAM_SET| 0x21	| Both		| 2	 | V1 - The API is used to set the pin parameter value			     |
  * | ^				| ^	| ^		| ^	 | V2 - Added support of MIO tri-state controlling functionality	     |
  * | ^				| ^	| ^		| ^	 | Note: V2 is supported in ZynqMP but Versal supports only V1		     |
- * | PM_IOCTL			| 0x22	| Both		| 3	 | V1 - The API is used to performs driver-like IOCTL functions on shared\n									system devices								    |
- * | ^				| ^	| ^		| ^	 | V2 - Added support of bitmask functionality, user can check the supported\n									ID first before performing the actual functionality			    |
+ * | PM_IOCTL			| 0x22	| Both		| 3	 | V1 - The API is used to perform driver-like IOCTL functions on shared\n									system devices									       |
+ * | ^				| ^	| ^		| ^	 | V2 - Added support of bitmask functionality, user can check the supported\n									ID first before performing the actual functionality				       |
  * | ^				| ^	| ^		| ^	 | V3 - Add support of zeroization of AIE data and program memory separately |
- * | ^				| ^	| ^		| ^	 | Note: V3 is supported in Versal but ZynqMP supports only V2	    |
- * | PM_QUERY_DATA		| 0x23	| Both		| 2	 | V1 - The API is used to queries information about the platform resources  |
- * | ^				| ^	| ^		| ^	 | V2 - Added support of bitmask functionality, user can check the supported\n									ID first before performing the actual functionality			    |
- * | PM_CLOCK_ENABLE		| 0x24	| Both		| 1	 | The API is used to enables the clock					     |
- * | PM_CLOCK_DISABLE		| 0x25	| Both		| 1	 | The API is used to disables the clock				     |
- * | PM_CLOCK_GETSTATE		| 0x26	| Both		| 1	 | The API is used to reads the clock state				     |
- * | PM_CLOCK_SETDIVIDER	| 0x27	| Both		| 1	 | The API is used to sets the divider value of the clock		     |
- * | PM_CLOCK_GETDIVIDER	| 0x28	| Both		| 1	 | The API is used to reads the clock divider				     |
- * | PM_CLOCK_SETRATE		| 0x29	| Versal	| 1	 | The API is used to sets the rate of the clock			     |
+ * | ^				| ^	| ^		| ^	 | Note: V3 is supported in Versal but ZynqMP supports only V2	             |
+ * | PM_QUERY_DATA		| 0x23	| Both		| 2	 | V1 - The API is used to query information about the platform resources  |
+ * | ^				| ^	| ^		| ^	 | V2 - Added support of bitmask functionality, user can check the supported\n									ID first before performing the actual functionality				       |
+ * | PM_CLOCK_ENABLE		| 0x24	| Both		| 1	 | The API is used to enable the clock					     |
+ * | PM_CLOCK_DISABLE		| 0x25	| Both		| 1	 | The API is used to disable the clock					     |
+ * | PM_CLOCK_GETSTATE		| 0x26	| Both		| 1	 | The API is used to read the clock state				     |
+ * | PM_CLOCK_SETDIVIDER	| 0x27	| Both		| 1	 | The API is used to set the divider value of the clock		     |
+ * | PM_CLOCK_GETDIVIDER	| 0x28	| Both		| 1	 | The API is used to read the clock divider				     |
+ * | PM_CLOCK_SETRATE		| 0x29	| Versal	| 1	 | The API is used to set the rate of the clock				     |
  * | ^				| ^	| ^		| ^	 | Note: Not supported in ZynqMP but supported in Versal		     |
- * | PM_CLOCK_GETRATE		| 0x2A	| Versal	| 1	 | The API is used to gets the rate of the clock			     |
+ * | PM_CLOCK_GETRATE		| 0x2A	| Versal	| 1	 | The API is used to get the rate of the clock				     |
  * | ^				| ^	| ^		| ^	 | Note: Not supported in ZynqMP but supported in Versal		     |
- * | PM_CLOCK_SETPARENT		| 0x2B	| Both		| 1	 | The API is used to sets the parent of the clock			     |
- * | PM_CLOCK_GETPARENT		| 0x2C	| Both		| 1	 | The API is used to reads the clock parent				     |
- * | PM_PLL_SET_PARAM		| 0x30	| Both		| 1	 | The API is used to sets the parameter of PLL clock			     |
- * | PM_PLL_GET_PARAM		| 0x31	| Both		| 1	 | The API is used to reads the parameter of PLL clock			     |
- * | PM_PLL_SET_MODE		| 0x32	| Both		| 1	 | The API is used to sets the mode of PLL clock			     |
- * | PM_PLL_GET_MODE		| 0x33	| Both		| 1	 | The API is used to reads the mode of PLL clock			     |
+ * | PM_CLOCK_SETPARENT		| 0x2B	| Both		| 1	 | The API is used to set the parent of the clock			     |
+ * | PM_CLOCK_GETPARENT		| 0x2C	| Both		| 1	 | The API is used to read the clock parent				     |
+ * | PM_PLL_SET_PARAM		| 0x30	| Both		| 1	 | The API is used to set the parameter of PLL clock			     |
+ * | PM_PLL_GET_PARAM		| 0x31	| Both		| 1	 | The API is used to read the parameter of PLL clock			     |
+ * | PM_PLL_SET_MODE		| 0x32	| Both		| 1	 | The API is used to set the mode of PLL clock				     |
+ * | PM_PLL_GET_MODE		| 0x33	| Both		| 1	 | The API is used to read the mode of PLL clock			     |
  * | PM_REGISTER_ACCESS		| 0x34	| ZynqMP	| 1	 | The API is used for register read/write access data			     |
  * | ^				| ^	| ^		| ^	 | Note: Deprecated in Versal but supported in ZynqMP			     |
- * | PM_EFUSE_ACCESS		| 0x35	| ZynqMP	| 1	 | The API is used to provides access to efuse memory			     |
+ * | PM_EFUSE_ACCESS		| 0x35	| ZynqMP	| 1	 | The API is used to provide access to efuse memory			     |
  * | ^				| ^	| ^		| ^	 | Note: Deprecated in Versal but supported in ZynqMP			     |
- * | PM_FEATURE_CHECK		| 0x3F	| Both		| 2	 | V1 - The API is used to returns supported version of the given API	     |
+ * | PM_FEATURE_CHECK		| 0x3F	| Both		| 2	 | V1 - The API is used to return supported version of the given API	     |
  * | ^				| ^	| ^		| ^	 | V2 - Added support of bitmask payload functionality			     |
  *
  *****************************************************************************/
@@ -141,7 +141,7 @@
  * | IOCTL_GET_QOS			| 34	| Versal	| Get Device QoS value			|
  * | IOCTL_GET_APU_OPER_MODE		| 35	| Versal	| Get APU operation mode		|
  * | IOCTL_SET_APU_OPER_MODE		| 36	| Versal	| Set APU operation mode		|
- * | IOCTL_PREPARE_DDR_SHUTDOWN	| 37	| Versal	| Prepare DDR for shut down		|
+ * | IOCTL_PREPARE_DDR_SHUTDOWN		| 37	| Versal	| Prepare DDR for shut down		|
  *
  *****************************************************************************/
 
