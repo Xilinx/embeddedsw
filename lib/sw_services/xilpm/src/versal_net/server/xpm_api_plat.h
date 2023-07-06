29ee676b6d331e9eb772f9bd174837106bd3240b
Trung Tran <trung.tran@xilinx.com>
2023-07-05 23:55:37 -0700

    xilpm:versal_net: add custom update handler
    - To save dynamic data of XilPm, we need a custom update handler for
    the XPlmi_Pm object.
    
    Signed-off-by: Trung Tran <trung.tran@xilinx.com>
    
    Acked-for-series: Sandeep Reddy Ghanapuram <sandeep.reddy-ghanapuram@xilinx.com>

--- lib/sw_services/xilpm/src/versal_net/server/xpm_api_plat.h
+++ lib/sw_services/xilpm/src/versal_net/server/xpm_api_plat.h
@@ -86,6 +86,7 @@ maybe_unused static inline XStatus IsOnSecondarySLR(u32 SubsystemId)
 	(void)SubsystemId;
 	return XST_FAILURE;
 }
+int XPm_UpdateHandler(XPlmi_ModuleOp Op);
 #ifdef __cplusplus
 }
 #endif
