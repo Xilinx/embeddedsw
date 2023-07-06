44e4ee3d2afc79a7f09328105b78eb1374343a0b
Harsha Harsha <harsha.harsha@amd.com>
2023-07-06 01:59:21 -1200

    sw_services: xilcert: Add support to create ASN.1 structures
    Add support to create required ASN.1 structures for X.509 Extensions
    and Certificate Signing Request.
    Also updated to fix doxygen and coverity warnings.
    Added support for XCert_GetTrailingZeroesCount() required for
    creating ASN.1 BITSTRING.
    
    Signed-off-by: Harsha Harsha <harsha.harsha@amd.com>
    
    Acked-for-series: Kalyani Akula <Kalyani.akula@xilinx.com>

--- lib/sw_services/xilcert/src/xcert_createfield.h
+++ lib/sw_services/xilcert/src/xcert_createfield.h
@@ -35,26 +35,53 @@ extern "C" {
 #include "xil_types.h"
 
 /************************** Constant Definitions *****************************/
+/** @name ASN.1 tags
+ * @{
+ */
+ /**< ASN.1 tags */
+#define XCERT_ASN1_TAG_BOOLEAN						(0x01U)
 #define XCERT_ASN1_TAG_INTEGER						(0x02U)
 #define XCERT_ASN1_TAG_BITSTRING					(0x03U)
 #define XCERT_ASN1_TAG_OCTETSTRING					(0x04U)
 #define XCERT_ASN1_TAG_NULL						(0x05U)
 #define XCERT_ASN1_TAG_SEQUENCE 					(0x30U)
+#define XCERT_ASN1_TAG_SET						(0x31U)
+/** @} */
 
 #define XCERT_LEN_OF_VALUE_OF_VERSION					(0x01U)
+						/**< Length of Version field */
 #define XCERT_LEN_OF_VALUE_OF_SERIAL					(0x14U)
+						/**< Length of Serial field */
+#define XCERT_LEN_OF_VALUE_OF_BOOLEAN					(0x1U)
+						/**< Length of ASN.1 Boolean */
+#define XCERT_LEN_OF_VALUE_OF_PATH_LEN_CONSTRAINT			(0x1U)
+				/**< Length of Path Length Constraint field */
 
+#define XCERT_VERSION_VALUE_V1						(0x0U)
+						/**< Value of Version 0 */
 #define XCERT_VERSION_VALUE_V3						(0x02U)
+						/**< Value of Version 3 */
 #define XCERT_NULL_VALUE						(0x00U)
+							/**< Value of NULL */
+#define XCERT_PATH_LEN_CONSTRAINT_VALUE_0x0				(0x0U)
+				/**< Value of Path Length Constraint - 0 */
+
+#define XCERT_BOOLEAN_TRUE						(0xFFU)
+						/**< Value of Boolean TRUE */
+#define XCERT_BOOLEAN_FALSE						(0x0U)
+						/**< Value of Boolean FALSE */
+#define XCERT_LEN_OF_VERSION_FIELD					(0x3U)
+						/**< Length of Version field */
 
 /************************** Function Prototypes ******************************/
-void XCert_CreateInteger(u8* DataBuf, const u8* IntegerVal, u32 IntegerLen, u32 *Len);
+int XCert_CreateInteger(u8* DataBuf, const u8* IntegerVal, u32 IntegerLen, u32 *FieldLen);
 void XCert_CreateBitString(u8* DataBuf, const u8* BitStringVal, u32 BitStringLen, u32* FieldLen);
 void XCert_CreateOctetString(u8* DataBuf, const u8* OctetStringVal, u32 OctetStringLen,
 	u32* FieldLen);
 int XCert_CreateRawDataFromStr(u8* DataBuf, const char* RawData, u32* RawDataLen);
 void XCert_CreateRawDataFromByteArray(u8* DataBuf, const u8* RawData, const u32 LenOfRawDataVal, u32* RawDataFieldLen);
-int XCert_UpdateEncodedLength(u8* LenIdx, u32 Len, u8* ValIndex);
+void XCert_CreateBoolean(u8* DataBuf, const u8 BooleanVal, u32* FieldLen);
+int XCert_UpdateEncodedLength(u8* LenIdx, u32 Len, u8* ValIdx);
 
 #ifdef __cplusplus
 }
