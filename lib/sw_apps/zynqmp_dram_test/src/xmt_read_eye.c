4d00d9f21f747e8cdc2565db1b38b83aa9aa972f
Harini T <harini.t@amd.com>
2023-07-06 18:03:11 -1200

    lib: sw_apps: zynqmp_dram_test: Add support for system device-tree flow
    Update the library to support for system device-tree flow.
    
    Signed-off-by: Harini T <harini.t@amd.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@amd.com>

--- lib/sw_apps/zynqmp_dram_test/src/xmt_read_eye.c
+++ lib/sw_apps/zynqmp_dram_test/src/xmt_read_eye.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 * Copyright (c) 2018 - 2021 Xilinx, Inc.  All rights reserved.
-* Copyright (c) 2022 Advanced Micro Devices, Inc. All Rights Reserved.
+* Copyright (c) 2022 - 2023 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
  ******************************************************************************/
 
