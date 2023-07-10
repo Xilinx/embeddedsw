2f8bae0b4a8eb3c66a33ea120ee9a2dfb679f357
O'Griofa, Conall <conall.ogriofa@amd.com>
2023-07-09 20:55:04 -1200

    sysmon: Modify addtogroup
    The addtogroup isn now Overview for all C files
    
    Signed-off-by: Conall O'Griofa <conall.ogriofa@amd.com>
    
    Acked-for-series: Erim, Salih <Salih.Erim@amd.com>

--- XilinxProcessorIPLib/drivers/sysmon/src/xsysmon_g.c
+++ XilinxProcessorIPLib/drivers/sysmon/src/xsysmon_g.c
@@ -1,5 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2007 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +8,7 @@
 /**
 *
 * @file xsysmon_g.c
-* @addtogroup sysmon_v7_7
+* @addtogroup Overview
 * @{
 *
 * This file contains a configuration table that specifies the configuration
