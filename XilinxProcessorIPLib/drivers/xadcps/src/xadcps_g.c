c86ba82b7c0e0f1381a01f2e4a2834e6b94d93e3
O'Griofa, Conall <conall.ogriofa@amd.com>
2023-07-09 20:55:05 -1200

    xadcps: Modify addtogroup
    The addtogroup isn now Overview for all C files
    
    Signed-off-by: Conall O'Griofa <conall.ogriofa@amd.com>
    
    Acked-for-series: Erim, Salih <Salih.Erim@amd.com>

--- XilinxProcessorIPLib/drivers/xadcps/src/xadcps_g.c
+++ XilinxProcessorIPLib/drivers/xadcps/src/xadcps_g.c
@@ -1,5 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2011 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +8,7 @@
 /**
 *
 * @file xadcps_g.c
-* @addtogroup xadcps_v2_6
+* @addtogroup Overview
 * @{
 *
 * This file contains a configuration table that specifies the configuration
