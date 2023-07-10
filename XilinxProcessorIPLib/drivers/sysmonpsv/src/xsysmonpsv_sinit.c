ac0e237d499deb869b9641deba6a5631d9c212f7
O'Griofa, Conall <conall.ogriofa@amd.com>
2023-07-09 20:55:03 -1200

    sysmonpsv: Modify addtogroup
    The addtogroup isn now Overview for all C files
    
    Signed-off-by: Conall O'Griofa <conall.ogriofa@amd.com>
    
    Acked-for-series: Erim, Salih <Salih.Erim@amd.com>

--- XilinxProcessorIPLib/drivers/sysmonpsv/src/xsysmonpsv_sinit.c
+++ XilinxProcessorIPLib/drivers/sysmonpsv/src/xsysmonpsv_sinit.c
@@ -1,5 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2016 - 2022 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +8,7 @@
 /**
 *
 * @file xsysmonpsv_sinit.c
-* @addtogroup sysmonpsv_v2_3
+* @addtogroup Overview
 * @{
 *
 * Functions in this file are the minimum required functions for the XSysMonPsv
