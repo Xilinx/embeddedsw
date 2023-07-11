a8e96076959668ca2f0f4235fda16c81841f9c54
Bhavana Jupalli <Bhavana.Jupalli@amd.com>
2023-07-10 18:02:46 -1200

    pciepsu: Add properties to support SDT flow
    - Update the driver to support for system device tree flow
    - Update mdd version
    - Added AMD copyright information
    
    Signed-off-by: Bhavana Jupalli <Bhavana.Jupalli@amd.com>
    Acked-by: Bharat Kumar Gogada <bharat.kumar.gogada@amd.com>

--- XilinxProcessorIPLib/drivers/pciepsu/src/xpciepsu.c
+++ XilinxProcessorIPLib/drivers/pciepsu/src/xpciepsu.c
@@ -1,5 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2018 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2022 - 2023 Advanced Micro Devices, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 *******************************************************************************/
 
