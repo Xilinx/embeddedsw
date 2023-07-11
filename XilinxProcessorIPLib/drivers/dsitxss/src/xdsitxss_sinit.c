3ca23e943d15cd710ecc07280bc31b2281196f0c
Gutti, Prasad <prasad.gutti@amd.com>
2023-07-10 23:04:11 -1200

    DSITXSS: Updated addtogroup to remove version number in src files.
    Modified addtogroup in driver src file by removing version number and
    adding Overview in it.
    
    Signed-off-by: Prasad Gutti <prasad.gutti@amd.com>
    
    Acked-for-series: Gaddipati, Naveen <naveen.gaddipati@amd.com>

--- XilinxProcessorIPLib/drivers/dsitxss/src/xdsitxss_sinit.c
+++ XilinxProcessorIPLib/drivers/dsitxss/src/xdsitxss_sinit.c
@@ -1,5 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2016 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -7,7 +8,7 @@
 /**
 *
 * @file xdsitxss_sinit.c
-* @addtogroup dsitxss_v2_2
+* @addtogroup dsitxss Overview
 * @{
 *
 * This file contains the implementation of the MIPI DSI Tx Subsystem
