01e1ad25695af7d8cf6a068a6e30f6ceb21e049d
Dhanawade, Mohan <mohan.dhanawade@amd.com>
2023-07-11 00:09:26 -1200

    sw_services:Added FIPS required info to VersalNet crypto algo
    This patch adds API to read FIPS information from VersalNet crypto library
    
    Signed-off-by: Mohan Marutirao Dhanawade <mohan.dhanawade@amd.com>
    
    Acked-for-series: VNSL Durga Challa <vnsl.durga.challa@xilinx.com>

---
+++ XilinxProcessorIPLib/drivers/trngpsx/src/xtrngpsx_alginfo.h
@@ -0,0 +1,60 @@
+/******************************************************************************
+* Copyright (c) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+
+/*****************************************************************************/
+/**
+*
+* @file xtrngpsx_alginfo.h
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who     Date     Changes
+* ----- ------  -------- ------------------------------------------------------
+* 1.1   mmd     07/04/23 Initial Release
+* </pre>
+*
+******************************************************************************/
+
+#ifndef XTRNGPSX_ALGINFO_H
+#define XTRNGPSX_ALGINFO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/***************************** Include Files *********************************/
+#include "xil_cryptoalginfo.h"
+#include "xil_util.h"
+
+/**************************** Constant Definitions ****************************/
+#define XTRNGPSX_MAJOR_VERSION	2023
+#define XTRNGPSX_MINOR_VERSION	2
+
+/****************** Macros (Inline Functions) Definitions *********************/
+
+/******************************************************************************/
+/**
+ *
+ * This function returns the TRNG crypto algorithm information.
+ *
+ * @param	AlgInfo  Pointer to memory for holding the crypto algorithm information
+ *
+ * @return	None
+ *
+ ******************************************************************************/
+static __attribute__((always_inline)) inline
+void XTrngpsx_GetCryptoAlgInfo (Xil_CryptoAlgInfo *AlgInfo)
+{
+	AlgInfo->Version = XIL_BUILD_VERSION(XTRNGPSX_MAJOR_VERSION, XTRNGPSX_MINOR_VERSION);
+	AlgInfo->NistStatus = NIST_COMPLIANT;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* XTRNGPSX_ALGINFO_H */
---
+++ lib/sw_services/xilsecure/src/versal_net/common/xsecure_aesalginfo.h
@@ -0,0 +1,60 @@
+/******************************************************************************
+* Copyright (c) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+
+/*****************************************************************************/
+/**
+*
+* @file xsecure_aesalginfo.h
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who     Date     Changes
+* ----- ------  -------- ------------------------------------------------------
+* 5.2   mmd     07/04/23 Initial Release
+* </pre>
+*
+******************************************************************************/
+
+#ifndef XSECURE_AESALGINFO_H
+#define XSECURE_AESALGINFO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/***************************** Include Files *********************************/
+#include "xil_cryptoalginfo.h"
+#include "xil_util.h"
+
+/**************************** Constant Definitions ****************************/
+#define XSECURE_AES_MAJOR_VERSION	2023
+#define XSECURE_AES_MINOR_VERSION	2
+
+/****************** Macros (Inline Functions) Definitions *********************/
+
+/******************************************************************************/
+/**
+ *
+ * This function returns the AES crypto algorithm information.
+ *
+ * @param	AlgInfo  Pointer to memory for holding the crypto algorithm information
+ *
+ * @return	None
+ *
+ ******************************************************************************/
+static __attribute__((always_inline)) inline
+void XSecure_AesGetCryptoAlgInfo (Xil_CryptoAlgInfo *AlgInfo)
+{
+	AlgInfo->Version = XIL_BUILD_VERSION(XSECURE_AES_MAJOR_VERSION, XSECURE_AES_MINOR_VERSION);
+	AlgInfo->NistStatus = NIST_COMPLIANT;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* XSECURE_AESALGINFO_H */
