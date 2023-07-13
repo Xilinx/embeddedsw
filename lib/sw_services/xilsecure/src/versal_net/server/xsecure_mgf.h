2261f1eed3d0fc33ef3664793c720f4b37389e7c
Kundanala, Praveen Teja <praveen.teja.kundanala@amd.com>
2023-07-12 23:37:06 -1200

    sw_services:xilsecure: Add mask generate function
    Mask generation function(MGF) provides mask of given output length with
    a given fixed input by using cryptographic hash function i.e. SHA1 or SHA3.
    
    Add mask generate function(MGF) in xilsecure for padding
    and other secure use cases and currently only SHA3-384 hash function
    is supported for MGF.
    
    Signed-off-by: Praveen Teja Kundanala <praveen.teja.kundanala@amd.com>
    
    Acked-for-series: Mohan Marutirao Dhanawade <mohan.dhanawade@amd.com>

---
+++ lib/sw_services/xilsecure/src/versal_net/server/xsecure_mgf.h
@@ -0,0 +1,75 @@
+/******************************************************************************
+* Copyright (C) 2023, Advanced Micro Devices, Inc. All Rights Reserved.
+* SPDX-License-Identifier: MIT
+*******************************************************************************/
+
+/*****************************************************************************/
+/**
+*
+* @file xsecure_mgf.h
+* @addtogroup xsecure_mgf_apis XilSecure MGF APIs
+* @{
+* @cond xsecure_internal
+* This file contains function declarations and enums related to mask generate function
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who  Date        Changes
+* ----- ---- -------- -------------------------------------------------------
+* 5.2   kpt  06/28/23 Initial release
+*
+* </pre>
+*
+* @endcond
+******************************************************************************/
+
+#ifndef XSECURE_MGF_H_
+#define XSECURE_MGF_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/***************************** Include Files *********************************/
+
+#include "xil_types.h"
+
+/************************** Constant Definitions ****************************/
+
+#define XSECURE_SHA3_HASH_LENGTH_IN_BYTES  (48U) /**< SHA3 Hash length in bytes */
+
+/***************************** Type Definitions ******************************/
+
+typedef enum {
+	XSECURE_SHA3_384 = 0, /**< SHA3 384 */
+} XSecure_ShaType;
+
+typedef struct {
+	XSecure_ShaType Shatype; /**< SHA type for MGF */
+	u32 HashLen;             /**< Hash length for MGF */
+	int (*ShaStart)(void *InstancePtr); /**< Function pointer to SHA start */
+	int (*ShaUpdate)(void *InstancePtr, u64 InputDataAddr, u32 Size); /**< Function pointer to SHA update */
+	int (*ShaFinish)(void *InstancePtr, u64 HashAddr); /**< Function pointer to SHA finish */
+	int (*ShaDigest)(void *InstancePtr, u64 InputDataAddr, u32 Size, u64 HashAddr); /**< Function pointer to SHA digest */
+} XSecure_HashAlgInfo;
+
+typedef struct {
+	u8 *Seed;      /**< Input seed on which mask should be generated */
+	u8 *Output;    /**< Buffer to store the mask */
+	u32 SeedLen;   /**< Seed length */
+	u32 OutputLen; /**< Buffer length */
+} XSecure_MgfInput;
+
+/***************************** Function Prototypes ***************************/
+
+int XSecure_MaskGenFunc(XSecure_ShaType Shatype,
+			void *InstancePtr, XSecure_MgfInput *MgfInput);
+XSecure_HashAlgInfo *XSecure_GetHashInstance(XSecure_ShaType Shatype);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* XSECURE_MGF_H_ */
+/* @} */
