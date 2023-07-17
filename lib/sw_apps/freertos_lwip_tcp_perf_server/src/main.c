7fe080b137a989ad83976729809abb87fc96daf3
Karumanchi, Vineeth <vineeth.karumanchi@amd.com>
2023-07-17 03:29:09 -1200

    lib: sw_apps: freertos_lwip_tcp_perf_server: Add SDT deviations
    Add SDT flow deviations.
    Update copyright and license information.
    Code format changes
    
    Signed-off-by: Vineeth Karumanchi <vineeth.karumanchi@amd.com>
    
    Acked-for-series: Harini Katakam <harini.katakam@amd.com>

--- lib/sw_apps/freertos_lwip_tcp_perf_server/src/main.c
+++ lib/sw_apps/freertos_lwip_tcp_perf_server/src/main.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2018 - 2019 Xilinx, Inc.
+ * Copyright (C) 2018 - 2022 Xilinx, Inc.
+ * Copyright (C) 2022 - 2023 Advanced Micro Devices, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -49,7 +50,7 @@ err_t dhcp_start(struct netif *netif);
 #endif /* LWIP_IPV6 */
 
 #ifdef XPS_BOARD_ZCU102
-#ifdef XPAR_XIICPS_0_DEVICE_ID
+#if defined(XPAR_XIICPS_0_DEVICE_ID) || defined(XPAR_XIICPS_0_BASEADDR)
 int IicPhyReset(void);
 #endif
 #endif
