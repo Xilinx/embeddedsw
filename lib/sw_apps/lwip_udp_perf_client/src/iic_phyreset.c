f8ee0dc7a33881602df5cdec8d861561527ffe71
Karumanchi, Vineeth <vineeth.karumanchi@amd.com>
2023-07-17 03:29:05 -1200

    lib: sw_apps: lwip_udp_server_client: Add SDT deviations
    - Add SDT deviations
    - Update license and copyright
    - Format the changes.
    
    Signed-off-by: Vineeth Karumanchi <vineeth.karumanchi@amd.com>
    
    Acked-for-series: Harini Katakam <harini.katakam@amd.com>

--- lib/sw_apps/lwip_udp_perf_client/src/iic_phyreset.c
+++ lib/sw_apps/lwip_udp_perf_client/src/iic_phyreset.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2016 - 2021 Xilinx, Inc.
+ * Copyright (C) 2016 - 2022 Xilinx, Inc.
+ * Copyright (C) 2022 - 2023 Advanced Micro Devices, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -29,10 +30,12 @@
 #include <stdio.h>
 
 #include "xparameters.h"
+#if defined (__arm__) || defined(__aarch64__)
 #include "xil_printf.h"
+#endif
 
 #ifdef XPS_BOARD_ZCU102
-#ifdef XPAR_XIICPS_0_DEVICE_ID
+#if defined(XPAR_XIICPS_0_DEVICE_ID) || defined(XPAR_XIICPS_0_BASEADDR)
 #include "xiicps.h"
 
 #define BUF_LEN		10U
@@ -58,14 +61,18 @@ int IicPhyReset(void)
 	int Status = XST_SUCCESS;
 
 	/* Initialize the IIC0 driver so that it is ready to use */
+#if defined(SDT) && defined(XPAR_XIICPS_0_BASEADDR)
+	I2c0CfgPtr = XIicPs_LookupConfig(XPAR_XIICPS_0_BASEADDR);
+#else
 	I2c0CfgPtr = XIicPs_LookupConfig(XPAR_XIICPS_0_DEVICE_ID);
+#endif
 	if (I2c0CfgPtr == NULL) {
 		Status = XST_FAILURE;
 		return Status;
 	}
 
 	Status = XIicPs_CfgInitialize(&I2c0InstancePtr, I2c0CfgPtr,
-			I2c0CfgPtr->BaseAddress);
+				      I2c0CfgPtr->BaseAddress);
 	if (Status != XST_SUCCESS) {
 		Status = XST_FAILURE;
 		return Status;
@@ -78,7 +85,7 @@ int IicPhyReset(void)
 	WriteBuffer[0] = CMD_CFG_0_REG;
 	WriteBuffer[1] = DATA_OUTPUT;
 	Status = XIicPs_MasterSendPolled(&I2c0InstancePtr,
-			WriteBuffer, 2, IOEXPANDER1_ADDR);
+					 WriteBuffer, 2, IOEXPANDER1_ADDR);
 	if (Status != XST_SUCCESS) {
 		Status = XST_FAILURE;
 		return Status;
@@ -98,7 +105,7 @@ int IicPhyReset(void)
 
 	/* Send the Data */
 	Status = XIicPs_MasterSendPolled(&I2c0InstancePtr,
-			WriteBuffer, 2, IOEXPANDER1_ADDR);
+					 WriteBuffer, 2, IOEXPANDER1_ADDR);
 	if (Status != XST_SUCCESS) {
 		Status = XST_FAILURE;
 		return Status;
@@ -107,8 +114,9 @@ int IicPhyReset(void)
 	/* Wait until bus is idle */
 	while (XIicPs_BusIsBusy(&I2c0InstancePtr));
 
-	xil_printf("IIC PHY reset on ZCU102 successful\n\r");
-	return Status;
+	xil_printf("IIC PHY reset on ZCU102 successful \n\r");
+
+	return XST_SUCCESS;
 }
 #endif
 #endif
