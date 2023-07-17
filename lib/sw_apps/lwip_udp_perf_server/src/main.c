d497ca49c9dd311cd0c8c7c0e6e230dfe94ddcce
Karumanchi, Vineeth <vineeth.karumanchi@amd.com>
2023-07-17 03:29:03 -1200

    lib: sw_apps: lwip_udp_perf_server: Add SDT flow deviations
    - Add SDT flow deviations
    - Update license and copyright
    - Format the changes
    
    Signed-off-by: Vineeth Karumanchi <vineeth.karumanchi@amd.com>
    
    Acked-for-series: Harini Katakam <harini.katakam@amd.com>

--- lib/sw_apps/lwip_udp_perf_server/src/main.c
+++ lib/sw_apps/lwip_udp_perf_server/src/main.c
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2017 - 2019 Xilinx, Inc.
+ * Copyright (C) 2017 - 2022 Xilinx, Inc.
+ * Copyright (C) 2022 - 2023 Advanced Micro Devices, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
@@ -64,7 +65,7 @@ int ProgramSfpPhy(void);
 #endif
 
 #ifdef XPS_BOARD_ZCU102
-#ifdef XPAR_XIICPS_0_DEVICE_ID
+#if defined (XPAR_XIICPS_0_DEVICE_ID) || defined (XPAR_XIICPS_0_BASEADDR)
 int IicPhyReset(void);
 #endif
 #endif
@@ -144,8 +145,10 @@ int main(void)
 	}
 	netif_set_default(netif);
 
+#ifndef SDT
 	/* now enable interrupts */
 	platform_enable_interrupts();
+#endif
 
 	/* specify that the network if is up */
 	netif_set_up(netif);
