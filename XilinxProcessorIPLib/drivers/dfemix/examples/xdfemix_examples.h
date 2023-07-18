f1d7a6a7be9ddb87dd22ff765a09a4e0b33818d5
O'Griofa, Conall <conall.ogriofa@amd.com>
2023-07-18 01:03:56 -1200

    dfemix: Add SDT examples
    This patch modifies the examples to work in SDT flow
    
    Signed-off-by: Conall O'Griofa <conall.ogriofa@amd.com>
    Acked-by: Cvetic, Dragan <dragan.cvetic@amd.com>

--- XilinxProcessorIPLib/drivers/dfemix/examples/xdfemix_examples.h
+++ XilinxProcessorIPLib/drivers/dfemix/examples/xdfemix_examples.h
@@ -1,5 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2021-2022 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -15,6 +16,7 @@
 * Ver   Who    Date     Changes
 * ----- -----  -------- -----------------------------------------------------
 * 1.1   dc     07/21/21 Add and reorganise examples
+* 1.6   cog    07/18/23 Modify example for SDT flow
 *
 * </pre>
 *
@@ -22,7 +24,9 @@
 
 /***************************** Include Files ********************************/
 #ifdef __BAREMETAL__
+#ifndef SDT
 #include "xparameters.h"
+#endif
 #include <metal/device.h>
 #endif
 #include "xdfemix.h"
@@ -39,7 +43,13 @@
 /***************** Macros (Inline Functions) Definitions ********************/
 #ifdef __BAREMETAL__
 #define printf xil_printf
+#ifndef SDT
 #define XDFEMIX_NODE_NAME XPAR_XDFEMIX_0_DEV_NAME
+#define XDFEMIX_NODE_BASE XPAR_XDFEMIX_0_S_AXI_BASEADDR
+#else
+#define XDFEMIX_NODE_NAME "a7c40000.xdfe_cc_mixer"
+#define XDFEMIX_NODE_BASE 0xa7c40000
+#endif
 #define XDFESI570_CURRENT_FREQUENCY 156.25
 #define XDFESI570_NEW_FREQUENCY 122.88
 #else
