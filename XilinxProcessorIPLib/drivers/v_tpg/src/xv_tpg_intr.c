08fea7176506c82c320f8dc025dc6d51ce065391
Gutti, Prasad <prasad.gutti@amd.com>
2023-07-19 20:04:06 -1200

    TPG: Ported driver for decoupling flow.
    Modified driver sources to use base address instead of device_id.
    Necessary modifications are done to handle decoupling flow.
    Added Cmakelists.txt file
    Updated all files with AMD copyright
    
    Signed-off-by: Prasad Gutti <prasad.gutti@amd.com>
    
    Acked-for-series: Gaddipati, Naveen <naveen.gaddipati@amd.com>

--- XilinxProcessorIPLib/drivers/v_tpg/src/xv_tpg_intr.c
+++ XilinxProcessorIPLib/drivers/v_tpg/src/xv_tpg_intr.c
@@ -1,5 +1,6 @@
 /******************************************************************************
  * Copyright (C) 2018 - 2021 Xilinx, Inc.	All rights reserved.
+ * Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
  * SPDX-License-Identifier: MIT
  ******************************************************************************/
 
@@ -7,7 +8,7 @@
 /**
  *
  * @file xv_tpg_intr.c
- * @addtogroup v_tpg_v8_4
+ * @addtogroup v_tpg Overview
  * @{
  *
  * The functions in this file provides interrupt handler and associated
--- XilinxProcessorIPLib/drivers/v_tpg/src/xv_tpg_sinit.c
+++ XilinxProcessorIPLib/drivers/v_tpg/src/xv_tpg_sinit.c
@@ -1,12 +1,15 @@
 // ==============================================================
 // Copyright (c) 2015 - 2021 Xilinx Inc. All rights reserved.
+// Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 // SPDX-License-Identifier: MIT
 // ==============================================================
 
 #ifndef __linux__
 
 #include "xstatus.h"
+#ifndef SDT
 #include "xparameters.h"
+#endif
 #include "xv_tpg.h"
 
 #ifndef XPAR_XV_TPG_NUM_INSTANCES
@@ -15,6 +18,7 @@
 
 extern XV_tpg_Config XV_tpg_ConfigTable[];
 
+#ifndef SDT
 XV_tpg_Config *XV_tpg_LookupConfig(u16 DeviceId) {
 	XV_tpg_Config *ConfigPtr = NULL;
 
@@ -45,5 +49,37 @@ int XV_tpg_Initialize(XV_tpg *InstancePtr, u16 DeviceId) {
                                 ConfigPtr,
                                 ConfigPtr->BaseAddress);
 }
+#else
+XV_tpg_Config *XV_tpg_LookupConfig(UINTPTR BaseAddress) {
+	XV_tpg_Config *ConfigPtr = NULL;
 
+	int Index;
+
+	for (Index = 0U; XV_tpg_ConfigTable[Index].Name != NULL; Index++) {
+		if ((XV_tpg_ConfigTable[Index].BaseAddress == BaseAddress) ||
+			!BaseAddress) {
+			ConfigPtr = &XV_tpg_ConfigTable[Index];
+		break;
+		}
+	}
+
+	return ConfigPtr;
+}
+
+int XV_tpg_Initialize(XV_tpg *InstancePtr, UINTPTR BaseAddress) {
+	XV_tpg_Config *ConfigPtr;
+
+	Xil_AssertNonvoid(InstancePtr != NULL);
+
+	ConfigPtr = XV_tpg_LookupConfig(BaseAddress);
+	if (ConfigPtr == NULL) {
+		InstancePtr->IsReady = 0;
+		return (XST_DEVICE_NOT_FOUND);
+	}
+
+	return XV_tpg_CfgInitialize(InstancePtr,
+                                ConfigPtr,
+                                ConfigPtr->BaseAddress);
+}
+#endif
 #endif
