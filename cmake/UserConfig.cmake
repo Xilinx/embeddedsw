abbd8c7ad88914fbf98512a0303b2620e0df603c
Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
2023-07-27 23:38:59 -1200

    cmake: UserConfig.cmake: Remove the quotes around variable initialization
    For a proper GUI experience remove the quotes around variable
    initialization.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>

--- cmake/UserConfig.cmake
+++ cmake/UserConfig.cmake
@@ -26,75 +26,79 @@ set(USER_UNDEFINED_SYMBOLS
 # Example 3: Adding ${CMAKE_SOURCE_DIR}/data/include to add data/include from this project.
 
 set(USER_INCLUDE_DIRECTORIES
-""
 )
 
 # -----------------------------------------
 
-# Turn on all optional warnings ("-Wall" | "")
-set(USER_COMPILE_WARNINGS_ALL "-Wall")
+# Turn on all optional warnings (-Wall)
+set(USER_COMPILE_WARNINGS_ALL -Wall)
 
-# Enable extra warning flags ("-Wextra" | "")
-SET(USER_COMPILE_WARNINGS_EXTRA "-Wextra")
+# Enable extra warning flags (-Wextra)
+set(USER_COMPILE_WARNINGS_EXTRA -Wextra)
 
-# Make all warnings into hard errors ("-Werror" | "")
-set(USER_COMPILE_WARNINGS_AS_ERRORS "")
+# Make all warnings into hard errors (-Werror)
+set(USER_COMPILE_WARNINGS_AS_ERRORS )
 
-# Check the code for syntax errors, but don’t do anything beyond that. ("-fsyntax-only" | "")
-set(USER_COMPILE_WARNINGS_CHECK_SYNTAX_ONLY "")
+# Check the code for syntax errors, but don't do anything beyond that. (-fsyntax-only)
+set(USER_COMPILE_WARNINGS_CHECK_SYNTAX_ONLY )
 
-# Issue all the mandatory diagnostics listed in the C standard ("-pedantic" | "")
-set(USER_COMPILE_WARNINGS_PEDANTIC "")
+# Issue all the mandatory diagnostics listed in the C standard (-pedantic)
+set(USER_COMPILE_WARNINGS_PEDANTIC )
 
-# Issue all the mandatory diagnostics, and make all mandatory diagnostics into errors. ("-pedantic-errors" | "")
-set(USER_COMPILE_WARNINGS_PEDANTIC_AS_ERRORS "")
+# Issue all the mandatory diagnostics, and make all mandatory diagnostics into errors. (-pedantic-errors)
+set(USER_COMPILE_WARNINGS_PEDANTIC_AS_ERRORS )
 
-# Suppress all warnings ("-w" | "")
-set(USER_COMPILE_WARNINGS_INHIBIT_ALL "")
+# Suppress all warnings (-w)
+set(USER_COMPILE_WARNINGS_INHIBIT_ALL )
 
 # -----------------------------------------
 
 # Optimization level   "-O0" [None] , "-O1" [Optimize] , "-O2" [Optimize More], "-O3" [Optimize Most] or "-Os" [Optimize Size]
-set(USER_COMPILE_OPTIMIZATION_LEVEL "-O0")
+set(USER_COMPILE_OPTIMIZATION_LEVEL -O0)
 
 # Other flags related to optimization
-set(USER_COMPILE_OPTIMIZATION_OTHER_FLAGS "")
+set(USER_COMPILE_OPTIMIZATION_OTHER_FLAGS )
 
 # -----------------------------------------
 
 # Debug level "" [None], "-g1" [Minimum], "g2" [Default], "g3" [Maximim]
-set(USER_COMPILE_DEBUG_LEVEL "-g3")
+set(USER_COMPILE_DEBUG_LEVEL -g3)
 
 # Other flags releated to debugging
-set(USER_COMPILE_DEBUG_OTHER_FLAGS "")
+set(USER_COMPILE_DEBUG_OTHER_FLAGS )
 
 # -----------------------------------------
 
-# Verbose ("-v" | "")
-set(USER_COMPILE_VERBOSE "")
+# Enable Profiling (-pg)
+set(USER_COMPILE_PROFILING_ENABLE )
 
-# Support ANSI_PROGRAM ("-ansi" | "")
-set(USER_COMPILE_ANSI "")
+# -----------------------------------------
+
+# Verbose (-v)
+set(USER_COMPILE_VERBOSE )
+
+# Support ANSI_PROGRAM (-ansi)
+set(USER_COMPILE_ANSI )
 
 # Add any compiler options that are not covered by the above variables, they will be added as extra compiler options
 # To enable profiling -pg [ for gprof ]  or -p [ for prof information ]
-set(USER_COMPILE_OTHER_FLAGS "")
+set(USER_COMPILE_OTHER_FLAGS )
 
 # -----------------------------------------
 
 # Linker options
 # Do not use the standard system startup files when linking.
-# The standard system libraries are used normally, unless -nostdlib or -nodefaultlibs is used. ("-nostartfiles" | "")
-set(USER_LINK_NO_START_FILES "")
+# The standard system libraries are used normally, unless -nostdlib or -nodefaultlibs is used. (-nostartfiles)
+set(USER_LINK_NO_START_FILES )
 
-# Do not use the standard system libraries when linking. ("-nodefaultlibs" | "")
-set(USER_LINK_NO_DEFAULT_LIBS "")
+# Do not use the standard system libraries when linking. (-nodefaultlibs)
+set(USER_LINK_NO_DEFAULT_LIBS )
 
-# Do not use the standard system startup files or libraries when linking. ("-nostdlib" | "")
-set(USER_LINK_NO_STDLIB "")
+# Do not use the standard system startup files or libraries when linking. (-nostdlib)
+set(USER_LINK_NO_STDLIB )
 
-# Omit all symbol information ("-s" | "")
-set(USER_LINK_OMIT_ALL_SYMBOL_INFO "")
+# Omit all symbol information (-s)
+set(USER_LINK_OMIT_ALL_SYMBOL_INFO )
 
 
 # -----------------------------------------
@@ -102,14 +106,12 @@ set(USER_LINK_OMIT_ALL_SYMBOL_INFO "")
 # Add any libraries to be linked below, they will be added as extra libraries.
 # User need to update USER_LINK_DIRECTORIES below with these library paths.
 set(USER_LINK_LIBRARIES
-""
 )
 
 # Add any directories to look for the libraries to be linked.
 # Example 1: Adding /proj/compression/lib will pass -L/proj/compression/lib to the linker.
 # Example adding Adding ../../common/lib will consider the path as relative to this directory. and will pass the path to -L option.
 set(USER_LINK_DIRECTORIES
-""
 )
 
 # -----------------------------------------
@@ -119,7 +121,6 @@ set(USER_LINKER_SCRIPT "${CMAKE_SOURCE_DIR}/lscript.ld")
 # Add linker options to be passed, they will be added as extra linker options
 # Example : adding -s will pass -s to the linker.
 set(USER_LINK_OTHER_FLAGS
-""
 )
 
 # -----------------------------------------
@@ -127,32 +128,30 @@ set(USER_LINK_OTHER_FLAGS
 ###   END OF USER SETTINGS SECTION ###
 ###   DO NOT EDIT BEYOND THIS LINE ###
 
-set(USER_COMPILE_OPTIONS "")
-string(APPEND USER_COMPILE_OPTIONS
-" ${USER_COMPILE_WARNINGS_ALL}"
-" ${USER_COMPILE_WARNINGS_EXTRA}"
-" ${USER_COMPILE_WARNINGS_AS_ERRORS}"
-" ${USER_COMPILE_WARNINGS_CHECK_SYNTAX_ONLY}"
-" ${USER_COMPILE_WARNINGS_PEDANTIC}"
-" ${USER_COMPILE_WARNINGS_PEDANTIC_AS_ERRORS}"
-" ${USER_COMPILE_WARNINGS_INHIBIT_ALL}"
-" ${USER_COMPILE_OPTIMIZATION_LEVEL}"
-" ${USER_COMPILE_OPTIMIZATION_OTHER_FLAGS}"
-" ${USER_COMPILE_DEBUG_LEVEL}"
-" ${USER_COMPILE_DEBUG_OTHER_FLAGS}"
-" ${USER_COMPILE_VERBOSE}"
-" ${USER_COMPILE_ANSI}"
-" ${USER_COMPILE_OTHER_FLAGS}"
+set(USER_COMPILE_OPTIONS
+    " ${USER_COMPILE_WARNINGS_ALL}"
+    " ${USER_COMPILE_WARNINGS_EXTRA}"
+    " ${USER_COMPILE_WARNINGS_AS_ERRORS}"
+    " ${USER_COMPILE_WARNINGS_CHECK_SYNTAX_ONLY}"
+    " ${USER_COMPILE_WARNINGS_PEDANTIC}"
+    " ${USER_COMPILE_WARNINGS_PEDANTIC_AS_ERRORS}"
+    " ${USER_COMPILE_WARNINGS_INHIBIT_ALL}"
+    " ${USER_COMPILE_OPTIMIZATION_LEVEL}"
+    " ${USER_COMPILE_OPTIMIZATION_OTHER_FLAGS}"
+    " ${USER_COMPILE_DEBUG_LEVEL}"
+    " ${USER_COMPILE_DEBUG_OTHER_FLAGS}"
+    " ${USER_COMPILE_VERBOSE}"
+    " ${USER_COMPILE_ANSI}"
+    " ${USER_COMPILE_OTHER_FLAGS}"
 )
 foreach(entry ${USER_UNDEFINED_SYMBOLS})
-    string(APPEND USER_COMPILE_OPTIONS " -U${entry}")
+    list(APPEND USER_COMPILE_OPTIONS " -U${entry}")
 endforeach()
 
-set(USER_LINK_OPTIONS "")
-string (APPEND USER_LINK_OPTIONS
-" ${USER_LINKER_NO_START_FILES}"
-" ${USER_LINKER_NO_DEFAULT_LIBS}"
-" ${USER_LINKER_NO_STDLIB}"
-" ${USER_LINKER_OMIT_ALL_SYMBOL_INFO}"
-" ${USER_LINK_OTHER_FLAGS}"
+set(USER_LINK_OPTIONS
+    " ${USER_LINKER_NO_START_FILES}"
+    " ${USER_LINKER_NO_DEFAULT_LIBS}"
+    " ${USER_LINKER_NO_STDLIB}"
+    " ${USER_LINKER_OMIT_ALL_SYMBOL_INFO}"
+    " ${USER_LINK_OTHER_FLAGS}"
 )
