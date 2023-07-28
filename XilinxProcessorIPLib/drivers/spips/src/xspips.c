9c289d9f23b0825d1cde5a049eda940f1caa1b71
sboyapal <srikanth.boyapally@amd.com>
2023-07-28 04:07:08 -1200

    spips: Fix data corruption issue at lower clock speed
    Data corruption issue on receiving side as there no FIFO data ready check.
    We are not relying on FIFO RX not empty status of ISR register due controller
    limitation(AR:65885), To fix this issue adding 10 micro seconds(1 byte delay)
    after TX FIFO empty and before reading the RX FIFO data.
    
    By adding delay of 10 micro seconds(1 byte delay) will fix the issue.
    
    Signed-off-by: Srikanth Boyapally <srikanth.boyapally@amd.com>
    Acked-by: Srinivas Goud <srinivas.goud@amd.com>

--- XilinxProcessorIPLib/drivers/spips/src/xspips.c
+++ XilinxProcessorIPLib/drivers/spips/src/xspips.c
@@ -55,6 +55,8 @@
 * 3.8   sg     08/16/22 Updated SPI enable and chip selection sequence
 * 			for manual mode.
 * 3.9   sb     07/05/23 Added support for system device-tree flow.
+* 3.9   sb     07/27/23 Fix the issue of driver giving junk values when its
+*                       running at low clock speed in slave mode.
 * </pre>
 *
 ******************************************************************************/
@@ -62,6 +64,7 @@
 /***************************** Include Files *********************************/
 
 #include "xspips.h"
+#include "sleep.h"
 
 /************************** Constant Definitions *****************************/
 
@@ -936,6 +939,11 @@ void XSpiPs_InterruptHandler(XSpiPs *InstancePtr)
 		TransCount = SpiPtr->RequestedBytes - SpiPtr->RemainingBytes;
 
 		while (TransCount != 0U) {
+			/* Fixed delay due to controller limitation with
+			 * RX_NEMPTY incorrect status
+			 * Xilinx AR:65885 contains more details
+			 */
+			usleep(10);
 			TempData = (u8)XSpiPs_RecvByte(SpiPtr->Config.BaseAddress);
 			if (SpiPtr->RecvBufferPtr != NULL) {
 				*SpiPtr->RecvBufferPtr = TempData;
