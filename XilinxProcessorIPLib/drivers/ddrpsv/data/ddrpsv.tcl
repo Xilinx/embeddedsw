3b9e8b74feb8e55b59a26c3a5cb507fd5dbd7c96
Goud, Srinivas <srinivas.goud@amd.com>
2023-08-06 20:54:20 -1200

    ddrpsv: Add VersalNet support
    Updated ddrpsv driver tcl for VersalNet support.
    
    Signed-off-by: Srinivas Goud <srinivas.goud@amd.com>
    Acked-by: Mubin Sayyed <mubin.sayyed@amd.com>

--- XilinxProcessorIPLib/drivers/ddrpsv/data/ddrpsv.tcl
+++ XilinxProcessorIPLib/drivers/ddrpsv/data/ddrpsv.tcl
@@ -1,5 +1,6 @@
 ###############################################################################
 # Copyright (C) 2019 - 2021 Xilinx, Inc.  All rights reserved.
+# Copyright (C) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
 # SPDX-License-Identifier: MIT
 #
 #
@@ -58,10 +59,12 @@
 #                          psv_psm processor. As macros exported by this tcl are
 #                          consumed by only ARM based BSP's, we can skip
 #                          it for firmware processor BSP. It fixes CR#1105828
+# 1.5   sg    08/04/23     Added VersalNet support
 #
 ###############################################################################
 set file_handle 0
 set sw_proc 0
+set is_versal_net 0
 
 # HW designs can have multiple NOC IPs, and each of them can be connected to
 # same DDR segment with different address ranges, and through different
@@ -77,12 +80,30 @@ set sw_proc 0
 #--------------------|-------------------|
 #  DDR_LOW_0         |        0          |
 #  DDR_LOW_1         |        1          |
-#..DDR_LOW_2         |        2          |
+#  DDR_LOW_2         |        2          |
 #  DDR_LOW_3         |        3          |
 #  DDR_CH_1          |        4          |
 #  DDR_CH_2          |        5          |
 #  DDR_CH_3          |        6          |
 #----------------------------------------
+#
+#Versal-Net
+#---------------------------------------
+# DDR segment        |       Index       |
+#--------------------|-------------------|
+#  DDR_CH0_LEGACY    |        0          |
+#  DDR_CH0_MED       |        1          |
+#  DDR_CH0_HIGH0     |        2          |
+#  DDR_CH0_HIGH1     |        3          |
+#  DDR_CH_1          |        4          |
+#  DDR_CH_1A         |        5          |
+#  DDR_CH_2	     |	      6		 |
+#  DDR_CH_2A         |        7		 |
+#  DDR_CH_3          |        8		 |
+#  DDR_CH_3A 	     |        9		 |
+#  DDR_CH_4          |        10         |
+#----------------------------------------
+
 #
 global base_addr_list
 global high_addr_list
@@ -91,11 +112,18 @@ proc generate {drv_handle} {
       set sw_proc_handle [hsi::get_sw_processor]
       set hw_proc_handle [hsi::get_cells -hier [common::get_property HW_INSTANCE $sw_proc_handle] ]
       set proctype [common::get_property IP_NAME $hw_proc_handle ]
+      set is_versal_net [hsi::get_cells -hier -filter {IP_NAME=="psxl_cortexr52" || \
+	      IP_NAME=="psx_cortexr52" || IP_NAME=="psxl_cortexa78" || IP_NAME=="psx_cortexa78"}]
 
       # define_addr_params macro proc exports macros related to available DDR regions to
       # xparameters.h. Those macro's are comsumed by MPU/MMU tables in ARM based BSPs.
-      if { $proctype != "psv_pmc" && $proctype != "psv_psm" } {
-              define_addr_params $drv_handle "xparameters.h"
+      if { $proctype != "psv_pmc" && $proctype != "psv_psm" && \
+	      $proctype != "psx_pmc" && $proctype != "psx_psm" } {
+		if {[llength $is_versal_net] > 0} {
+	              define_addr_params_versal_net $drv_handle "xparameters.h"
+		} else {
+	              define_addr_params $drv_handle "xparameters.h"
+		}
       }
 }
 
@@ -190,7 +218,7 @@ proc define_addr_params {drv_handle file_name} {
 			# for this DDR region
 			#
 			set ddr_region_id 5
-			xddrpsv_handle_address_details $i $periph $block_name $is_ddr_ch_2 5
+			xddrpsv_handle_address_details $i $periph $block_name $is_ddr_ch_2 $ddr_region_id
 
 			set is_ddr_ch_2 1
 		} elseif {[string match "C*_DDR_CH3*" $block_name]} {
@@ -205,19 +233,12 @@ proc define_addr_params {drv_handle file_name} {
 		}
 
 		incr i
-
 	}
-
-
-
-
    }
 
 	puts $file_handle ""
 	puts $file_handle "/* Canonicals definitions for NOC DDR to be consumed by MMU/MPU tables in BSP*/"
 	if {$is_ddr_low_0 == 1} {
-
-
 		set ddr_region_id 0
 		set value  [lindex $base_addr_list $ddr_region_id]
 		puts $file_handle "#define XPAR_AXI_NOC_DDR_LOW_0_BASEADDR $value"
@@ -227,7 +248,6 @@ proc define_addr_params {drv_handle file_name} {
 		puts $file_handle ""
 
 	}
-
 
 	if {$is_ddr_low_1 == 1} {
 		set ddr_region_id 1
@@ -239,7 +259,6 @@ proc define_addr_params {drv_handle file_name} {
 		puts $file_handle ""
 	}
 
-
 	if {$is_ddr_low_2 == 1} {
 		set ddr_region_id 2
 		set value  [lindex $base_addr_list $ddr_region_id]
@@ -249,7 +268,6 @@ proc define_addr_params {drv_handle file_name} {
 		puts $file_handle "#define XPAR_AXI_NOC_DDR_LOW_2_HIGHADDR $value"
 		puts $file_handle ""
 	}
-
 
 	if {$is_ddr_low_3 == 1} {
 		set ddr_region_id 3
@@ -261,7 +279,6 @@ proc define_addr_params {drv_handle file_name} {
 		puts $file_handle ""
 	}
 
-
 	if {$is_ddr_ch_1 == 1} {
 		set ddr_region_id 4
 		set value  [lindex $base_addr_list $ddr_region_id]
@@ -272,7 +289,7 @@ proc define_addr_params {drv_handle file_name} {
 		puts $file_handle "#define XPAR_AXI_NOC_DDR_CH_1_HIGHADDR $value"
 		puts $file_handle ""
 	}
-	
+
 	if {$is_ddr_ch_2 == 1} {
 		set ddr_region_id 5
 		set value  [lindex $base_addr_list $ddr_region_id]
@@ -297,6 +314,273 @@ proc define_addr_params {drv_handle file_name} {
    close $file_handle
 }
 
+proc define_addr_params_versal_net {drv_handle file_name} {
+   global file_handle
+   global sw_proc
+   global base_addr_list
+   global high_addr_list
+
+   set base_addr_list {0 0 0 0 0 0 0 0 0 0 0}
+   set high_addr_list {0 0 0 0 0 0 0 0 0 0 0}
+   # Open include file
+   set file_handle [::hsi::utils::open_include_file $file_name]
+   set sw_proc_handle [hsi::get_sw_processor]
+   set hw_proc_handle [hsi::get_cells -hier [common::get_property HW_INSTANCE $sw_proc_handle] ]
+   set proctype [common::get_property IP_NAME $hw_proc_handle]
+
+   #Flags to check if specific DDR region is present in design
+   set is_ddr_low_0 0
+   set is_ddr_low_1 0
+   set is_ddr_low_2 0
+   set is_ddr_low_3 0
+   set is_ddr_ch_1  0
+   set is_ddr_ch_1a 0
+   set is_ddr_ch_2  0
+   set is_ddr_ch_2a 0
+   set is_ddr_ch_3  0
+   set is_ddr_ch_3a 0
+   set is_ddr_ch_4  0
+
+   set sw_proc [hsi::get_sw_processor]
+   set periphs [::hsi::utils::get_common_driver_ips $drv_handle]
+
+   foreach periph $periphs {
+	set ipname [string toupper [common::get_property NAME $periph]]
+	puts $file_handle ""
+	puts $file_handle "/* Definitions for peripheral $ipname */"
+	set interface_block_names [get_property ADDRESS_BLOCK [get_mem_ranges \
+                -of_objects [hsi::get_cells -hier $sw_proc] $periph]]
+
+	set i 0
+	foreach block_name $interface_block_names {
+
+		if {[string match "C*_DDR_CH0_LEGACY*" $block_name]} {
+			#
+			# ddr_region_id specifies index of base_addr_list/high_addr_list
+			# for this DDR region
+			#
+			set ddr_region_id 0
+			xddrpsv_handle_address_details $i $periph $block_name $is_ddr_low_0 $ddr_region_id
+			set is_ddr_low_0 1
+
+		} elseif {[string match "C*_DDR_CH0_MED*" $block_name]} {
+					#
+			# ddr_region_id specifies index of base_addr_list/high_addr_list
+			# for this DDR region
+			#
+			set ddr_region_id 1
+			xddrpsv_handle_address_details $i $periph $block_name $is_ddr_low_1 $ddr_region_id
+			set is_ddr_low_1 1
+
+		} elseif {[string match "C*_DDR_CH0_HIGH0*" $block_name]} {
+			#
+			# ddr_region_id specifies index of base_addr_list/high_addr_list
+			# for this DDR region
+			#
+			set ddr_region_id 2
+			xddrpsv_handle_address_details $i $periph $block_name $is_ddr_low_2 $ddr_region_id
+
+			set is_ddr_low_2 1
+
+		} elseif {[string match "C*_DDR_CH0_HIGH1*" $block_name]} {
+			#
+			# ddr_region_id specifies index of base_addr_list/high_addr_list
+			# for this DDR region
+			#
+			set ddr_region_id 3
+			xddrpsv_handle_address_details $i $periph $block_name $is_ddr_low_3 $ddr_region_id
+
+			set is_ddr_low_3 1
+		} elseif {[string match "C*_DDR_CH1*" $block_name]} {
+                        #
+                        # ddr_region_id specifies index of base_addr_list/high_addr_list
+                        # for this DDR region
+                        #
+                        set ddr_region_id 4
+                        xddrpsv_handle_address_details $i $periph $block_name $is_ddr_ch_1 $ddr_region_id
+
+                        set is_ddr_ch_1 1
+                } elseif {[string match "C*_DDR_CH1A*" $block_name]} {
+                        #
+                        # ddr_region_id specifies index of base_addr_list/high_addr_list
+                        # for this DDR region
+                        #
+                        set ddr_region_id 5
+                        xddrpsv_handle_address_details $i $periph $block_name $is_ddr_ch_1a $ddr_region_id
+
+                        set is_ddr_ch_1a 1
+                } elseif {[string match "C*_DDR_CH2*" $block_name]} {
+                        #
+                        # ddr_region_id specifies index of base_addr_list/high_addr_list
+                        # for this DDR region
+                        #
+                        set ddr_region_id 6
+                        xddrpsv_handle_address_details $i $periph $block_name $is_ddr_ch_2  $ddr_region_id
+
+                        set is_ddr_ch_2 1
+
+		} elseif {[string match "C*_DDR_CH2A*" $block_name]} {
+			#
+			# ddr_region_id specifies index of base_addr_list/high_addr_list
+			# for this DDR region
+			#
+			set ddr_region_id 7
+			xddrpsv_handle_address_details $i $periph $block_name $is_ddr_ch_2a $ddr_region_id
+
+			set is_ddr_ch_2a 1
+		} elseif {[string match "C*_DDR_CH3*" $block_name]} {
+			#
+			# ddr_region_id specifies index of base_addr_list/high_addr_list
+			# for this DDR region
+			#
+			set ddr_region_id 8
+			xddrpsv_handle_address_details $i $periph $block_name $is_ddr_ch_3 5
+
+			set is_ddr_ch_3 1
+		} elseif {[string match "C*_DDR_CH3A*" $block_name]} {
+			#
+			# ddr_region_id specifies index of base_addr_list/high_addr_list
+			# for this DDR region
+			#
+			set ddr_region_id 9
+			xddrpsv_handle_address_details $i $periph $block_name $is_ddr_ch_3a  $ddr_region_id
+
+			set is_ddr_ch_3a 1
+		} elseif {[string match "C*_DDR_CH4*" $block_name]} {
+                        #
+                        # ddr_region_id specifies index of base_addr_list/high_addr_list
+                        # for this DDR region
+                        #
+                        set ddr_region_id 10
+                        xddrpsv_handle_address_details $i $periph $block_name $is_ddr_ch_4  $ddr_region_id
+
+                        set is_ddr_ch_4 1
+                }
+		incr i
+	}
+
+   }
+
+	puts $file_handle ""
+	puts $file_handle "/* Canonicals definitions for NOC2 DDR to be consumed by MMU/MPU tables in BSP*/"
+	if {$is_ddr_low_0 == 1} {
+		set ddr_region_id 0
+		set value  [lindex $base_addr_list $ddr_region_id]
+		puts $file_handle "#define XPAR_AXI_NOC2_DDR_LOW_0_BASEADDR $value"
+
+		set value  [lindex $high_addr_list $ddr_region_id]
+		puts $file_handle "#define XPAR_AXI_NOC2_DDR_LOW_0_HIGHADDR $value"
+		puts $file_handle ""
+
+	}
+
+	if {$is_ddr_low_1 == 1} {
+		set ddr_region_id 1
+		set value  [lindex $base_addr_list $ddr_region_id]
+		puts $file_handle "#define XPAR_AXI_NOC2_DDR_LOW_1_BASEADDR $value"
+
+		set value  [lindex $high_addr_list $ddr_region_id]
+		puts $file_handle "#define XPAR_AXI_NOC2_DDR_LOW_1_HIGHADDR $value"
+		puts $file_handle ""
+	}
+
+	if {$is_ddr_low_2 == 1} {
+		set ddr_region_id 2
+		set value  [lindex $base_addr_list $ddr_region_id]
+		puts $file_handle "#define XPAR_AXI_NOC2_DDR_LOW_2_BASEADDR $value"
+
+		set value  [lindex $high_addr_list $ddr_region_id]
+		puts $file_handle "#define XPAR_AXI_NOC2_DDR_LOW_2_HIGHADDR $value"
+		puts $file_handle ""
+	}
+
+	if {$is_ddr_low_3 == 1} {
+		set ddr_region_id 3
+		set value  [lindex $base_addr_list $ddr_region_id]
+		puts $file_handle "#define XPAR_AXI_NOC2_DDR_LOW_3_BASEADDR $value"
+
+		set value  [lindex $high_addr_list $ddr_region_id]
+		puts $file_handle "#define XPAR_AXI_NOC2_DDR_LOW_3_HIGHADDR $value"
+		puts $file_handle ""
+	}
+
+	if {$is_ddr_ch_1 == 1} {
+		set ddr_region_id 4
+		set value  [lindex $base_addr_list $ddr_region_id]
+		puts $file_handle ""
+		puts $file_handle "#define XPAR_AXI_NOC2_DDR_CH_1_BASEADDR $value"
+
+		set value  [lindex $high_addr_list $ddr_region_id]
+		puts $file_handle "#define XPAR_AXI_NOC2_DDR_CH_1_HIGHADDR $value"
+		puts $file_handle ""
+	}
+
+	if {$is_ddr_ch_1a == 1} {
+		set ddr_region_id 5
+		set value  [lindex $base_addr_list $ddr_region_id]
+		puts $file_handle "#define XPAR_AXI_NOC2_DDR_CH_1A_BASEADDR $value"
+
+		set value  [lindex $high_addr_list $ddr_region_id]
+		puts $file_handle "#define XPAR_AXI_NOC2_DDR_CH_1A_HIGHADDR $value"
+		puts $file_handle ""
+	}
+
+	if {$is_ddr_ch_2 == 1} {
+                set ddr_region_id 6
+                set value  [lindex $base_addr_list $ddr_region_id]
+                puts $file_handle "#define XPAR_AXI_NOC2_DDR_CH_2_BASEADDR $value"
+
+                set value  [lindex $high_addr_list $ddr_region_id]
+                puts $file_handle "#define XPAR_AXI_NOC2_DDR_CH_2_HIGHADDR $value"
+                puts $file_handle ""
+        }
+
+	if {$is_ddr_ch_2a == 1} {
+		set ddr_region_id 7
+		set value  [lindex $base_addr_list $ddr_region_id]
+		puts $file_handle "#define XPAR_AXI_NOC2_DDR_CH_2A_BASEADDR $value"
+
+		set value  [lindex $high_addr_list $ddr_region_id]
+		puts $file_handle "#define XPAR_AXI_NOC2_DDR_CH_2A_HIGHADDR $value"
+		puts $file_handle ""
+	}
+
+	if {$is_ddr_ch_3 == 1} {
+                set ddr_region_id 8
+                set value  [lindex $base_addr_list $ddr_region_id]
+                puts $file_handle ""
+                puts $file_handle "#define XPAR_AXI_NOC2_DDR_CH_3_BASEADDR $value"
+
+                set value  [lindex $high_addr_list $ddr_region_id]
+                puts $file_handle "#define XPAR_AXI_NOC2_DDR_CH_3_HIGHADDR $value"
+                puts $file_handle ""
+        }
+
+        if {$is_ddr_ch_3a == 1} {
+                set ddr_region_id 9
+                set value  [lindex $base_addr_list $ddr_region_id]
+                puts $file_handle "#define XPAR_AXI_NOC2_DDR_CH_3A_BASEADDR $value"
+
+                set value  [lindex $high_addr_list $ddr_region_id]
+                puts $file_handle "#define XPAR_AXI_NOC2_DDR_CH_3A_HIGHADDR $value"
+                puts $file_handle ""
+        }
+
+        if {$is_ddr_ch_4 == 1} {
+                set ddr_region_id 10
+                set value  [lindex $base_addr_list $ddr_region_id]
+                puts $file_handle "#define XPAR_AXI_NOC2_DDR_CH_4_BASEADDR $value"
+
+                set value  [lindex $high_addr_list $ddr_region_id]
+                puts $file_handle "#define XPAR_AXI_NOC2_DDR_CH_4_HIGHADDR $value"
+                puts $file_handle ""
+        }
+
+
+   puts $file_handle "\n/******************************************************************/\n"
+   close $file_handle
+}
+
 proc xddrpsv_get_base_addr { sw_proc periph index } {
 	return [common::get_property BASE_VALUE [lindex [get_mem_ranges \
 					-of_objects [hsi::get_cells -hier $sw_proc] $periph] $index]]
@@ -312,7 +596,8 @@ proc xddrpsv_generate_defines { periph block_name master_interface value is_base
 	global sw_proc
 
 	if { $is_baseaddr == 1 } {
-		if {$value == 0 && ($sw_proc == "psu_cortexr5" || $sw_proc == "psv_cortexr5")} {
+		if {$value == 0 && ($sw_proc == "psu_cortexr5" || $sw_proc == "psv_cortexr5" \
+			|| $sw_proc =="psxl_cortexr52" || $sw_proc =="psx_cortexr52")} {
 			puts $file_handle [format "#define XPAR_%s_%s_$s_BASEADDR 0x00100000" \
 				[string toupper $periph] [string toupper $block_name] [string toupper $master_interface]]
 		} else {
