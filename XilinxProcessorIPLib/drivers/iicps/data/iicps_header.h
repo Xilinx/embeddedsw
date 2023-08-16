60c28b3ac98d2e894eb24c4d58f379c7b3876943
Siva Addepalli <siva.addepalli@amd.com>
2023-08-16 19:33:47 +0530

    Revert "iicps: Add support for peripheral test for iicps in SDT flow"
    This reverts commit c7ac57580c2f7d0046f5af8e10f5dce33630c771.

--- XilinxProcessorIPLib/drivers/iicps/data/iicps_header.h
+++ XilinxProcessorIPLib/drivers/iicps/data/iicps_header.h
@@ -1,6 +1,5 @@
 /******************************************************************************
 * Copyright (C) 2011 - 2021 Xilinx, Inc.  All rights reserved.
-* Copyright (C) 2022 - 2023 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -11,10 +10,6 @@
 #include "xil_assert.h"
 #include "xstatus.h"
 
-#ifndef SDT
 int IicPsSelfTestExample(u16 DeviceId);
-#else
-int IicPsSelfTestExample(UINTPTR BaseAddress);
-#endif
 
 #endif
