41fdb414161d0cd0cdf29fec8ab244a3e41631c9
Siva Addepalli <siva.addepalli@amd.com>
2023-08-16 19:33:10 +0530

    Revert "uartps: Add support for peripheral tests in SDT flow"
    This reverts commit 0864ce37a59210ea6ee8737bae301c0d77554de0.

--- XilinxProcessorIPLib/drivers/uartps/data/uartps_header.h
+++ XilinxProcessorIPLib/drivers/uartps/data/uartps_header.h
@@ -1,6 +1,5 @@
 /******************************************************************************
 * Copyright (C) 2011 - 2021 Xilinx, Inc.  All rights reserved.
-* Copyright (C) 2022 - 2023 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -12,18 +11,6 @@
 #include "xil_assert.h"
 #include "xstatus.h"
 
-#ifndef SDT
-#ifdef XPAR_INTC_0_DEVICE_ID
-int UartPsIntrExample(XIntc *IntcInstPtr, XUartPs *UartInstPtr,
-			u16 DeviceId, u16 UartIntrId);
-#else
-int UartPsIntrExample(XScuGic *IntcInstPtr, XUartPs *UartInstPtr,
-			u16 DeviceId, u16 UartIntrId);
-#endif
 int UartPsPolledExample(u16 DeviceId);
-#else
-int UartPsPolledExample(XUartPs *UartInstPtr, UINTPTR BaseAddress);
-int UartPsIntrExample(XUartPs *UartInstPtr, UINTPTR BaseAddress);
-#endif
 
 #endif
