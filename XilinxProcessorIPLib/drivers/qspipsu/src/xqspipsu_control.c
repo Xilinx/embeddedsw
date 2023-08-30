4fb723765008d80bc7de83b5ae22a9f72b1afe93
sboyapal <srikanth.boyapally@amd.com>
2023-08-29 17:38:29 -1200

    qspipsu: src: Fix code format issues with checkpatch tool
    Fix code formatting issues with checkpatch tool.
    It fixes the indentation, formats the code as per coding standards.
    
    Signed-off-by: Srikanth Boyapally <srikanth.boyapally@amd.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@amd.com>

--- XilinxProcessorIPLib/drivers/qspipsu/src/xqspipsu_control.c
+++ XilinxProcessorIPLib/drivers/qspipsu/src/xqspipsu_control.c
@@ -91,11 +91,11 @@ void XQspiPsu_GenFifoEntryData(XQspiPsu *InstancePtr, XQspiPsu_Msg *Msg)
 	}
 	/* If Byte Count is less than 8 bytes do the transfer in IO mode */
 	if ((Msg->ByteCount < 8U) &&
-		(InstancePtr->ReadMode == XQSPIPSU_READMODE_DMA)) {
+	    (InstancePtr->ReadMode == XQSPIPSU_READMODE_DMA)) {
 		InstancePtr->ReadMode = XQSPIPSU_READMODE_IO;
 		XQspiPsu_WriteReg(InstancePtr->Config.BaseAddress, XQSPIPSU_CFG_OFFSET,
-			(XQspiPsu_ReadReg(InstancePtr->Config.BaseAddress, XQSPIPSU_CFG_OFFSET) &
-					~XQSPIPSU_CFG_MODE_EN_MASK));
+				  (XQspiPsu_ReadReg(InstancePtr->Config.BaseAddress, XQSPIPSU_CFG_OFFSET) &
+				   ~XQSPIPSU_CFG_MODE_EN_MASK));
 		InstancePtr->IsUnaligned = 1;
 	}
 
@@ -105,7 +105,7 @@ void XQspiPsu_GenFifoEntryData(XQspiPsu *InstancePtr, XQspiPsu_Msg *Msg)
 
 	/* One dummy GenFifo entry in case of IO mode */
 	if ((InstancePtr->ReadMode == XQSPIPSU_READMODE_IO) &&
-			((Msg->Flags & XQSPIPSU_MSG_FLAG_RX) != (u32)FALSE)) {
+	    ((Msg->Flags & XQSPIPSU_MSG_FLAG_RX) != (u32)FALSE)) {
 		GenFifoEntry = 0x0U;
 #ifdef DEBUG
 		xil_printf("\nDummy FifoEntry=%08x\r\n", GenFifoEntry);
@@ -144,9 +144,9 @@ void XQspiPsu_PollDataConfig(XQspiPsu *InstancePtr, XQspiPsu_Msg *FlashMsg)
 
 	Value = XQspiPsu_CreatePollDataConfig(InstancePtr, FlashMsg);
 	XQspiPsu_WriteReg(InstancePtr->Config.BaseAddress,
-			XQSPIPSU_POLL_CFG_OFFSET, Value);
+			  XQSPIPSU_POLL_CFG_OFFSET, Value);
 	XQspiPsu_WriteReg(InstancePtr->Config.BaseAddress,
-			XQSPIPSU_P_TO_OFFSET, FlashMsg->PollTimeout);
+			  XQSPIPSU_P_TO_OFFSET, FlashMsg->PollTimeout);
 
 	XQspiPsu_GenFifoEntryCSAssert(InstancePtr);
 
@@ -158,7 +158,7 @@ void XQspiPsu_PollDataConfig(XQspiPsu *InstancePtr, XQspiPsu_Msg *FlashMsg)
 	GenFifoEntry |= (u32)FlashMsg->PollStatusCmd;
 
 	XQspiPsu_WriteReg(InstancePtr->Config.BaseAddress,
-		XQSPIPSU_GEN_FIFO_OFFSET, GenFifoEntry);
+			  XQSPIPSU_GEN_FIFO_OFFSET, GenFifoEntry);
 
 	GenFifoEntry = (u32)0;
 	GenFifoEntry |= (u32)XQSPIPSU_GENFIFO_POLL;
@@ -172,32 +172,32 @@ void XQspiPsu_PollDataConfig(XQspiPsu *InstancePtr, XQspiPsu_Msg *FlashMsg)
 		GenFifoEntry &= ~XQSPIPSU_GENFIFO_STRIPE;
 	}
 	XQspiPsu_WriteReg(InstancePtr->Config.BaseAddress, XQSPIPSU_GEN_FIFO_OFFSET,
-			GenFifoEntry);
+			  GenFifoEntry);
 
 	/* One Dummy entry required for IO mode */
 	GenFifoEntry = 0x0U;
 	XQspiPsu_WriteReg(InstancePtr->Config.BaseAddress, XQSPIPSU_GEN_FIFO_OFFSET,
-			GenFifoEntry);
+			  GenFifoEntry);
 
 	InstancePtr->Msg = FlashMsg;
 	InstancePtr->NumMsg = (s32)1;
 	InstancePtr->MsgCnt = 0;
 
 	Value = XQspiPsu_ReadReg(InstancePtr->Config.BaseAddress,
-			XQSPIPSU_CFG_OFFSET);
+				 XQSPIPSU_CFG_OFFSET);
 	Value &= ~XQSPIPSU_CFG_MODE_EN_MASK;
 	Value |= (XQSPIPSU_CFG_START_GEN_FIFO_MASK |
-			XQSPIPSU_CFG_GEN_FIFO_START_MODE_MASK |
-			XQSPIPSU_CFG_EN_POLL_TO_MASK);
+		  XQSPIPSU_CFG_GEN_FIFO_START_MODE_MASK |
+		  XQSPIPSU_CFG_EN_POLL_TO_MASK);
 	XQspiPsu_WriteReg(InstancePtr->Config.BaseAddress, XQSPIPSU_CFG_OFFSET,
-			Value);
+			  Value);
 
 	/* Enable interrupts */
 	Value = ((u32)XQSPIPSU_IER_RXNEMPTY_MASK |
-		(u32)XQSPIPSU_IER_POLL_TIME_EXPIRE_MASK);
+		 (u32)XQSPIPSU_IER_POLL_TIME_EXPIRE_MASK);
 
 	XQspiPsu_WriteReg(InstancePtr->Config.BaseAddress, XQSPIPSU_IER_OFFSET,
-			Value);
+			  Value);
 
 }
 
@@ -240,9 +240,9 @@ s32 XQspipsu_Calculate_Tapdelay(const XQspiPsu *InstancePtr, u8 Prescaler)
 		goto END;
 	} else {
 
-		Divider = (u32)1U << (Prescaler+1U);
+		Divider = (u32)1U << (Prescaler + 1U);
 
-		FreqDiv = (InstancePtr->Config.InputClockHz)/Divider;
+		FreqDiv = (InstancePtr->Config.InputClockHz) / Divider;
 
 #if defined (versal)
 		if (FreqDiv <= XQSPIPSU_FREQ_37_5MHZ) {
@@ -250,23 +250,23 @@ s32 XQspipsu_Calculate_Tapdelay(const XQspiPsu *InstancePtr, u8 Prescaler)
 		if (FreqDiv <= XQSPIPSU_FREQ_40MHZ) {
 #endif
 			Tapdelay |= (TAPDLY_BYPASS_VALVE_40MHZ <<
-					 IOU_TAPDLY_BYPASS_LQSPI_RX_SHIFT);
+				     IOU_TAPDLY_BYPASS_LQSPI_RX_SHIFT);
 		} else if (FreqDiv <= XQSPIPSU_FREQ_100MHZ) {
 			Tapdelay |= (TAPDLY_BYPASS_VALVE_100MHZ <<
-					 IOU_TAPDLY_BYPASS_LQSPI_RX_SHIFT);
+				     IOU_TAPDLY_BYPASS_LQSPI_RX_SHIFT);
 			LBkModeReg |= (USE_DLY_LPBK << XQSPIPSU_LPBK_DLY_ADJ_USE_LPBK_SHIFT);
 #if defined (versal)
 			delayReg |= (u32)USE_DATA_DLY_ADJ  <<
-					XQSPIPSU_DATA_DLY_ADJ_USE_DATA_DLY_SHIFT;
+				    XQSPIPSU_DATA_DLY_ADJ_USE_DATA_DLY_SHIFT;
 #else
 			delayReg |= ((u32)USE_DATA_DLY_ADJ  <<
-					XQSPIPSU_DATA_DLY_ADJ_USE_DATA_DLY_SHIFT) |
-							((u32)DATA_DLY_ADJ_DLY  << XQSPIPSU_DATA_DLY_ADJ_DLY_SHIFT);
+				     XQSPIPSU_DATA_DLY_ADJ_USE_DATA_DLY_SHIFT) |
+				    ((u32)DATA_DLY_ADJ_DLY  << XQSPIPSU_DATA_DLY_ADJ_DLY_SHIFT);
 #endif
 		} else if (FreqDiv <= XQSPIPSU_FREQ_150MHZ) {
 #if defined (versal)
 			LBkModeReg |= (USE_DLY_LPBK  << XQSPIPSU_LPBK_DLY_ADJ_USE_LPBK_SHIFT) |
-					(LPBK_DLY_ADJ_DLY1 << XQSPIPSU_LPBK_DLY_ADJ_DLY1_SHIFT);
+				      (LPBK_DLY_ADJ_DLY1 << XQSPIPSU_LPBK_DLY_ADJ_DLY1_SHIFT);
 #else
 			LBkModeReg |= USE_DLY_LPBK  << XQSPIPSU_LPBK_DLY_ADJ_USE_LPBK_SHIFT;
 #endif
@@ -278,7 +278,7 @@ s32 XQspipsu_Calculate_Tapdelay(const XQspiPsu *InstancePtr, u8 Prescaler)
 		Status =  XQspipsu_Set_TapDelay(InstancePtr, Tapdelay, LBkModeReg, delayReg);
 	}
 
-	END:
+END:
 	return Status;
 }
 #endif
@@ -301,7 +301,7 @@ s32 XQspipsu_Calculate_Tapdelay(const XQspiPsu *InstancePtr, u8 Prescaler)
  *
  ******************************************************************************/
 s32 XQspiPsu_PolledMessageTransfer(XQspiPsu *InstancePtr, XQspiPsu_Msg *Msg,
-			     u32 NumMsg)
+				   u32 NumMsg)
 {
 	s32 Index;
 	u32 QspiPsuStatusReg;
@@ -373,7 +373,7 @@ END:
  *
 ******************************************************************************/
 void XQspiPsu_IntrDataTransfer(XQspiPsu *InstancePtr,
-			   u32 *QspiPsuStatusReg, u8 *DeltaMsgCnt)
+			       u32 *QspiPsuStatusReg, u8 *DeltaMsgCnt)
 {
 	u32 DmaIntrStatusReg = 0;
 	XQspiPsu_Msg *Msg = InstancePtr->Msg;;
@@ -422,7 +422,7 @@ void XQspiPsu_IntrDataTransfer(XQspiPsu *InstancePtr,
  *
 ******************************************************************************/
 void XQspiPsu_IntrDummyDataTransfer(XQspiPsu *InstancePtr, u32 QspiPsuStatusReg,
-				u8 DeltaMsgCnt)
+				    u8 DeltaMsgCnt)
 {
 	XQspiPsu_Msg *Msg = InstancePtr->Msg;
 	s32 NumMsg = InstancePtr->NumMsg;
