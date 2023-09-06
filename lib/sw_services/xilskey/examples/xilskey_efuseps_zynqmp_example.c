b687a40f7a23b57414ddaac6aff5a62ebc6262f6
Nandanikhil Gajulapally <Nandanikhil.Gajulapally@amd.com>
2023-09-05 22:25:31 -1200

    xilskey: added SDT support for examples
    Added SDT support for xilskey examples.
    
    Signed-off-by: Nandanikhil Gajulapally <nandanikhil.gajulapally@amd.com>
    Acked-by: VNSL Durga Challa <vnsl.durga.challa@xilinx.com>

--- lib/sw_services/xilskey/examples/xilskey_efuseps_zynqmp_example.c
+++ lib/sw_services/xilskey/examples/xilskey_efuseps_zynqmp_example.c
@@ -54,6 +54,7 @@
 *       kpt      05/21/21 Added print before programming PPK hash into non-zero
 *                         PPK efuses
 * 7.5   ng       07/13/23 Added support for system device tree flow
+*       ng       08/31/23 removed redundant header file inclusion
 *
 * </pre>
 *
@@ -62,10 +63,6 @@
 /***************************** Include Files *********************************/
 #include "xilskey_efuseps_zynqmp_input.h"
 #include "xil_printf.h"
-
-#ifdef SDT
-#include "xilskey_config.h"
-#endif
 
 /***************** Macros (Inline Functions) Definitions *********************/
 #define XSK_EFUSEPS_AES_KEY_STRING_LEN			(64)
