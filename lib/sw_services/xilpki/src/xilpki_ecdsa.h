eecdde7dc1055cd925e9e11d0a90833a645643fb
Nava kishore Manne <nava.kishore.manne@amd.com>
2023-09-11 18:24:42 -1200

    sw_services:xilpki:Fixed doxygen warnings
    Resolves the doxygen warnings of the Xilpki library.
    
    Signed-off-by: Nava kishore Manne <nava.kishore.manne@amd.com>
    Acked-by: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>

--- lib/sw_services/xilpki/src/xilpki_ecdsa.h
+++ lib/sw_services/xilpki/src/xilpki_ecdsa.h
@@ -7,7 +7,7 @@
 /**
  *
  * @file xilpki_ecdsa.h
- * @addtogroup xilpki Overview
+ * @addtogroup xilpki-api XilPKI Library APIs
  *
  * @{
  * @details
@@ -19,10 +19,10 @@
  * ----- ----  --------  -------------------------------------------------------
  * 1.0   Nava  12/05/22  Initial Release
  * 2.0   Nava  06/21/23  Added PKI multi-queue support for ECC operations.
+ * 2.0   Nava  09/11/23  Fixed doxygen warnings.
  *
  * </pre>
  *
- * @note
  *
  ******************************************************************************/
 #ifndef XILPKI_ECDSA_H
@@ -36,84 +36,93 @@ extern "C" {
 #include "xilpki.h"
 
 /************************** Constant Definitions *****************************/
-#define NIST_P192_LEN_BYTES	24U /* Bytes */
-#define NIST_P384_LEN_BYTES	48U /* Bytes */
-#define NIST_P256_LEN_BYTES	32U /* Bytes */
-#define NIST_P521_LEN_BYTES	66U /* Bytes */
+#define NIST_P192_LEN_BYTES	24U /**< Bytes */
+#define NIST_P384_LEN_BYTES	48U /**< Bytes */
+#define NIST_P256_LEN_BYTES	32U /**< Bytes */
+#define NIST_P521_LEN_BYTES	66U /**< Bytes */
 
-#define XPKI_ECDSA_NON_SUPPORTED_CRV		0x1U
-#define XPKI_ECDSA_INVALID_PARAM		0x2U
-#define XPKI_ECDSA_SIGN_GEN_ERR			0x3U
-#define XPKI_ECDSA_SIGN_VERIFY_ERR		0x4U
-#define XPKI_ECDSA_POINT_MULT_ERR		0x5U
-#define XPKI_MOD_ADD_ERR			0x6U
-#define XPKI_DONE_STATUS_ERR			0x7U
+/*
+@cond internal
+*/
+#define XPKI_ECDSA_NON_SUPPORTED_CRV		0x1U	/**< Unsupported curve */
+#define XPKI_ECDSA_INVALID_PARAM		0x2U	/**< Invalid parameters */
+#define XPKI_ECDSA_SIGN_GEN_ERR			0x3U	/**< Ecdsa signature gneration error */
+#define XPKI_ECDSA_SIGN_VERIFY_ERR		0x4U	/**< Ecdsa signature verification error */
+#define XPKI_ECDSA_POINT_MULT_ERR		0x5U	/**< Ecc point multiplication error */
+#define XPKI_MOD_ADD_ERR			0x6U	/**< Modulus Addtion error */
+#define XPKI_DONE_STATUS_ERR			0x7U	/**< PKI done status error */
+/** @endcond*/
 /**************************** Type Definitions *******************************/
 
 typedef enum {
-	ECC_NIST_P192 = 1,
-	ECC_NIST_P256,
-	ECC_NIST_P384,
-	ECC_NIST_P521
+	ECC_NIST_P192 = 1,	/**< NIST P-192 curve */
+	ECC_NIST_P256,	/**< NIST P-256 curve */
+	ECC_NIST_P384,	/**< NIST P-384 curve */
+	ECC_NIST_P521	/**< NIST P-251 curve */
 } XPki_EcdsaCrvType;
 
 typedef enum {
-	ECC_BINARY = 1,
-	ECC_PRIME
+	ECC_BINARY = 1,		/**< ECC Binary curve */
+	ECC_PRIME		/**< ECC Prime curve */
 } XPki_EcdsaCrv;
 
 typedef struct {
-	XPki_EcdsaCrvType CrvType;
-	XPki_EcdsaCrv Crv;
+	XPki_EcdsaCrvType CrvType;	/**< Curve type(NIST-P192, P256, P384, and P521) */
+	XPki_EcdsaCrv Crv;	/**< Type of ECC curve class either prime or binary class */
 } XPki_EcdsaCrvInfo;
 
 typedef struct {
-	const u8 *Qx;	/* Public key curve point x */
-	const u8 *Qy;	/* Public key curve point y */
+	const u8 *Qx;	/**< ECC Public key curve point x */
+	const u8 *Qy;	/**< ECC Public key curve point y */
 } XPki_EcdsaKey;
 
 typedef struct {
-	u8 *SignR;	/* The signature component R */
-	u8 *SignS;	/* The signature component S */
+	u8 *SignR;	/**< The signature component R */
+	u8 *SignS;	/**< The signature component S */
 } XPki_EcdsaSign;
 
 typedef struct {
-	const u8 *Gx;	/* Generator curve point x */
-	const u8 *Gy;	/* Generator curve point y */
-	u8 Gxlen;
-	u8 Gylen;
+	const u8 *Gx;	/**< ECC Curve Generator point x */
+	const u8 *Gy;	/**< ECC Curve Generator curve point y */
+	u8 Gxlen;    /**< Length of Generator point x */
+	u8 Gylen;     /**< Length of Generator point y */
 } XPki_EcdsaGpoint;
 
 typedef struct {
-	XPki_EcdsaCrvType CrvType;
-	u8 Hashlen;
-	u8 Dlen;
-	u8 Klen;
-	const u8 *Hash;
-	const u8 *D;
-	const u8 *K;
+	XPki_EcdsaCrvType CrvType; /**< ECC Curve type */
+	u8 Hashlen;		 /**< Data hash length */
+	u8 Dlen;		/**< ECC Private key length */
+	u8 Klen;		/**< ECC ephemeral key lenth */
+	const u8 *Hash;		/**< Pointer to the data hash */
+	const u8 *D;		/**< Pointer to the private key */
+	const u8 *K;		/**< Pointer to the ephemeral key */
 } XPki_EcdsaSignInputData;
 
 typedef struct {
-	XPki_EcdsaCrvType CrvType;
-	XPki_EcdsaKey PubKey;
-	XPki_EcdsaSign Sign;
-	u8 Hashlen;
-	const u8 *Hash;
+	XPki_EcdsaCrvType CrvType;	/**< ECC Curve type */
+	XPki_EcdsaKey PubKey;		/**< Structure to the ECC public key */
+	XPki_EcdsaSign Sign;		/**< Structure to the ECC signature */
+	u8 Hashlen;			/**< Data hash length */
+	const u8 *Hash;			/**< Pointer to the Data Hash */
 } XPki_EcdsaVerifyInputData;
 
 typedef struct {
-	XPki_EcdsaCrvType CrvType;
-	XPki_EcdsaGpoint Gpoint;
-	u8 Dlen;
-	const u8 *D;
+	XPki_EcdsaCrvType CrvType;	/**< ECC Curve type */
+	XPki_EcdsaGpoint Gpoint;	/**< Structure to the ECC genetator points */
+	u8 Dlen;			/**< ECC Private key length */
+	const u8 *D;			/**< Pointer to the private key */
 } XPki_EcdsaPointMultiInputData;
 
 typedef struct {
-	XPki_EcdsaCrvType CrvType;
-	const u8 *Order;
-	u8 Dlen;
-	const u8 *D;
+	XPki_EcdsaCrvType CrvType;	/**< ECC Curve type */
+	const u8 *Order;		/**< Pointer to the ECC Curve Order */
+	u8 Dlen;			/**< ECC Private key length */
+	const u8 *D;			/**< Pointer to the private key */
 } XPki_ModAddInputData;
 
+#ifdef __cplusplus
+}
+#endif
+
 #endif  /* XILPKI_ECDSA_H */
+/**@}*/
