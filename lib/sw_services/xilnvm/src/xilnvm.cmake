11c9ac402514f3d493c9c6e340d0bdadddb070b8
Nandanikhil Gajulapally <Nandanikhil.Gajulapally@amd.com>
2023-09-27 16:39:57 -1200

    xilnvm: changed BSP options visibility
    - changed the xilnvm mode bsp option visibility based on the processor
    selected.
    - fixed the cmake logic to add the files based on the xilnvm mode.
    - removed the fatal error message, since it is not required, because of
    the bsp options visibility changes.
    
    Signed-off-by: Nandanikhil Gajulapally <nandanikhil.gajulapally@amd.com>
    
    Acked-for-series: VNSL Durga Challa <vnsl.durga.challa@xilinx.com>

--- lib/sw_services/xilnvm/src/xilnvm.cmake
+++ lib/sw_services/xilnvm/src/xilnvm.cmake
@@ -1,19 +1,25 @@
 # Copyright (C) 2023 Advanced Micro Devices, Inc.  All rights reserved.
 # SPDX-License-Identifier: MIT
 
+if("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "plm_microblaze")
+  set(XILNVM_mode "server")
+elseif(("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "microblaze") OR ("${CMAKE_MACHINE}" STREQUAL "VersalNet"))
+  # For soft microblaze and Versal_net APU/RPU cores, mode is client.
+  set(XILNVM_mode "client")
+else()
+  set(XILNVM_mode "client" CACHE STRING "Enables A72/R5 server and client mode support for XilNvm library for Versal")
+  set_property(CACHE XILNVM_mode PROPERTY STRINGS "client" "server")
+endif()
+
 option(XILNVM_use_puf_hd_as_user_efuse "Enables API's to use PUF Helper data efuses as user efuses." OFF)
 if(XILNVM_use_puf_hd_as_user_efuse)
   set(XNVM_ACCESS_PUF_USER_DATA " ")
 endif()
 
 option(XILNVM_cache_disable "Enables/Disables Cache for XilNvm client library." ON)
-if(XILNVM_cache_disable)
-  if(("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "cortexa72") OR
-     ("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "cortexr5") OR
-     ("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "cortexa78") OR
-     ("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "cortexr52") OR
-     ("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "microblaze"))
-     set(XNVM_CACHE_DISABLE " ")
+if(XILNVM_mode STREQUAL "client")
+  if(XILNVM_cache_disable)
+    set(XNVM_CACHE_DISABLE " ")
   endif()
 endif()
 
@@ -21,8 +27,5 @@ option(XILNVM_en_add_ppks "Enables or Disables additional PPKs" OFF)
 if (XILNVM_en_add_ppks OR ("${DEVICE_ID}" STREQUAL "xcvp1052"))
   set(PLM_EN_ADD_PPKS " ")
 endif()
-
-set(XILNVM_mode "client" CACHE STRING "Enables A72/R5 server and client mode support for XilNvm library for Versal")
-set_property(CACHE XILNVM_mode PROPERTY STRINGS "client" "server")
 
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/xilnvm_bsp_config.h.in ${CMAKE_BINARY_DIR}/include/xilnvm_bsp_config.h)
