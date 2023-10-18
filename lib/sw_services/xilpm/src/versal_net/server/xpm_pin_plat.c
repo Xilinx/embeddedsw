f22753b845f0d857cc1dabb0ca59769a7bed9889
Buddhabhatti, Jay <jay.buddhabhatti@amd.com>
2023-10-18 00:26:41 -1200

    xilpm: versal_common: server: move common pinctrl functions
    Move common pin control functions to versal_common directory.
    
    Signed-off-by: Jay Buddhabhatti <jay.buddhabhatti@amd.com>
    
    Acked-for-series: Sandeep Reddy Ghanapuram <sandeep.reddy-ghanapuram@amd.com>

--- lib/sw_services/xilpm/src/versal_net/server/xpm_pin_plat.c
+++ lib/sw_services/xilpm/src/versal_net/server/xpm_pin_plat.c
@@ -7,15 +7,7 @@
 #include "xil_util.h"
 #include "xpm_pin.h"
 
-static XPm_PinNode *PmMioPins[XPM_NODEIDX_STMIC_MAX];
-static u16 PmNumPins;
-
-struct PmPinGroup {
-	u16 GroupCount;
-	u16 *GroupList;
-};
-
-static struct PmPinGroup PmPinGroups[XPM_NODEIDX_STMIC_MAX] = {
+static XPm_PinGroup PmPinGroups[XPM_NODEIDX_STMIC_MAX] = {
 	[XPM_NODEIDX_STMIC_PMIO_0] = {
 		.GroupCount = 1,
 		.GroupList = ((u16 []) {
@@ -110,117 +102,16 @@ static struct PmPinGroup PmPinGroups[XPM_NODEIDX_STMIC_MAX] = {
 
 /****************************************************************************/
 /**
- * @brief  This function initializes the XPm_PinNode data staructure.
- *
- * @param Pin		XPm_PinNode data staructure.
- * @param PinId		PinNode ID.
- * @param BaseAddress	BaseAddress of the pin.
- *
- * @return XST_SUCCESS if successful else XST_FAILURE or an error code.
- *
- ****************************************************************************/
-XStatus XPmPin_Init(XPm_PinNode *Pin, u32 PinId, u32 BaseAddress)
-{
-	XStatus Status = XST_FAILURE;
-	u32 PinIdx;
-
-	PinIdx = NODEINDEX(PinId);
-	if ((PinIdx >= (u32)XPM_NODEIDX_STMIC_MAX) ||
-	    (PinIdx == (u32)XPM_NODEIDX_STMIC_MIN)) {
-		goto done;
-	}
-
-	XPmNode_Init(&Pin->Node, PinId, (u8)XPM_PINSTATE_UNUSED, BaseAddress);
-
-	Pin->Groups = PmPinGroups[PinIdx].GroupList;
-	Pin->NumGroups = (u8)(PmPinGroups[PinIdx].GroupCount);
-	Pin->PinFunc = NULL;
-	Pin->SubsysIdx = (u16)NODEINDEX(INVALID_SUBSYSID);
-
-	if (PinIdx <= PINS_PER_BANK) {
-		Pin->Bank = 0;
-	} else {
-		Pin->Bank =
-			(u8)(((PinIdx - PINS_PER_BANK - 1U) / PINS_PER_BANK) &
-			BITMASK(PIN_NODE_BANK_BIT_FIELD_SIZE));
-	}
-
-	Pin->BiasStatus = (u8)PINCTRL_BIAS_ENABLE;
-	Pin->PullCtrl = (u8)PINCTRL_BIAS_PULL_UP;
-	Pin->TriState = (u8)PINCTRL_TRI_STATE_ENABLE;
-
-	PmMioPins[PinIdx] = Pin;
-	PmNumPins++;
-	Status = XST_SUCCESS;
-
-done:
-	return Status;
-}
-
-/****************************************************************************/
-/**
- * @brief  This function returns instance to XPm_PinNode based on PinId.
- *
- * @param PinId		PinNode ID.
- *
- * @return Instance of XPm_PinNode if successful else NULL.
- *
- ****************************************************************************/
-XPm_PinNode *XPmPin_GetById(u32 PinId)
-{
-	XPm_PinNode *PinNode = NULL;
-	u32 PinIndex = NODEINDEX(PinId);
-
-	if ((u32)XPM_NODECLASS_STMIC != NODECLASS(PinId)) {
-		goto done;
-	} else if ((u32)XPM_NODESUBCL_PIN != NODESUBCLASS(PinId)) {
-		goto done;
-	} else if (((u32)XPM_NODETYPE_LPD_MIO != NODETYPE(PinId)) &&
-		   ((u32)XPM_NODETYPE_PMC_MIO != NODETYPE(PinId))) {
-		goto done;
-	} else if (PinIndex >= (u32)XPM_NODEIDX_STMIC_MAX) {
-		goto done;
-	} else {
-		/* Required by MISRA */
-	}
-
-	PinNode =  PmMioPins[PinIndex];
-
-done:
-	return PinNode;
-}
-
-/****************************************************************************/
-/**
- * @brief  This function returns total number of pins added.
- *
- * @param NumPins	Number of pins.
- *
- * @return XST_SUCCESS.
- *
- ****************************************************************************/
-XStatus XPmPin_GetNumPins(u32 *NumPins)
-{
-	*NumPins = PmNumPins;
-	return XST_SUCCESS;
-}
-
-/****************************************************************************/
-/**
- * @brief  Get requested pin node by node index
+ * @brief  Get requested pin group by node index
  *
  * @param PinIndex     Pin Index.
  *
- * @return Pointer to requested XPm_PinNode, NULL otherwise
+ * @return Pointer to requested XPm_PinGroup, NULL otherwise
  *
  * @note Requires only node index
  *
  ****************************************************************************/
-XPm_PinNode *XPmPin_GetByIndex(const u32 PinIndex)
+XPm_PinGroup *XPmPin_GetGroupByIdx(const u32 PinIndex)
 {
-	XPm_PinNode *Pin = NULL;
-
-	Pin = PmMioPins[PinIndex];
-
-	return Pin;
+	return &PmPinGroups[PinIndex];
 }
