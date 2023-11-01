e87e226bc23d699315512b10a21af50c978f9942
Gutti, Prasad <prasad.gutti@amd.com>
2023-10-31 22:16:24 -1200

    Deintercaler Driver: Deintercaler driver ported for decoupling flow
    1. Ported Deintercaler driver for decoupling flow, added new yaml file,
    Cmakelist.txt file.
    2. Incremented driver version.
    
    Signed-off-by: Prasad Gutti <prasad.gutti@amd.com>
    
    Acked-for-series:  Gaddipati, Naveen <naveen.gaddipati@amd.com>

--- XilinxProcessorIPLib/drivers/v_deinterlacer/src/xv_deinterlacer_sinit.c
+++ XilinxProcessorIPLib/drivers/v_deinterlacer/src/xv_deinterlacer_sinit.c
@@ -1,12 +1,15 @@
 // ==============================================================
 // Copyright (c) 2015 - 2020 Xilinx Inc. All rights reserved.
+// Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 // SPDX-License-Identifier: MIT
 // ==============================================================
 
 #ifndef __linux__
 
 #include "xstatus.h"
+#ifndef SDT
 #include "xparameters.h"
+#endif
 #include "xv_deinterlacer.h"
 
 #ifndef XPAR_XV_DEINTERLACER_NUM_INSTANCES
@@ -15,6 +18,7 @@
 
 extern XV_deinterlacer_Config XV_deinterlacer_ConfigTable[];
 
+#ifndef SDT
 XV_deinterlacer_Config *XV_deinterlacer_LookupConfig(u16 DeviceId) {
     XV_deinterlacer_Config *ConfigPtr = NULL;
 
@@ -45,5 +49,37 @@ int XV_deinterlacer_Initialize(XV_deinterlacer *InstancePtr, u16 DeviceId) {
                                          ConfigPtr,
                                          ConfigPtr->BaseAddress);
 }
+#else
+XV_deinterlacer_Config *XV_deinterlacer_LookupConfig(UINTPTR BaseAddress) {
+    XV_deinterlacer_Config *ConfigPtr = NULL;
 
+    int Index;
+
+    for (Index = (u32)0x0; XV_deinterlacer_ConfigTable[Index].Name != NULL; Index++) {
+		if ((XV_deinterlacer_ConfigTable[Index].BaseAddress == BaseAddress) ||
+		     !BaseAddress) {
+			ConfigPtr = &XV_deinterlacer_ConfigTable[Index];
+			break;
+		}
+	}
+
+    return ConfigPtr;
+}
+
+int XV_deinterlacer_Initialize(XV_deinterlacer *InstancePtr, UINTPTR BaseAddress) {
+    XV_deinterlacer_Config *ConfigPtr;
+
+    Xil_AssertNonvoid(InstancePtr != NULL);
+
+    ConfigPtr = XV_deinterlacer_LookupConfig(BaseAddress);
+    if (ConfigPtr == NULL) {
+        InstancePtr->IsReady = 0;
+        return (XST_DEVICE_NOT_FOUND);
+    }
+
+    return XV_deinterlacer_CfgInitialize(InstancePtr,
+                                         ConfigPtr,
+                                         ConfigPtr->BaseAddress);
+}
+#endif
 #endif
