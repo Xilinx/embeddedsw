d03d60bdbbb25f0e07978db048d6a4aedfa2c7b2
Gutti, Prasad <prasad.gutti@amd.com>
2023-10-31 22:16:25 -1200

    hcresampler Driver: hcresampler driver ported for decoupling flow
    1. Ported hcresampler driver for decoupling flow, added new yaml file,
    Cmakelist.txt file.
    2. Incremented driver version.
    
    Signed-off-by: Prasad Gutti <prasad.gutti@amd.com>
    
    Acked-for-series:  Gaddipati, Naveen <naveen.gaddipati@amd.com>

--- XilinxProcessorIPLib/drivers/v_hcresampler/src/xv_hcresampler_sinit.c
+++ XilinxProcessorIPLib/drivers/v_hcresampler/src/xv_hcresampler_sinit.c
@@ -1,12 +1,15 @@
 // ==============================================================
 // Copyright (c) 2015 - 2020 Xilinx Inc. All rights reserved.
+// Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
 // SPDX-License-Identifier: MIT
 // ==============================================================
 
 #ifndef __linux__
 
 #include "xstatus.h"
+#ifndef SDT
 #include "xparameters.h"
+#endif
 #include "xv_hcresampler.h"
 
 #ifndef XPAR_XV_HCRESAMPLER_NUM_INSTANCES
@@ -15,6 +18,7 @@
 
 extern XV_hcresampler_Config XV_hcresampler_ConfigTable[];
 
+#ifndef SDT
 XV_hcresampler_Config *XV_hcresampler_LookupConfig(u16 DeviceId) {
     XV_hcresampler_Config *ConfigPtr = NULL;
 
@@ -45,5 +49,37 @@ int XV_hcresampler_Initialize(XV_hcresampler *InstancePtr, u16 DeviceId) {
                                         ConfigPtr,
                                         ConfigPtr->BaseAddress);
 }
+#else
+XV_hcresampler_Config *XV_hcresampler_LookupConfig(UINTPTR BaseAddress) {
+    XV_hcresampler_Config *ConfigPtr = NULL;
 
+    int Index;
+
+    for (Index = (u32)0x0; XV_hcresampler_ConfigTable[Index].Name != NULL; Index++) {
+		if ((XV_hcresampler_ConfigTable[Index].BaseAddress == BaseAddress) ||
+		     !BaseAddress) {
+			ConfigPtr = &XV_hcresampler_ConfigTable[Index];
+			break;
+		}
+    }
+
+    return ConfigPtr;
+}
+
+int XV_hcresampler_Initialize(XV_hcresampler *InstancePtr, UINTPTR BaseAddress) {
+    XV_hcresampler_Config *ConfigPtr;
+
+    Xil_AssertNonvoid(InstancePtr != NULL);
+
+    ConfigPtr = XV_hcresampler_LookupConfig(BaseAddress);
+    if (ConfigPtr == NULL) {
+        InstancePtr->IsReady = 0;
+        return (XST_DEVICE_NOT_FOUND);
+    }
+
+    return XV_hcresampler_CfgInitialize(InstancePtr,
+                                        ConfigPtr,
+                                        ConfigPtr->BaseAddress);
+}
+#endif
 #endif
