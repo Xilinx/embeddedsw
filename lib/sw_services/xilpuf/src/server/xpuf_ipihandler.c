3807e6ec6ac48d9922f2919a82746fa5d05b91da
Kalyani Akula <kalyani.akula@xilinx.com>
2023-11-05 20:51:14 -1200

    xilpuf: Add input validations in all ipihandlers
    Input validations for ipi handler APIs are added in Versal and Versal
    Net xilpuf.
    
    Signed-off-by: Kalyani Akula <kalyani.akula@xilinx.com>
    
    Acked-for-series: Harsha <harsha.harsha@xilinx.com>=20

--- lib/sw_services/xilpuf/src/server/xpuf_ipihandler.c
+++ lib/sw_services/xilpuf/src/server/xpuf_ipihandler.c
@@ -66,12 +66,18 @@ static INLINE int XPuf_MemCopy(u64 SourceAddr, u64 DestAddr, u32 Len);
 int XPuf_IpiHandler(const XPlmi_Cmd *Cmd)
 {
 	volatile int Status = XST_INVALID_PARAM;
-	const u32 *Pload = Cmd->Payload;
+	u32 *Pload = NULL;
 
 	/**
 	 *  Validate the input parameters. Return XST_FAILURE if input parameters are invalid
 	 */
-	if (Pload == NULL) {
+	if (NULL == Cmd) {
+		goto END;
+	}
+
+	Pload = Cmd->Payload;
+
+	if (NULL == Pload) {
 		goto END;
 	}
 
