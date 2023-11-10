18a0ef8fdcc89754bebdd441342d00cdcfb08150
Tran, Trung <trung.tran@amd.com>
2023-11-10 07:16:58 -1200

    xilpm: versal: split xpm_pin headers
    - In versal_net, we change the layout of XPm_PinNode to support plm
    update. This layout change increase the size of XPm_PinNode by 4 bytes
    due to extra padding. This increase in size is affecting the current
    versal target where there's no need to support plm update.
    - This change is splitting the definition of the XPm_PinNode for versal
    and versal_net. This split will make sure versal code unchanged. and
    versal_net will have the layout change to support plm update.
    
    Signed-off-by: Trung Tran <trung.tran@amd.com>
    
    Nicole Baze <nicole.baze@xilinx.com>

---
+++ lib/sw_services/xilpm/src/versal_net/server/xpm_pin_plat.h
@@ -0,0 +1,42 @@
+/******************************************************************************
+* Copyright (c) 2022 - 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+#ifndef XPM_PIN_PLAT_H_
+#define XPM_PIN_PLAT_H_
+
+#include "xpm_pinfunc.h"
+#include "xpm_node.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/* Size of bit fields for XPm_PinNode structure */
+#define PIN_NODE_BANK_BIT_FIELD_SIZE 		2
+#define PIN_NODE_BIASSTATUS_BIT_FIELD_SIZE 	1
+#define PIN_NODE_PULLCTRL_BIT_FIELD_SIZE 	1
+#define PIN_NODE_TRISTATE_BIT_FIELD_SIZE 	1
+/**
+ * The Pin class.
+ */
+struct XPm_PinNode {
+	XPm_Node Node; /**< Node: Base class */
+	u8 NumGroups; /**< Number of function groups allocated to this pin */
+	SAVE_REGION(
+	u16 SubsysIdx;  /**< Subsystem Idx of the owner who is using this pin */
+	u8 Bank:PIN_NODE_BANK_BIT_FIELD_SIZE; /**< Specifies the bank number */
+	u8 BiasStatus:PIN_NODE_BIASSTATUS_BIT_FIELD_SIZE; /**< BiasStatus: 0 – Disable; 1 – Enable */
+	u8 PullCtrl:PIN_NODE_PULLCTRL_BIT_FIELD_SIZE; /**< PullCtrl: 0 – Pull Down; 1 – Pull Up */
+	u8 TriState:PIN_NODE_TRISTATE_BIT_FIELD_SIZE; /**< TriState: 0 – Disable; 1 – Enable */
+	)
+	XPm_PinFunc *PinFunc; /**< Function that this pin is allocated to */
+	u16 *Groups; /**< Array of group identifier supported by this pin */
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
+#endif /* XPM_PIN_PLAT_H_ */
