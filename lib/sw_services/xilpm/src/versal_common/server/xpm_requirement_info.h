4eb003f76da269931c0f08c1beffcefe5fe6713d
Tran, Trung <trung.tran@amd.com>
2023-11-14 04:47:29 -1200

    xilpm: versal_net: split xpm_requirement.h to platform specific headers
    - Split xpm_requirement headers to platform specific. Versal platform
    doesn't need to participate to plm update save and restore region.
    
    Signed-off-by: Trung Tran <trung.tran@amd.com>
    
    Acked-for-series: Sandeep Reddy Ghanapuram <sandeep.reddy-ghanapuram@amd.com>

---
+++ lib/sw_services/xilpm/src/versal_common/server/xpm_requirement_info.h
@@ -0,0 +1,36 @@
+/******************************************************************************
+* Copyright (c) 2022 - 2023 Advanced Micro Devices, Inc. All rights reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+#ifndef XPM_REQUIREMENT_INFO_H_
+#define XPM_REQUIREMENT_INFO_H_
+
+#include "xil_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Size of bit fields for XPm_ReqmInfo structure */
+#define REQ_INFO_CAPS_BIT_FIELD_SIZE		4
+#define REQ_INFO_LATENCY_BIT_FIELD_SIZE		21
+#define REQ_INFO_RESERVED_BIT_FIELD_SIZE	7
+
+typedef struct XPm_ReqmInfo XPm_ReqmInfo;
+/**
+ * Specific requirement information.
+ */
+struct XPm_ReqmInfo {
+	u32 Capabilities:REQ_INFO_CAPS_BIT_FIELD_SIZE; /**< Device capabilities (1-hot) */
+	u32 Latency:REQ_INFO_LATENCY_BIT_FIELD_SIZE; /**< Maximum device latency */
+	u32 Reserved:REQ_INFO_RESERVED_BIT_FIELD_SIZE; /**< Reserved for future use */
+	u32 QoS; /**< QoS requirement */
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
+#endif /* XPM_REQUIREMENT_INFO_H_ */
