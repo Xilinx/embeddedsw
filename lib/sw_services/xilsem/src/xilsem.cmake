f3e3c4443b55a4a0f206e8b96984fb2221bd8bd3
ANANTHA NAGA VIDHYA LAVANYA <lavanya.ananthanagavidhya@amd.com>
2023-11-16 16:45:05 -1200

    Xilsem:Enable XILSEM_ENABLE_SSIT macro using tcl
    XilSEM client interface has two sets of APIs, one for mono devices and other for
    SSIT devices.To enable SSIT client interface, users need to build the library
    with XILSEM_ENABLE_SSIT macro. Instead of user defining, add the logic in
    XilSEM build scripts to enable the macro automatically if the device type is SSIT
    
    Signed-off-by: Anantha Naga Vidhya Lavanya <lavanya.ananthanagavidhya@amd.com>
    Acked-by: Rama devi Veggalam <rama.devi.veggalam@amd.com>

--- lib/sw_services/xilsem/src/xilsem.cmake
+++ lib/sw_services/xilsem/src/xilsem.cmake
@@ -2,13 +2,9 @@
 # SPDX-License-Identifier: MIT
 
 cmake_minimum_required(VERSION 3.3)
-
-option(XILSEM_ssit_en "Defines if XilSEM SSIT client is enabled(1) or disabled (0)." OFF)
-
 if("${CMAKE_MACHINE}" STREQUAL "Versal")
-	if(${XILSEM_ssit_en})
+	if(NUMBER_OF_SLRS GREATER 1)
 		set(XILSEM_ENABLE_SSIT " ")
 	endif()
 endif()
-
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/xilsem_config.h.in ${CMAKE_BINARY_DIR}/include/xilsem_config.h)
