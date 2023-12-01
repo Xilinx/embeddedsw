6b1382c83bd4c94f34dba5843c7df4e31c8f3d6b
Shubhrajyoti Datta <shubhrajyoti.datta@amd.com>
2023-11-30 20:01:28 -1200

    i3cpsx: Update the slave property
    The latest xsa has the number of slaves connected as the DEVICE_COUNT.
    Update the same.
    
    Signed-off-by: Shubhrajyoti Datta <shubhrajyoti.datta@amd.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@amd.com>

--- XilinxProcessorIPLib/drivers/i3cpsx/data/i3cpsx.tcl
+++ XilinxProcessorIPLib/drivers/i3cpsx/data/i3cpsx.tcl
@@ -10,16 +10,17 @@
 # Ver  Who Date     Changes
 # ---- --- -------- -----------------------------------------------
 # 1.0  sd  07/15/22 First Release
+# 1.3  sd  11/27/23 Update the slave name
 #
 ##############################################################################
 
 #uses "xillib.tcl"
 
 proc generate {drv_handle} {
-    ::hsi::utils::define_zynq_include_file $drv_handle "xparameters.h" "XI3cPsx"  "NUM_INSTANCES" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR" "C_I3C_CLK_FREQ_HZ" "SLAVES"
+    ::hsi::utils::define_zynq_include_file $drv_handle "xparameters.h" "XI3cPsx"  "NUM_INSTANCES" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR" "C_I3C_CLK_FREQ_HZ" "C_DEVICE_COUNT"
 
-    ::hsi::utils::define_zynq_config_file $drv_handle "xi3cpsx_g.c" "XI3cPsx"  "DEVICE_ID" "C_S_AXI_BASEADDR" "C_I3C_CLK_FREQ_HZ" "SLAVES"
+    ::hsi::utils::define_zynq_config_file $drv_handle "xi3cpsx_g.c" "XI3cPsx"  "DEVICE_ID" "C_S_AXI_BASEADDR" "C_I3C_CLK_FREQ_HZ" "C_DEVICE_COUNT"
 
-    ::hsi::utils::define_zynq_canonical_xpars $drv_handle "xparameters.h" "XI3cPsx" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR" "C_I3C_CLK_FREQ_HZ" "SLAVES"
+    ::hsi::utils::define_zynq_canonical_xpars $drv_handle "xparameters.h" "XI3cPsx" "DEVICE_ID" "C_S_AXI_BASEADDR" "C_S_AXI_HIGHADDR" "C_I3C_CLK_FREQ_HZ" "C_DEVICE_COUNT"
 
 }
