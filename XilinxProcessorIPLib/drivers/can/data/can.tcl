bfd09af50f7605cee5bd42c7086c6c846f7a1a73
Harini T <harini.t@amd.com>
2023-12-22 01:18:59 -1200

    can: Add new member to config structure
    CAN 5.1 has the property enable_ecc. Update the tcl and yaml to add new
    member in config structure.
    
    Signed-off-by: Harini T <harini.t@amd.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@amd.com>

--- XilinxProcessorIPLib/drivers/can/data/can.tcl
+++ XilinxProcessorIPLib/drivers/can/data/can.tcl
@@ -7,8 +7,8 @@
 #uses "xillib.tcl"
 
 proc generate {drv_handle} {
-    ::hsi::utils::define_include_file $drv_handle "xparameters.h" "XCan" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" "c_can_rx_dpth" "c_can_tx_dpth" "c_can_num_acf"
-    ::hsi::utils::define_config_file $drv_handle "xcan_g.c" "XCan" "DEVICE_ID" "C_BASEADDR" "c_can_num_acf"
+    ::hsi::utils::define_include_file $drv_handle "xparameters.h" "XCan" "NUM_INSTANCES" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" "c_can_rx_dpth" "c_can_tx_dpth" "c_can_num_acf" "ENABLE_ECC"
+    ::hsi::utils::define_config_file $drv_handle "xcan_g.c" "XCan" "DEVICE_ID" "C_BASEADDR" "c_can_num_acf" "ENABLE_ECC"
 
-    ::hsi::utils::define_canonical_xpars $drv_handle "xparameters.h" "Can" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" "c_can_rx_dpth" "c_can_tx_dpth" "c_can_num_acf"
+    ::hsi::utils::define_canonical_xpars $drv_handle "xparameters.h" "Can" "DEVICE_ID" "C_BASEADDR" "C_HIGHADDR" "c_can_rx_dpth" "c_can_tx_dpth" "c_can_num_acf" "ENABLE_ECC"
 }
