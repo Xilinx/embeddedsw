3dbcd6325f4a4421ec069fa5175bf00a9688dbef
Gugulothu, Rajesh <rajesh.gugulothu@amd.com>
2024-02-05 17:07:04 -1200

    v_hdmitxss: Add SDT support in PT app for vck190 platform
    Add SDT support in PT app for vck190 platform.
    
    Signed-off-by: Rajesh Gugulothu <rajesh.gugulothu@amd.com>
    
    Acked-for-series: Gaddipati, Naveen <naveen.gaddipati@amd.com>

--- XilinxProcessorIPLib/drivers/v_hdmitxss/examples/xhdmi_example/tmds181.h
+++ XilinxProcessorIPLib/drivers/v_hdmitxss/examples/xhdmi_example/tmds181.h
@@ -1,5 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2018 - 2020 Xilinx, Inc.  All rights reserved.
+* Copyright 2023-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -11,6 +12,9 @@
 #include "xparameters.h"
 #include "xhdmiphy1.h"
 
+#ifdef SDT
+#define XPAR_IIC_0_BASEADDR XPAR_XIIC_0_BASEADDR
+#endif
 // Function prototypes
 u8 i2c_tmds181_read(u8 dev, u8 addr);
 u32 i2c_tmds181_write(u8 dev, u8 addr, u8 dat);
