258f05fdce02f35bf5855499151b8e285bc49a51
Tran, Trung <trung.tran@amd.com>
2024-02-09 07:25:30 -1200

    xilpm:versal_common: xpm_device reset PendingReqm
    - PendingReqm is only used during device in used; when it is release, we
    should set PendingReqm to NULL regardless the error status.
    
    Signed-off-by: Trung tran <trung.tran@amd.com>
    Acked-by: Izhar Ameer Shaikh <izhar.ameer.shaikh@amd.com>

--- lib/sw_services/xilpm/src/versal_common/server/xpm_device.c
+++ lib/sw_services/xilpm/src/versal_common/server/xpm_device.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 * Copyright (c) 2018 - 2022 Xilinx, Inc.  All rights reserved.
-* Copyright (c) 2022 - 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+* Copyright (c) 2022 - 2024 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -1179,8 +1179,8 @@ static XStatus DevRelease(XPm_Device *Device, const XPm_Subsystem *Subsystem, u3
 	}
 
 done:
+	Device->PendingReqm = NULL;
 	if (XST_SUCCESS != Status) {
-		Device->PendingReqm = NULL;
 		PmErr("0x%x, Id: 0x%x\r\n", Status, Device->Node.Id);
 	}
 	return Status;
