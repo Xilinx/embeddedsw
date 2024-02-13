82364481646bc4b9fc8ca8f811cfb13d0f4baf81
Kunal Rane <kunal.rane@amd.com>
2024-02-13 01:19:36 -1200

    dsirxss: data: Add initial revision of yaml
    Add initial version of yaml and other files required to build
    dsirxss design BSP.
    
    Signed-off-by: Kunal Rane <kunal.rane@amd.com>
    
    Acked-for-series: Gaddipati, Naveen <naveen.gaddipati@amd.com>

---
+++ XilinxProcessorIPLib/drivers/dsirxss/data/dsirxss_header.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+* Copyright 2024 Advanced Micro Devices, Inc. All Rights Reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+#ifndef XDSIRX_HEADER_H		/* prevent circular inclusions */
+#define XDSIRX_HEADER_H		/* by using protection macros */
+
+#include "xil_types.h"
+#include "xil_assert.h"
+#include "xstatus.h"
+
+u32 DsiRxSs_SelfTestExample(u32 DeviceId);
+
+#endif
