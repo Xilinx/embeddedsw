3116cc66bdf85b143bfb61f6c1030bd131ed34d8
Neelima Patel <Neelima.Patel@amd.com>
2024-02-19 22:46:09 -1200

    ipipsu: Add Doxygen fixes
    Editorial and Doxygen fixes for ipipsu driver.
    
    Signed-off-by: Neelima Patel <Neelima.Patel@amd.com>
    Acked-by: Shubhrajyoti Datta <shubhrajyoti.datta@amd.com>

--- XilinxProcessorIPLib/drivers/ipipsu/src/xipipsu_sinit.c
+++ XilinxProcessorIPLib/drivers/ipipsu/src/xipipsu_sinit.c
@@ -7,7 +7,7 @@
 /**
 *
 * @file xipipsu_sinit.c
-* @addtogroup ipipsu Overview
+* @addtogroup ipipsu_api IPIPSU APIs
 * @{
 *
 * The xipipsu_sinit.c file contains the implementation of the XIpiPsu
@@ -37,9 +37,9 @@
 /**
 *
 * Looks up the device configuration based on the unique device ID. A table
-* contains the configuration info for each device in the system.
+* contains the configuration information for each device in the system.
 *
-* @param	DeviceId contains the ID of the device to look up the
+* @param	DeviceId Contains the ID of the device to look up the
 *			configuration for.
 *
 * @return	A pointer to the configuration found or NULL if the specified
