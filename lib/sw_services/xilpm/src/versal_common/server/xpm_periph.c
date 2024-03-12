d703dc8c8a1218ee426e8cbb47d4fb72cb13e174
Buddhabhatti, Jay <jay.buddhabhatti@amd.com>
2024-03-11 23:49:38 -1200

    xilpm: versal_common: server: fix MISRA-C rule 4.7
    Fix the MISRA-C rule 4.7 violation.
    
    Problem: Calling XPm_SubsystemIdleCores without checking return value.
    
    Solution: Check the return value after calling a non-void function.
    
    Signed-off-by: Jay Buddhabhatti <jay.buddhabhatti@amd.com>
    Acked-by: Ronak Jain <ronak.jain@amd.com>

--- lib/sw_services/xilpm/src/versal_common/server/xpm_periph.c
+++ lib/sw_services/xilpm/src/versal_common/server/xpm_periph.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 * Copyright (c) 2018 - 2022 Xilinx, Inc.  All rights reserved.
-* Copyright (c) 2022 - 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+* Copyright (c) 2022 - 2024 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -281,6 +281,9 @@ static int HbMon_Scheduler(void *data)
 				}
 
 				Status = XPm_SubsystemIdleCores(Device->Requirements->Subsystem);
+				if (XST_SUCCESS != Status) {
+					goto done;
+				}
 			} else {
 				PmErr("Healthy Boot Timer %lu Expired. Triggering recovery\r\n",
 									Idx);
