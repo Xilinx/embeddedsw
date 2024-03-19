c288fd999820dd6c9b22738eee34a57057a519d6
Paul Alvin <alvin.paulp@amd.com>
2024-03-19 06:38:44 -1200

    wdttb: Add SDT check to use interrupt wrapper API
    Update xwdttb_intr, xwdttb_winwdt_intr and xwdttb_winwdt_qamode example to use interrupt wrapper API for SDT flow.
    
    Signed-off-by: Paul Alvin <alvin.paulp@amd.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@amd.com>

--- XilinxProcessorIPLib/drivers/wdttb/examples/xwdttb_winwdt_intr_example.c
+++ XilinxProcessorIPLib/drivers/wdttb/examples/xwdttb_winwdt_intr_example.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2016 - 2022 Xilinx, Inc.  All rights reserved.
-* Copyright (c) 2022 - 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+* Copyright (c) 2022 - 2024 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -38,6 +38,7 @@
 #include "xwdttb.h"
 #include "xil_exception.h"
 
+#ifndef SDT
 #ifdef XPAR_INTC_0_DEVICE_ID
 #include "xintc.h"
 #include <stdio.h>
@@ -45,7 +46,7 @@
 #include "xscugic.h"
 #include "xil_printf.h"
 #endif
-#ifdef SDT
+#else
 #include "xinterrupt_wrap.h"
 #endif
 
