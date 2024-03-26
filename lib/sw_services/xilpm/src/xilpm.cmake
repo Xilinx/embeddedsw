f8057a409475b0c5e670b4746b0a87c6736495ef
Bhatt, Madhav <madhav.bhatt@amd.com>
2024-03-25 23:46:27 -1200

    sw_services: xilpm: Enable the capability to adjust bsp config flags in the Rigel flow
    This feature introduces the ability to adjust BSP configuration flag in
    the rigel flow. It provides nine bsp configuration flag with three each
    for enabling/disabling APU/RPU0/RPU1 as power management master, another
    three for enabling/disabling APU/RPU0/RPU1 as reset management master,
    and final three for load overlay config object permission.
    
    Signed-off-by: Madhav Bhatt <madhav.bhatt@amd.com>
    Acked-by: Izhar Ameer Shaikh <izhar.ameer.shaikh@amd.com>

--- lib/sw_services/xilpm/src/xilpm.cmake
+++ lib/sw_services/xilpm/src/xilpm.cmake
@@ -10,4 +10,17 @@ if (IS_DEVICE_ID_PRESENT)
   set(XCVP1902 " ")
 endif()
 
+
+option(XILPM_rpu0_as_power_management_master "true: RPU0 as power management master, false: Disable RPU0 as power management master." ON)
+option(XILPM_rpu1_as_power_management_master "true: RPU1 as power management master, false: Disable RPU1 as power management master." ON)
+option(XILPM_apu_as_power_management_master "true: APU as power management master, false: Disable APU as power management master." ON)
+
+option(XILPM_rpu0_as_reset_management_master "true: RPU0 as reset management master, false: Disable RPU0 as reset management master" ON)
+option(XILPM_rpu1_as_reset_management_master "true: RPU1 as reset management master, false: Disable RPU1 as reset management master" ON)
+option(XILPM_apu_as_reset_management_master "true: APU as reset management master, false: Disable APU as reset management master" ON)
+
+option(XILPM_rpu0_as_overlay_config_master "true: RPU0 has permission to load overlay config objects, false: RPU0 doesn't have permission to load overlay config objects" OFF)
+option(XILPM_rpu1_as_overlay_config_master "true: RPU1 has permission to load overlay config objects, false: RPU1 doesn't have permission to load overlay config objects" OFF)
+option(XILPM_apu_as_overlay_config_master "true: APU has permission to load overlay config objects, false: APU doesn't have permission to load overlay config objects" OFF)
+
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/xpm_config.h.in ${CMAKE_BINARY_DIR}/include/xpm_config.h)
