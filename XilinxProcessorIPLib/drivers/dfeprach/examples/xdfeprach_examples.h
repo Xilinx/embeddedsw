94ab6427298a85f4dca98606a3c947ee71d24360
Dragan Cvetic <dragan.cvetic@amd.com>
2024-04-01 01:22:09 -1200

    dfeprach: Update device node name
    The example code registers libmetal for PRACH device with
    the device name.
    Device name has a base address in it.
    As the base address has been changed in IP the Update of
    device name in example code is required.
    
    Signed-off-by: Dragan Cvetic <dragan.cvetic@amd.com>
    Acked-by: Kadamathikuttiyil Karthikeyan Pillai, Anish <anish.kadamathikuttiyil-karthikeyan-pillai@amd.com>

--- XilinxProcessorIPLib/drivers/dfeprach/examples/xdfeprach_examples.h
+++ XilinxProcessorIPLib/drivers/dfeprach/examples/xdfeprach_examples.h
@@ -17,7 +17,8 @@
 * ----- -----  -------- -----------------------------------------------------
 * 1.1   dc     07/21/21 Add and reorganise examples
 * 1.7   dc     11/29/23 Add continuous scheduling
-*       cog    19/02/24 SDT Support
+*       cog    02/19/24 SDT Support
+*       dc     03/29/24 Update device node name
 *
 * </pre>
 *
@@ -54,7 +55,7 @@
 #define XDFESI570_CURRENT_FREQUENCY 156.25
 #define XDFESI570_NEW_FREQUENCY 122.88
 #else
-#define XDFEPRACH_NODE_NAME "a7d40000.xdfe_nr_prach"
+#define XDFEPRACH_NODE_NAME "a7e00000.xdfe_nr_prach"
 #endif
 
 /************************** Function Prototypes *****************************/
