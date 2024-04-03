fad0afffb17d9006bf178dd0af6723008c2f1af8
Siva Addepalli <sivaprasad.addepalli@xilinx.com>
2024-04-03 18:34:09 +0530

    Revert "sysmonpsv: Added SSIT Support"
    This reverts commit b135af38d3685aa33714f018f43ab9590679da6b.

--- lib/sw_apps/versal_plm/misc/versal_net/xparameters.h
+++ lib/sw_apps/versal_plm/misc/versal_net/xparameters.h
@@ -1318,7 +1318,6 @@
 /* Definitions for peripheral PSV_PMC_SYSMON_0 */
 #define XPAR_PSV_PMC_SYSMON_0_S_AXI_BASEADDR 0xF1270000
 #define XPAR_PSV_PMC_SYSMON_0_S_AXI_HIGHADDR 0xF129FFFF
-#define XPAR_PSV_PMC_SYSMON_0_NUMSUPPLIES 0
 
 
 /******************************************************************/
@@ -1326,7 +1325,6 @@
 /* Canonical definitions for peripheral PSV_PMC_SYSMON_0 */
 #define XPAR_XSYSMONPSV_0_S_AXI_BASEADDR 0xF1270000
 #define XPAR_XSYSMONPSV_0_S_AXI_HIGHADDR 0xF129FFFF
-#define XPAR_XSYSMONPSV_0_NUMSUPPLIES 0
 
 #define XPAR_XSYSMONPSV_0_NO_MEAS	161
 
