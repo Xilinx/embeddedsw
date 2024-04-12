94b6e21e43246d8b9ac49a2f99af6c2aa5f29d43
Jain, Ronak <ronak.jain@amd.com>
2024-04-11 18:55:00 -1200

    xilpm: versal: server: Add extern 'C' signature in header file
    Add extern 'C' signature in the missing header files.
    
    Signed-off-by: Ronak Jain <ronak.jain@amd.com>
    Acked-by: Naman Trivedi Manojbhai <naman.trivedimanojbhai@amd.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_noc_config.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_noc_config.h
@@ -18,6 +18,7 @@
 * ----- ---- -------- -------------------------------------------------------
 * 1.0  rpoolla	 23/01/18 Initial release
 * 2.0  tnt	 01/11/22 Porting to xilpm
+* 3.0  ronakj	 11/04/24 Add extern 'C' signature
 * </pre>
 *
 * @note
@@ -31,10 +32,18 @@
 #include "xpm_config.h"
 #endif
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 XStatus XPm_NoCConfig(void);
 XStatus XPm_NoCHWConfig(void);
 #ifdef XCVP1902
 XStatus XPm_NocConfig_vp1902(void);
+#endif
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif /* SRC_XPMCR_NOC_H_ */
