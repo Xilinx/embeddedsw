bc050385f3290e1f08ba876f92d02a272dbc5622
Levinsky, Ben <ben.levinsky@amd.com>
2024-04-18 16:50:18 -1200

    OpenAMP: Remove hard-coding cmake paths and dependencies
    As setup script in surrounding Vitis environment now provide both
    LD_LIBRARY_PATH and cmake binary setup, remove any logic of
    setting up LD_LIBRARY_PATH or pointing to explicit cmake path.
    
    Signed-off-by: Ben Levinsky <ben.levinsky@amd.com>
    Acked-by: Arun Kannan <arunbala@amd.com>

--- ThirdParty/sw_services/libmetal/src/run_cmake.bat
+++ ThirdParty/sw_services/libmetal/src/run_cmake.bat
@@ -1,8 +1,1 @@
-if not "%HDI_APPROOT%" == "" (
-%HDI_APPROOT%\tps\win64\cmake-3.3.2\bin\cmake.exe %* 2>&1
-) else (
-if "%XILINX_SDK%"== "" (
-%XILINX_VITIS%\tps\win64\cmake-3.3.2\bin\cmake.exe %* 2>&1
-) else (
-%XILINX_SDK%\tps\win64\cmake-3.3.2\bin\cmake.exe %* 2>&1
-))
+cmake.exe %* 2>&1
