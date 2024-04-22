23c4695eee182469ab046405e2cf2f3b16a73561
Sayyed, Mubin <mubin.sayyed@amd.com>
2024-04-22 05:09:21 -1200

    BSP: intr: Add software interrupt support
    Existing logic treats interrupts as SPI or PPI based on
    bit number 20th encoded in interrupt ID, if said bit is not
    set interrupt is always treated as SPI and offset 32 will be
    added to interrupt ID. This is breaking software generated
    interrupts.
    
    Update logic to allocate bit 22 in encoded interrupt ID for
    SGI interrupt, and modify offset calculation logic accordingly.
    If bit 22 is set, offset added to interrupt id will be 0.
    
    For configuring software generated interrupts, user needs to
    use following APIs to get encoded parent intc base address and encoded
    interrupt ID,
    
           XGetEncodedIntcBaseAddr
           XGetEncodedIntrId
    
    Encoded parent intc base address and interrupt ID needs to be
    passed to XSetupInterruptSystem.
    
    Signed-off-by: Mubin Sayyed <mubin.sayyed@amd.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>

--- lib/bsp/standalone/src/common/intr/xinterrupt_wrap.h
+++ lib/bsp/standalone/src/common/intr/xinterrupt_wrap.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2021 - 2022 Xilinx, Inc.  All rights reserved.
-* Copyright (C) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+* Copyright (C) 2023 - 2024 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 /*****************************************************************************/
@@ -20,6 +20,7 @@
 * 8.0	sk   03/17/22 Update XINTC_BASEADDR_MASK macro to unsigned to
 * 		      fix misra_c_2012_rule_7_2 violation.
 * 9.0   ml   03/03/23 Add description to fix doxygen warnings.
+* 9.1   mus  16/04/24 Add support for software generated interrupts.
 * </pre>
 *
 ******************************************************************************/
@@ -47,25 +48,58 @@
 #define XSPI_INTR_OFFSET		32U
 #endif
 
+enum XIntrType {
+	XSPI,
+	XPPI,
+	XSGI
+};
+
+
 #define XINTERRUPT_DEFAULT_PRIORITY     0xA0U /* AXI INTC doesnt support priority setting, it is default priority for GIC interrupts */
 #define XINTC_TYPE_IS_SCUGIC		0U
 #define XINTC_TYPE_IS_INTC		1U
 #define XINTR_IS_EDGE_TRIGGERED		3U
 #define XINTR_IS_LEVEL_TRIGGERED	1U
+#define XINTR_IS_SPI			0U
+#define XINTR_IS_PPI			1U
+#define XINTR_IS_SGI			2U
 
 #define XINTC_TYPE_MASK		0x1
 #define XINTC_INTR_TYPE_MASK		0x100000
 #define XINTC_BASEADDR_MASK		0xFFFFFFFFFFFFFFFEU
 #define XINTC_INTRID_MASK		0xFFF
 #define XINTC_TRIGGER_MASK		0xF000
+#define XINTC_IS_SGI_INTR_MASK		0x400000
 #define XINTC_TRIGGER_SHIFT		12
 #define XINTC_INTR_TYPE_SHIFT		20U
+#define XINTC_IS_SGI_INTR_SHIFT		22U
 #define XGet_IntcType(IntParent)	(IntParent & XINTC_TYPE_MASK)
 #define XGet_IntrType(IntId)		((IntId & XINTC_INTR_TYPE_MASK) >> XINTC_INTR_TYPE_SHIFT)
 #define XGet_BaseAddr(IntParent)	(IntParent & XINTC_BASEADDR_MASK)
 #define XGet_IntrId(IntId)		(IntId & XINTC_INTRID_MASK)
 #define XGet_TriggerType(IntId)		((IntId & XINTC_TRIGGER_MASK) >> XINTC_TRIGGER_SHIFT)
-#define XGet_IntrOffset(IntId)		(( XGet_IntrType(IntId) == 1) ? 16 : 32) /* For PPI offset is 16 and for SPI it is 32 */
+#define XGet_IntrOffset(IntId)		((XIsIntrTypeSGI(IntrId) == 1) ? 0 : (( XGet_IntrType(IntId) == 1) ? 16 : 32)) /* For PPI offset is 16 and for SPI it is 32 */
+#define XIsIntrTypeSGI(IntId)		((IntId & XINTC_IS_SGI_INTR_MASK) >> XINTC_IS_SGI_INTR_SHIFT)
+
+/*****************************************************************************/
+/**
+*
+* @brief    Get base address of parent interrupt controller in encoded format
+*           needed by interrupt wrapper layer
+*
+* @param    IntcBaseAddr Physical base address of parent interrupt controller.
+* @param    IntcType Parent interrupt controller type of targeted interrupt ID
+*               1 = AXI INTC
+*               0 = GIC
+
+* @return   base address of parent interrupt controller in encoded format
+*           needed by interrupt wrapper layer.
+*
+* @note     None.
+*
+******************************************************************************/
+
+#define XGetEncodedIntcBaseAddr(IntParent, IntcType)	(IntParent | IntcType)
 
 extern int XConfigInterruptCntrl(UINTPTR IntcParent);
 extern int XConnectToInterruptCntrl(u32 IntrId, void *IntrHandler, void *CallBackRef, UINTPTR IntcParent);
@@ -79,6 +113,8 @@ extern void XStopInterruptCntrl( UINTPTR IntcParent);
 extern void XRegisterInterruptHandler( void *IntrHandler, UINTPTR IntcParent);
 extern int XSetupInterruptSystem(void *DriverInstance, void *IntrHandler, u32 IntrId,  UINTPTR IntcParent,
 				 u16 Priority);
+extern s32 XGetEncodedIntrId(u32 LegacyIntrId, u32 TriggerType, u8 IntrType, u8 IntcType, u32 *IntrId);
+extern s32 XTriggerSoftwareIntr(u32 IntrId, UINTPTR IntcParent, u32 Cpu_Id);
 #endif
 
 #endif  /* end of protection macro */
