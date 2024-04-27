ec962c3dc7a8c20b67f75a494a7ac615bf132fae
Paladugu, Siva Durga Prasad <siva.durga.prasad.paladugu@amd.com>
2024-04-27 02:21:32 -1200

    scugic: Fix declaration for routines declared in scugic_header.h
    The declaration for routines ScuGicSelfTestExample() and ScuGicInterruptSetup()
    in scugic_header.h arent updated for SDT flow and this results in taking of
    truncated baseaddress in SDT flow, hence correct the declarations for these
    routines.
    
    Signed-off-by: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>
    Acked-by: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>

--- XilinxProcessorIPLib/drivers/scugic/data/scugic_header.h
+++ XilinxProcessorIPLib/drivers/scugic/data/scugic_header.h
@@ -1,5 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2011 - 2021 Xilinx, Inc.  All rights reserved.
+* Copyright (c) 2024 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -10,7 +11,12 @@
 #include "xil_types.h"
 #include "xstatus.h"
 
+#ifndef SDT
 int ScuGicSelfTestExample(u16 DeviceId);
 int ScuGicInterruptSetup(XScuGic *IntcInstancePtr, u16 DeviceId);
+#else
+int ScuGicSelfTestExample(u32 BaseAddr);
+int ScuGicInterruptSetup(XScuGic *IntcInstancePtr, u32 BaseAddr);
+#endif
 
 #endif
