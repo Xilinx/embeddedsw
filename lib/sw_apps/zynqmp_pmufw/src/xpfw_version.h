93f648f5ef525f79da974c00738ffbd1661bef59
Jain, Ronak <ronak.jain@amd.com>
2024-05-27 19:01:17 -1200

    sw_apps: zynqmp_pmufw: Update PMUFW release version to 2024.2
    Update PMUFW SDK release version to 2024.2.
    
    Signed-off-by: Ronak Jain <ronak.jain@amd.com>
    
    Acked-for-series:  Nicole Baze <amanda.baze@amd.com>

--- lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
+++ lib/sw_apps/zynqmp_pmufw/src/xpfw_version.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 * Copyright (c) 2019 - 2022 Xilinx, Inc.  All rights reserved.
-* Copyright (c) 2022 - 2023, Advanced Micro Devices, Inc. All Rights Reserved.
+* Copyright (c) 2022 - 2024, Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -11,7 +11,7 @@
 extern "C" {
 #endif
 
-#define ZYNQMP_XPFW_VERSION	"2024.1"
+#define ZYNQMP_XPFW_VERSION	"2024.2"
 
 #ifdef __cplusplus
 }
