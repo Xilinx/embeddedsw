6c827471e6fae23554726a863408d9714a40bcfc
Ghanapuram, Sandeep Reddy <sandeep.reddy-ghanapuram@amd.com>
2024-06-03 22:08:10 -1200

    xilpm: versal_net: Sync with GOQ
    move macros to xpm_repair.c to sync with GOQ FW and remove typecasts.
    
    Signed-off-by: Sandeep Reddy Ghanapuram <sandeep.reddy-ghanapuram@amd.com>
    
    Acked-for-series: Nicole Baze <amanda.baze@amd.com>

--- lib/sw_services/xilpm/src/versal_net/server/xpm_repair.c
+++ lib/sw_services/xilpm/src/versal_net/server/xpm_repair.c
@@ -12,6 +12,22 @@
 
 /*HNICX Repair*/
 #define HNICX_LCS_INDIRECT_NPI_BISR_CACHE_DATA0 (0x0009C074U)
+#define HNICX_DPU_INDIRECT_NPI_BISR_CACHE_DATA0		(0x30182500U)
+
+/* DDRMC5 repair */
+#define DDRMC5_NPI_CACHE_STATUS_MAIN_REGISTER_OFFSET    (0x00000254U)
+#define DDRMC5_NPI_CACHE_DATA_MAIN_REGISTER_OFFSET      (0x00000244U)
+#define DDRMC5_NPI_PCSR_CONTROL_REGISTER_OFFSET         (0x00000004U)
+#define DDRMC5_NPI_PCSR_BISR_MAIN_TRIGGER_MASK          (0x02000000U)
+#define DDRMC5_NPI_CACHE_STATUS_BISR_DONE_MASK          (0x00000001U)
+#define DDRMC5_NPI_CACHE_STATUS_BISR_PASS_MASK          (0x00000002U)
+#define DDRMC5_NPI_CLK_GATE_REGISTER_OFFSET             (0x00000238U)
+#define DDRMC5_NPI_CLK_GATE_MAIN_BISREN_MASK            (0x00000040U)
+#define DDRMC5_NPI_CLK_GATE_CRYPTO_BISREN_MASK          (0x00000080U)
+#define DDRMC5_NPI_CACHE_STATUS_CRYPTO_REGISTER_OFFSET  (0x00000414U)
+#define DDRMC5_NPI_CACHE_DATA_CRYPTO_REGISTER_OFFSET    (0x00000404U)
+#define DDRMC5_NPI_PCSR_BISR_CRYPTO_TRIGGER_MASK        (0x04000000U)
+#define DDRMC_NPI_PCSR_MASK_REGISTER_OFFSET             (0x00000000U)
 
 XStatus XPmRepair_Lpx(u32 EfuseTagAddr, u32 TagSize,
 			u32 TagOptional, u32 *TagDataAddr)
@@ -82,7 +98,7 @@ XStatus XPmRepair_Fpx(u32 EfuseTagAddr, u32 TagSize,
 	/* Copy maximum 16 words to the non-continuous BISR_CACHE register space*/
 	while (0U < TagSize)
 	{
-		TempTagSize = (TagSize >= BISR_CACHE_SUB_SIZE) ? BISR_CACHE_SUB_SIZE : TagSize;
+		TempTagSize = (BISR_CACHE_SUB_SIZE <= TagSize) ? BISR_CACHE_SUB_SIZE : TagSize;
 		*TagDataAddr = XPmBisr_CopyStandard((u32)EfuseTagAddr, TempTagSize, BisrDataDestAddr[i]);
 		EfuseTagAddr += (TempTagSize * 4U);
 		TagSize -= TempTagSize;
@@ -134,12 +150,10 @@ XStatus XPmRepair_Hnicx_Nthub(u32 EfuseTagAddr, u32 TagSize,
 {
 	XStatus Status = XST_FAILURE;
 	u32 RegValue;
-	u64 BisrDataDestAddr;
+	u64 BisrDataDestAddr = HNICX_NPI_0_BISR_CACHE_DATA0;
 
 	/* Unused argument */
 	(void)TagOptional;
-
-	BisrDataDestAddr = HNICX_NPI_0_BISR_CACHE_DATA0;
 
 	/* Copy repair data */
 	*TagDataAddr = XPmBisr_CopyStandard(EfuseTagAddr, TagSize, BisrDataDestAddr);
@@ -215,10 +229,10 @@ XStatus XPmRepair_Ddrmc5_Main(u32 EfuseTagAddr, u32 TagSize,
 {
 	XStatus Status = XST_FAILURE;
 	u32 BaseAddr = NPI_FIXED_BASEADDR | (TagOptional << NPI_EFUSE_ENDPOINT_SHIFT);
-	u64 BisrDataDestAddr = BaseAddr | (u64)DDRMC5_NPI_CACHE_DATA_MAIN_REGISTER_OFFSET;
+	u64 BisrDataDestAddr = BaseAddr | DDRMC5_NPI_CACHE_DATA_MAIN_REGISTER_OFFSET;
 
 	/* Copy repair data */
-	*TagDataAddr = XPmBisr_CopyStandard(EfuseTagAddr, TagSize, BisrDataDestAddr);
+	*TagDataAddr = XPmBisr_CopyStandard((u32)EfuseTagAddr, TagSize, BisrDataDestAddr);
 
 	/* Enable BISR clock */
 	XPm_RMW32(BaseAddr | DDRMC5_NPI_CLK_GATE_REGISTER_OFFSET, DDRMC5_NPI_CLK_GATE_MAIN_BISREN_MASK, DDRMC5_NPI_CLK_GATE_MAIN_BISREN_MASK);
@@ -248,10 +262,10 @@ XStatus XPmRepair_Ddrmc5_Crypto(u32 EfuseTagAddr, u32 TagSize,
 {
 	XStatus Status = XST_FAILURE;
 	u32 BaseAddr = NPI_FIXED_BASEADDR | (TagOptional << NPI_EFUSE_ENDPOINT_SHIFT);
-	u64 BisrDataDestAddr = BaseAddr | (u64)DDRMC5_NPI_CACHE_DATA_CRYPTO_REGISTER_OFFSET;
+	u64 BisrDataDestAddr = BaseAddr | DDRMC5_NPI_CACHE_DATA_CRYPTO_REGISTER_OFFSET;
 
 	/* Copy repair data */
-	*TagDataAddr = XPmBisr_CopyStandard(EfuseTagAddr, TagSize, BisrDataDestAddr);
+	*TagDataAddr = XPmBisr_CopyStandard((u32)EfuseTagAddr, TagSize, BisrDataDestAddr);
 
     /* Enable BISR clock */
     XPm_RMW32(BaseAddr | DDRMC5_NPI_CLK_GATE_REGISTER_OFFSET, DDRMC5_NPI_CLK_GATE_CRYPTO_BISREN_MASK, DDRMC5_NPI_CLK_GATE_CRYPTO_BISREN_MASK);
@@ -278,7 +292,7 @@ done:
 
 static XStatus XPmRepair_WriteIndirectNpi(u32 addr, u32 val)
 {
-	(void)XPm_In32(HNICX_NPI_0_NPI_CSR_WR_STATUS);
+	(void)XPm_PollForMask(HNICX_NPI_0_NPI_CSR_WR_STATUS, 0, 1);
 	XPm_Out32(HNICX_NPI_0_NPI_CSR_WDATA, val);
 	XPm_Out32(HNICX_NPI_0_NPI_CSR_INST, (addr & 0x3FFFFFFFU) | ((u32)1<<30));
 	return XPm_PollForMask(HNICX_NPI_0_NPI_CSR_WR_STATUS, HNICX_NPI_0_NPI_CSR_WR_STATUS_BVALID_MASK, XPM_POLL_TIMEOUT);
@@ -286,45 +300,25 @@ static XStatus XPmRepair_WriteIndirectNpi(u32 addr, u32 val)
 
 static u32 XPmRepair_CopyIndirectNpi(u32 EfuseTagAddr, u32 TagSize, u64 BisrDataDestAddr)
 {
-	u32 TagData;
-	XStatus Status = XST_FAILURE;
-	u32 EfuseCacheBaseAddr;
-	u32 EfuseTagBitS1Addr;
-	u32 EfuseTagBitS2Addr;
-	u32 TagDataAddr;
+    u32 TagData;
+    u32 TagDataAddr;
 
-	const XPm_Device *EfuseCache = XPmDevice_GetById(PM_DEV_EFUSE_CACHE);
-	if (NULL == EfuseCache) {
-		/* Return max possible address so error can be identified by caller */
-		TagDataAddr = ~0U;
-		goto done;
-	}
+    /*EFUSE tag data start pos */
+    TagDataAddr = EfuseTagAddr+4;
 
-	EfuseCacheBaseAddr = EfuseCache->Node.BaseAddress;
-	EfuseTagBitS1Addr = (EfuseCacheBaseAddr + EFUSE_CACHE_TBITS1_BISR_RSVD_OFFSET);
-	EfuseTagBitS2Addr = (EfuseCacheBaseAddr + EFUSE_CACHE_TBITS2_BISR_RSVD_OFFSET);
-
-	/*EFUSE tag data start pos */
-	TagDataAddr = (u32)(EfuseTagAddr + 4U);
-
-	/* Collect repair data from EFUSE and write to NPI indirect address */
-	for (u32 TagRow = 0; TagRow<TagSize; TagRow++)
-	{
-		if ((TagDataAddr == EfuseTagBitS1Addr) || (TagDataAddr == EfuseTagBitS2Addr))
-		{
-			TagDataAddr += 4U;
-		}
-		TagData = XPm_In32(TagDataAddr);
-		u64 TmpAddr = BisrDataDestAddr + ((u64)TagRow<<2);
-		Status = XPmRepair_WriteIndirectNpi((u32)TmpAddr, TagData);
-		if (XST_SUCCESS != Status) {
-			break;
-		}
-		TagDataAddr += 4U;
-	}
-
-done:
-	return TagDataAddr;
+    /* Collect repair data from EFUSE and write to NPI indirect address */
+    for (u32 TagRow = 0; TagRow<TagSize; TagRow++)
+    {
+        if ((TagDataAddr == EFUSE_CACHE_TBITS1_BISR_RSVD) || (TagDataAddr == EFUSE_CACHE_TBITS2_BISR_RSVD))
+        {
+            TagDataAddr+=4;
+        }
+        TagData = XPm_In32(TagDataAddr);
+        u64 TmpAddr = BisrDataDestAddr+(TagRow<<2);
+        (void)XPmRepair_WriteIndirectNpi((u32)TmpAddr, TagData);
+        TagDataAddr += 4;
+    }
+    return TagDataAddr;
 }
 
 XStatus XPmRepair_Hnicx_Lcs(u32 EfuseTagAddr, u32 TagSize,
