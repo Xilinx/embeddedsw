7e00586815ff078845130311eacaae0cf3db757f
Harini T <harini.t@amd.com>
2024-06-04 18:05:54 -1200

    scuwdt: Fix HIS_COMF violations
    Fix HIS_COMF (comment frequency) violations of scuwdt driver by adding
    comments to the code.
    
    Signed-off-by: Harini T <harini.t@amd.com>
    
    Acked-for-series: Srinivas Goud <srinivas.goud@amd.com>

--- XilinxProcessorIPLib/drivers/scuwdt/src/xscuwdt_sinit.c
+++ XilinxProcessorIPLib/drivers/scuwdt/src/xscuwdt_sinit.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2010 - 2022 Xilinx, Inc.  All rights reserved.
-* Copyright (C) 2022 - 2023 Advanced Micro Devices, Inc.  All rights reserved.
+* Copyright (C) 2022 - 2024 Advanced Micro Devices, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -23,6 +23,7 @@
 * 2.1 	sk  02/26/15 Modified the code for MISRA-C:2012 compliance.
 * 2.5   asa 07/18/23 Made updates to support system device tree based
 *                    workflow decoupling flow.
+* 2.6 	ht  06/03/24 Fix HIS_COMF violations.
 * </pre>
 *
 ******************************************************************************/
@@ -61,6 +62,7 @@ XScuWdt_Config *XScuWdt_LookupConfig(u16 DeviceId)
 	XScuWdt_Config *CfgPtr = NULL;
 	u32 Index;
 
+	/* Checks all the instances */
 	for (Index = 0U; Index < (u32)XPAR_XSCUWDT_NUM_INSTANCES; Index++) {
 		if (XScuWdt_ConfigTable[Index].DeviceId == DeviceId) {
 			CfgPtr = &XScuWdt_ConfigTable[Index];
@@ -68,6 +70,7 @@ XScuWdt_Config *XScuWdt_LookupConfig(u16 DeviceId)
 		}
 	}
 
+	/* Returns reference to config record if found, else NULL */
 	return (XScuWdt_Config *)CfgPtr;
 }
 #else
