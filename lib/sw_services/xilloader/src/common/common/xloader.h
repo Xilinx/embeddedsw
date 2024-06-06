3ccf75dd3f50b3ab5f41695dd8b72d6895a2db91
Kalyani Akula <kalyani.akula@xilinx.com>
2024-06-05 19:50:40 -1200

    xilloader: Move SecureConfig Pcr extension to ProcessAuthEncPrtn
    When there are multiple chunks of data is present in a partition,
    SPK and SPK ID PCR extension is failing. This is because AC is assigned with
    chunk memory and when SPK info is taken to extend to PCR after last chunk
    is processed the chunk memory assigned to AC would have overwritten with
    other data.
    Instead of extending secure config after all chunks are processed,
    extending SPK info immediately after chunk 0 processing is done will
    solve the issue.
    
    Signed-off-by: Kalyani Akula <kalyani.akula@xilinx.com>
    Acked-by: Durga Challa <durga.challa@amd.com>

--- lib/sw_services/xilloader/src/common/common/xloader.h
+++ lib/sw_services/xilloader/src/common/common/xloader.h
@@ -118,6 +118,7 @@
 *       mss  03/06/2024 Added ClearAtfHandoff member in XilPdi structure and
 *                       ClearATFHandoffParams function prototype
 *       har  03/12/2024 Fixed doxygen warnings
+*       kal  06/04/2024 Added ImagePrtnId to XilPdi structure
 *
 * </pre>
 *
@@ -333,7 +334,8 @@ typedef struct {
 	u32 DecKeySrc; /**< Decryption Key Source */
 	u32 PpdiKatStatus; /**< PPDI Known Answer Test Status */
 #endif
-	u32 DigestIndex;	/**< Digest index for data measurement */
+	u32 DigestIndex; /**< Digest index for data measurement */
+	u32 ImagePrtnId; /**< Partition number in the Image */
 } XilPdi;
 
 /*
@@ -450,7 +452,7 @@ int XLoader_IdCodeCheck(const XilPdi_ImgHdrTbl * ImgHdrTbl);
 void Xloader_SaveBootPdiInfo(XilPdi *BootPdiPtr);
 int XLoader_EnableJtag(u32 CfgState);
 int XLoader_DisableJtag(void);
-int XLoader_ProcessPrtn(XilPdi* PdiPtr, u32 PrtnIndex);
+int XLoader_ProcessPrtn(XilPdi* PdiPtr);
 int XLoader_InitPdiInstanceForExtractMHAndOptData(XPlmi_Cmd* Cmd, XilPdi* PdiPtr, u64 SrcAddr, u64 DestAddr, u32 DestSize);
 
 /************************** Variable Definitions *****************************/
