3120473fe58d7324dd7820c63c4f0a833e4e6ddf
M Lakshmaiah <Lakshmaiah.M@amd.com>
2024-07-01 02:59:12 -1200

    sw_apps: memory_tests: Prevent False Memory Test Failures
    Update the reject memory list to exclude specific RAM regions
    (psm_ram  and pmu_ram) regions from the memory test to avoid
    incorrect failures.
    
    Signed-off-by: M Lakshmaiah <lakshmaiah.m@amd.com>
    Acked-by: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>

--- lib/sw_apps/memory_tests/data/memory_tests.tcl
+++ lib/sw_apps/memory_tests/data/memory_tests.tcl
@@ -150,14 +150,14 @@ proc get_mem_info { proc_instance } {
     set is_versal_net [hsi::get_cells -hier -filter {IP_NAME=="psx_cortexr52" || IP_NAME=="psx_cortexa78"}]
     set is_versal [hsi::get_cells -hier -filter {IP_NAME=="psv_cortexr5" || IP_NAME=="psv_cortexa72"}]
 
-    set RejectListForAddressBlocks {"ai_engine" "pcie" "pmc_ram"}
+    set RejectListForAddressBlocks {"ai_engine" "pcie" "pmc_ram" "psm_ram" "psu_pmu_ram"}
 
     if { [llength $is_zynqmp] > 0 } {
-    set RejectListForBaseValue {"E0000000" "600000000" "8000000000"}
+    set RejectListForBaseValue {"E0000000" "600000000" "8000000000" "FFDC0000"}
     } elseif { [llength $is_versal_net] > 0 } {
-    set RejectListForBaseValue {"A0000000" "20000000000" "F2000000"}
+    set RejectListForBaseValue {"A0000000" "20000000000" "F2000000" "EBC00000" "EBC20000"}
     } elseif { [llength $is_versal] > 0 } {
-    set RejectListForBaseValue {"E0000000" "20000000000" "F2000000" "600000000"}
+    set RejectListForBaseValue {"E0000000" "20000000000" "F2000000" "600000000" "FFC00000" "FFC20000"}
     } else {
     set RejectListForBaseValue {}
     }
