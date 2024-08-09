37f4a1de09d0ae1a6b389ecd88570ea3094b7e33
Karumanchi, Vineeth <vineeth.karumanchi@amd.com>
2024-08-08 23:32:14 -1200

    drivers: emacps: Add new register offsets and bit fields
    Adding registers to support multi queue mode and
    screening type registers to support priority queuing.
    
    Signed-off-by: Vineeth Karumanchi <vineeth.karumanchi@amd.com>
    
    Acked-for-series: Harini Katakam <harini.katakam@amd.com>

--- XilinxProcessorIPLib/drivers/emacps/src/xemacps_hw.h
+++ XilinxProcessorIPLib/drivers/emacps/src/xemacps_hw.h
@@ -279,16 +279,22 @@ typedef enum { MDC_DIV_8 = 0U, MDC_DIV_16, MDC_DIV_32, MDC_DIV_48,
 #define XEMACPS_PCS_CONTROL_OFFSET	0x00000200U /** PCS control register */
 #define XEMACPS_PCS_STATUS_OFFSET	0x00000204U /** PCS status register */
 
+#define XEMACPS_DCFG6_OFFSET		0x00000294U /** designcfg_debug6 register */
+
 #define XEMACPS_INTQ1_STS_OFFSET     0x00000400U /**< Interrupt Q1 Status
 							reg */
 #define XEMACPS_TXQ1BASE_OFFSET	     0x00000440U /**< TX Q1 Base address
 							reg */
 #define XEMACPS_RXQ1BASE_OFFSET	     0x00000480U /**< RX Q1 Base address
 							reg */
+#define XEMACPS_DMA_RXQ1_BUFSIZE_OFFSET	0x000004A0U /**< RX Q1 DMA buffer size
+							reg */
 #define XEMACPS_MSBBUF_TXQBASE_OFFSET  0x000004C8U /**< MSB Buffer TX Q Base
 							reg */
 #define XEMACPS_MSBBUF_RXQBASE_OFFSET  0x000004D4U /**< MSB Buffer RX Q Base
 							reg */
+#define XEMACPS_SCREEN_TYPE2_REG0       0x00000540U /** Screening Type2 Reg0 **/
+
 #define XEMACPS_INTQ1_IER_OFFSET     0x00000600U /**< Interrupt Q1 Enable
 							reg */
 #define XEMACPS_INTQ1_IDR_OFFSET     0x00000620U /**< Interrupt Q1 Disable
@@ -463,9 +469,15 @@ typedef enum { MDC_DIV_8 = 0U, MDC_DIV_16, MDC_DIV_32, MDC_DIV_48,
  */
 #define XEMACPS_INTQ1SR_TXCOMPL_MASK	0x00000080U /**< Transmit completed OK */
 #define XEMACPS_INTQ1SR_TXERR_MASK	0x00000040U /**< Transmit AMBA Error */
+#define XEMACPS_INTQ1SR_RXCOMPL_MASK	0x00000002U /**< Receive completed OK */
 
 #define XEMACPS_INTQ1_IXR_ALL_MASK	((u32)XEMACPS_INTQ1SR_TXCOMPL_MASK | \
+					 (u32)XEMACPS_INTQ1SR_TXERR_MASK | \
+                                         (u32)XEMACPS_INTQ1SR_RXCOMPL_MASK)
+
+#define XEMACPS_INTQ1_IXR_TX_MASK	((u32)XEMACPS_INTQ1SR_TXCOMPL_MASK | \
 					 (u32)XEMACPS_INTQ1SR_TXERR_MASK)
+#define XEMACPS_INTQ1_IXR_RX_MASK	(u32)XEMACPS_INTQ1SR_RXCOMPL_MASK
 
 /*@}*/
 
@@ -532,6 +544,13 @@ typedef enum { MDC_DIV_8 = 0U, MDC_DIV_16, MDC_DIV_32, MDC_DIV_48,
 #define XEMACPS_RXWM_HIGH_MASK		0x0000FFFFU	/**< RXWM high mask */
 #define XEMACPS_RXWM_LOW_MASK		0xFFFF0000U	/**< RXWM low mask */
 #define XEMACPS_RXWM_LOW_SHFT_MSK	16U	/**< Shift for RXWM low */
+/*@}*/
+
+/** @name Screening Type2 bit definitions
+ * @{
+ */
+#define XEMACPS_CMPA_ENABLE_MASK	0x00040000U	/**< Enable Compare A */
+#define XEMACPS_CMPA_MASK		0x0003E000U	/**< Compare reg number */
 /*@}*/
 
 /* Transmit buffer descriptor status words offset
