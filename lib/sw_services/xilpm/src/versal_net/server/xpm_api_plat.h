3f76615ffb695c0738a1be99f7fb2be7f543af04
Bhatt, Madhav <madhav.bhatt@amd.com>
2024-08-22 20:55:05 -1200

    xilpm: versal_net: Add API for LLC flush in CMN block
    During subsystem restart, add an API to flush the LLC in the CMN block.
    And remove the workaround for CMN flush.
    
    Signed-off-by: Madhav Bhatt <madhav.bhatt@amd.com>
    Acked-by: Izhar Ameer Shaikh <izhar.ameer.shaikh@amd.com>

--- lib/sw_services/xilpm/src/versal_net/server/xpm_api_plat.h
+++ lib/sw_services/xilpm/src/versal_net/server/xpm_api_plat.h
@@ -28,7 +28,7 @@ typedef struct XPm_Subsystem XPm_Subsystem;
 int XPm_RegisterWakeUpHandlers(void);
 
 
-XStatus XPm_PlatCmnFlushWorkaround(void);
+XStatus XPm_PlatCmnFlush(const u32 SubsystemId);
 XStatus XPm_InitNode(u32 NodeId, u32 Function, const u32 *Args, u32 NumArgs);
 XStatus XPm_HookAfterPlmCdo(void);
 
