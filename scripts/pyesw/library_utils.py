4005d176ac89f7d2974949014e994f6276634965
Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
2024-09-03 16:12:10 -1200

    scripts: pyesw: Add support for logging in the scripts
    Add support for the logging in the scripts, using optional verbose
    option user can see the detailed timing numbers of each API.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
    
    Acked-for-series: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>

--- scripts/pyesw/library_utils.py
+++ scripts/pyesw/library_utils.py
@@ -15,7 +15,7 @@ import utils
 import re
 from repo import Repo
 from open_amp import open_amp_copy_lib_src
-
+from utils import log_time
 
 class Library(Repo):
     """
@@ -284,6 +284,7 @@ class Library(Repo):
         return lib_list
 
 
+    @log_time
     def add_lib_for_apps(self, app_name):
         """
         Adds library to the bsp. Creates metadata if needed for the library,
@@ -333,6 +334,7 @@ class Library(Repo):
 
         return lib_list, cmake_cmd_append
 
+    @log_time
     def config_lib(self, comp_name, lib_list, cmake_cmd_append, is_app=False):
         lib_config = {}
         if lib_list:
@@ -353,11 +355,12 @@ class Library(Repo):
                 self.modify_cmake_subdirs(lib_list, action='add')
                 if is_app:
                     cmake_cmd_append = cmake_cmd_append.replace('\\', '/')
-                    utils.runcmd(f'cmake -G "{self.cmake_generator}" {self.domain_path} {self.cmake_paths_append} -DSUBDIR_LIST="{cmake_lib_list}" {cmake_cmd_append} -LH > cmake_lib_configs.txt', cwd = build_metadata)
+                    utils.runcmd(f'cmake -G "{self.cmake_generator}" {self.domain_path} {self.cmake_paths_append} -DSUBDIR_LIST="{cmake_lib_list}" {cmake_cmd_append} -LH > cmake_lib_configs.txt', cwd = build_metadata, log_message=None)
                 else:
                     utils.runcmd(
                         f'cmake -G "{self.cmake_generator}" {self.domain_path} {self.cmake_paths_append} -DSUBDIR_LIST="{cmake_lib_list}" -LH > cmake_lib_configs.txt',
-                        cwd = build_metadata
+                        cwd = build_metadata,
+                        log_message=None
                     )
                     utils.update_yaml(self.domain_config_file, "domain", "config", "reconfig")
             except:
