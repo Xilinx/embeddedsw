dceeefe3c0842d9dd7cf7dec4b7da43c68dc4c02
M Lakshmaiah <Lakshmaiah.M@amd.com>
2024-09-11 06:06:33 -1200

    tmrctr: Fix missing extern 'C' keyword in header files
    Add the missing extern "C" keyword to the header files,
    as it is required for components that support C++.
    
    Signed-off-by: M Lakshmaiah <lakshmaiah.m@amd.com>
    Acked-by: Siva Durga Prasad Paladugu <siva.durga.prasad.paladugu@amd.com>

--- XilinxProcessorIPLib/drivers/tmrctr/data/tmrctr_header.h
+++ XilinxProcessorIPLib/drivers/tmrctr/data/tmrctr_header.h
@@ -1,11 +1,15 @@
 /******************************************************************************
 * Copyright (C) 2005 - 2020 Xilinx, Inc.  All rights reserved.
-* Copyright (C) 2022 - 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+* Copyright (C) 2022 - 2024 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
 #ifndef TMRCTR_HEADER_H		/* prevent circular inclusions */
 #define TMRCTR_HEADER_H		/* by using protection macros */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include "xil_types.h"
 #include "xil_assert.h"
@@ -17,6 +21,10 @@ int TmrCtrSelfTestExample(u16 DeviceId);
 int TmrCtrSelfTestExample(UINTPTR BaseAddr);
 int TmrCtrIntrExample(XTmrCtr *InstancePtr,
 		      UINTPTR BaseAddr);
+#endif
+
+#ifdef __cplusplus
+}
 #endif
 
 #endif
