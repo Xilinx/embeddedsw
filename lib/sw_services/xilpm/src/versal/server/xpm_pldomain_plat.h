9832366bb1f3ed975846bfc63ffcca825780b300
jaychou <jay.chou@amd.com>
2024-09-24 07:30:20 -1200

    xilpm: server: Fix some potential issues when reducing and restoring CFU clk freq
    Fix 2 potential security issues related to the CFU clk frequency control.
    1. When PLM reduces frequency, it should check if MSB is set; otherwise, the frequency remains the same.
       This is to prevent from overflowing.
    2. When PLM sets the frequency register, it uses XSECURE_REDUNDANT_IMPL() for the double write.
    
    Signed-off-by: jaychou <jay.chou@amd.com>
    Acked-by: Jesus De Haro <jesus.de-haro@amd.com>

--- lib/sw_services/xilpm/src/versal/server/xpm_pldomain_plat.h
+++ lib/sw_services/xilpm/src/versal/server/xpm_pldomain_plat.h
@@ -4,12 +4,10 @@
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
-#ifndef XPM_PLDOMAIN_H_
-#define XPM_PLDOMAIN_H_
+#ifndef XPM_PLDOMAIN_PLAT_H_
+#define XPM_PLDOMAIN_PLAT_H_
 
-#include "xpm_bisr.h"
 #include "xpm_powerdomain.h"
-#include "xpm_regs.h"
 #include "xcframe.h"
 #include "xcfupmc.h"
 #ifdef SDT
@@ -30,20 +28,14 @@ typedef struct XPm_PlDomain {
 	u32 Cframe0RegBaseAddr; /**< CFRAME0 Register base address */
 } XPm_PlDomain;
 
-/* TRIM Types */
-#define XPM_PL_TRIM_VGG		 (0x1U)
-#define XPM_PL_TRIM_CRAM	 (0x2U)
-#define XPM_PL_TRIM_BRAM	 (0x3U)
-#define XPM_PL_TRIM_URAM	 (0x4U)
 
 /************************** Function Prototypes ******************************/
 XStatus XPmPlDomain_Init(XPm_PlDomain *PlDomain, u32 Id, u32 BaseAddress,
 			 XPm_Power *Parent, const u32 *OtherBaseAddresses,
 			 u32 OtherBaseAddressCnt);
-XStatus XPmPlDomain_RetriggerPlHouseClean(void);
 
 #ifdef __cplusplus
 }
 #endif
 /** @} */
-#endif /* XPM_PLDOMAIN_H_ */
+#endif /* XPM_PLDOMAIN_PLAT_H_ */
--- lib/sw_services/xilpm/src/versal_net/server/xpm_pldomain_plat.h
+++ lib/sw_services/xilpm/src/versal_net/server/xpm_pldomain_plat.h
@@ -4,12 +4,10 @@
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
-#ifndef XPM_PLDOMAIN_H_
-#define XPM_PLDOMAIN_H_
+#ifndef XPM_PLDOMAIN_PLAT_H_
+#define XPM_PLDOMAIN_PLAT_H_
 
-#include "xpm_bisr.h"
 #include "xpm_powerdomain.h"
-#include "xpm_regs.h"
 #include "xcframe.h"
 #include "xcfupmc.h"
 #include "xpm_nodeid.h"
@@ -18,7 +16,6 @@
 extern "C" {
 #endif
 
-extern u32 HcleanDone;
 /**
  * The PL power domain node class.
  */
@@ -29,21 +26,15 @@ typedef struct XPm_PlDomain {
 	SAVE_REGION()
 } XPm_PlDomain;
 
-/* TRIM Types */
-#define XPM_PL_TRIM_VGG		 (0x1U)
-#define XPM_PL_TRIM_CRAM	 (0x2U)
-#define XPM_PL_TRIM_BRAM	 (0x3U)
-#define XPM_PL_TRIM_URAM	 (0x4U)
 
 /************************** Function Prototypes ******************************/
 XStatus XPmPlDomain_Init(XPm_PlDomain *PlDomain, u32 Id, u32 BaseAddress,
 			 XPm_Power *Parent, const u32 *OtherBaseAddresses,
 			 u32 OtherBaseAddressCnt);
-XStatus XPmPlDomain_RetriggerPlHouseClean(void);
 XStatus XPm_PldApplyTrim(u32 TrimType);
 
 #ifdef __cplusplus
 }
 #endif
 /** @} */
-#endif /* XPM_PLDOMAIN_H_ */
+#endif /* XPM_PLDOMAIN_PLAT_H_ */
