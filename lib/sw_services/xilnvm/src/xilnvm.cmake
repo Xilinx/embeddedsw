fc0a7e1f7776b43c02df60da46757c47c91f8fc7
Kundanala, Praveen Teja <praveen.teja.kundanala@amd.com>
2024-10-29 17:46:06 -1200

    sw_services: xilnvm: SpartanUP SDT fixes
    - Change processor name from SPARTANUP to SpartanUP
    - Remove dependency on xilmailbox and xilplmi
    
    Signed-off-by: Praveen Teja Kundanala <praveen.teja.kundanala@amd.com>
    
    Acked-for-series: Gajulapally, Nandanikhil <Nandanikhil.Gajulapally@amd.com>

--- lib/sw_services/xilnvm/src/xilnvm.cmake
+++ lib/sw_services/xilnvm/src/xilnvm.cmake
@@ -1,7 +1,8 @@
 # Copyright (C) 2023 - 2024 Advanced Micro Devices, Inc.  All rights reserved.
 # SPDX-License-Identifier: MIT
 
-if("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "plm_microblaze")
+if("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "plm_microblaze" OR
+   "${CMAKE_MACHINE}" STREQUAL "SpartanUP")
   set(XILNVM_mode "server")
 elseif(("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "microblaze") OR ("${CMAKE_MACHINE}" STREQUAL "VersalNet"))
   # For soft microblaze and Versal_net APU/RPU cores, mode is client.
