703b77b18fc761c008653ae7847a096e7c5953d0
Nandanikhil Gajulapally <Nandanikhil.Gajulapally@amd.com>
2024-11-26 19:55:41 -1200

    spartanup_plm: add support for boot time measurements
    Implement boot time measurements for full and partial PDI loads.
    
    Signed-off-by: Nandanikhil Gajulapally <nandanikhil.gajulapally@amd.com>
    Acked-by: Sreedhar Kundella <sreedhar.kundella@amd.com>

--- lib/sw_apps/spartanup_plm/src/xplm_init.h
+++ lib/sw_apps/spartanup_plm/src/xplm_init.h
@@ -16,6 +16,7 @@
  * Ver   Who  Date     Changes
  * ----- ---- -------- -------------------------------------------------------
  * 1.00  ng   05/31/24 Initial release
+ * 1.01  ng   11/05/24 Add boot time measurements
  * </pre>
  *
  ******************************************************************************/
@@ -33,15 +34,35 @@ extern "C" {
 #include "xil_types.h"
 #include "xplm_debug.h"
 #include "xplm_error.h"
+#include "xiomodule.h"
 
 /************************** Constant Definitions *****************************/
+#define XPLM_PIT1_RESET_VALUE		(0xFFFFFFFDU)
+#define XPLM_PIT2_RESET_VALUE		(0xFFFFFFFEU)
+#define XPLM_PIT1_CYCLE_VALUE		(XPLM_PIT1_RESET_VALUE + 1U)
+#define XPLM_PIT2_CYCLE_VALUE		(XPLM_PIT2_RESET_VALUE + 1U)
+#define XPLM_PIT1			(0U)
+#define XPLM_PIT2			(1U)
+#define XPLM_PIT3			(2U)
+#define XPLM_MEGA			(1000000U)
+#define XPLM_KILO			(1000U)
 
 /**************************** Type Definitions *******************************/
+typedef struct {
+	u64 TPerfMs;	/**< Whole part of time in milliseconds */
+	u64 TPerfMsFrac; /**< Fractional part of time in milliseconds */
+} XPlm_PerfTime;
 
 /***************** Macros (Inline Functions) Definitions *********************/
 
 /************************** Function Prototypes ******************************/
 u32 XPlm_Init(void);
+XIOModule *XPlm_GetIOModuleInst(void);
+u64 XPlm_GetTimerValue(void);
+void XPlm_PrintRomTime(void);
+void XPlm_PrintPlmTime(void);
+u32 XPlm_PmcIroFreq(void);
+void XPlm_GetPerfTime(u64 TCur, u64 TStart, u32 IroFreq, XPlm_PerfTime *PerfTime);
 
 #ifdef __cplusplus
 }
