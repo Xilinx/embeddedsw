74d9bd10db69433101bde09e3f405090dc36790e
Akula, Mounika <mounika.akula@amd.com>
2024-12-19 19:28:04 -1200

    sw_services: xilasu: Corrected SHA HASH length macro in examples
    SHA HASH length macro name in incorrect in SHA examples. Corrected the
    same.
    
    Signed-off-by: Mounika Akula <mounika.akula@amd.com>
    Acked-by: Durga Challa <durga.challa@amd.com>

--- lib/sw_services/xilasu/examples/xasu_sha3_example.c
+++ lib/sw_services/xilasu/examples/xasu_sha3_example.c
@@ -23,13 +23,13 @@
 * Following is the procedure to compile the example on OCM or any memory region which can be accessed by
 * server
 *
-*		1. Open example linker script(lscript.ld) in Vitis project and section to memory mapping should
-*			be updated to point all the required sections to shared memory(OCM or TCM)
-*			using a memory region drop down selection
+* 1. Open example linker script(lscript.ld) in Vitis project and section to memory mapping should
+*    be updated to point all the required sections to shared memory(OCM or TCM)
+*    using a memory region drop down selection
 *
-*						OR
+*					OR
 *
-*		1. In linker script(lscript.ld) user can add new memory section in source tab as shown below
+* 1. In linker script(lscript.ld) user can add new memory section in source tab as shown below
 *			.sharedmemory : {
 *   			. = ALIGN(4);
 *   			__sharedmemory_start = .;
@@ -39,22 +39,24 @@
 *   			__sharedmemory_end = .;
 * 			} > ocm_ram_0_psv_ocm_ram_0
 *
-* 		2. In this example ".data" section elements that are passed by reference to the server-side
-* 		   should be stored in the above shared memory section. To make it happen in below example,
-*		   replace ".data" in attribute section with ".sharedmemory". For example,
-* 		   static const char Sha3Hash[SHA3_HASH_LEN_IN_BYTES] __attribute__ ((section (".data.Sha3Hash")));
-* 					should be changed to
-* 		   static const char Sha3Hash[SHA3_HASH_LEN_IN_BYTES]
-*					__attribute__ ((section (".sharedmemory.Sha3Hash")));
+* 2. In this example ".data" section elements that are passed by reference to the server-side
+*    should be stored in the above shared memory section. To make it happen in below example,
+*    replace ".data" in attribute section with ".sharedmemory". For example,
+*    static const char Sha3Hash[ASU_SHA3_HASH_LEN_IN_BYTES]
+*    			__attribute__ ((section (".data.Sha3Hash")));
+* 		should be changed to
+*    static const char Sha3Hash[ASU_SHA3_HASH_LEN_IN_BYTES]
+*			__attribute__ ((section (".sharedmemory.Sha3Hash")));
 *
 * To keep things simple, by default the cache is disabled for this example
 *
 * MODIFICATION HISTORY:
 * <pre>
 * Ver   Who    Date     Changes
-* ----- ------ -------- -------------------------------------------------
+* ----- ------ -------- ---------------------------------------------------------------------------
 * 1.0   vns    08/28/24 Initial Release
 *       am     09/24/24 Added SDT support
+* 1.1   ma     12/20/24 Corrected macro name
 *
 * </pre>
  *************************************************************************************************/
