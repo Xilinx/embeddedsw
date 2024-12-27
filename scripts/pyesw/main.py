af77fd5a55e0d8eaadad02760bfbb251d0e75cf1
Onkar Harsh <onkar.harsh@amd.com>
2024-12-26 21:52:23 -1200

    scripts: pyesw: main: Add support for LOCAL_PYESW_SCRIPTS
    With pyinstaller binary, it is going to be tough to debug issues in these scripts.
    Add an environment variable LOCAL_PYESW_SCRIPTS to pick the sub command scripts
    from local location instead of the one packed with the binary.
    
    Signed-off-by: Onkar Harsh <onkar.harsh@amd.com>

--- scripts/pyesw/main.py
+++ scripts/pyesw/main.py
@@ -1,7 +1,16 @@
 # Copyright (C) 2023 - 2024 Advanced Micro Devices, Inc.  All rights reserved.
 # SPDX-License-Identifier: MIT
 
+import os
 import sys
+
+if os.environ.get("LOCAL_PYESW_SCRIPTS"):
+    local_scripts_path = os.environ.get("LOCAL_PYESW_SCRIPTS")
+    if not os.path.isdir(local_scripts_path):
+        print(f"[WARNING]: LOCAL_PYESW_SCRIPTS variable is set but {local_scripts_path} doesnt exist")
+    else:
+        sys.path.insert(0, local_scripts_path)
+        print(f"[INFO]: LOCAL_PYESW_SCRIPTS variable is set to {local_scripts_path}")
 
 from build_app import main as build_app_main
 from build_bsp import main as build_bsp_main
@@ -20,14 +29,15 @@ from validate_bsp import main as validate_bsp_main
 
 
 def info():
-    info=f"""Usage: use "pyesw [command] --help" for more information
-
-    pyesw command line tool
+    info=f"""\b
+pyesw command line tool
 
     Positional arguments:
         create_app          Create a template App using the BSP path
         create_bsp          Create bsp for the given sdt, os, processor and template app
-        build_app           Build the created app. It expects either -w <app ws path> or -s <app src dir path> and -b <app build dir path> passed during create_app
+        build_app           Build the created app.
+                            It expects either -w <app ws path> or
+                            -s <app src dir path> and -b <app build dir path> passed during create_app
         build_bsp           Build the created bsp
         config_bsp          Modify BSP Settings
         create_example      Create driver or library example using the BSP path
@@ -42,33 +52,53 @@ def info():
 
     Optional arguments:
         -h, --help          Show this help message and exit
+
     Examples:
-        create_bsp:
-            pyesw create_bsp -w <bsp path> -s <system device-tree system-top.dts path> -p <processor name> -t <template name>
-            Ex: pyesw create_bsp -t hello_world -s /proj/ssw_xhd/verification/no_delete/design_dts_10GB/2023.1/xbuilds_designs/zcu102/dts/system-top.dts
-                -w zcu102_xbuilds -p psu_cortexa53_0
-            Output:
-                :ls zcu102_xbuilds
-                app_list.yaml  CMakeLists.txt             cpulist.yaml      include  lib_list.yaml  psu_cortexa53_0_baremetal.dts
-                bsp.yaml       cortexa53_toolchain.cmake  Findcommon.cmake  lib      libsrc         Xilinx.spec
-        build_bsp:
-            pyesw build_bsp -d <Directory Path till bsp>
-            Ex: pyesw build_bsp -d zcu102_xbuilds
-        create_app:
-            pyesw create_app -d <Specify the Directory path till BSP> -t <template app name> -w <app_ws>
-            Ex: pyesw create_app -d zcu102_xbuilds -t lwip_echo_server -w app_ws
-            Output:
-                :ls app_ws/src
-                CMakeLists.txt  i2c_access.c    lscript.ld              Lwip_echo_serverExample.cmake  memory.ld   platform_config.h.in  README.txt  si5324.c
-                echo.c          iic_phyreset.c  lwip_echo_server.cmake  main.c
-        build_app:
-            pyesw build_app -w <Specify the app workspace>
-            Ex: pyesw build_app -w app_ws
-            Output:
-                :ls app_ws/build
-                CMakeCache.txt  CMakeFiles  cmake_install.cmake  compile_commands.json  include  lwip_echo_server.elf  Makefile
-        For more examples use "pyesw [command] --help" for more information
-    """
+
+    create_bsp:
+        pyesw create_bsp -w <bsp path> -s <system device-tree system-top.dts path> -p <processor name> -t <template name>
+
+        sample command:
+        pyesw create_bsp -t hello_world -s /home/abc/dts/system-top.dts -w bsp_ws -p psu_cortexa53_0
+
+        Output inside test_bsp:
+            app_list.yaml  CMakeLists.txt             cpulist.yaml      include  lib_list.yaml  psu_cortexa53_0_baremetal.dts
+            bsp.yaml       cortexa53_toolchain.cmake  Findcommon.cmake  lib      libsrc         Xilinx.spec
+
+    build_bsp:
+        pyesw build_bsp -d <Directory Path till bsp>
+
+        sample command:
+        pyesw build_bsp -d bsp_ws
+
+    create_app:
+        pyesw create_app -d <Specify the Directory path till BSP> -t <template app name> -w <app_ws>
+
+        sample command:
+        pyesw create_app -d bsp_ws -t lwip_echo_server -w app_ws
+
+        Output inside app_ws/src:
+            CMakeLists.txt  i2c_access.c          lscript.ld              Lwip_echo_serverExample.cmake
+            memory.ld       platform_config.h.in  README.txt              si5324.c
+            echo.c          iic_phyreset.c        lwip_echo_server.cmake  main.c
+
+    build_app:
+        pyesw build_app -w <Specify the app workspace>
+
+        sample command:
+        pyesw build_app -w app_ws
+
+        Output inside app_ws/build:
+        CMakeCache.txt  CMakeFiles            cmake_install.cmake  compile_commands.json
+        include         lwip_echo_server.elf  Makefile
+
+Use "pyesw [command] -h" for more information on the available sub commands.
+
+examples to get sub command help:
+    pyesw config_bsp -h
+    pyesw load_example -h
+    pyesw create_example -h
+"""
     print(info)
 
 func_map={"create_app" : create_app_main,
