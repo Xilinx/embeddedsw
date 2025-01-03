d9825c92d7740b1a9356840b119df6ea5068e16e
Levinsky, Ben <ben.levinsky@amd.com>
2025-01-03 04:57:39 -1200

    lib: sw_apps: OpenAMP Demos: Update to use latest OpenAMP System Reference Repo
    As the OpenAMP-based demos are updated, update corresponding CMake
    configuration steps:
    - Remove unneeded CMake files being hosted and dropped in
    - OpenAMP Python script updated app-configure cleaned up
    - Remove now uneeded OpenAMP Info from shared python scripts
    - Simplify OpenAMP Lopper run
    
    Signed-off-by: Ben Levinsky <ben.levinsky@amd.com>
    
    this has been acked in email and on code collab

--- scripts/pyesw/open_amp.py
+++ scripts/pyesw/open_amp.py
@@ -13,18 +13,18 @@ import sys
 import utils
 from utils import is_file
 
-APP_NAMES = {
+openamp_app_names = {
     'openamp_echo_test': 'echo',
     'openamp_matrix_multiply': 'matrix_multiply',
     'openamp_rpc_demo': 'rpc_demo'
 }
 
 def open_amp_app_name(app_name):
-    if app_name not in APP_NAMES.keys():
+    if app_name not in openamp_app_names.keys():
         print("ERROR: ", app_name, " not found in list of openamp apps")
         sys.exit(1)
 
-    return APP_NAMES[app_name]
+    return openamp_app_names[app_name]
 
 def open_amp_copy_lib_src(libdir, dstdir, lib):
     """
@@ -83,7 +83,7 @@ def open_amp_copy_lib_src(libdir, dstdir, lib):
     with open(lib_dstdir, 'w', encoding='utf-8') as file:
         file.write(content)
 
-def openamp_app_configure_common(obj, esw_app_dir, enable_generated_header=False):
+def openamp_app_configure_common(obj, esw_app_dir):
     """
     Template that can be used to copy toolchain file and ensure it is present
     in CMake configure step
@@ -93,39 +93,15 @@ def openamp_app_configure_common(obj, esw_app_dir, enable_generated_header=False
     Returns:
         None
     """
-    if obj.template in ['openamp_echo_test', 'openamp_matrix_multiply', 'openamp_rpc_demo']:
-        new_src = os.path.join(esw_app_dir, '..', 'openamp_sdt_common', 'src', 'sdt')
-        obj.cmake_paths_append += ' -DOPENAMP_APP_NAME=' + APP_NAMES[obj.template]
-    else:
-        new_src = os.path.join(esw_app_dir, 'src', 'sdt')
-
-    if enable_generated_header:
+    if obj.template in openamp_app_names.keys():
+        obj.cmake_paths_append += ' -DOPENAMP_APP_NAME=' + openamp_app_names[obj.template]
         obj.cmake_paths_append += " -D_AMD_GENERATED_=ON "
 
-def create_app(mappings, obj, esw_app_dir):
-    """
-    Template that can be used to copy repo sources for apps.
+    obj.cmake_paths_append += " -DPROJECT_VENDOR=\"xlnx\" "
+    obj.cmake_paths_append += " -DWITH_DOC=OFF "
 
-    This can be used for OpenAMP and Libmetal apps.
-
-    Args:
-        mappings (dict): dictionary of source to destination files
-        obj (App): App object that contains Takes all the user inputs in a dictionary.
-        esw_app_dir (str): path to application files in embeddedsw repo
-    Returns:
-        None
-    """
-    if obj.template in ['openamp_echo_test', 'openamp_matrix_multiply', 'openamp_rpc_demo']:
-        new_src = os.path.join(esw_app_dir, '..', 'openamp_sdt_common', 'src', 'sdt')
-    else:
-        new_src = os.path.join(esw_app_dir, 'src', 'sdt')
-
-    for key in mappings.keys():
-        src = os.path.join(new_src, key)
-        dst = os.path.join(obj.app_src_dir, mappings[key])
-        utils.copy_file(src, dst, silent_discard=True)
-
-    openamp_app_configure_common(obj, esw_app_dir)
+    if obj.os == "freertos":
+        obj.cmake_paths_append += " -DUSE_FREERTOS=ON"
 
 def create_openamp_app(obj, esw_app_dir):
     """
@@ -138,35 +114,17 @@ def create_openamp_app(obj, esw_app_dir):
         None
     """
 
-    common_mappings = {
-        'top-CMakeLists.txt'         : ['CMakeLists.txt'],
-        'app-CMakeLists.txt'         : ['apps', 'CMakeLists.txt'],
-        'app-example-CMakeLists.txt' : ['apps', 'examples', 'CMakeLists.txt'],
-    }
+    openamp_app_configure_common(obj, esw_app_dir)
 
-    app_cmake_name = APP_NAMES[obj.template]
-    common_mappings['demo-dir-CMakeLists.txt'] = ['apps', 'examples',
-                    app_cmake_name, 'CMakeLists.txt']
+    # Point to correct demo to build
+    utils.replace_line(os.path.join(obj.app_src_dir, "examples", "legacy_apps", "CMakeLists.txt"),
+                       f'project (osr_legacy_apps C)',
+                       f'set (OPENAMP_APP_NAME \"' + open_amp_app_name(obj.template) + '\")\n')
 
-    # Create new mappings based on app name. Each record is (embeddedsw-src : app-workspace
-    # location)
-    #
-    # Paths must be generated to ensure that paths are system
-    # agnostic using os.path.join(). This is because paths are different
-    # on Windows vs. UNIX based systems.
-    #
-    new_file_mappings = {}
-    for key in common_mappings.keys():
-        path_array = common_mappings[key]
-        esw_path_str  = path_array[0]
-
-        # for each subsequent element after the first, append to path
-        for i in path_array[1:]:
-            esw_path_str = os.path.join(esw_path_str,  i)
-
-        new_file_mappings[key] = esw_path_str
-
-    create_app(new_file_mappings, obj, esw_app_dir)
+    # Specify app name if applicable
+    if obj.app_name:
+        utils.replace_line(os.path.join(obj.app_src_dir, "CMakeLists.txt"),
+                           f'project(openamp_sys_ref)', f'project ({obj.app_name} C)\n')
 
 def create_libmetal_app(obj, esw_app_dir):
     """
@@ -178,86 +136,54 @@ def create_libmetal_app(obj, esw_app_dir):
     Returns:
         None
     """
+    utils.copy_file(os.path.join(esw_app_dir, 'src', 'sdt', 'top-CMakeLists.txt'),
+                    os.path.join(obj.app_src_dir, 'CMakeLists.txt'),
+                    silent_discard=True)
 
-    app_os = obj.os if obj.os == 'freertos' else 'generic'
-    common_mappings = {
-        'top-CMakeLists.txt'         : ['CMakeLists.txt'],
-    }
+    openamp_app_configure_common(obj, esw_app_dir)
 
-    # Create new mappings based on app name. Each record is (embeddedsw-src : app-workspace
-    # location)
-    #
-    # Paths must be generated to ensure that paths are system
-    # agnostic using os.path.join(). This is because paths are different
-    # on Windows vs. UNIX based systems.
-    #
-    new_file_mappings = {}
-    for key in common_mappings.keys():
-        path_array = common_mappings[key]
-        esw_path_str  = path_array[0]
+    # Specify app name if applicable
+    if obj.app_name:
+        utils.replace_line(os.path.join(obj.app_src_dir, "CMakeLists.txt"),
+                           f'project (libmetal_amp_demod C)', f'project ({obj.app_name} C)')
 
-        # for each subsequent element after the first, append to path
-        for i in path_array[1:]:
-            esw_path_str = os.path.join(esw_path_str,  i)
-
-        new_file_mappings[key] = esw_path_str
-
-    create_app(new_file_mappings, obj, esw_app_dir)
-
-def openamp_lopper_run(openamp_overlay, original_sdt, bsp_sdt,
-                       app_src_dir, proc, linker_cmd):
+def openamp_lopper_run(bsp_sdt, linker_cmd, obj, esw_app_dir):
     """
     Run Lopper upon OpenAMP YAML information if present.
     Args:
-        openamp_overlay (str): Path to OpenAMP Overlay information. This will
-                               be present in domain.yaml
-        original_sdt (str): Path to unpruned system device tree
-        app_src_dir (str): Path to application workspace
+        bsp_sdt (str): Path to unpruned system device tree
+        linker_cmd (str): Current lopper linker command
+        obj (App): App object that contains Takes all the user inputs in a dictionary.
+        esw_app_dir (str): Path to application source area
     Returns:
         None
     """
-    # Map proc to lopper targets
-    if len(openamp_overlay) == 0:
-        print('No overlay provided for OpenAMP app. Exiting Build.')
-        sys.exit(1)
-    remote = {
-        'psx_cortexr52_0' : 'r52_0',
-        'psx_cortexr52_1' : 'r52_1',
-        'psx_cortexr52_2' : 'r52_2',
-        'psx_cortexr52_3' : 'r52_3',
-        'psv_cortexr5_0' : 'r5_0',
-        'psv_cortexr5_1' : 'r5_1',
-        'psu_cortexr5_0' : 'r5_0',
-        'psu_cortexr5_1' : 'r5_1',
+    openamp_lopper_host_remote_tuples = {
+        'psx_cortexr52_0' : ('a78_0', 'r52_0', 'versal-net'),
+        'psx_cortexr52_1' : ('a78_0', 'r52_1', 'versal-net'),
+        'psx_cortexr52_2' : ('a78_0', 'r52_2', 'versal-net'),
+        'psx_cortexr52_3' : ('a78_0', 'r52_3', 'versal-net'),
+        'psv_cortexr5_0'  : ('a72_0', 'r5_0', 'versal'),
+        'psv_cortexr5_1'  : ('a72_0', 'r5_1', 'versal'),
+        'psu_cortexr5_0'  : ('a53_0', 'r5_0', 'zynqmp'),
+        'psu_cortexr5_1'  : ('a53_0', 'r5_1', 'zynqmp'),
     }
 
-    host = {
-        'psx_cortexr52_0' : 'a78_0',
-        'psx_cortexr52_1' : 'a78_0',
-        'psx_cortexr52_2' : 'a78_0',
-        'psx_cortexr52_3' : 'a78_0',
-        'psv_cortexr5_0' : 'a72_0',
-        'psv_cortexr5_1' : 'a72_0',
-        'psu_cortexr5_0' : 'a53_0',
-        'psu_cortexr5_1' : 'a53_0',
-    }
+    (host, remote, soc) = openamp_lopper_host_remote_tuples[obj.proc]
+    overlay_dst = os.path.join(obj.domain_path, "hw_artifacts", "domain.yaml")
+    output_sdt = os.path.join(obj.app_src_dir, "openamp_output.dts")
 
+    utils.copy_file(os.path.join(esw_app_dir, "..", "openamp_sdt_common", "src", "sdt", f"openamp-overlay-{soc}.yaml"),
+                    overlay_dst)
 
-    lops = ["lop-load.dts", "lop-xlate-yaml.dts"]
-    lopper_cmd = "lopper -f -v  --enhanced  --permissive -i " + openamp_overlay
-    for lop in lops:
-        lopper_cmd += " -i " + lop
-    # add remote role for this Lopper run after the lop-openamp-versal.dts lop
-    output_sdt = os.path.join(app_src_dir, "openamp_output.dts")
-    lopper_cmd += " -O . " + original_sdt + " " + output_sdt
-    lopper_cmd += " -- openamp --openamp_role=remote "
-    lopper_cmd += " --openamp_host=" + host[proc]
-    lopper_cmd += " --openamp_remote=" + remote[proc]
+    cmd =  f"lopper -f -v --enhanced --permissive -i lop-xlate-yaml.dts "
+    cmd += f"-i {overlay_dst} -O {obj.app_src_dir} {bsp_sdt} {output_sdt} "
+    cmd += f"-- openamp --openamp_role=remote --openamp_host={host} --openamp_remote={remote}"
+    utils.runcmd(cmd, log_message="OpenAMP Lopper Run")
 
-    utils.runcmd(lopper_cmd)
     if is_file('amd_platform_info.h'):
-        utils.copy_file('amd_platform_info.h', os.path.join(app_src_dir, 'apps',
-                        'machine', 'zynqmp_r5', 'amd_platform_info.h'))
+        utils.copy_file('amd_platform_info.h', os.path.join(obj.app_src_dir,
+                        'examples', 'legacy_apps', 'machine', 'zynqmp_r5', 'amd_platform_info.h'))
         utils.remove('amd_platform_info.h')
 
         # Change out SDT to use OpenAMP SDT here
@@ -272,27 +198,4 @@ def openamp_lopper_run(openamp_overlay, original_sdt, bsp_sdt,
         sys.exit(1)
     return 0
 
-def copy_openamp_app_overlay(obj, esw_app_dir):
-    """
-    Copy relevant default OpenAMP Overlay YAML to BSP area
-    Args:
-        obj (App): App object that contains Takes all the user inputs in a dictionary.
-        overlay_dst (str): Path in BSP to place overlay
-    Returns:
-        None
-    """
-    core_soc_map = {
-        'psx_cortexr52_0' : 'versal-net',
-        'psx_cortexr52_1' : 'versal-net',
-        'psx_cortexr52_2' : 'versal-net',
-        'psx_cortexr52_3' : 'versal-net',
-        'psv_cortexr5_0' : 'versal',
-        'psv_cortexr5_1' : 'versal',
-        'psu_cortexr5_0' : 'zynqmp',
-        'psu_cortexr5_1' : 'zynqmp',
-    }
 
-    new_src = os.path.join(esw_app_dir, '..', 'openamp_sdt_common', 'src', 'sdt')
-    overlay_src = os.path.join(new_src, 'openamp-overlay-' + core_soc_map[obj.proc] + '.yaml')
-    overlay_dst = os.path.join(obj.domain_path, 'hw_artifacts', 'domain.yaml')
-    utils.copy_file(overlay_src, overlay_dst)
