dd07029efa9419f1b7eaa742595cd0b3c65e614b
Akula, Mounika <mounika.akula@amd.com>
2025-01-24 04:49:36 -1200

    sw_apps: asufw: Added KDF driver support
    Implementation for HMAC based Key Derivation Function API in ASUFW. The
    XKdf_Compute API takes KeyIn, Fixed input data from the user and
    calculates the requested number of bytes keying material output in
    counter mode. KDF uses HMAC APIs for calculating the output key. The
    counter size is fixed to 4 bytes and placed before the fixed input data
    in big endian format.
    
    Signed-off-by: Mounika Akula <mounika.akula@amd.com>
    
    Acked-for-series: Durga Challa <durga.challa@amd.com>

---
+++ lib/sw_apps/asufw/src/kdf/xkdf.c
@@ -0,0 +1,175 @@
+/**************************************************************************************************
+* Copyright (c) 2025 Advanced Micro Devices, Inc. All Rights Reserved.
+* SPDX-License-Identifier: MIT
+**************************************************************************************************/
+/*************************************************************************************************/
+/**
+*
+* @file xkdf.c
+*
+* This file contains the implementation of the HMAC based Key Derivation Function (HKDF) APIs
+* using counter mode.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who  Date     Changes
+* ----- ---- -------- -----------------------------------------------------------------------------
+* 1.0   ma   01/15/25 Initial release
+*
+* </pre>
+*
+*
+**************************************************************************************************/
+/**
+* @addtogroup xkdf_server_apis KDF Server APIs
+* @{
+*/
+
+/*************************************** Include Files *******************************************/
+#include "xkdf.h"
+#include "xhmac.h"
+#include "xasufw_status.h"
+#include "xasufw_util.h"
+#include "xfih.h"
+
+/************************************** Type Definitions *****************************************/
+
+/************************************ Variable Definitions ***************************************/
+
+/************************************ Function Prototypes ****************************************/
+
+/************************************** Macros Definitions ***************************************/
+
+/************************************** Function Definitions *************************************/
+
+/*************************************************************************************************/
+/**
+ * @brief	This function performs KDF compute operation using HMAC as pseudorandom function with
+ * the user provided inputs and generates the keying material object of specified number of bytes
+ * in counter mode.
+ *
+ * @param	DmaPtr			Pointer to the XAsufw_Dma instance.
+ * @param	ShaInstancePtr	Pointer to the XSha instance.
+ * @param	KdfParams		Pointer to the KDF structure containing user input parameters.
+ *
+ * @return
+ * 	- XASUFW_SUCCESS, if KDF compute operation was successful.
+ * 	- XASUFW_KDF_INVALID_PARAM, if input parameters are invalid.
+ * 	- Errors codes from HMAC, if HMAC operation fails.
+ *
+ *************************************************************************************************/
+s32 XKdf_Compute(XAsufw_Dma *DmaPtr, XSha *ShaInstancePtr, const XAsu_KdfParams *KdfParams)
+{
+	CREATE_VOLATILE(Status, XASUFW_FAILURE);
+	XFih_Var XFihBufferClear = XFih_VolatileAssignXfihVar(XFIH_FAILURE);
+	CREATE_VOLATILE(ClearStatus, XASUFW_FAILURE);
+	XHmac *HmacPtr = XHmac_GetInstance();
+	static u32 Iterations = 0U;
+	static u32 KdfIndex = 0U;
+	u32 KdfValue;
+	static u32 HashLen = 0U;
+	u8 KOut[XASU_SHA_512_HASH_LEN];
+	static u64 KeyOutAddr = 0U;
+
+	/** Validate input parameters. */
+	if ((DmaPtr == NULL) || (KdfParams == NULL) || (ShaInstancePtr == NULL)) {
+		Status = XASUFW_KDF_INVALID_PARAM;
+		goto END;
+	}
+
+	if ((KdfParams->ContextAddr == 0U) ||
+			(KdfParams->ContextLen == 0U) || (KdfParams->ContextLen > XASU_KDF_MAX_CONTEXT_LEN) ||
+		(KdfParams->KeyOutAddr == 0U) || (KdfParams->KeyOutLen == 0U)) {
+		Status = XASUFW_KDF_INVALID_PARAM;
+		goto END;
+	}
+
+	/** Get SHA hash length based on SHA Mode. */
+	Status = XSha_GetHashLen(KdfParams->ShaMode, &HashLen);
+	if (Status != XASUFW_SUCCESS) {
+		goto END;
+	}
+
+	/**
+	 * Calculate number of iterations based on KDF output key length and SHA algorithm hash length.
+	 * According to algorithm, maximum iterations should not exceed (2^(counter size in bits) - 1).
+	 * As in ASU, we are considering the counter size as 32 bits and taking only 32 bit Iterations
+	 * variable, checking for maximum iterations is not required as this variable always holds
+	 * only in range numbers.
+	 */
+	Iterations = KdfParams->KeyOutLen/HashLen;
+	if ((KdfParams->KeyOutLen % HashLen) != 0x0U) {
+		++Iterations;
+	}
+
+	/** Use HMAC as pseudorandom function, calculate and form the output. */
+	KeyOutAddr = KdfParams->KeyOutAddr;
+	for (KdfIndex = 0U; KdfIndex < Iterations; ++KdfIndex) {
+		ASSIGN_VOLATILE(Status, XASUFW_FAILURE);
+		/** Initialize HMAC with KeyIn provided. */
+		Status = XHmac_Init(HmacPtr, DmaPtr, ShaInstancePtr, KdfParams->KeyInAddr,
+				KdfParams->KeyInLen, KdfParams->ShaMode, HashLen);
+		if (Status != XASUFW_SUCCESS) {
+			goto END_CLR;
+		}
+
+		/**
+		 * As we implement counter mode, give Iteration count as data to HMAC.
+		 * Iteration count should be send in big endian format.
+		 */
+		KdfValue = Xil_Htonl(KdfIndex + 1U);
+		ASSIGN_VOLATILE(Status, XASUFW_FAILURE);
+		Status = XHmac_Update(HmacPtr, DmaPtr, (u64)(UINTPTR)&KdfValue, XASUFW_WORD_LEN_IN_BYTES,
+				XASU_FALSE);
+		if (Status != XASUFW_SUCCESS) {
+			goto END_CLR;
+		}
+
+		/** Update context provided by user to HMAC. */
+		ASSIGN_VOLATILE(Status, XASUFW_FAILURE);
+		Status = XHmac_Update(HmacPtr, DmaPtr, KdfParams->ContextAddr, KdfParams->ContextLen,
+				XASU_TRUE);
+		if (Status != XASUFW_SUCCESS) {
+			goto END_CLR;
+		}
+
+		/** Get final HMAC for the running iteration. */
+		ASSIGN_VOLATILE(Status, XASUFW_FAILURE);
+		Status = XHmac_Final(HmacPtr, DmaPtr, (u32 *)KOut);
+		if (Status != XASUFW_SUCCESS) {
+			goto END_CLR;
+		}
+
+		/**
+		 * HMAC output size is the SHA algorithm hash length. KDF key output can be any legnth.
+		 * Copy the final HMAC of each iteration to the destination key output buffer.
+		 * In last iteration, data to be copied to the destination can be less than or equal to
+		 * SHA algorithm hash length which varies based on the requested key output length.
+		 */
+		if (KdfIndex == (Iterations - 1U)) {
+			HashLen = KdfParams->KeyOutLen - (KdfIndex * HashLen);
+		}
+
+		ASSIGN_VOLATILE(Status, XASUFW_FAILURE);
+		Status = XAsufw_DmaXfr(DmaPtr, (u64)(UINTPTR)KOut, KeyOutAddr, HashLen, 0U);
+		if (Status != XASUFW_SUCCESS) {
+			goto END_CLR;
+		}
+		KeyOutAddr = KeyOutAddr + HashLen;
+	}
+
+	/** Check if the desired number iterations are executed. */
+	if (KdfIndex != Iterations) {
+		Status = XASUFW_KDF_ITERATION_COUNT_MISMATCH;
+	}
+
+END_CLR:
+	/** Zeroize the intermediate KOut buffer. */
+	XFIH_CALL(Xil_SecureZeroize, XFihBufferClear, ClearStatus, KOut, XASU_SHA_512_HASH_LEN);
+	Status = XAsufw_UpdateBufStatus(Status, ClearStatus);
+
+END:
+	return Status;
+}
+/** @} */
---
+++ lib/sw_services/xilasu/src/common/xasu_kdfinfo.h
@@ -0,0 +1,72 @@
+/**************************************************************************************************
+* Copyright (c) 2025 Advanced Micro Devices, Inc. All Rights Reserved.
+* SPDX-License-Identifier: MIT
+**************************************************************************************************/
+
+/*************************************************************************************************/
+/**
+ *
+ * @file xasu_kdfinfo.h
+ *
+ * This file contains the KDF definitions which are common across the client and server
+ *
+ * <pre>
+ * MODIFICATION HISTORY:
+ *
+ * Ver   Who  Date     Changes
+ * ----- ---- -------- ----------------------------------------------------------------------------
+ * 1.0   ma   01/15/25 Initial release
+ *
+ * </pre>
+ *
+ *************************************************************************************************/
+/**
+ * @addtogroup xasu_common_defs Common Defs
+ * @{
+*/
+
+#ifndef XASU_KDFINFO_H
+#define XASU_KDFINFO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*************************************** Include Files *******************************************/
+#include "xil_types.h"
+
+/************************************ Constant Definitions ***************************************/
+/* KDF module command IDs */
+#define XASU_KDF_COMPUTE_SHA2_CMD_ID	(0U) /**< Command ID for KDF compute for SHA2 command */
+#define XASU_KDF_COMPUTE_SHA3_CMD_ID	(1U) /**< Command ID for KDF compute for SHA3 command */
+#define XASU_KDF_KAT_CMD_ID				(2U) /**< Command ID for KDF KAT command */
+#define XASU_KDF_GET_INFO_CMD_ID		(3U) /**< Command ID for KDF Get Info command */
+
+#define XASU_KDF_MAX_CONTEXT_LEN		(1024U) /**< Maximum context length */
+
+/************************************** Type Definitions *****************************************/
+/**
+ * @brief This structure contains KDF params info
+ */
+typedef struct {
+	u64 KeyInAddr; /**< Address of the input key buffer */
+	u64 ContextAddr; /**< Address of the buffer holding the fixed input data */
+	u64 KeyOutAddr; /**< Address of the buffer to hold the keying material output from KDF */
+	u32 KeyInLen; /**< Length of the input key */
+	u32 ContextLen; /**< Length of the Context */
+	u32 KeyOutLen; /**< Length of the keying material output to be generated from KDF */
+	u8 ShaType; /**< Hash family type (SHA2/SHA3) */
+	u8 ShaMode; /**< Sha mode (SHA256/SHA384/SHA512/SHAKE256) */
+} XAsu_KdfParams;
+
+/*************************** Macros (Inline Functions) Definitions *******************************/
+
+/************************************ Function Prototypes ****************************************/
+
+/************************************ Variable Definitions ***************************************/
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* XASU_KDFINFO_H */
+/** @} */
