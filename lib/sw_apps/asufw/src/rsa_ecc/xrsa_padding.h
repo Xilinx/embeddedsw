9d29f80d4f64162ae359abc95708531d860317f4
Sai Srinivas Sirimisetty <SaiSrinivas.Sirimisetti@amd.com>
2025-02-03 23:07:50 -1200

    sw_apps:asufw:Added driver files for RSA padding
    Added seperate files for padding related API's for generic use.
    
    Signed-off-by: Sai Srinivas Sirimisetty <SaiSrinivas.Sirimisetti@amd.com>
    
    Acked-for-series: Durga Challa <durga.challa@amd.com>

---
+++ lib/sw_apps/asufw/src/rsa_ecc/xrsa_padding.h
@@ -0,0 +1,62 @@
+/**************************************************************************************************
+* Copyright (C) 2025 Advanced Micro Devices, Inc. All Rights Reserved.
+* SPDX-License-Identifier: MIT
+**************************************************************************************************/
+
+/*************************************************************************************************/
+/**
+*
+* @file xrsa_padding.h
+* This file contains declarations for xrsa_padding.c file.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who  Date        Changes
+* ----- ---- -------- -----------------------------------------------------------------------------
+* 1.0   ss   02/04/25 Initial Release
+*
+* </pre>
+*
+**************************************************************************************************/
+/**
+* @addtogroup xasufw_application ASUFW Functionality
+* @{
+*/
+
+#ifndef XRSA_PADDING_H_
+#define XRSA_PADDING_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*************************************** Include Files *******************************************/
+#include "xrsa.h"
+#include "xsha.h"
+#include "xasu_rsainfo.h"
+
+/************************************ Constant Definitions ***************************************/
+
+/************************************** Type Definitions *****************************************/
+
+/*************************** Macros (Inline Functions) Definitions *******************************/
+
+/************************************ Function Prototypes ****************************************/
+s32 XRsa_OaepEncode(XAsufw_Dma *DmaPtr, XSha *ShaInstancePtr,
+		    const XAsu_RsaOaepPaddingParams *PaddingParamsPtr);
+s32 XRsa_OaepDecode(XAsufw_Dma *DmaPtr, XSha *ShaInstancePtr,
+		    const XAsu_RsaOaepPaddingParams *PaddingParamsPtr);
+s32 XRsa_PssEncode(XAsufw_Dma *DmaPtr, XSha *ShaInstancePtr,
+		   const XAsu_RsaPaddingParams *PaddingParamsPtr);
+s32 XRsa_PssDecode(XAsufw_Dma *DmaPtr, XSha *ShaInstancePtr,
+		   const XAsu_RsaPaddingParams *PaddingParamsPtr);
+
+/************************************ Variable Definitions ***************************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* XRSA_PADDING_H_ */
+/** @} */
