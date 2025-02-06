a31163aa912fceb5728df6645b33a765f2a24f99
Kopparapu,Mounika <Mounika.Kopparapu@amd.com>
2025-02-06 14:40:45 +0530

    Revert "sw_apps: asufw: Fix typo in macro definition"
    This reverts commit e321699d4f2342051c64b63def70683d69be45d7.

--- lib/sw_apps/asufw/src/trng/xtrng_tests.c
+++ lib/sw_apps/asufw/src/trng/xtrng_tests.c
@@ -18,7 +18,6 @@
  * 1.0   ma   05/20/24 Initial release
  *       ma   07/26/24 Added platform check to skip DRBG KAT on QEMU
  *       yog  09/26/24 Added doxygen groupings and fixed doxygen comments.
- * 1.1   ma   02/04/25 Fixed typo in macros
  *
  * </pre>
  *
@@ -34,7 +33,7 @@
 #include "xasufw_hw.h"
 
 /************************************ Constant Definitions ***************************************/
-#define XTRNG_KAT_DEFAULT_DF_LENGTH 7U /**<Default Derivative function length of TRNG KAT*/
+#define XTRNG_KAT_DEFAULT_DF_lENGTH 7U /**<Default Derivative function length of TRNG KAT*/
 #define XTRNG_KAT_DEFAULT_SEED_LIFE 2U /**<Default seed life of TRNG KAT*/
 #define XTRNG_KAT_SEED_LEN_IN_BYTES 128U /**<Seed length in bytes of TRNG KAT*/
 
@@ -125,7 +124,7 @@ s32 XTrng_DrbgKat(XTrng *InstancePtr)
 		goto END;
 	}
 
-	UsrCfg.DFLength = XTRNG_KAT_DEFAULT_DF_LENGTH;
+	UsrCfg.DFLength = XTRNG_KAT_DEFAULT_DF_lENGTH;
 	UsrCfg.Mode = XTRNG_DRBG_MODE;
 	UsrCfg.SeedLife = XTRNG_KAT_DEFAULT_SEED_LIFE;
 	UsrCfg.IsBlocking = XASU_TRUE;
