04fd9b8d15588ae3b03c451eb87b0c965d9d8115
Kunal Rane <kunal.rane@amd.com>
2025-02-11 23:26:23 -1200

    csi2tx: data: Match yaml properties with IP
    Match yaml properties with IP properties for effective
    generation of _g.c file.
    
    Signed-off-by: Kunal Rane <kunal.rane@amd.com>
    Acked-by: Gaddipati, Naveen <naveen.gaddipati@amd.com>

--- XilinxProcessorIPLib/drivers/csi2tx/src/xcsi2tx_g.c
+++ XilinxProcessorIPLib/drivers/csi2tx/src/xcsi2tx_g.c
@@ -1,3 +1,8 @@
+###############################################################################
+# Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+# SPDX-License-Identifier: MIT
+#
+##############################################################################
 #include "xcsi2tx.h"
 
 XCsi2Tx_Config XCsi2Tx_ConfigTable[] __attribute__ ((section (".drvcfg_sec"))) = {
@@ -5,9 +10,9 @@ XCsi2Tx_Config XCsi2Tx_ConfigTable[] __attribute__ ((section (".drvcfg_sec"))) =
 	{
 		"xlnx,mipi-csi2-tx-ctrl-1.0", /* compatible */
 		0x0, /* reg */
-		0x0, /* xlnx,csi2-tx-lanes */
-		0x0, /* xlnx,csi2-tx-en-active-lanes */
-		0x0 /* xlnx,csi2-tx-en-reg-based-fe-gen */
+		0x4, /* xlnx,csi-lanes */
+		0x0, /* xlnx,csi-en-activelanes */
+		0x0 /* xlnx,en-reg-based-fe-gen */
 	},
 	 {
 		 NULL
