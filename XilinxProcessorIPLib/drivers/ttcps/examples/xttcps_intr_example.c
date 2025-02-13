23b0c6ef5859fef5a9ab099fdd82fd173ef47f0a
M Lakshmaiah <Lakshmaiah.M@amd.com>
2025-02-12 20:38:15 -1200

    drivers: Fix compilation errors and warnings
    - Resolved invalid function pointer conversion from void (*)(void*) to void*
      by explicitly casting function pointers in XSetupInterruptSystem().
    - Suppressed unused parameter warnings in TickHandler() and PWMHandler()
      by using (void)CallBackRef to explicitly mark them as unused.
    - Ensured correct function signatures and parameter usage for clean compilation.
    
    Fixed the following errors and warnings:
    error: invalid conversion from 'void (*)(void*)' to 'void*' [-fpermissive]
    error: invalid conversion from 'u32 (*)(XTtcPs*)' {aka 'unsigned int (*)(XTtcPs*)'}
           to 'void*' [-fpermissive]
    warning: unused parameter 'CallBackRef' [-Wunused-parameter]
    warning: unused parameter 'BaseAddr' [-Wunused-parameter]
    warning: 'GicConfig' defined but not used [-Wunused-variable]
    
    Signed-off-by: M Lakshmaiah <lakshmaiah.m@amd.com>
    
    Acked-for-series: Mubin Sayyed <mubin.sayyed@amd.com>

--- XilinxProcessorIPLib/drivers/ttcps/examples/xttcps_intr_example.c
+++ XilinxProcessorIPLib/drivers/ttcps/examples/xttcps_intr_example.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2010 - 2021 Xilinx, Inc.  All rights reserved.
-* Copyright (c) 2022 - 2024 Advanced Micro Devices, Inc. All Rights Reserved.
+* Copyright (c) 2022 - 2025 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -44,6 +44,7 @@
 * 		       timeout loop.
 * 3.18 gm     07/25/23 Invoke XTtcPs_Release to release ttc node.
 * 3.18 ml     03/06/24 Added support for system device-tree flow.
+* 3.21 ml     02/05/25 Fixed compilation warnings and errors
 *
 *</pre>
 ******************************************************************************/
@@ -373,7 +374,7 @@ int SetupTicker(void)
 #else
 	TtcPsTick = &(TtcPsInst[Index_Tick]);
 
-	Status = XSetupInterruptSystem(TtcPsTick,XTtcPs_InterruptHandler,
+	Status = XSetupInterruptSystem(TtcPsTick,(void *)XTtcPs_InterruptHandler,
                         TtcPsTick->Config.IntrId[0],
                         TtcPsTick->Config.IntrParent,
                         XINTERRUPT_DEFAULT_PRIORITY);
@@ -464,7 +465,7 @@ int SetupPWM(void)
 #else
 	TtcPsPWM = &(TtcPsInst[Index_PWM]);
 
-	Status = XSetupInterruptSystem(TtcPsPWM,XTtcPs_InterruptHandler,
+	Status = XSetupInterruptSystem(TtcPsPWM,(void *)XTtcPs_InterruptHandler,
                         TtcPsPWM->Config.IntrId[0],
                         TtcPsPWM->Config.IntrParent,
                         XINTERRUPT_DEFAULT_PRIORITY);
@@ -726,6 +727,7 @@ static int SetupInterruptSystem(u16 IntcDeviceID,
 *****************************************************************************/
 static void TickHandler(void *CallBackRef, u32 StatusEvent)
 {
+	(void)CallBackRef;
 	if (0 != (XTTCPS_IXR_INTERVAL_MASK & StatusEvent)) {
 		TickCount++;
 
@@ -763,6 +765,7 @@ static void TickHandler(void *CallBackRef, u32 StatusEvent)
 *****************************************************************************/
 static void PWMHandler(void *CallBackRef, u32 StatusEvent)
 {
+	(void)CallBackRef;
 	XTtcPs *Timer;
 
 #ifndef SDT
