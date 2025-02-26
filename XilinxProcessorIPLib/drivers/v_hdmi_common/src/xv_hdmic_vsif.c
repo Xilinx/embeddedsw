10552c960ece0315f2fa76059e92e5988f152b33
Gugulothu, Rajesh <rajesh.gugulothu@amd.com>
2025-02-26 05:37:06 -1200

    v_hdmi_common: Add support for HF-VSIF
    Add support for HDMI Forum vendor specific infoframe
    
    Signed-off-by: Rajesh Gugulothu <rajesh.gugulothu@amd.com>
    Acked-by: Gaddipati, Naveen <naveen.gaddipati@amd.com>

--- XilinxProcessorIPLib/drivers/v_hdmi_common/src/xv_hdmic_vsif.c
+++ XilinxProcessorIPLib/drivers/v_hdmi_common/src/xv_hdmic_vsif.c
@@ -1,5 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2014 - 2021 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2022 - 2025 Advanced Micro Devices, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -266,6 +267,255 @@ static XVidC_3DSamplingPosition XV_HdmiC_VSIF_Conv3DSampPosTo3DSampPos(XHdmiC_3D
 /*****************************************************************************/
 /**
 *
+* This function parses a HDMI Vendor Specific InfoFrame (VSIF).
+*
+* @param AuxPtr is a pointer to the XV_HdmiTx_Rx_Aux instance.
+*
+* @param VSIFPtr is a pointer to the XV_HdmiC_VSIF instance.
+*
+* @return
+*  - XST_SUCCESS if operation was successful
+*  - XST_FAILURE if an error was detected during parsing
+*
+******************************************************************************/
+int XV_HdmiC_Parse_HDMI_VSIF(XHdmiC_Aux *AuxPtr, XHdmiC_VSIF  *VSIFPtr)
+{
+    int Status = XST_FAILURE;
+    u8 *pData;
+    u32 temp;
+
+    pData = &AuxPtr->Data.Byte[0];
+
+    /* HDMI Video Format */
+    temp = (pData[4] & XHDMIC_VSIF_VIDEO_FORMAT_MASK) >> XHDMIC_VSIF_VIDEO_FORMAT_SHIFT;
+    if (temp >= XHDMIC_VSIF_VF_UNKNOWN) {
+        VSIFPtr->Format = XHDMIC_VSIF_VF_UNKNOWN;
+    } else {
+        VSIFPtr->Format = (XHdmiC_VSIF_Video_Format)temp;
+    }
+
+    switch (VSIFPtr->Format) {
+    /* HDMI VIC */
+    case XHDMIC_VSIF_VF_EXTRES:
+        VSIFPtr->HDMI_VIC = pData[5];
+        Status = XST_SUCCESS;
+        break;
+        /* 3D Information */
+    case XHDMIC_VSIF_VF_3D:
+        Status = XV_HdmiC_VSIF_Extract3DInfo(pData, &VSIFPtr->Info_3D);
+        break;
+        /* No additional information */
+    case XHDMIC_VSIF_VF_NOINFO:
+        Status = XST_SUCCESS;
+        break;
+
+    default:
+
+        break;
+    }
+
+    return Status;
+}
+
+static void XV_HdmiC_RemoveECCBytes(XHdmiC_Aux *AuxPtr)
+{
+    u8 DataSize = AuxPtr->Header.Byte[2];
+    u8 Dest[DataSize - 3];
+    int Index = 0;
+
+    for (int i = 0; i < DataSize; i++) {
+        /* Skip every 8th byte (7, 15, 23, 31) */
+        if ((i + 1) % 8 != 0) {
+            Dest[Index++] = AuxPtr->Data.Byte[i];
+        }
+    }
+    memcpy(AuxPtr->Data.Byte, Dest, Index);
+
+}
+
+/*****************************************************************************/
+/**
+*
+* This function parses a HDMI forum Vendor Specific InfoFrame (HF-VSIF).
+*
+* @param AuxPtr is a pointer to the XV_HdmiTx_Rx_Aux instance.
+*
+* @param VSIFPtr is a pointer to the XV_HdmiC_VSIF instance.
+*
+* @return
+*  - XST_SUCCESS if operation was successful
+*  - XST_FAILURE if an error was detected during parsing
+*
+******************************************************************************/
+
+int XV_HdmiC_Parse_HF_VSIF(XHdmiC_Aux *AuxPtr, XHdmiC_VSIF  *VSIFPtr)
+{
+    u8 *pData;
+    u8 temp;
+    u8 Len;
+    int Status = XST_FAILURE;
+    XHdmiC_3D_Info *Info_3D =  &VSIFPtr->Info_3D;
+
+    /* Clear the instance */
+    (void)memset((void *)&VSIFPtr->Info_3D, 0, sizeof(XHdmiC_3D_Info));
+
+    XV_HdmiC_RemoveECCBytes(AuxPtr);
+    pData = &AuxPtr->Data.Byte[5];
+
+    /* Parse 3D_Valid */
+    VSIFPtr->Is_3D_Valid = *pData & XHDMIC_HFVSIF_3D_VALID_MASK;
+    /* Parse ALLM (Auto Low-Latency Mode) */
+    VSIFPtr->ALLM_Mode = (*pData & XHDMIC_HFVSIF_ALLM_MASK) >> XHDMIC_HFVSIF_ALLM_SHIFT;
+    /* Parse CCBPC (Color Content Bits Per Component) */
+    temp = (*pData & XHDMIC_HFVSIF_CCBPC_MASK) >> XHDMIC_HFVSIF_CCBPC_SHIFT;
+    if (!temp) {
+        /* No Indication */
+        VSIFPtr->CCBPC = 0;
+    } else if (temp >= 1 && temp <= 9) {
+        /* Number of meaningful bits per component */
+        VSIFPtr->CCBPC = temp + 7;
+    } else {
+        VSIFPtr->CCBPC = -1;
+    }
+
+    pData++;
+    /* Detect 3D Metadata presence */
+    if (*pData & XHDMIC_HFVSIF_3D_META_PRESENT_MASK) {
+        Info_3D->MetaData.IsPresent = TRUE;
+    } else {
+        Info_3D->MetaData.IsPresent = FALSE;
+    }
+    /* Detect 3D Disparity Data presence */
+    if (*pData & XHDMIC_HFVSIF_3D_DISP_PRESENT_MASK) {
+        Info_3D->DisparityData.IsPresent = TRUE;
+    } else {
+        Info_3D->DisparityData.IsPresent = FALSE;
+    }
+    /* Detect 3D Additional Info presence */
+    if (*pData & XHDMIC_HFVSIF_3D_ADDINFO_PRESENT_MASK) {
+        Info_3D->Additionalinfo.IsPresent = TRUE;
+    } else {
+        Info_3D->Additionalinfo.IsPresent = FALSE;
+    }
+    if (VSIFPtr->Is_3D_Valid) {
+        /* Extract the 3D_F_Structure */
+        temp = (*pData & XHDMIC_3D_STRUCT_MASK) >> XHDMIC_3D_STRUCT_SHIFT;
+        if (temp >= XHDMIC_3D_STRUCT_UNKNOWN || temp == 7) {
+            Info_3D->Stream.Format =
+                    XV_HdmiC_VSIF_Conv3DStructTo3DFormat(XHDMIC_3D_STRUCT_UNKNOWN);
+        } else {
+            Info_3D->Stream.Format =
+                    XV_HdmiC_VSIF_Conv3DStructTo3DFormat((XHdmiC_3D_Struct_Field)temp);
+}
+    }
+    /* Extract the 3D_F_Ext_Data */
+    if (temp >= XHDMIC_3D_STRUCT_SIDE_BY_SIDE_HALF && VSIFPtr->Is_3D_Valid) {
+        pData++;
+        temp = (*pData & XHDMIC_3D_SAMP_METHOD_MASK) >> XHDMIC_3D_SAMP_METHOD_SHIFT;
+        if (temp >= XHDMIC_3D_SAMPLING_UNKNOWN) {
+            Info_3D->Stream.Sampling.Method =
+                    XV_HdmiC_VSIF_Conv3DSampMethodTo3DSampMethod(XHDMIC_3D_SAMPLING_UNKNOWN);
+        } else {
+            Info_3D->Stream.Sampling.Method =
+                    XV_HdmiC_VSIF_Conv3DSampMethodTo3DSampMethod((XHdmiC_3D_Sampling_Method)temp);
+        }
+        temp = (*pData & XHDMIC_3D_SAMP_POS_MASK) >> XHDMIC_3D_SAMP_POS_SHIFT;
+        if (temp >= XHDMIC_3D_SAMPPOS_UNKNOWN) {
+            Info_3D->Stream.Sampling.Position =
+                    XV_HdmiC_VSIF_Conv3DSampPosTo3DSampPos(XHDMIC_3D_SAMPPOS_UNKNOWN);
+        } else {
+            Info_3D->Stream.Sampling.Position =
+                    XV_HdmiC_VSIF_Conv3DSampPosTo3DSampPos((XHdmiC_3D_Sampling_Position)temp);
+        }
+    }
+    /* Extract the 3D_AdditionalInfo data */
+    if (Info_3D->Additionalinfo.IsPresent) {
+        pData++;
+        Info_3D->Additionalinfo.Preferred2DView =
+                (*pData & XHDMIC_HFVSIF_3D_PREFERRED_VIEW_MASK);
+        Info_3D->Additionalinfo.ViewDependency =
+                 ((*pData & XHDMIC_HFVSIF_3D_VIEW_DEPENDENCY_MASK) >>
+                 XHDMIC_HFVSIF_3D_VIEW_DEPENDENCY_SHIFT);
+        Info_3D->Additionalinfo.DualView =
+                 ((*pData & XHDMIC_HFVSIF_3D_DUAL_VIEW_MASK) >>
+                 XHDMIC_HFVSIF_3D_DUAL_VIEW_SHIFT);
+    }
+    /* Extract the 3D_DiparityData */
+    if (Info_3D->DisparityData.IsPresent) {
+        pData++;
+        Info_3D->DisparityData.Length =
+                (*pData & XHDMIC_HFVSIF_3D_DISPARITY_LENGTH_MASK);
+        Info_3D->DisparityData.Version =
+                ((*pData >> XHDMIC_HFVSIF_3D_DISPARITY_VERSION_SHIFT) &
+                XHDMIC_HFVSIF_3D_DISPARITY_VERSION_MASK);
+        switch (Info_3D->DisparityData.Version) {
+        case XHDMIC_3D_DISPARITY_INFO_UNKNOWN:
+             break;
+        case XHDMIC_3D_PROD_DISPARITY_HINT_INFO:
+            for (int i = 0; i < XHDMIC_3D_DISPARITY_VERSION1_LENGTH; i++)
+                Info_3D->DisparityData.Video_Min_Max_Disparity_Hint[i] = *(++pData);
+
+            Status = XST_SUCCESS;
+            break;
+        case XHDMIC_3D_MULTI_REGION_DISPARITY_INFO:
+            Info_3D->DisparityData.Multi_Region_Disparity_Length = *(++pData);
+            Len = Info_3D->DisparityData.Multi_Region_Disparity_Length;
+            /* value 0x1 is prohibited and 0x11 is the max value */
+            if (Len > 0x1 && (Len < (XHDMIC_3D_DISPARITY_DATA_MAX_LENGTH - 1))) {
+                Info_3D->DisparityData.Max_Disparity_In_Picture = *(++pData);
+                /* Refer Table 7-10 in HDMI 2.1b spec */
+                for (int i = 0; i < (Len - 2); i++) {
+                    Info_3D->DisparityData.Min_Disparity_In_Region[i] = *(++pData);
+                }
+            }
+            Status = XST_SUCCESS;
+            break;
+        case XHDMIC_3D_PROD_AND_MULTI_DIPARITY_INFO:
+            /* First three bytes are production disparity hint info */
+            for (int i = 0; i < XHDMIC_3D_DISPARITY_VERSION1_LENGTH; i++) {
+               Info_3D->DisparityData.Video_Min_Max_Disparity_Hint[i] = *(++pData);
+            }
+            Info_3D->DisparityData.Multi_Region_Disparity_Length = *(++pData);
+            Len = Info_3D->DisparityData.Multi_Region_Disparity_Length;
+            //TODO: check this length
+            /* Refer Table 7-10 in HDMI 2.1b spec */
+            for (int i = 0; i < (Len - 2); i++) {
+               Info_3D->DisparityData.Min_Disparity_In_Region[i] = *(++pData);
+            }
+            Status = XST_SUCCESS;
+            break;
+        default:
+            Status = XST_FAILURE;
+            break;
+          }
+    }
+
+    /* Extract the 3D_Metadata */
+    if (Info_3D->MetaData.IsPresent) {
+        pData++;
+        temp = (*pData & XHDMIC_3D_META_TYPE_MASK) >> XHDMIC_3D_META_TYPE_SHIFT;
+        if (temp >= XHDMIC_3D_META_UNKNOWN) {
+            Info_3D->MetaData.Type = XHDMIC_3D_META_UNKNOWN;
+        } else {
+            Info_3D->MetaData.Type = (XHdmiC_3D_MetaData_Type)temp;
+        }
+
+        Info_3D->MetaData.Length = (*pData & XHDMIC_3D_META_LENGTH_MASK) >>
+                                   XHDMIC_3D_META_LENGTH_SHIFT;
+        for (int i = 0; i < Info_3D->MetaData.Length && i < XHDMIC_3D_META_MAX_SIZE; i++) {
+            Info_3D->MetaData.Data[i] = *(pData + i + 1);
+        }
+        if (Info_3D->MetaData.Length > XHDMIC_3D_META_MAX_SIZE) {
+            return XST_FAILURE;
+        }
+    }
+
+    return Status;
+}
+
+/*****************************************************************************/
+/**
+*
 * This function parses a Vendor Specific InfoFrame (VSIF).
 *
 * @param AuxPtr is a pointer to the XV_HdmiTx_Rx_Aux instance.
@@ -303,41 +553,17 @@ int XV_HdmiC_VSIF_ParsePacket(XHdmiC_Aux *AuxPtr, XHdmiC_VSIF  *VSIFPtr)
     }
 
     /*
-     * Check if the IEEE_ID is 0x000C03 as per
-     * Table H1 from HDMI 1.4b spec
+     * Check if the IEEE_ID is 0x000C03 (as per Table H1 from HDMI 1.4b spec)
+     * or 0xD85DC4(as per the Table 10-5 from HDMI 2.1B spec)
+     *
      */
-    if (VSIFPtr->IEEE_ID != 0xC03)
-	    return XST_FAILURE;
-
-    /* HDMI Video Format */
-    temp = (pData[4] & XHDMIC_VSIF_VIDEO_FORMAT_MASK) >> XHDMIC_VSIF_VIDEO_FORMAT_SHIFT;
-    if (temp >= XHDMIC_VSIF_VF_UNKNOWN) {
-        VSIFPtr->Format = XHDMIC_VSIF_VF_UNKNOWN;
-    }
-    else {
-        VSIFPtr->Format = (XHdmiC_VSIF_Video_Format)temp;
-    }
-
-    switch(VSIFPtr->Format) {
-        /* HDMI VIC */
-        case XHDMIC_VSIF_VF_EXTRES :
-            VSIFPtr->HDMI_VIC = pData[5];
-            Status = XST_SUCCESS;
-            break;
-
-        /* 3D Information */
-        case XHDMIC_VSIF_VF_3D :
-            Status = XV_HdmiC_VSIF_Extract3DInfo(pData, &VSIFPtr->Info_3D);
-            break;
-
-        /* No additional information */
-        case XHDMIC_VSIF_VF_NOINFO :
-            Status = XST_SUCCESS;
-            break;
-
-        default :
-
-            break;
+    switch (VSIFPtr->IEEE_ID) {
+    case XHDMIC_HDMI_VSIF_IEEE:
+        Status = XV_HdmiC_Parse_HDMI_VSIF(AuxPtr, VSIFPtr);
+        break;
+    case XHDMIC_HF_VSIF_IEEE:
+        Status = XV_HdmiC_Parse_HF_VSIF(AuxPtr, VSIFPtr);
+        break;
     }
 
     return Status;
@@ -438,6 +664,172 @@ int XV_HdmiC_VSIF_Extract3DInfo(u8 *VSIFRaw, XHdmiC_3D_Info *InstancePtr)
     }
 
     return XST_SUCCESS;
+}
+
+static void XV_HdmiC_InsertECCBytes(XHdmiC_Aux *AuxPtr, u8 *ByteCount)
+{
+    u8 temp[32];
+    int j = 0;
+
+    for (int i = 0; i < *ByteCount; i++) {
+        if ((i + 1) % 8 != 0) {
+            temp[i] = AuxPtr->Data.Byte[j++];
+        } else {
+            temp[i] = 0x0;
+            (*ByteCount)++;
+        }
+    }
+    memcpy(AuxPtr->Data.Byte, temp, *ByteCount);
+}
+
+/*****************************************************************************/
+/**
+*
+* This function generates a HDMI fourm Vendor Specific InfoFrame (HF-VSIF).
+*
+* @param VSIFPtr is a pointer to the XV_HdmiC_VSIF instance.
+*
+* @param AuxPtr is a pointer to the XV_HdmiTx_Tx_Aux instance.
+*
+* @return
+*  - XST_SUCCESS if operation was successful
+*  - XST_FAILURE if an error was detected during generation
+*
+******************************************************************************/
+XHdmiC_Aux XV_HdmiC_HF_VSIF_GeneratePacket(XHdmiC_VSIF  *VSIFPtr)
+{
+    u8 Index = 0;
+    u8 ByteCount = 0;
+    u8 Crc = 0;
+    u8 temp;
+    XHdmiC_Aux Aux;
+    XHdmiC_3D_Info *Info_3D =  &VSIFPtr->Info_3D;
+
+    (void)memset((void *)&Aux, 0, sizeof(XHdmiC_Aux));
+
+    XHdmiC_3D_Struct_Field Format;
+    XHdmiC_3D_Sampling_Method SampMethod;
+    XHdmiC_3D_Sampling_Position SampPos;
+
+    /* Header, Packet type*/
+    Aux.Header.Byte[0] = AUX_VSIF_TYPE;
+
+    /* Version */
+    Aux.Header.Byte[1] = VSIFPtr->Version;
+
+    /* Checksum (this will be calculated by the HDMI TX IP) */
+    Aux.Header.Byte[3] = 0;
+
+    /* Data */
+
+    /* IEEE Registration ID */
+    Aux.Data.Byte[++ByteCount] = VSIFPtr->IEEE_ID;		/* PB1 */
+    Aux.Data.Byte[++ByteCount] = VSIFPtr->IEEE_ID >> 8;		/* PB2 */
+    Aux.Data.Byte[++ByteCount] = VSIFPtr->IEEE_ID >> 16;	/* PB3 */
+
+    Aux.Data.Byte[++ByteCount] = 0x1;
+    Aux.Data.Byte[++ByteCount] = ((VSIFPtr->Is_3D_Valid) |(VSIFPtr->ALLM_Mode << 1) |
+				(VSIFPtr->CCBPC << 4));
+    Aux.Data.Byte[++ByteCount] |= (Info_3D->MetaData.IsPresent << 1) & (0x1 << 1);
+    Aux.Data.Byte[ByteCount] |= (Info_3D->DisparityData.IsPresent << 2) & (0x1 << 2);
+    Aux.Data.Byte[ByteCount] |= (Info_3D->Additionalinfo.IsPresent << 3) & (0x1 << 3);
+    Format = XV_HdmiC_VSIF_Conv3DInfoTo3DStruct(&VSIFPtr->Info_3D.Stream);
+    Aux.Data.Byte[ByteCount] |= (Format << XHDMIC_3D_STRUCT_SHIFT) & XHDMIC_3D_STRUCT_MASK;
+
+    /* 3D_Ext_Data */
+    if (Format >= XHDMIC_3D_STRUCT_SIDE_BY_SIDE_HALF && VSIFPtr->Is_3D_Valid) {
+        SampMethod = XV_HdmiC_VSIF_Conv3DInfoTo3DSampMethod(&VSIFPtr->Info_3D.Stream);
+        Aux.Data.Byte[++ByteCount] = (SampMethod << XHDMIC_3D_SAMP_METHOD_SHIFT) &
+                XHDMIC_3D_SAMP_METHOD_MASK;
+        SampPos = XV_HdmiC_VSIF_Conv3DInfoTo3DSampPos(&VSIFPtr->Info_3D.Stream);
+        Aux.Data.Byte[ByteCount] |= (SampPos << XHDMIC_3D_SAMP_POS_SHIFT) &
+                XHDMIC_3D_SAMP_POS_MASK;
+    }
+    /* 3D_Additionalinfo */
+    if (Info_3D->Additionalinfo.IsPresent) {
+        Aux.Data.Byte[++ByteCount] = (Info_3D->Additionalinfo.Preferred2DView &
+                XHDMIC_HFVSIF_3D_PREFERRED_VIEW_MASK);
+        Aux.Data.Byte[ByteCount] |= (Info_3D->Additionalinfo.ViewDependency <<
+                XHDMIC_HFVSIF_3D_VIEW_DEPENDENCY_SHIFT) &
+                XHDMIC_HFVSIF_3D_VIEW_DEPENDENCY_MASK;
+        Aux.Data.Byte[ByteCount] |= (Info_3D->Additionalinfo.DualView <<
+                XHDMIC_HFVSIF_3D_DUAL_VIEW_SHIFT) &
+                XHDMIC_HFVSIF_3D_DUAL_VIEW_MASK;
+    }
+    if (Info_3D->DisparityData.IsPresent) {
+        Aux.Data.Byte[++ByteCount] = (Info_3D->DisparityData.Length &
+                XHDMIC_HFVSIF_3D_DISPARITY_LENGTH_MASK);
+        Aux.Data.Byte[ByteCount] |= ((Info_3D->DisparityData.Version << 5) & 0xE0);
+
+        switch (Info_3D->DisparityData.Version) {
+        case XHDMIC_3D_DISPARITY_INFO_UNKNOWN:
+            break;
+        case XHDMIC_3D_PROD_DISPARITY_HINT_INFO:
+            for (int i = 0; i < XHDMIC_3D_DISPARITY_VERSION1_LENGTH; i++)
+                Aux.Data.Byte[++ByteCount] =
+                        Info_3D->DisparityData.Video_Min_Max_Disparity_Hint[i];
+            break;
+        case XHDMIC_3D_MULTI_REGION_DISPARITY_INFO:
+            temp = Info_3D->DisparityData.Multi_Region_Disparity_Length;
+            /* value 0x1 is prohibited and 0x11 is the max value */
+            if (temp > 0x1 && (temp < (XHDMIC_3D_DISPARITY_DATA_MAX_LENGTH - 1))) {
+               Aux.Data.Byte[++ByteCount] =
+                       Info_3D->DisparityData.Max_Disparity_In_Picture;
+               /* Refer Table 7-10 in HDMI 2.1b spec */
+               for (int i = 0; i < (temp - 2); i++) {
+                   Aux.Data.Byte[++ByteCount] =
+                           Info_3D->DisparityData.Min_Disparity_In_Region[i];
+               }
+            }
+            break;
+        case XHDMIC_3D_PROD_AND_MULTI_DIPARITY_INFO:
+            /* First three bytes are production disparity hint info */
+            for (int i = 0; i < XHDMIC_3D_DISPARITY_VERSION1_LENGTH; i++) {
+                Aux.Data.Byte[++ByteCount] =
+                        Info_3D->DisparityData.Video_Min_Max_Disparity_Hint[i];
+                Aux.Data.Byte[++ByteCount] =
+                        Info_3D->DisparityData.Multi_Region_Disparity_Length;
+                Aux.Data.Byte[++ByteCount] =
+                        Info_3D->DisparityData.Multi_Region_Disparity_Length;
+                /* Refer Table 7-10 in HDMI 2.1b spec */
+                for (int i = 0; i < 2; i++) {
+                    Aux.Data.Byte[++ByteCount] =
+                            Info_3D->DisparityData.Min_Disparity_In_Region[i];
+                }
+           }
+           break;
+        default:
+           break;
+        }
+    }
+
+    /* 3D Metadata */
+    if (Info_3D->MetaData.IsPresent) {
+        Aux.Data.Byte[++ByteCount] = (Info_3D->MetaData.Type << XHDMIC_3D_META_TYPE_SHIFT) &
+                                     XHDMIC_3D_META_TYPE_MASK;
+        Aux.Data.Byte[ByteCount] |= (Info_3D->MetaData.Length << XHDMIC_3D_META_LENGTH_SHIFT) &
+                                    XHDMIC_3D_META_LENGTH_MASK;
+        for (Index = 0; Index < (Info_3D->MetaData.Length + 1); Index++) {
+            Aux.Data.Byte[++ByteCount] = Info_3D->MetaData.Data[Index];
+        }
+    }
+    XV_HdmiC_InsertECCBytes(&Aux, &ByteCount);
+    /* Set the payload length */
+    Aux.Header.Byte[2] = ByteCount;
+    /* Calculate checksum */
+    /* Header */
+    for (Index = 0; Index < 3; Index++) {
+        Crc += Aux.Header.Byte[Index];
+    }
+    /* Data */
+    for (Index = 1; Index <= ByteCount; Index++) {
+        Crc += Aux.Data.Byte[Index];
+    }
+
+    /* Set the checksum */
+    Aux.Data.Byte[0] = 256 - Crc;
+
+    return Aux;
 }
 
 /*****************************************************************************/
--- XilinxProcessorIPLib/drivers/v_hdmi_common/src/xv_hdmic_vsif.h
+++ XilinxProcessorIPLib/drivers/v_hdmi_common/src/xv_hdmic_vsif.h
@@ -1,5 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2014 - 2021 Xilinx, Inc.  All rights reserved.
+* Copyright (C) 2022 - 2025 Advanced Micro Devices, Inc.  All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -40,6 +41,23 @@ extern "C" {
 /** @name Vendor Specific InfoFrame Field Masks and Shifts.
  * @{
  */
+#define XHDMIC_HFVSIF_3D_VALID_MASK	0x01
+
+#define XHDMIC_HFVSIF_ALLM_SHIFT	1
+#define XHDMIC_HFVSIF_ALLM_MASK	0x02
+
+#define XHDMIC_HFVSIF_CCBPC_SHIFT	4
+#define XHDMIC_HFVSIF_CCBPC_MASK	0xF0
+
+#define XHDMIC_HFVSIF_3D_META_PRESENT_SHIFT 1
+#define XHDMIC_HFVSIF_3D_META_PRESENT_MASK	(0x1 << XHDMIC_HFVSIF_3D_META_PRESENT_SHIFT)
+
+#define XHDMIC_HFVSIF_3D_DISP_PRESENT_SHIFT 2
+#define XHDMIC_HFVSIF_3D_DISP_PRESENT_MASK	(0x1 << XHDMIC_HFVSIF_3D_DISP_PRESENT_SHIFT)
+
+#define XHDMIC_HFVSIF_3D_ADDINFO_PRESENT_SHIFT 3
+#define XHDMIC_HFVSIF_3D_ADDINFO_PRESENT_MASK	(0x1 << XHDMIC_HFVSIF_3D_ADDINFO_PRESENT_SHIFT)
+
 #define XHDMIC_VSIF_VIDEO_FORMAT_SHIFT 5
 #define XHDMIC_VSIF_VIDEO_FORMAT_MASK (0x7 << XHDMIC_VSIF_VIDEO_FORMAT_SHIFT)
 
@@ -60,6 +78,17 @@ extern "C" {
 
 #define XHDMIC_3D_META_LENGTH_SHIFT 0
 #define XHDMIC_3D_META_LENGTH_MASK (0x1F << XHDMIC_3D_META_LENGTH_SHIFT)
+
+#define XHDMIC_HFVSIF_3D_PREFERRED_VIEW_MASK		0x3
+#define XHDMIC_HFVSIF_3D_VIEW_DEPENDENCY_SHIFT		2
+#define XHDMIC_HFVSIF_3D_VIEW_DEPENDENCY_MASK		(0x3 << XHDMIC_HFVSIF_3D_VIEW_DEPENDENCY_SHIFT)
+#define XHDMIC_HFVSIF_3D_DUAL_VIEW_SHIFT		4
+#define XHDMIC_HFVSIF_3D_DUAL_VIEW_MASK			(0x1 << XHDMIC_HFVSIF_3D_DUAL_VIEW_SHIFT)
+
+#define XHDMIC_HFVSIF_3D_DISPARITY_LENGTH_MASK         0x1F
+#define XHDMIC_HFVSIF_3D_DISPARITY_VERSION_SHIFT       5
+#define XHDMIC_HFVSIF_3D_DISPARITY_VERSION_MASK        0x7
+
 /* @} */
 
 /**************************** Type Definitions *******************************/
@@ -118,9 +147,50 @@ typedef enum {
     XHDMIC_3D_META_UNKNOWN
 } XHdmiC_3D_MetaData_Type;
 
+/**
+ * 3D Disparity Data Version
+ */
+typedef enum {
+    XHDMIC_3D_DISPARITY_INFO_UNKNOWN= 0, /**<Parallax information */
+	XHDMIC_3D_PROD_DISPARITY_HINT_INFO,
+	XHDMIC_3D_MULTI_REGION_DISPARITY_INFO,
+	XHDMIC_3D_PROD_AND_MULTI_DIPARITY_INFO,
+	XHDMIC_3D_DISPARITY_INFO_RESERVED
+} XHdmiC_3D_DisparityData_Version;
+
+/**
+ * 3D Preferred 2D View - Indicates which view is preferred for 2D viewing.
+ */
+typedef enum {
+	XHDMIC_3D_PREFERRED_2D_VIEW_UNKNOWN,
+	XHDMIC_3D_PREFERRED_2D_VIEW_RIGHT,
+	XHDMIC_3D_PREFERRED_2D_VIEW_LEFT,
+	XHDMIC_3D_PREFERRED_2D_VIEW_DONT_CARE
+} XHdmiC_3D_Preferred2DView;
+
+/**
+ * 3D View Dependency
+ */
+typedef enum {
+	XHDMIC_3D_VIEW_DEPENDENCY_UNKNOWN,
+	XHDMIC_3D_VIEW_DEPENDENCY_RIGHT,
+	XHDMIC_3D_VIEW_DEPENDENCY_LEFT,
+	XHDMIC_3D_VIEW_DEPENDENCY_BOTH
+} XHdmiC_3D_ViewDependency;
+
+/**
+ * 3D View Dependency
+ */
+
 
 // 8 is the maximum size for currently defined meta types (HDMI 1.4a)
 #define XHDMIC_3D_META_MAX_SIZE 8 /**<Maximum 3D Metadata size in bytes */
+
+// 17 is the maximum value for multi region disparity length (Table 7-10 in HDMI 2.1b)
+#define XHDMIC_3D_MULTI_REGION_DISPARITY_MAX_LENTH 17 /**<Maximum 3D Metadata size in bytes */
+#define XHDMIC_3D_DISPARITY_DATA_MAX_LENGTH 18 /**<Maximum 3D Metadata size in bytes */
+#define XHDMIC_3D_DISPARITY_VERSION1_LENGTH     3
+
 
 /**
  * Structure for 3D meta data
@@ -133,12 +203,43 @@ typedef struct {
 } XHdmiC_3D_MetaData;
 
 /**
+ * Structure for 3D Disparity Data
+ */
+typedef struct {
+    u8					IsPresent;                    /**<Indicates 3D Disparity data presence */
+    XHdmiC_3D_DisparityData_Version	Version;                         /**<Type */
+    u8					Length;                       /**<Length in bytes */
+    u8                             	Video_Min_Max_Disparity_Hint[XHDMIC_3D_DISPARITY_VERSION1_LENGTH];
+    u8                              Multi_Region_Disparity_Length;
+    u8                              Max_Disparity_In_Picture;
+    u8                              Min_Disparity_In_Region[XHDMIC_3D_DISPARITY_DATA_MAX_LENGTH];
+} XHdmiC_3D_DisparityData;
+
+/**
+ * Structure for 3D Additional Info
+ */
+typedef struct {
+    u8					IsPresent;                    /**<Indicates 3D Additional info presence */
+    XHdmiC_3D_Preferred2DView		Preferred2DView;
+    XHdmiC_3D_ViewDependency		ViewDependency;
+    u8					DualView;
+} XHdmiC_3D_AdditionalInfo;
+
+
+/**
  * Structure containing 3D information
  */
 typedef struct {
     XVidC_3DInfo               Stream;
     XHdmiC_3D_MetaData          MetaData;   /**<3D Metadata */
+    XHdmiC_3D_DisparityData	DisparityData;	/**<3D Disparity Data */
+    XHdmiC_3D_AdditionalInfo	Additionalinfo;
 } XHdmiC_3D_Info;
+
+typedef enum {
+	XHDMIC_HDMI_VSIF_IEEE = 0xC03,
+	XHDMIC_HF_VSIF_IEEE = 0xD85DC4,
+} XHdmiC_IEEE_Id_Type;
 
 /**
  * Structure for holding the VSIF.
@@ -146,9 +247,11 @@ typedef struct {
  */
 typedef struct {
     u8                      Version; /**<Version */
-    u32                     IEEE_ID; /**<IEEE Registration Identifier */
+    XHdmiC_IEEE_Id_Type		IEEE_ID; /**<IEEE Registration Identifier */
+    u8				Is_3D_Valid; /**<Indicates Valid 3D data present */
+    u8				ALLM_Mode; /**<The Source shall use this field to enable and disable the Sinkâ?Ts ALLM */
+    u8				CCBPC; /**<Color Content Bits per Component */
     XHdmiC_VSIF_Video_Format Format;  /**<HDMI Video Format */
-
     union {
         u8            HDMI_VIC; /**<XHDMIC_VSIF_VF_EXTRES: HDMI Video Format Identification Code */
         XHdmiC_3D_Info Info_3D;  /**<XHDMIC_VSIF_VF_3D: 3D Information */
@@ -162,6 +265,7 @@ typedef struct {
 /* Vendor Specific Infomation related functions */
 int XV_HdmiC_VSIF_ParsePacket(XHdmiC_Aux *AuxPtr, XHdmiC_VSIF  *VSIFPtr);
 XHdmiC_Aux XV_HdmiC_VSIF_GeneratePacket(XHdmiC_VSIF  *VSIFPtr);
+XHdmiC_Aux XV_HdmiC_HF_VSIF_GeneratePacket(XHdmiC_VSIF  *VSIFPtr);
 
 void XV_HdmiC_VSIF_DisplayInfo(XHdmiC_VSIF  *VSIFPtr);
 char* XV_HdmiC_VSIF_3DStructToString(XHdmiC_3D_Struct_Field Item);
