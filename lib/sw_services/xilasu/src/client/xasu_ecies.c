779999ae56ddabbf41d038efe87ea8667e9747e9
Yogitha, Palakurthy <Palakurthy.Yogitha@amd.com>
2025-02-26 19:31:31 -1200

    sw_services:xilasu:Added ECIES client interface
    Added client interface for ECIES module.
    
    Signed-off-by: Palakurthy Yogitha <palakurthy.yogitha@amd.com>
    
    Acked-for-series: Mounika Akula <mounika.akula@amd.com>

---
+++ lib/sw_services/xilasu/src/client/xasu_ecies.c
@@ -0,0 +1,221 @@
+/**************************************************************************************************
+* Copyright (c) 2025 Advanced Micro Devices, Inc. All Rights Reserved.
+* SPDX-License-Identifier: MIT
+**************************************************************************************************/
+
+/*************************************************************************************************/
+/**
+ *
+ * @file xasu_ecies.c
+ *
+ * This file contains the implementation of the client interface functions for ECIES module.
+ *
+ * <pre>
+ * MODIFICATION HISTORY:
+ *
+ * Ver   Who  Date     Changes
+ * ----- ---- -------- ----------------------------------------------------------------------------
+ * 1.0   yog  02/21/25 Initial release
+ *
+ * </pre>
+ *
+ *************************************************************************************************/
+/**
+ * @addtogroup xasu_ecies_client_apis ECIES Client APIs
+ * @{
+*/
+
+/*************************************** Include Files *******************************************/
+#include "xasu_ecies.h"
+#include "xasu_def.h"
+#include "xasu_status.h"
+#include "xasu_shainfo.h"
+#include "xasu_ecies_common.h"
+#include "xasu_ecc.h"
+
+/************************************ Constant Definitions ***************************************/
+
+/************************************** Type Definitions *****************************************/
+
+/*************************** Macros (Inline Functions) Definitions *******************************/
+
+/************************************ Function Prototypes ****************************************/
+
+/************************************ Variable Definitions ***************************************/
+
+/*************************************************************************************************/
+/**
+ * @brief	This function performs the encryption of the given plaintext message using the
+ * 		ECIES protocol. ECIES combines ECC for key exchange with symmetric encryption for
+ * 		encrypting the plaintext. It generates a random ephemeral key pair, derives a
+ * 		shared secret using ECDH, derives a key using KDF and uses this key to encrypt the
+ * 		plaintext using AES.
+ *
+ * @param	ClientParamsPtr	Pointer to the XAsu_ClientParams structure which holds the client
+ * 				input parameters.
+ * @param	EciesParamsPtr	Pointer to XAsu_EciesParams structure which holds the parameters of
+ * 				ECIES encryption input arguments.
+ *
+ * @return
+ * 		- XST_SUCCESS, if IPI request to ASU is sent successfully.
+ * 		- XASU_INVALID_ARGUMENT, if any argument is invalid.
+ * 		- XASU_INVALID_UNIQUE_ID, if received Queue ID is invalid.
+ * 		- XST_FAILURE, if sending IPI request to ASU fails.
+ *
+ *************************************************************************************************/
+s32 XAsu_EciesEncrypt(XAsu_ClientParams *ClientParamsPtr, XAsu_EciesParams *EciesParamsPtr)
+{
+	s32 Status = XST_FAILURE;
+	u32 Header;
+	u8 CommandId;
+	u8 UniqueId;
+
+	/** Validate input parameters. */
+	Status = XAsu_ValidateClientParameters(ClientParamsPtr);
+	if (Status != XST_SUCCESS) {
+		goto END;
+	}
+
+	Status = XAsu_ValidateEciesParameters(EciesParamsPtr);
+	if (Status != XST_SUCCESS) {
+		Status = XASU_INVALID_ARGUMENT;
+		goto END;
+	}
+
+	Status = XAsu_EccValidateCurveInfo(EciesParamsPtr->EccCurveType, EciesParamsPtr->EccKeyLength);
+	if (Status != XST_SUCCESS) {
+		Status = XASU_INVALID_ARGUMENT;
+		goto END;
+	}
+
+	UniqueId = XAsu_RegCallBackNGetUniqueId(ClientParamsPtr, NULL, 0U, XASU_TRUE);
+	if (UniqueId >= XASU_UNIQUE_ID_MAX) {
+		Status = XASU_INVALID_UNIQUE_ID;
+		goto END;
+	}
+
+	if (EciesParamsPtr->ShaType == XASU_SHA2_TYPE) {
+		CommandId = XASU_ECIES_ENCRYPT_SHA2_CMD_ID;
+	} else {
+		CommandId = XASU_ECIES_ENCRYPT_SHA3_CMD_ID;
+	}
+
+	Header = XAsu_CreateHeader(CommandId, UniqueId, XASU_MODULE_ECIES_ID, 0U);
+
+	/** Update request buffer and send an IPI request to ASU. */
+	Status = XAsu_UpdateQueueBufferNSendIpi(ClientParamsPtr, EciesParamsPtr,
+					(u32)(sizeof(XAsu_EciesParams)), Header);
+
+END:
+	return Status;
+}
+
+/*************************************************************************************************/
+/**
+ * @brief	This function performs the decryption of the given ciphertext using the
+ * 		ECIES protocol. ECIES combines ECC for key exchange with symmetric decryption for
+ * 		decrypting the ciphertext. With the received keys, derives a shared secret using
+ * 		ECDH, derives a key using KDF and uses the key from KDF to decrypt the ciphertext
+ * 		using AES.
+ *
+ * @param	ClientParamsPtr	Pointer to the XAsu_ClientParams structure which holds the client
+ * 				input parameters.
+ * @param	EciesParamsPtr	Pointer to XAsu_EciesParams structure which holds the parameters of
+ * 				ECIES decryption input arguments.
+ *
+ * @return
+ * 		- XST_SUCCESS, if IPI request to ASU is sent successfully.
+ * 		- XASU_INVALID_ARGUMENT, if any argument is invalid.
+ * 		- XASU_INVALID_UNIQUE_ID, if received Queue ID is invalid.
+ * 		- XST_FAILURE, if sending IPI request to ASU fails.
+ *
+ *************************************************************************************************/
+s32 XAsu_EciesDecrypt(XAsu_ClientParams *ClientParamsPtr, XAsu_EciesParams *EciesParamsPtr)
+{
+	s32 Status = XST_FAILURE;
+	u32 Header;
+	u8 CommandId;
+	u8 UniqueId;
+
+	/** Validate input parameters. */
+	Status = XAsu_ValidateClientParameters(ClientParamsPtr);
+	if (Status != XST_SUCCESS) {
+		goto END;
+	}
+
+	Status = XAsu_ValidateEciesParameters(EciesParamsPtr);
+	if (Status != XST_SUCCESS) {
+		Status = XASU_INVALID_ARGUMENT;
+		goto END;
+	}
+
+	Status = XAsu_EccValidateCurveInfo(EciesParamsPtr->EccCurveType,
+			EciesParamsPtr->EccKeyLength);
+	if (Status != XST_SUCCESS) {
+		Status = XASU_INVALID_ARGUMENT;
+		goto END;
+	}
+
+	UniqueId = XAsu_RegCallBackNGetUniqueId(ClientParamsPtr, NULL, 0U, XASU_TRUE);
+	if (UniqueId >= XASU_UNIQUE_ID_MAX) {
+		Status = XASU_INVALID_UNIQUE_ID;
+		goto END;
+	}
+
+	if (EciesParamsPtr->ShaType == XASU_SHA2_TYPE) {
+		CommandId = XASU_ECIES_DECRYPT_SHA2_CMD_ID;
+	} else {
+		CommandId = XASU_ECIES_DECRYPT_SHA3_CMD_ID;
+	}
+
+	Header = XAsu_CreateHeader(CommandId, UniqueId, XASU_MODULE_ECIES_ID, 0U);
+
+	/** Update request buffer and send an IPI request to ASU. */
+	Status = XAsu_UpdateQueueBufferNSendIpi(ClientParamsPtr, EciesParamsPtr,
+					(u32)(sizeof(XAsu_EciesParams)), Header);
+
+END:
+	return Status;
+}
+
+/*************************************************************************************************/
+/**
+ * @brief	This function performs ECIES Known Answer Tests (KAT's).
+ *
+ * @param	ClientParamsPtr	Pointer to the XAsu_ClientParams structure which holds the client
+ * 				input parameters.
+ *
+ * @return
+ * 	- XST_SUCCESS, if IPI request to ASU is sent successfully.
+ * 	- XASU_INVALID_ARGUMENT, if any argument is invalid.
+ * 	- XASU_INVALID_UNIQUE_ID, if received Queue ID is invalid.
+ * 	- XST_FAILURE, if sending IPI request to ASU fails.
+ *
+ *************************************************************************************************/
+s32 XAsu_EciesKat(XAsu_ClientParams *ClientParamsPtr)
+{
+	s32 Status = XST_FAILURE;
+	u32 Header;
+	u8 UniqueId;
+
+	/** Validate input parameters. */
+	Status = XAsu_ValidateClientParameters(ClientParamsPtr);
+	if (Status != XST_SUCCESS) {
+		goto END;
+	}
+
+	UniqueId = XAsu_RegCallBackNGetUniqueId(ClientParamsPtr, NULL, 0U, XASU_TRUE);
+	if (UniqueId >= XASU_UNIQUE_ID_MAX) {
+		Status = XASU_INVALID_UNIQUE_ID;
+		goto END;
+	}
+
+	Header = XAsu_CreateHeader(XASU_ECIES_KAT_CMD_ID, UniqueId, XASU_MODULE_ECIES_ID, 0U);
+
+	/** Update request buffer and send an IPI request to ASU. */
+	Status = XAsu_UpdateQueueBufferNSendIpi(ClientParamsPtr, NULL, 0U, Header);
+
+END:
+	return Status;
+}
+/** @} */
