40fbe88225fb5a128933dbef7ace35ee879932d5
Sai Srinivas Sirimisetty <SaiSrinivas.Sirimisetti@amd.com>
2025-02-28 07:07:17 -1200

    sw_services:xilasu:Added client support for keywrap
    Added client support for key wrap and unwrap
    
    Signed-off-by: Sai Srinivas Sirimisetty <SaiSrinivas.Sirimisetti@amd.com>
    
    Acked-for-series: Mounika Akula <mounika.akula@amd.com>

---
+++ lib/sw_services/xilasu/src/client/xasu_keywrap.h
@@ -0,0 +1,57 @@
+/**************************************************************************************************
+* Copyright (c) 2025 Advanced Micro Devices, Inc. All Rights Reserved.
+* SPDX-License-Identifier: MIT
+**************************************************************************************************/
+
+/*************************************************************************************************/
+/**
+ *
+ * @file xasu_keywrap.h
+ *
+ * This file Contains the key wrap unwrap client function prototypes.
+ *
+ * <pre>
+ * MODIFICATION HISTORY:
+ *
+ * Ver   Who  Date     Changes
+ * ----- ---- -------- ----------------------------------------------------------------------------
+ * 1.0   ss   02/24/25 Initial release
+ *
+ * </pre>
+ *
+ *************************************************************************************************/
+/**
+ * @addtogroup xasu_keywrap_client_apis key wrap unwrap Client APIs
+ * @{
+*/
+
+#ifndef XASU_KEYWRAP_H
+#define XASU_KEYWRAP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*************************************** Include Files *******************************************/
+#include "xil_types.h"
+#include "xasu_client.h"
+#include "xasu_keywrapinfo.h"
+
+/************************************ Constant Definitions ***************************************/
+
+/************************************** Type Definitions *****************************************/
+
+/*************************** Macros (Inline Functions) Definitions *******************************/
+
+/************************************ Function Prototypes ****************************************/
+s32 XAsu_KeyWrap(XAsu_ClientParams *ClientParamsPtr, XAsu_KeyWrapParams *KeyWrapParamsPtr);
+s32 XAsu_KeyUnwrap(XAsu_ClientParams *ClientParamsPtr, XAsu_KeyWrapParams *KeyUnwrapParamsPtr);
+s32 XAsu_KeyWrapKat(XAsu_ClientParams *ClientParamsPtr);
+
+/************************************ Variable Definitions ***************************************/
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* XASU_KEYWRAP_H */
+/** @} */
