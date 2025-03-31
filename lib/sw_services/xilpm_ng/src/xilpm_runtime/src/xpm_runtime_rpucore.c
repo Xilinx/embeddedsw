80ab10af711eac5ddccd4eb9febfe1677528423a
Tran, Trung <trung.tran@amd.com>
2025-03-31 21:12:21 +0530

    xilpm_ng: Add redundancy call
    - Remove unused function in xilpm_boot.
    - Add redundancy call to SetBootAddr to improve security.
    
    Signed-off-by: Trung tran <trung.tran@amd.com>
    Acked-by: Nicole Baze <amanda.baze@amd.com>

--- lib/sw_services/xilpm_ng/src/xilpm_runtime/src/xpm_runtime_rpucore.c
+++ lib/sw_services/xilpm_ng/src/xilpm_runtime/src/xpm_runtime_rpucore.c
@@ -1,20 +1,15 @@
 /******************************************************************************
-* Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved.
+* Copyright (c) 2024 - 2025 Advanced Micro Devices, Inc. All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
 #include "xpm_rpucore.h"
 #include "xpm_runtime_rpucore.h"
+#include "xil_sutil.h"
 
-XStatus XPm_RpuBootAddrConfig(const u32 DeviceId, const u32 BootAddr)
+static XStatus SetBootAddr(const struct XPm_RpuCore *RpuCore, const u32 BootAddr)
 {
-	XStatus Status = XST_FAILURE;
-	const XPm_RpuCore *RpuCore = (XPm_RpuCore *)XPmDevice_GetById(DeviceId);
-	if (NULL == RpuCore) {
-		PmErr("Unable to get RPU Core for Id: 0x%x\n\r", DeviceId);
-		goto done;
-	}
-        if (0U == BootAddr) {
+	if (0U == BootAddr) {
 		PmRmw32(RpuCore->RpuBaseAddr + XPM_CORE_CFG0_OFFSET, XPM_RPU_TCMBOOT_MASK,
 			XPM_RPU_TCMBOOT_MASK);
 	} else {
@@ -22,7 +17,22 @@ XStatus XPm_RpuBootAddrConfig(const u32 DeviceId, const u32 BootAddr)
 		PmRmw32(RpuCore->RpuBaseAddr + XPM_CORE_CFG0_OFFSET, XPM_RPU_TCMBOOT_MASK, 0U);
 	}
 
-	Status = XST_SUCCESS;
+	return XST_SUCCESS;
+}
+
+XStatus XPm_RpuBootAddrConfig(const u32 DeviceId, const u32 BootAddr)
+{
+	volatile XStatus Status = XST_FAILURE;
+	volatile XStatus StatusTmp = XST_FAILURE;
+	const XPm_RpuCore *RpuCore = (XPm_RpuCore *)XPmDevice_GetById(DeviceId);
+	if (NULL == RpuCore) {
+		PmErr("Unable to get RPU Core for Id: 0x%x\n\r", DeviceId);
+		goto done;
+	}
+	XSECURE_REDUNDANT_CALL(Status, StatusTmp, SetBootAddr, RpuCore, BootAddr);
+	if ((XST_SUCCESS != Status) || (XST_SUCCESS != StatusTmp)) {
+			PmErr("Error while setting boot address\n");
+	}
 
 done:
 	return Status;
