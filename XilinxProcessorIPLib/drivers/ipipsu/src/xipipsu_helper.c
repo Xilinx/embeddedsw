face9f77deb9c8258433f67944a7ed618cf7280d
Akula, Mounika <mounika.akula@amd.com>
2025-04-05 18:27:36 +0530

    drivers: ipipsu: Avoid single byte reads from IPI buffer in CRC logic
    The CRC calculation logic in IPI driver does single byte reads from IPI
    buffer to calculate 16 byte CRC. When XPPU protections are enabled, this
    is generating XPPU access violation as single byte access is not allowed
    through XPPU. To fix this issue, update the IPI CRC logic to always read
    4 bytes from the IPI buffer.
    
    Signed-off-by: Mounika Akula <mounika.akula@amd.com>
    Acked-by: Harini T <Harini.T@amd.com>

--- XilinxProcessorIPLib/drivers/ipipsu/src/xipipsu_helper.c
+++ XilinxProcessorIPLib/drivers/ipipsu/src/xipipsu_helper.c
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+* Copyright (C) 2023 - 2025 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -20,7 +20,8 @@
 * Ver	Who	Date	 Changes
 * ----- ------ --------  ----------------------------------------------
 * 2.14	ht	06/13/23 Restructured the code for more modularity
-*       ht	07/28/23 Fix MISRA-C warnings
+*     	ht	07/28/23 Fix MISRA-C warnings
+* 2.17	ma	04/05/25 Read always in 4 bytes from IPI buffer
 *
 * </pre>
 *
@@ -36,8 +37,10 @@
 #define POLYNOM					0x8005U /**< Polynomial */
 #define INITIAL_CRC_VAL			0x4F4EU		/**< Initial crc value */
 #define CRC16_MASK				0xFFFFU /**< CRC mask */
+#define BYTE_MASK				0xFFU /**< Mask for byte */
 #define CRC16_HIGH_BIT_MASK		0x8000U		/**< CRC high bit mask */
 #define NUM_BITS_IN_BYTE		0x8U		/**< 8 bits in a byte */
+#define WORD_LEN_IN_BYTES		0x4U /**< Word length in bytes */
 #endif
 
 /************************** Variable Definitions *****************************/
@@ -60,25 +63,31 @@ u32 XIpiPsu_CalculateCRC(u32 BufAddr, u32 BufSize)
 	u32 Bits = 0;
 	volatile u32 Temp1Crc;
 	volatile u32 Temp2Crc;
+	u32 Data;
+	u32 ByteIdx;
 
-	for (Idx = 0U; Idx < BufSize; Idx++) {
-		/* Move byte into MSB of 16bit CRC */
-		DataIn = (u32)Xil_In8(BufAddr + Idx);
-		Crc16 ^= (DataIn << NUM_BITS_IN_BYTE);
+	for (Idx = 0U; Idx < BufSize; Idx = Idx + WORD_LEN_IN_BYTES) {
+		Data = Xil_In32(BufAddr + Idx);
+		for (ByteIdx = 0U; ByteIdx < WORD_LEN_IN_BYTES; ByteIdx++) {
+			/* Move byte into MSB of 16bit CRC */
+			DataIn = (Data >> (ByteIdx * NUM_BITS_IN_BYTE)) & BYTE_MASK;
+			Crc16 ^= (DataIn << NUM_BITS_IN_BYTE);
 
-		/* Process each bit of 8 bit value */
-		for (Bits = 0; Bits < NUM_BITS_IN_BYTE; Bits++) {
-			Temp1Crc = ((Crc16 << 1U) ^ POLYNOM);
-			Temp2Crc = Crc16 << 1U;
+			/* Process each bit of 8 bit value */
+			for (Bits = 0; Bits < NUM_BITS_IN_BYTE; Bits++) {
+				Temp1Crc = ((Crc16 << 1U) ^ POLYNOM);
+				Temp2Crc = Crc16 << 1U;
 
-			if ((Crc16 & CRC16_HIGH_BIT_MASK) != 0) {
-				Crc16 = Temp1Crc;
-			} else {
-				Crc16 = Temp2Crc;
+				if ((Crc16 & CRC16_HIGH_BIT_MASK) != 0) {
+					Crc16 = Temp1Crc;
+				} else {
+					Crc16 = Temp2Crc;
+				}
 			}
+			Crc16 &= CRC16_MASK;
 		}
-		Crc16 &= CRC16_MASK;
 	}
+
 	return Crc16;
 }
 #endif
