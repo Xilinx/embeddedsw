564702e557c2b242fafff4342655bd94ae12c669
Kalyani Akula <kalyani.akula@amd.com>
2025-04-09 13:17:16 +0530

    xilnvm: Add CalculateCrc logic for Manufacturing eFuse list
    During eFuse cache reload, NVM checks for eFuse CRC
    protection bits. Update the check to calculate the CRC
    value of the XILINX manufacturing eFuse bits and check if
    the CRC programmed into eFuse is correct else report the
    error.
    
    Signed-off-by: Kalyani Akula <kalyani.akula@amd.com>
    Acked-by: Harsha <harsha.harsha@xilinx.com>

--- lib/sw_services/xilnvm/src/versal_gen/versal_net/server/xnvm_efuse.c
+++ lib/sw_services/xilnvm/src/versal_gen/versal_net/server/xnvm_efuse.c
@@ -93,6 +93,8 @@ static int XNvm_EfusePrgmProtectionBits(void);
 static int XNvm_EfuseProtectionChecks(void);
 static int XNvm_EfuseChangeEndianness(u8 *Dest, u8 *Src, u32 Size);
 static int XNvm_EfuseReadRow(XNvm_EfuseType Page, u32 Row, u32 *RegData);
+static u32 XNvm_EfuseCalculateCrc32(const u8* Data, u32 Len);
+static int XNvm_EfuseValidateCrc(void);
 
 /************************** Constant Definitions *****************************/
 #define XNVM_EFUSE_ERROR_BYTE_SHIFT	(8U) /**< Byte shift used in error code */
@@ -102,6 +104,7 @@ static int XNvm_EfuseReadRow(XNvm_EfuseType Page, u32 Row, u32 *RegData);
 #define XNVM_EFUSE_BITS_IN_A_BYTE	(8U) /**< Number of bits in a byte */
 #define XNVM_EFUSE_SEC_DEF_VAL_ALL_BIT_SET	(0xFFFFFFFFU)
 						 /**< Secure Default Value for a register */
+#define XNVM_EFUSE_BYTE_MASK			(0xFF)	/**< Mask for 8 bits */
 #define REVERSE_POLYNOMIAL			(0x82F63B78U)
 						/**< Value used in CRC calculation to reverse a polynomial */
 #define XNVM_EFUSE_SKIP_VERIFY			(1U)
@@ -131,7 +134,87 @@ static int XNvm_EfuseReadRow(XNvm_EfuseType Page, u32 Row, u32 *RegData);
 			/**< PPK HASH upper 128 bit hash start word offset */
 #define XNVM_EFUSE_LAST_ROW_IN_PAGE			(255U)
 			/**< Row number of last eFuse row in page */
+#define XNVM_EFUSE_CACHE_ANLG_TRIM_0		(0xF1250004U)
+			/**< ANLG_TRIM_0 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_ANLG_TRIM_1		(0xF1250008U)
+			/**< ANLG_TRIM_1 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_ANLG_TRIM_2		(0xF125000CU)
+			/**< ANLG_TRIM_2 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_IP_DISABLE_1		(0xF125001CU)
+			/**< IP_DISABLE_1 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_DNA_0			(0xF1250020U)
+			/**< DNA_0 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_DNA_1			(0xF1250024U)
+			/**< DNA_1 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_DNA_2			(0xF1250028U)
+			/**< DNA_2 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_DNA_3			(0xF125002CU)
+			/**< DNA_3 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_BRAM		(0xF1250098U)
+			/**< TRIM_BRAM eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_URAM		(0xF125009CU)
+			/**< TRIM_URAM eFuse cache offset */
+#define XNVM_EFUSE_CACHE_PMC_BISR_0		(0xF12500D8U)
+			/**< PMC_BISR_0 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_PMC_BISR_1		(0xF12500DCU)
+			/**< PMC_BISR_1 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_ANLG_TRIM_5		(0xF12500E0U)
+			/**< ANLG_TRIM_5 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_ANLG_TRIM_6		(0xF12500F4U)
+			/**< ANLG_TRIM_6 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_ANLG_TRIM_7		(0xF12500F8U)
+			/**< ANLG_TRIM_7 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_ME_ID_CODE		(0xF12500FCU)
+			/**< ME_ID_CODE eFuse cache offset */
+#define XNVM_EFUSE_CACHE_EXPORT_DFT		(0xF125018CU)
+			/**< EXPORT_DFT eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_AMS_4		(0xF1250190U)
+			/**< TRIM_AMS_4 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_AMS_5		(0xF1250194U)
+			/**< TRIM_AMS_5 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_AMS_6		(0xF1250198U)
+			/**< TRIM_AMS_6 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_AMS_7		(0xF125019CU)
+			/**< TRIM_AMS_7 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_AMS_8		(0xF12501A0U)
+			/**< TRIM_AMS_8 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_AMS_9		(0xF12501A4U)
+			/**< TRIM_AMS_9 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_AMS_10		(0xF12501A8U)
+			/**< TRIM_AMS_10 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_AMS_11		(0xF12501ACU)
+			/**< TRIM_AMS_11 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_AMS_12		(0xF12501B0U)
+			/**< TRIM_AMS_12 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_CFRM_VGG_0	(0xF12501B4U)
+			/**< TRIM_CFRM_VGG_0 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_CFRM_VGG_1	(0xF12501B8U)
+			/**< TRIM_CFRM_VGG_1 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_CFRM_VGG_2	(0xF12501BCU)
+			/**< TRIM_CFRM_VGG_2 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_CRAM		(0xF12501C0U)
+			/**< TRIM_CRAM eFuse cache offset */
+#define XNVM_EFUSE_CACHE_NIDB_0			(0xF12501C4U)
+			/**< NIDB_0 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_NIDB_1			(0xF12501C8U)
+			/**< NIDB_1 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_NIDB_2			(0xF12501CCU)
+			/**< NIDB_2 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_AMS_0		(0xF12501F4U)
+			/**< TRIM_AMS_0 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_AMS_1		(0xF12501F8U)
+			/**< TRIM_AMS_1 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_AMS_2		(0xF12501FCU)
+			/**< TRIM_AMS_2 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_TRIM_AMS_3		(0xF1250200U)
+			/**< TRIM_AMS_3 eFuse cache offset */
+#define XNVM_EFUSE_CACHE_ME_ID_CODE_3_0_MASK	(0x0000000FU)
+			/**< ME_ID_CODE_3_0_MASK */
+#define XNVM_CRC_DATA_WORD_COUNT		(37U)
+			/**< CRC DATA word count */
 
+#define XNVM_CRC_DATA_BYTE_COUNT 		(XNVM_CRC_DATA_WORD_COUNT * XNVM_WORD_LEN)
+			/**< CRC data byte count */
 /***************** Macros (Inline Functions) Definitions *********************/
 
 /*************************** Function Prototypes ******************************/
@@ -3008,10 +3091,12 @@ static int XNvm_EfuseProtectionChecks(void)
 				XNVM_EFUSE_CACHE_ME_ID_CODE_OFFSET) &
 				XNVM_EFUSE_CACHE_ME_ID_CODE_CRC_SALT_MASK;
 
-		if ((RowVal != RowValTmp) ||
-			(RowVal != XNVM_EFUSE_CACHE_ME_ID_CODE_CRC_SALT_MASK)) {
-			Status = (int)XNVM_EFUSE_ERR_IN_PROTECTION_CHECK;
-			goto END;
+		if ((RowVal != 0x0U) || (RowValTmp != 0x0U)) {
+			Status = XNvm_EfuseValidateCrc();
+			if (Status != XST_SUCCESS) {
+				Status = (int)XNVM_EFUSE_ERR_IN_PROTECTION_CHECK;
+				goto END;
+			}
 		}
 	}
 
@@ -3894,4 +3979,237 @@ static int XNvm_EfuseChangeEndianness(u8 *Dest, u8 *Src, u32 Size)
 	}
 END:
 	return Status;
+}
+
+/******************************************************************************/
+/**
+* @brief	This function calculate CRC on below eFUSE cache registers and
+*		compares against eFUSE CRC value
+*
+*| Register Name	|  Register Adress	|
+*| -------------  	|  ---------------	|
+*|  ANLG_TRIM_0		|     0xF1250004	|
+*|  ANLG_TRIM_1		|     0xF1250008	|
+*|  ANLG_TRIM_2		|     0xF125000C	|
+*|  IP_DISABLE_1	|     0xF125001C	|
+*|  DNA_0		|     0xF1250020	|
+*|  DNA_1		|     0xF1250024	|
+*|  DNA_2		|     0xF1250028	|
+*|  DNA_3		|     0xF125002C	|
+*|  TRIM_BRAM		|     0xF1250098	|
+*|  TRIM_URAM		|     0xF125009C	|
+*|  PMC_BISR_0		|     0xF12500D8	|
+*|  PMC_BISR_1		|     0xF12500DC	|
+*|  ANLG_TRIM_5		|     0xF12500E0	|
+*|  ALNG_TRIM_6		|     0xF12500F4	|
+*|  ANLG_TRIM_7		|     0xF12500F8	|
+*|  ME_ID_CODE		|     0xF12500FC	|
+*|  XPORT_DFT		|     0xF125018C	|
+*|  TRIM_AMS_4		|     0xF1250190	|
+*|  TRIM_AMS_5		|     0xF1250194	|
+*|  TRIM_AMS_6		|     0xF1250198	|
+*|  TRIM_AMS_7		|     0xF125019C	|
+*|  TRIM_AMS_8		|     0xF12501A0	|
+*|  TRIM_AMS_9		|     0xF12501A4	|
+*|  TRIM_AMS_10		|     0xF12501A8	|
+*|  TRIM_AMS_11		|     0xF12501AC	|
+*|  TRIM_AMS_12		|     0xF12501B0	|
+*|  TRIM_CFRM_VGG_0	|     0xF12501B4	|
+*|  TRIM_CFRM_VGG_1	|     0xF12501B8	|
+*|  TRIM_CFRM_VGG_2	|     0xF12501BC	|
+*|  TRIM_CRAM		|     0xF12501C0	|
+*|  NIDB_0		|     0xF12501C4	|
+*|  NIDB_1		|     0xF12501C8	|
+*|  NIDB_2		|     0xF12501CC	|
+*|  TRIM_AMS_0		|     0xF12501F4	|
+*|  TRIM_AMS_1		|     0xF12501F8	|
+*|  TRIM_AMS_2		|     0xF12501FC	|
+*|  TRIM_AMS_3		|     0xF1250200	|
+*
+* @return
+*	- XST_SUCCESS - Up on Success
+*	- XNVM_EFUSE_XLNX_MANFAC_BITS_CRC_ERROR - Error as calculated CRC and CRC
+*					value written in eFuse doesnt match.
+*******************************************************************************/
+static int XNvm_EfuseValidateCrc(void)
+{
+	volatile int Status;
+	volatile u32 CalCrcVal = ~XNvm_EfuseReadReg(XNVM_EFUSE_CACHE_BASEADDR,
+				XNVM_EFUSE_CACHE_CRC_OFFSET);
+	volatile u32 EfuseCrcVal = 0U;
+	volatile u32 EfuseCrcValTmp = 0U;
+	u32 InputData[XNVM_CRC_DATA_WORD_COUNT];
+	u32 Index = 0U;
+
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_ANLG_TRIM_0);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_ANLG_TRIM_1);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_ANLG_TRIM_2);
+
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_IP_DISABLE_1);
+
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_DNA_0);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_DNA_1);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_DNA_2);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_DNA_3);
+
+
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_BRAM);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_URAM);
+
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_PMC_BISR_0);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_PMC_BISR_1);
+
+
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_ANLG_TRIM_5);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_ANLG_TRIM_6);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_ANLG_TRIM_7);
+
+	/*
+	 * Only the lower nibble is considered, so the value
+	 * considered for CRC calculation would be
+	 * 0x0000000X
+	 */
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_ME_ID_CODE) &
+				XNVM_EFUSE_CACHE_ME_ID_CODE_3_0_MASK;
+
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_EXPORT_DFT);
+
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_AMS_4);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_AMS_5);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_AMS_6);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_AMS_7);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_AMS_8);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_AMS_9);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_AMS_10);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_AMS_11);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_AMS_12);
+
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_CFRM_VGG_0);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_CFRM_VGG_1);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_CFRM_VGG_2);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_CRAM);
+
+
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_NIDB_0);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_NIDB_1);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_NIDB_2);
+
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_AMS_0);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_AMS_1);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_AMS_2);
+	InputData[Index++] = Xil_In32(XNVM_EFUSE_CACHE_TRIM_AMS_3);
+
+	if (Index != XNVM_CRC_DATA_WORD_COUNT) {
+		Status = XST_GLITCH_ERROR;
+		goto END;
+	}
+
+	CalCrcVal = XNvm_EfuseCalculateCrc32((u8*)InputData, XNVM_CRC_DATA_BYTE_COUNT);
+
+	Status = (int)XNVM_EFUSE_XLNX_MANFAC_BITS_CRC_ERROR;
+
+	EfuseCrcVal = XNvm_EfuseReadReg(XNVM_EFUSE_CACHE_BASEADDR,
+			XNVM_EFUSE_CACHE_CRC_OFFSET);
+	EfuseCrcValTmp = XNvm_EfuseReadReg(XNVM_EFUSE_CACHE_BASEADDR,
+                        XNVM_EFUSE_CACHE_CRC_OFFSET);
+	if ((CalCrcVal != EfuseCrcVal) || (CalCrcVal != EfuseCrcValTmp)) {
+		Status = (int)XNVM_EFUSE_XLNX_MANFAC_BITS_CRC_ERROR;
+	}
+	else {
+		Status = XST_SUCCESS;
+	}
+
+END:
+	return Status;
+}
+/******************************************************************************/
+/**
+* @brief	This function Calculates the 32-bit CRC using CRC32 algorithm.
+*		This function uses lookup table to speedup the calculation.
+*
+* @param	Data  - Input Data over which CRC is to be calculated
+* @param	Len - Length of the input data
+
+* @return
+*	-	 Crc - 32-bit CRC Value
+*******************************************************************************/
+static u32 XNvm_EfuseCalculateCrc32(const u8* Data, u32 Len)
+{
+	u32 Crc = XNVM_EFUSE_SEC_DEF_VAL_ALL_BIT_SET;
+	u32 Index;
+	/** CRC32 Lookup Table
+	 * Polynomial : 0x1EDC6F41
+	 */
+	static const u32 Crc32_LookupTbl[] = {
+		0x00000000UL, 0xF26B8303UL, 0xE13B70F7UL, 0x1350F3F4UL,
+		0xC79A971FUL, 0x35F1141CUL, 0x26A1E7E8UL, 0xD4CA64EBUL,
+		0x8AD958CFUL, 0x78B2DBCCUL, 0x6BE22838UL, 0x9989AB3BUL,
+		0x4D43CFD0UL, 0xBF284CD3UL, 0xAC78BF27UL, 0x5E133C24UL,
+		0x105EC76FUL, 0xE235446CUL, 0xF165B798UL, 0x030E349BUL,
+		0xD7C45070UL, 0x25AFD373UL, 0x36FF2087UL, 0xC494A384UL,
+		0x9A879FA0UL, 0x68EC1CA3UL, 0x7BBCEF57UL, 0x89D76C54UL,
+		0x5D1D08BFUL, 0xAF768BBCUL, 0xBC267848UL, 0x4E4DFB4BUL,
+		0x20BD8EDEUL, 0xD2D60DDDUL, 0xC186FE29UL, 0x33ED7D2AUL,
+		0xE72719C1UL, 0x154C9AC2UL, 0x061C6936UL, 0xF477EA35UL,
+		0xAA64D611UL, 0x580F5512UL, 0x4B5FA6E6UL, 0xB93425E5UL,
+		0x6DFE410EUL, 0x9F95C20DUL, 0x8CC531F9UL, 0x7EAEB2FAUL,
+		0x30E349B1UL, 0xC288CAB2UL, 0xD1D83946UL, 0x23B3BA45UL,
+		0xF779DEAEUL, 0x05125DADUL, 0x1642AE59UL, 0xE4292D5AUL,
+		0xBA3A117EUL, 0x4851927DUL, 0x5B016189UL, 0xA96AE28AUL,
+		0x7DA08661UL, 0x8FCB0562UL, 0x9C9BF696UL, 0x6EF07595UL,
+		0x417B1DBCUL, 0xB3109EBFUL, 0xA0406D4BUL, 0x522BEE48UL,
+		0x86E18AA3UL, 0x748A09A0UL, 0x67DAFA54UL, 0x95B17957UL,
+		0xCBA24573UL, 0x39C9C670UL, 0x2A993584UL, 0xD8F2B687UL,
+		0x0C38D26CUL, 0xFE53516FUL, 0xED03A29BUL, 0x1F682198UL,
+		0x5125DAD3UL, 0xA34E59D0UL, 0xB01EAA24UL, 0x42752927UL,
+		0x96BF4DCCUL, 0x64D4CECFUL, 0x77843D3BUL, 0x85EFBE38UL,
+		0xDBFC821CUL, 0x2997011FUL, 0x3AC7F2EBUL, 0xC8AC71E8UL,
+		0x1C661503UL, 0xEE0D9600UL, 0xFD5D65F4UL, 0x0F36E6F7UL,
+		0x61C69362UL, 0x93AD1061UL, 0x80FDE395UL, 0x72966096UL,
+		0xA65C047DUL, 0x5437877EUL, 0x4767748AUL, 0xB50CF789UL,
+		0xEB1FCBADUL, 0x197448AEUL, 0x0A24BB5AUL, 0xF84F3859UL,
+		0x2C855CB2UL, 0xDEEEDFB1UL, 0xCDBE2C45UL, 0x3FD5AF46UL,
+		0x7198540DUL, 0x83F3D70EUL, 0x90A324FAUL, 0x62C8A7F9UL,
+		0xB602C312UL, 0x44694011UL, 0x5739B3E5UL, 0xA55230E6UL,
+		0xFB410CC2UL, 0x092A8FC1UL, 0x1A7A7C35UL, 0xE811FF36UL,
+		0x3CDB9BDDUL, 0xCEB018DEUL, 0xDDE0EB2AUL, 0x2F8B6829UL,
+		0x82F63B78UL, 0x709DB87BUL, 0x63CD4B8FUL, 0x91A6C88CUL,
+		0x456CAC67UL, 0xB7072F64UL, 0xA457DC90UL, 0x563C5F93UL,
+		0x082F63B7UL, 0xFA44E0B4UL, 0xE9141340UL, 0x1B7F9043UL,
+		0xCFB5F4A8UL, 0x3DDE77ABUL, 0x2E8E845FUL, 0xDCE5075CUL,
+		0x92A8FC17UL, 0x60C37F14UL, 0x73938CE0UL, 0x81F80FE3UL,
+		0x55326B08UL, 0xA759E80BUL, 0xB4091BFFUL, 0x466298FCUL,
+		0x1871A4D8UL, 0xEA1A27DBUL, 0xF94AD42FUL, 0x0B21572CUL,
+		0xDFEB33C7UL, 0x2D80B0C4UL, 0x3ED04330UL, 0xCCBBC033UL,
+		0xA24BB5A6UL, 0x502036A5UL, 0x4370C551UL, 0xB11B4652UL,
+		0x65D122B9UL, 0x97BAA1BAUL, 0x84EA524EUL, 0x7681D14DUL,
+		0x2892ED69UL, 0xDAF96E6AUL, 0xC9A99D9EUL, 0x3BC21E9DUL,
+		0xEF087A76UL, 0x1D63F975UL, 0x0E330A81UL, 0xFC588982UL,
+		0xB21572C9UL, 0x407EF1CAUL, 0x532E023EUL, 0xA145813DUL,
+		0x758FE5D6UL, 0x87E466D5UL, 0x94B49521UL, 0x66DF1622UL,
+		0x38CC2A06UL, 0xCAA7A905UL, 0xD9F75AF1UL, 0x2B9CD9F2UL,
+		0xFF56BD19UL, 0x0D3D3E1AUL, 0x1E6DCDEEUL, 0xEC064EEDUL,
+		0xC38D26C4UL, 0x31E6A5C7UL, 0x22B65633UL, 0xD0DDD530UL,
+		0x0417B1DBUL, 0xF67C32D8UL, 0xE52CC12CUL, 0x1747422FUL,
+		0x49547E0BUL, 0xBB3FFD08UL, 0xA86F0EFCUL, 0x5A048DFFUL,
+		0x8ECEE914UL, 0x7CA56A17UL, 0x6FF599E3UL, 0x9D9E1AE0UL,
+		0xD3D3E1ABUL, 0x21B862A8UL, 0x32E8915CUL, 0xC083125FUL,
+		0x144976B4UL, 0xE622F5B7UL, 0xF5720643UL, 0x07198540UL,
+		0x590AB964UL, 0xAB613A67UL, 0xB831C993UL, 0x4A5A4A90UL,
+		0x9E902E7BUL, 0x6CFBAD78UL, 0x7FAB5E8CUL, 0x8DC0DD8FUL,
+		0xE330A81AUL, 0x115B2B19UL, 0x020BD8EDUL, 0xF0605BEEUL,
+		0x24AA3F05UL, 0xD6C1BC06UL, 0xC5914FF2UL, 0x37FACCF1UL,
+		0x69E9F0D5UL, 0x9B8273D6UL, 0x88D28022UL, 0x7AB90321UL,
+		0xAE7367CAUL, 0x5C18E4C9UL, 0x4F48173DUL, 0xBD23943EUL,
+		0xF36E6F75UL, 0x0105EC76UL, 0x12551F82UL, 0xE03E9C81UL,
+		0x34F4F86AUL, 0xC69F7B69UL, 0xD5CF889DUL, 0x27A40B9EUL,
+		0x79B737BAUL, 0x8BDCB4B9UL, 0x988C474DUL, 0x6AE7C44EUL,
+		0xBE2DA0A5UL, 0x4C4623A6UL, 0x5F16D052UL, 0xAD7D5351UL
+	};
+
+	for (Index = 0U; Index < Len; Index++) {
+		Crc = Crc32_LookupTbl[(Crc ^ Data[Index]) & XNVM_EFUSE_BYTE_MASK] ^ (Crc >> XNVM_EFUSE_BITS_IN_A_BYTE);
+	}
+
+	return (Crc ^ (u32)XNVM_EFUSE_SEC_DEF_VAL_ALL_BIT_SET);
 }
