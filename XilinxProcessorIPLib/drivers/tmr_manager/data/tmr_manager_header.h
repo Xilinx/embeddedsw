4864d6c466ed9c505e932369dca8da49c0a87377
Rao, Appana Durga Kedareswara <appana.durga.kedareswara.rao@amd.com>
2025-04-09 10:20:55 +0530

    drivers: tmr_*: Add support for peripheral test in SDT flow
    Added tapp section in the tmr_manager and tmr_inject drivers to pull
    examples for peripheral test in sdt flow.
    
    Signed-off-by: Appana Durga Kedareswara rao <appana.durga.kedareswara.rao@amd.com>
    Acked-by: Manne, Nava kishore <nava.kishore.manne@amd.com>

--- XilinxProcessorIPLib/drivers/tmr_manager/data/tmr_manager_header.h
+++ XilinxProcessorIPLib/drivers/tmr_manager/data/tmr_manager_header.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2017 - 2020 Xilinx, Inc.  All rights reserved.
-* Copyright (C) 2024 Advanced Micro Devices, Inc. All Rights Reserved.
+* Copyright (C) 2024 - 2025 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -15,7 +15,14 @@ extern "C" {
 #include "xil_assert.h"
 #include "xstatus.h"
 
+#ifndef SDT
 int TMR_ManagerSelfTestExample(u16 DeviceId);
+#else
+int TMR_ManagerSelfTestExample(UINTPTR BaseAddress);
+int TMR_ManagerIntrExample(XTMR_Manager *TMR_ManagerInstancePtr,
+			   UINTPTR BaseAddress);
+#endif
+
 
 #ifdef __cplusplus
 }
