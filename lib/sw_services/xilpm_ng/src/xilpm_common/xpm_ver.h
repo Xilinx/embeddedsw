4acd10ca293e68349922a8c3db1d0936c8a2c00f
Naman Trivedi <naman.trivedimanojbhai@amd.com>
2025-04-30 15:44:00 +0530

    xilpm_ng: add library version macros
    Add header file containing new macros for xilpm_ng library version.
    Also, since xilpm_ng is a new library, change the version to 1.0 in the
    yaml file.
    
    Signed-off-by: Naman Trivedi <naman.trivedimanojbhai@amd.com>
    Acked-by: Izhar Ameer Shaikh <izhar.ameer.shaikh@amd.com>

---
+++ lib/sw_services/xilpm_ng/src/xilpm_common/xpm_ver.h
@@ -0,0 +1,21 @@
+/******************************************************************************
+* Copyright (c) 2025 Advanced Micro Devices, Inc. All Rights Reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+#ifndef XPM_VER_H
+#define XPM_VER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**************************** Constant Definitions ****************************/
+#define XPM_MAJOR_VERSION	1U /**< Major Version of xilpm_ng */
+#define XPM_MINOR_VERSION	0U /**< Minor version of xilpm_ng */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* XPM_VER_H */
