22f298b88bed678857ed21556f64d5eca652532c
Sai Priya M <SaiPriya.Muthoju@amd.com>
2025-08-28 14:59:41 +0530

    canfd: Fix GCC warnings and update driver version
    Remove extraneous parentheses in equality comparison in xcanfd_intr.c
    to fix [-Wparentheses-equality] warning.
    
    Signed-off-by: Sai Priya M <SaiPriya.Muthoju@amd.com>
    Acked-by: Srinivas Goud <srinivas.goud@amd.com>

--- XilinxProcessorIPLib/drivers/canfd/src/xcanfd_intr.c
+++ XilinxProcessorIPLib/drivers/canfd/src/xcanfd_intr.c
@@ -276,7 +276,7 @@ void XCanFd_InterruptDisable_RxBuffFull(XCanFd *InstancePtr,
 	Xil_AssertVoid(InstancePtr != NULL);
 	Xil_AssertVoid(InstancePtr->IsReady == XIL_COMPONENT_IS_READY);
 
-	if ((RxBuffNumber == (u32)0)) {
+	if (RxBuffNumber == (u32)0) {
 		IntrValue =
 			XCanFd_ReadReg(InstancePtr->CanFdConfig.BaseAddress,
 				XCANFD_RXBFLL1_OFFSET);
