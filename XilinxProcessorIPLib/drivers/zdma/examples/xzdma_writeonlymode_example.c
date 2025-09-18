d7b28ed790d01681c8b7a4e6f76ed0b36dcf8f18
Abin Joseph <abin.joseph@amd.com>
2025-09-18 12:39:30 +0530

    zdma: Add PM support
    Add Power Management (PM) Implements node request/release and reset
    handling for better resource management.Includes new Stop API and
    updates example usage for proper cleanup.
    
    Signed-off-by: Abin Joseph <abin.joseph@amd.com>
    Signed-off-by: Golla Nagendra <nagendra.golla@amd.com>
    
    Acked-for-series: Harini Katakam <harini.katakam@amd.com>

--- XilinxProcessorIPLib/drivers/zdma/examples/xzdma_writeonlymode_example.c
+++ XilinxProcessorIPLib/drivers/zdma/examples/xzdma_writeonlymode_example.c
@@ -37,6 +37,7 @@
 *                        Changes were done for other cleanups and also to
 *                        ensure that the DMA is reset before the program exits.
 * 1.16  sa     09/29/22  Fix infinite loops in the example.
+* 1.20	aj     06/16/25  Add Stop API for the example
 * </pre>
 *
 ******************************************************************************/
@@ -298,6 +299,7 @@ int XZDma_WriteOnlyExample(UINTPTR BaseAddress)
 
 	/* Reset the DMA to remove all configurations done in this example  */
 	XZDma_Reset(&ZDma);
+	XZDma_Stop(&ZDma);
 
 	return XST_SUCCESS;
 
