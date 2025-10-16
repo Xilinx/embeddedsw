85aeedece3b5115a65a9df213ae9450fa57fb9c3
Buddhabhatti, Jay <jay.buddhabhatti@amd.com>
2025-10-16 00:34:05 -0700

    sw_services: xilpm_ng: Remove unused xpm_device_idle files
    Remove xpm_device_idle.c and xpm_device_idle.h as they are no longer
    needed in the codebase. Also remove hardcoded compilation macros for
    addresses, instead use addresses passed from topology CDO.
    
    Signed-off-by: Jay Buddhabhatti <jay.buddhabhatti@amd.com>
    
    Acked-for-series: Ronak Jain <ronak.jain@amd.com>

--- lib/sw_services/xilpm_ng/src/xilpm_runtime/src/xpm_runtime_reset.c
+++ lib/sw_services/xilpm_ng/src/xilpm_runtime/src/xpm_runtime_reset.c
@@ -47,25 +47,17 @@ static XStatus AdmaResetAssert(const XPm_ResetNode *Rst)
 {
 	XStatus Status = XST_FAILURE;
 	const XPm_Device *Device;
+	const u32 Mask = BITNMASK(Rst->Shift, Rst->Width);
+	const u32 ControlReg = Rst->Node.BaseAddress;
 
 	Device = XPmDevice_GetById(PM_DEV_ADMA_0);
 	if (NULL == Device) {
 		goto done;
 	}
 
-#if (defined(XILPM_ZDMA_0) || defined(XILPM_ZDMA_1) || defined(XILPM_ZDMA_2) || \
-	defined(XILPM_ZDMA_3) || defined(XILPM_ZDMA_4) || defined(XILPM_ZDMA_5) || \
-	defined(XILPM_ZDMA_6) || defined(XILPM_ZDMA_7))
-	(void)Rst;
-	Status = NodeZdmaIdle(0U, Device->Node.BaseAddress);
-#else
-	const u32 Mask = BITNMASK(Rst->Shift, Rst->Width);
-	const u32 ControlReg = Rst->Node.BaseAddress;
-
 	XPm_RMW32(ControlReg, Mask, Mask);
 
 	Status = XST_SUCCESS;
-#endif
 
 done:
 	return Status;
