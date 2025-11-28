9c6eb3109560d8ef0c46581369d6a6c1e10efc79
Ippili Vaikunta Rao <ippilivaikunta.rao@amd.com>
2025-11-28 14:56:34 +0530

    mipicsiss : Adding CPHY CSI-2 RXSS Design
    Adding Support for CPHY based MIPI CSI-2 RXSS Example design application for VEK385
    
    Signed-off-by: Ippili Vaikunta Rao <ippilivaikunta.rao@amd.com>

---
+++ XilinxProcessorIPLib/drivers/mipicsiss/examples/xmipi_vek385_cphy_example/platform_config.h
@@ -0,0 +1,13 @@
+/******************************************************************************
+
+Copyright (C) 2018 - 2020 Xilinx, Inc. All rights reserved.
+Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+SPDX-License-Identifier: MIT
+******************************************************************************/
+
+#ifndef __PLATFORM_CONFIG_H_
+#define __PLATFORM_CONFIG_H_
+
+#define STDOUT_IS_PSU_UART
+#define UART_DEVICE_ID 0
+#endif
