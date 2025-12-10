b7b6a92294a3646d6dc60881dd8ddb84b3fe851f
Kopparapu,Mounika <Mounika.Kopparapu@amd.com>
2025-12-10 22:14:07 +0530

    Revert "sw_services: add error code print returned from IsAddrRangeValid api"
    This reverts commit 4b473cbeaec08a3fc6fff9f531930366c47d0947.

--- lib/sw_services/xilplmi/src/versal_net/server/xplmi_plat.h
+++ lib/sw_services/xilplmi/src/versal_net/server/xplmi_plat.h
@@ -579,13 +579,6 @@ typedef XStatus (*XPlmi_IsAddrRangeValid_t)(u32 SubsystemId, u64 Addr, u64 Size)
 /**< Global address range validation handler */
 extern XPlmi_IsAddrRangeValid_t XPlmi_IsAddrRangeValid;
 
-#ifdef PLM_VERIFY_ADDR_RANGE_DEBUG_PRINT
-#define VERIFY_ADDR_DEBUG_PRINT(error) \
-	xil_printf("VAR_ERR: 0x%x\n\r", error);
-#else
-#define VERIFY_ADDR_DEBUG_PRINT(error)	/* do nothing, if debug print is disabled */
-#endif
-
 /******************************************************************************/
 /**
  *
@@ -612,7 +605,6 @@ extern XPlmi_IsAddrRangeValid_t XPlmi_IsAddrRangeValid;
 		StatusVar = XPlmi_IsAddrRangeValid(SubsysId, StartAddr, Len); \
 		RedundantStatus = XPlmi_IsAddrRangeValid(SubsysId, StartAddr, Len); \
 		if ((StatusVar != XST_SUCCESS) || (RedundantStatus != XST_SUCCESS)) { \
-			VERIFY_ADDR_DEBUG_PRINT(StatusVar); \
 			StatusVar = ErrorCode; \
 			goto Label; \
 		} \
@@ -622,7 +614,6 @@ extern XPlmi_IsAddrRangeValid_t XPlmi_IsAddrRangeValid;
 	{ \
 		StatusVar = XPlmi_IsAddrRangeValid(SubsysId, StartAddr, Len); \
 		if (StatusVar != XST_SUCCESS) { \
-			VERIFY_ADDR_DEBUG_PRINT(StatusVar); \
 			StatusVar = ErrorCode; \
 			goto Label; \
 		} \
