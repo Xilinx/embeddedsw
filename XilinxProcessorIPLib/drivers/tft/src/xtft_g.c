fd142d9aa4645a81cd2a22b82f45acdd8fc5a062
Vasantha Likitha T <vasanthalikitha.tandra@amd.com>
2026-01-30 22:10:42 +0530

    tft: Fix codespell errors
    Corrected spelling errors identified by codespell.
    This update only fixes spelling issues and does not change any functionality.
    
    Signed-off-by: Vasantha Likitha T <vasanthalikitha.tandra@amd.com>
    
    Acked-for-series: Shubhrajyoti Datta <shubhrajyoti.datta@amd.com>

--- XilinxProcessorIPLib/drivers/tft/src/xtft_g.c
+++ XilinxProcessorIPLib/drivers/tft/src/xtft_g.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 * Copyright (C) 2008 - 2022 Xilinx, Inc.  All rights reserved.
-* Copyright (C) 2023 Advanced Micro Devices, Inc. All Rights Reserved.
+* Copyright (C) 2023 - 2026 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
@@ -22,7 +22,9 @@
 * 1.00a  sg    03/24/08  First release
 * 4.00a  bss   01/25/13  Removed the XPAR_TFT_0_DCR_SPLB_SLAVE_IF and
 *			 XPAR_TFT_0_DCR_BASEADDR as AXI TFT controller
-*			 doesnot support them
+*			 does not support them
+* 6.8    vlt   01/27/26  Fixed codespell issues
+*
 * </pre>
 *
 ****************************************************************************/
