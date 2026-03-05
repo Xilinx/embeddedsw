cd339ceec2fabdcb29a726ce06ba1b61d901e9e5
Ben Levinsky <ben.levinsky@amd.com>
2026-03-05 13:25:31 -0800

    ThirdParty: sw_services: libmetal: Remove dead code
    Remove now unused stale copy of Libmetal library.
    
    Signed-off-by: Ben Levinsky <ben.levinsky@amd.com>
    
    Acked-by: Tanmay Shah <tanmay.shah@amd.com>

--- ThirdParty/sw_services/libmetal/src/libmetal/cmake/platforms/cross-freertos-gcc.cmake
+++
@@ -1,5 +0,0 @@
-# cmake 3.3.2 does not know CMAKE_SYSTEM_NAME=FreeRTOS, we set it to Generic
-include (cross-generic-gcc)
-string (TOLOWER "FreeRTOS"                PROJECT_SYSTEM)
-string (TOUPPER "FreeRTOS"                PROJECT_SYSTEM_UPPER)
-
--- ThirdParty/sw_services/libmetal/src/libmetal/cmake/platforms/zynqmp-linux.cmake
+++
@@ -1,4 +0,0 @@
-set (CMAKE_SYSTEM_PROCESSOR "aarch64"            CACHE STRING "")
-set (CROSS_PREFIX           "aarch64-linux-gnu-" CACHE STRING "")
-include (cross-linux-gcc)
-
