6c5f36d848db50c746efa3e4ab2da2bf55c91cab
Akhil Raghu Menon <akhil.raghu.menon@amd.com>
2026-04-02 19:57:00 -0700

    mmidp: driver: Add HPD interrupt infrastructure
    Add HpdHotUnplugHandler typedef, struct fields, and registration API.
    Fix XMmiDp_HpdInterruptHandler to dispatch HOT_UNPLUG (bit 2) and
    simplify dispatch to test status bits directly against register masks.
    
    Re-enable HPD interrupts after handling:
    - hotplug enables IRQ+UNPLUG
    - hotunplug enables IRQ+PLUG+UNPLUG.
    
    Add GeneralInterruptDisable and HpdInterruptDisable APIs mirroring
    Add XMmiDp_GeneralInterruptHandler as top-level ISR dispatching HPD
    events and logging FIFO events.
    
    Fix XMmiDp_SdpStatusInterruptEnable writing to
    XMMIDP_HPD_INTERRUPT_ENABLE instead of XMMIDP_SDP_STATUS_EN.
    
    Signed-off-by: Akhil Raghu Menon <akhil.raghu.menon@amd.com>
    
    Acked-for-series: Varunkumar Allagadapa <varunkumar.allagadapa@amd.com>

--- XilinxProcessorIPLib/drivers/mmidp/src/xmmidp_intr.c
+++ XilinxProcessorIPLib/drivers/mmidp/src/xmmidp_intr.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (C) 2025 Advanced Micro Devices, Inc.  All rights reserved.
+* Copyright (c) 2025 - 2026 Advanced Micro Devices, Inc. All Rights Reserved.
 * SPDX-License-Identifier: MIT
 *******************************************************************************/
 
@@ -41,7 +41,7 @@
 *******************************************************************************/
 void XMmiDp_ClearGeneralHpdEvent(XMmiDp *InstancePtr)
 {
-	u32 RegVal = 0;;
+	u32 RegVal = 0;
 
 	RegVal = XMmiDp_ReadReg(InstancePtr->Config.BaseAddr,
 				XMMIDP_GEN_INT0);
@@ -65,7 +65,7 @@ void XMmiDp_ClearGeneralHpdEvent(XMmiDp *InstancePtr)
 *******************************************************************************/
 void XMmiDp_GeneralInterruptEnable(XMmiDp *InstancePtr, u32 Mask)
 {
-	u32 RegVal = 0;;
+	u32 RegVal = 0;
 
 	RegVal = XMmiDp_ReadReg(InstancePtr->Config.BaseAddr,
 				XMMIDP_GEN_INT_ENABLE0);
@@ -89,11 +89,59 @@ void XMmiDp_GeneralInterruptEnable(XMmiDp *InstancePtr, u32 Mask)
 *******************************************************************************/
 void XMmiDp_HpdInterruptEnable(XMmiDp *InstancePtr, u32 Mask)
 {
-	u32 RegVal = 0;;
+	u32 RegVal = 0;
 
 	RegVal = XMmiDp_ReadReg(InstancePtr->Config.BaseAddr,
 				XMMIDP_HPD_INTERRUPT_ENABLE);
 	RegVal |= Mask;
+
+	XMmiDp_WriteReg(InstancePtr->Config.BaseAddr,
+			XMMIDP_HPD_INTERRUPT_ENABLE, RegVal);
+}
+
+/******************************************************************************/
+/**
+ * This function disables XMMIDP_GENERAL_INTERRUPT_0 bits based on the mask
+ *
+ * @param       InstancePtr is a pointer to the XMmiDp instance.
+ * @param       Mask specifies the interrupt bits to disable
+ *
+ * @return	None
+ *
+ * @note        None.
+ *
+*******************************************************************************/
+void XMmiDp_GeneralInterruptDisable(XMmiDp *InstancePtr, u32 Mask)
+{
+	u32 RegVal;
+
+	RegVal = XMmiDp_ReadReg(InstancePtr->Config.BaseAddr,
+				XMMIDP_GEN_INT_ENABLE0);
+	RegVal &= ~Mask;
+
+	XMmiDp_WriteReg(InstancePtr->Config.BaseAddr,
+			XMMIDP_GEN_INT_ENABLE0, RegVal);
+}
+
+/******************************************************************************/
+/**
+ * This function disables XMMIDP_HPD_INTERRUPT_ENABLE bits based on the mask
+ *
+ * @param       InstancePtr is a pointer to the XMmiDp instance.
+ * @param       Mask specifies the interrupt bits to disable
+ *
+ * @return	None
+ *
+ * @note        None.
+ *
+*******************************************************************************/
+void XMmiDp_HpdInterruptDisable(XMmiDp *InstancePtr, u32 Mask)
+{
+	u32 RegVal;
+
+	RegVal = XMmiDp_ReadReg(InstancePtr->Config.BaseAddr,
+				XMMIDP_HPD_INTERRUPT_ENABLE);
+	RegVal &= ~Mask;
 
 	XMmiDp_WriteReg(InstancePtr->Config.BaseAddr,
 			XMMIDP_HPD_INTERRUPT_ENABLE, RegVal);
@@ -119,8 +167,10 @@ void XMmiDp_SetHpdIrqHandler(XMmiDp *InstancePtr,
 {
 	/* Verify arguments. */
 	Xil_AssertVoid(InstancePtr != NULL);
-	Xil_AssertVoid(CallbackFunc != NULL);
-	Xil_AssertVoid(CallbackRef != NULL);
+	if (CallbackFunc == NULL) {
+		xil_printf("[HPD] WARNING: HpdIrq callback is NULL, handler not set\r\n");
+		return;
+	}
 
 	InstancePtr->HpdIrqHandler = CallbackFunc;
 	InstancePtr->HpdIrqCallbackRef = CallbackRef;
@@ -146,11 +196,42 @@ void XMmiDp_SetHpdHotPlugHandler(XMmiDp *InstancePtr,
 {
 	/* Verify arguments. */
 	Xil_AssertVoid(InstancePtr != NULL);
-	Xil_AssertVoid(CallbackFunc != NULL);
-	Xil_AssertVoid(CallbackRef != NULL);
+	if (CallbackFunc == NULL) {
+		xil_printf("[HPD] WARNING: HotPlug callback is NULL, handler not set\r\n");
+		return;
+	}
 
 	InstancePtr->HpdHotPlugHandler = CallbackFunc;
 	InstancePtr->HpdHotPlugCallbackRef = CallbackRef;
+}
+
+/******************************************************************************/
+/**
+ * This function installs a callback function for when a hpd hot unplug
+ * interrupt occurs.
+ *
+ * @param       InstancePtr is a pointer to the XMmiDp instance.
+ * @param       CallbackFunc is the address to the callback function.
+ * @param       CallbackRef is the user data item that will be passed to the
+ *              callback function when it is invoked.
+ *
+ * @return      None.
+ *
+ * @note        None.
+ *
+*******************************************************************************/
+void XMmiDp_SetHpdHotUnplugHandler(XMmiDp *InstancePtr,
+				   XMmiDp_HpdHotUnplugHandler CallbackFunc, void *CallbackRef)
+{
+	/* Verify arguments. */
+	Xil_AssertVoid(InstancePtr != NULL);
+	if (CallbackFunc == NULL) {
+		xil_printf("[HPD] WARNING: HotUnplug callback is NULL, handler not set\r\n");
+		return;
+	}
+
+	InstancePtr->HpdHotUnplugHandler = CallbackFunc;
+	InstancePtr->HpdHotUnplugCallbackRef = CallbackRef;
 }
 
 /******************************************************************************/
@@ -170,33 +251,142 @@ void XMmiDp_SetHpdHotPlugHandler(XMmiDp *InstancePtr,
 void XMmiDp_HpdInterruptHandler(XMmiDp *InstancePtr)
 {
 	u32 IntrStatus;
-	u32 HpdIrq;
-	u32 HpdHotPlug;
-	u32 HpdStatus;
+	u32 HpdIen;
 
 	Xil_AssertVoid(InstancePtr != NULL);
 
 	IntrStatus = XMmiDp_ReadReg(InstancePtr->Config.BaseAddr,
 				    XMMIDP_HPD_STATUS0);
+	HpdIen = XMmiDp_ReadReg(InstancePtr->Config.BaseAddr,
+				XMMIDP_HPD_INTERRUPT_ENABLE);
 
+	xil_printf("[HPD-ISR] HPD_STATUS0=0x%08X", IntrStatus);
+	if (IntrStatus & XMMIDP_HPD_STATUS0_HPD_IRQ_MASK)
+		xil_printf(" IRQ");
+	if (IntrStatus & XMMIDP_HPD_STATUS0_HOT_PLUG_MASK)
+		xil_printf(" PLUG");
+	if (IntrStatus & XMMIDP_HPD_STATUS0_HOT_UNPLUG_MASK)
+		xil_printf(" UNPLUG");
+	if (IntrStatus & XMMIDP_HPD_STATUS0_UNPLUG_ERR_MASK)
+		xil_printf(" UNPLUG_ERR");
+	xil_printf(" state=%d\r\n",
+		   (IntrStatus & XMMIDP_HPD_STATUS0_STATE_MASK) >>
+		   XMMIDP_HPD_STATUS0_STATE_SHIFT);
+
+	/* Write-1-to-clear the HPD status bits */
 	XMmiDp_WriteReg(InstancePtr->Config.BaseAddr,
 			XMMIDP_HPD_STATUS0, IntrStatus);
 
-	HpdStatus = (IntrStatus & XMMIDP_HPD_STATUS0_STATUS_MASK) >>
-		    XMMIDP_HPD_STATUS0_STATUS_SHIFT;
-	HpdIrq = (IntrStatus & XMMIDP_HPD_STATUS0_HPD_IRQ_MASK) >>
-		 XMMIDP_HPD_STATUS0_HPD_IRQ_SHIFT;
-	HpdHotPlug =  (IntrStatus & XMMIDP_HPD_STATUS0_HOT_PLUG_MASK) >>
-		      XMMIDP_HPD_STATUS0_HOT_PLUG_SHIFT;
-
-	if (HpdStatus & HpdIrq) {
+	if ((IntrStatus & XMMIDP_HPD_STATUS0_HPD_IRQ_MASK) &&
+	    (InstancePtr->HpdIrqHandler != NULL))
 		InstancePtr->HpdIrqHandler(InstancePtr->HpdIrqCallbackRef);
-	}
 
-	if (HpdStatus & HpdHotPlug) {
+	if ((IntrStatus & XMMIDP_HPD_STATUS0_HOT_PLUG_MASK) &&
+	    (InstancePtr->HpdHotPlugHandler != NULL))
 		InstancePtr->HpdHotPlugHandler(InstancePtr->HpdHotPlugCallbackRef);
-	}
 
+	if ((IntrStatus & XMMIDP_HPD_STATUS0_HOT_UNPLUG_MASK) &&
+	    (InstancePtr->HpdHotUnplugHandler != NULL))
+		InstancePtr->HpdHotUnplugHandler(InstancePtr->HpdHotUnplugCallbackRef);
+
+	/*
+	 * Re-enable HPD interrupts after handling.
+	 * After hotplug:   enable IRQ + UNPLUG (detect disconnect while connected)
+	 * After hotunplug: enable IRQ + PLUG + UNPLUG (detect reconnect)
+	 * After IRQ only:  preserve existing enable bits
+	 */
+	if (IntrStatus & XMMIDP_HPD_STATUS0_HOT_PLUG_MASK)
+		HpdIen |= XMMIDP_HPD_IRQ_EN_MASK |
+			  XMMIDP_HPD_UNPLUG_EN_MASK;
+
+	if (IntrStatus & XMMIDP_HPD_STATUS0_HOT_UNPLUG_MASK)
+		HpdIen |= XMMIDP_HPD_IRQ_EN_MASK |
+			  XMMIDP_HPD_PLUG_EN_MASK |
+			  XMMIDP_HPD_UNPLUG_EN_MASK;
+
+	XMmiDp_WriteReg(InstancePtr->Config.BaseAddr,
+			XMMIDP_HPD_INTERRUPT_ENABLE, HpdIen);
+
+}
+
+/******************************************************************************/
+/**
+ * This function is the top-level general interrupt handler for the XMmiDp
+ * driver. It reads the GENERAL_INTERRUPT register, dispatches HPD events
+ * to XMmiDp_HpdInterruptHandler, and clears all pending event bits.
+ *
+ * @param       InstancePtr is a pointer to the XMmiDp instance.
+ *
+ * @return      None.
+ *
+ * @note        None.
+ *
+*******************************************************************************/
+void XMmiDp_GeneralInterruptHandler(XMmiDp *InstancePtr)
+{
+	u32 GenIntrStatus;
+	u32 GenIntrEnable;
+	u32 ActiveEvents;
+
+	Xil_AssertVoid(InstancePtr != NULL);
+
+	GenIntrStatus = XMmiDp_ReadReg(InstancePtr->Config.BaseAddr,
+				       XMMIDP_GEN_INT0);
+	GenIntrEnable = XMmiDp_ReadReg(InstancePtr->Config.BaseAddr,
+				       XMMIDP_GEN_INT_ENABLE0);
+
+	ActiveEvents = GenIntrStatus & GenIntrEnable;
+	if (!ActiveEvents)
+		return;
+
+	/* Write-1-to-clear all active event bits */
+	XMmiDp_WriteReg(InstancePtr->Config.BaseAddr,
+			XMMIDP_GEN_INT0, ActiveEvents);
+
+	if (ActiveEvents & XMMIDP_GEN_INT0_HPD_EVENT_MASK)
+		XMmiDp_HpdInterruptHandler(InstancePtr);
+
+	if (ActiveEvents & XMMIDP_GEN_INT0_VID_FIFO_OVRFLW_STREAM0_MASK)
+		xil_printf("[ISR] Video FIFO overflow stream 0\r\n");
+
+	if (ActiveEvents & XMMIDP_GEN_INT0_VID_FIFO_UNDRFLW_STREAM0_MASK)
+		xil_printf("[ISR] Video FIFO underflow stream 0\r\n");
+
+	if (ActiveEvents & XMMIDP_GEN_INT0_AUD_FIFO_OVRFLW_STREAM0_MASK)
+		xil_printf("[ISR] Audio FIFO overflow stream 0\r\n");
+
+	if (ActiveEvents & XMMIDP_GEN_INT0_VID_FIFO_OVRFLW_STREAM1_MASK)
+		xil_printf("[ISR] Video FIFO overflow stream 1\r\n");
+
+	if (ActiveEvents & XMMIDP_GEN_INT0_VID_FIFO_UNDRFLW_STREAM1_MASK)
+		xil_printf("[ISR] Video FIFO underflow stream 1\r\n");
+
+	if (ActiveEvents & XMMIDP_GEN_INT0_AUD_FIFO_OVRFLW_STREAM1_MASK)
+		xil_printf("[ISR] Audio FIFO overflow stream 1\r\n");
+
+	if (ActiveEvents & XMMIDP_GEN_INT0_VID_FIFO_OVRFLW_STREAM2_MASK)
+		xil_printf("[ISR] Video FIFO overflow stream 2\r\n");
+
+	if (ActiveEvents & XMMIDP_GEN_INT0_VID_FIFO_UNDRFLW_STREAM2_MASK)
+		xil_printf("[ISR] Video FIFO underflow stream 2\r\n");
+
+	if (ActiveEvents & XMMIDP_GEN_INT0_AUD_FIFO_OVRFLW_STREAM2_MASK)
+		xil_printf("[ISR] Audio FIFO overflow stream 2\r\n");
+
+	if (ActiveEvents & XMMIDP_GEN_INT0_VID_FIFO_OVRFLW_STREAM3_MASK)
+		xil_printf("[ISR] Video FIFO overflow stream 3\r\n");
+
+	if (ActiveEvents & XMMIDP_GEN_INT0_VID_FIFO_UNDRFLW_STREAM3_MASK)
+		xil_printf("[ISR] Video FIFO underflow stream 3\r\n");
+
+	if (ActiveEvents & XMMIDP_GEN_INT0_AUD_FIFO_OVRFLW_STREAM3_MASK)
+		xil_printf("[ISR] Audio FIFO overflow stream 3\r\n");
+
+	if (ActiveEvents & XMMIDP_GEN_INT0_DSC_EVENT_MASK)
+		xil_printf("[ISR] DSC event\r\n");
+
+	if (ActiveEvents & XMMIDP_GEN_INT0_PM_EVENT_MASK)
+		xil_printf("[ISR] PM event\r\n");
 }
 
 /******************************************************************************/
@@ -213,13 +403,13 @@ void XMmiDp_HpdInterruptHandler(XMmiDp *InstancePtr)
 *******************************************************************************/
 void XMmiDp_SdpStatusInterruptEnable(XMmiDp *InstancePtr, u32 Mask)
 {
-	u32 RegVal = 0;;
+	u32 RegVal;
 
 	RegVal = XMmiDp_ReadReg(InstancePtr->Config.BaseAddr,
 				XMMIDP_SDP_STATUS_EN);
 	RegVal |= Mask;
 
 	XMmiDp_WriteReg(InstancePtr->Config.BaseAddr,
-			XMMIDP_HPD_INTERRUPT_ENABLE, RegVal);
+			XMMIDP_SDP_STATUS_EN, RegVal);
 }
 /** @} */
