3dd07db762a4d93bc72efe8a1072664afebf579c
shiva pinagani <shiva.pinagani@amd.com>
2026-04-23 13:22:24 +0530

    visp_ss: remove misleading copy-paste log messages
    Remove three LOGI calls in VsiVispSsSetATM that all printed
    "after VsiVvdeviceInstanceInit" — a message copy-pasted from a
    different function. These add no diagnostic value and are actively
    misleading during debugging.
    
    Signed-off-by: shiva pinagani <Shiva.Pinagani@amd.com>
    
    Acked-for-series: Mamidala, Anil <anil.mamidala@amd.com>

--- XilinxProcessorIPLib/drivers/visp_ss/src/visp_ss/common/visp_common.c
+++ XilinxProcessorIPLib/drivers/visp_ss/src/visp_ss/common/visp_common.c
@@ -1,5 +1,5 @@
 #include <string.h>
-// Copyright (C) 2024 - 2025 Advanced Micro Devices, Inc. All Rights Reserved.
+// Copyright (C) 2024 - 2026 Advanced Micro Devices, Inc. All Rights Reserved.
 #include "visp_common.h"
 int SetATM(void)
 {
@@ -35,16 +35,13 @@ int SetATM(void)
 
 	if (packet.payload_size > MAX_ITEM)
 		return RET_OUTOFRANGE;
-	LOGI("%s   %d: after VsiVvdeviceInstanceInit ", __func__, __LINE__);
 
 	result = Send_Command(APU_2_RPU_MB_CMD_SET_ATM, &packet, packet.payload_size + payload_extra_size,
 			      dest_cpu_id, src_cpu_id);
 	if (RET_SUCCESS != result)
 		return RET_FAILURE;
-	LOGI("%s   %d: after VsiVvdeviceInstanceInit ", __func__, __LINE__);
 
 	apu_wait_for_ACK(packet.cookie, packet.payload_data);; //replace with wait_response();
-	LOGI("%s   %d: after VsiVvdeviceInstanceInit ", __func__, __LINE__);
 
 	return result;
 }
