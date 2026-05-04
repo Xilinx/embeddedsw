940a5643d25bce6eca73c58850d57056af870c24
Mounika Botcha <mounika.botcha@amd.com>
2026-05-04 17:48:46 +0530

    xilsecure: Versal_net: Update Minor version for crypto algorithm's
    Updated minor versions for AES, SHA, ECDSA, ECDH, RSA and Keyunwrap algorithms.
    
    Signed-off-by: Mounika Botcha <mounika.botcha@amd.com>
    
    Acked-for-series: Durga Challa <durga.challa@amd.com>

--- lib/sw_services/xilsecure/src/server/versal_net/xsecure_server_keyunwrapalginfo.h
+++ lib/sw_services/xilsecure/src/server/versal_net/xsecure_server_keyunwrapalginfo.h
@@ -16,6 +16,7 @@
 * Ver   Who     Date     Changes
 * ----- ------  -------- ---------------------------------------------------------------------------
 * 5.7   tbk     04/09/26 Initial Release
+*       mb      04/28/26 Updated minor version to 6.1
 *
 * </pre>
 *
@@ -37,7 +38,7 @@ extern "C" {
  */
 
 #define XSECURE_KEYUNWRAP_MODULE_MAJOR_VERSION (6U) /**< Key Unwrap module major version */
-#define XSECURE_KEYUNWRAP_MODULE_MINOR_VERSION (0U) /**< Key Unwrap module minor version */
+#define XSECURE_KEYUNWRAP_MODULE_MINOR_VERSION (1U) /**< Key Unwrap module minor version */
 
 /** @} */
 
